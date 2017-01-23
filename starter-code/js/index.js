62189e6330c984042dbc2c19f62d3495ce724c38
Peter Fantasia <peter.vittorio@gmail.com>
2017-01-23 08:49:22 +0100

    1st exercise complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,8 +2,15 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(){
+//  var totalPrice = 0;
+//  var itemUnitPrice = document.getElementsByClassName("item-unit-price");
+//  for (var i = 0; i<= itemUnitPrice.length; i++) {
+//    var UnitPrice = itemUnitPrice[i].innerHTML;
+//    var UnitPriceInt = Number(UnitPrice.substr(1));
+//    var input = Number(document.getElementsByClassName("input")[i].value);
+//    document.getElementsByClassName("item-price")[i].innerHTML = '$'+UnitPriceInt * input;
+//  }
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,11 +18,26 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+var itemUnitPrice = document.getElementsByClassName("item-unit-price");
+var N = Number(itemUnitPrice.substr(1));
+console.log(N);
+//  getPriceByProduct();
+//  document.getElementsByTagName("h2").innerText =
+//  var UnitPrice = document.getElementsByClassName("item-unit-price")[0].innerText;
+//  var UnitPriceInt = Number(UnitPrice.substr(1));
+//  var input = Number(document.getElementsByClassName("input")[0].value);
+//  document.getElementsByClassName("item-price")[0].innerHTML = '$'+UnitPriceInt * input;
 
 }
 
 function createQuantityInput(){
-
+//   var getQuant = getElementsByClassName('quantity');
+//   var createQuant1 = document.createElement('input');
+//   var createLab = document.createElement('label');
+//   var createLabT = document.createTextNode('QTY');
+//   getQuant.appendChild(createQuant1);
+//   createLab.appendChild(createLabT);
+//   document.getElementById
 }
 
 function createDeleteButton(){
@@ -45,6 +67,7 @@ window.onload = function(){
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
+
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
