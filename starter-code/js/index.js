009e8eaf6c39a16789b40974eb8c09c9f6d39386
Tahira Farid <tahirafarid@hotmail.co.uk>
2017-01-26 23:49:29 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,50 +3,82 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+  this.itemNode = document.getElementById('price').textContent;
+  document.getElementById('price').innerHTML = this.itemNode;
+  return this.itemNode;
 }
 
 function updatePriceByProduct(productPrice, index){
 
+var quantity = document.getElementById('qty');
+var defaultVal = quantity.defaultValue;
+var currentVal = quantity.value;
+return quantity.value;
 }
 
-function getTotalPrice() {
 
+function getTotalPrice() {
+  var totalPrice = document.getElementById('priceQty');
+  return totalPrice.innerHTML = getPriceByProduct() * updatePriceByProduct();
 }
 
 function createQuantityInput(){
-
+newQty = document.getElementById('qty');
+var clnQty = newQty.cloneNode(true);
 }
+//
+// function createDeleteButton(){
+// newDelete = document.getElementsByClassName('btn-delete');
+// var clnDelete = newDelete.clone(true);
+//
+// }
 
-function createDeleteButton(){
-
-}
+// createDeleteButton();
 
 function createQuantityNode(){
 
 }
-
-function createItemNode(dataType, itemData){
-
-}
-
+//
+// function createItemNode(dataType, itemData){
+//
+// }
+//
 function createNewItemRow(itemName, itemUnitPrice){
+this.itemName = itemName;
+this.itemUnitPrice = itemUnitPrice;
+var newRow = document.getElementsByTagName('DIV')[0];
+var cln = newRow.cloneNode(true);
+newRow.appendChild(cln, createQuantityInput);
+
 
 }
+//  var item2 = new createNewItemRow ("Spider rolls", 17.25);
+// createNewItemRow(item);
+
 
 function createNewItem(){
-
+ var itemNew = document;
 }
+//
+// var item1 = new updatePriceByProduct (10.00);
+// item1.showPrice();
 
+
+// window is the whole main object of JS. this function means that when everything
+// is loaded on the window to run the function below//
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  // var createItemButton = document.getElementById('new-item-create');
+  // var deleteButtons = document.getElementsByClassName('btn-delete');
+//
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //onclick is an EVENT// We are not "calling" the function getTotalPrice because we are saying
+  // to call the function only when the onclick is clicked//
+  // createItemButton.onclick = createNewItem();
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
+
+window.onload();
