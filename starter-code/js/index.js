f44537be763e29d0456f7283c2b588b34730af2d
Alejandro <ajimenez1724@gmail.com>
2017-02-19 21:59:08 -0500

    it adds prices without button

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,56 +3,118 @@
 // Use at least once getElementsByTagName
 // Use at least once getElementsByClassName
 
+// ------------------- PING --------------------
 
-function deleteItem(e){
 
+
+function getQty() {
+    var qty = document.getElementById("qty1").value;
+    document.getElementById("total-1").innerHTML = qty;
+    return qty;
 }
 
-function getPriceByProduct(itemNode){
+getQty();
 
+function getPrice() {
+    var price = parseInt(document.getElementById("priceTshirt").innerHTML);
+    document.getElementById("total-1").innerHTML = price * getQty();
 }
 
-function updatePriceByProduct(productPrice, index){
+getPrice();
 
-}
+// function getPrice(price, qty) {
+//   getPrice.call(this, price);
+//   getQty.call(this, qty);
+//     return this.price * this.qty ;
+//   }
+//
+// getPrice();
 
-function getTotalPrice() {
 
-}
 
-function createQuantityInput(){
 
-}
 
-function createDeleteButton(){
+// function getQty() {
+//     var x = document.getElementById("qty1").innerHTML;
+// }
+//
+// function getTotalPrice (price, qty) {
+//   getPrice.call(this, price);
+//   getQty.call(this, qty);
+//   this.total = function() {
+//     return this.price * this.qty;
+//   };
+// }
+//
+// function changePrice (price) {
+//   getTotalPrice.call(this, price);
+//   this.name = name;
+//   this.receiveDamage =  function(damage) {
+//    this.health -= damage;
+//  };
+//  }
 
-}
+  //   if (this.health > 0 ){
+  //     return (this.name + ' has received ' + damage + ' points of damage');
+  //   } else {
+  //     return (this.name + ' has died in act of combat');
+  //   };
+  // }
 
-function createQuantityNode(){
 
-}
+//
+// var totalToPay = document.getElementById('total-1');
+// totalToPay.modify = getTotalPrice.total;
 
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+// function deleteItem(e){
+//
+// }
+//
+// function getPriceByProduct(itemNode){
+//
+// }
+//
+// function updatePriceByProduct(productPrice, index){
+//
+// }
+//
+// // function getTotalPrice() {
+// //
+// // }
+//
+// function createQuantityInput(){
+//
+// }
+//
+// function createDeleteButton(){
+//
+// }
+//
+// function createQuantityNode(){
+//
+// }
+//
+// function createItemNode(dataType, itemData){
+//
+// }
+//
+// function createNewItemRow(itemName, itemUnitPrice){
+//
+// }
+//
+// function createNewItem(){
+//
+// }
+//
+// window.onload = function(){
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+//   var deleteButtons = document.getElementsByClassName('btn-delete');
+//
+//   calculatePriceButton.onclick = getTotalPrice;
+//   createItemButton.onclick = createNewItem;
+//
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
+// };
