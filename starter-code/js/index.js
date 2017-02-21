dd9675c5d9f5cf7bc18afe50b5ede330e592b3ae
Alice Guan <aliguan726@gmail.com>
2017-02-20 23:04:45 -0500

    Add function to add up price for all products

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,8 @@
+var getProduct = document.getElementsByClassName('productName');
+var getUnitPrice = document.getElementsByClassName('productCost');
+var getUnitQty = document.getElementsByClassName('qty');
+var getTotalProductPrice = document.getElementsByClassName('totalProductPrice');
+
 function deleteItem(e){
 
 }
@@ -8,9 +13,18 @@ function getPriceByProduct(itemNode){
 function updatePriceByProduct(productPrice, index){
 
 }
+function addTotalProductPrice(a, b) {
+  return a + b;
+}
 
 function getTotalPrice() {
-
+  var priceArray = [];
+  for(i = 0; i < getProduct.length; i++) {
+    var productPriceValue = parseInt(getTotalProductPrice[i].innerHTML);
+    priceArray.push(productPriceValue);
+  }
+  console.log(priceArray);
+  document.getElementById('spanTotal').innerHTML = priceArray.reduce(addTotalProductPrice, 0);
 }
 
 function createQuantityInput(){
@@ -38,12 +52,9 @@ function createNewItem(){
 }
 
 function calculatePrices() {
-  var getProduct = document.getElementsByClassName('productName');
-  var getUnitPrice = document.getElementsByClassName('productCost');
-  var getUnitQty = document.getElementsByClassName('qty');
-  console.log(getUnitQty);
-  for(i = 0; i < getProduct.length; i ++) {
+  for( i = 0; i < getProduct.length; i++ ) {
     var totalPrice = Number(getUnitPrice[i].innerHTML.replace(/[^0-9\.]+/g,"")) * getUnitQty[i].value;
     document.getElementsByClassName('totalProductPrice')[i].innerHTML = totalPrice;
   }
+  getTotalPrice();
 }
