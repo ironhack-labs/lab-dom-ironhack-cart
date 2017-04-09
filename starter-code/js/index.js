31ea81ac1b0729cbedb708cee66194f681810c19
Rafael Melón <info@rafaelmelon.es>
2017-04-09 19:24:56 +0200

    Primer commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,31 @@
-function deleteItem(e){
 
-}
 
-function getPriceByProduct(itemNode){
+var boxProductPrice = document.getElementById('js-price').innerHTML,
+    boxProductPrice2 = boxProductPrice.split('$').join(''),
+    boxProductPrice3 = parseInt(boxProductPrice2),
+    boxQty = document.getElementById('js-qty').value,
+    boxPriceTotal = document.getElementById('js-priceTotal');
 
-}
+var calculatePriceButton = document.getElementById('calc-prices-button');
 
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+var getTotalPrice = function(value,price) {
+  var calcPrice = "$"+ value * price,
+      innerCalcPrice = boxPriceTotal.innerHTML = calcPrice;
+  return innerCalcPrice;
 };
+
+calculatePriceButton.onclick = function() {
+  var boxQty = document.getElementById('js-qty').value;
+  getTotalPrice(boxQty,boxProductPrice3);
+};
+
+
+var deleteButtons = document.getElementsByClassName('btn-delete');
+
+function deleteItem(e){
+  e.currentTarget.parentNode.parentNode.parentNode.innerHTML = "";
+}
+
+for(var i = 0; i<deleteButtons.length ; i++){
+  deleteButtons[i].onclick = deleteItem;
+}
