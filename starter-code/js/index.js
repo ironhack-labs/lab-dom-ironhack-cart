6a586686d436b4e16739251859531b4e599100fa
Rafael Melón <info@rafaelmelon.es>
2017-04-09 20:03:23 +0200

    Show calculate product alert

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,29 +3,39 @@
 var boxProductPrice = document.getElementById('js-price').innerHTML,
     boxProductPrice2 = boxProductPrice.split('$').join(''),
     boxProductPrice3 = parseInt(boxProductPrice2),
-    boxQty = document.getElementById('js-qty').value,
     boxPriceTotal = document.getElementById('js-priceTotal');
 
 var calculatePriceButton = document.getElementById('calc-prices-button');
 
-var getTotalPrice = function(value,price) {
+function getTotalPrice(value,price) {
   var calcPrice = "$"+ value * price,
       innerCalcPrice = boxPriceTotal.innerHTML = calcPrice;
   return innerCalcPrice;
-};
+}
 
+var alertPrice = document.createElement('h1');
+document.getElementById('alert').appendChild(alertPrice);
+
+function totalPrice(total){
+  alertPrice.innerHTML = 'TOTAL PRICE '+total;
+}
+
+/*
+CALCULATE BUTTON
+*/
 calculatePriceButton.onclick = function() {
   var boxQty = document.getElementById('js-qty').value;
   getTotalPrice(boxQty,boxProductPrice3);
+  totalPrice(getTotalPrice(boxQty,boxProductPrice3));
 };
 
-
+/*
+DELETE BUTTON
+*/
 var deleteButtons = document.getElementsByClassName('btn-delete');
-
 function deleteItem(e){
   e.currentTarget.parentNode.parentNode.parentNode.innerHTML = "";
 }
-
 for(var i = 0; i<deleteButtons.length ; i++){
   deleteButtons[i].onclick = deleteItem;
 }
