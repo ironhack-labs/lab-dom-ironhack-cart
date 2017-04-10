6a64219ca92ae8b44bf718f9a037b8019d98d941
Sergi Garcia <sergigarciapena@gmail.com>
2017-04-10 15:07:44 +0200

    do count two prices

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,14 +4,36 @@ function deleteItem(e){
 
 function getPriceByProduct(itemNode){
 
+  var input = itemNode.getElementsByTagName('input')[0];
+  var price = itemNode.getElementsByClassName("price")[0];
+
+  return parseInt(input.value) * parseInt(price.innerHTML);
 }
 
 function updatePriceByProduct(productPrice, index){
 
+  var itemNodes = document.getElementsByClassName("item");
+  var price = itemNodes[index].getElementsByClassName('final-product-price')[0];
+  price.innerHTML = productPrice.toString();
 }
 
 function getTotalPrice() {
 
+  var itemNodes = document.getElementsByClassName("item");
+  var totalPrice = 0;
+
+  for(var i=0; i < itemNodes.length; i++){
+
+      var price = getPriceByProduct(itemNodes[i]);
+      console.log(price);
+      updatePriceByProduct(price, i);
+
+      totalPrice += price;
+
+    }
+
+  var displayTotalPrice = document.getElementById('display-total-price');
+  displayTotalPrice.innerHTML = totalPrice.toString();
 }
 
 function createQuantityInput(){
@@ -40,13 +62,14 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
+  //console.log(calculatePriceButton);
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // createItemButton.onclick = createNewItem;
+  //
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
