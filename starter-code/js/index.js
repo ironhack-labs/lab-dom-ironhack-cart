ee20224a052f6017ebce26493ecd08fe779bfcb8
Manuel Valle <vallerem17@gmail.com>
2017-05-30 21:21:38 +0200

    Done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
 
   // --[!!DISPLAY] -- Deletes the row
 
-  $(".btn-delete").on("click", function(event) {
+  // written as a function tu "unbind" it later on the code. This way the event handler is not added several times on .btn-delete
+
+  var deletenn = function() {
     $(this).parent().parent().remove();
-  });
+    console.log("lol");
+  };
+
+  $(".btn-delete").on("click",deletenn);
 
   // --[DISPLAY] --  Shows the total price and multiplies each product price for every unit.
 
@@ -29,9 +34,10 @@ $(document).ready(function() {
   $(".btn-primary").click(function(event) {
     var newProduct = ('<div class=\"row product\"><div class=\"col-xs-2\"><span class=\"productName\">' + $("#product-name").val() + '</span></div><div class=\"col-xs-2\"><span class=\"product-price\">' + '$' + $("#product-price").val() + '</span></div><div class=\"col-xs-4\"><label class=\"quantity\">QTY</label><input type=\"text\" name=\"\" class="mainInput"></div><div class=\"col-xs-2\"><span class=\"totalPrice\">$0.00</span></div><div class="col-xs-2 "><button class="btn btn-delete">Delete</button></div></div>');
     $(".all-products").append(newProduct);
-    $(".btn-delete").on("click", function(event) {
-      $(this).parent().parent().remove();
-    });
+    $(".btn-delete").unbind('click'); // AAAARGHHH
+    $(".btn-delete").on("click",deletenn);
+
+
   });
 
 
