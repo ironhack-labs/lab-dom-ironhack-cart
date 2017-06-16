39e64f0a250fd72b43bbfd4ff6379508a8ca635a
sara <sara-cg@github.com>
2017-06-16 19:27:53 +0200

    first copy

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,65 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
 
-}
+  //var itemPrice = document.getElementsByClassName('price')[0].innerHTML;
+  //var quantity = parseInt(document.getElementsByTagName('input')[0].value);
+  //var totalItemPrice = itemPrice * quantity;
+  //document.getElementsByClassName('total-price')[0].innerHTML = totalItemPrice;
 
-function createQuantityInput(){
+  for (var i = 0; i < document.getElementsByClassName('price').length; i++) {
+    var itemPrice = document.getElementsByClassName('price');
+    var quantity = document.getElementsByTagName('input');
+    var totalItemPrice[i] = itemPrice[i].innerHTML * parseInt(quantity[i].value);
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
+    document.getElementsByClassName('total-price')[i].innerHTML = totalItemPrice[i];
+  }
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createQuantityInput() {
 
 }
 
-function createNewItem(){
+function createDeleteButton() {
 
 }
 
-window.onload = function(){
+function createQuantityNode() {
+
+}
+
+function createItemNode(dataType, itemData) {
+
+}
+
+function createNewItemRow(itemName, itemUnitPrice) {
+
+}
+
+function createNewItem() {
+
+}
+
+window.onload = function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
