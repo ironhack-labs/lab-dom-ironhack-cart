4fe61de3891d41a5a74cced321fdc2568e601742
Fran Bosquet <frbosquet@gmail.com>
2017-06-16 17:25:53 +0200

    Firs iteration done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,19 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var unitPriceSpans = document.getElementsByClassName("unitPrice");
+  var quantitySpans = document.getElementsByClassName("quantity");
+  var totalPriceSpans = document.getElementsByClassName("totalPrice");
 
+  for(var i = 0; i < unitPriceSpans.length ; i++){
+    var units = parseInt( unitPriceSpans[0].innerHTML.replace(/[^0-9\.]+/g,"") );
+    var qtyText = quantitySpans[0].value.replace(/[^0-9\.]+/g,"");
+    var qty = (qtyText === "") ? 0 : parseInt(qtyText);
+
+    if(qty === 0){
+      quantitySpans[i].value = "";
+    }else totalPriceSpans[i].innerHTML = "$" + (qty*units).toFixed(2);
+  }
 }
 
 function createQuantityInput(){
@@ -44,9 +56,9 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // createItemButton.onclick = createNewItem;
+  //
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
