85e737f0cf160c572681a2da39ab78eff9d232ce
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-06-17 12:45:20 +0200

    b

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,14 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var quantities = document.getElementsByClassName("quantities");
+  var prices = document.getElementsByClassName("costs");
+  var totalPrice = 0;
+  for (var i=0; i<quantities.length; i++) {
+     var sumProduct = prices[i].innerHTML * quantities[i].value;
+    totalPrice += sumProduct;
+  }
+  document.getElementById("showTotal").innerHTML = totalPrice;
 }
 
 function createQuantityInput(){
@@ -39,14 +46,16 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('btn-sucess');
+  var calculatePriceButton = document.getElementById('btn-success');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+var totalPrice = document.createElement('total');
