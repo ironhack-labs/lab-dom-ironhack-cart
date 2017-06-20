cba589fb496a015e63101b3944b65a401a53e933
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-06-20 11:41:15 +0200

    Iteration 3 done, 4 & 5 WIP, bug: need to account for decimals when calculating

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,13 +1,14 @@
 
   var calculatePriceButton = document.getElementById("calc-button");
   //var createItemButton = document.getElementById('new-item-create');
-  //var deleteButtons = document.getElementsByClassName('btn-delete');
+  var deleteButtons = document.getElementsByClassName("btn-delete");
+  var productsSection = document.getElementById("main-product-list");
+  var productRow = document.getElementsByClassName("info-container");
 
   //calculatePriceButton.onclick = getTotalPrice;
   //createItemButton.onclick = createNewItem;
 
-  //for(var i = 0; i<deleteButtons.length ; i++){
-    //deleteButtons[i].onclick = deleteItem;
+
   //}
 
 function getUnitPrice(productPriceIndex){
@@ -42,15 +43,14 @@ calculatePriceButton.onclick = function(){
     getTotalPrice();
 };
 
-
-
-
-
-    
 /*
 function deleteItem(e){
-    
+  for(var i = 0; i<deleteButtons.length ; i++){
+      if(e.currentTarget === true){
+        deleteButtons.onclick = productsSection.removeChild(productRow[i]);
+      }
 }
+
 
 
 function updatePriceByProduct(productPrice, index){
