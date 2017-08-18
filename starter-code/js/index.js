efc50c091927dc3ebb07f93d3616a456ff40c884
Mark Allen <mark@traversify.com>
2017-08-19 00:19:48 +0200

    1-4 done, half 5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,55 +1,3 @@
-// function deleteItem(e){
-// }
-
-// function getPriceByProduct(itemNode){
-	
-// }
-
-// function updatePriceByProduct(productPrice, index){
-
-// }
-
-// function getTotalPrice() {
-
-// }
-
-// function createQuantityInput(){
-
-// }
-
-// function createDeleteButton(){
-
-// }
-
-// function createQuantityNode(){
-
-// }
-
-// function createItemNode(dataType, itemData){
-
-// }
-
-// function createNewItemRow(itemName, itemUnitPrice){
-
-// }
-
-// function createNewItem(){
-
-// }
-
-// window.onload = function(){
-//   var calculatePriceButton = document.getElementById('calc-prices-button');
-//   var createItemButton = document.getElementById('new-item-create');
-//   var deleteButtons = document.getElementsByClassName('btn-delete');
-
-//   calculatePriceButton.onclick = getTotalPrice;
-//   createItemButton.onclick = createNewItem;
-
-//   for(var i = 0; i<deleteButtons.length ; i++){
-//     deleteButtons[i].onclick = deleteItem;
-//   }
-// };
-
 function getPriceFromPage(item) {
 	var item = document.getElementById(item);
 	var price = item.querySelector(".product-cost p");
@@ -74,7 +22,8 @@ function updatePrice(item, price) {
 }
 window.onload = function() {
 	var priceButton = document.getElementById('price-calculator')
-	var deleteButtons = document.getElementsByClassName('btn-delete')
+	var deleteButtons = document.getElementsByClassName('btn-delete');
+	var createButton = document.getElementById('create-button');
 	
 	// calculate prices
 
@@ -83,6 +32,7 @@ window.onload = function() {
 		totalPrice = 0
 		for (i = 0; i < shopItems.length; i++) {
 			processItem = shopItems[i].querySelector("p").innerHTML.toLowerCase()
+			console.log(processItem)
 			var newPrice = updatePrice(processItem, getTotalPrice(processItem))
 			totalPrice += newPrice
 		} 
@@ -92,10 +42,28 @@ window.onload = function() {
 	
 	for (i = 0; i < deleteButtons.length; i++) {
 		deleteButtons[i].onclick = function() {
-			var body = document.querySelector("body")
-			body.removeChild(this.parentElement.parentElement)
+			var products = document.querySelector("#products")
+			products.removeChild(this.parentElement.parentElement)
 		}
 	}
+
+
+	createButton.onclick = function() {
+		var productName = document.getElementById('create-name-text').value;
+		var productPrice = document.getElementById('create-name-price').value || 0;
+		var productExample = document.getElementById('ironbubble-head');
+		newProduct = productExample.cloneNode(true);
+		var productDiv = document.getElementById('products');
+		productDiv.appendChild(newProduct)
+		var shopItems = document.getElementsByClassName('product-name');
+		shopItems[shopItems.length - 1].innerHTML = productName;
+		var shopItemsPrices = document.getElementsByClassName('product-cost');
+		shopItemsPrices[shopItemsPrices.length - 1].innerHTML = "$" + productPrice;
+		var productList = document.getElementsByClassName('wrapper');
+		(productList[productList.length - 1].removeAttribute('id'));
+		(productList[productList.length - 1].setAttribute('id', productName.toLowerCase()));
+		console.log(productList[productList.length - 1]);
+		dispatchEvent(new Event('load'));
+
+	}
 }
-
-
