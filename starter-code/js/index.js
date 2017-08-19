ff2a1b15b32f55172f5356074222594190da07b2
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-08-19 17:24:59 +0200

    hecho hasta total price sin seguir la documentacion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,30 @@
-function deleteItem(e){
 
+function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
+//  var button = document.getElementById("calculate");
+  var prices = document.getElementsByClassName("item-price");
+  var total = document.getElementsByClassName("total-price");
+  var items = document.getElementsByClassName("quantity");
 
-}
+  var a = Number(prices[itemNode].textContent.slice(1,prices[itemNode].textContent.length));
+  var b = items[itemNode].value;
+
+  total[itemNode].textContent = "$" + (a*b).toFixed(2);
+  }
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
-
+  var a = document.getElementsByClassName("total-price");
+  var final = document.getElementsByClassName("final-price");
+  var valora = Number(a[0].textContent.slice(1,a[0].length));
+  var valorb = Number(a[1].textContent.slice(1,a[1].length));
+  var total = valora + valorb;
+  final[0].textContent = "Total Price: $" + total.toFixed(2);
 }
 
 function createQuantityInput(){
@@ -38,6 +51,13 @@ function createNewItem(){
 
 }
 
+document.getElementById("calculate").onclick = function(){
+  getPriceByProduct(0);
+  getPriceByProduct(1);
+  getTotalPrice();
+};
+
+/*
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
@@ -50,3 +70,4 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+*/
