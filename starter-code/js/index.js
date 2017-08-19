533f15650b4bd87f1f168c9dbb99db2afe54594c
y4izus <yaiza.garcia.mm@gmail.com>
2017-08-19 20:18:52 +0200

    Iteration 3: Calculating the total price of all products DONE

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,19 +6,27 @@ function getPriceByProduct(product){
   var price = Number((product.children[1].children[0].innerHTML).slice(1))
   var quantity = Number(product.children[2].children[1].value)
   var totalPrice = product.children[3].children[0]
+  updatePriceByProduct("$" + price*quantity, totalPrice)
   totalPrice.innerHTML = "$" + price*quantity
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(productPrice, totalPrice){
+  totalPrice.innerHTML = productPrice
 }
 
 function getTotalPrice() {
-  var products = document.getElementsByClassName("product")
+  var products = document.getElementsByClassName('product')
 
   for(var i=0; i<products.length; i++){
     getPriceByProduct(products[i])
   }
+
+  var productsPrices = document.getElementsByClassName('total-price')
+  for (var i=0, sumPrices=0; i<productsPrices.length; i++){
+    sumPrices += Number((productsPrices[i].innerHTML).slice(1))
+  }
+  var totalPrices = document.getElementById('final-price')
+  totalPrices.innerHTML = "$" + sumPrices
 }
 
 function createQuantityInput(){
@@ -46,9 +54,9 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var calculatePriceButton = document.getElementById('calc-prices-button')
+  var createItemButton = document.getElementById('new-item-create')
+  var deleteButtons = document.getElementsByClassName('btn-delete')
 
   calculatePriceButton.onclick = function(){
     getTotalPrice()
