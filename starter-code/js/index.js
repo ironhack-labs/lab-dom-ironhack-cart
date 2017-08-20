fb33953116092f3f793e6fa8ebbe6d686ce7bf0f
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-08-20 19:02:59 +0200

    3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,33 @@
+
+//var calcular = document.getElementById("calculate");
+
+//function calcular(){
+
+//var price = document.getElementsByClassName('price');
+  //var quantity = document.getElementsByTagName('input').value;
+
+//for(var i =0; i<price.length;i++){
+  //var totalPrice = price[i].innerHTML * quantity[i];
+//}return document.getElementsByClassName('total')[0].innerHTML = totalPrice;
+
+//}
+
+//var price = document.getElementById('price').innerHTML;
+//var quantity = document.getElementById('number').value;
+//console.log(price);
+//console.log(quantity);
+//var totalPrice = parseInt(price) * quantity;
+//return document.getElementsByClassName('total')[0].innerHTML = totalPrice;
+
+
+
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
+
+
 
 }
 
@@ -11,6 +36,8 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+
+
 
 }
 
@@ -42,6 +69,41 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+
+
+function getPrice(){
+  var price = document.getElementsByClassName('price');
+
+  var quantity= document.getElementsByTagName('input');
+
+  //var totalPrice = price * quantity;
+
+  for(var i = 0; i < price.length; i++){
+      var totalPrice = price[i].innerHTML * quantity[i].value;
+      document.getElementsByClassName('total')[i].innerHTML = totalPrice;
+  }
+}
+
+var getTotal = function(){
+  var prices = document.getElementsByClassName('total');
+  var finalPrice = 0;
+  for(var i=0; i < prices.length; i++){
+    finalPrice += parseInt(prices[i].innerHTML);
+    console.log(finalPrice);
+}
+return finalPrice;
+}
+
+  function getTotalPrice(){
+    getPrice();
+    
+
+    document.getElementById('showTotal').innerHTML = ("<h2>Total Price: " + getTotal() + "</h2>");
+
+
+
+  }
+
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
