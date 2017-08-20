71162cd27f4c7f5a1248bbbad472e75e08b0dc58
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-08-20 20:32:06 +0200

    problemas con currenttarget y getattribute

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,11 @@
 
+/*
 function deleteItem(e){
-  var clear = document.getElementById("bubble-head");
-  clear.innerHTML = "";
+  /*var clear = document.getElementById("bubble-head");
+  var clear = document.getElementsByClassName("items");
+  clear[e].innerHTML = "";
 }
-
+*/
 function getPriceByProduct(itemNode){
 //  var button = document.getElementById("calculate");
   var prices = document.getElementsByClassName("item-price");
@@ -16,9 +18,9 @@ function getPriceByProduct(itemNode){
   total[itemNode].textContent = "$" + (a*b).toFixed(2);
   }
 
-function updatePriceByProduct(productPrice, index){
+/*function updatePriceByProduct(productPrice, index){
 
-}
+}*/
 
 function getTotalPrice() {
   var a = document.getElementsByClassName("total-price");
@@ -53,16 +55,24 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
-
-document.getElementById("calculate").onclick = function(){
+/*
+document.getElementById("calc-prices-button").onclick = function(){
   getPriceByProduct(0);
   getPriceByProduct(1);
   getTotalPrice();
 };
+*/
 
+var delclick = document.getElementsByClassName("btn-delete");
+for (var j = 0; j < delclick.length; j++){
+    delclick[j].onclick = function(e){
+      console.log(j + "length: " + delclick.length);
+      console.log("Current: " + e.currentTarget.innerHTML);
+  //  var id = .getAttribute('id');
+    };
+}
 
-
-
+/*
 document.getElementsByClassName("delete")[0].onclick = function(){
   deleteItem(0);
 };
@@ -70,16 +80,23 @@ document.getElementsByClassName("delete")[0].onclick = function(){
 document.getElementsByClassName("delete")[1].onclick = function(){
   deleteItem(1);
 };
+*/
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var items = document.getElementsByClassName("items");
 
-  //calculatePriceButton.onclick = getTotalPrice();
+  calculatePriceButton.onclick = function (){
+    for (var i = 0; i < items.length; i++){
+      getPriceByProduct(i);
+      getTotalPrice();
+    }
+  };
   //createItemButton.onclick = createNewItem();
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+/*  for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
-  }
+  }*/
 };
