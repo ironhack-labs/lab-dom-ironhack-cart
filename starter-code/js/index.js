adc6e794cf0c920cd3a153bfd9505366c9b9932f
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-20 13:55:46 +0200

    Error al sacar el total de precios por producto -> NaN

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,67 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
-
+function updatePriceByProduct(productPrice, index) {
+  var a = document.getElementsByClassName('total-amount')
+  a[index].innerHTML = "$" + productPrice.toFixed(2)
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function getPriceByProduct(itemNode) {
+  var total
+  var oneItem = parseFloat(document.getElementById('price-product').innerHTML)
+  var oneQuantity = parseFloat(document.getElementById('qProduct').value)
+  
+  total = oneItem[itemNode] * oneQuantity[itemNode]
+  console.log(total)
+  // Update price product one by one
+  updatePriceByProduct(total, itemNode)
+  return total
 }
 
 function getTotalPrice() {
+  var price = document.getElementsByClassName('price')
+  var tPrice = 0
+  for(var i = 0; i < price.length; i++){
+    tPrice += getPriceByProduct(i)
+  } 
+  document.getElementsByClassName('total-price').innerHTML = "<h2>Total Price: $" + tPrice.toFixed(2)+ "</h2>"
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem() {
 
 }
 
-function createNewItem(){
+window.onload = function () {
+  var calculatePriceButton = document.getElementById('calc-prices-button')
+  var createItemButton = document.getElementById('new-item-create')
+  var deleteButtons = document.getElementsByClassName('btn-delete')
 
-}
+  calculatePriceButton.onclick = getTotalPrice
+  console.log(calculatePriceButton)
+  //createItemButton.onclick = createNewItem
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = deleteItem
   }
-};
+}
