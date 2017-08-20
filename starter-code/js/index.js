183cd06d96b0e4d190ff64fff6a4e7a74d51e156
Ander Galdeano <andergaldeano@gmail.com>
2017-08-20 15:18:56 +0200

    first itineration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,11 +2,27 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(){
+var precio = Number(document.getElementById("price1").innerHTML);
+var cantidad = Number(document.getElementById("QTY").value);
+
+var total = document.getElementById("firstTotal");
+
+var multi= precio * cantidad;
+total.innerHTML = multi;
 
 }
 
+
+
+
+
+
+/*
+
 function updatePriceByProduct(productPrice, index){
+
+
 
 }
 
@@ -36,17 +52,22 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 function createNewItem(){
 
-}
+}*/
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+var calculatePriceButton = document.getElementById('calc-prices-button');
+var createItemButton = document.getElementById('new-item-create');
+var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+/*calculatePriceButton.onclick = getTotalPrice;
+createItemButton.onclick = createNewItem;*/
+calculatePriceButton.onclick = function() {
+  getPriceByProduct();
+}
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+
+/*
+for(var i = 0; i<deleteButtons.length ; i++){
+  deleteButtons[i].onclick = deleteItem;
+}*/
 };
