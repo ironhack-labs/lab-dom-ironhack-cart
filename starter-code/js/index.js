8432d0421afbf1cda6ed2e8bbc0a8a94c8416c92
trigoporres <santiagotrigoporres@gmail.com>
2017-08-20 17:44:01 +0200

    tercera iteracion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,13 +2,19 @@ function calculatePrice (){
   var price = document.getElementsByClassName("prices");
   var unit = document.getElementsByTagName("input");
   var priceTotales = document.getElementsByClassName("priceTotal");
+  var sum = 0;
   //console.log(price, unit, priceTotales);
 
   for (var i = 0; i < price.length; i++) {
      var totalPrice = price[i].innerHTML * unit[i].value;
      //console.log(totalPrice);
      priceTotales[i].innerHTML = totalPrice;
+     console.log(sum);
+     sum = sum + parseInt(priceTotales[i].innerHTML);
+     console.log(sum);
    }
+
+  document.getElementById("sumT").innerHTML = sum;
 }
 
 
