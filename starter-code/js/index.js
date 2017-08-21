693d6bef6b9d4dabeb0e76d67a0f32c61e340f72
willjthorp <30321077+willjthorp@users.noreply.github.com>
2017-08-21 14:42:36 +0200

    Update index.js

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,29 @@
-function deleteItem(e){
-
+function deleteItem(event){
+  var body = event.currentTarget.parentNode.parentNode.parentNode;
+  var product = event.currentTarget.parentNode.parentNode;
+  body.removeChild(product);
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(i){
+  return parseFloat(document.getElementsByClassName("price")[i].innerHTML);
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(){
+  for (i = 0; i < document.getElementsByClassName("total").length; i++) {
+  document.getElementsByClassName("total")[i].innerHTML = (getPriceByProduct(i) * parseFloat(document.getElementsByTagName("input")[i].value)).toFixed(2);
+  }
 
 }
 
 function getTotalPrice() {
-
+  var total = 0;
+  for (i = 1; i < document.getElementsByClassName("total").length; i++) {
+    total += parseFloat(document.getElementsByClassName("total")[i].innerHTML);
+    console.log(total);
+  }
+  document.getElementById("totalprice").innerHTML = total.toFixed(2);
 }
+
 
 function createQuantityInput(){
 
@@ -30,8 +41,14 @@ function createItemNode(dataType, itemData){
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
 
+function createNewItemRow(){
+  var newProduct = document.getElementById("hidden-product").cloneNode(true);
+  newProduct.removeAttribute("id");
+  newProduct.children[0].children[0].children[0].innerHTML = document.getElementById("newItemName").value;
+  newProduct.children[1].children[0].children[0].innerHTML = document.getElementById("newItemPrice").value;
+  newProduct.children[3].children[0].children[0].innerHTML = document.getElementById("newItemPrice").value;
+  document.body.insertBefore(newProduct, document.getElementById("body").children[(document.getElementById("body").children.length) - 3]);
 }
 
 function createNewItem(){
@@ -39,6 +56,8 @@ function createNewItem(){
 }
 
 window.onload = function(){
+   getTotalPrice();
+
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
