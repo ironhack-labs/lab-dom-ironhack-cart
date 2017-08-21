e248bec5bb5022fab0966bec8f081b05310acc91
Driggs88 <riggs_david@hotmail.com>
2017-08-21 01:11:54 -0400

    finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,24 +1,17 @@
-var getProduct = document.getElementsByClassName('product');
+var getCreateBtn = document.getElementById('createBtn');
+var getCreateDiv = document.getElementById('create');
+
+var getProduct = document.getElementsByClassName('productName');
 var getUnitPrice = document.getElementsByClassName('productCost');
 var getUnitQty = document.getElementsByClassName('qty');
-var getTotalProductPrice = document.getElementsByClassName('totalPrice');
+var getTotalProductPrice = document.getElementsByClassName('totalProductPrice');
 var deleteButtons = document.getElementsByClassName('btn-delete');
 
-//Click event for delete buttons
-// function deleteRow(){
-  for(i = 0; i < getProduct.length; i++) {
-    deleteButtons[i].addEventListener('click', function (e) {
-      e.currentTarget.parentNode.parentNode.remove();
-    });
-  }
-// }
-
-
-// replace(/[^0-9\.]+/g,""))
 //Calculate prices for individual products
   function calculatePrices() {
+
     for( i = 0; i < getProduct.length; i++ ) {
-      var totalPrice = Number(getUnitPrice[i].innerHTML * getUnitQty[i].value);
+      var totalPrice = Number(getUnitPrice[i].innerHTML.replace(/[^0-9\.]+/g,"")) * getUnitQty[i].value;
       document.getElementsByClassName('totalProductPrice')[i].innerHTML = totalPrice;
     }
     getTotalPrice();
@@ -36,19 +29,28 @@ function getTotalPrice() {
     var productPriceValue = parseInt(getTotalProductPrice[i].innerHTML);
     priceArray.push(productPriceValue);
   }
-  document.getElementById('spanTotal').innerHTML = priceArray.reduce(addTotalProductPrice, 0);
+  var total = priceArray.reduce(addTotalProductPrice, 0);
+
+  document.getElementById('spanTotal').innerHTML =  total;
 }
 
-var getValueofProductName = document.getElementById('productNameValue').value;
-var getValueofProductCost = document.getElementById('productCostValue').value;
-
-// Create a new row of product
 
 
-var getCreateBtn = document.getElementById('createBtn');
-var getCreateDiv = document.getElementById('create');
 
-// function createRow() {
+
+
+
+function deleteRow(){
+  for(i = 0; i < getProduct.length; i++) {
+    deleteButtons[i].addEventListener('click', function (e) {
+      e.currentTarget.parentNode.parentNode.remove();
+    });
+  }
+}
+
+
+function createRow() {
+  // anonymous function added to even listener
   getCreateBtn.addEventListener('click', function (e) {
     var getValueofProductName = document.getElementById('productNameValue').value;
     var getValueofProductCost = document.getElementById('productCostValue').value;
@@ -57,11 +59,12 @@ var getCreateDiv = document.getElementById('create');
     newProductRow.innerHTML = newProductHtml();
     newProductRow.querySelector('.productName').innerHTML = getValueofProductName;
     newProductRow.querySelector('.productCost').innerHTML = getValueofProductCost;
-    document.getElementsByClassName('container')[0].appendChild(newProductRow);
-    //deleteRow();
+    document.getElementById('something').appendChild(newProductRow);
+    // document.getElementsByClassName('container')[0].insertBefore(newProductRow, getCreateDiv.parentNode);
+    deleteRow();
   });
-  // , getCreateDiv.parentNode
-// }
+}
+
 
 function newProductHtml(){
   return '<div class="col-xs-5"> <span class="productName"> </span> </div> <div class="col-xs-5">' +
@@ -77,7 +80,6 @@ function newProductHtml(){
   '<div class="col-xs-5 delete">' +
     '<button class="btn btn-delete">DELETE</button></div>';
 }
-// createRow();
-// deleteRow();
 
-//done
+
+createRow();
