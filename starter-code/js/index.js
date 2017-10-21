3e7b04cc0904c42a6432a77428a06fe670590e20
Alejandro Dietta <alexdietta@hotmail.com>
2017-10-21 20:11:17 +0200

    Iteration 3 Finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,4 @@
+
 function deleteItem(e){
 
 }
@@ -10,9 +11,7 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
 
-}
 
 function createQuantityInput(){
 
@@ -42,11 +41,26 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var productos = document.getElementsByClassName('wrapper');
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
+  }
+
+  function getTotalPrice() {
+    var total = 0;
+    for (var i = 0; i < productos.length; i++) {
+      var totalProduct = document.getElementsByClassName('totalProduct')[i].getElementsByTagName('span')[0];
+      var unitPrice = document.getElementsByClassName('price')[i].getElementsByTagName('span')[0].innerHTML;
+      var quantity = document.getElementsByClassName('qty')[i].getElementsByTagName('input')[0].value;
+      totalProduct.innerHTML = unitPrice * quantity;
+
+      total += totalProduct = unitPrice * quantity;
+    }
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('total-price').style.display = 'flex';
   }
 };
