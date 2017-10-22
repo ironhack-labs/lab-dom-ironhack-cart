1ca45b3af084b567b0c5ece669444eaebf8d4560
Dominik Lasch <dl@exutec.de>
2017-10-22 23:11:29 +0200

    done everything and styled it

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -33,59 +33,68 @@ function getTotalPrice() {
 }
 
 function createQuantityInput() {
-  var newQuantity = document.createElement('input');
-  newQuantity.type = "text";
-  newQuantity.setAttribute('class', 'input');
-  var parent1 = document.getElementById('products').lastChild;
-  parent1.appendChild(newQuantity);
-  var newProductTotalPrice = document.createElement('div');
-  var newProductTotalPriceContent = document.createElement('p');
-  newProductTotalPriceContent.innerHTML = "$";
-  var newProductTotalPriceNumber = document.createElement('span');
-  newProductTotalPriceNumber.innerHTML = "0";
-  newProductTotalPrice.innerHTML = newProductTotalPriceContent.innerHTML + newProductTotalPriceNumber.innerHTML ;
-  var parent2 = document.getElementById('products').lastChild;
-  parent2.appendChild(newProductTotalPrice);
+  // var newQuantity = document.createElement('input');
+  // newQuantity.type = "text";
+  // newQuantity.setAttribute('class', 'input');
+  // var parent1 = document.getElementById('products').lastChild;
+  // parent1.appendChild(newQuantity);
+  // var newProductTotalPrice = document.createElement('div');
+  // var newProductTotalPriceContent = document.createElement('p');
+  // newProductTotalPriceContent.innerHTML = "$";
+  // var newProductTotalPriceNumber = document.createElement('span');
+  // newProductTotalPriceNumber.innerHTML = "0";
+  // newProductTotalPrice.innerHTML = newProductTotalPriceContent.innerHTML + newProductTotalPriceNumber.innerHTML ;
+  // var parent2 = document.getElementById('products').lastChild;
+  // parent2.appendChild(newProductTotalPrice);
 }
 
 function createDeleteButton() {
-  var newButton = document.createElement('button');
-  newButton.setAttribute('class', 'btn btn-delete');
-  newButton.innerHTML = "Delete";
-  var parent = document.getElementById('products').lastChild;
-  parent.appendChild(newButton);
+  // var newButton = document.createElement('button');
+  // newButton.setAttribute('class', 'btn btn-delete');
+  // newButton.innerHTML = "Delete";
+  // var parent = document.getElementById('products').lastChild;
+  // parent.appendChild(newButton);
 }
 
 function createQuantityNode() {
-  var newQuantityLabel = document.createElement('label');
-  newQuantityLabel.innerHTML = "QTY";
-  var parent = document.getElementById('products').lastChild;
-  parent.appendChild(newQuantityLabel);
+  // var newQuantityLabel = document.createElement('label');
+  // newQuantityLabel.innerHTML = "QTY";
+  // var parent = document.getElementById('products').lastChild;
+  // parent.appendChild(newQuantityLabel);
 }
 
 function createItemNode(dataType, itemData) {
-  var newProductPrice = document.createElement('div');
+  var product = document.querySelector('.product-line');
+  var newProductLine = product.cloneNode(true);
 
-  newProductPrice.setAttribute('class', 'product-price price');
-  newProductPrice.innerHTML = "$"+document.getElementsByClassName("new-item-price")[0].value;
-  var newProductName = document.createElement('div');
-  newProductName.setAttribute('class', 'product-name');
+  var newProductName = newProductLine.children[0];
   newProductName.innerHTML = document.getElementsByClassName("new-item-name")[0].value;
-  var parent = document.getElementById('products').lastChild;
-  parent.appendChild(newProductName);
-  parent.appendChild(newProductPrice);
+
+  var newProductPrice = newProductLine.children[1].children[1];
+  newProductPrice.innerHTML = document.getElementsByClassName("new-item-price")[0].value;
+
+  // ------ Inserting the new Product-Line!!! -----
+  var parent = document.getElementById('products');
+  parent.appendChild(newProductLine);
 
 
 
-  //P-Tag with Dollar-Sign, in Produt-Price-Div
+  // var newProductPrice = document.createElement('div');
+  // newProductPrice.setAttribute('class', 'product-price price');
+  // newProductPrice.innerHTML = "$"+document.getElementsByClassName("new-item-price")[0].value;
+  // var newProductName = document.createElement('div');
+  // newProductName.setAttribute('class', 'product-name');
+  // newProductName.innerHTML = document.getElementsByClassName("new-item-name")[0].value;
+
+  // parent.appendChild(newProductPrice);
 
 }
 
 function createNewItemRow(itemName, itemUnitPrice) {
-  var newItemRow = document.createElement('div');
-  newItemRow.setAttribute('class', 'product-line');
-  var parent = document.getElementById('products');
-  parent.appendChild(newItemRow);
+  // var newItemRow = document.createElement('div');
+  // newItemRow.setAttribute('class', 'product-line');
+  // var parent = document.getElementById('products');
+  // parent.appendChild(newItemRow);
 }
 
 function createNewItem() {
