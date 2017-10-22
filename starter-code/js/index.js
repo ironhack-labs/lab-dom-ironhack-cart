3cf3886894ef4a79cb4f56c5f4fbcc98692a2f50
acampos91 <acampos91@hotmail.com>
2017-10-22 16:44:37 +0200

    iteración 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,16 +2,21 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+// function getPriceByProduct(itemNode){
+function getPriceByProduct(){
+ var productPrice = document.getElementById("product-price").innerHTML;
+ updatePriceByProduct(productPrice);
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(productPrice){
+//function updatePriceByProduct(productPrice, index){
+var quantity = document.getElementById("numbers").value;
+var totalPrice = quantity * productPrice;
+document.getElementById("final").innerHTML = totalPrice;
 }
 
 function getTotalPrice() {
-
+  getPriceByProduct();
 }
 
 function createQuantityInput(){
