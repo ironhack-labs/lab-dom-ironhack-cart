0a81ab918e9c16b4b7c95ee06f1f9d7016dc20b7
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-10-22 22:01:18 +0200

    ironhack-DOM-No completo

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,23 @@
-function deleteItem(e){
+function calculatePrice () {
+  var price = document.getElementsByClassName('prices');
+  var unit = document.getElementsByTagName('input');
+  var priceTotales = document.getElementsByClassName('priceTotal');
+  var sum = 0;
 
+  for (var i = 0; i < price.length; i++) {
+     var totalPrice = price[i].innerHTML * unit[i].value;
+     priceTotales[i].innerHTML = totalPrice;
+     console.log(sum);
+     sum = sum + parseInt(priceTotales[i].innerHTML);
+     console.log(sum);
+   }
+
+  document.getElementById("sumT").innerHTML = sum;
 }
 
-function getPriceByProduct(itemNode){
+function del(){
+  var btn = document.getElementsByClassName("product");
+  var div = document.getElementById("containeres");
 
+  div.removeChild(this.btn);
 }
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
