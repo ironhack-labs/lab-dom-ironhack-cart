6652cac3ad5ebf3ece05e33098a2960ba0e421fb
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-22 20:56:00 +0200

    Iteration 3 finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,20 +2,28 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(item){
+  return parseFloat(item.getElementsByClassName('cost')[0].innerHTML);
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(productPrice, product){
+  var itemQuantity = product.getElementsByClassName('QTY')[0].value;
+  product.getElementsByClassName('totalcost')[0].innerHTML = productPrice * itemQuantity + " $";
+  return productPrice * itemQuantity;
 }
 
 function getTotalPrice() {
-  var itemValue = document.getElementById('QTY1').value;
-  var itemCostString = document.getElementById('cost').innerHTML;
-  var itemCostNumber = parseFloat(itemCostString);
-  document.getElementById('totalcost').innerHTML = itemValue*itemCostNumber + " $";
+  var items = document.getElementsByTagName('article');
+  var totalPrices = 0;
+
+  for (var i=0; i<items.length; i++) {
+    var itemCostNumber = getPriceByProduct(items[i]);
+    var price = updatePriceByProduct(itemCostNumber, items[i]);
+    totalPrices = totalPrices + price;
+    document.getElementById('final_price').innerHTML = totalPrices + ' $'; 
+  }
 }
+
 
 function createQuantityInput(){
 
@@ -47,6 +55,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
+
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
