38e8547b2f8db357461f8b023fd9338a4152f3ed
FranRom <maildefranz@gmail.com>
2017-10-22 14:43:36 +0200

    First iteration done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,17 +2,34 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode, i){
+  var itemQTY = document.getElementsByClassName("item-quantity");
+  return Math.floor(itemQTY[i].value) * itemNode;
+}
+
+function updatePriceByProduct(productPrice, index) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+//button event
 
+var button = document.getElementById("btn-success");
+
+function getTotalPrice () {
+
+  var productPrice = document.getElementsByClassName("product-price");
+  var totalPrice = document.getElementsByClassName("total-price");
+  var sum = 0;
+  for (var i = 0; i < productPrice.length; i++) {
+   var finalPrice = getPriceByProduct(productPrice[i].innerHTML, i);
+   totalPrice[i].innerHTML = parseFloat(finalPrice).toFixed(2);
+   sum += Number(parseFloat(finalPrice).toFixed(2));
+    }
+    var finalSum = document.getElementById("finalSum");
+    finalSum.innerHTML = sum;
 }
 
-function getTotalPrice() {
 
-}
 
 function createQuantityInput(){
 
