a351e980447f8f53dc3fefbd4d7145deb5f8d938
Cristina Moreno <moreno.medran@gmail.com>
2017-10-22 21:34:51 +0200

    j

--- starter-code/CSS + HTML DONE/index.js
+++ starter-code/CSS + HTML DONE/index.js
@@ -11,10 +11,6 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-  
-pricePerUnit * input.quantityInput = totalPrice;
-
-totalPrice.createTextNode(totalPrice)
 
 }
 
@@ -43,17 +39,14 @@ function createNewItem(){
 }
 
 window.onload = function(){
-
   var calculatePriceButton = document.getElementById('calculate-price');
-  var quantityInput = document.getElementsByClassName('quantity-input');
+  var quantityInput = document.getElementsByClassName('qty-input');
   var deleteButton = document.getElementsByClassName('btn-delete');
-  var totalPrice = document.getElementsByClassName('total-price');
-  var pricePerUnit = document.getElementsByClassName('price-per-unit');
 
   calculatePriceButton.onclick = getTotalPrice;
   quantityInput.onclick = createNewItem;
 
   for(var i = 0; i<deleteButton.length ; i++){
-    deleteButton[i].onclick = deleteItem;
+    deleteButtons[i].onclick = deleteItem;
   }
 };
