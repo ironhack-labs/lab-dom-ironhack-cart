d8d0d5e1752b3d73922dcc54474aa79583c86c7b
Dominik Lasch <dl@exutec.de>
2017-10-22 12:40:07 +0200

    1. unit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,10 @@
+'use.strict';
+
 function deleteItem(e){
 
 }
+
+
 
 function getPriceByProduct(itemNode){
 
@@ -11,7 +15,10 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var unitPrice = document.getElementById('unit-price');
+  var unitQTY = document.getElementById('unit-qty');
+  var totalPrice = document.getElementById('total-price');
+  totalPrice.innerHTML = unitPrice.innerHTML * unitQTY.value;
 }
 
 function createQuantityInput(){
@@ -44,7 +51,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
