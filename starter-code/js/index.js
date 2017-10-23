2231796b463c6a82fa1d87d59fe08a327d94ca5e
Asier Cisneros <ascisneros14@gmail.com>
2017-10-23 03:39:31 +0200

    Ironhack Cart done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,43 +1,3 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
@@ -47,6 +7,41 @@ window.onload = function(){
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+    deleteButtons[i].onclick = function() {
+      this.parentNode.remove();
+    };
   }
+
+function getTotalPrice() {
+  var itemPrice = document.getElementsByClassName("price");
+  var qty = document.getElementsByClassName("quantity");
+  var totalPrice = document.getElementsByClassName("totalprice");
+  var mult;
+  for (var i = 0; i < itemPrice.length; i++) {
+    mult = itemPrice[i].innerHTML * qty[i].value;
+    totalPrice[i].innerHTML = mult;
+  }
+  changeTotalPrice();
+}
+
+function changeTotalPrice () {
+  var newTotalPrice = document.getElementsByClassName("totalprice");
+  var sum = 0;
+  for (var j = 0; j < newTotalPrice.length; j++) {
+    sum += Number(newTotalPrice[j].innerHTML);
+  }
+  document.getElementById("finalsum").innerHTML = sum;
+}
+
+var copiedDiv = document.querySelector(".container");
+var bodyItem = document.getElementById("totalitemlist");
+function createNewItem() {
+  var newDiv = copiedDiv.cloneNode(true);
+  newDiv.querySelector(".name").innerHTML = document.getElementById("newitem").value;
+  newDiv.querySelector(".price").innerHTML = document.getElementById("newprice").value;
+  newDiv.querySelector(".btn-delete").onclick = function() {
+    this.parentNode.remove();
+  };
+  bodyItem.appendChild(newDiv);
+}
 };
