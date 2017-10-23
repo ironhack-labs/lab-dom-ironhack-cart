a4af2f9396553993c48d315b268ae26607e62d2d
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-10-23 09:49:12 +0200

    precio total actualizado

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -11,23 +11,24 @@ window.onload = function(){
   para hacerla desaparecer, eso lo conseguimos borrando el padre de
   button*/
 
-  for(var i = 0; i<buttonDelete.length ; i++){
-     buttonDelete[i].onclick = function() {
-       this.parentNode.remove();
+  for(var i = 0; i<deleteButtons.length ; i++){
+     deleteButtons[i].onclick = function() {
+       deleteButtons[i].parentNode.parentNode.remove();
      };
    }
 
-/*hacemos una funcion donde nos guarda el valor resultante de los parametros
-que le pasamos (price,qty y totalprice) mediante las clases.
-guardamos en una nuevo array el resultado y creamos un bucle
-para recorrer el array*/
 
-  function getTotalPrice(){
+  /*hacemos una funcion donde nos guarda el valor resultante de los parametros
+  que le pasamos (price,qty y totalprice) mediante las clases.
+  guardamos en una nuevo array el resultado y creamos un bucle
+  para recorrer el array*/
+
+  function getTotalPrice() {
     var priceItem = document.getElementsByClassName('price');
     var quantityItem = document.getElementsByClassName('qty');
-    var totalPriceItem = document.getElementsByClassName ('totalprice');
+    var totalPriceItem = document.getElementsByClassName('totalprice');
     var result;
-    for (var i = 0; i < priceItem.length; i++){
+    for (var i = 0; i < quantityItem.length; i++) {
       result = priceItem[i].innerHTML * quantityItem[i].value;
       totalPriceItem[i].innerHTML = result;
     }
@@ -37,25 +38,25 @@ para recorrer el array*/
   /*Cambiar el precio total, por el nuevo precio (dos multiplicaciones + el resultante
 de estas dos)*/
 
-  function changeTotalPriceItem () {
-  var newTotalPrice = document.getElementsByClassName("totalprice");
-  var sum = 0;
-  for (var j = 0; j < newTotalPrice.length; j++) {
-    sum += Number(newTotalPrice[j].innerHTML);
+  function changeTotalPriceItem() {
+    var newTotalPrice = document.getElementsByClassName("totalprice");
+    var sum = 0;
+    for (var j = 0; j < newTotalPrice.length; j++) {
+      sum += Number(newTotalPrice[j].innerHTML);
+    }
+    document.getElementById("resultFinal").innerHTML = sum;
   }
-  document.getElementById("resultFinal").innerHTML = sum;
-}
-var copiedDiv = document.querySelector(".itemdiv");
-var bodyItem = document.getElementsByClassName("items");
+  var copiedDiv = document.querySelector(".itemdiv");
+  var bodyItem = document.getElementsByClassName("items");
+  var newItemDiv;
 
-
-function createNewItem() {
-  var newItemDiv = copiedDiv.cloneNode(true);
-  newDiv.querySelector(".nameItem").innerHTML = document.getElementById("newName").value;
-  newDiv.querySelector(".price").innerHTML = document.getElementById("newPrice").value;
-  newDiv.querySelector(".btn-delete").onclick = function() {
-    this.parentNode.remove();
-  };
-  bodyItem.appendChild(newItemDiv);
-}
+  function createNewItem() {
+    var newItemDiv = bodyItem.cloneNode(true);
+    newItemDiv.querySelector(".nameItem").innerHTML = document.getElementById("newName").value;
+    newItemDiv.querySelector(".price").innerHTML = document.getElementById("newPrice").value;
+    newItemDiv.querySelector(".btn-delete").onclick = function() {
+      this.parentNode.parentNode.remove();
+    };
+    bodyItem.appendChild(newItemDiv);
+  }
 };
