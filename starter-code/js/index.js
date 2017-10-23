bc7f66396d096b9fdeecb545bcc3f1ce11566601
Ernesmh <ernesmh@gmail.com>
2017-10-23 08:57:25 +0200

    practica sin ningún éxito

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,38 @@
+/*Once you have the HTML and CSS ready, use JavaScript and functions
+of the document object to retrieve the data you need to calculate the
+total price for that product. Change the value of the element in the DOM
+ that displays the total price:
+
+Create a click event for the Calculate Prices button
+This event will execute a function that:
+Retrieves the unit price of the product
+Retrieves the quantity of items desired
+Calculates the total price based on this data
+Updates the total price in the DOM*/
+
+window.onload = function(){
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+
+
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+};
+
+
 function deleteItem(e){
 
+  }
 }
 
 function getPriceByProduct(itemNode){
-
+  var unitary = document.getElementById("unitary");
+  return unitary;
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +40,11 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var unitary = document.getElementById("unitary");
+  var quantity= label.setAttribute('input')
+  console.log(unitary);
+  console.log(quantity);
+  return unitary * quantity;
 }
 
 function createQuantityInput(){
@@ -31,22 +64,9 @@ function createItemNode(dataType, itemData){
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
-
+  var  = document.createTextNode('Ironshirt')
 }
 
 function createNewItem(){
 
 }
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
