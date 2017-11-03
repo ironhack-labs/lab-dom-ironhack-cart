7a757c1fc5eac34c53b5a647df1425d27a0534bc
David Cohen <davidmartincohen@gmail.com>
2017-11-03 23:55:23 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,29 @@
-function deleteItem(e){
+$(".calc-prices-button").click(function () {
+	var sum = 0;
+	$(".product").each(function(index) {
+		var qty = $(this).find('input').val();
+		var price = $(this).find(".unit-cost").text();
+		$(this).find(".total-price").text(qty*price);
+		var tempNum = parseInt($(this).find(".total-price").text());
+		sum += tempNum;
+	})
 
-}
+	$(".total-cost").text(sum);
+  	
+  });
 
-function getPriceByProduct(itemNode){
+$(".btn-delete").click(function () {
+	$(this).closest(".product").remove();
+})
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+$(".btn-add").click(function () {
+	var divCopy = $(".first").first().clone();
+	$(divCopy).appendTo(".product-wrapper");
+	var name = $(".name-input").val()
+	var price = $(".cost-input").val()
+	$(divCopy).find(".product-name").text(name);
+	$(divCopy).find(".unit-cost").text(price);
+	$(divCopy).find(".btn-delete").click(function () {
+		$(this).closest(".product").remove();
+	})
+})
