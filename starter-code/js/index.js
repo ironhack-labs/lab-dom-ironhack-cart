f0822af76f39aadf82cefb12dfe35edb426ce2f8
Vivian <vivian.sarazin@gmail.com>
2017-11-04 10:22:35 +0100

    PAR // @vivian.sarazin @Vivian

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,25 @@
-function deleteItem(e){
+// Delete item
+$("body > div > div.del > input").click(function() {
+  $(this).parent().parent().remove();
 
-}
+});
 
-function getPriceByProduct(itemNode){
 
-}
+  $("body > div.calculate > div").click(function() {
+    $('.product').each(function(){
+      var cpu = $(this).find('.cpu > p').text();
+      var qty = $(this).find(' div.unit > input[type="text"]').val();
+      var pce = cpu * qty;
+      $(this).find('.price').text(pce);
 
-function updatePriceByProduct(productPrice, index){
+      var gt = parseInt($('body > div:nth-child(2) > div.price').text()) + parseInt($('body > div:nth-child(3) > div.price').text());
+      // console.log(gt)  
 
-}
+      $('body > div.sum > p').text(gt);
+    });
+  });
 
-function getTotalPrice() {
 
-}
 
-function createQuantityInput(){
 
-}
 
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
