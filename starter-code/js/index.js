28d223a2df438e79b1073698e67120cf38152f26
Charlottebtmn <c.bottemanne@gmail.com>
2017-11-04 12:15:26 +0100

    PAR Charlotte DOM exercice iteration 1-3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,20 @@
-function deleteItem(e){
 
-}
 
-function getPriceByProduct(itemNode){
+//$('.btn-success').click(function() {
+  //$('.product1 .total-price').text($('.product1 .spanPrix').text() * $('.product1 .quantity').val());
+  //$('.product2 .total-price').text($('.product2 .spanPrix').text() * $('.product2 .quantity').val());
+  //$('h2 > span').text();
+//});
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+$('.btn-success').click(function () {
+  var currentSum = 0;
+  $('.product').each(function () {
+    var qty = $(this).find('.quantity').val();
+    var ppu = $(this).find('.spanPrix').text();
+    console.log(qty, ppu);
+    $(this).find('.total-price').text(qty*ppu);
+    currentSum = parseInt($(this).find('.total-price').text());
+    currentSum+=currentSum;
+  });
+  $('.global-price').text(currentSum);
+});
