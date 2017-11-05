4e016e88fcafd683e81d42dfeb7ad1c6fafe2659
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-05 23:01:36 +0100

    Feat: done gettotalprice

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,44 +1,52 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
+  unitCost = document.getElementsByClassName("cost-unit-span");
+  units = document.getElementsByClassName("quantity-input");
+  totalCost = document.getElementsByClassName('span-total');
+  totalPrice = 0;
+  for (i = 0; i < unitCost.length; i++) {
+    totalCost[i].innerHTML = unitCost[i].innerText * units[i].value;
+    totalPrice += unitCost[i].innerText * units[i].value;
+  }
+  document.getElementById("total-price").innerHTML = totalPrice;
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem() {
 
 }
 
-function createNewItem(){
-
-}
-
-window.onload = function(){
+window.onload = function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -46,7 +54,7 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
