831fe4bff6707b5e7ffb6276ebbb04a6c56dc7f0
mouyaq <mouyaq@gmail.com>
2017-11-13 20:41:33 +0100

    fix: XSS fixed

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,7 +1,7 @@
 function deleteItem(e){
-  product = e.target.parentElement.parentElement;
+  product = e.parentNode.parentNode;
+  console.log(product);
   product.parentNode.removeChild(product);
-  console.log(document);
 }
 
 function getPriceByProduct(itemNode){
@@ -14,17 +14,19 @@ function updatePriceByProduct(productPrice, index){
 
 function getTotalPrice() {
   products = document.getElementsByClassName("product");
-  console.log(products.length);
   var finalPrice = 0;
-  for(i = 0; i < products.length; i++) {
-    var unitPrice = parseFloat(document.getElementsByClassName("product-unit-price")[i].innerText);
+  for(i = 1; i < products.length; i++) {
+    var unitPrice = parseFloat(document.getElementsByClassName("product-unit-price")[i].innerHTML);
     var qty = parseFloat(document.getElementsByClassName("product-qty")[i].value);
     var totalItemPrice = unitPrice * qty;
     finalPrice += totalItemPrice;
-    document.getElementsByClassName("total-price")[i].innerHTML = totalItemPrice;
+    totalItemPriceText = document.createTextNode(totalItemPrice);
+    document.getElementsByClassName("total-price")[i].innerHTML = "";
+    document.getElementsByClassName("total-price")[i].appendChild(totalItemPriceText);
   }
-  document.getElementById("final-price").innerHTML = "<h2>Total Price: <span>$" + finalPrice + "</span></h2>"
-  
+  finalPriceText = document.createTextNode(finalPrice);
+  document.getElementById("final-price").innerHTML = "";
+  document.getElementById("final-price").appendChild(finalPriceText);
 }
 
 function createQuantityInput(){
@@ -48,18 +50,58 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
+  /*
+  var template = document.getElementsByClassName("template");
+  var clon = template
+  template[0].getElementsByClassName("product-name")[0].innerText = document.getElementById("p-name").value;
+  template[0].getElementsByClassName("product-unit-price").innerText = document.getElementById("p-price").value;
+  document.getElementById("cart").appendChild(template);
+  */
 
+var itm = document.getElementById("cart").firstElementChild;
+console.log(itm);
+var cln = itm.cloneNode(true);
+console.log(cln);
+cln.className="product";
+console.log("AQUI");
+console.log(cln);
+
+// Append the cloned <li> element to <ul> with id="myList1"
+document.getElementById("cart").appendChild(cln);
+console.log(document);
 }
-
+/*
+<div id ="template" class="product">
+      <div class="product-name">
+        <span>producto0</span>
+      </div>
+      <div class="product-cost">
+        <span class="product-unit-price">0</span>
+      </div>
+      <div class="product-form">
+        <form>
+          <label for="product-qty">QTY</label>
+          <input class="product-qty" type="text" name="qty">
+        </form>
+      </div>
+      <div class="product-total-price">
+        <span class="total-price">0</span>
+      </div>
+      <div>
+        <button class="btn btn-delete" class="btn-delete" onclick="deleteItem(this)">Delete</button>
+      </div>
+    </div>
+*/
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  //calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+  
 };
