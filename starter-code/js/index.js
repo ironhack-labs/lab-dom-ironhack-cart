d218ef878d0c51df56e7134f46e40b5926538c58
mouyaq <mouyaq@gmail.com>
2017-11-13 03:13:42 +0100

    feat: Iteration 3 complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -12,13 +12,17 @@ function updatePriceByProduct(productPrice, index){
 
 function getTotalPrice() {
   products = document.getElementsByClassName("product");
+  var finalPrice = 0;
   for ( i = 0; i < products.length; i++ ) {
     console.log(products[i].childNodes)
     var unitPrice = parseInt(products[i].childNodes[3].innerText);
     var qty = parseInt(products[i].childNodes[5].childNodes[1].childNodes[3].value);
     var totalItemPrice = unitPrice * qty;
+    finalPrice += totalItemPrice;
     products[i].childNodes[7].innerHTML = totalItemPrice;
   }
+  document.getElementById("final-price").innerHTML = "<h2>Total Price: <span>$" + finalPrice + "</span></h2>"
+  
 }
 
 function createQuantityInput(){
