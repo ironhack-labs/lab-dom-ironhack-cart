2cbd8894e2d663f98d8cffe8e0ba3723a878bdc8
Jorge Yago <jorge.yago.meniz@gmail.com>
2017-11-14 13:33:53 +0100

    Iteration 1: price of 1 product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,22 @@
+var IronBubblePrice = 25;
+var udCost = document.getElementById ("ud-cost");
+udCost.innerHTML = IronBubblePrice + " €";
+
+var productPrice = document.getElementById("calc-prices-button");
+productPrice.onclick = function (){
+  var units = document.getElementsByTagName("input")[0];
+  var price = document.getElementsByClassName ("price");
+  [].slice.call(price).forEach ( function(price) {
+    price.innerHTML = units.value * IronBubblePrice + " €";
+  });
+};
+
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
+
 
 }
 
@@ -38,15 +52,15 @@ function createNewItem(){
 
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+//window.onload = function(){
+//  var calculatePriceButton = document.getElementById("calc-prices-button");
+//  var createItemButton = document.getElementById("new-item-create");
+//  var deleteButtons = document.getElementsByClassName("btn-delete");
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+//  calculatePriceButton.onclick = getTotalPrice;
+//  createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+//  for(var i = 0; i<deleteButtons.length ; i++){
+//    deleteButtons[i].onclick = deleteItem;
+//  }
+//};
