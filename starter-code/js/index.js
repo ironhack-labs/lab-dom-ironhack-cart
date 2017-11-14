9d277d3d87f12277749943d69dbb0114a6207966
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-14 15:44:04 +0100

    feat: calculate prices and delete buttons done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,23 +1,43 @@
-function deleteItem(){
-  parent = document.getElementById('page-wrapper');
 
-  currentDeleteBtn = event.currentTarget;
-
-  deletedItem = currentDeleteBtn.parentNode.parentNode;
-
-  parent.removeChild(deletedItem);
+function deleteItem(e){
+	unitPrice = e.currentTarget.parentNode.parentNode;
+  unitPrice.parentNode.removeChild(unitPrice);
 }
 
-function getPriceByProduct(){
-  
+function getPriceByProduct(unitPrice){
+	return unitPrice.innerHTML;
 }
 
 function updatePriceByProduct(productPrice, index){
+	quantity = document.getElementsByClassName('product-quantity');
+	finalProductPrice = document.getElementsByClassName('total-product-price');
 
+  finalProductPrice[index].innerHTML = "";
+
+	if(quantity[index].value >= 0){
+    newText = quantity[index].value * productPrice;
+    newTextNode = document.createTextNode(newText);
+    finalProductPrice[index].appendChild(newTextNode);
+	} else {
+    ceroText = "0.00";
+    ceroTextNode = document.createTextNode(ceroText);
+    finalProductPrice[index].appendChild(ceroTextNode);
+  }
+
+	return finalProductPrice[index].innerHTML;
 }
 
-function getTotalPrice() {
+function getfinalProductPrice() {
+	var unitPrice = document.getElementsByClassName('cost-unit');
+	var totalPrice = 0;
+	for(var i = 0; i < unitPrice.length; i++){
+		totalPrice += parseInt(updatePriceByProduct(getPriceByProduct(unitPrice[i]), i));
+	}
+	var totalPriceResult = document.getElementById('total-price-span');
+  totalPriceResult.innerHTML = "";
 
+  newTotalPrice = document.createTextNode(totalPrice);
+  totalPriceResult.appendChild(newTotalPrice);
 }
 
 function createQuantityInput(){
@@ -32,7 +52,7 @@ function createQuantityNode(){
 
 }
 
-function createItemNode(dataType, itemData){
+function createunitPrice(dataType, itemData){
 
 }
 
@@ -45,11 +65,11 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  //var calculatePriceButton = document.getElementById('calc-prices-button');
+  var calculatePriceButton = document.getElementById('calc-prices-button');
   //var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  //calculatePriceButton.onclick = getTotalPrice;
+  calculatePriceButton.onclick = getfinalProductPrice;
   //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
