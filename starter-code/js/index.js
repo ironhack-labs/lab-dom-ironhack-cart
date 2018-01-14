5d65b3c90ba187ec2c32d1fe32a008a9dc67d5a9
Beltran Rengifo <merloc@gmail.com>
2018-01-14 17:11:20 +0100

    done. iteraciones completadas. minibug en add_product: resetea el total_items_number al valor inicial (1)

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,6 @@
-var qtyInputs;
-var qtyInputsArr;
+/*===============================
+stock array
+===============================*/
 var stock = [{
             name: 'Fancy T-Shirt',
             price: 35
@@ -26,17 +27,114 @@ var stock = [{
       }
 ];
 
+var userCart = stock;
+
+/*===============================
+variables para qty btn
+===============================*/
+var qtyInputs;
+var qtyInputsArr;
+
+function createQuantityInput() {
+      //get qty inputs
+      qtyInputs = document.getElementsByClassName('qty-input');
+      // transformar qty inputs a array
+      qtyInputsArr = [].slice.call(qtyInputs);
+      //add ev.listen a todos los inputs de qty
+      qtyInputsArr.forEach(function (input, i) {
+            var thisProduct = input.closest('.product');
+            var thisProductPrice = [].slice.call(thisProduct.getElementsByClassName('span-price'));
+            input.addEventListener("blur", function () {
+                  return getTotalPrice(thisProductPrice[0].innerHTML, input.value, thisProduct);
+            });
+      });
+}
+
+/*===============================
+get total price
+===============================*/
+function getTotalPrice(thisProductPrice, numItems, thisProduct) {
+      //get html collection y devolver obj como array
+      [].slice.call(thisProduct.getElementsByClassName('span-total-price'))[0].innerHTML;
+      //setear el innerHTML con el valor multiplicado
+      thisProduct.getElementsByClassName('span-total-price')[0].innerHTML = thisProductPrice * numItems;
+      getCartTotal();
+}
+
+/*===============================
+listener y llamada para crear producto
+===============================*/
+//get inputs values, vaya cosa más torpe para el get y el set de los values...
+var newItemName = document.getElementById('new-item-name-input');
+newItemName.value = newItemName.value;
+var newItemPrice = document.getElementById('new-item-price-input');
+newItemPrice.value = newItemPrice.value;
+//listener para el btn del modal e invocación de la fx
+var addItemBtn = document.getElementById('addProductBtn').addEventListener('click', function () {
+      createNewItem(newItemName.value, newItemPrice.value);
+      //un poco de jQuery para cerrar el modal
+      $('#addProductModal').modal('hide');
+});
+
+function createNewItem(newItemName, newItemPrice, defaultQty) {
+      var newItem = {
+            name: newItemName,
+            price: parseInt(newItemPrice)
+      };
+      stock.push(newItem);
+      showStock(userCart);
+      createQuantityInput();
+      listenerRemoveButtons();
+      getCartTotal();
+}
+
+/*===============================
+pintar stock
+===============================*/
 function showStock(products) {
       var productsList = document.getElementById('product-list');
-      products.forEach(function (product) {
-            var product = '<div class="row product"><div class="align-self-center col product-title"><span>' + product.name + '</span></div><div class="align-self-center col col-2 product-price"><span class="span-price">' + product.price + '</span><span> €</span></div><div class="align-self-center col col-2 product-qty"><label for="qty">Nº of items: </label><input class="qty-input" type="number" name="quantity" min="1" value="1"> </div><div class="align-self-center col col-2 product-total-price"><span class="span-total-price">' + product.price + '</span><span> €</span></div><div class="align-self-center col product-remove"><button type="button" class="btn btn-outline-danger">Delete</button></div></div>';
+      productsList.innerHTML = '';
+
+      products.forEach(function (product, i) {
+
+            var product = '<div class="row product"><div class="align-self-center col product-title"><span>' + product.name + '</span></div><div class="align-self-center col col-2 product-price"><span class="span-price">' + product.price + '</span><span> €</span></div><div class="align-self-center col col-2 product-qty"><label for="qty">Nº of items: </label><input class="qty-input" type="number" name="quantity" min="0" value="1"> </div><div class="align-self-center col col-2 product-total-price"><span class="span-total-price">' + product.price + '</span><span> €</span></div><div class="align-self-center col product-remove"><button type="button" class="btn btn-outline-danger remove-product-btn">Delete</button></div></div>';
+
             productsList.innerHTML += product;
       });
 }
 
-// function deleteItem(e){
+/*===============================
+get cart total
+===============================*/
+function getCartTotal() {
+      var totalResultContainer = document.getElementById('total-cart-result');
+      var totalProductsPriceArray = [].slice.call(document.getElementsByClassName('span-total-price'));
+      var cart_total = totalProductsPriceArray.reduce(function (acc, e, i) {
+            return parseInt(e.innerHTML) + acc;
+      }, 0);
+      totalResultContainer.innerHTML = cart_total + ' €';
+}
 
-// }
+/*===============================
+remove item
+===============================*/
+
+var removeProductBtn;
+
+function listenerRemoveButtons() {
+      removeProductBtn = [].slice.call(document.getElementsByClassName('remove-product-btn'));
+      removeProductBtn.forEach(function (btn, i) {
+            btn.addEventListener('click', function () {
+                  deleteItem(btn, i);
+            });
+      })
+}
+
+function deleteItem(btn, i) {
+      btn.closest('.product').remove();
+      userCart.splice(i,1);
+      getCartTotal();
+}
 
 // function getPriceByProduct(product) {
 //       return product.price;
@@ -46,15 +144,8 @@ function showStock(products) {
 
 // }
 
-//get total price on blur
-function getTotalPrice(thisProductPrice, numItems, thisProduct) {
-      [].slice.call(thisProduct.getElementsByClassName('span-total-price'))[0].innerHTML;
-      thisProduct.getElementsByClassName('span-total-price')[0].innerHTML = thisProductPrice * numItems;
-}
 
-// function createQuantityInput(){
 
-// }
 
 // function createDeleteButton(){
 
@@ -68,29 +159,24 @@ function getTotalPrice(thisProductPrice, numItems, thisProduct) {
 
 // }
 
-// function createNewItemRow(itemName, itemUnitPrice){
+// function createNewItemRow(prodName, prodPrice) {
 
 // }
 
-// function createNewItem(){
 
-// }
+
+
+/*===============================
+onload event
+===============================*/
 
 window.onload = function () {
       //pintar array stock
-      showStock(stock);
-      //get qty inputs
-      qtyInputs = document.getElementsByClassName('qty-input');
-      // transformar qty inputs a array
-      qtyInputsArr = [].slice.call(qtyInputs);
-      //add ev.listen a todos los inputs de qty
-      qtyInputsArr.forEach(function(input, i){
-            var thisProduct = input.closest('.product');
-            var thisProductPrice = [].slice.call(thisProduct.getElementsByClassName('span-price'));
-            input.addEventListener("blur", function() {
-                  return getTotalPrice(thisProductPrice[0].innerHTML, input.value, thisProduct);
-            })
-      });
+      showStock(userCart);
+      createQuantityInput();
+      getCartTotal();
+      listenerRemoveButtons();
+
       // getTotalPrice();
       //   var calculatePriceButton = document.getElementById('calc-prices-button');
       //   var createItemButton = document.getElementById('new-item-create');
