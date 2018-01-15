fe211034c9c3e2199c375ec711b0bd3db8c52948
Nacho Baile <nachobaile@gmail.com>
2018-01-15 09:22:50 +0100

    Hecho primer punto.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,65 @@
 function deleteItem(e){
 
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
+  var searchParent = e.currentTarget.parentNode.parentNode;
+  var foundParent = searchParent.parentNode;
+  foundParent.removeChild(searchParent);
+ 
+ }
+ 
+ function getPriceByProduct(itemNode){
+ 
+ 
+ }
+ 
+ function updatePriceByProduct(productPrice, index){
+ 
+ }
+ 
+ function getTotalPrice() {
+;
+  var calcularPrecio = document.getElementsByClassName('precio-producto')[0].innerText;
+  console.log(calcularPrecio);
+  var recogerProducto = document.getElementsByClassName('cantidad_input')[0].value;
+  console.log(recogerProducto);
+ 
+  document.getElementById('cantidad_total').innerHTML = calcularPrecio * recogerProducto + "€";
+ }
+ 
+ function createQuantityInput(){
+ 
+ }
+ 
+ function createDeleteButton(){
+ 
+ }
+ 
+ function createQuantityNode(){
+ 
+ }
+ 
+ function createItemNode(dataType, itemData){
+ 
+  var nodoNuevo = nodoDiv.cloneNode();
+ 
+ }
+ 
+ function createNewItemRow(itemName, itemUnitPrice){
+ 
+ }
+ 
+ function createNewItem(){
+ 
+ }
+ 
+ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+ 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
-
+ 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+ };
