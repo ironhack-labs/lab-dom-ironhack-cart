1796c848857133342cbe68f9ec4fd5df07653836
davidalexteixeira <david5teixeira@gmail.com>
2018-01-15 22:03:13 +0100

    updating

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,7 @@
 function deleteItem(e){
-
+   
+  
+ 
 }
 
 function getPriceByProduct(itemNode){
@@ -11,7 +13,22 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var productPrice = document.getElementsByClassName('product-cost');
+  var productQty = document.getElementsByClassName('product-quantity');
+  var productTotal = document.getElementsByClassName('product-total');
+  var totalPriceContainer = document.getElementById('total-price');
+  var totalPrice = 0;
 
+  for(var i = 0; i < productPrice.length; i++){
+
+    var totalProductPrice = Math.round((Number(productPrice[i].textContent) * Number(productQty[i].value))*100)/100;
+    totalPrice += totalProductPrice;    
+    productTotal[i].innerHTML = totalProductPrice;
+    
+  }
+  
+  totalPriceContainer.innerHTML = totalPrice;
+ 
 }
 
 function createQuantityInput(){
