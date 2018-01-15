4a4009be9167b30738b41129eafbd81b65c312e6
Filipe Rainho <filipenrainho@gmail.com>
2018-01-15 18:12:25 +0100

    Working on calc product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,15 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var numOfProducts = document.querySelectorAll('.product-list-item');
+  for (var i = 0; i < numOfProducts.length; i++) {
+    
+  }
+  var productPrice = Number(document.querySelectorAll('.value')[0].innerText);
+  var quantity = document.querySelectorAll('.quantity')[0].valueAsNumber;
+  var totalField = document.querySelectorAll('.total-value')[0];
+  var total = productPrice * quantity;
+  totalField.innerText = total;
 }
 
 function createQuantityInput(){
@@ -44,7 +52,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
