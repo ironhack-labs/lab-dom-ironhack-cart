d37df7b70af4d4cd0b0bcf3331c8f761c2bdcabf
Matt Megarry <mattmegarry@gmail.com>
2018-01-16 01:10:31 +0100

    Finished Iteration 4 - in a kinda strange way...

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -20,8 +20,28 @@ function createNewItem (){
   console.log('createNewItem CLICKED');//tether- to do
 }
 
-function deleteItem () {
+function deleteItem (id) {
+  //var i = 0;
+  //console.log(document.getElementsByClassName('prod-wrapper')[i]);
+  document.body.removeChild(document.getElementById(id));
   console.log('deleteItem CLICKED');
+
+  //But what if we delete a middle product? Let's make the deleteItems params go in order!
+  var ProdsRemaining = document.getElementsByClassName('prod-wrapper').length;
+  var newButton = '';
+  for (var i = 0; i < ProdsRemaining; i++){
+    document.getElementsByClassName('prod-wrapper')[i].id = i;
+    newButton = '<button onclick="deleteItem(\'' + i + '\')" class="btn btn-delete" type="button">Delete</button>';
+    document.getElementsByClassName('delete-box')[i].innerHTML = newButton;
+  }
+  
+  //LOL!!! I didn't read this sentence in the instructions:
+  //"Use e.currentTarget to access the "Delete" button that 
+  //was just clicked and select the parent node of an HTML element with parentNode."
+  //Learn e.currentTarget properly later.
+  
+  productTotal();
+  console.log(ProdsRemaining); 
 }
 
 //--------htmlCollectionToInnerHTMLArray function----------
