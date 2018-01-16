7ed9db61105d586356302482133de9ecadc5dde9
theRealSheng <sheng.huchai@gmail.com>
2018-01-16 09:17:14 +0100

    first commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,11 @@
+
 function deleteItem(e){
+  e.target.parentElement.parentElement.remove();
 
 }
 
 function getPriceByProduct(itemNode){
+  // var unitPrice = document.querySelectorAll('.unit-price')[i].innerText;
 
 }
 
@@ -11,6 +14,19 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+
+  var len = document.querySelectorAll('.unit-price').length;
+
+  var total = 0;
+  for (var i = 0; i < len; i++) {
+    var unitPrice = document.querySelectorAll('.unit-price')[i].innerText;
+    var numberItems = Number(document.querySelectorAll('.number-Items')[i].value);
+    var totalItem = Number(unitPrice.slice(1)) * numberItems;
+    total += totalItem;
+    document.querySelectorAll('.total')[i].innerText = "$" + totalItem.toFixed(2);
+  }
+
+  document.querySelector('.totalTotal').innerText = "$" + total.toFixed(2);
 
 }
 
@@ -40,11 +56,12 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  var inputs = document.querySelectorAll('.number-Items').value;
+  
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
