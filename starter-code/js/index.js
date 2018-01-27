217d1235e2c67c944db5212ada4602b2a71458a9
Alexinder Virdee <alex.virdee08@gmail.com>
2018-01-27 18:56:29 -0500

    working on changing h2 inner html for shop total

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -39,8 +39,12 @@ function createNewItem(){
 }
 
 window.onload = function(){
+
   var calculatePriceButton = document.getElementById('calc-prices-button');
+
   var createItemButton = document.getElementById('new-item-create');
+
+
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
 
@@ -66,10 +70,38 @@ window.onload = function(){
 
     var total = document.getElementById("total") 
     
-    total.innerHTML = price;
+    total.innerHTML = "$"+price+".00";
    
-    // now need to get above code to work for a second product
-    
+    // now need to implement code above to work for a second product
+    // get t shirt price 
+    var secondUnit = document.getElementById("secondUnit");
+    var secondUnitPrice = secondUnit.innerHTML;
+    secondUnitPrice = secondUnitPrice.slice(1, secondUnit.length);
+    console.log(secondUnitPrice);
+
+    // get the quantity of t shirts user requests
+    var shirtQuantity = document.querySelector(".shirt-quant");
+
+    console.log(shirtQuantity.value);
+
+    shirtQuantity = shirtQuantity.value;
+
+    // calculate total price: t shirts * quantity
+    var shirtPrice = shirtQuantity * secondUnitPrice;
+    console.log(shirtPrice);
+
+    var shirtTotal = document.getElementById("shirt-total");
+
+    shirtTotal.innerHTML = "$"+shirtPrice+".00";
+
+    // change total price h2 tag to show total amount based on user selection
+    var shopTotal = document.getElementById("totalPriceH2");
+
+    var completeTotal = shirtTotal + total;
+
+    shopTotal.innerHTML = completeTotal;
+    console.log(shopTotal);
+ 
   };
 
 
