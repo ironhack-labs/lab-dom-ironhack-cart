082bb147e279c2b6e61ffd1b494dc4414f1c8731
Alexinder Virdee <alex.virdee08@gmail.com>
2018-01-27 11:34:55 -0500

    styling

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -35,7 +35,7 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-
+  
 }
 
 window.onload = function(){
@@ -43,8 +43,24 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+
+  calculatePriceButton.onclick = function getTotalPrice() {
+    // variable for unit price
+    // variable for quantity of items
+    // innerhtml to the total className
+    var unitPrice = document.getElementById("initial");
+    var newUnitPrice = unitPrice.innerHTML;
+    newUnitPrice = newUnitPrice.slice(1, unitPrice.length);
+
+    console.log(newUnitPrice);
+
+    var quantity = document.querySelector(".quant-class");
+    console.log(quantity.value);
+    // var newQuant = document.querySelector(quantity);
+    // console.log(newQuant.value);
+    
+  };
+
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
