28fbad2e7af98130f54bc6ae1066361c2a98ab63
Rakeem <thomas.rakeem.29@gmail.com>
2018-01-28 17:59:12 -0500

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,14 +1,6 @@
-// Use at least 3 onclick events
-
-// Use at least one getElementById
-
-// Use at least one getElementsByTagName
-
-// Use at least one getElementsByClassName
-
 function deleteItem(e) {
-  const parent = e.currentTarget.parentElement.parentElement.parentElement;
-  parent.removeChild(e.currentTarget.parentElement.parentElement);
+  const parent = e.target.parentElement.parentElement.parentElement;
+  parent.removeChild(e.target.parentElement.parentElement);
 }
 
 function getPriceByProduct(itemNode) {
@@ -19,7 +11,6 @@ function getPriceByProduct(itemNode) {
   for (let i = 0; i < productCosts.length; i++) {
     let currentPrice = productCosts[i].innerHTML.slice(1);
     totalPriceEls[i].innerHTML = `Cost: $${(parseFloat(currentPrice) * quantityEls[i].value).toFixed(2)}`;
-    quantityEls[i].value = "";
   }
 }
 
@@ -31,7 +22,7 @@ function getTotalPrice() {
   const totalPriceEls = document.getElementsByClassName("unit-price");
   let totalCost = 0;
 
-  getPriceByProduct(first);
+  getPriceByProduct();
 
   for (let i = 0; i < totalPriceEls.length; i++) {
     totalCost += parseFloat(totalPriceEls[i].innerHTML.slice(7));
@@ -42,14 +33,20 @@ function getTotalPrice() {
 function createQuantityInput() {
   const input = document.createElement("input");
   input.setAttribute("name", "name");
+  input.setAttribute("type", "number");
+  input.setAttribute("class", "quantity-input");
   input.setAttribute("placeholder", "Enter product quantity");
   return input;
 }
 
 function createDeleteButton() {
+  const deleteDiv = document.createElement("div");
   const deleteButton = document.createElement("button");
-  button.setAttribute("class", "btn-delete");
-  return deleteButton;
+  deleteButton.setAttribute("class", "btn-delete");
+  deleteButton.classList.add("btn");
+  deleteButton.innerHTML = "Delete";
+  deleteDiv.appendChild(deleteButton);
+  return deleteDiv;
 }
 
 function createQuantityNode() {
@@ -57,6 +54,7 @@ function createQuantityNode() {
   const label = document.createElement("label");
   const qtyInput = createQuantityInput();
   label.setAttribute("for", "qty");
+  label.innerHTML = "Qty ";
   qtyDiv.appendChild(label);
   qtyDiv.appendChild(qtyInput);
   return qtyDiv;
@@ -71,7 +69,10 @@ function createItemNode(dataType, itemData) {
   return itemNode;
 }
 
-function createNewItemRow(itemName, itemUnitPrice) {
+function createNewItemRow(itemName, itemUnitCost) {
+  const parent = document.getElementById("first");
+  const createRow = document.getElementById("create-item");
+
   // create item div and add each node to it
   const itemContainer = document.createElement("div");
   itemContainer.setAttribute("class", "container");
@@ -79,13 +80,36 @@ function createNewItemRow(itemName, itemUnitPrice) {
   // name div
   const nameDiv = document.createElement("div");
   const nameSpan = document.createElement("span");
+  nameSpan.setAttribute("class", "unit-name");
   nameSpan.innerHTML = itemName;
   nameDiv.appendChild(nameSpan);
+  itemContainer.appendChild(nameDiv);
 
   // unit cost
-  const itemUnitPrice = itemUnitPrice;
-  const itemInput = createQuantityNode();
-  // const totalCostDiv =
+  const unitCostDiv = document.createElement("div");
+  const costSpan = document.createElement("span");
+  costSpan.setAttribute("class", "unit-cost");
+  costSpan.innerHTML = itemUnitCost;
+  unitCostDiv.appendChild(costSpan);
+  itemContainer.appendChild(unitCostDiv);
+
+  // qtyNode
+  const qytNode = createQuantityNode();
+  itemContainer.appendChild(qytNode);
+
+  // total unit cost
+  const totalCostDiv = document.createElement("div");
+  const unitPriceSpan = document.createElement("span");
+  unitPriceSpan.setAttribute("class", "unit-price");
+  unitPriceSpan.innerHTML = "$0";
+  totalCostDiv.appendChild(unitPriceSpan);
+  itemContainer.appendChild(totalCostDiv);
+
+  // deleteBtnDiv
+  const deleteBtnDiv = createDeleteButton();
+  itemContainer.appendChild(deleteBtnDiv);
+  // insert new .container before all-items-cost and after last .container
+  parent.insertBefore(itemContainer, createRow);
 }
 
 function createNewItem() {
@@ -106,7 +130,18 @@ document.addEventListener("DOMContentLoaded", function() {
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].onclick = deleteItem;
-  }
+  document.addEventListener("click", function(e) {
+    for (let i = 0; i < e.target.classList.length; i++) {
+      if (e.target.classList[i] === "btn-delete") {
+        deleteItem(e);
+      }
+    }
+  });
+  createItemButton.addEventListener("click", function() {
+    const itemName = document.getElementById("new-item-name");
+    const itemPrice = document.getElementById("new-item-price");
+    createNewItemRow(itemName.value, itemPrice.value);
+    itemName.value = "";
+    itemPrice.value = "";
+  });
 });
