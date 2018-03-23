86c72641f8c5c2874290952b27917f83ccc0a6cb
aberouch <a.aberouch@gmail.com>
2018-03-23 18:02:49 +0100

    Punto 1 completado·

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,6 +11,14 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+    console.log("Total price clicked!");
+    var price = document.getElementById("priceValue").textContent;
+    var quantity = document.getElementById("quantity").value;
+    console.log("Price is: " + price + " and qty is: " + quantity);
+
+    var totalPrice = parseFloat(price) * quantity;
+
+    document.getElementById('totalPrice').innerHTML = '$' + totalPrice;
 
 }
 
