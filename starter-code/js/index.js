a2b88c1ae8d7950398f631a6c015c31b754d5939
Raul.dll <rdl.llave@gmail.com>
2018-03-24 21:25:16 +0100

    Iteracion 1 completed

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,18 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var price = document.getElementById("price").getAttribute("value");
+  console.log(price);
+  var quantity = document.getElementById("quantity").value;
+  console.log(quantity);
+  quantity=parseInt(quantity);
+  var totalPrice = document.getElementById("total-price")
+  totalPrice.innerHTML = price*quantity;
+totalPrice=parseFloat(totalPrice.innerHTML);
+  console.log(totalPrice);
 
+
+  
 }
 
 function createQuantityInput(){
@@ -40,13 +51,28 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
+  console.log(calculatePriceButton);
+  calculatePriceButton.onclick = getTotalPrice;
+
   var createItemButton = document.getElementById('new-item-create');
+  
+  createItemButton.onclick = createNewItem;
+
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  
+
+  
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+}
+
+var price;
+function generateRandomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+generateRandomColor();
+console.log(generateRandomColor());
