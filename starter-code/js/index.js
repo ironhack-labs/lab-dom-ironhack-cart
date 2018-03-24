e2bf80d9884022e4ba11f259397fc52ca4d74979
luisnueda <nueda.luis@gmail.com>
2018-03-24 19:23:15 +0100

    iteración 1 completada

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,26 +1,3 @@
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  
-  
-  
-  
-  
-  
-  
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-
-
-
-  
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
 function deleteItem(e){
 
 }
@@ -34,6 +11,16 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+
+  // accedemos y guardamos en una variable los valores
+  var costeUnitario = document.getElementById("coste-unitario").getAttribute("value");
+  var quantity = document.getElementById("quantity").value;
+  var totalPrecio = document.getElementById("total-precio");
+
+  //Le añadimos a la etiqueta total-precio el atributo value, con el valor de la multiplicación.
+  totalPrecio.setAttribute("value", costeUnitario * quantity);
+  //Modificamos el valor en el html
+  totalPrecio.innerHTML = costeUnitario * quantity;
 
 }
 
@@ -61,4 +48,15 @@ function createNewItem(){
 
 }
 
+window.onload = function(){
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
 
+  calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+};
