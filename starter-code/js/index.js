3660e880dc83b1078279daee35e5eee7e01dd922
Javier Valiente <javara00@gmail.com>
2018-03-24 23:08:05 +0100

    Beltrán, halp please

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,60 +1,91 @@
-function deleteItem(e){
 
-}
+// function deleteItem(e){
 
-function getPriceByProduct(itemNode){
+// }
 
-}
+// function getPriceByProduct(itemNode){
 
-function updatePriceByProduct(productPrice, index){
+// }
 
-}
+// function updatePriceByProduct(productPrice, index){
+
+// }
 
 
+// function createQuantityInput(){
+
+// }
+
+// function createDeleteButton(){
+
+// }
+
+// function createQuantityNode(){
+
+// }
+
+// function createItemNode(dataType, itemData){
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
+
+// window.onload = function(){
   
-   
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+//   var deleteButtons = document.getElementsByClassName('btn-delete');
+
+//   // calculatePriceButton.onclick = getTotalPrice;
+//   createItemButton.onclick = createNewItem;
+
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
   
-    
-  
+//   document.getElementsByClassName("btn").onclick = function() {
+//     console.log("fdsadvsdv")
+//   } 
+// };
+console.log("connected");
 
+var calculate = document.getElementById("calculate");
 
-function createQuantityInput(){
+var items = document.getElementsByClassName("item");
+var prices = document.getElementsByClassName("price");
+var inputs = document.getElementsByTagName("input");
+var subtotals = document.getElementsByClassName("price-subtotal")
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = function () {
+    this.setAttribute("value", "");
   }
-  
-  document.getElementsByClassName("btn").onclick = function() {
-    console.log("fdsadvsdv")
-  } 
-};
+}
+
+
+
+  calculate.onclick = function () {
+    for (var i = 0; i < items.length; i++) {
+    var counter = 0;
+    var inputValue = parseFloat(inputs[i].value);
+    var priceValue = parseFloat(prices[i].innerHTML);
+    counter = priceValue * inputValue;
+    subtotals[i].innerHTML = counter;
+    console.log("hola")
+    }
+    
+}
+
+
+
+
+
+
+
+
