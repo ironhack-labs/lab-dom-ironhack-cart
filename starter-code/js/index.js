9664d39cf3c6ba9e349c2e4484e6835cc1722101
aacecandev <aacecan.dev@gmail.com>
2018-03-24 14:18:43 +0100

    Segunda iteración completada

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,8 +2,11 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemPrice, itemQuantity){
 
+  var price = parseFloat(itemPrice);
+  var quantity = parseFloat(itemQuantity);
+  return price * quantity;
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +14,22 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var products = document.getElementsByClassName("product-wrapper");
 
+  for(var i=0; i<products.length; i++){
+    itemPrice = products[i].childNodes[3].getAttribute("value");
+    itemQuantity = products[i].getElementsByClassName("quantity-text")[0].value;
+  
+    if(itemQuantity === ""){
+      alert("Introduce una cantidad");
+    } else {
+      var total = getPriceByProduct(itemPrice, itemQuantity);
+      products[i].childNodes[7].setAttribute("value", 2);
+      products[i].childNodes[7].innerHTML = "$" + total;
+    }
+  }
+  
+  // total-price = products[0].childNodes[7].getAttribute("value")
 }
 
 function createQuantityInput(){
@@ -50,3 +68,5 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
