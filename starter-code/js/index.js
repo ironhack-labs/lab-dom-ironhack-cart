9a6cd61c84c94c9ec01cec860f5157f34fa08dbd
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-25 20:55:27 +0200

    Todo funciona correctamente, menos la oganización css

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,7 @@
 
 function deleteItem(pepe){
-  var margin = document.getElementsByClassName("qq")[0];
+  var margin = document.body;
   var dTag = pepe.currentTarget.parentNode.parentNode;
-  console.log(dTag);
-  console.log(margin);
   margin.removeChild(dTag);
 }
 
@@ -57,16 +55,23 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
   var miDiv = document.createElement("div");
   miDiv.setAttribute("class", "margin");
-  miDiv.innerHTML =( "<span>IronBubble-head</span>" + "<span class=\"a\" >$25.00</span>" +  "<label for=\"Cantidad\">QTY</label>" +  "<input type=\"number\" name=\"Cantidad\" class=\"b\">" + "<span class=\"multi\" >$0.00</span>" +  "<div><button class=\"btn btn-delete\">Delete</button>");
-  console.log("Peina para atras!")
-  
+  miDiv.innerHTML =( "<span class=\"nameproduc\"></span>" + "<span class=\"a\" ></span>" +  "<label for=\"Cantidad\">QTY</label>" +  "<input type=\"number\" name=\"Cantidad\" class=\"b\">" + "<span class=\"multi\" >$0.00</span>" +  "<div><button class=\"btn btn-delete\">Delete</button>");
+
+
   // document.getElementsByClassName("margin").appendChild(miDiv);
   if (document.getElementsByClassName("margin")[document.getElementsByClassName("margin").length-1].nextSibling){
     document.getElementsByClassName("margin")[document.getElementsByClassName("margin").length-1].parentNode.insertBefore(miDiv,document.getElementsByClassName("margin")[document.getElementsByClassName("margin").length-1].nextSibling);
   } else {
     document.getElementsByClassName("margin")[document.getElementsByClassName("margin").length-1].parentNode.appendChild(miDiv);
   }
-  document.getElementsByClassName("margin")[document.getElementsByClassName("margin").length-1].value = 25.00;
+  document.getElementsByClassName("nameproduc")[document.getElementsByClassName("margin").length-1].innerHTML = document.getElementById("producname").value;
+  document.getElementsByClassName("a")[document.getElementsByClassName("margin").length-1].innerHTML ="$" + parseFloat(document.getElementById("price").value).toFixed(2);
+  document.getElementsByClassName("a")[document.getElementsByClassName("margin").length-1].value = document.getElementById("price").value;
+  document.getElementById("producname").value = "";
+  document.getElementById("price").value = "";
+  for(var i = 0; i<document.getElementsByClassName('btn-delete').length ; i++){
+    document.getElementsByClassName('btn-delete')[i].onclick = deleteItem;
+  }
 }
 
 window.onload = function(){
