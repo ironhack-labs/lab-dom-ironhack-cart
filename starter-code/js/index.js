a2ba42d6c7c2a4612d01fc307522517f20545113
Louis Masurel <masurellouis@gmail.com>
2018-03-25 18:01:48 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,42 @@
-function deleteItem(e){
-
-}
-
 function getPriceByProduct(itemNode){
 
-}
+  var getPrice = document.querySelector(".price").innerHTML;
+    console.log (getPice);
 
-function updatePriceByProduct(productPrice, index){
+  var getNumberOfProduct = document.querySelector(".unit-input").innerHTML;
+  console.log (getNumberOfProduct);
 
+  var getPriceByProduct = getPrice * getNumberOfProduct ;
+  console.log (getPriceByProduct);
+
+  var theCost = document.querySelector(".cost").innerHTML;
+  console.log (theCost);
+
+  getPriceByProduct.replaceChild(theCost)
+  console.log (getPriceByProduct);
 }
 
 function getTotalPrice() {
 
+  var calculteButton = document.querySelector (".btn-success")
+
+  calculateButton.onclick = function(calcuteTotalPrice){
+    
+    var clickedBtnCalculate = event.target; 
+
+    var theTotalCost = clickedBtnCalculate.parentNode;
+
+    var theTotalCost = document.querySelectorAll (".cost").innerHTML
+    
+    var totalCost = document.querySelector(".total-cost").innerHTML;
+
+    theTotalCost.replaceChild(totalCost) 
+
+  }
+}
+
+function updatePriceByProduct(productPrice, index){
+  var cal
 }
 
 function createQuantityInput(){
@@ -49,4 +74,31 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+};
+
+- Use at least 3 `onclick` events
+- Use at least one `getElementById`
+- Use at least one `getElementsByTagName`
+- Use at least one `getElementsByClassName`
+
+
+var products [
+  {tshirtIronHack :   "10,50"},
+  {cupIronHack :      "5,50"},
+  {stickerIronHack :  "1,50"},
+]
+
+function deleteItem(e){
+ 
+  var deleteButton = document.querySelector(".btn-delete");
+  
+    deleteButton.onclick = function (deleteItem) {
+      var clickedButton = event.target; 
+      var clickedDelete = clickedButton.parentNode;
+                                         
+      var unitInput = document.querySelector("unit-input");
+      console.log(unitInput)
+      unitInput.replaceChild (clickedDelete);
+      newInput.value = "0";
+    };
 };
