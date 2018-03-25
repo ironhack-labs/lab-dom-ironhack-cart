c2acf44ca8a3eabddfcb3138c9d0aa8cadeb24c2
muflonex <muflonex@gmail.com>
2018-03-25 11:46:03 +0200

    Inicio del proyecto

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -9,8 +9,20 @@ function getPriceByProduct(itemNode){
 function updatePriceByProduct(productPrice, index){
 
 }
-
+function updateQuantity (){
+  input.onkeyup.setAttribute(name, value);
+}
 function getTotalPrice() {
+  //We initialize our price variable at 0
+  var totalPrice = 0;
+  //
+  var products = document.getElementsByClassName('product')
+
+  for ( i = 0 ; i < products.length ; i++){
+    totalPrice 
+  }
+  console.log(input*price)
+return input * price
 
 }
 
@@ -42,6 +54,9 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var input = document.getElementById('quantity')[0]
+  var price = document.getElementsByClassName('price')[0]
+  
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
