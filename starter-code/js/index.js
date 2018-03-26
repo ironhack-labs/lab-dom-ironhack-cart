fe3cbec73c9fee991fb0c76e78e406eefc29508e
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-03-26 16:07:43 +0200

    first version

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,15 @@
+// document.getElementById('site-header')
+// document.getElementsByClassName('feature')
+// document.querySelector('.features .feature')
+// document.querySelectorAll('.features .feature')
+// node.appendChild(child)
+// node.remove()
+// newElement = document.createElement('div')
+
+
+
+/*
+
 function deleteItem(e){
 
 }
@@ -37,11 +49,55 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
+*/
+// Create a click event for the Calculate Prices button
+// This event will execute a function that:
+  // Retrieves the unit price of the product
+  // Retrieves the quantity of items desired
+  // Calculates the total price based on this data
+  // Updates the total price in the DOM
 
-window.onload = function(){
+window.onload = function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  
+  // Create a click event for the Calculate Prices button
+  calculatePriceButton.addEventListener('click', calculatePrice);
+
+/*
+  calculatePriceButton.onclick = function() {
+    var priceProduct = document.getElementById('priceProduct');
+    console.log(priceProduct);
+  }
+  */
+
+};
+
+function calculatePrice() {
+  // Retrieves the unit price of the product
+  var priceProduct = document.getElementById('priceProduct').innerHTML;
+  var priceProductNumber = parseInt(priceProduct.replace(/[^\d\.]/ ,''));
+  console.log(typeof priceProductNumber);
+  console.log(priceProductNumber);
+
+  // Retrieves the quantity of items desired
+  var quantityItems = parseInt(document.getElementById('quantityItems').value);
+  console.log(typeof quantityItems);
+  //quantityItemsNumber = parseInt(quantityItems)
+  console.log(quantityItems);
+
+  // Calculates the total price based on this data
+  var totalPrice = priceProductNumber * quantityItems;
+  console.log(totalPrice);
+
+  // Updates the total price in the DOM
+  document.getElementById('totalPrice').innerHTML = totalPrice;
+}
+
+
+
+/*
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
@@ -49,4 +105,5 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+  */
+
