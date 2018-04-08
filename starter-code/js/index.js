b0f6f04c68f0a9484f03c29041b7e4c006297f38
Maximo Campo <themaxomoto@gmail.com>
2018-04-07 21:37:46 -0500

    terminado hasta la cuarta iteracion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,27 @@
-function deleteItem(e){
 
-}
-
-function getPriceByProduct(itemNode){
-
-}
+var calculatePriceButton = document.getElementById('calc-prices-button');
+var createItemButton = document.getElementById('new-item-create');
+var deleteButtons = [document.getElementsByClassName('btn-delete')[0], document.getElementsByClassName('btn-delete')[1]];
+var products = document.getElementsByClassName('product');
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
+  var totalPrice = document.getElementById('total-price');
+  var priceUnit = [document.getElementsByTagName('span')[2],document.getElementsByTagName('span')[8]];
+  var qty = [document.getElementsByTagName('input')[0], document.getElementsByTagName('input')[1]];
+  var totalValues = [document.getElementsByTagName('span')[5], document.getElementsByTagName('span')[11]]
 
+  totalPrice.innerHTML = 0;
+
+  for(var i = 0; i < qty.length; i++){
+    totalValues[i].innerHTML = priceUnit[i].innerHTML * qty[i].value;
+    totalPrice.innerHTML = parseInt(totalPrice.innerHTML) + parseInt(totalValues[i].innerHTML) 
+    }
 }
+
 
 function createQuantityInput(){
 
@@ -38,15 +47,13 @@ function createNewItem(){
 
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+calculatePriceButton.onclick = getTotalPrice;
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+for(var i = 0; i < deleteButtons.length ; i++){
+  deleteButtons[i].onclick = function(e){
+    var allProducts = document.getElementById('all-products');
+    var products = document.getElementsByClassName('product');
+    e.currentTarget.parentNode.parentNode.removeChild(products[deleteButtons.indexOf(e.currentTarget)]);
+    deleteButtons.splice(deleteButtons.indexOf(e.currentTarget),1);
+}
+}
