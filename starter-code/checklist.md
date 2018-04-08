b2a97aa4020c6396f3c716ce961f6cd1043d68d9
Ryan Martin <rmart106@fiu.edu>
2018-04-08 17:34:14 -0400

    Able to update values in the DOM so far.

---
+++ starter-code/checklist.md
@@ -0,0 +1,8 @@
+- [X] Create a click event for the Calculate Prices button.
+
+This event will execute a function that:
+
+- [X] Retrieves the unit price of the product
+- [X] Retrieves the quantity of items desired
+- [X] Calculates the total price based on this data
+- [ ] Updates the total price in the DOM
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,27 @@
+function calculatePriceButton(){
+  
+  var getPriceID = document.getElementById('cost-of-item'); // Gets the priceID
+  var itemPrice = getPriceID.innerHTML; // Gets the price from the ID.
+  itemPrice = itemPrice.replace("$",""); // Removes the $ from the price.
+  itemPrice = Number(itemPrice); // Turns price into a #.
+
+  var quantityInput = document.getElementById('qty-input').value;  // Grabs user input for quantity.
+  quantityInput = Number(quantityInput); // Turns quantity into a #.
+
+  var inputTotal = itemPrice * quantityInput; // inputTotal is the item price multiplied by quantity.
+
+  var totalPrice = document.getElementById('total-price'); 
+  var currentQtyTotal = totalPrice.innerHTML; // Gets the totalPrice. 
+
+  currentQtyTotal = currentQtyTotal.replace("$",""); // Removes the $ from the total.
+  currentQtyTotal = Number(currentQtyTotal); // Turns currentQtyTotal into a #.
+
+  var updatedQtyTotal = currentQtyTotal + inputTotal;
+
+  return document.getElementById('total-price').innerHTML = "$" + updatedQtyTotal;
+};
+
+
 function deleteItem(e){
 
 }
@@ -37,6 +61,8 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
+
+
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
