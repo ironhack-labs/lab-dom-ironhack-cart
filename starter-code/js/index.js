0289db566bfd279ac2a9ac1314786ee8165b378d
Diego Leal <diegoleallinux@gmail.com>
2018-04-08 20:38:38 -0400

    not finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,16 +1,39 @@
-function deleteItem(e){
+// Use at least 3 onclick events
 
+// Use at least one getElementById
+
+// Use at least one getElementsByTagName
+
+// Use at least one getElementsByClassName
+var getProduct = document.getElementsByClassName('product-name');
+var getUnitPrice = document.getElementsByClassName('unit-cost');
+var getQuantity = document.getElementsByClassName('quantity')[0];
+var getTotalProductPrice = document.getElementsByClassName('total-product-price')
+var deleteButtons = document.getElementsByClassName('btn-delete');
+
+var unitPrice = getUnitPrice[0].innerHTML.split("$")[1];
+var quantity = getQuantity.nodeValue;
+getTotalProductPrice[0].setAttribute('total-product-price', getTotalPrice());
+
+function getTotalPrice(){
+  for (var i = 0; i < getProduct.length; i++){
+    var totalCost = unitPrice[i] * quantity[i];
+  }
+  return totalCost;
+}
+
+function deleteItem(e){
+  var row = document.getElementsByClassName('row');
+  for (var i = 0; i < row.length; i++){
+  }
+  document.appendChild(row)
 }
 
 function getPriceByProduct(itemNode){
-
+  
 }
 
 function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
 
 }
 
@@ -35,14 +58,13 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-
+  
 }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
