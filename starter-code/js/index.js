de97cb24978c4c16884c8fc27e9ee7c94f6028e1
Fernanda <mfernandasrmz8201@gmail.com>
2018-04-09 11:53:22 -0500

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,15 +1,37 @@
+/*Calculating the total price for the product
+Once you have the HTML and CSS ready, use JavaScript and functions of the document object to retrieve 
+the data you need to calculate the total price for that product. 
+Change the value of the element in the DOM that displays the total price:
+
+Create a click event for the Calculate Prices button
+This event will execute a function that:
+Retrieves the unit price of the product
+Retrieves the quantity of items desired
+Calculates the total price based on this data
+Updates the total price in the DOM */
+
+
+
+
+
 function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
 
+
+function getPriceByProduct(number){
+var unitPrice = parseFloat(document.getElementById("unit-price").textContent); 
+var numberOfProducts = document.getElementById("quantity").value;
+var totalUnitPrice = unitPrice * +numberOfProducts;
+
+document.getElementById("total-price-for-product").innerHTML = totalUnitPrice; 
 }
-
 function updatePriceByProduct(productPrice, index){
 
-}
 
+  
+}
 function getTotalPrice() {
 
 }
@@ -34,11 +56,12 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
-function createNewItem(){
-
+function createNewItem(name){
+var newProduct = document.getElementsByClassName ("item").textContent
+newProduct[1].innerHTML = "IronShirt";
 }
 
-window.onload = function(){
+/*window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -49,4 +72,4 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+};*/
