72dd0dcc77a74f9ed39ce17f4af903061926bd54
fodon11 <francisco.odon11@gmail.com>
2018-04-15 13:11:17 -0400

    1/4 of the way done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,8 +2,18 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+//calculate's product's individual price
+// let button = document.getElementById("calc-price")
+// button.onClick = function(){
+//   let x = document.getElementById('price');
+//   let pPrice = x.textContent;
+//   let y = document.getElementById('quantity').value;
+//   return pPrice * y
+//   document.getElementById("priceq").innerHTML = "$" + productPrice();
+//   }
 
+function getPriceByProduct(itemNode){
+  
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +21,24 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  for (var i=0; i< document.getElementsByClassName('price').length; i++){
+    let x = document.getElementsByClassName('price')[i].innerHTML;
+    let y = document.getElementsByClassName('quantity')[i].value;
+    pPrice = x*y;
+    document.getElementsByClassName('product-price')[i].innerHTML = '$' + pPrice;
+  }
+  for (var i=0; i<document.getElementsByClassName('product-price').length; i++){
+    let x = document.getElementsByClassName('product-price')[i].textContent;
+    document.getElementsByClassName('total-price')[0].innerHTML = x;
+  }
+  // let x = document.getElementsByClassName('product-price')[0].textContent;
+  //   document.getElementsByClassName('total-price')[0].innerHTML = x;
 
+  // let x = document.getElementsByClassName('price')[0];
+  // let pPrice = x.textContent;
+  // let y = document.getElementsByClassName('quantity').value;
+  // let productPrice = pPrice * y;
+  // document.getElementsByClassName('priceq').innerHTML = "$" + productPrice;
 }
 
 function createQuantityInput(){
