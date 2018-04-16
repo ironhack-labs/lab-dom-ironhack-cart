e3bfcd564af641b4feaecf1f7e9b4b6908742305
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-04-16 16:11:10 -0400

    Add, update total price, Finish Iteration 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -10,8 +10,8 @@ function updatePriceByProduct(productPrice, index){
   return productPrice * index;
 }
 
-function getTotalPrice() {
-
+function getTotalPrice(arrOfPricesPerItem) {
+  return arrOfPricesPerItem.reduce((accum, curr) => accum + curr, 0);
 }
 
 function createQuantityInput(){
@@ -53,25 +53,31 @@ window.onload = function(){
   // Create a click event for the Calculate Prices button
 
   let button = document.getElementsByClassName('btn-success')[0]; // access button
+  let tPriceNode = document.getElementById('total-price'); // access total price in html
 
   button.onclick = function() {
     let children = document.getElementsByClassName('items-price'); // define an array of children to remove
     let parents = document.getElementsByClassName('parent'); // define an array of parents to remove/appendChild
     let pArr = Array.prototype.slice.call(parents); // convert HTML Collection into an Array
     let cArr = Array.prototype.slice.call(children); // convert HTML Collection into an Array
-    let node, input, updated, text, span;
+    let node, input, updated, text, span, subtotal;
+    let totalPrice = 0;
 
     for (let i = 0; i < pArr.length; i++) {
       node = document.getElementsByClassName('unit-price')[i].innerHTML; // access unit price node
       input = document.getElementsByTagName('input')[i]; // get the users input
       updated = updatePriceByProduct(getPriceByProduct(node), input.value); // multiply item-price * qty
-      text = document.createTextNode('$' + updated.toString() + '.00'); // convert product into textNode
+      text = document.createTextNode('$' + updated.toString()); // convert product into textNode
       span = document.createElement('span'); // create span element
       span.setAttribute('class', 'items-price'); // set class attibute to span
       pArr[i].removeChild(cArr[i]); // remove outdated item price
       pArr[i].appendChild(span); // append span element to div
       span.appendChild(text); // append updated price to span
+      subtotal = parseInt(span.innerHTML.slice(1)); // get subtotal. Number type.
+      totalPrice += subtotal; // adds subtotals
     }
+
+    tPriceNode.innerHTML = '$' + totalPrice.toString(); // updates total price.
   }
 
 };
