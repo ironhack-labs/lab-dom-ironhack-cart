6b762dd920721a27e4787b01abb000a52994d069
fodon11 <francisco.odon11@gmail.com>
2018-04-15 22:50:26 -0400

    Currently attempting to get the deleteItem button to delete it's respective item. No luck so far

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,10 @@
-function deleteItem(e){
-
-}
+function deleteItem(e) {
+    e.currentTarget.parentNode;
+    for (var i = 0; i < this.deleteButtons.length; i++){
+    let x = document.getElementsByClassName('item')[i];
+    let body = document.getElementsByTagName('body')[i];
+    body.removeChild(x);
+}}
 
 //calculate's product's individual price
 // let button = document.getElementById("calc-price")
@@ -12,68 +16,56 @@ function deleteItem(e){
 //   document.getElementById("priceq").innerHTML = "$" + productPrice();
 //   }
 
-function getPriceByProduct(itemNode){
-  
+function getPriceByProduct(itemNode) {
+  for (var i = 0; i < document.getElementsByClassName("price").length; i++) {
+    let x = document.getElementsByClassName("price")[i].innerHTML;
+    let y = document.getElementsByClassName("quantity")[i].value;
+    let pPrice = x * y;
+    document.getElementsByClassName("product-price")[i].innerHTML =
+      pPrice;
+  }
 }
 
-function updatePriceByProduct(productPrice, index){
-
-}
+function updatePriceByProduct(productPrice, index) {}
 
 function getTotalPrice() {
-  for (var i=0; i< document.getElementsByClassName('price').length; i++){
-    let x = document.getElementsByClassName('price')[i].innerHTML;
-    let y = document.getElementsByClassName('quantity')[i].value;
-    pPrice = x*y;
-    document.getElementsByClassName('product-price')[i].innerHTML = '$' + pPrice;
-  }
-  for (var i=0; i<document.getElementsByClassName('product-price').length; i++){
-    let x = document.getElementsByClassName('product-price')[i].textContent;
-    document.getElementsByClassName('total-price')[0].innerHTML = x;
-  }
-  // let x = document.getElementsByClassName('product-price')[0].textContent;
-  //   document.getElementsByClassName('total-price')[0].innerHTML = x;
-
-  // let x = document.getElementsByClassName('price')[0];
-  // let pPrice = x.textContent;
-  // let y = document.getElementsByClassName('quantity').value;
-  // let productPrice = pPrice * y;
-  // document.getElementsByClassName('priceq').innerHTML = "$" + productPrice;
+  getPriceByProduct();
+  for (
+    var i = 0, l = []; i < document.getElementsByClassName("product-price").length; i++){
+    let totalPrice = eval(document.getElementsByClassName("product-price")[i].textContent);
+    l.push(totalPrice);
+    }
+    // let price = 0;
+    // for (var i =0; i<l.length; i++){
+    //   price += l[i];  
+    // }
+    let pop = l.reduce((acc, elem) => {
+      return acc + elem;
+      }, 0)
+  document.getElementsByClassName("total-price")[0].innerHTML = pop;
 }
 
-function createQuantityInput(){
+function createQuantityInput() {}
 
-}
+function createDeleteButton() {}
 
-function createDeleteButton(){
+function createQuantityNode() {}
 
-}
+function createItemNode(dataType, itemData) {}
 
-function createQuantityNode(){
+function createNewItemRow(itemName, itemUnitPrice) {}
 
-}
+function createNewItem() {}
 
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+window.onload = function() {
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  var createItemButton = document.getElementById("new-item-create");
+  var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  deleteButtons = function(){for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
-  }
+  }}
 };
