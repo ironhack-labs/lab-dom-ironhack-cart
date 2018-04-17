8344202da152b490af591f8340e20a9485337593
ruben <rubenbustos33@gmail.com>
2018-04-16 21:09:25 -0400

    Restructured layout, and added todo list

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,60 @@
-function deleteItem(e){
+// let price = document.querySelector('.price').innerHTML;
+// let qty = document.querySelector('.quantity').value;
+// let total = document.querySelector('.total');
 
+function deleteItem(e) {
+  e.currentTarget.parentNode;
+  for (var i = 0; i < this.deleteButtons.length; i++) {
+    let x = document.getElementsByClassName('item')[i];
+    let body = document.getElementsByTagName('body')[i];
+    body.removeChild(x);
+  }
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
+function getPriceByProduct(itemNode) {
+  for (var i = 0; i < document.getElementsByClassName('price').length; i++) {
+    let x = document.getElementsByClassName('price')[i].innerHTML;
+    let y = document.getElementsByClassName('quantity')[i].value;
+    let pPrice = x * y;
+    document.getElementsByClassName('product-price')[i].innerHTML = pPrice;
+  }
 }
 
 function getTotalPrice() {
-
+  getPriceByProduct();
+  var newPricesArray = [];
+  for (var i = 0; i < allProducts.length; i++) {
+    var singleProductTotalPrice = Number(updatedPricesArray[i].innerHTML);
+    console.log('blah: ', singleProductTotalPrice);
+    newPricesArray.push(singleProductTotalPrice);
+    console.log('newPricesArray: ', newPricesArray);
+  }
+  var total = newPricesArray.reduce(sumOfTwoNumbers, 0);
+  console.log('total is: ', total);
+  document.getElementById('total-price').innerHTML = total;
 }
 
-function createQuantityInput(){
+// Pending - Todo
+// function updatePriceByProduct(productPrice, index) {}
+// function createDeleteButton() {}
+// function createQuantityInput() {}
+// function createQuantityNode() {}
+// function createItemNode(dataType, itemData) {}
+// function createNewItemRow(itemName, itemUnitPrice) {}
+// function createNewItem() {}
+//  /Pending
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+window.onload = function() {
+  let calculatePriceButton = document.getElementById('calc-prices-button');
+  let createItemButton = document.getElementById('new-item-create');
+  let deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  deleteButtons = function() {
+    for (let i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].onclick = deleteItem;
+    }
+  };
 };
