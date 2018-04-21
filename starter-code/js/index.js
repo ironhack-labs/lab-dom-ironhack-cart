1156539cc80c89dd4dc0f08d1216c0747e06de77
Teresa Plaz <teresaplaz@gmail.com>
2018-04-21 08:13:02 -0400

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,15 +1,29 @@
 
 
-function deleteItem(e){
+function deleteItem(){
+  
+  var deleting = document.getElementsByTagName("ul")[0];
 
+      deleting.onclick = function(e) {
+        if (e.target.className === "btn-delete")
+          {
+            // going from delete button to li
+
+            var li = e.target.parentNode.parentNode.parentNode;
+            deleting.removeChild(li);
+          }
+       };
 }
 
 function getPriceByProduct(){
-  var obj1Elem = document.getElementsByTagName("p");
+  var obj1Elem = document.getElementsByClassName("objsPrices");
   var actualPrices = [];
+
+    // taking an array of the prices
+
     for (i=0; i < obj1Elem.length; i++)
     {
-      actualPrices.push(Number(obj1Elem[i].textContent));
+      actualPrices.push(Number(obj1Elem[i].textContent.replace('$', '')));
     }
   return actualPrices;
 }
@@ -17,12 +31,12 @@ function getPriceByProduct(){
 
 
 function updatePriceByProduct(){
-  var button = document.getElementsByClassName("btn-success")[0];
+  var button = document.getElementById("calculate-btn");
   button.onclick = function(){
     var productPrice = getPriceByProduct();
     var productInput = createQuantityInput();
 
-      for (i=0; i < productInput.length; i++)
+      for (i=0; i < productPrice.length; i++)
       {
         var newTotal = productPrice[i]*productInput[i];
         var newElem = document.getElementsByClassName('total');
@@ -50,6 +64,7 @@ function getTotalPrice() {
       }
      
       // using reduce on the numbers to get the total sum
+
       total = grandTotalArray.reduce(function(a,b){
         return a+b;
       },0);
@@ -63,44 +78,36 @@ function createQuantityInput(){
   var actualInputs = [];
     for (i=0; i < inputQuantity.length; i++)
     {
+      // making an array of given quantities in text and changing them to numbers
+
       actualInputs.push(Number(inputQuantity[i].value));
     }
   return actualInputs;
   
 }
 
-
-
-function createDeleteButton(){
-
+function takeNewItemInput()  {
+  var createBtn = document.getElementsByClassName('createBtn')[0];
+  createBtn.onclick = function (){
+    var newItemName = document.getElementsByClassName('newItem1')[0].value;
+    var newItemPrice = document.getElementsByClassName('newItem2')[0].value;
+    createNewItem(newItemName,newItemPrice);
+  };
+  
+  
 }
 
-function createQuantityNode(){
-
+function createNewItem(newItemName,newItemPrice){
+  var newItemParent = document.getElementsByTagName('ul')[0];
+  var newItemModel = '<div class="nodeWrapper"> <div class="space "> <span> ' + newItemName + ' </span></div> <div class="space "><span class="objsPrices"> $' + newItemPrice + ' </span></div><div class="space "><form><label for="quantity">QTY</label><input type="number" class="input" id="btn-quantity"></form></div><div class="space "><span class="total">0</span></div><div class="space "><button type="button" class="btn-delete">Delete</button></div></div>';
+  var newItemContainer = document.createElement('li');
+  newItemContainer.innerHTML = newItemModel;
+  newItemParent.appendChild(newItemContainer);
 }
 
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
 
 window.onload = function(){
   updatePriceByProduct();
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  deleteItem();
+  takeNewItemInput();
 };
