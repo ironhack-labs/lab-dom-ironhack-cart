e9a03c8038a984a3fa118174ecd8c8ef21460e8a
fran <fmls1989@gmail.com>
2018-05-12 21:57:52 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,80 @@
-function deleteItem(e){
+var buttonCalculate = document.getElementsByClassName('calculate')[0];
+var quantityInput = document.querySelector('#quantity');
+var totalIndividual = document.getElementsByClassName('total');
 
-}
+var totalSumSpan = document.getElementsByClassName('totalSum')[0];
 
-function getPriceByProduct(itemNode){
+//elements
+var newItem = document.getElementById('new-item-create');
+var container = document.getElementsByClassName('container')[0];
+var deleted = document.getElementsByClassName('btn-delete');
 
-}
+//Create element
+newItem.onclick = function (){
+  
+  //row flex
+  var newRow = document.createElement('div');
+  newRow.classList.add('flex'); newRow.style.margin = '5px';
+  
+  //div name
+  var newDivName = document.createElement('div'), newLabelName = document.createElement('input');
+  newLabelName.setAttribute('placeholder', 'Name...');
+  newDivName.appendChild(newLabelName); newRow.appendChild(newDivName);  
+  
+  ////div price
+  var newDivPrice= document.createElement('div'), newLabelPrice = document.createElement('input');
+  newDivPrice.appendChild(newLabelPrice); newRow.appendChild(newDivPrice);
+  newLabelPrice.style.width = '50px';
+  
+  //div quantity
+  var newDivQuantity = document.createElement('div'), newLabel = document.createElement('label'), newInput = document.createElement('input') ;
+  newDivQuantity.classList.add('quantity'); newLabel.innerText = 'QTY';
+  newLabel.appendChild(newInput); newDivQuantity.appendChild(newLabel); newRow.appendChild(newDivQuantity); 
+  
+  //span total
+  var newSpanTotal = document.createElement('span'), newSpanHolder = document.createElement('span');
+  newSpanTotal.classList.add('total');
+  newSpanTotal.appendChild(newSpanHolder); newRow.appendChild(newSpanTotal);  
+  
+  //event calculate
+  newDivQuantity.onkeyup = function(){    
+    newSpanHolder.innerText = newLabelPrice.value * newInput.value;
+    // var suma = totalSumArray.reduce(function(acc, elem){
+    //   return acc + elem;
+    // },0);
+    // totalSumSpan.innerText = suma;
+  };
+  
+  //delete button
+  var newDivButton = document.createElement('div'), newButton = document.createElement('button');
+  newButton.classList.add('btn-delete', 'btn'); newButton.textContent = 'Delete';
+  newDivButton.appendChild(newButton); newRow.appendChild(newDivButton);  
+  
+  newButton.onclick = function(e){  
+    this.parentElement.parentElement.remove();
+  };
+  
+  container.appendChild(newRow);
+};
 
-function updatePriceByProduct(productPrice, index){
 
-}
+buttonCalculate.onclick = function(){
 
-function getTotalPrice() {
+  // totalIndividual.map(function(elem){
+  //   console.log(elem);
+    
+  // });
 
-}
+  
+  for (var count = 0, i = 0; i < totalIndividual.length; i++) {
+    count+= Number(totalIndividual[i].firstChild.innerText);
+  }
+  totalSumSpan.innerText = count;
+  
 
-function createQuantityInput(){
+};
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  quantityInput.value = '0';
 };
