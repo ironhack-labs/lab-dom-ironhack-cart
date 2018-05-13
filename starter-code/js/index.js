d1f04c7f44536bffa0c331fef656191123066075
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-13 20:23:24 +0200

    all iterations except bug with remove button on second click

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,4 @@
-function deleteItem(e){
+/*function deleteItem(e){
 
 }
 
@@ -36,30 +36,29 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 function createNewItem(){
 
-}
+}*/
 
 window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
   var productsListContainer = document.getElementById("products-list");
 
-
-  //calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    var productWrapper = deleteButtons[i].parentElement.parentElement;
-    
-    deleteButtons[i].onclick = function() {
-      productsListContainer.removeChild(productWrapper)
-    }
-    //deleteButtons[i].onclick = deleteItem;
-  }
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var productPrice = document.getElementsByClassName('product-price');
   var quantity = document.getElementsByTagName('input');
   var total = document.getElementsByClassName('total');
   var totalOverall = document.getElementById('total-overall');
+
+  //calculatePriceButton.onclick = getTotalPrice;
+  //createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    var productWrapper = deleteButtons[i].parentElement;
+    deleteButtons[i].onclick = function() {
+      productsListContainer.removeChild(productWrapper);
+    }
+    //deleteButtons[i].onclick = deleteItem;
+  }
 
   calculatePriceButton.onclick = function() {
     for (var i = 0; i < productPrice.length; i++) {
@@ -70,8 +69,6 @@ window.onload = function(){
 
   createItemButton.onclick = function() {
     var itm =productsListContainer.lastElementChild;
-    
-
     var cln = itm.cloneNode(true);
     cln.getElementsByClassName("product-name")[0].innerText = document.getElementById("product-name").value;
     cln.getElementsByClassName("product-price")[0].innerText = document.getElementById("product-price").value;
@@ -79,7 +76,6 @@ window.onload = function(){
     productsListContainer.appendChild(cln);   
     //var newElement = document.createElement("div");
     //newElement.innerHTML = ;
-    
     //productsListContainer.appendChild(newElement);
   }
 
