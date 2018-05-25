13484b58c50a14e0080f9d23fe5df65469e61d1a
Alma Alonso <alma.aa93@gmail.com>
2018-05-25 21:59:31 +0200

    ninguna iteración completada

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Every product will have:
 
 - A wrapper div that contains all the HTML for that product.
 - A div with a span, showing the product name.
-- A div with a span, showing the cost of one unit.
+- A div with a span, showing the Price of one unit.
 - A div with one label and one input, where the user will indicate how many units they will buy.
 - A div with a span, showing the total price for this product. This number should be the result of multiplying the amount of units multiplied by the price of one unit of that product. The default total price should be 0.
 - A div with a delete button, to remove the product from the list.
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,109 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  var products = document.getElementsByClassName("product");
+  for (var i = 0; i < products.length; i++){
+    var product = products[i];
   }
-};
+  console.log(products);
+
+function getUnitPrice(products, i){
+  price = document.getElementsByClassName("unitPrice")[0];
+  return Number(price.innerText.replace("$", ""));
+}
+console.log(getUnitPrice(product));
+
+
+function getUnitQuantity(product){
+  quantity = document.getElementsByClassName("quantity")[0];
+  return Number(quantity.value);
+}
+console.log(getUnitQuantity(product));
+
+function getPriceByProduct(product){
+  return price = getUnitPrice(product) * getUnitQuantity(product);
+}
+console.log(getPriceByProduct(product));
+
+function updatePriceByProduct(products){
+  totalPriceByProduct = document.getElementsByClassName("total-product-price")[0];
+  totalPriceByProduct.innerText = "$" + getPriceByProduct(products);
+}
+console.log(updatePriceByProduct(products));
+
+//no sé como hacer para que esto me lo aplique a cada producto del nodo. 
+
+
+}
+
+
+
+
+
+// window.onload = function(){
+
+
+
+// function updatePriceByProduct(product, index){
+//   price = getUnitPrice(product);
+//   quantity = getUnitQuantity(product);
+//   totalProductPrice = price * quantity;
+// }
+
+
+
+
+// }
+// // function getTotalPrice(){
+// //   total = 0.00
+// //   price = getUnitPrice(product);
+// //   quantity = getUnitQuantity(product);
+// //   totalUnitPrice = price * quantity;
+// //   total += totalUnitPrice;
+// // }
+
+//  function deleteItem(e){
+
+// }
+
+
+
+
+
+
+
+
+// function createQuantityInput(){
+
+// }
+
+// function createDeleteButton(){
+
+// }
+
+// function createQuantityNode(){
+
+// }
+
+// function createItemNode(dataType, itemData){
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
+
+// // window.onload = function(){
+// //   var calculatePriceButton = document.getElementById('calc-prices-button');
+// //   var createItemButton = document.getElementById('new-item-create');
+// //   var deleteButtons = document.getElementsByClassName('btn-delete');
+
+// //   calculatePriceButton.onclick = getTotalPrice;
+// //   createItemButton.onclick = createNewItem;
+
+// //   for(var i = 0; i<deleteButtons.length ; i++){
+// //     deleteButtons[i].onclick = deleteItem;
+// //   }};
