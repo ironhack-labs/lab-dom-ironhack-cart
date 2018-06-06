43222bfe48d4cc17b04ea1a09c479d6bf7de6f7d
Diane de Lassence <dianedelassence@gmail.com>
2018-06-06 13:36:41 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,5 @@
+console.log('hello');
+
 function deleteItem(e){
 
 }
@@ -10,12 +12,12 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
-
+function getTotalPrice(costOfOneUnit, quantity) {
+  return (costOfOneUnit * quantity)
 }
 
 function createQuantityInput(){
-
+  document.querySelector("costOfOneUnit")*document.querySelector("quantity");
 }
 
 function createDeleteButton(){
@@ -42,11 +44,35 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var ironStickerInput = document.getElementById('ironSticker');
+  var ironStickerTotal = document.getElementById('ironStickerTotal');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+  ironStickerInput.onchange = function (e) {
+    console.log(ironStickerTotal);
+    var unitPrice = parseFloat(ironStickerUnitPrice.innerText.substr(1));
+    ironStickerTotal.innerHTML = "$" + (ironStickerInput*ironStickerUnitPrice);
+    console.log(e.target.value);
+    console.log(unitPrice);
+  }
 };
+
+var btnsuccess = document.querySelector(".btn-success");
+
+var numBox = document.querySelector("input");
+
+var totalPrice = document.querySelector("total");
+
+// btnsuccess.onclick = function (){
+//   if (quantity.value === 0){
+//     return getTotalPrice
+//   }
+//   var result = document.createElement("numBox")
+// };
+
