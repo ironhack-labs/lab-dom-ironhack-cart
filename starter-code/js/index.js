0a4d0343374ce9ab5d5075c3e6d9be428b66aaf3
MateoReparaz <mateoreparaz1989@gmail.com>
2018-06-16 20:46:54 +0200

    1era funcion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,7 +6,28 @@ function getPriceByProduct(itemNode){
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(){
+/*var unit = document.getElementById("unit").innerHTML;
+unit = unit.slice(1,unit.length);
+var total = document.getElementById("total");
+var qty = parseInt(document.getElementById("qty").value);
+var totalPrice = "$" + (unit*qty) + ".00";
+total.innerHTML = totalPrice;*/
+var units = document.getElementsByClassName("unit");
+var total = document.getElementsByClassName("total");
+var qty = document.getElementsByClassName("qty");
+var tato = units.length;
+for (var i = 0; i < tato; i++){
+  unit = parseInt(units[i].innerHTML);
+  qt = parseInt(qty[i].value);
+  if (qt>0){
+  var totalPrice = "$" + (unit*qt) + ".00";
+  total[i].innerHTML = totalPrice;
+  }else{
+    total[i].innerHTML="$" + 0 + ".00";
+  }
+}
+
 
 }
 
