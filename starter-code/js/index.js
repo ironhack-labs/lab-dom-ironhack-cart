1eaa4017cee5d9538e1592f59a6461d369158fb1
Abel Alonso <abel.alonso@gmail.com>
2018-06-17 01:32:22 +0200

    Iteration 5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -53,8 +53,58 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
-function createNewItem(){
+function createNewItem(e){
+  console.log("hola");
+  var parent = document.getElementsByTagName("body")[0];
+  var newWrapper=document.createElement("div");
+  newWrapper.setAttribute("class", "wrapper");
+  
+  //create and add name
+  var wrapperElement=document.createElement("div");
+  wrapperElement.setAttribute("class", "product-name");
+  wrapperElement.innerHTML=document.getElementById("new-name").value;
+  newWrapper.appendChild(wrapperElement);
 
+  //create and add price
+  wrapperElement=document.createElement("div");
+  wrapperElement.setAttribute("class", "product-price");
+  wrapperElement.innerHTML="$"+parseFloat(document.getElementById("new-price").value).toFixed(2);
+  newWrapper.appendChild(wrapperElement);
+
+  //create and add units
+  wrapperElement=document.createElement("div");
+  wrapperElement.setAttribute("class", "units");
+  var innerElement=document.createElement("label");
+  innerElement.innerHTML="QTY";
+  wrapperElement.appendChild(innerElement);
+  innerElement=document.createElement("input");
+  innerElement.setAttribute("class", "quantity");
+  innerElement.setAttribute("value", "0");
+  wrapperElement.appendChild(innerElement);
+  newWrapper.appendChild(wrapperElement);
+
+  //create and add total price
+  wrapperElement=document.createElement("div");
+  wrapperElement.setAttribute("class", "total-price");
+  wrapperElement.innerHTML="$0.00";
+  newWrapper.appendChild(wrapperElement);
+
+  //create an add the delete button
+  wrapperElement=document.createElement("div");
+  wrapperElement.setAttribute("class", "delete-button");
+  innerElement=document.createElement("button");
+  innerElement.setAttribute("class", "btn btn-delete");
+  innerElement.innerHTML="Delete";
+  wrapperElement.appendChild(innerElement);
+  newWrapper.appendChild(wrapperElement);
+
+  parent.insertBefore(newWrapper, document.getElementsByClassName("new-wrapper")[0]);
+
+  //clear inputs
+  document.getElementById("new-name").value="";
+  document.getElementById("new-price").value="";
+
+  
 }
 
 window.onload = function(){
@@ -64,7 +114,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i < deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
