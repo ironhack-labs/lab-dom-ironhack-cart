4144d98ea884b3554d94f38794ea3606e0b11861
Jesus Martinez <jesmart1969@gmail.com>
2018-06-17 17:44:17 +0200

    Manipulción del DOM: Primera iteración

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,39 @@
 function deleteItem(e){
+  var selectDelete=e.currentTarget;
+  var rowDelete=selectDelete.parentNode.parentNode;
+  var parentRowToDelete=rowDelete.parentNode;
+  parentRowToDelete.removeChild(rowDelete);
 
 }
 
-function getPriceByProduct(itemNode){
+/*function getPriceByProduct(itemNode){
 
 }
 
 function updatePriceByProduct(productPrice, index){
 
-}
+}*/
 
 function getTotalPrice() {
-
+  var itemElement = document.getElementsByClassName("item");
+  var priceElements = [];
+  var price = 0;
+  var subTotal = 0;
+  var total = 0;
+  var qty = 0;
+  for (var i=0; i < itemElement.length; i++){
+      elementPrice=document.getElementById("itemPrice"+i);
+      price = parseFloat(elementPrice.innerHTML.split("$")[1]);
+      elementQty=document.getElementById("itemQty"+i);
+      qty = parseInt(elementQty.value);
+      subTotal=qty*price;
+      elementSubTotal = document.getElementById("itemTotal"+i);
+      elementSubTotal.innerHTML = "$"+ subTotal.toString();
+      total=total+subTotal;
+ /*     priceElement.push({"price": price, "qty": qty, "total": total}); */
+    }
+    elementTotal=document.getElementById("totalOrder");
+    elementTotal.innerHTML = "$"+total.toString();
 }
 
 function createQuantityInput(){
@@ -35,6 +57,7 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
+  console.log("Entre a createNewItem");
 
 }
 
@@ -48,5 +71,5 @@ window.onload = function(){
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
-  }
+  } 
 };
