57f0df1f13014967d873d0b9c74f9cadd643dac9
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2018-06-17 20:55:14 +0200

    delete and unit price

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,50 +1,66 @@
 function deleteItem(e){
+  // var deleteProduct = e.currentTarget.parentElement.parentElement;
+  // deleteProduct.remove();
+  e.currentTarget.parentElement.parentElement.remove();
 
-}
+};
 
 function getPriceByProduct(itemNode){
-
+  var itemText = itemNode.innerText;
+  return Number(itemText.substr(1));
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  var finalPrice = "$" + productPrice;
+  index.innerText = finalPrice;
 }
 
 function getTotalPrice() {
+  var products = document.getElementsByClassName("product");
+  for (var i = 0; i < products.length; i++) {
+    var unitPrice = getPriceByProduct(products[i].children[1].childNodes[0]);
 
-}
+    var productQuantity = products[i].children[2].childNodes[1].value;
 
-function createQuantityInput(){
+    var productPrice = unitPrice * productQuantity;
 
-}
+    updatePriceByProduct(productPrice, products[i].children[3]);
+  }
 
-function createDeleteButton(){
 
-}
+};
 
-function createQuantityNode(){
+// function createQuantityInput(){
 
-}
+// }
 
-function createItemNode(dataType, itemData){
+// function createDeleteButton(){
 
-}
+// }
 
-function createNewItemRow(itemName, itemUnitPrice){
+// function createQuantityNode(){
 
-}
+// }
 
-function createNewItem(){
+// function createItemNode(dataType, itemData){
 
-}
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
