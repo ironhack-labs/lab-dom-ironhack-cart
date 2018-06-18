68c6fdbe9393057a7ea3db5190694f771f37ccc5
Pablo Rezola <pablorezola20@yahoo.es>
2018-06-18 08:58:47 +0200

    carrito

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,54 @@
+
+innerElement.setAttribute("class", "buttonn btn-delete");
+innerElement.innerHTML="Delete";
+innerElement.onclick = deleteItem;
+
 function deleteItem(e){
+  parent = document.getElementsByTagName("div")[0];
+  parent.removeChild(e.currentTarget.parentNode.parentNode);
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(){
 
-}
+var product = document.getElementsByClassName("name");
 
-function updatePriceByProduct(productPrice, index){
+var unit = document.getElementsByClassName("number");
+
+var quantity = document.getElementsByClassName("barra");
+
+var total = document.getElementsByClassName("multiplication");
+
+for (i = 0; i < quantity.length; i++) {
+    if(parseInt(quantity[i].value)>0){
+    total[i].innerHTML= (parseInt(unit[i].innerHTML) * parseInt(quantity[i].value))+".00";
+   
+  }
+  console.log(unit)
+  console.log(quantity)  
+  }
+
+} 
+
+function updatePriceByProduct(){
 
 }
 
 function getTotalPrice() {
 
-}
+var acquisitions = document.getElementsByClassName("multiplication");
+var final = document.getElementById("buy");
+var addition = 0;
+for (i = 0; i < acquisitions.length; i++) {
+var price = parseInt(acquisitions[i].innerHTML);
+ additon += price;
+
+} if (addition > 0) {
+       final.innerHTML = addition + ".00"
+    }
+    console.log(unit)
+    console.log(quantity) 
+  }
 
 function createQuantityInput(){
 
@@ -40,13 +76,13 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+ // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+ // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for(var i = 0; i < deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
