761fa595f9043fc12157e8027a1a3599f03e3a83
Andrey Barrantes <abarrantes@clavisco.com>
2018-06-17 23:04:51 -0400

    iteraciones 1 y 2 completas

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -44,9 +44,46 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+function getTotalPrice(){
+
+    for(i=0; i<2; i++){
+      var productPrice = document.getElementById('productPrice'+i);
+      var productQuantity = document.getElementById('productQuantity'+i);
+      var lineTotal = document.getElementById('lineTotal'+i);
+    
+      lineTotal.innerHTML = Number(productPrice.innerHTML)*Number(productQuantity.value);
+    }
+};
+
+// theButton.onclick = function(){
+
+//   var howManyTimes = document.getElementById('quantity');
+//   var quantity = howManyTimes.value;
+  
+//   if(quantity ===""){quantity = 1};
+
+//   for(var i = 0; i < quantity; i++){
+
+//   var theInput = document.getElementById('the-input');
+
+//   var whatToAppend = theInput.value;
+
+//   var newNew = document.createElement('li');
+
+//   newNew.innerHTML = whatToAppend;
+
+//   theList.appendChild(newNew);
+//   }
+
+//   theInput.value = "";
+//   howManyTimes.value = "";
+
+
+// }
