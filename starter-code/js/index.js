c7a46f3e35a5df566c1d1880e2f405fbb6d4ae28
adiyathereh <ad.kydy@gmail.com>
2018-08-05 18:06:25 +0200

    implement delete functionality

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,90 +1,111 @@
-console.log("js is there");
+(function() {
+  let numberOfProducts = Array.from(document.querySelectorAll(".product"))
+    .length;
 
-function deleteItem(e) {}
+  function getPriceByProduct(itemNode) {
+    //get ID of product
+    const productNumber = itemNode.target.id.split("-").slice(-1)[0];
+    const numberInput = document.querySelector("#input-" + productNumber); //access input
+    const itemPrice = document.querySelector("#product-price-" + productNumber); //access single item price
+    const totalItemPrice = document.querySelector("#sum-" + productNumber); //access total item price
 
-function getPriceByProduct(itemNode) {
-  //get ID of product
-  const productNumber = itemNode.path[2].classList[1].split("-").slice(-1)[0];
-  console.log(itemNode.path[2].classList[1].split("-"));
-  const numberInput = document.querySelector("#input-" + productNumber); //access input
-  const itemPrice = document.querySelector("#product-price-" + productNumber); //access single item price
-  const totalItemPrice = document.querySelector("#sum-" + productNumber); //access total item price
+    totalItemPrice.innerHTML = numberInput.value * itemPrice.innerHTML;
+    getTotalPrice();
+  }
 
-  totalItemPrice.innerHTML = numberInput.value * itemPrice.innerHTML;
-  getTotalPrice();
-}
+  function getTotalPrice() {
+    let allPrices = Array.from(document.querySelectorAll(".sum")).map(el =>
+      parseInt(el.innerHTML)
+    );
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    allPrices = allPrices.reduce(reducer, 0);
+    var calculatePriceButton = document.getElementById("total-price-value");
+    calculatePriceButton.innerHTML = allPrices.toFixed(2);
+  }
+  function updatePriceByProduct(productPrice, index) {}
 
-function updatePriceByProduct(productPrice, index) {}
+  function createQuantityInput() {}
 
-function getTotalPrice() {
-  let allPrices = Array.from(document.querySelectorAll(".sum")).map(el =>
-    parseInt(el.innerHTML)
-  );
-  console.log(allPrices);
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  allPrices = allPrices.reduce(reducer, 0);
-  var calculatePriceButton = document.getElementById("total-price-value");
-  calculatePriceButton.innerHTML = allPrices.toFixed(2);
-  console.log("Get total price was fired ", calculatePriceButton);
-}
+  function createDeleteButton() {}
 
-function createQuantityInput() {}
+  function createQuantityNode() {}
 
-function createDeleteButton() {}
+  function createItemNode(dataType, itemData) {}
 
-function createQuantityNode() {}
+  function createNewItemRow(itemName, itemUnitPrice) {}
 
-function createItemNode(dataType, itemData) {}
+  function createNewItem() {
+    let newProductName = document.querySelector("#new-product-name");
+    let newProductPrice = document.querySelector("#new-product-price");
+    const shop = document.querySelector("#shop");
 
-function createNewItemRow(itemName, itemUnitPrice) {}
-
-function createNewItem() {
-  let newProductName = document.querySelector("#new-product-name");
-  let newProductPrice = document.querySelector("#new-product-price");
-  const shop = document.querySelector("#shop");
-
-  let div = document.createElement("div");
-  div.innerHTML = `<div class="product product-${3}">
+    if (newProductName.value && newProductPrice.value) {
+      let div = document.createElement("div");
+      div.innerHTML = `<div class="product" id="product-${numberOfProducts +
+        1}">
       <div>
         <span>${newProductName.value}</span>
         <span>$</span>
-        <span id='product-price-${3}'>${newProductPrice.value}</span>
+        <span id='product-price-${numberOfProducts + 1}'>${
+        newProductPrice.value
+      }</span>
       </div>
       <div>
         <label for="product">QUANTITY</label>
-        <input id='input-${3}' type="number" name="product" class="quantity" />
+        <input id='input-${numberOfProducts +
+          1}' type="number" name="product" class="quantity" />
       </div>
       <div>
         <span>$</span>
-        <span class="sum" id='sum-${3}'>0</span>
+        <span class="sum" id='sum-${numberOfProducts + 1}'>0</span>
       </div>
-      <button class="btn-delete">DELETE PRODUCT</button>
+      <button id="delete-${numberOfProducts +
+        1}" class="btn-delete">DELETE PRODUCT</button>
     </div>`;
 
-  shop.appendChild(div);
-  updateFields();
+      shop.appendChild(div);
+      updateFields();
 
-  console.log(newProductName, newProductPrice);
-
-  newProductName.value = "";
-  newProductPrice.value = "";
-}
-
-function updateFields() {
-  var allQuantityInputs = Array.from(document.querySelectorAll(".quantity"));
-  allQuantityInputs.forEach(el => (el.onkeyup = getPriceByProduct));
-}
-window.onload = function() {
-  var calculatePriceButton = document.getElementById("calc-prices-button");
-  var createItemButton = document.getElementById("new-item-create");
-  var deleteButtons = document.getElementsByClassName("btn-delete");
-
-  updateFields();
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-  calculatePriceButton.onclick = getTotalPrice;
-
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].onclick = deleteItem;
+      newProductName.value = "";
+      newProductPrice.value = "";
+      numberOfProducts++;
+    }
   }
-};
+
+  function deleteItem(itemNode) {
+    let productNumber = itemNode.target.id.split("-").slice(-1)[0];
+    let element = document.querySelector("#product-" + productNumber);
+
+    element.parentElement.removeChild(element);
+
+    //Update total price
+    getTotalPrice()
+  }
+
+  function updateFields() {
+    let allQuantityInputs = Array.from(document.querySelectorAll(".quantity"));
+    allQuantityInputs.forEach(el => {
+      el.onkeyup = getPriceByProduct;
+      //in firefox input fields don't get cleared after page reload
+      el.value = "";
+    });
+
+    let allDeleteButtons = Array.from(document.querySelectorAll(".btn-delete"));
+    allDeleteButtons.forEach(button => (button.onclick = deleteItem));
+  }
+
+  window.onload = function() {
+    var calculatePriceButton = document.getElementById("calc-prices-button");
+    var createItemButton = document.getElementById("new-item-create");
+    var deleteButtons = document.getElementsByClassName("btn-delete");
+
+    updateFields();
+    calculatePriceButton.onclick = getTotalPrice;
+    createItemButton.onclick = createNewItem;
+    calculatePriceButton.onclick = getTotalPrice;
+
+    for (var i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].onclick = deleteItem;
+    }
+  };
+})();
