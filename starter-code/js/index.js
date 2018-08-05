9da84df8474f00b6ab5038f3e8564710bb07148c
Sean Walsh <scjwalsh@gmail.com>
2018-08-05 13:52:58 +0200

    Finished - everything works as instructions expect. The code need cleaned up. There is a lot of repeated code and empty functions

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,7 @@ function getTotalPrice() {
     var cartItem = Array.from(document.querySelectorAll('.cart-item')); //retrieves unit price
     var unitCost = Array.from(document.querySelectorAll('.unit-cost')); //retrieves unit price
     var totalCostEl = Array.from(document.querySelectorAll('.total-cost')); //retrieves total cost
-    var unitQuantity = Array.from(document.querySelectorAll('.input-qty input'));
+    var unitQuantity = Array.from(document.querySelectorAll('.input-qty'));
     var totalPrice = document.querySelector('#total-price');
     // const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
@@ -37,15 +37,69 @@ function createItemNode(dataType, itemData) {}
 
 function createNewItemRow(itemName, itemUnitPrice) {}
 
-function createNewItem() {}
+function createNewItem() {
+    var itemNameInput = document.querySelector('input.input-item-name');
+    var itemPrice = document.querySelector('input.input-price');
+    var cartItemsArr = document.querySelector('.all-carts');
+
+    // itemNameInput.value; //value of first input
+    // itemPrice.value; //value of second input
+
+    var newCartItem = document.createElement('div');
+    newCartItem.classList.add('cart-item');
+    console.log(newCartItem);
+    cartItemsArr.appendChild(newCartItem);
+
+    //insert before last item of cartItems array
+
+    var newProductName = document.createElement('span');
+    newProductName.classList.add('product-name');
+    newProductName.innerHTML = itemNameInput.value;
+    newCartItem.appendChild(newProductName);
+
+    newQMark = document.createElement('span');
+    newQMark.classList.add('currency');
+    newQMark.innerHTML = '$';
+    newCartItem.appendChild(newQMark);
+
+    var newUnitCost = document.createElement('span');
+    newUnitCost.classList.add('unit-cost');
+    newUnitCost.innerHTML = itemPrice.value;
+    newCartItem.appendChild(newUnitCost);
+
+    var newLabel = document.createElement('label');
+    newLabel.setAttribute('for', 'input');
+    newLabel.innerHTML = 'QTY';
+    newCartItem.appendChild(newLabel);
+
+    var newInput = document.createElement('input');
+    newInput.classList.add('input-qty');
+    newInput.setAttribute('type', 'number');
+    newCartItem.appendChild(newInput);
+
+    newQMark = document.createElement('span');
+    newQMark.classList.add('currency');
+    newQMark.innerHTML = '$';
+    newCartItem.appendChild(newQMark);
+
+    var newTotalCost = document.createElement('span');
+    newTotalCost.classList.add('total-cost');
+    newTotalCost.innerHTML = '0.00';
+    newCartItem.appendChild(newTotalCost);
+
+    var newDeleteBtn = document.createElement('button');
+    newDeleteBtn.classList.add('btn-delete');
+    newDeleteBtn.innerHTML = 'Delete';
+    newCartItem.appendChild(newDeleteBtn);
+}
 
 window.onload = function() {
     var calculatePriceButton = document.getElementById('calc-prices-button');
-    var createItemButton = document.getElementById('new-item-create');
+    var createItemButton = document.getElementById('create-item');
     var deleteButtons = document.getElementsByClassName('btn-delete');
 
     calculatePriceButton.onclick = getTotalPrice; //works
-    // createItemButton.onclick = createNewItem;    //uncomment WHEN READY
+    createItemButton.onclick = createNewItem; //uncomment WHEN READY
 
     for (var i = 0; i < deleteButtons.length; i++) {
         deleteButtons[i].addEventListener('click', deleteItem, false);
