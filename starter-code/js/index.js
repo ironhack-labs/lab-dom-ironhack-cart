0f4e72c4255a18a5b0e308facdfe9cc4ffd94d6e
Sean Walsh <scjwalsh@gmail.com>
2018-08-05 12:35:31 +0200

    Iteration 4: deletes cart item when delete button is clicked

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,7 @@
-function deleteItem(e) {}
+function deleteItem(e) {
+    var deletBtn = e.currentTarget.parentNode; //targets parent of delete btn
+    deletBtn.parentNode.removeChild(deletBtn); //deletes parent of delete btn
+}
 
 function getPriceByProduct(itemNode) {}
 
@@ -10,7 +13,7 @@ function getTotalPrice() {
     var totalCostEl = Array.from(document.querySelectorAll('.total-cost')); //retrieves total cost
     var unitQuantity = Array.from(document.querySelectorAll('.input-qty input'));
     var totalPrice = document.querySelector('#total-price');
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    // const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
     //HOW TO DO WITH FOR EACH?
     var calcTotal = 0;
@@ -44,5 +47,7 @@ window.onload = function() {
     calculatePriceButton.onclick = getTotalPrice; //works
     // createItemButton.onclick = createNewItem;    //uncomment WHEN READY
 
-    for (var i = 0; i < deleteButtons.length; i++) {}
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', deleteItem, false);
+    }
 };
