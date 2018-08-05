d30653dba0b9218ea827268fc2160900a5146a76
Sean Walsh <scjwalsh@gmail.com>
2018-08-05 09:12:34 +0200

    almost finished iteration 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,7 +4,17 @@ function getPriceByProduct(itemNode) {}
 
 function updatePriceByProduct(productPrice, index) {}
 
-function getTotalPrice() {}
+function getTotalPrice() {
+    var unitCost = Array.from(document.querySelectorAll('.unit-cost'));
+    var unitQuantity = unitCost.length; //retrieves the quantity of items
+    var curTotal = 0;
+
+    unitCost.forEach(function(cost) {
+        var costVal = parseFloat(cost.innerHTML); //retrieves the unit price of the product
+        curTotal += costVal; //calculates total price
+    });
+    return curTotal;
+}
 
 function createQuantityInput() {}
 
@@ -23,7 +33,7 @@ window.onload = function() {
     var createItemButton = document.getElementById('new-item-create');
     var deleteButtons = document.getElementsByClassName('btn-delete');
 
-    calculatePriceButton.onclick = getTotalPrice;
+    calculatePriceButton.onclick = getTotalPrice; //works
     createItemButton.onclick = createNewItem;
 
     for (var i = 0; i < deleteButtons.length; i++) {
