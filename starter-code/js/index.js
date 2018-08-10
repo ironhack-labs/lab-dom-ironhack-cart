af0685aa60fda5db8da8dad51f882610b54ef640
Adolfo Vela <vela@tekplanner.com>
2018-08-10 15:29:06 -0500

    Listo iteracion 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,44 +1,58 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
+  // Obtenemos la cantidad de productos a comprar -- Array
+  var inputValue = document.getElementsByClassName("qty");
+
+  // Obtenemos el precio de cada producto - Array
+  var valueQ = document.getElementsByClassName("values");
+
+  // Obtenemos el elemento de la columna total por producto - Array
+  var totalByProduct = document.getElementsByClassName("total-by-product");
+
+  for (i = 0; i < inputValue.length; i++) {
+    // Calculo del precio total por artículo
+    var total = inputValue[i].value * valueQ[i].innerHTML;
+     totalByProduct[i].innerHTML = total;
+  };
 
 }
 
-function createQuantityInput(){
+function createQuantityInput() {
 
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
 
 }
 
-function createQuantityNode(){
+function createQuantityNode() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItem(){
+function createNewItem() {
 
 }
 
-window.onload = function(){
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -46,7 +60,7 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
