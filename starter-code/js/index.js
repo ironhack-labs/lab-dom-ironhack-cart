db133332fb086e07da548901747aa8567505221d
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-08-10 22:59:08 -0500

    Done Iteration 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,24 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var costs = document.getElementsByClassName('unit-price');
+  var quantities = document.getElementsByClassName('quantity');
+  var allPrices = [];
+  var allQuantities = [];
+  var totalPrices = 0.0;
 
+  for(i=0; i<costs.length; i++) {
+    allPrices.push(parseFloat(costs[i].innerHTML.replace('$', '')));
+    allQuantities.push(parseInt(quantities[i].value));
+    totalPrices += (allPrices[i]*allQuantities[i]);
+  }
+
+  var parent = document.getElementsByClassName('div-total');
+  var pTag = document.createElement('div');
+  var text = document.createTextNode('Total price is: ' + totalPrices);
+
+  pTag.appendChild(text);
+  parent[0].insertBefore(pTag, null);
 }
 
 function createQuantityInput(){
