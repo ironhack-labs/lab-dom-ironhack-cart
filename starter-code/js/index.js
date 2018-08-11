5fbe68952bde3355aa1b646cfce319f293679a3a
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-08-11 08:08:32 -0500

    Agregar producto hecho

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -13,45 +13,107 @@ function updatePriceByProduct(productPrice, index){
 function getTotalPrice() {
   var costs = document.getElementsByClassName('unit-price');
   var quantities = document.getElementsByClassName('quantity');
+  var totales = document.getElementsByClassName('total');
   var allPrices = [];
   var allQuantities = [];
   var totalPrices = 0.0;
+  var price = 0.0;
 
   for(i=0; i<costs.length; i++) {
     allPrices.push(parseFloat(costs[i].innerHTML.replace('$', '')));
     allQuantities.push(parseInt(quantities[i].value));
-    totalPrices += (allPrices[i]*allQuantities[i]);
+    price = allPrices[i]*allQuantities[i];
+    totales[i].innerHTML = '$' + price;
+    totalPrices += price;
   }
 
-  var parent = document.getElementsByClassName('div-total');
-  var pTag = document.createElement('div');
-  var text = document.createTextNode('Total price is: ' + totalPrices);
-
-  pTag.appendChild(text);
-  parent[0].insertBefore(pTag, null);
+  createQuantityInput(totalPrices);
 }
 
-function createQuantityInput(){
-
+function createQuantityInput(totalPrices){
+  var h2Tag = document.getElementsByTagName('h2');
+  h2Tag[0].innerHTML= 'Total Price: $' + totalPrices;
 }
 
 function createDeleteButton(){
+  var div = document.createElement('div');
+  var input = document.createElement('button');
+  input.classList.add('btn');
+  input.classList.add('btn-delete');
+  input.innerHTML = 'Delete';
+  input.onclick = deleteItem;
+  div.appendChild(input);
 
+  return div;
+}
+
+function createTotalPriceNode(){
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  span.className = 'total';
+  span.innerHTML = '$0.00';
+  div.appendChild(span)
+  
+  return div;
 }
 
 function createQuantityNode(){
+  var div = document.createElement('div');
+  var label = document.createElement('label');
+  label.innerHTML = 'QTY';
+  var input = document.createElement('input');
+  input.className = 'quantity';
+  input.type = 'number';
+  input.value = 0;
+  input.min = 0;
 
+  div.appendChild(label);
+  div.appendChild(input);
+
+  return div;
+}
+
+function createCostNode(itemUnitPrice) {
+  var div = document.createElement('div');
+  var span = document.createElement('span');
+  span.className = 'unit-price';
+  span.innerHTML = '$' + parseFloat(itemUnitPrice);
+  div.appendChild(span)
+  
+  return div;
 }
 
 function createItemNode(dataType, itemData){
+  var div = document.createElement('div');
+  div.className = 'product-name';
+  var span = document.createElement('span');
+  span.innerHTML = dataType;
+  div.appendChild(span)
 
+  return div;
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
+  var wrapper = document.createElement('div');
+  wrapper.className = 'wrapper';
+  wrapper.appendChild(createItemNode(itemName, ''));
+  wrapper.appendChild(createCostNode(itemUnitPrice));
+  wrapper.appendChild(createQuantityNode());
+  wrapper.appendChild(createTotalPriceNode());
+  wrapper.appendChild(createDeleteButton());
 
+  return wrapper;
 }
 
 function createNewItem(){
+  var input = document.getElementsByClassName('container')[0];
+  var name = document.getElementById('input-name');
+  var price = document.getElementById('input-price');
+  input.appendChild(createNewItemRow(name.value, price.value));
+
+  var parent = document.getElementsByClassName('container')[0];
+  var lastChild = document.getElementsByClassName('div-add')[0];
+  parent.insertBefore(input, lastChild);
 
 }
 
