69744ea00599d1812c660387a360b133975229aa
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-08-12 10:41:49 -0500

    Se finaliza lab de cart by Jose Alberto

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,9 @@
 function deleteItem(e){
-
+  var parent = e.path[3];
+  var child = e.path[2];
+  var curr = e.currentTarget.parentNode;
+  //console.log("Entro al delete",parent,child,curr);
+  parent.removeChild(child);
 }
 
 function getPriceByProduct(itemNode){
@@ -9,7 +13,7 @@ function getPriceByProduct(itemNode){
 function updatePriceByProduct(productPrice, index){
   var totalPriceColle = document.getElementsByClassName('totalPrice');
   var totalPrice = totalPriceColle[index];
-  totalPrice.innerHTML = productPrice;
+  totalPrice.innerHTML = '$ ' + productPrice;
 }
 
 function getTotalPrice() {
@@ -19,35 +23,89 @@ function getTotalPrice() {
   for(var i=0;i<unitPriceColle.length;i++){
     var unitPrice = unitPriceColle[i];
     var qoutity = qoutityColle[i];
-    var multiply = getPriceByProduct(unitPrice) * parseInt(qoutity.value);
+    var multiply = getPriceByProduct(unitPrice) * parseFloat(qoutity.value == "" ? 0 : qoutity.value);
     updatePriceByProduct(multiply,i);
     totalPrice += multiply;
   }
-
+  var totaFinal = document.getElementById('totalFinal');
+  totaFinal.innerText = '$ ' + totalPrice;
 }
 
 function createQuantityInput(){
+  var div = document.createElement('div');
+  var label = document.createElement('label');
+  var input = document.createElement('input');
+  var data = document.createTextNode('Qty ');
+  input.setAttribute('class','inputQuatity');
+  input.setAttribute('type','number');
+  label.setAttribute('for','inputQuatity');
+  label.appendChild(data);
+  div.appendChild(label);
+  div.appendChild(input);
+  return div;
 
 }
 
 function createDeleteButton(){
-
+  var div = document.createElement('div');
+  var button = document.createElement('button');
+  var del = document.createTextNode('Delete');
+  button.setAttribute('class','btn btn-delete');
+  button.appendChild(del);
+  div.appendChild(button);
+  return div;
 }
 
 function createQuantityNode(){
-
+  var div = document.createElement('div');
+  var label = document.createElement('label');
+  var price = document.createTextNode('$0.00');
+  label.setAttribute('class','totalPrice');
+  label.appendChild(price);
+  div.appendChild(label);
+  return div;
 }
 
 function createItemNode(dataType, itemData){
-
+  var div = document.createElement('div');
+  var label = document.createElement('label');
+  var data = document.createTextNode(itemData);
+  if(dataType == 'number') {
+    data = document.createTextNode('$ ' + itemData);
+    label.setAttribute('class','unitPrice');
+  }
+  label.appendChild(data);
+  div.appendChild(label);
+  return div;
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
-
+  var div = document.createElement('div');
+  div.appendChild(createItemNode('text',itemName));
+  div.appendChild(createItemNode('number',itemUnitPrice));
+  div.appendChild(createQuantityInput());
+  div.appendChild(createQuantityNode());  
+  div.appendChild(createDeleteButton());
+  console.log(div)
+  return div;
 }
 
 function createNewItem(){
-
+  var itemName = document.getElementById('newProductName').value;
+  var itemUnitPrice = document.getElementById('newProductPrice').value;
+  if(itemName == '' || itemUnitPrice == ''){
+    alert('Falta capturar valores');
+    return;
+  }
+  var parent = document.getElementById('productsList');
+  var row = createNewItemRow(itemName,itemUnitPrice);
+  row.setAttribute('class','content');
+  parent.appendChild(row);
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+  
 }
 
 window.onload = function(){
@@ -56,7 +114,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
