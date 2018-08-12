2e9af07f11ed20bb0d0d3f8be807432c2c195264
Andrei Fitai <fitai.andrei@gmail.com>
2018-08-12 22:11:47 +0200

    added basic html structure, ability to delete item, and calculate total prices

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,8 @@
 function deleteItem(e){
-
+ var button = e.currentNode
+ console.log(button)
+ button.parentElement.parentElement.remove()
+ getTotalPrice();
 }
 
 function getPriceByProduct(itemNode){
@@ -11,7 +14,21 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var prices  = document.getElementsByClassName('price')
+  var quantities = document.getElementsByClassName('quantity')
+  var totalCosts = document.getElementsByClassName('total-unit')
+  var accumulator = 0;
+  for (var x = 0; x < prices.length;x++){
+    var cost = prices[x].getElementsByTagName('span')[0].innerHTML
+    cost = cost.split("")
+    cost.shift();
+    cost = cost.join("")
+    cost *= quantities[x].getElementsByTagName('input')[0].value
+    accumulator += cost;
+    totalCosts[x].getElementsByTagName('span')[0].innerHTML = "$"+parseFloat(cost).toFixed(2)
+  }
+  console.log(accumulator)
+  document.getElementsByClassName('total-price')[0].getElementsByTagName('span')[0].innerHTML = "$"+parseFloat(accumulator).toFixed(2)
 }
 
 function createQuantityInput(){
@@ -44,7 +61,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
