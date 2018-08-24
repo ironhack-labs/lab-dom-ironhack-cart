a6784fbf93d6cc1eeacabe228c9a475a6b49de6a
agdwm <almudenaguerraswm@gmail.com>
2018-08-24 18:07:44 +0200

    createQuantityNode done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -14,15 +14,20 @@ function updatePriceByProduct(productPrice, index){
 
 function getTotalPrice() {
 	console.log( getPriceByProduct() * createQuantityInput());
+	return getPriceByProduct() * createQuantityInput();
 	
 }
 
 function createQuantityInput(){
 	var quantity = document.getElementById('quant1').value;
+
+	console.log(quantity);
+
 	if (quantity === ''){
 		alert('Debes introducir un número');
 		return;
 	}
+	
 	return parseInt(quantity);
 }
 
@@ -31,7 +36,8 @@ function createDeleteButton(){
 }
 
 function createQuantityNode(){
-
+	var totalPrice = getTotalPrice();
+	document.getElementById('total').innerHTML = totalPrice;
 }
 
 function createItemNode(dataType, itemData){
@@ -48,10 +54,14 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
+  var inputPrice = document.getElementById('quant1');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
+  //inputPrice.onchange = document.getElementById('total').innerHTML(getTotalPrice());
+  inputPrice.onchange = createQuantityNode;
+
 //   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
