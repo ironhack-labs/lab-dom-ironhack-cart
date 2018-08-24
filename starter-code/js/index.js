1723b3a007f52fdc878fc268dd9f21d6e617bc16
mfgomezb <mgomezb89@gmail.com>
2018-08-24 20:35:24 +0200

    Solved up to total price

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,7 +11,19 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  list_length = document.getElementsByClassName('unit-price').length
+  var total = 0;
+  for (i = 0; i <list_length; i++){
+    price = parseInt(document.getElementsByClassName('unit-price')[i].innerHTML);
+    quantity = parseInt(document.getElementsByClassName('input-quantity')[i].value);
+    subtotal = document.getElementsByClassName('subtotal')[i];
+    calcSubtotal = price * quantity;
+    total += calcSubtotal;
+    console.log(total)
+    subtotal.innerText = "$"+calcSubtotal
+  }
+  totalPrice = document.getElementsByClassName("total-price")[0].innerHTML
+  totalPrice.innerHtml = "$"+total;
 }
 
 function createQuantityInput(){
@@ -43,10 +55,10 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+   calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+     deleteButtons[i].onclick = deleteItem;
+   }
 };
