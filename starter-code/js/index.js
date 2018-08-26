6af68f235ed0c39209fee675690ee17ac0db9c8c
zalo Riera <nadalriera84@gmail.com>
2018-08-27 00:15:56 +0200

    esto es lo que subo

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,8 +11,37 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var quantityItem = document.getElementsByClassName("quantity");
+  var priceItem = document.getElementsByClassName("cost-Unit");//[i]innerHtml;
+  var subtotalPriceItem= document.getElementsByClassName("item-totalprice")
+   
+  var totalSubItem = 0 ;
+  var totalCart= 0;
+    for ( i= 0 ; i < priceItem.length; i++) {
+         
+         var qty = parseInt(quantityItem[i].value);
+         var cost = parseInt(priceItem[i].innerHTML.slice(1));
+
+         totalSubItem = qty*cost;
+         
+         
+         subtotalPriceItem[i].innerHTML= "$"+ totalSubItem;
+
+         
+    
+   
+       totalCart += totalSubItem;
+       
+      document.getElementById("cartCost").innerHTML= totalCart + "$";
+       
+    }
+   
+    
+    
 
 }
+
+
 
 function createQuantityInput(){
 
@@ -39,7 +68,7 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var calculatePriceButton = document.getElementById('btn-success');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
