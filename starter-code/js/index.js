2751cd1ed5864ba438fb2c903381594f20877bc8
cyril.attie <attiecyril@gmail.com>
2018-08-26 12:44:51 +0300

    [MAD PP] Cyril Attie & John Scarzo

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,133 @@
-function deleteItem(e){
-
+function deleteItem(){
+  
+  console.log("removing this ", this.parentElement.parentElement)  
+  /*this.parentElement.removeChild(this);*/
 }
 
 function getPriceByProduct(itemNode){
-
+  //get the price and the quantity
+  prodPrice = Number(itemNode.childNodes[1].childNodes[0].innerHTML);
+  quantity = Number(itemNode.childNodes[2].childNodes[0].childNodes[0].lastChild.value);
+  
+  //html structure creation
+  priceByProductDiv = createItemNode("div",{"class" : "price-by-product"});
+  priceByProductSpan = createItemNode("span",{"innerHTML":quantity*prodPrice});
+  priceByProductDiv.appendChild(priceByProductSpan);
+  //return div node class price-by-product containing the span  
+   return priceByProductDiv;
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(el){
+  getPriceByProduct(el)
 
 }
 
 function getTotalPrice() {
-
+/*para cada producto sumar (precio por qty) en un total 
+poner el total en la columna */ 
 }
 
 function createQuantityInput(){
+  //create the label element and appends QTY and input form to it 
+  var quantityForm = createItemNode("form",{"action":"#"});
+  var quantityLabel = document.createElement("label");
+  quantityLabel.innerHTML= "QTY";
 
+  quantityInput = document.createElement("input");
+  quantityInput.type = "number";
+  quantityInput.class = "quantity-number";
+  quantityInput.defaultValue = "1";
+  
+  quantityLabel.appendChild(quantityInput);
+  quantityForm.appendChild(quantityLabel);
+  
+  return quantityForm;
 }
 
 function createDeleteButton(){
-
+  //Creates and returns a div with a <a> tag containing a delete button
+  var deleteButtonDiv =createItemNode("div", {"class":"button-delete"});
+  var deleteButtonA =createItemNode("a", {"href":"#","onclick": "deleteItem()","class":"btn btn-delete","innerHTML":"Delete"});
+  deleteButtonDiv.appendChild(deleteButtonA);
+  return deleteButtonDiv;
 }
 
 function createQuantityNode(){
-
+  var quantityDiv = createItemNode("div", {"class":"quantity"});
+  quantityFormInput = createQuantityInput();
+  quantityDiv.appendChild(quantityFormInput);
+  return quantityDiv;
 }
 
-function createItemNode(dataType, itemData){
-
+function createItemNode(dataType, properties){
+  newItemNode = document.createElement(dataType);
+   Object.keys(properties).forEach(function(property){
+    switch (property) {
+      case "innerHTML":
+      newItemNode.innerHTML=properties[property];
+      case "class":
+      newItemNode.class=properties[property]
+      case "id":
+      newItemNode.id = properties[property]
+      case "onclick":
+      newItemNode.onclick = properties[property]      
+      default:
+      newItemNode.setAttribute(property,properties[property]);
+    };    
+  });
+  return newItemNode
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
-
+//create a new product div  
+  counter = document.getElementById("products").childNodes.length-1;
+  var newProductRow = createItemNode("div", {"class":"product", "id":"product"+counter})  
+  var productName = createItemNode("div", {"class":"product-name"});
+  var spanName = createItemNode("span",{"innerHTML":itemName});
+  productName.appendChild(spanName);
+  
+  var productPrice = createItemNode("div", {"class":"price"});
+  var spanPrice = createItemNode("span", {"innerHTML":itemUnitPrice});
+  productPrice.appendChild(spanPrice);
+  
+  newProductRow.appendChild(productName);
+  newProductRow.appendChild(productPrice);
+  return newProductRow
 }
 
 function createNewItem(){
+  //get the name and price of the new item
+var pName= document.getElementById("item-name").value;
+var pPrice= document.getElementById("item-price").value;
 
+// create the row DOM element within which we have the name and price 
+productRow= createNewItemRow(pName, pPrice);
+
+//append to the row the quantity DOM element and the button
+productRow.appendChild(createQuantityNode());
+productRow.appendChild(getPriceByProduct(productRow));
+productRow.appendChild(createDeleteButton());
+
+//append the new product row to the #products div 
+var products = document.getElementById('products');
+products.appendChild(productRow)
+ 
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  var createItemButton = document.getElementById("new-item-create");
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  var quantities = document.getElementsByClassName('quantity-number');
+  productList = document.getElementsByClassName('product');
+   
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
+    console.log(productList[i])
     deleteButtons[i].onclick = deleteItem;
+    quantities[i].onchange = updatePriceByProduct;
   }
 };
