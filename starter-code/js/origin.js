b25d61e73bcdec80fdea78120e82bc8f0107e915
Esperanza Amaya <eamayag@gmail.com>
2018-08-27 08:45:42 +0200

    first attempt

--- starter-code/js/origin.js
+++ starter-code/js/origin.js
@@ -3,7 +3,7 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+ 
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +11,15 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var items = document.getElementsByClassName("wrapper");
+  var totalPrice = 0;
+  for (i=0; i<items.length; i++) {
+    var unitPrice = parseInt(items[i].getElementsByClassName("unitPrice")[0].innerHTML);
+    var unitQty = parseInt(items[i].getElementsByClassName("unitQty")[0].value);
+    var productPrice = unitPrice * unitQty;
+    items[i].getElementsByClassName("totalPerProduct")[0].innerHTML = productPrice;
+    totalPrice += productPrice;
+  }  
 }
 
 function createQuantityInput(){
@@ -44,7 +52,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
