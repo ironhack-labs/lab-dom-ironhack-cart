de453a09d7a2c15ab6a0634fb68dcc16e8395f6b
beatrice-yun <beatrice.yk.daniel@gmail.com>
2018-10-05 10:23:01 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -34,6 +34,13 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
+function createNewItemRow2(itemName, itemUnitPrice, itemClass){
+  var elem = document.createElement(itemName);
+  elem.className = itemClass;
+  document.getElementsByClassName(itemClass).innerHTML=itemUnitPrice;
+}
+
+
 function createNewItem(){
 
 }
@@ -42,10 +49,8 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
+  //calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItemRow2("INPUT", "10", "btn");
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
