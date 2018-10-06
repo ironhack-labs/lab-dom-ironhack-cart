c56f70621b55d9af2116e57bf6ac102997d33367
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2018-10-06 12:08:21 +0200

    second-push

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,17 @@
+var IHProduct = [
+  // {name = 'Iron BubbleHead', price= 49.99 }
+  // {name = 'Iron Shirt', price= 9.99 }
+  // {name = 'Iron Cup', price= 6.99 }
+  // {name = 'Iron Cup Holder', price= 5.99 }
+  // {name = 'Iron Sticker', price= 3.99 }
+  // {name = 'Iron Sticker', price= 0.10 }
+  // {name = 'Iron Axe', price= 4.99 }
+
+]
+
+
+
+
 function deleteItem(e){
 
 }
@@ -43,10 +57,30 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
+  calculatePriceButton.onclick = calculateSubTotal;
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+
+
+// function addNeweProduct(){
+//   var productName = {name};
+//   var addProductHTML = productName.replace({name
+// })
+
+
+function calculateSubTotal() {
+  var qty = document.getElementById('orderQty').value;
+  var productCost = document.getElementById('productCost').textContent;
+  var subTotal = qty * productCost;
+  var displaySubTotal = document.getElementById('subTotal');
+  displaySubTotal.textContent = subTotal;
+ console.log(subTotal)
+}
+
+
