4053a29c852c89593b8a22f91af9ce03da04fee0
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-10-14 15:40:45 +0200

    Add finished project. Delete Button not working properly yet.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,120 @@
-function deleteItem(e){
+
+
+
+function deleteItem(item){
+var container =  document.querySelectorAll(".container")[item];
+container.innerHTML = ""
+deleteCounter--
+console.log(deleteCounter)
 
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
 
 function getTotalPrice() {
 
+  var priceInput = []
+  price = [];
+  var quantityInput = [];
+  var quantity = [];
+  var totalInput = [];
+  var totalPrice = [];
+
+
+  priceInput = Array.prototype.slice.call(document.querySelectorAll(".cost"));
+  priceInput.map(element => {
+    price.push((Number(element.innerText[1] + element.innerText[2] + element.innerText[3])))
+    return price;
+  });
+  quantityInput = Array.prototype.slice.call(document.querySelectorAll(".qty"));
+  quantityInput.forEach(element => {
+    quantity.push(Number(element.value));  
+  });
+
+  //calculating the price for each product
+  quantity.forEach((element,index) => {
+    totalPrice.push(price[index] * element);
+  })
+
+  totalInput = Array.prototype.slice.call(document.querySelectorAll(".total"));
+  totalInput.forEach((element, index) => {
+    element.innerText = "$" + totalPrice[index] + ".00"
+  })
+
+  document.getElementById("end-price").innerText ="Total Price: $" + totalPrice.reduce((acc, element) => {
+    return acc + element;
+  }) + ".00"
+
 }
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
 
 function createNewItem(){
 
+var wrapper = document.getElementById("wrapper");
+
+//Container
+var newProduct = document.createElement("div");
+newProduct.classList.add("container")
+wrapper.appendChild(newProduct);
+
+//name of Product
+var nameDiv = document.createElement("div")
+nameDiv.classList.add("name");
+nameDiv.innerText = document.querySelector(".name-input").value;
+newProduct.appendChild(nameDiv);
+
+//cost of Product
+var costDiv = document.createElement("div")
+costDiv.classList.add("cost");
+costDiv.innerText = "$" + document.querySelector(".price-input").value + ".00";
+newProduct.appendChild(costDiv);
+
+//Quantity-container and Input
+var quantityContainer = document.createElement("div")
+quantityContainer.classList.add("qty-container");
+quantityContainer.innerText = "QTY"
+var quantityInput = document.createElement("input")
+quantityContainer.innerText = "QTY"
+quantityInput.classList.add("qty");
+quantityInput.setAttribute("type", "number");
+quantityInput.setAttribute("placeholder", "0");
+quantityContainer.appendChild(quantityInput);
+newProduct.appendChild(quantityContainer);
+
+// total cost of item
+var totalDiv = document.createElement("div")
+totalDiv.classList.add("total");
+totalDiv.innerText = "$0.00";
+newProduct.appendChild(totalDiv);
+
+// delete Button
+var deleteButton = document.createElement("button")
+deleteButton.classList.add("btn-delete");
+deleteButton.innerText = "Delete";
+newProduct.appendChild(deleteButton);
+deleteCounter++;
+console.log(deleteCounter)
+document.querySelectorAll(".btn-delete")[deleteCounter].addEventListener("click",() => {
+  deleteItem(deleteCounter)
+})
+
+//default values
+document.querySelector(".price-input").value = "";
+document.querySelector(".name-input").value = "";
+
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+//Click-Events
+document.querySelector(".btn-calc").addEventListener("click",() => {
+  getTotalPrice()})
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+for (let i=0; i<(document.querySelectorAll(".btn-delete").length); i++) {
+  document.querySelectorAll(".btn-delete")[i].addEventListener("click",() => {
+  deleteItem(i)
+  })}
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+var deleteCounter = 1;
+document.querySelector(".btn-create").addEventListener("click",() => {createNewItem()})
+
+
+
+
