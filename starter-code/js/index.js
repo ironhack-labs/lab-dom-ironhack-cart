d6972d142adb4079ada7df2f26c90ec637755fbb
Corrado Passera <corradopassera@hotmail.com>
2018-10-19 17:48:45 +0200

    works but messy and not styled

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,144 @@
-function deleteItem(e){
+// function deleteItem(e){
 
-}
+// }
 
-function getPriceByProduct(itemNode){
+// function getPriceByProduct(itemNode){
 
-}
+// }
 
-function updatePriceByProduct(productPrice, index){
+// function updatePriceByProduct(productPrice, index){
 
-}
+// }
 
-function getTotalPrice() {
+// function getTotalPrice() {
 
-}
+// }
 
-function createQuantityInput(){
+// function createQuantityInput(){
 
-}
+// }
 
-function createDeleteButton(){
+// function createDeleteButton(){
 
-}
+// }
 
-function createQuantityNode(){
+// function createQuantityNode(){
 
-}
+// }
 
-function createItemNode(dataType, itemData){
+// function createItemNode(dataType, itemData){
 
-}
+// }
 
-function createNewItemRow(itemName, itemUnitPrice){
+// function createNewItemRow(itemName, itemUnitPrice){
 
-}
+// }
 
-function createNewItem(){
+// function createNewItem(){
 
-}
+// }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var createItemButton = document.querySelector('.btn-create');
+  var bodySel = document.querySelector('body');
+  var deleteButtons = document.querySelectorAll('.btn-delete');
+  var itemsSel = document.querySelectorAll('.item');
+  var newItemSel = document.querySelector('.new-item');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  createItemButton.onclick = function() {
+    var newItemName = document.querySelector('#new-item-name').value;
+    var newItemPrice = document.querySelector('#new-item-price').value;
+    var newNode = document.createElement("div");
+    newNode.innerHTML = [
+      '<div><span>' + newItemName + '</span></div>',
+      '<div>$<span class="item-price">' + newItemPrice + '</span></div>',
+      '<div><label for="quantity">QTY</label><input type="text" placeholder="0"/></div>',
+      '<div>$<span class="total-item-price">0.00</span></div>',
+      '<div><button class="btn-delete">Delete</button></div>',
+    ].join("\n");
+    newNode.classList.add("item");
+    newNode.classList.add("flex-disp");
+
+    bodySel.insertBefore(newNode, newItemSel);
+    deleteButtons = document.querySelectorAll('.btn-delete');
+    itemsSel = document.querySelectorAll('.item');
+    
+    deleteButtonsFunc();
+
+
+    // <div class="item">
+    //   <div><span>IronShirt</span></div>
+    //   <div>$<span class="item-price">15.00</span></div>
+    //   <div><label for="quantity">QTY</label><input type="text" placeholder="0"/></div>
+    //   <div>$<span class="total-item-price">0.00</span></div>
+    //   <div><button class="btn-delete">Delete</button></div>
+    // </div>
+  };
+
+  function deleteButtonsFunc() {
+    deleteButtons.forEach(function(button, idx){
+      button.onclick = function(){
+        deleteItem(idx);
+      };
+    });
+  };
+
+  deleteButtonsFunc();
+  
+  function deleteItem(item){
+    bodySel.removeChild(itemsSel[item]);
+  };
+
+
+
+  calculatePriceButton.onclick = function () {
+    // set the selectors
+    var itemCostSel = document.querySelectorAll('.item-price');
+    var quantitySel = document.querySelectorAll('input');
+    var totalItemCostSel = document.querySelectorAll('.total-item-price');
+    var checkoutTotalSel = document.querySelector(".total-price");
+    
+    // OPTIONAL FOR LOOP
+    // for (var i=0; i<itemCostSel.length; i++) {
+    //   // get cost of one item
+    //   var itemCost = itemCostSel[i].innerHTML;
+    //   // get quantity of one item    
+    //   var quantity = quantitySel[i].value;
+    //   if (quantity >= 0) {
+    //     var totalItemCost = itemCost*quantity;
+    //   } else {
+    //     var totalItemCost = 0;
+    //   };      
+    //   totalItemCostSel[i].innerHTML = totalItemCost;
+    // };
+
+    // initialize total checkout
+    var totalCheckout = 0;
+
+    // loop over arrays
+    totalItemCostSel.forEach(function (el, i){
+      // get cost of one item
+      var itemCost = Number(itemCostSel[i].innerHTML);
+      // get quantity of one item
+      var quantity = Number(quantitySel[i].value);
+      // multiply quantity by cost
+      if (quantity >= 0) {
+        var totalItemCost = itemCost*quantity;
+      } else {
+        var totalItemCost = 0;
+      };
+      // update total price of one item in the DOM
+      el.innerHTML = totalItemCost;
+      // add price of one item to the total of checkout
+      totalCheckout += Number(el.innerHTML);
+    });
+
+    // update total checkout in the DOM 
+    checkoutTotalSel.innerHTML = totalCheckout;
+    
+  };
 };
