9f27285b8804a1d5eab106c82bbd1811da37b165
Rebeca Cabañas Arauz <beckyarauz@users.noreply.github.com>
2018-10-23 17:55:31 +0200

    getTotalPrice function finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,6 +11,34 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  let productTotalsArr = [...document.getElementsByClassName('total')]
+
+  let total = document.getElementById('total');
+
+  let qtyArr = [...document.getElementsByClassName('qty')]
+          .map(input => input.value);
+
+  let pricesArr = [...document.getElementsByClassName('price')]
+          .map(price => price.textContent)
+          .map(price => price.trim());
+
+  let finalPrices = [];
+
+  for(let i = 0; i < pricesArr.length; i++){
+    finalPrices.push(pricesArr[i] * qtyArr[i]);
+  }
+  for(let i = 0; i < finalPrices.length; i++){
+    productTotalsArr[i].textContent = finalPrices[i];
+  }
+  //option#2
+  // productTotalsArr.map(total => {
+  //   for(let i = 0; i < finalPrices.length; i++){
+  //       total.textContent = finalPrices[i];
+  //     }
+  // });
+  let totalPrice = finalPrices.reduce((sum,price) => sum + Number(price),0);
+
+  total.innerHTML = totalPrice;
 
 }
 
@@ -44,7 +72,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
