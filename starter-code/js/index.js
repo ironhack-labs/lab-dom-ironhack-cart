56eca0ca50679dc7ae95a819ffaeb805c80b81f5
Sonia Ruiz <yune.vk@gmail.com>
2018-10-26 18:02:13 +0200

    Iteration 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -38,15 +38,30 @@ function createNewItem(){
 
 }
 
+function calculatePrices() {
+  var products = document.querySelectorAll('.product');
+
+  products.forEach(function(product) {
+    var cantidad = Number(product.querySelectorAll("input")[0].value);
+    var precio = parseFloat(product.querySelectorAll(".price span")[0].innerHTML);
+
+    product.querySelectorAll(".total span")[0].innerHTML = cantidad * precio;
+
+    console.log(`Total: ${cantidad * precio}`)
+  });
+}
+
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  //var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+
+  calculatePriceButton.onclick = calculatePrices;
 };
