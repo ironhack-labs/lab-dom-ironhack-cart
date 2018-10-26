d4613470efdf3cad8aa7a7915913b3fe5d379b0b
Luise Schneider <luise.schneider@berlin.de>
2018-10-26 03:19:32 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,36 +1,54 @@
-function deleteItem(e) {}
-
-function getPriceByProduct(itemNode) {}
-
-function updatePriceByProduct(productPrice, index) {}
-
-function createQuantityInput() {}
-
-function createDeleteButton() {}
-
-function createQuantityNode() {}
-
-function createItemNode(dataType, itemData) {}
-
-function createNewItemRow(itemName, itemUnitPrice) {}
-
-function createNewItem() {}
-
 window.onload = function() {
-  var calculatePriceButton = document.getElementById("calc-prices-button");
-  var createItemButton = document.getElementById("new-item-create");
-  var deleteButtons = document.getElementsByClassName("btn-delete");
-
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].onclick = deleteItem;
-  }
-
   var calculateButton = document.getElementsByClassName("btn-success")[0];
-  console.log(calculateButton);
   calculateButton.onclick = function() {
-    var singlePrice = document.getElementById("singlePrice").innerText;
-    var quantity = document.getElementsByTagName("input")[0].value;
-    var totalPrice = parseFloat(Number(singlePrice) * Number(quantity));
-    console.log(totalPrice);
+    // calculate total price for each product
+    var products = document.getElementsByClassName("product");
+    for (var i = 0; i < products.length; i++) {
+      var quantity = products[i].getElementsByTagName("input")[0].value;
+      var singlePrice = products[i].getElementsByClassName("singlePrice")[0]
+        .innerText;
+      var totalPrice = Number(quantity) * Number(singlePrice);
+      products[i].getElementsByClassName(
+        "totalPrice"
+      )[0].innerText = parseFloat(totalPrice).toFixed(2);
+    }
+    // calculate price of all products in the cart
+    var prices = document.getElementsByClassName("totalPrice");
+    for (var i = 0, totalCart = 0; i < prices.length; i++) {
+      totalCart += Number(prices[i].innerText);
+      document.getElementById("totalCart").innerText = parseFloat(
+        totalCart
+      ).toFixed(2);
+    }
   };
+
+  var createItemButton = document.getElementsByClassName("newItemButton")[0];
+  createItemButton.onclick = function() {
+    var newName = document.getElementById("nameNewProduct").value;
+    var newPrice = Number(document.getElementById("priceNewProduct").value);
+    var newProduct = document
+      .getElementsByClassName("product")[0]
+      .cloneNode(true);
+    newProduct
+      .getElementsByClassName("name")[0]
+      .getElementsByTagName("span")[0].innerText = newName;
+    newProduct
+      .getElementsByClassName("singlePrice")[0]
+      .getElementsByTagName("span")[0].innerText = parseFloat(newPrice).toFixed(
+      2
+    );
+    newProduct.getElementsByTagName("input")[0].value = "";
+    newProduct
+      .getElementsByClassName("totalPrice")[0]
+      .getElementsByTagName("span")[0].innerText = parseFloat(0).toFixed(2);
+    var parent = document.getElementsByClassName("products")[0];
+    parent.appendChild(newProduct);
+  };
+
+  var deleteButtons = document.getElementsByClassName("btn-delete");
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = function(e) {
+      e.currentTarget.parentNode.parentNode.remove();
+    };
+  }
 };
