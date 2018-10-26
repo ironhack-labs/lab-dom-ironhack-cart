e336387dc42adc0503c680855c148d2f4d3f62af
Axel Heinz <axelmheinz@gmail.com>
2018-10-26 17:59:20 +0200

    done two products

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 function deleteItem(e){
 
 }
@@ -43,10 +47,39 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+ 
+ 
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  calculatePriceButton.onclick = function(){
+    var uprice1 = document.getElementById('unitprice1').innerText;
+    console.log(uprice1);
+    var input1 = document.getElementById('quantity1').value;
+    console.log(input1);
+    var tprice1 = document.getElementById('totalPriceEl1');
+    tprice1.innerHTML = (Number(uprice1) * parseFloat(input1)); 
+
+    var uprice2 = document.getElementById('unitprice2').innerText;
+    console.log(uprice1);
+    var input2 = document.getElementById('quantity2').value;
+    console.log(input2);
+    var tprice2 = document.getElementById('totalPriceEl2');
+    tprice2.innerHTML = (Number(uprice2) * parseFloat(input2)); 
+
+    var total = document.getElementById('total-price');
+    total.innerHTML = (Number(uprice1) * parseFloat(input1)) + (Number(uprice2) * parseFloat(input2));
+    
+    //element = document.createElement('span')
+    //element.innerHTML =(Number(uprice) * parseFloat(input));  
+    //tprice.appendChild(element)
+    // 
   }
+
 };
+
+
+ // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
+
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
