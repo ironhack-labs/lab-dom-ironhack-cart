8a1e32dc876d6693d692827dd87c90290822c8b4
sitobusnan <sito@citytonight.es>
2018-10-27 17:41:54 +0200

    segunda iteracion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,15 +3,27 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+  
+  
 }
 
-function updatePriceByProduct(productPrice, index){
 
+function updatePriceByProduct(productPrice, index){
+  document.getElementsByClassName('total')[index].innerHTML =`$${productPrice}`;
+  
 }
 
 function getTotalPrice() {
-
+  var elements = document.getElementsByClassName('prices').length;
+  for(i=0;i < elements; i++){
+    var oneprice = document.getElementsByClassName('prices')[i].textContent.substring(1)
+    var quantity = document.getElementsByTagName('input')[i].value;
+    oneprice = parseFloat(oneprice);
+    quantity = parseInt(quantity);
+    var mountprice = oneprice * quantity;
+    updatePriceByProduct(mountprice, i);
+  }
+  
 }
 
 function createQuantityInput(){
@@ -39,12 +51,16 @@ function createNewItem(){
 }
 
 window.onload = function(){
+  
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
+  
+  
+
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
