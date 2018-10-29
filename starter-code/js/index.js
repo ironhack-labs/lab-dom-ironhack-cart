3eacbef19fdd785a0d6f52e2f6b3dd0442d2c348
Alex McDaniel <amcdaniel186@gmail.com>
2018-10-29 16:20:27 -0400

    update-ironhackcart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,26 @@
+
+
+let byClass = (theClass=>{return document.getElementsByClassName(theClass)})
+
+
+let by = (id=>{return document.getElementById(id)})
+
+let byId = (id=>{return document.getElementById(id)})
+
+
+let button = byId("cal-button")
+
+let inputF = byId("inputF") 
+
+
+let inputArr = byClass("totalPrice")
+console.log(inputArr)
+// button.onclick(()=>{
+
+// })
+console.log(button)
+console.log(inputF)
+
 function deleteItem(e){
 
 }
@@ -12,7 +35,11 @@ function updatePriceByProduct(productPrice, index){
 
 function getTotalPrice() {
 
+
+  // let totalPrice = document.("itemPrice");
 }
+
+// let calcButton = (getDocumentByID);
 
 function createQuantityInput(){
 
@@ -39,14 +66,20 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  // var calculatePriceButton = document.getElementByClassName('calc-prices-button');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // var createItemButton = document.getElementByClassName('new-item-create');
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // var deleteButtons = document.getElementsByClassName('btn-delete');
+
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
+
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
+
+  // <div class="container">
+  //     <button onclick="this.innerHTML = 'You clicked'">Click Me</button>
+  // </div>
 };
