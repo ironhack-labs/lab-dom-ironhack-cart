8e8c2af6bb5128d1798b5863cf8780049511a999
Alex McDaniel <amcdaniel186@gmail.com>
2018-10-29 20:00:47 -0400

    dom-cart-updated

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,40 @@
-//
+// let inputF = byId("inputF")
 
 
+
+
+function deleteItem(e){
+  
+}
+
+function getPriceByProduct(itemNode){
+  
+}
+
+function updatePriceByProduct(productPrice, index){
+  
+}
+
+function getTotalPrice() {
+    let inputF = byClass("inputF");
+    let totalPrice = byClass("totalPrice");
+    let itemPrice = byClass("itemPrice");
+    for(let i = 0; i < itemPrice.length; i++){
+      let currPrice = itemPrice[i].innerHTML;
+      let currInput = inputF[i].value;
+      let currTPrice = currPrice*currInput;
+      totalPrice[i].innerHTML = currTPrice;
+      console.log(currTPrice);
+    }
+    
+    // take value in the center bar and multiply it by the item price.
+    
+    // let totalPrice = document.("itemPrice");
+  }
+  
+
+// multiply amount units by itemPrice
+// default price = 0
 
 let byClass = (theClass=>{return document.getElementsByClassName(theClass)})
 
@@ -10,9 +44,8 @@ let by = (id=>{return document.getElementById(id)})
 let byId = (id=>{return document.getElementById(id)})
 
 
-let button = byId("cal-button")
+let button = byId("calc-button")
 
-let inputF = byId("inputF") 
 
 
 let inputArr = byClass("totalPrice")
@@ -20,31 +53,9 @@ console.log(inputArr)
 // button.onclick(()=>{
 
 // })
-console.log(button)
-console.log(inputF)
-
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-
-  // let totalPrice = document.("itemPrice");
-}
-
-// let calcButton = (getDocumentByID);
 
 function createQuantityInput(){
-
+  
 }
 
 function createDeleteButton(){
@@ -68,13 +79,13 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  // var calculatePriceButton = document.getElementByClassName('calc-prices-button');
+  let calculatePriceButton = document.getElementById('calc-prices-button');
 
   // var createItemButton = document.getElementByClassName('new-item-create');
 
   // var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  // calculatePriceButton.onclick = getTotalPrice;
+  calculatePriceButton.onclick = getTotalPrice;
   // createItemButton.onclick = createNewItem;
 
   // for(var i = 0; i<deleteButtons.length ; i++){
@@ -85,3 +96,23 @@ window.onload = function(){
   //     <button onclick="this.innerHTML = 'You clicked'">Click Me</button>
   // </div>
 };
+
+
+
+
+
+
+// var arr = [].slice.call(htmlCollection)
+// Generates a random color in hexadecimal (ie. #62b9cc)
+// function generateRandomColor() {
+//   return '#'+Math.floor(Math.random()*16777215).toString(16);
+// }
+
+// function changeColor() {
+//   var discoBoxes = document.getElementsByClassName("disco-box");
+//   for (i=0; i<discoBoxes.length; i++) {
+//     discoBoxes[i].style.backgroundColor = generateRandomColor();
+//   }
+// }
+
+// setInterval(changeColor, 2400)
