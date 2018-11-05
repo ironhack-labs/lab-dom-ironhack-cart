1d89bb39cf9c6150614999de56ad1eb2b10bea11
MontseOrtiz <montse_2908@hotmail.com>
2018-11-04 19:49:31 -0600

    MEX DE MONTSEORTIZ

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,15 @@
 function deleteItem(e){
-
+  var selectedRow = e.currentTarget.parentNode.parentNode;
+  var producto = selectedRow.parentNode;
+  producto.removeChild(selectedRow)
 }
 
 function getPriceByProduct(itemNode){
 
+  var cantidad = document.getElementsByClassName ("quantity").value;
+  var precio = document.getElementsByClassName ("productPrice").value;
+  
+  document.getElementById ("totalUnProd").innerHTML = cantidad * precio
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +17,14 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var totalPrice = 0;
+  var totalUnProd = document.getElementsById ("totalUnProd")
 
+  for (var i = 0; i < totalUnProd.length; i++){
+    var price = getPriceByProduct(totalUnProd[i]);
+
+  }
+  
 }
 
 function createQuantityInput(){
