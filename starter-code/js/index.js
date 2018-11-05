90079232f2d097dc65a0cb70ed5153e90b59563b
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-05 01:51:05 -0600

    Not Done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,12 @@
+function calculatePrices(number){
+  let quantity = document.getElementsById("unidadesBubble");
+  let price = document.getElementsByClassName("price");
+  let priceTotal = (quantity * price)
+  var final = document.getElementById("totalBubble");
+  final.innerHTML=("$"+priceTotal);
+  return priceTotal
+}
+
 function deleteItem(e){
-
+  
 }
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
