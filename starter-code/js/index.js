864a412236c3fde98935ac74c4e7b8fbd54bff6f
jaqusa <dev@javierquinte.com>
2018-11-05 01:12:32 -0600

    stuck

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,9 @@
+var unitprice = document.getElementById("unitprice")
+var quantity = document.getElementsByTagName("input")
+var  price = document.getElementById("price")
+// var delete = document.getElementsByClassName("btn-delete")
+// var create = document.getElementsByClassName("btn-success")
+
 function deleteItem(e){
 
 }
@@ -11,6 +17,8 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  console.log(price)
+  
 
 }
 
@@ -38,15 +46,15 @@ function createNewItem(){
 
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+// window.onload = function(){
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+//   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+//   calculatePriceButton.onclick = getTotalPrice;
+//   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
+//}
