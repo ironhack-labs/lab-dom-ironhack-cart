dbe19ef6d77a69f44a25783753cff68db7c36109
Fernando Hernández Báez <Fernando1@MacBook-Pro-de-Fernando-6.local>
2018-11-04 22:39:02 -0600

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,13 +6,59 @@ function getPriceByProduct(itemNode){
 
 }
 
+function calculatePrices(){
+
+  var totalSum = 0;
+  var total = document.getElementById ("flex-container");
+  
+  }
+  //
+
+
+{
+			var price = document.getElementsByTagName("precioUnitario").value;
+			var quantity = document.getElementsByTagName("input").value;
+			var totalPriceProduct = price * quantity;
+
+
+
+		}
+
+
+
+function getTotalPrice() {
+
+  document.getElementById("totalpriceProduct").innerHTML = totalSum
+
+  getTotalPrice();
+
+
+
+
+calculatePrices()
+
+  
+
 function updatePriceByProduct(productPrice, index){
 
 }
 
+
+
 function getTotalPrice() {
 
+
+
+  function getTotalPrice() {
+    document.getElementById("suma").innerHTML = "Hello World";
+    
+  }
+
 }
+var paragraph = document.getElementById('paragraph');
+var links = document.getElementsByClassName('link');
+var divs = document.getElementsByTagName('div');
+
 
 function createQuantityInput(){
 
@@ -49,4 +95,10 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+
+}
+
+
+
+
+}     
