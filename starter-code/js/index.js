0a718039bf3ff21568ee17fb0d8f35c948d16164
sanmiranda <sanmiranda@hotmail.com>
2018-11-04 23:32:04 -0600

    va

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,21 +1,33 @@
 function deleteItem(e){
-
+  
+  document.getElementsByClass("btn-delete").onclick = function(){
+    var productoDiv = document.getElementsByClass("producto1")
+    productoDiv.innerHTML = ""; 
+  };
 }
+ 
 
 function getPriceByProduct(itemNode){
-
+    var precioItem = document.getElementById('precio');
 }
 
 function updatePriceByProduct(productPrice, index){
+var pretot = precioItem * input
+pretot = document.setElementById('total')
+};
 
-}
 
 function getTotalPrice() {
-
+document.setElementsByClass('btn-success').onclick = function(){
+(document.getElementById('precio') * (document.getElementsByClass('quantity'))
+};
 }
+console.log(setElementById('total'))
+
 
 function createQuantityInput(){
-
+  var input = document.getElementsByTagName('input')[0];
+  console.log(input.value); 
 }
 
 function createDeleteButton(){
@@ -23,8 +35,10 @@ function createDeleteButton(){
 }
 
 function createQuantityNode(){
+var quantity = document.getElementsByTagName('input');
 
 }
+
 
 function createItemNode(dataType, itemData){
 
