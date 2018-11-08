ca74d748072ec7c78dccaea4bcdc952804b26269
Clara Garcia <clara.garcia.pascual@accenture.com>
2018-11-08 12:27:49 +0100

    feat: Iteration 1: Creating one product done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,15 +1,17 @@
-function productPriceTotal() {
+function productsPriceTotal() {
+  var myNodelist = document.querySelectorAll(".product");
+  for (var i = 0; i < myNodelist.length; i++) {
+  var eachProduct = myNodelist.item(i);
 
-  var priceProduct = document.querySelector('.product .product-price span');
+  var priceProduct = eachProduct.querySelector('.product-price span');
   var priceProductNum = priceProduct.textContent;
-
-  var quantityProduct = document.querySelector('.product .product-quantity .qyt');
+  var quantityProduct = eachProduct.querySelector('.product-quantity .qyt');
   var quantityProductNum = quantityProduct.value;
-
   var productPrice = priceProductNum * quantityProductNum;
+  var finalProductPrice = eachProduct.querySelector('.product-total-price span');
 
-  var finalProductPrice = document.querySelector('.product .product-total-price span');
   finalProductPrice.innerText = productPrice;
+  }
 }
 
 function deleteItem(event){
@@ -54,11 +56,12 @@ function createNewItem(){
 }
 
 window.onload = function(){
+
   var calculatePriceButton = document.getElementById('calc-prices-button');
   //var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = productPriceTotal;
+  calculatePriceButton.onclick = productsPriceTotal;
   //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
