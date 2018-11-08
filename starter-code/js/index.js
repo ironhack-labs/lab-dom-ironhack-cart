e05765cc964945463054084aead9f8b0f956b072
Clara Garcia <clara.garcia.pascual@accenture.com>
2018-11-08 17:38:50 +0100

    feat: Iteration 2: Add another product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,30 @@
-function productsPriceTotal() {
+var totalPrices = 0
+
+function getTotalPrice() {
   var myNodelist = document.querySelectorAll(".product");
+
   for (var i = 0; i < myNodelist.length; i++) {
-  var eachProduct = myNodelist.item(i);
+    var eachProduct = myNodelist.item(i);
+    
+    var priceProduct = eachProduct.querySelector('.product-price span');
+    var priceProductNum = priceProduct.textContent;
 
-  var priceProduct = eachProduct.querySelector('.product-price span');
-  var priceProductNum = priceProduct.textContent;
-  var quantityProduct = eachProduct.querySelector('.product-quantity .qyt');
-  var quantityProductNum = quantityProduct.value;
-  var productPrice = priceProductNum * quantityProductNum;
-  var finalProductPrice = eachProduct.querySelector('.product-total-price span');
+    var quantityProduct = eachProduct.querySelector('.product-quantity .qty');
+    var quantityProductNum = quantityProduct.value;
 
-  finalProductPrice.innerText = productPrice;
+    var productPrice = priceProductNum * quantityProductNum;
+    var finalProductPrice = eachProduct.querySelector('.product-total-price span');
+    var totalPrice = finalProductPrice
+    totalPrice.innerHTML;
+
+    finalProductPrice.innerText = productPrice;
+
+    totalPrices += finalProductPrice;
   }
+  console.log(totalPrices);
+  document.getElementById('cart-total span').innerHTML;
 }
+
 
 function deleteItem(event){
   var deleteBtn = event.target;
@@ -24,10 +36,6 @@ function getPriceByProduct(itemNode){
 }
 
 function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
 
 }
 
@@ -61,7 +69,7 @@ window.onload = function(){
   //var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = productsPriceTotal;
+  calculatePriceButton.onclick = getTotalPrice;
   //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
