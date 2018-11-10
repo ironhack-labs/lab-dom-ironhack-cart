1b264a7d3797a033fa8525d17c454cad2a0e8ec5
jose-martinez-albarran <jose.jesus.martz@gmail.com>
2018-11-10 09:25:45 -0600

    Pending Delete row and add Row by Jose Martinez

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,20 +2,58 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(){
+  var ItemPrice = document.getElementById("itemNode");
+   return ItemPrice.innerText;
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  
 }
 
 function getTotalPrice() {
-
+  var price = getPriceByProduct().replace('$','');
+  var quantity = document.getElementById("quantity").value;  
+  var totalP = "$" + ((price * quantity).toFixed(2));
+  var Total = document.getElementById("Total");
+  zero = 0;
+  isNaN(quantity) ? Total.innerText = ('$' + zero.toFixed(2)) : Total.innerText = totalP;
 }
 
-function createQuantityInput(){
 
+function getPriceByProduct1(){
+  var ItemPrice1 = document.getElementById("itemNode1");
+   return ItemPrice1.innerText;
+}
+
+
+function getTotalPrice1() {
+  var price1 = getPriceByProduct1().replace('$','');
+  var quantity1 = document.getElementById("quantity1").value;  
+  var totalP1 = "$" + ((price1 * quantity1).toFixed(2));
+  var Total1 = document.getElementById("Total1");
+  zero = 0;
+  isNaN(quantity1) ? Total1.innerText = ('$' + zero.toFixed(2)) : Total1.innerText = totalP1;
+}
+
+function getTotalPriceT(){
+  getTotalPrice();
+  getTotalPrice1();
+  Total();
+}
+
+function Total(){
+  var totalp1 = document.getElementById("Total").innerText;
+  var totalp2 = document.getElementById("Total1").innerText;
+  var sum = parseInt((totalp1.replace('$',''))) + parseInt((totalp2.replace('$','')));
+  var sumTotal = document.getElementById("GrandToltal");
+  zero = 0;
+  isNaN(sum) ? sumTotal.innerText = ('$' + zero.toFixed(2)) : sumTotal.innerText = '$'+(sum.toFixed(2));
+}
+
+
+function createQuantityInput(){
+   
 }
 
 function createDeleteButton(){
@@ -40,13 +78,18 @@ function createNewItem(){
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
+  getPriceByProduct();
+  getTotalPriceT();
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  calculatePriceButton.onclick = getTotalPriceT;
+  //addbutton.onclick = createQuantityInput;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+
+  
 };
