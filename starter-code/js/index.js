f420787c5682a9df4bc0af00b2f7d0d3f52b5a39
Marco Monter <monter.marco@gmail.com>
2018-11-10 08:59:16 -0600

    started

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,28 @@
 function deleteItem(e){
-
-}
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+};
 
 function getPriceByProduct(itemNode){
+  var ietmPrice =   document.getElementById("cost-unit");
+  var items = document.getElementsByTagName('input')[0]
+  if (items.value.length == 0){
+  console.log("Enter item quantity")  
+  } else {
+  return items.value.length * itemPrice;
+}    
+};
+console.log(getTotalPrice());
 
-}
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
-
 }
+
 
 function createQuantityInput(){
 
@@ -45,8 +55,3 @@ window.onload = function(){
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
