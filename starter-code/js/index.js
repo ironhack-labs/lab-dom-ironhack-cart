3e3ac4ea4babb4a105d303625d95b62de2c3e200
JZunigaV <35680089+JZunigaV@users.noreply.github.com>
2018-11-09 18:33:34 -0600

    Update index.js

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -10,7 +10,24 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
+//Función para calcular el precio total
 function getTotalPrice() {
+
+  debugger;
+  var products = document.getElementsByClassName("product");
+
+  for (var index = 0; index < products.length; index++) {
+    
+    var quantity = products[index].getElementsByTagName("input")[0].value;
+    var singlePrice = 
+    products[index].getElementsByClassName("product-price")[0].innerText.replace("$","");
+    
+    var totalPrice = Number(quantity) * Number(singlePrice);
+
+    products[index].getElementsByClassName("totalPrice")[0].innerText =
+    parseFloat(totalPrice).toFixed(2);
+  }
+
 
 }
 
@@ -39,14 +56,17 @@ function createNewItem(){
 }
 
 window.onload = function(){
+ 
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+
+
+  // var createItemButton = document.getElementById('new-item-create');
+  // var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
