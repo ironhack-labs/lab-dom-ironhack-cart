266534c1d5e77e277e6af20354e802f0a37a47fd
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2018-11-10 01:14:02 -0600

    i need more timegit status

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,30 +1,71 @@
+
+var parentElement = document.getElementsByTagName('body')[0];
+//console.log(parentElement);
+var parentDiv = document.getElementById("main-div")
+//console.log(parentDiv);
+
+var nameOfProductDiv = document.getElementById("nameOfProduct");
+var spanNameOfProduct = document.createElement("span");
+var nameOfProduct = document.createTextNode("IronBubble-head");
+//console.log(spanNameOfProduct);
+spanNameOfProduct.appendChild(nameOfProduct);
+nameOfProductDiv.appendChild(spanNameOfProduct);
+
+var singlePriceDiv = document.getElementById("singlePrice");
+//console.log(singlePriceDiv);
+var spanSingleCost = document.createElement("span");
+var singleCost = document.createTextNode("$25.00")
+spanSingleCost.appendChild(singleCost);
+singlePriceDiv.appendChild(spanSingleCost);
+
+var qtyInputDiv = document.getElementById("qty");
+var labelQTY = document.createElement("label");
+var qtyText = document.createTextNode("QTY");
+labelQTY.appendChild(qtyText);
+var inputQTY = document.createElement("input");
+inputQTY.setAttribute("type", "text");
+qtyInputDiv.appendChild(labelQTY);
+qtyInputDiv.appendChild(inputQTY);
+
+
+
+
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
-  let singlePrice = document.getElementsByClassName("single-price");
-  return singlePrice
+  var gettingSinglePrice = document.getElementById("single-price").textContent.split("$");
+  var singlePrice = parseInt(gettingSinglePrice[1])
+  //console.log(singlePrice);
+  return singlePrice;
 }
+//getPriceByProduct('<span id="single-price">$27.00</span>');
 
-let individualPrice = getPriceByProduct();
+
 
 function updatePriceByProduct(productPrice, index){
-  var updatePrice = document.getElementById("total-price");
-  updatePrice.value = individualPrice * quantityInput;
+  
 
 }
 
 function getTotalPrice() {
+ var totalPrice = createQuantityInput * getPriceByProduct;
+ console.log(totalPrice);
+}
+//getTotalPrice()
 
+ function createQuantityInput(){
+   var calcPricesBtn = document.getElementById("calculate-prices-button");
+
+  calcPricesBtn.onclick = function(){
+    var input = document.getElementsByTagName("input")[0];
+    console.log(input.value);
+    return input.value
+  }
 }
 
-function createQuantityInput(){
-  let quantity = document.getElementById("qty");
-  console.log(quantity)
-  return quantity;
-}
-
+//createQuantityInput();
 
 function createDeleteButton(){
 
