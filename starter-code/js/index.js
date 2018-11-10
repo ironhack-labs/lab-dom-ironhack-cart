a2ef822fe97d3f4a410115ca5c7d40f6c4982407
JzunigaV <j_zuniga4@hotmail.com>
2018-11-10 00:29:22 -0600

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,17 @@
 function deleteItem(e){
+ 
+  var target = document.getElementById(e.currentTarget.id);
+  var parent = target.parentElement.parentElement;
+  
+  
+       while (parent.firstChild != null) {
+         parent.removeChild(parent.firstChild);
+       }
 
-}
+
+
+
+} 
 
 function getPriceByProduct(itemNode){
 
@@ -13,22 +24,27 @@ function updatePriceByProduct(productPrice, index){
 //Función para calcular el precio total
 function getTotalPrice() {
 
-  debugger;
-  var products = document.getElementsByClassName("product");
+  var quantity = 0;
+  var products = document.getElementsByClassName("product-div");
+  var singlePrice = 0;
+  var totalPrice = 0;
+  var precioTotal = 0;
 
+  //precios indivuduales
   for (var index = 0; index < products.length; index++) {
     
-    var quantity = products[index].getElementsByTagName("input")[0].value;
-    var singlePrice = 
-    products[index].getElementsByClassName("product-price")[0].innerText.replace("$","");
-    
-    var totalPrice = Number(quantity) * Number(singlePrice);
+     quantity = products[index].getElementsByTagName("input")[0].value;
+     singlePrice = products[index].getElementsByClassName("product-price")[0].innerText.replace("$","");
+     totalPrice = parseFloat(quantity) * parseFloat(singlePrice);
+     products[index].getElementsByClassName("totalPrice")[0].innerText =
+     parseFloat(totalPrice).toFixed(2);
 
-    products[index].getElementsByClassName("totalPrice")[0].innerText =
-    parseFloat(totalPrice).toFixed(2);
+    //para calucular el precio total
+     precioTotal = precioTotal + totalPrice;
   }
 
-
+    //asignación del precio total a la etiqueta
+    document.getElementById("total-final").innerText = precioTotal;
 }
 
 function createQuantityInput(){
@@ -53,20 +69,31 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 function createNewItem(){
 
+  debugger;
+  var inputName = document.getElementById("input-text").value;
+  var inputPrice = document.getElementById("input-price").value;
+
+  var father = document.getElementById("father");
+  var item =  document.getElementsByClassName("product-div")[0];
+  clone = item.cloneNode(true); // true means clone all childNodes and all event handlers
+  clone.class = "cloned-node";
+  father.appendChild(clone);
+
 }
 
 window.onload = function(){
  
   var calculatePriceButton = document.getElementById('calc-prices-button');
-
-
-  // var createItemButton = document.getElementById('new-item-create');
-  // var deleteButtons = document.getElementsByClassName('btn-delete');
-
   calculatePriceButton.onclick = getTotalPrice;
-  // createItemButton.onclick = createNewItem;
 
-  // for(var i = 0; i<deleteButtons.length ; i++){
-  //   deleteButtons[i].onclick = deleteItem;
-  // }
+
+  var createItemButton = document.getElementById('new-item-create');
+   createItemButton.onclick = createNewItem;
+
+ 
+   var deleteButtons = document.getElementsByClassName('btn-delete');
+  
+   for(var i = 0; i<deleteButtons.length ; i++){
+     deleteButtons[i].onclick = deleteItem;
+   }
 };
