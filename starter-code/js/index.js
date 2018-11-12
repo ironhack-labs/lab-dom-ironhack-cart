0da3eae3727c31552fc6ebeecd36143709ad0ec5
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2018-11-12 01:44:51 +0100

    Almost finished, I have to make the delette button.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 function deleteItem(e){
-
+  
 }
 
 function getPriceByProduct(itemNode){
@@ -13,7 +13,7 @@ function getQuantityByProduct(itemNode){
 }
 
 function updatePriceByProduct(product, price) {
-  var result = document.getElementById('result');
+  var result = product.getElementsByClassName('result')[0];
   var price = document.createTextNode('$' + price.toFixed(2));
   result.innerText = '';
   result.appendChild(price);
@@ -41,32 +41,80 @@ function updateTotalPrice(price) {
   totalPrice.appendChild(price);
 }
 
-function calculateProductPrice(product) {
+function createName(name) {
+  var nameDiv = document.createElement('div');
+  var name = document.createTextNode(name);
+  var nameSpan = document.createElement('span');
+  nameDiv.classList.add('name');
+  nameSpan.appendChild(name);
+  nameDiv.appendChild(nameSpan);
+  return nameDiv;
+}
 
+function createPriceValue(price) {
+  var priceDiv = document.createElement('div');
+  priceDiv.classList.add('price-value');
+  var price = document.createTextNode(price);
+  var priceSpan = document.createElement('span');
+  priceSpan.appendChild(price);
+  priceDiv.appendChild(priceSpan);
+  return priceDiv;
 }
 
 function createQuantityInput(){
+  var quantityDiv = document.createElement('div');
+  var label = document.createElement('label');
+  label.appendChild(document.createTextNode('QTY'));
+  var quantityInput = document.createElement('input');
+  quantityInput.setAttribute('type', 'number');
+  quantityInput.setAttribute('placeholder', '0');
+  quantityInput.setAttribute('class', 'quantity');
+  quantityDiv.appendChild(label);
+  quantityDiv.appendChild(quantityInput);
+  return quantityDiv;
+}
 
+function createTotalPrice(){
+  var totalDiv = document.createElement('div');
+  var totalSpan = document.createElement('span')
+  totalSpan.appendChild(document.createTextNode('$0.00'));
+  totalSpan.classList.add('result');
+  totalDiv.appendChild(totalSpan);
+  return totalDiv;
 }
 
 function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
+  var deleteDiv = document.createElement('div');
+  var deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('btn');
+  deleteBtn.classList.add('btn-delete');
+  deleteBtn.appendChild(document.createTextNode('Delete'));
+  deleteBtn.onclick = deleteItem();
+  deleteDiv.appendChild(deleteBtn);
+  return deleteDiv;
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
+  var product = document.createElement('div');
+  product.classList.add('product');
+  product.appendChild(createName(itemName));
+  product.appendChild(createPriceValue(itemUnitPrice));
+  product.appendChild(createQuantityInput());
+  product.appendChild(createTotalPrice());
+  product.appendChild(createDeleteButton());
 
+  var chart = document.getElementById('chart');
+  chart.appendChild(product);
 }
 
 function createNewItem(){
-
+  var itemName = document.querySelector('.new-name').value;
+  var itemUnitPrice = parseFloat(document.querySelector('.new-price').value);
+  if (itemUnitPrice >= 0) {
+    createNewItemRow(itemName, itemUnitPrice);
+  } else {
+    alert('Introduce a correct price for your product.');
+  }
 }
 
 window.onload = function(){
