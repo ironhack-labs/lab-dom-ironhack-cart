947ac3a7f0db17f6d932d2099e8b02f995b5e387
sogchs <i2implantologia@Mac-Pro-de-i2.local>
2018-11-14 13:03:22 +0100

    Tercera subida, el código sigue sin funcionar

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -63,23 +63,74 @@ function getTotalPrice() {
 
 
 function createDeleteButton(){
+  var buttonDiv = document.createEvent ('div');
+  var deleteBtn = document.createElement ('button');
 
+  deleteBtn.setAttribute ('class', 'btn btn-delete');
+  deleteBtn.appendChild(document.createTextNode('Delete'));
+  deleteBtn.onclick = deleteItem;
+
+  buttonDiv.appendChild(deleteBtn);
+  return buttonDiv;
 }
 
 function createQuantityNode(){
+  var quantityNode = document.createElement ('div');
+  var labelNode = document.createElement('label');
+
+  quantityNode.appendChild(document.createElement('input'));
+  labelNode.appendChild(document.createTextNode("QTY"));
   
+  return quantityNode;
 }
 
-function createItemNode(dataType, itemData){
+function createItemName(name, price){
+  var nameDiv = document.createElement('div');
+  var name = document.createTextNode(name);
+  var nameSpan = document.createElement('span');
 
+  nameSpan.classList.add('name');
+  nameSpan.appendChild(name);
+  nameDiv.appendChild(nameSpan);
+
+  var priceDiv = document.createElement('div');
+  var price = document.createTextNode("$" + price.toFixed(2));
+  var priceSpan = document.createElement('span');
+
+  priceSpan.classList.add('price');
+  priceSpan.appendChild(price);
+  priceDiv.appendChild(priceSpan);
+
+  return nameDiv, priceDiv;
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(name, price){
+  product = document.createElement ('div');
+  product.classList.add('product');
 
+  product.appendChild(createDeleteButton());
+  product.appendChild(createItemNode(name, price));
+  product.appendChild(createQuantityNode());
+  product.appendChild(createTotalNode());
+}
+
+function createTotalNode () {
+  var totalNode = document.createElement('div');
+  var priceContainer = document.createElement('span');
+
+  priceContainer.classList.add('product-total-price');
+  priceContainer.appendChild(document.createTextNode("$0.00"));
+
+  totalNode.appendChild(priceContainer);
+  return totalNode;
 }
 
 function createNewItem(){
-
+  var name = document.querySelector(".new-product input[name = 'name']");
+  var price = parseFloat(document.querySelector(".new-price input[name = 'price']"));
+    if (name && price >= 0) {
+      createNewItemRow (name, price);
+    }
 }
 
 window.onload = function(){
