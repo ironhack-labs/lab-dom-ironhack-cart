0f1ea9a697478fab7d9822aa3945ad808e929935
Nicolas Castro <nicolascastro@me.com>
2018-11-16 21:26:45 -0500

    Added comments explaining my code

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,14 @@
 const priceButton = document.getElementById("calculateTotal");
 
+/*
+First Variables iterate through their corresponding class and get the quantity value and 
+item price. After that I loop through each class to obtain item price & quantity.
+
+Once I have the price and qty I add that value using innerHTML into the total class for each product
+
+At the end I loop through each total for each product add those into a variable that has 0 as its value
+and then push that final value using innerHTML into the final-total class.
+*/
 
 function getTotalPrice() {
   let pricePerUnit = document.querySelectorAll(".unit-price");
@@ -8,30 +17,12 @@ function getTotalPrice() {
   for (var i = 0 ; i < pricePerUnit.length; i++){
   let calculatePrice = pricePerUnit[i].innerHTML * qtyOrdered[i].value;  
   document.getElementsByClassName('total')[i].innerHTML = "$" + calculatePrice.toFixed(2);
-
  result += calculatePrice; 
   }
   document.getElementsByClassName('total-price')[0].innerHTML = "$" + result.toFixed(2);
 }
 
 priceButton.onclick = getTotalPrice;
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
