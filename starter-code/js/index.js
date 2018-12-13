ef1db9935ab3f1b9956b15536855a2ff4f0e3d60
Mar Gal <mgalloperalta@gmail.com>
2018-12-13 18:36:59 +0100

    separated functionality into various functions, getprice, get quantity, calculate total. Button does only one thing

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,43 +1,33 @@
 
-
-// let productPriceString = document.getElementsByClassName('product')[0].innerText;
-// let productPriceNumber = parseFloat(productPriceString.substring(1));
-// let calcButton = document.getElementById('calc-prices-button');
-
-// calcButton.onclick = function(){
-// 	let input = document.getElementsByTagName('input')[0].value;
-//   let total = productPriceNumber * input;
-//   let totalPricePerProduct = document.getElementsByClassName('total-per-product')[0];
-//   totalPricePerProduct.innerText = `$ ${total}.00`;
-//   return totalPricePerProduct;
-// };
-
-
-
 let calcButton = document.getElementById('calc-prices-button');
 
-function quantity (item){
-  let input = item.getElementsByTagName('input')[0].value;
-  return input;
-}
-
-function getPrice (item){
-  let totalPricePerProduct = item.getElementsByClassName('total-per-product')[0];
-  totalPricePerProduct.innerText = `$ ${total}.00`;
-  return totalPricePerProduct;
-}
-
-let items = document.getElementsByClassName('item');
-
-calcButton.onclick = function(item){
-  // let totalPricePerProduct = item[i].getElementsByClassName('total-per-product')[0];
-  for (var i = 0; i > item.length; i++){
-    quantity(item[i]);
-    getPrice(item[i]);
-
-  }
-  return totalPricePerProduct;
+calcButton.onclick = function(){
+	getfinalProductPrice();
 };
 
 
-  
+
+
+function getPrice (product){
+  let productPriceString = product.querySelector('.product>span').innerText;
+  let productPriceNumber = parseFloat(productPriceString.substring(1));
+  let input = product.querySelector('.quantity').value;
+  return productPriceNumber * input;
+}
+
+function updateTotalByProduct (product){
+  let totalPricePerProduct = product.querySelector('.total-per-product');
+  totalPricePerProduct.innerText = getPrice(product); 
+  return totalPricePerProduct;     
+}
+
+function getfinalProductPrice() {
+  let products = document.querySelector('.shopping-elements').children;
+  let total = 0; 
+  for (let i = 0; i < products.length; i++) { 
+    total += parseFloat(updateTotalByProduct(products[i]).innerText); 
+    }
+    console.log(total);
+}
+
+
