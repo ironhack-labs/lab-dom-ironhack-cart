a5ec1c0dc956447b839b44cead8cf10579028da8
Mar Gal <mgalloperalta@gmail.com>
2018-12-13 15:20:44 +0100

    added new item to the shopping list html document

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,22 +1,43 @@
 
 
-let productPriceString = document.getElementsByClassName('product')[0].innerText;
-let productPriceNumber = parseFloat(productPriceString.substring(1));
+// let productPriceString = document.getElementsByClassName('product')[0].innerText;
+// let productPriceNumber = parseFloat(productPriceString.substring(1));
+// let calcButton = document.getElementById('calc-prices-button');
+
+// calcButton.onclick = function(){
+// 	let input = document.getElementsByTagName('input')[0].value;
+//   let total = productPriceNumber * input;
+//   let totalPricePerProduct = document.getElementsByClassName('total-per-product')[0];
+//   totalPricePerProduct.innerText = `$ ${total}.00`;
+//   return totalPricePerProduct;
+// };
+
+
+
 let calcButton = document.getElementById('calc-prices-button');
 
-calcButton.onclick = function(){
-	let input = document.getElementsByTagName('input')[0].value;
-  let total = productPriceNumber * input;
-  let totalPricePerProduct = document.getElementsByClassName('total-per-product')[0];
-  totalPricePerProduct.innerText = `$ ${total}.00`;
-  return totalPricePerProduct;
-};
-
-function calcPrices (product, index){
-
+function quantity (item){
+  let input = item.getElementsByTagName('input')[0].value;
+  return input;
 }
 
+function getPrice (item){
+  let totalPricePerProduct = item.getElementsByClassName('total-per-product')[0];
+  totalPricePerProduct.innerText = `$ ${total}.00`;
+  return totalPricePerProduct;
+}
 
+let items = document.getElementsByClassName('item');
+
+calcButton.onclick = function(item){
+  // let totalPricePerProduct = item[i].getElementsByClassName('total-per-product')[0];
+  for (var i = 0; i > item.length; i++){
+    quantity(item[i]);
+    getPrice(item[i]);
+
+  }
+  return totalPricePerProduct;
+};
 
 
   
