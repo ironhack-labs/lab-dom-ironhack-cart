b3196e068cb1f83047959f8e23c5416b1a700a75
Oriol Carbo <oriol.carbo@gmail.com>
2018-12-14 13:57:28 +0100

    Working. Pending comments and polish few details

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -26,28 +26,95 @@ function getTotalPrice() {
   document.querySelector('.total-price').innerText = totalPrice;
 }
 
-function createQuantityInput(){
+function createProductName(name) {
+  let divBorn = document.createElement('div');
+  
+  let spanBorn = document.createElement('span');
+  let spanText = document.createTextNode(name);
+  spanBorn.appendChild(spanText);
+  divBorn.appendChild(spanBorn);
 
+  return divBorn;
+}
+
+function createProductPrice(price) {
+  let divBorn = document.createElement('div');
+  
+  let spanBorn = document.createElement('span');
+  let spanText = document.createTextNode(price + '$');
+  spanBorn.setAttribute('class', 'unitPrice');
+  spanBorn.appendChild(spanText);
+  divBorn.appendChild(spanBorn);
+
+  return divBorn;
+}
+
+function createQuantityInput(){
+  let divBorn = document.createElement('div');
+
+  let labelBorn = document.createElement('label');
+  let labelText = document.createTextNode('QTY');
+  labelBorn.setAttribute('for', 'texttest');
+  labelBorn.appendChild(labelText);
+
+  let inputBorn = document.createElement('input');
+  inputBorn.setAttribute('class', 'quantity');
+  inputBorn.setAttribute('type', 'number');
+  inputBorn.setAttribute('value', '0');
+  inputBorn.setAttribute('placeholder', '0');
+  inputBorn.setAttribute('name', 'qty');
+  
+  divBorn.appendChild(labelBorn);
+  divBorn.appendChild(inputBorn);
+
+  return divBorn;
+}
+
+function createSubtotalPrice(){
+  let divBorn = document.createElement('div');
+  
+  let spanBorn = document.createElement('span');
+  let spanText = document.createTextNode('0');
+  spanBorn.setAttribute('class', 'subTotal');
+  spanBorn.appendChild(spanText);
+  divBorn.appendChild(spanBorn);
+
+  return divBorn;
 }
 
 function createDeleteButton(){
+  let divBorn = document.createElement('div');
 
-}
+  let buttonBorn = document.createElement('button');
+  let buttonText = document.createTextNode('Delete');
+  buttonBorn.setAttribute('class', 'btn-delete btn');
+  buttonBorn.appendChild(buttonText);
+  divBorn.appendChild(buttonBorn);
 
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
+  return divBorn;
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
+  let divBorn = document.createElement('div');
+  divBorn.setAttribute('class', 'product');
 
+  divBorn.appendChild(createProductName(itemName));
+  divBorn.appendChild(createProductPrice(itemUnitPrice));
+  divBorn.appendChild(createQuantityInput());
+  divBorn.appendChild(createSubtotalPrice());
+  divBorn.appendChild(createDeleteButton());
+
+  return divBorn;
 }
 
 function createNewItem(){
+  let newItemName = document.getElementById('create-name').value;
+  let newItemPrice = document.getElementById('create-price').value;
+  let containerProducts = document.getElementById('container-products');
 
+  if ( newItemName !== '' || newItemPrice >= 0){
+    containerProducts.appendChild(createNewItemRow(newItemName, newItemPrice));
+  } 
 }
 
 window.onload = function(){
@@ -56,7 +123,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
