e0052b1e57362c3766e3fb0a533657277e48bd41
Goldziher <nhirschfeld@gmail.com>
2019-01-13 22:45:10 +0100

    streamlined

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,5 @@
+//global scope 
+
 class Item {
   constructor(name, price, quantity) {
     this.name = name;
@@ -6,33 +8,7 @@ class Item {
   }
 }
 
-var itemArray = [
-  {
-    name: "IronBubble-Head",
-    price: 25,
-    quantity: 0
-  },
-  {
-    name: "IronShirt",
-    price: 15,
-    quantity: 0
-  },
-  {
-    name: "IronCup",
-    price: 10,
-    quantity: 0
-  },
-  {
-    name: "IronSticker",
-    price: 1,
-    quantity: 0
-  },
-  {
-    name: "IronAxe",
-    price: 100,
-    quantity: 0
-  }
-];
+var itemArray = [];
 
 var htmlCollectionInput;
 var htmlCollectionDelButton;
@@ -42,7 +18,14 @@ var createButton;
 var createFields;
 
 window.onload = function() {
-  mainHTMLTag = document.getElementById('main');
+
+  generateItem("IronBubble-Head", 25, 0);
+  generateItem("IronShirt", 15, 0);
+  generateItem("IronCup", 10, 0);
+  generateItem("IronSticker", 1, 0);
+  generateItem("IronAxe", 100, 0);
+  
+  mainHTMLTag = document.getElementById('main'); //the <main> element
   generateObjects(itemArray);
   setEventListeners();
 
@@ -51,24 +34,33 @@ window.onload = function() {
   createFields = document.getElementsByClassName("create-input");
   createButton.addEventListener('click', function () {
     if (createFields[0].value.length > 0) {
-      let createName = String(createFields[0].value);
-      let createPrice = parseInt(createFields[1].value);
-      let newItem = new Item(createName, createPrice, 0);
-      itemArray.push(newItem);
-    }
-    generateObjects(itemArray);
-    setEventListeners();
+      if (createFields[1].value.length > 0 && parseFloat(createFields[1].value) != NaN && parseFloat(createFields[1].value) > 0) {
+        let createName = String(createFields[0].value);
+        let createPrice = parseFloat(createFields[1].value);
+        
+        //generate the new item and regenerate elements
+        generateItem(createName, createPrice, 0);
+        generateObjects(itemArray);
+        setEventListeners();
+      }  
+      else { alert("invalid price")}
+    }  
+    else { alert("invalid product-name")}  
   });
   
   caulculatePricesButton = document.getElementById("calculate-button");
   caulculatePricesButton.addEventListener('click', function(event) {
-    var total = 0;
+    let total = 0;
     for(i = 0; i < itemArray.length; i++) {
-      total += parseInt(itemArray[i].price * itemArray[i].quantity);
+      total += parseFloat(itemArray[i].price * itemArray[i].quantity);
     };
     event.currentTarget.nextElementSibling.innerHTML = "Total: " + "$" + total.toFixed(2).toString();
   })
 };
+
+function generateItem(name, price, quantity) {
+  itemArray.push(new Item(name, price, quantity)); 
+}
 
 function generateObjects(arr) {
   //clear the main tag children nodes
@@ -77,7 +69,7 @@ function generateObjects(arr) {
   }
 
   //generate elements
-  for (i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let div = document.createElement("div");
       div.className = "parent-row main-child";
       mainHTMLTag.appendChild(div);
@@ -129,13 +121,14 @@ function setEventListeners() {
 
   for (let i = 0; i < htmlCollectionInput.length ; i++) {
     htmlCollectionInput[i].addEventListener('click', function (event) {
-      itemArray[i].quantity = parseInt(event.currentTarget.value);
-      event.currentTarget.nextSibling.innerHTML = "$" + (parseInt(itemArray[i].quantity) * parseInt(itemArray[i].price)).toFixed(2);
+      itemArray[i].quantity = parseFloat(event.currentTarget.value);
+      event.currentTarget.nextSibling.innerHTML = "$" + (parseFloat(itemArray[i].quantity) * parseFloat(itemArray[i].price)).toFixed(2);
     })    
   };
   for (let i = 0; i < htmlCollectionDelButton.length; i++) {
     htmlCollectionDelButton[i].addEventListener('click', function(event) {
       event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode);
+      itemArray.splice(i,1);
     })
   };
 }
