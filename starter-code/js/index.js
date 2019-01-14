c280e1a807a97d0a006577aeae176053e36a289b
Jules <jules.badin@gmail.com>
2019-01-14 23:37:43 +0100

    almost done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,27 +1,9 @@
-function deleteItem(e) {}
-
 function updatePriceByProduct(productPrice, index) {}
 
 var calculatePriceBtn = document.querySelector(".btn-success");
 
 calculatePriceBtn.onclick = function getTotalPrice() {
-  // //Retrieves the unit price of the product :
-  // var unitPrice = document.querySelector('.unit-price');
-  // console.log(unitPrice.innerHTML);
-
-  //Retrieves the unit quantity of the product :
-
-  // var unitQty = document.querySelector('.quantity');
-
   var textField = document.querySelector(".quantity");
-  // unitQty.innerHTML = textField.value;
-  // console.log(unitQty.value);
-
-  //Calculates the total price based on this data :
-  // var totalPrice = unitPrice.innerHTML * unitQty.value;
-  // var total = document.querySelector('.total');
-  // total.innerHTML = totalPrice;
-  // console.log(totalPrice);
 
   var checkout = document.querySelectorAll(".unit-price");
   var unitQty = document.querySelectorAll(".quantity");
@@ -29,30 +11,68 @@ calculatePriceBtn.onclick = function getTotalPrice() {
   var total = document.querySelectorAll(".total");
   var checkoutPrice = document.querySelector("checkout-price");
 
-  // onePrice.value;
-
   checkout.forEach(function(oneItem, index) {
-    console.log(unitPrice[index].innerHTML);
+    // console.log(unitPrice[index].innerHTML);
 
     unitQty[index].innerHTML = textField.value;
-    console.log(unitQty[index].value);
+    // console.log(unitQty[index].value);
 
     var totalPrice = unitPrice[index].innerHTML * unitQty[index].value;
     total[index].innerHTML = totalPrice;
-    console.log(totalPrice);
+    // console.log(totalPrice);
 
     checkoutPrice = checkoutPrice + totalPrice;
+    checkoutPrice.innerHTML = checkoutPrice;
   });
 
   console.log(checkoutPrice);
 };
 
-function createDeleteButton() {}
+var deleteBtn = document.querySelectorAll(".btn-delete");
 
-function createQuantityNode() {}
+deleteBtn.forEach(function(oneBtn) {
+  oneBtn.onclick = function() {
+    // console.log("hello deleteBtn");
 
-function createItemNode(dataType, itemData) {}
+    var parentLine = oneBtn.parentNode.parentNode;
+    parentLine.remove();
+  };
+});
 
-function createNewItemRow(itemName, itemUnitPrice) {}
+var createBtn = document.querySelector(".create");
 
-function createNewItem() {}
+createBtn.onclick = function() {
+  var textField = document.querySelector(".product-name");
+  var priceField = document.querySelector(".product-price");
+
+  var newLine = document.createElement("section");
+  newLine.style.display = "flex";
+  newLine.style.justifyContent = "space-around";
+  newLine.style.paddingTop = "3%";
+
+  newLine.innerHTML =
+    "<div><span>" +
+    textField.value +
+    " </span></div>" +
+    "<div>$ <span class='unit-price'>" +
+    priceField.value +
+    "</span></div>" +
+    "<div><label>QTY : </label> <input class='quantity' type='number' placeholder='0' /></div>" +
+    "<div>$ <span class='total'>0.00</span></div> " +
+    "<div><button class='btn btn-delete'>Delete</button></div>";
+
+  var line = document.querySelector(".checkout");
+  line.appendChild(newLine);
+
+  textField.value = "";
+  priceField.value = "";
+
+  var oneBtn = newLine.querySelector(".btn-delete");
+
+  oneBtn.onclick = function() {
+    // console.log("hello deleteBtn");
+
+    var parentLine = oneBtn.parentNode.parentNode;
+    parentLine.remove();
+  };
+};
