6cfc001628bfde78161468afb67dbf0194c797ba
unknown <aurelie.benovici@gmail.com>
2019-01-14 17:23:59 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,30 @@
-function deleteItem(e){
+function deleteItem(){
+
+  var deleteItemArray = document.querySelectorAll(".btn-delete");
+
+  deleteItemArray.forEach(function (oneButton) {
+      oneButton.onclick = function () {
+  
+        var parentContainer = oneButton.parentNode;
+  console.log(parentContainer);
+        parentContainer.parentNode.remove();
+      }
+   
+  
+  })
+
 
 }
+
+deleteItem();
+
+
+
+
+
+
+
+
 
 function getPriceByProduct(itemNode){
 
@@ -10,9 +34,58 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
+var calcBtn = document.querySelector(".btn-success");
+calcBtn.onclick = function getTotalPrice() {
+
+var quantityArray =[];
+var quantity= document.querySelectorAll(".quantity");
+var itemUnitPriceArray = [];
+var itemUnitPrice = document.querySelectorAll(".price");
+var totalPriceArray =[];
+var totalPrice = document.querySelectorAll(".total");
+
+// Loop in quantity 
+quantity.forEach(function (oneQuantity) {
+quantityArray.push(oneQuantity.value);
+  })
+// console.log(quantityArray);
+
+//Loop in itemUnitPrice
+itemUnitPrice.forEach(function(onePrice) {
+  console.log(onePrice.innerHTML)
+  itemUnitPriceArray.push(Number(onePrice.innerHTML.substring(2)))
+
+})
+
+console.log(itemUnitPriceArray)
+ 
+// totalPrice.innerHTML = "$" + (quantity.value * parseFloat(convertPrice).toFixed(2));
+
+
+for(var i=0; i <quantityArray.length; i++) {
+
+  totalPrice[i].innerHTML ="$" + (quantityArray[i] * itemUnitPriceArray[i]);
+
+totalPriceArray.push((quantityArray[i] * itemUnitPriceArray[i]))
 
 }
+console.log(totalPrice)
+
+var displayPrice = document.querySelector(".total-price");
+
+console.log(displayPrice);
+displayPrice.innerHTML = totalPriceArray.reduce(function (sum, currentValue){
+  
+   displayPrice = sum + currentValue;
+
+   return "$" + displayPrice;
+
+})
+
+
+}
+
+
 
 function createQuantityInput(){
 
@@ -30,9 +103,48 @@ function createItemNode(dataType, itemData){
 
 }
 
+// var createButton = document.querySelector(".create-btn")
+// createButton.onclick = function () {
+
+//   console.log("Coucou new item");
+
+//   // var newDiv = document.createElement("div");
+//   // console.log(newDiv);
+//   var newProductInput = document.querySelector(".newProductName")
+//   console.log(newProductInput);
+
+// // var newPriceInput = document.querySelector(".newPrice")
+
+//  var newProduct = document.createElement("span");
+//  console.log(newProduct);
+
+//  newProduct.innerHTML = newProductInput.value;
+
+//  var newCheckout = document.querySelector(".All-product")
+
+//  newCheckout.appendChild(productInput);
+
 function createNewItemRow(itemName, itemUnitPrice){
 
 }
+
+
+// }
+
+// var newProductInput = document.querySelector(".newProductName")
+// var newPriceInput = document.querySelector(".newPrice")
+
+//  var newProduct = document.createElement("span");
+
+//  newProduct.innerHTML = newProductInput.value
+
+//  var newCheckout = document.querySelector(".new-checkout")
+
+//  newCheckout.appendChild(".checkout");
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
 
 function createNewItem(){
 
