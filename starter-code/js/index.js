ff36e612e39656f2cf1d81dfea64c3f62c2e03c1
Bruno Dolce <brudolce@gmail.com>
2019-01-14 09:21:02 -0300

    it is missing the last iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,39 @@
+function calcPrices(){
+  
+  //evil array of prices
+  const prices0 = Array.from(document.getElementsByClassName("price"));
+  let prices = []
+  for(let i = 0; i < prices0.length; i += 1) {
+    prices.push(prices0[i].innerHTML);
+  }
+  console.log(prices)
+  
+  //good array of quantities
+  const qtys0 = Array.from(document.getElementsByClassName("qty"));
+  let qtys = []
+  
+  for(let i = 0; i < qtys0.length; i += 1) {
+    qtys.push(qtys0[i].value);
+  }
+  console.log(qtys)
+  
+  //calculating
+  const merc = Array.from(document.getElementsByClassName("mercadoria"))
+  let total = 0 
+  let sum=0
+  for(let i = 0; i < qtys0.length; i += 1){
+    total=prices[i]*qtys[i];
+    sum+=prices[i]*qtys[i];
+    document.getElementsByClassName("mercadoria")[i].childNodes[7].innerHTML=total
+  }
+  document.getElementById("totalPrice").innerHTML=" Total Price = " + sum
+    console.log("total price = " + sum)
+
+}
+
+const merc = Array.from(document.getElementsByClassName("mercadoria"))
+console.log(merc)
+
 function deleteItem(e){
 
 }
@@ -43,8 +79,8 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
