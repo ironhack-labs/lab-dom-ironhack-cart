9778bea2cd0bfaf1906904ce8b98adb7f06d317b
CGuillotG <c.guillot@outlook.com>
2019-01-21 09:29:03 -0600

    -

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,5 @@
 function deleteItem(e){
+  
 
 }
 
@@ -6,12 +7,26 @@ function getPriceByProduct(itemNode){
 
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(){ //, index
+  let prices = document.getElementsByClassName('price')
+  let quantities = document.getElementsByClassName('quantity')
+  let totals = document.getElementsByClassName('total')
+  
+  for (num in prices) {
+    let quantity = quantities[num].value
+    let price = prices[num].innerText.substring(1,prices[num].innerText.length)
+    totals[num].innerText = "$"+(price*quantity)
+}
 }
 
 function getTotalPrice() {
-
+   let accum = 0
+  for (num in prices) {
+    let price = parseFloat(prices[num].slice(prices[num].indexOf('$')+1,prices[num].length))
+    let quantity = parseFloat(quantities[num])
+    accum += (price*quantity)
+  }
+  totalPrice.value = "Total Price: $"+accum
 }
 
 function createQuantityInput(){
@@ -39,14 +54,18 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var calculatePriceButton = document.getElementById('btn-success');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  let prices = document.getElementsByClassName('price')
+  let quantities = document.getElementsByClassName('quantity')
+  let totals = document.getElementsByClassName('total')
+  let totalPrice = document.getElementById('total-price');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+}
