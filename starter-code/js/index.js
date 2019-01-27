26add53ae159f41cd36802459e164bba51477834
Matt Harrison <fittocode@gmail.com>
2019-01-27 22:15:32 +0100

    needs ability to add new items

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,56 @@
-function deleteItem(e){
-
+function deleteItem(e) {
+  // 1. target parent element of containers (products)
+  // 2. target the container
+  // 3. delete the container from the parent element
+  // removeChild(), parentElement()
+  var bigContainer = document.querySelector('#products');
+  var smallContainer = e.currentTarget.parentElement.parentElement;
+  bigContainer.removeChild(smallContainer);
+  getTotalPrice();
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {}
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
+function updatePriceByProduct(productPrice, index) {}
 
 function getTotalPrice() {
+  var priceOfItems = [...document.querySelectorAll('.price')];
+  var quantityOfItems = [...document.querySelectorAll('.input')];
+  var totalOfItems = document.querySelector('#productsTotal');
+  var value = [...document.querySelectorAll('.value')];
+  var productsTotal = 0;
 
+  for (let i = 0; i < priceOfItems.length; i++) {
+    var priceEntry = Number(priceOfItems[i].textContent.slice(1));
+    var quantityEntry = Number(quantityOfItems[i].value);
+    var total = priceEntry * quantityEntry;
+    value[i].textContent = '$' + total;
+    productsTotal += total;
+  }
+  totalOfItems.textContent = '$' + productsTotal;
 }
 
-function createQuantityInput(){
+function createQuantityInput() {}
 
-}
+function createDeleteButton() {}
 
-function createDeleteButton(){
+function createQuantityNode() {}
 
-}
+function createItemNode(dataType, itemData) {}
 
-function createQuantityNode(){
+function createNewItemRow(itemName, itemUnitPrice) {}
 
-}
+function createNewItem() {}
 
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
+window.onload = function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
