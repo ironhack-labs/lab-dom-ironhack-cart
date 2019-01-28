0bfb2b20a322cc0c4d105f31907c9a82c7180c40
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-28 14:41:55 -0500

    push before I try something

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,9 +11,12 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-var quantity = document.getElementById('quantity-input').value
+var gumQuantity = document.getElementById('quantity-input').value;
 var gumPrice = document.querySelector('#product-bubble-gum').getAttribute('data-price')
-var price = ( quantity * parseFloat(gumPrice));
+var doubleBubblePrice = document.querySelector('#product-double-gum').getAttribute('data-price')
+var doubleQuantity = document.getElementById('product-double-gum').value;
+var price = ( gumQuantity * parseFloat(gumPrice) + parseFloat(doubleBubblePrice)*doubleQuantity);
+
 
 var priceDiv = document.getElementById('purchase-details')
 priceDiv.innerHTML = price
@@ -44,7 +47,7 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
-var bubbleGum
+var itemsForSale = document.getElementsByClassName('product-name')
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
