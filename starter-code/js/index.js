065098db9e07d23157293fd1efcde86803edb1ad
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-28 15:32:37 -0500

    -delete item functions -modified html classes

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 function deleteItem(e){
-
+  e.currentTarget.parentNode.parentNode.innerHTML = ''
 }
 
 function getPriceByProduct(itemNode){
@@ -11,12 +11,11 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-var gumQuantity = document.getElementById('quantity-input').value;
+var bubbleQuantity = document.getElementById('bubble-quantity-input').value
 var gumPrice = document.querySelector('#product-bubble-gum').getAttribute('data-price')
-var doubleBubblePrice = document.querySelector('#product-double-gum').getAttribute('data-price')
-var doubleQuantity = document.getElementById('product-double-gum').value;
-var price = ( gumQuantity * parseFloat(gumPrice) + parseFloat(doubleBubblePrice)*doubleQuantity);
-
+var dubBubbleQuantity = document.getElementById('dub-bubble-quantity-input').value
+var dubleGumPrice = document.querySelector('#product-double-gum').getAttribute('data-price')
+var price = ( bubbleQuantity * parseFloat(gumPrice) + (dubBubbleQuantity * parseFloat(dubleGumPrice)))
 
 var priceDiv = document.getElementById('purchase-details')
 priceDiv.innerHTML = price
@@ -47,7 +46,7 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
-var itemsForSale = document.getElementsByClassName('product-name')
+
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
