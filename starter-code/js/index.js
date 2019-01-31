6b96a09f90dfccf95b3961f294ebfda71c3720e8
Monroe <mgetz10@gmail.com>
2019-01-31 11:41:51 -0500

    Monroe Getz FTWD Miami

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,6 @@
 function deleteItem(e){
+  console.log(e);
+  e.target.parentElement.parentElement.remove()
 
 }
 
@@ -6,11 +8,30 @@ function getPriceByProduct(itemNode){
 
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(){
+  let products = document.getElementsByClassName('product');
+  for (let i = 0; i < products.length; i++){
+    let price = products[i].children[1].children[0].innerText;
+    let quantity = products[i].children[2].children[1].value;
+    let totalValue = products[i].children[3].children[0];
+    totalValue.innerText = 0.00
+  
+    totalValue.innerText = price * quantity
+  }
+  
 }
 
 function getTotalPrice() {
+  updatePriceByProduct();
+  var prices = document.getElementsByClassName('total-item-price');
+  let sum = 0;
+  for (let i = 0; i < prices.length; i++){
+    let currentPrice = Number(prices[i].children[0].innerText);
+    sum += currentPrice;
+  }
+  let totalPrice = document.getElementById('total-price').children[0].children[0];
+  
+  totalPrice.innerText = sum;
 
 }
 
@@ -43,8 +64,9 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
+
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
