830eb795363d5992bfe32b58af606c95b230ed97
LiranGib <liran.gibor@gmail.com>
2019-02-01 12:52:41 -0500

    still working

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,4 +1,7 @@
-function deleteItem(e){
+function deleteItem(ele){
+  //console.log(e);
+ // window.liranBtn = e;
+  ele.target.parentElement.parentElement.remove();
 
 }
 
@@ -11,7 +14,14 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  let rows = document.getElementsByClassName("row");
+  console.log(rows);
+  for (let i=0; i < rows.length; i++){
+    console.log(rows[i]);
+    window.liranRow = rows[i];
+    let liranRow = rows[i].getElementsByClassName("inputData")[0].value 
+    console.log(liranRow)
+  }
 }
 
 function createQuantityInput(){
@@ -43,10 +53,10 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for(var i = 0; i < deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
