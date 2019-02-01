438c8ffcf552ef36ebb21ed93e18173d78887265
Ric Taglietti <rictaglietti@gmail.com>
2019-02-01 12:49:40 -0500

    ironhack cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,11 @@
 function deleteItem(e){
+  window.button = e;
 
+  e.target.parentNode.parentNode.remove();
 }
 
 function getPriceByProduct(itemNode){
-
+  // let productPrice = document.
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,7 +13,30 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  let theQuantity = document.getElementsByClassName("quantity-input");
+  // console.log(theQuantity);
 
+  let productPrice = document.getElementsByClassName("price");
+  // console.log(productPrice);
+  let totalPrice = document.getElementsByClassName("total-price");
+  // let totalPrice = theQuantity
+  let totalCartPrice = 0;
+
+  let cartPrice = document.getElementById("cart-total");
+
+  for(let i = 0; i < theQuantity.length; i++) {
+    let currentQuantity = parseInt(theQuantity[i].value);
+    console.log(currentQuantity);
+    let currentPrice = parseInt(productPrice[i].innerText.replace("$", ''));
+    console.log(currentPrice);
+    let currentTotal = currentQuantity * currentPrice
+    let currentTotalForItem = totalPrice[i]
+    currentTotalForItem.innerText = currentTotal;
+    totalCartPrice += currentTotal;
+
+  }
+  console.log(totalCartPrice);
+  cartPrice.innerText = cartPrice.innerText + " " + "$" + totalCartPrice;
 }
 
 function createQuantityInput(){
@@ -44,7 +69,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
