c8cea16f5c77ec04c937322b305e478b2a101512
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-03 23:34:27 +0100

    calculate prices

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,88 @@
-function deleteItem(e){
+// function deleteItem(e) {
 
-}
+// }
 
-function getPriceByProduct(itemNode){
+// function getPriceByProduct(itemNode) {
 
-}
+// }
 
-function updatePriceByProduct(productPrice, index){
+// function updatePriceByProduct(productPrice, index) {
 
-}
+// }
 
-function getTotalPrice() {
+// function getTotalPrice() {
 
-}
+// }
 
-function createQuantityInput(){
+// function createQuantityInput() {
 
-}
+// }
 
-function createDeleteButton(){
+// function createDeleteButton() {
 
-}
+// }
 
-function createQuantityNode(){
+// function createQuantityNode() {
 
-}
+// }
 
-function createItemNode(dataType, itemData){
+// function createItemNode(dataType, itemData) {
 
-}
+// }
 
-function createNewItemRow(itemName, itemUnitPrice){
+// function createNewItemRow(itemName, itemUnitPrice) {
 
-}
+// }
 
-function createNewItem(){
+// function createNewItem() {
 
-}
+// }
 
-window.onload = function(){
+var products = document.querySelectorAll('.product');
+
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", () => deleteItem(i))
   }
-};
+
+  const deleteItem = i => {
+    let prod = products[i];
+    document.getElementById('product-container').removeChild(prod);
+
+  };
+
+}
+
+
+
+const getTotalPrice = () => {
+
+  let totalAmount = 0;
+  for (let i = 0; i < products.length; i++) {
+    let productPrice = eval(products[i].querySelector('.product-unit-cost').innerText)
+    let productQuantity = products[i].querySelector('.amount-input').value;
+    let productTotal = productPrice * productQuantity;
+    products[i].querySelector('.product-total-cost').innerHTML = productTotal;
+    totalAmount += productTotal;
+  } return document.getElementById('total-price').innerText = totalAmount;
+}
+
+
+const deleteItem = i => {
+  let prod = products[i];
+  document.getElementById('product-container').removeChild(prod);
+
+}
+
+
+
+
+
+
