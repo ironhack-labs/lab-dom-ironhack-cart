6032a63a108c937dd4548ea37d930125f03f5077
Laura PH <lau.p.hern@hotmail.com>
2019-02-04 10:37:06 +0100

    iteration 2 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -68,25 +68,34 @@ function createCart() {
     var product = document.createElement("div");
     product.id = "product";
     allProducts.appendChild(product);
+    product.style.display = "flex";
+    product.style.flexDirection = "row";
+    product.style.justifyContent = "space-around";
+    //main title
+    var mainTitle = document.createElement("h1");
+    mainTitle.innerHTML = "Ironhack Merchandising Shop";
+    product.before(mainTitle);
     //name div
     var productName = document.createElement("div");
     product.appendChild(productName);
     var productNameSpan = document.createElement("span");
-    productNameSpan.innerHTML = "IronBubble-head"
+    productNameSpan.innerHTML = "IronBubble-head";
     productName.appendChild(productNameSpan);
     //cost div
     var productCost = document.createElement("div");
     product.appendChild(productCost);
     var productCostSpan = document.createElement("span");
+    productCostSpan.className = "productCostSpan";
     productCostSpan.innerHTML = "25.00$";
     productCost.appendChild(productCostSpan);
     //units div
     var productUnits = document.createElement("div");
     product.appendChild(productUnits);
     var productUnitsLabel = document.createElement("label");
-    productUnitsLabel.innerHTML = "QTY";
+    productUnitsLabel.innerHTML = "QTY ";
     var productUnitsInput = document.createElement("input");
     productUnitsInput.placeholder = "0";
+    productUnitsInput.className = "productUnitsInput";
     productUnits.appendChild(productUnitsLabel);
     productUnits.appendChild(productUnitsInput);
     //total product price
@@ -94,23 +103,63 @@ function createCart() {
     product.appendChild(productTotalPrice);
     var productTotalPriceSpan = document.createElement("span");
     productTotalPriceSpan.innerHTML = "0.00$";
+    productTotalPriceSpan.className = "productTotalPriceSpan";
     productTotalPrice.appendChild(productTotalPriceSpan);
     //delete button
     var deleteButton = document.createElement("button");
     deleteButton.innerHTML = "Delete";
     product.appendChild(deleteButton);
-    //main title
-    var mainTitle = document.createElement("h1");
-    mainTitle.innerHTML = "Ironhack Merchandising Shop";
-    product.before(mainTitle);
+    //SECOND PRODUCT
+        //product div
+        var product = document.createElement("div");
+        product.id = "product2";
+        allProducts.appendChild(product);
+        product.style.display = "flex";
+        product.style.flexDirection = "row";
+        product.style.justifyContent = "space-around";
+        //name div
+        var productName = document.createElement("div");
+        product.appendChild(productName);
+        var productNameSpan = document.createElement("span");
+        productNameSpan.innerHTML = "IronShirt";
+        productName.appendChild(productNameSpan);
+        //cost div
+        var productCost = document.createElement("div");
+        product.appendChild(productCost);
+        var productCostSpan = document.createElement("span");
+        productCostSpan.className = "productCostSpan";
+        productCostSpan.innerHTML = "15.00$";
+        productCost.appendChild(productCostSpan);
+        //units div
+        var productUnits = document.createElement("div");
+        product.appendChild(productUnits);
+        var productUnitsLabel = document.createElement("label");
+        productUnitsLabel.innerHTML = "QTY ";
+        var productUnitsInput = document.createElement("input");
+        productUnitsInput.placeholder = "0";
+        productUnitsInput.className = "productUnitsInput";
+        productUnits.appendChild(productUnitsLabel);
+        productUnits.appendChild(productUnitsInput);
+        //total product price
+        var productTotalPrice = document.createElement("div");
+        product.appendChild(productTotalPrice);
+        var productTotalPriceSpan = document.createElement("span");
+        productTotalPriceSpan.innerHTML = "0.00$";
+        productTotalPriceSpan.className = "productTotalPriceSpan";
+        productTotalPrice.appendChild(productTotalPriceSpan);
+        //delete button
+        var deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete";
+        product.appendChild(deleteButton);
     //calculate prices button
     var calculatePricesButton = document.createElement("button");
     calculatePricesButton.innerHTML = "Calculate Prices";
     cart.appendChild(calculatePricesButton);
     calculatePricesButton.addEventListener("click", function () {
-        var totalPrice = parseInt(productCostSpan.textContent, 10) * productUnitsInput.value;
-        productTotalPriceSpan.innerHTML = totalPrice + "$";
-        console.log(totalPrice);
+        for (i = 0; i < document.getElementsByClassName("productCostSpan").length; i++) {
+            var totalPrice = parseInt(document.getElementsByClassName("productCostSpan")[i].textContent, 10) * document.getElementsByClassName("productUnitsInput")[i].value;
+            document.getElementsByClassName("productTotalPriceSpan")[i].innerHTML = totalPrice + "$";
+        }
     });
 }
 
@@ -136,3 +185,70 @@ function createCart() {
 //         createRoomsFlexbox();
 //     });
 // };
+
+
+
+//BACKUP
+
+// function createCart() {
+//     var body = document.getElementsByTagName("body")[0];
+//     //cart div
+//     var cart = document.createElement("div");
+//     cart.id = "cart";
+//     body.appendChild(cart);
+//     //allProducts div
+//     var allProducts = document.createElement("div");
+//     allProducts.id = "all-products";
+//     cart.appendChild(allProducts);
+//     //product div
+//     var product = document.createElement("div");
+//     product.id = "product";
+//     allProducts.appendChild(product);
+//     product.style.display = "flex";
+//     product.style.flexDirection = "row";
+//     product.style.justifyContent = "space-around";
+//     //name div
+//     var productName = document.createElement("div");
+//     product.appendChild(productName);
+//     var productNameSpan = document.createElement("span");
+//     productNameSpan.innerHTML = "IronBubble-head"
+//     productName.appendChild(productNameSpan);
+//     //cost div
+//     var productCost = document.createElement("div");
+//     product.appendChild(productCost);
+//     var productCostSpan = document.createElement("span");
+//     productCostSpan.innerHTML = "25.00$";
+//     productCost.appendChild(productCostSpan);
+//     //units div
+//     var productUnits = document.createElement("div");
+//     product.appendChild(productUnits);
+//     var productUnitsLabel = document.createElement("label");
+//     productUnitsLabel.innerHTML = "QTY";
+//     var productUnitsInput = document.createElement("input");
+//     productUnitsInput.placeholder = "0";
+//     productUnits.appendChild(productUnitsLabel);
+//     productUnits.appendChild(productUnitsInput);
+//     //total product price
+//     var productTotalPrice = document.createElement("div");
+//     product.appendChild(productTotalPrice);
+//     var productTotalPriceSpan = document.createElement("span");
+//     productTotalPriceSpan.innerHTML = "0.00$";
+//     productTotalPrice.appendChild(productTotalPriceSpan);
+//     //delete button
+//     var deleteButton = document.createElement("button");
+//     deleteButton.innerHTML = "Delete";
+//     product.appendChild(deleteButton);
+//     //main title
+//     var mainTitle = document.createElement("h1");
+//     mainTitle.innerHTML = "Ironhack Merchandising Shop";
+//     product.before(mainTitle);
+//     //calculate prices button
+//     var calculatePricesButton = document.createElement("button");
+//     calculatePricesButton.innerHTML = "Calculate Prices";
+//     cart.appendChild(calculatePricesButton);
+//     calculatePricesButton.addEventListener("click", function () {
+//         var totalPrice = parseInt(productCostSpan.textContent, 10) * productUnitsInput.value;
+//         productTotalPriceSpan.innerHTML = totalPrice + "$";
+//         console.log(totalPrice);
+//     });
+// }
