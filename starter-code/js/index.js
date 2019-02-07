517972de0dc9aaf3ed5448b7487653eecd1a7ef9
NuriaTired <nuriatired@gmail.com>
2019-02-07 20:34:28 +0100

    errores

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,48 +1,61 @@
-function deleteItem(e){
+var productUnitPrice = document.getElementsByClassName("price-unit");
+var demo2 = document.getElementsByClassName("price-unit").innerText;
+
+console.log(typeof (productUnitPrice)); // Me dice que es un objeto
+console.log(productUnitPrice); //Me da el objeto definido
+console.log(productUnitPrice[0].innerText); //Intento entrar al valor de 25
+
+//¿Que hago mal?
+
+
+
+function deleteItem(e) {
+
+}
+//Estoy tratando de declarar 2 variables, para poder multiplicarlas luego,
+//pero intento consolearlas arriba, para ver que resultado me da cada variable,
+//pero no consigo nada, que hago mal?
+
+function getPriceByProduct(itemNode) {
+  var productUnitPrice = document.getElementsByClassName("price-unit");
+  var productUnits = document.getElementsByClassName("product-quantity");
+
 
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
-  var precioUnidad = document.getElementsByClassName("btn");
-  var cantidad = document.getElementById("cantidad");
-  var total=(precioUnidad * cantidad);
-  //console.log(total);
 
 }
 
-function createQuantityInput(){
+function createQuantityInput() {
 
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
 
 }
 
-function createQuantityNode(){
+function createQuantityNode() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItem(){
+function createNewItem() {
 
 }
 
-window.onload = function(){
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -50,7 +63,8 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
