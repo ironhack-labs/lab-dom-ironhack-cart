496e01b3c701efb65b30439158df6f5c248306be
Fedde <feddetilma@hotmail.com>
2019-02-08 14:05:02 +0100

    added create new item

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,6 @@
 function deleteItem(e) {
-  // console.log(e);
-  // console.log(e.currentTarget);
+  console.log(e);
+  console.log(e.currentTarget);
   // deleteTarget (2 higher: product-wrapper) + parent of deleteTarget (3 higher: wrapper)
   // e.currentTarget takes the current element of the click event listener (e)
   var deleteTarget = e.currentTarget.parentNode.parentNode;
@@ -57,20 +57,49 @@ function createNewItemRow(itemName, itemUnitPrice) {
 }
 
 function createNewItem() {
+  // retrieve values from input fields
+  var prodName = document.getElementById("input-product-name").value;
+  var prodCost = document.getElementById("input-product-cost").value;
 
+  if(prodName.length == 0) {return alert("No productname provided"); };
+  if(prodCost.length == 0) {return alert("No price provided"); }; 
+
+  // create product row div with class product-wrapper
+  var divEle = document.createElement("div");
+  divEle.setAttribute("class", "product-wrapper")
+
+  // creating input for div with the provided input (the innerHTML)
+  var input = "<div><span class='product-name'>" + prodName + "</span></div>";
+  input += "<div>€ <span class='product-cost'>" + prodCost + "</span></div>";
+  input += '<div><label class="quantity" for="quantity">QTY</label><input class="product-quantity" type="number" value="1" /></div>'
+  input += '<div>€ <span class="product-total">0</span></div>'
+  input += '<div><button class="btn btn-delete">Delete</button></div>'
+
+  // push new row of the product behind previous one, but above create button.
+  divEle.innerHTML = input;
+  var wrapperParent = document.getElementById('product-overview');
+  wrapperParent.appendChild(divEle);
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+  console.log(deleteButtons);
 }
+
+//------------------------------------------------//
+// -- Event listeners
+//------------------------------------------------//
 
 window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var deleteButtons = document.getElementsByClassName('btn-delete'); 
 
   calculatePriceButton.onclick = getTotalPrice;
-  // createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
-  console.log(deleteButtons);
+  // console.log(deleteButtons);
   for (var i = 0; i < deleteButtons.length; i++) {
-    // deleteButtons[i].onclick = deleteItem;
-    deleteButtons[i].addEventListener("click",deleteItem) // (differen, more clear, way of writing the same)
+    // deleteButtons[i].onclick = deleteItem; // option 1, but unclear it is a callback function
+    deleteButtons[i].addEventListener("click", deleteItem) // (different, more clear, way of writing the same)
   }
 };
+
+ 
