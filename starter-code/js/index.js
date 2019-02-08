d10957a678320e3f5d217aa97bf86f191765c737
David Gallego Gomez <davizs@hotmail.com>
2019-02-08 23:41:11 +0100

    Terminada la iteración 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -14,19 +14,16 @@ function getTotalPrice() {
   // DGG: Para obtener las cantidades
   const qt = document.getElementsByTagName('input');
   var arrQty = [].slice.call(qt).map( input => parseInt(input.value) );
-  console.log(arrQty);
 
   // DGG: Para obtener los precios
   const priceX = document.getElementsByClassName('product-price');
   var arrPrice = [].slice.call(priceX).map( price => parseFloat( price.textContent.substr(1) ) );
-  console.log(arrPrice);
 
   // DGG: Para actualizar precios
   var totalPriceArray = [];
   for (var i= 0; i < arrPrice.length; i++) {
     totalPriceArray[i] = arrPrice[i] * arrQty[i];
   }
-  console.log(totalPriceArray);
   
   const spanTotalParcial = document.getElementsByClassName('total-price');
   var arrTP = [].slice.call(spanTotalParcial);
@@ -34,25 +31,12 @@ function getTotalPrice() {
   for (var i= 0; i < totalPriceArray.length; i++) {
     spanTotalParcial[i].textContent = '$' + totalPriceArray[i].toFixed(2);
   }
-  console.log(totalPriceArray);
 
-  
+  var toPay = totalPriceArray.reduce((acc, value) => {
+    return acc + value;
+  })
 
-  
-  
-  
-  /*var qty = document.getElementsByTagName('input')[0];
-  var price = document.getElementsByClassName('product-price')[0].textContent;
-  var a = price.substr(1);
-  var totalPrice = a * qty.value;
-  var spanTotal = document.getElementsByClassName('total-price');
-  //spanTotal[0].textContent = '$' + totalPrice.toFixed(2);
-
-  var qty = document.getElementsByTagName('input')[1];
-  var price = document.getElementsByClassName('product-price')[1].textContent;
-  var a = price.substr(1);
-  var totalPrice = a * qty.value;
-  //spanTotal[1].textContent = '$' + totalPrice.toFixed(2);*/
+  var hola = document.querySelector('.total').textContent = '$' + toPay.toFixed(2);
 }
 
 function createQuantityInput(){
