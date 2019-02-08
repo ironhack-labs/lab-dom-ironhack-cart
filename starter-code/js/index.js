cb13d0156095e5d8bd83279cbc32f337f71c51ae
Javi L <jlopezro@gmail.com>
2019-02-09 00:58:03 +0100

    Iteration 2 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,6 +4,20 @@ function deleteItem(e){
 
 function getPriceByProduct(itemNode){
 
+  var prices = document.getElementsByClassName('price');
+  var price1 = prices[0].innerHTML;
+  var price2 = prices[1].innerHTML;
+  var priceNumber1 = price1.substring(1);
+  var priceNumber2 = price2.substring(1);
+  var input1 = document.getElementsByTagName('input')[0];
+  var input2 =document.getElementsByTagName('input')[2];  
+  var totalPrice1 = parseInt(priceNumber1) * input1.value;
+  var totalPrice2 = parseInt(priceNumber2) * input2.value;
+  var totalSum = document.getElementsByClassName('totalSumClass');
+  var sum1 = totalSum[0];
+  var sum2 = totalSum[1];
+  sum1.innerHTML = '$' + totalPrice1;
+  sum2.innerHTML = '$' + totalPrice2;
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,11 +25,15 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+
+/*  
   //var productPrice = getElementByValue
  // var idPrice = document.getElementById('price');
  // console.log(idPrice);
- /* var prices = price.getAttribute('id');
-  console.log(prices);*/
+ // var prices = price.getAttribute('id');
+ // console.log(prices);
+
+ 
   var prices = document.getElementById('price').innerHTML;
 // console.log(prices);
   var priceNumber = prices.substring(1);
@@ -26,10 +44,12 @@ function getTotalPrice() {
  // console.log(totalPrice);
   var totalSum = document.getElementById('totalSumId');
   totalSum.innerHTML = '$' + totalPrice;
+  
+*/
 
 }
 
-function createQuantityInput(){
+function createQuantityInput(){ 
 
 }
 
@@ -37,7 +57,7 @@ function createDeleteButton(){
 
 }
 
-function createQuantityNode(){
+function createQuantityNode(){ 
 
 }
 
@@ -45,23 +65,30 @@ function createItemNode(dataType, itemData){
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice){ 
 
 }
 
 function createNewItem(){
+  
 
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-//  var createItemButton = document.getElementById('new-item-create');
+
+  var getProductPrice = document.getElementById('calc-prices-button');
+
+//var calculatePriceButton = document.getElementById('calc-prices-button');
+ // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
+  getProductPrice.onclick = getPriceByProduct;
+
+//  calculatePriceButton.onclick = getTotalPrice;
  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
