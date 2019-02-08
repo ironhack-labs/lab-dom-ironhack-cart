2ac675e71bad8ded4b97ab46dca1d42c46c94515
David Gallego Gomez <davizs@hotmail.com>
2019-02-08 22:01:48 +0100

    Terminadas iteraciones 1 y 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -7,19 +7,52 @@ function getPriceByProduct(itemNode){
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  
 }
 
 function getTotalPrice() {
-  var qty = document.getElementsByTagName('input')[0];
+  // DGG: Para obtener las cantidades
+  const qt = document.getElementsByTagName('input');
+  var arrQty = [].slice.call(qt).map( input => parseInt(input.value) );
+  console.log(arrQty);
+
+  // DGG: Para obtener los precios
+  const priceX = document.getElementsByClassName('product-price');
+  var arrPrice = [].slice.call(priceX).map( price => parseFloat( price.textContent.substr(1) ) );
+  console.log(arrPrice);
+
+  // DGG: Para actualizar precios
+  var totalPriceArray = [];
+  for (var i= 0; i < arrPrice.length; i++) {
+    totalPriceArray[i] = arrPrice[i] * arrQty[i];
+  }
+  console.log(totalPriceArray);
+  
+  const spanTotalParcial = document.getElementsByClassName('total-price');
+  var arrTP = [].slice.call(spanTotalParcial);
+
+  for (var i= 0; i < totalPriceArray.length; i++) {
+    spanTotalParcial[i].textContent = '$' + totalPriceArray[i].toFixed(2);
+  }
+  console.log(totalPriceArray);
+
+  
+
+  
+  
+  
+  /*var qty = document.getElementsByTagName('input')[0];
   var price = document.getElementsByClassName('product-price')[0].textContent;
-  //var arrPrice = [].slice.call(price)
-  console.log(qty.value);
-  //console.log( arrPrice );
   var a = price.substr(1);
-  console.log( parseFloat(a) );
   var totalPrice = a * qty.value;
-  console.log(totalPrice);
+  var spanTotal = document.getElementsByClassName('total-price');
+  //spanTotal[0].textContent = '$' + totalPrice.toFixed(2);
+
+  var qty = document.getElementsByTagName('input')[1];
+  var price = document.getElementsByClassName('product-price')[1].textContent;
+  var a = price.substr(1);
+  var totalPrice = a * qty.value;
+  //spanTotal[1].textContent = '$' + totalPrice.toFixed(2);*/
 }
 
 function createQuantityInput(){
