cbc5e0c4e6d595a7f21211acc8b07e1173f95323
Guille LA <guillermolucena90@gmail.com>
2019-02-08 14:09:21 +0100

    trying functions

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,7 +3,20 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+  var productSpan = document.getElementsByClassName("product-price");
+  var arr = [].slice.call(productSpan)
+  var priceProduct = parseFloat(arr[0].innerHTML, 10);
+  var amount = document.getElementsByClassName("units");
+  var arr2 = [].slice.call(amount)
+  var amountTotal = parseInt(arr2[0].getAttribute("value"), 10);
+  var priceLabel = document.getElementsByClassName(itemNode);
+  var arr3 = [].slice.call(priceLabel);
+  var finalProductPrice = priceProduct * amountTotal;
+  finalProductPrice = finalProductPrice.toFixed(2);
+  console.log(finalProductPrice);
+  finalProductPrice = finalProductPrice.toString();
+  console.log(finalProductPrice);
+  priceLabel.innerHTML =  finalProductPrice;
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -37,7 +50,7 @@ function createNewItemRow(itemName, itemUnitPrice){
 function createNewItem(){
 
 }
-
+getPriceByProduct();
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
@@ -49,4 +62,5 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+
 };
