6051c20bf863d515f531420c41639842f96d4ee2
Merle Jansen <merlevanjansen@gmail.com>
2019-02-10 13:39:20 +0100

    updated version

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,110 +1,95 @@
 window.onload = function () {
-  //////IronBubbleProduct
- 
-  var calcPriceButton = document.getElementById("calculate-price-btn");
-  var basketValue = document.getElementById("total-sum").innerHTML;
+    //////Products
 
-  calcPriceButton.addEventListener("click", function () {
-    for (let i = 0; i < document.getElementsByClassName("product-wrapper").length; i++) {
-    var price = document.getElementsByClassName("price")[i].innerHTML;
-    var quantity = document.getElementsByTagName("input")[i].value;
-    
-    //setting a new total value:
-    var total = parseInt(price) * quantity;
-      document.getElementsByClassName("total")[i].innerHTML = total;
-      // getting basket total sum
-      for (let i = 0; i < document.getElementsByClassName("total").length; i++) {
-        let basketlValue = 0;
-        basketValue += total;
-        document.getElementById("total-sum").innerHTML = basketValue;
+    var calcPriceButton = document.getElementById("calculate-price-btn");
+    var total = document.getElementsByClassName("total")[0];
+    var basketValue = document.getElementById("total-sum");
+    var price = document.getElementsByClassName("price")[0];
+    var deleteButton = document.getElementsByClassName("btn-delete")[0];
+    var creatingNewBtn = document.getElementById("create");
+
+    calcPriceButton.onclick = function () {
+      for (let i = 0; i < document.getElementsByClassName("product-wrapper").length; i++) {
+        var quantity = document.getElementsByTagName("input")[0];
+
+        total[i].innerHTML = price[i].innerHTML * quantity[i].value;
+        // getting basket total sum
+        basketValue += total[i];
       }
     };
-  });
-
-  //delete 
-  var deleteButton = document.getElementsByClassName("btn-delete");
-
-  deleteButton.addEventListener("click", function () {
-      var wrapperDiv =  e.currentTarget.parentNode.parentNode;
-      var parentDiv = e.currentTarget.parentNode.parentNode.parentNode;
+    //delete 
+    deleteButton.onclick = function (event) {
+      var wrapperDiv = event.currentTarget.parentNode.parentNode;
+      var parentDiv = eevent.currentTarget.parentNode.parentNode.parentNode;
       parentDiv.removeChild(wrapperDiv);
-// should also update the BasketValue
-  });
+      // should also update the BasketValue?
+    };
 
-  //new
-  // Get the data from the inputs.
-var creatingNew = document.getElementById("create");
-creatingNew.addEventListener("click", function () {
-  var newProductName = document.getElementById("new-productname").value;
-  var newProductPrice = document.getElementById("new-productprice").value;
-  // Create a new product row with the data from the inputs
-  var newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "product-wrapper");
+    // new
+    creatingNewBtn.onclick = function () {
+      var newProductName = document.getElementById("new-productname").value;
+      var newProductPrice = document.getElementById("new-productprice").value;
+      // Create a new product row with the data from the inputs
+      var newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "product-wrapper");
 
-  //The structure of the new product should be the same
-  var input = "<div><span id='product-name'>" + newProductName + "</span></div>";
-  input += "<div>$<span class='price'>" + newProductPrice + "</span></div>";
-  input += '<div><label>QTY</label><input type="text" placeholder="Quantity">/></div>';
-  input += '<div><span class="total">0</span></div>';
-  input += '<div><button class="btn-delete btn">Delete</button></div>';
- 
+      //The structure of the new product should be the same
+      var add = "<div><span id='product-name'>" + newProductName + "</span></div>";
+      add += "<div>$<span class='price'>" + newProductPrice + "</span></div>";
+      add += '<div><label>QTY</label><input value="0"></div>';
+      add += '<div><span class="total">0</span></div>';
+      add += '<div><button class="btn-delete btn">Delete</button></div>';
+      newDiv.innerHTML = add;
 
-  newDiv.innerHTML = input;
-  var wrapParent = document.getElementById("cart-section");
-  wrapParent.appendChild(newDiv);
+      var wrapParent = document.getElementById("cart-section");
+      wrapParent.appendChild(newDiv);
+      };
 
- 
-  };
+    };
 
 
 
+    // function deleteItem(e){
 
+    // }
 
-}
+    // function getPriceByProduct(itemNode){
 
+    // }
 
+    // function updatePriceByProduct(productPrice, index){
 
-// function deleteItem(e){
+    // }
 
-// }
+    // function getTotalPrice() {
 
-// function getPriceByProduct(itemNode){
+    // }
 
-// }
+    // function createQuantityInput(){
 
-// function updatePriceByProduct(productPrice, index){
+    // }
 
-// }
+    // function createDeleteButton(){
 
-// function getTotalPrice() {
+    // }
 
-// }
+    // function createQuantityNode(){
 
-// function createQuantityInput(){
+    // }
 
-// }
+    // function createItemNode(dataType, itemData){
 
-// function createDeleteButton(){
+    // }
 
-// }
+    // function createNewItemRow(itemName, itemUnitPrice){
 
-// function createQuantityNode(){
+    // }
 
-// }
+    // function createNewItem(){
 
-// function createItemNode(dataType, itemData){
+    // }
 
-// }
-
-// function createNewItemRow(itemName, itemUnitPrice){
-
-// }
-
-// function createNewItem(){
-
-// }
-
-// window.onload = function(){
+    // window.onload = function(){
 
 
 
@@ -112,14 +97,14 @@ creatingNew.addEventListener("click", function () {
 
 
 
-// // var calculatePriceButton = document.getElementById('calc-prices-button');
-// //   var createItemButton = document.getElementById('new-item-create');
-// //   var deleteButtons = document.getElementsByClassName('btn-delete');
+    // // var calculatePriceButton = document.getElementById('calc-prices-button');
+    // //   var createItemButton = document.getElementById('new-item-create');
+    // //   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-// //   calculatePriceButton.onclick = getTotalPrice;
-// //   createItemButton.onclick = createNewItem;
+    // //   calculatePriceButton.onclick = getTotalPrice;
+    // //   createItemButton.onclick = createNewItem;
 
-// //   for(var i = 0; i<deleteButtons.length ; i++){
-// //     deleteButtons[i].onclick = deleteItem;
-// //   }
-// };
+    // //   for(var i = 0; i<deleteButtons.length ; i++){
+    // //     deleteButtons[i].onclick = deleteItem;
+    // //   }
+    // };
