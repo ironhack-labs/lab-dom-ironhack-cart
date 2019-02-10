5d575cadafc173a418f5906b94b3f58a16fb2c84
Dan Mihai Grigoriciuc <griganu06@gmail.com>
2019-02-10 17:35:34 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -10,9 +10,33 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
+ 
 function getTotalPrice() {
+  // Tomamos elementos de la seccion .product
+   var campoPrecioUnitario = document.getElementById("priceUnit").innerText;
+   var precioUnitNumber = parseFloat(campoPrecioUnitario.substring(1, campoPrecioUnitario.length));
+   //console.log(precioUnitNumber);
+   var campoCantidad = document.getElementById("quantity").value;
+  // Sumamos el precio del producto y su cantidad
+   var precioTotal = precioUnitNumber * campoCantidad; 
+   //console.log(precioTotal);
+  // Insertamos subtotal en html
+   document.getElementById("priceTotal").innerHTML = precioTotal;
+   //almacenamos subtotales en nuevo Array
+  newArr = [];
+  for (var i = 0; i <= document.getElementsByClassName("product").length; i++) {
+   newArr.push(precioTotal)
+  } 
+  // Aplicamos un reduce para conseguir el sumatorio
+  reducer = function(ac, elm){
+   return ac + elm
+  }
+  var totalPriceProducts = newArr.reduce(reducer)
+  // Insertamos sumatorio en html
+  document.getElementById("totalProdPric").innerHTML = precioTotal;
+  return
+  }
 
-}
 
 function createQuantityInput(){
 
