f71ee4e319ff1229e4c4face1587ea779cc8a964
Raluca-Andreea <danaralucaandreea@gmail.com>
2019-02-10 17:13:54 +0100

    first commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,41 +2,85 @@ function deleteItem(e){
 
 }
 
+
+
 function getPriceByProduct(itemNode){
 
+  var unitPriceString = document.querySelector(".unit-price").innerText
+
+  var unitPrice = unitPriceString.slice(1)
+
+  unitPrice = parseFloat(unitPrice).toFixed(2)
+
+  return unitPrice
+
 }
+
+
 
 function updatePriceByProduct(productPrice, index){
 
+  var updatedPriceString = document.querySelector("total-price")
+
+  var updatedPrice = updatedPriceString.slice(1)
+  
+  updatedPrice = parseFloat(updatedPrice).toFixed(2)
+
+  updatePrice.innerHTML = getTotalPrice()
+
 }
 
-function getTotalPrice() {
+
+
+function getTotalPrice(e) {
+
+  var totalPrice = getPriceByProduct() * createQuantityInput()
+
+  return totalPrice
 
 }
+
+
 
 function createQuantityInput(){
 
+  var quantityInput = document.getElementById("quantity").value
+
+  return quantityInput
+
 }
+
+
 
 function createDeleteButton(){
 
 }
 
+
+
 function createQuantityNode(){
 
 }
+
+
 
 function createItemNode(dataType, itemData){
 
 }
 
+
+
 function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
+
+
 function createNewItem(){
 
 }
+
+
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
