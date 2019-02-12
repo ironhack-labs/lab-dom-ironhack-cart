2e6399e055f2a02671fc853b760c3775b078575a
Dnox1 <david@nestrategia.com>
2019-02-12 18:41:31 +0100

    iteracion 1 hecha

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,34 @@
+
+//var productPrice = document.getElementById('prd-price').textContent;
+//var productPrice = productPriceBox.getElementsByTagName('p').childNode
+//console.log(productPriceBox);
+
+
+//var productqty = document.getElementById("qty").value;
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
-
+var productPrice = document.getElementById('prd-price').textContent;
+var productqty = document.getElementById("qty").value;
+document.getElementById('qty-price').innerHTML = productPrice * productqty;
+document.querySelector('#qty-price').innerHTML = productPrice * productqty;
+console.log(productPrice);
+console.log(productqty);
+console.log(productPrice * productqty);
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+  for(var i = 0; i<deleteButtons.length ; i++){
+    getPriceByProduct()
+  }
+  
 }
 
 function getTotalPrice() {
-
+updatePriceByProduct()
 }
 
 function createQuantityInput(){
@@ -23,6 +40,7 @@ function createDeleteButton(){
 }
 
 function createQuantityNode(){
+
 
 }
 
@@ -38,15 +56,25 @@ function createNewItem(){
 
 }
 
+
+
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+ // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+// Use at least 3 onclick events
+
+// Use at least one getElementById
+
+// Use at least one getElementsByTagName
+
+// Use at least one getElementsByClassName
