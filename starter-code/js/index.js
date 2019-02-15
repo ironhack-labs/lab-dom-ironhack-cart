73642c24d3b116fbf31d9f88f01f18c3f8acb4d5
Melissa Delgado Infante <melissadelgadoinfante@Melissas-MacBook-Pro.local>
2019-02-15 21:39:58 +0100

    first commit, not completed with js portion, issues with in line elements html

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -16,15 +16,51 @@ function getTotalPrice() {
 
 function createQuantityInput(){
 
+  var createDiv = document.createElement("div");
+  createDiv.setAttribute("class", "inputdiv");
+
+    //create label
+  var createLabel = document.createElement("label");
+  createLabel.setAttribute("for", "quantity");
+  createLabel.setAttribute("class", "quantity");
+  var labelText = document.createTextNode("QTY");
+  createLabel.appendChild(labelText);
+    //create input
+  var createInput = document.createElement("input");
+  createInput.setAttribute("type", "number");
+  createInput.setAttribute("min", "0");
+  createInput.setAttribute("name", "number-units");
+  createInput.setAttribute("class", "units");
+  createInput.setAttribute("value", "0");
+    //append them to div
+  createDiv.appendChild(createLabel);
+  createDiv.appendChild(createInput);
+    //append the div to document
+    var col = document.getElementsByClassName("product");
+    var arr = [].slice.call(col);
+    arr[col.length-1].appendChild(createDiv);
 }
 
 function createDeleteButton(){
 
+  var createDiv = document.createElement("div");
+    //create delete button
+  var createButton = document.createElement("button");
+  createButton.setAttribute("class", "btn btn-delete");
+  var buttonText = document.createTextNode("Delete");
+  createButton.appendChild(buttonText);
+    //append them to div
+  createDiv.appendChild(createButton);
+    //append the div to document
+  var col = document.getElementsByClassName("product");
+  var arr = [].slice.call(col);
+  arr[col.length-1].appendChild(createDiv);
 }
+
 
 function createQuantityNode(){
 
-}
+
 
 function createItemNode(dataType, itemData){
 
