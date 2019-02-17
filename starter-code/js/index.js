fb5862b725b452806eda9df77d598169127a20f4
Eva Francés <evafrances@MacBook-Pro-de-Eva.local>
2019-02-17 17:20:25 +0100

    seguimos

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,67 +1,69 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-  var unitPrice = itemNode.getElementsByClassName('dollars').innerText.replace('$','');
-  //return unitPrice;
-  console.log(unitPrice);
-}
-getPriceByProduct();
-
-function updatePriceByProduct(productPrice, index){
-  var totalPrice = index.querySelector('.total');
-  var quantity = index.getElementsByTagName('input').value;
-  var productTotal = (quantity > 0 ? quantity : 0) * productPrice;
-  totalPrice.innerText = "$" + productTotal.toFixed(2);
-  return productoTotal;
-}
-
-function getTotalPrice() {
-  var items = document.getElementsByClassName('item');
-  var totalAmount = document.querySelector('.total-amount');
-  var totalPrice = 0;
-  items.forEach(element => {
-  totalPrice += updatePriceByProduct(getPriceByProduct(element), element);
-  });
-  totalAmount.innerText = "$" + totalPrice.toFixed(2);
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-  for(var i=0; i<deleteButtons.length; i++){
-		(function(indice){
-			deleteButtons[indice].onclick = function(evento){
-        evento.preventDefault();
-        
-			}
-		})(i);
-	}
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
 window.onload = function(){
-  var calculatePriceButton = document.getElementsByClassName('btn-success')[0];
-  var createItemButton = document.getElementById('new-item-create');
+
+  function Product(nombre, precio){
+    Product.nombre = nombre;
+    Product.precio = precio;
+  }
+  var productos = [];
+  var cantidadProductos = [];
+
+  
+crearProducto("IronBubble-head",25);
+crearProducto("IronShirt",15);
+crearProducto("IronShirt",10);
   
  
-};
+  function crearProducto(nombre, precio){
+    var producto = new Product(nombre, precio); 
+    productos.push(producto);
+}
+
+
+  var contenedorProductos = document.querySelector(".contenedor-productos");
+  var productosDOM = [];
+  var botonesBorrar = [];
+
+
+
+function pintarProducto(producto){
+    var item = document.createElement("div");
+    item.className = "item";
+
+    var newP = document.createElement("p");
+    newP.className = "newProduct";
+    newP.innerHTML = Product.nombre;
+    item.appendChild(newP);
+
+    var productPrice = document.createElement("p");
+    productPrice.className = "oneProductPrice";
+    productPrice.innerHTML = Product.precio;
+    item.appendChild(productPrice);
+
+    var divQty = document.createElement("div");
+    divQty.className = "qty";
+    var parrafoQty = document.createElement("p");
+    parrafoQty.innerHTML = "QTY";
+    divQty.appendChild(parrafoQty);
+    var input = document.createElement("input");
+    input.type = "number";
+    input.className = "unit";
+    divQty.appendChild(input);
+    item.appendChild(divQty);
+
+    var unitsPrice = document.createElement("p");
+    unitsPrice.className = "precioUnidades";
+    unitsPrice.innerHTML = "$0.00"
+    item.appendChild(unitsPrice);
+
+    var boton = document.createElement("a");
+    boton.href = "#";
+    boton.className = "btn-delete";
+    boton.innerHTML = "Delete";
+    botonesBorrar.push(boton);
+    item.appendChild(boton);
+
+    productosDOM.push(item);
+    contenedorProductos.appendChild(item);
+  }
+  pintarProducto();
+}
