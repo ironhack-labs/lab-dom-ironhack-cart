6223dcdf24d630bab4f8ed4e216139458482e20b
Victor Garcia <v.garcia.mestre@gmail.com>
2019-02-18 15:54:13 +0100

    Iteration number 2 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,16 +4,16 @@ function deleteItem(e){
 
 //Event listener over the button
 var button = document.getElementsByClassName("btn-success")[0];
-button.onclick = function(){
+button.onclick =function(){ 
+  for (var i=0;i<100;i++){
   //pick up the price, the amount of objets and the total amount of cost
-  var precio= document.getElementsByClassName("product-price")[0].innerHTML;
-  var cantidad= document.getElementsByClassName("quantity")[0].value;
+  var precio= document.getElementsByClassName("product-price")[i].innerHTML;
+  var cantidad= document.getElementsByClassName("quantity")[i].value;
   var total=parseFloat(precio.replace('$', ''))*cantidad;
  //Equal the final price to the pricexunits price
-  document.getElementsByClassName('total-price')[0].innerHTML=total;
-  
+  document.getElementsByClassName('total-price')[i].innerHTML=total;
 }
-
+}
 function createDeleteButton(){
 
 }
