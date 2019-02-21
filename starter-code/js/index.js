d23606fa656351ff8ab43b391d35bcc0eec46586
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-02-21 22:22:40 +0100

    Segunda Iteracion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,10 +1,31 @@
 function getPrice() {
-  var result = document.getElementById("productPrice").innerHTML;
-  var regex = /(\d+)/g;
-  var price = result.match(regex);
-  var qty = document.getElementById("quantity").value;
-  var realQty = Number(qty);
-  var totalPrice = price * realQty;
-  document.getElementById("finalPrice").innerHTML = '$'+totalPrice;
-  console.log(totalPrice); 
+  var arr = document.getElementsByClassName("aa");
+  for (i=0; i < arr.length; i++) {
+    var arr2 = document.getElementsByClassName("bb");
+    for (j=0; j < arr2.length; j++) {
+      if (i == j) {
+        var result = arr[i].innerHTML;
+        var regex = /(\d+)/g;
+        var price = result.match(regex);
+        var qty = arr2[j].value;
+        var realQty = Number(qty);
+        var totalPrice = price * realQty;
+        var arr3 = document.getElementsByClassName("cc");
+        for (k=0; k < arr3.length; k++) {
+          if (j==k){
+          document.getElementsByClassName("cc")[k].innerHTML = '$'+totalPrice;
+          console.log(totalPrice); 
+          }
+        }
+      }
+    }
   }
+}
+  
+window.onload = function(){	
+var x = document.getElementById("calc-prices-button");
+    x.addEventListener("click", getPrice,false);
+//var y = document.getElementById("calc-prices-button");
+//    y.addEventListener("click", getPrice,false);
+
+}
