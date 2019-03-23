538344070e278e2352347307ec7ebc1e0ce5dee6
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-03-23 17:11:35 +0100

    last commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,7 +3,12 @@
 // Use at least one getElementsByTagName
 // Use at least one getElementsByClassName
 
-function deleteItem(e) {}
+function deleteItem(e) {
+  let padre = document.getElementById("creator");
+  let hijo = e.currentTarget.parentNode.parentNode;
+
+  padre.removeChild(hijo);
+}
 
 function getPriceByProduct(itemNode) {}
 
@@ -12,13 +17,20 @@ function updatePriceByProduct(productPrice, index) {}
 function getTotalPrice() {
   var lines = document.getElementsByClassName("price").length;
   //console.log(lines)
+  var pProduct = 0;
 
   for (i = 0; i < lines; i++) {
-    var itemUnitPrice = +document.getElementsByClassName("price")[i].innerHTML
+    var itemUnitPrice = +document.getElementsByClassName("price")[i].innerHTML;
     var qtyItems = +document.getElementsByClassName("qty")[i].value;
     //var totalUnitPrice = +document.getElementsByClassName("total-price")[i].innerHTML;
-    document.getElementsByClassName("total-price")[i].innerHTML = itemUnitPrice * qtyItems;
+    document.getElementsByClassName("total-price")[i].innerHTML =
+      itemUnitPrice * qtyItems;
+    var xPrice = +document.getElementsByClassName("total-price")[i].innerHTML;
+    pProduct += xPrice;
   }
+  document.querySelector(".masterPrice").innerHTML = pProduct;
+
+  console.log(pProduct);
 }
 
 function createQuantityInput() {}
@@ -31,12 +43,28 @@ function createItemNode(dataType, itemData) {}
 
 function createNewItemRow(itemName, itemUnitPrice) {}
 
-function createNewItem() {}
+function createNewItem() {
+  var text = document.getElementById("add-product-name").value;
+  var qtyCreate = +document.getElementById("add-product-price").value;
+  var para = document.createElement("div");
+  para.setAttribute("class", "row")
+  para.innerHTML = `<div><span>${text}</span></div>
+  <div>€ <span class="price">${qtyCreate}</span></div>
+  <div><label>Qty  </label><input class="qty" id="qty" type="number" placeholder="0"></div>
+  <div>€ <span class="total-price">0</span> </div>
+  <div><button class="btn btn-delete">
+      Delete
+    </button></div>`;
+  document.getElementById("creator").appendChild(para);
+
+}
+
 
 window.onload = function() {
   var calculatePriceButton = document.getElementById("calc-prices-button");
-  var createItemButton = document.getElementById("new-item-create");
+  var createItemButton = document.getElementById("add-product-btn");
   var deleteButtons = document.getElementsByClassName("btn-delete");
+
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
@@ -45,3 +73,12 @@ window.onload = function() {
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+window.onchange = function (params) {
+  var deleteButtons = document.getElementsByClassName("btn-delete");
+
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = deleteItem;
+  }
+  
+}
