bfc210ff17296f24ab15954b8bf40a5d8d363874
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-29 14:46:36 +0100

    add product done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,26 +1,60 @@
 var pressCalculate = document.getElementById('calc-prices-button');
 pressCalculate.onclick = getTotalPrice;
 
+var createButton = document.querySelector('.new');
+console.log(createButton);
+createButton.onclick = newProduct;
+
+var productsList = document.getElementById('product');
+
+function newProduct() {
+	console.log('ey you have a new product');
+	let newName = document.getElementById('new-name').value;
+	console.log(newName + ' nuevo nombre');
+	let newPrice = document.getElementById('new-price').value;
+	console.log(newPrice + ' nuevo precio');
+	let parent = document.getElementById('product');
+	console.log(parent + ' ey parent');
+
+	var addingArticle = document.createElement('article');
+	addingArticle.className = 'products grid';
+	addingArticle.innerHTML = `
+  <div><span>${newName}</span></div>
+  <div>$<span class="product-price">${newPrice}</span></div>
+  <div class="quantity">
+    <label>QTY</label>
+    <input
+      class="input"
+      type="number"
+      autocomplete="off"
+      placeholder="enter the amount..."
+      min="1"
+    />
+  </div>
+  <div>$<span class="total-price">0.00</span></div>
+  <div><button class="btn btn-delete">Delete</button></div>
+`;
+
+	parent.appendChild(addingArticle);
+}
+
 var deleteButtons = document.getElementsByClassName('btn btn-delete');
-console.log(deleteButtons);
 for (var i = 0; i < deleteButtons.length; i++) {
 	deleteButtons[i].addEventListener(
 		'click',
 		function(e) {
-			e.currentTarget.parentNode.parentNode.remove();
+			e.target.parentNode.parentNode.remove();
 		},
 		false
 	);
 }
 
 function getTotalPrice() {
-	console.log('clickeddddddd calculate total price');
 	var prices = document.querySelectorAll('.total-price');
 	var lengthPrices = prices.length;
 	var totalPrice = 0;
 	for (let i = 0; i < lengthPrices; i++) {
 		totalPrice = (createQuantityInput(i) * getPriceByProduct(i)).toFixed(2);
-		console.log('precio ' + prices[i].innerHTML);
 		prices[i].innerHTML = totalPrice;
 	}
 }
@@ -29,14 +63,12 @@ function getPriceByProduct(index) {
 	//itemNode as argumen???
 	var productPrices = document.querySelectorAll('.product-price');
 	var singleProductPrice = Number(productPrices[index].innerHTML);
-	console.log(singleProductPrice, 'single price');
 	return Number(singleProductPrice);
 }
 
 function createQuantityInput(index) {
 	var qantityInputs = document.querySelectorAll('.input');
 	var quantity = Number(qantityInputs[index].value);
-	console.log(quantity, 'number of items');
 	return Number(quantity);
 }
 //window.onload = function () {
