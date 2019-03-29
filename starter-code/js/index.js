9a84cc571518a4f33631449b6e8bcb7e702f9a9d
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-29 12:56:28 +0100

    delete buttons work

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,19 +1,26 @@
 var pressCalculate = document.getElementById('calc-prices-button');
-console.log(pressCalculate);
 pressCalculate.onclick = getTotalPrice;
 
+var deleteButtons = document.getElementsByClassName('btn btn-delete');
+console.log(deleteButtons);
+for (var i = 0; i < deleteButtons.length; i++) {
+	deleteButtons[i].addEventListener(
+		'click',
+		function(e) {
+			e.currentTarget.parentNode.parentNode.remove();
+		},
+		false
+	);
+}
+
 function getTotalPrice() {
-	console.log('clickeddddddd');
-
+	console.log('clickeddddddd calculate total price');
 	var prices = document.querySelectorAll('.total-price');
-
 	var lengthPrices = prices.length;
 	var totalPrice = 0;
 	for (let i = 0; i < lengthPrices; i++) {
 		totalPrice = (createQuantityInput(i) * getPriceByProduct(i)).toFixed(2);
-
 		console.log('precio ' + prices[i].innerHTML);
-
 		prices[i].innerHTML = totalPrice;
 	}
 }
