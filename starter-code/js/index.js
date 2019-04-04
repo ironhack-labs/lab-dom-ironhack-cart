4d022bea556b60d691381c5fd9e136a87aa30a19
gustavvopenna <gustavo.pema@gmail.com>
2019-04-04 07:36:48 -0600

    Minor changes

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,12 +11,12 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  let totalNode = document.getElementsByClassName('total-price')[0].childNodes[1]
   let price = document.getElementsByClassName('unit-cost')[0].childNodes[1].innerHTML
   let priceToNumber = Number(price.slice(1))
   let quantity = document.getElementsByClassName('input-quantity')[0].value
   let quantityToNumber = Number(quantity)
   let totalPrice = priceToNumber * quantityToNumber
-  let totalNode = document.getElementsByClassName('total-price')[0].childNodes[1]
   totalNode.innerHTML = `$${totalPrice}.00`
 }
 
@@ -56,3 +56,27 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+
+
+
+// --------
+// let parentNode = document.getElementsByClassName('parent')
+//   let parentArr = [...parentNode]
+//   console.log(parentNode.childNodes[1])
+//   // for(let i = 0; i < parentArr.length; i++) {
+//   //   let totalNode = document.getElementsByClassName('parent')
+//   // }
+
+
+//   let totalNode = document.getElementsByClassName('total-price')
+//   let totalArr = [...totalNode]
+//   //console.log(totalArr)
+//   let price = document.getElementsByClassName('unit-cost')[0].childNodes[1].innerHTML
+//   //console.log(price)
+//   let priceToNumber = Number(price.slice(1))
+//   let quantity = document.getElementsByClassName('input-quantity')[0].value
+//   let quantityToNumber = Number(quantity)
+//   let totalPrice = priceToNumber * quantityToNumber
+//   totalNode.innerHTML = `$${totalPrice}.00`
