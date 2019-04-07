1df04af5e9c438e46ab3beb6fc0dc0a504bf9276
Katy Brown <thisis.katybrown@gmail.com>
2019-04-07 18:29:31 -0400

    initial

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,16 +3,52 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
+  let cost= document.getElementById('cost').innerHTML
+  let quantity= document.getElementById('quantity').innerHTML
+  let totalCost= document.getElementById('total cost').innerHTML
+  return (totalCost[i].innerHTML = `${(
+    Number(cost[i].innerHTML) * quantity[i].value
+  ).toFixed("2")}`);
+  }
+  
+  /*var total = 0;
+  var totalItems = 0;
+  for (i=0;i<totalItems.length;i++){
+    total = price[i].innrHTML;
+    theQuantity = quantity[i].innerHTML
+    total.split("$")
+    return Number(thePrice[1]) * Number(theQuantity)
+    document.getElementById(totalItems).innerHTML = '$' + total;
+  }*/
 
-}
+ 
+    /*var quantity = document.getElementById("quantity").value
+  var price = document.getElementById("calculate-price").InnerHTML;
+  console.log(quantity)
+  console.log(price)
+  document.getElementById("total").value = parseInt(price * quantity);
+  return price * quantity*/
+
+
+
 
 function updatePriceByProduct(productPrice, index){
-
+  let cost= document.getElementById('cost').innerHTML
+  let quantity= document.getElementById('quantity').innerHTML
+  let totalCost= document.getElementById('total cost').innerHTML
+  return (totalCost[i].innerHTML = `${(
+    Number(cost[i].innerHTML) * quantity[i].value
+  ).toFixed("2")}`);
 }
 
 function getTotalPrice() {
+  
 
 }
+//console.log(document.querySelector)
+
+
+//var calculatePriceButton = document.getElementById('btn-success');
 
 function createQuantityInput(){
 
