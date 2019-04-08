c0866c4d1680128f5d572b353d5a9acb538eddad
Ahui Vargas <Ahui@ahui.local>
2019-04-08 01:36:50 -0500

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,41 +1,79 @@
-function deleteItem(e){
+const container = document.querySelector('.container')
+const products = document.querySelector('.products')
+const calculatePriceButton = document.getElementById('calc-prices-button')
+const createItemButton = document.querySelector('.new-item-create')
+const totalPriceSpan = document.getElementById('total-price-span')
 
+
+function deleteItem(e){
+  const deleteItem = e => {
+    products.removeChild(e.currentTarget.parentNode.parentNode) 
+  } 
 }
 
 function getPriceByProduct(itemNode){
-
+  const inputQty = document.getElementsByClassName('label')
+  for(let i = 0; i < inputQty.length; i++) {
+    inputQty[i].onkeyup = updatePriceByProduct
+  }
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  const updatePriceByProduct = e => { 
+    e.currentTarget.parentNode.parentNode.querySelector('.quantity').innerHTML = '$' + Number(e.currentTarget.value * e.currentTarget.parentNode.parentNode.querySelector('.cost').innerHTML.split('$')[1]).toFixed(2)
+  }
 }
 
 function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
+  const getTotalPrice = () => {
+    let sumPrices = 0
+    const totalPrices = document.querySelectorAll('.quantity')
+    totalPrices.forEach(price => sumPrices += Number(price.innerHTML.split('$')[1]));
+    totalPriceSpan.innerHTML = `$${sumPrices.toFixed(2)}`
+  }
+} 
 
 function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
+  const createDeleteButton = () => {
+    const deleteButtons = document.getElementsByClassName('btn-delete')
+    for(let i = 0; i < deleteButtons.length ; i++) {
+      deleteButtons[i].onclick = deleteItem
+    }
 }
 
 function createNewItem(){
+  const createNewItem = () => {
 
+    const nameProduct = document.getElementById('name-product').value
+    const costProduct = document.getElementById('cost-product').value
+    if(nameProduct && costProduct) {
+      const divProduct = document.createElement('div')
+      divProduct.setAttribute('class', 'product')
+  
+      divProduct.innerHTML = `
+        <div>
+          <span>${nameProduct}</span>
+        </div>
+        <div>
+          <span class="cost">$${Number(costProduct).toFixed(2)}</span>
+        </div>
+        <div>
+          <label for="">QTY</label>
+          <input type="text" class="label" value="1">
+        </div>
+        <div>
+          <span class="quantity">$${Number(costProduct).toFixed(2)}</span>
+        </div>
+        <div>
+          <button class="btn btn-delete">Delete</button>
+        </div>
+      `
+    }
+      products.appendChild(divProduct)
+      createDeleteButton()
+      getPriceByProduct()
+    }
+  } 
 }
 
 window.onload = function(){
@@ -49,4 +87,9 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+}
+
+createDeleteButton()
+calculatePriceButton.onclick = getTotalPrice
+createItemButton.onclick = createNewItem
+getPriceByProduct()
