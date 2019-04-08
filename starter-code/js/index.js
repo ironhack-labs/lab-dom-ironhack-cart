65c737ab367bfb6d5ed17a5998fee3dfbb3e2dea
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-08 08:58:00 -0400

    FIrst Commit.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,7 @@
-function deleteItem(e){
 
+function deleteItem(e){
+  console.log(itemContainer);
+  itemContainer[0].innerHTML = "";
 }
 
 function getPriceByProduct(itemNode){
@@ -11,30 +13,28 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+  var itemContainer = document.getElementById("item-total");
+  var quantity =document.getElementsByClassName("quantity");
+  var itemPrice = document.getElementById("item-price");
+  var total = document.getElementById("total-price");
+  itemContainer.innerHTML =  '$' + Number(quantity[1].value) * Number(itemPrice.textContent);
+  total.textContent = itemContainer.innerHTML;
+  
 }
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
 function createNewItem(){
+  var div = document.createElement("div")
+  var newContainer = itemContainer[0].innerHTML;
+  div.setAttribute("class", "items-container");
+  div.innerHTML = newContainer;
+  document.body.insertBefore(div, document.querySelector(".new-items-container"));
+
+  //var itemContainer = document.getElementsByClassName("div1-container");
+  
+  // itemContainer[0].lastElementChild.innerHTML = newContainer;
+  // var div = document.createElement("div")
+
+  // console.log(itemContainer[0].lastElementChild.innerHTML);
+
 
 }
 
@@ -46,7 +46,18 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+    for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
-  }
-};
+    }
+  };
+
+
+
+
+
+
+  //My added JS
+  var itemContainer = document.getElementsByClassName("items-container");
+  var itemName = document.getElementById("item-name");
+  var itemPrice = document.getElementById("item-price");
+  var itemTotal = document.getElementById("item-total");
