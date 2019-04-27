aeb0b505a551834f058766f5da24e3792f5f169a
benjamingarciap <bengarc@gmail.com>
2019-04-27 13:26:13 +0200

    first commit- getTotalPrice function working with button

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,17 @@
+//Variables
+let unitPrice = document.getElementById("cost")
+let userInput = document.getElementById("user-input")
+userInput.value = 1;
+let calculatePriceButton = document.querySelector(".btn-success")
+let totalPrice = document.querySelector("#total-price") 
+// Button working
+//calculatePriceButton.onclick = () => {console.log("oeoe")}
+calculatePriceButton.onclick = () => {getTotalPrice()}
+
+
+
+
+
 function deleteItem(e){
 
 }
@@ -10,8 +24,12 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
 
+function getTotalPrice() {
+    let result = unitPrice.innerText * parseInt(userInput.value)
+    //console.log(result)
+    //return result
+    totalPrice.innerHTML = result;
 }
 
 function createQuantityInput(){
@@ -39,12 +57,13 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+  //var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  
+  //calculatePriceButton.onclick = getTotalPrice;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
