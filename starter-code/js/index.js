6be4feba9c4fcdaa1e43750ce96ceb3caf744cd1
benjamingarciap <bengarc@gmail.com>
2019-04-27 20:10:12 +0200

    sixth commit - everything working fine just a little tidying up the code

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,8 @@
 //===== Variables =====// 
+
 // product variables
 let productName = document.getElementsByClassName("product-name")
 let unitPrice = document.getElementsByClassName("cost")
-let userInput = document.getElementsByClassName("input")
 let totalPrice = document.getElementsByClassName("total-price")
 let resultPrice = document.getElementsByClassName("result-price")[0]
 // list variables
@@ -11,39 +11,39 @@ let productList = document.getElementsByTagName('ul')[0]
 // button variables
 let calculatePriceButton = document.getElementsByClassName("btn-success")[0]
 let deleteBtn = document.getElementsByClassName("btn-delete")
-
-
+let createBtn = document.getElementById("createBtn")
+// inputs
+let quantityInput = document.getElementsByClassName("QTYinput")
+let newItemName = document.getElementById("newProductName")
+let newItemPrice = document.getElementById("newProductPrice")
 
 ///===== Buttons =====// 
-calculatePriceButton.onclick = () => {
-      updatePriceToAllProducts()
-      updateResultPrice()
-}
 
-for(i = 0; i < deleteBtn.length; i++){
-  deleteBtn[i].addEventListener("click", function(e){
-    let target = e.currentTarget;
-    let parent1 = target.parentElement
-    let parent2 = parent1.parentElement
-    let parent3 = parent2.parentElement
-    let parent4 = parent3.parentElement
-    parent4.removeChild(parent3)
-    console.log(parent4)
-  })
-  console.log("click")
-}
+calculatePriceButton.addEventListener("mousedown", function(){
+    updatePriceToAllProducts()
+    updateResultPrice()
+})
+
+createBtn.addEventListener("mousedown", function(){
+  newItem()
+  deleteBtnSetup()
+  //totalPrice -= 
+})
+
+deleteBtnSetup()
 
 
 
 ///===== Functionality =====// 
+
 function getTotalPrice() {
-      let result = unitPrice.innerText * parseInt(userInput.value)
+      let result = unitPrice.innerText * parseInt(quantityInput.value)
       totalPrice.innerHTML = result;
 }
 
 function updatePriceToAllProducts(){
       for(i=0; i<products.length; i++){
-        let result = unitPrice[i].innerText * parseInt(userInput[i].value)
+        let result = unitPrice[i].innerText * parseInt(quantityInput[i].value)
         totalPrice[i].innerHTML = result;
       }
 } 
@@ -53,66 +53,65 @@ function updateResultPrice(){
       for(i=0; i<products.length; i++){
         result += parseInt(totalPrice[i].innerHTML) 
       }
-      console.log(result)
+      //console.log(result)
       resultPrice.innerHTML = result
 }
 
-
-
-
-
-
-function deleteItem(e){
-  var id = e.target.id;
-  console.log("delete an item: " + id)
-}
-
-function deleteProduct(){
-
-}
-
-function getPriceByProduct(itemNode){
- 
-}
-
-function updatePriceByProduct(productPrice, index){
-  
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){}
-  //var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  
-  //calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
-
- /* for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+function deleteBtnSetup(){
+  for(i = 0; i < deleteBtn.length; i++){
+    deleteBtn[i].addEventListener("mousedown", function(e){
+      let target = e.currentTarget;
+      let parent1 = target.parentElement
+      let parent2 = parent1.parentElement
+      let parent3 = parent2.parentElement
+      let parent4 = parent3.parentElement
+      parent4.removeChild(parent3)
+      //console.log(parent2)
+    })
+    //console.log("click")
   }
-};*/
+}
+
+function newItem(){
+  let name = newItemName.value
+  let price = newItemPrice.value
+  var li = document.createElement("li");
+  li.innerHTML = `<div class="row">
+  <div class="col-md-2">
+    <span class="product-name">${name}</span>
+  </div>
+  <div class="col-md-2">
+    <span class="cost">
+    ${price}
+    </span>
+  </div>
+  <div class="col-md-2">
+    <form>
+      <label for="name">QTY</label>
+      <input type="number" class="QTYinput" value="0">
+    </form>
+  </div>
+  <div class="col-md-2">
+    <span class="total-price">0.00</span>
+  </div>
+  <div class="col-md-2 delete">
+    <button class="btn-delete">Delete</button>
+  </div>`;
+  productList.appendChild(li);
+  newItemName.value = ""
+  newItemPrice.value = ""
+  deleteBtnSetup()
+}
+// input event listeners
+newItemName.addEventListener("keyup", function(event) { 
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      newItem()
+    }
+});
+newItemPrice.addEventListener("keyup", function(event) { 
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    newItem()
+  }
+});
