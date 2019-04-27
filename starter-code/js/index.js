ef73452bd05f9385c5c236de9ee8f4ee48fa22fc
letiescanciano <leticia.escanciano@gmail.com>
2019-04-27 11:02:25 +0200

    Iteration 2: Add another product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,14 @@
+/* const products = [{
+    name: 'ironbubble-head',
+    price: 25.00
+  },
+  {
+    name: 'iron shirt',
+    price: 15.00
+  },
+] */
+
+
 function deleteItem(e) {
 
 }
@@ -11,22 +22,25 @@ function updatePriceByProduct(productPrice, index) {
 }
 
 function getTotalPrice() {
-  /* Iteration 1 */
-  /* 1. Acceder al valor del input qty */
-  const qty = document.getElementById('qty').value
-  console.log(qty)
-  /* 2. Acceder al valor del precio por item */
-  const price = document.getElementById('price').innerText
-  console.log(price)
 
-  /* 3. Calcular el precio total */
+  /* 0. Construir el array de todos mis productos */
 
-  const result = (qty * price).toFixed(2);
-  console.log(result)
-  /* 4. Mostar por pantalla el precio total */
-  //document.getElementById('total-price').innerText = result
-  const totalPrice = document.getElementById('total-price')
-  totalPrice.innerText = result;
+  /* 0.1. Accedo a todas las cantidades y las incluyo en el array productsQtys */
+  const productsQtys = document.getElementsByClassName('quantity')
+  /* 0.2. Accedo a todas los precios y las incluyo en el array productsPrices */
+  const productsPrices = document.getElementsByClassName('price')
+  /* 0.3. Accedo a todas los precios totales y las incluyo en el array totalPrices */
+  const totalPrices = document.getElementsByClassName('total-price')
+
+  /* 1. Accedemos a los valores unitarios de cada producto */
+
+  for (let i = 0; i < productsQtys.length; i++) {
+    /* 1.1 Calculamos el totalPrice para cada producto */
+    const totalPrice = (productsPrices[i].innerText * productsQtys[i].value).toFixed(2);
+    /* 1.2 Actualizamos el totalPrice en el elemento html para que se muestre */
+    totalPrices[i].innerText = totalPrice;
+  }
+
 }
 
 function createQuantityInput() {
