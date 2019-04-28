14241e90625ffaf0779ef2ad809c7de8286da2c7
Marnelas <marnelasbenito@gmail.com>
2019-04-28 16:50:46 +0200

    advances pp

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -11,6 +11,13 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+const unit1 = document.getElementsByClassName("cost1Unit")
+const numberUnits = document.getElementsByClassName("numberUnits")
+const totalLabel = document.getElementsByClassName("totalValue")
+for (let i = 0; i < unit1.length; i++) {
+  totalLabel[i].innerHTML = unit1[i].innerHTML * numberUnits[i].valueAsNumber  
+  
+}
 
 }
 
@@ -35,7 +42,14 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-
+const newline = document.createElement("div")
+const producName = document.createElement("div")
+const unit1 = document.createElement("div")
+const numberUnits = document.createElement("div")
+const total = document.createElement("div")
+const remove = document.createElement("div")
+const currentLine = document.getElementsByClassName("flex-list")
+document.body.insertBefore(newline,currentLine)
 }
 
 window.onload = function(){
