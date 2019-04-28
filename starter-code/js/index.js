5c1352677e2601e0db98441505c9abaac2589c00
Marnelas <marnelasbenito@gmail.com>
2019-04-28 17:09:40 +0200

    added total calculator

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -18,8 +18,16 @@ for (let i = 0; i < unit1.length; i++) {
   totalLabel[i].innerHTML = unit1[i].innerHTML * numberUnits[i].valueAsNumber  
   
 }
+console.log({totalLabel})
+let prize = 0;
+for(let i = 0;i < totalLabel.length;i++){
+prize += parseInt(totalLabel[i].innerHTML)
+}
+const finalPrize = document.getElementById("finalPrize")
+finalPrize.innerHTML = prize
 
 }
+
 
 function createQuantityInput(){
 
@@ -42,23 +50,15 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-const newline = document.createElement("div")
-const producName = document.createElement("div")
-const unit1 = document.createElement("div")
-const numberUnits = document.createElement("div")
-const total = document.createElement("div")
-const remove = document.createElement("div")
-const currentLine = document.getElementsByClassName("flex-list")
-document.body.insertBefore(newline,currentLine)
 }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+ // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
