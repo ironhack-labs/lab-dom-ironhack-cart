463f27a4663f6c7063bc189e9a63b1639938132e
davidh <iq.davidh@gmail.com>
2019-05-09 21:25:42 -0500

    ya se cuenta el total sigue sacar los elementos parent

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,12 +1,12 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
@@ -14,41 +14,84 @@ function getTotalPrice() {
 
 }
 
-function createQuantityInput(){
+function createQuantityInput() {
 
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
 
 }
 
-function createQuantityNode(){
+function createQuantityNode() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItem(){
+function createNewItem() {
 
 }
 
-window.onload = function(){
-  // var calculatePriceButton = document.getElementById('calc-prices-button');
-  // var createItemButton = document.getElementById('new-item-create');
-  // var deleteButtons = document.getElementsByClassName('btn-delete');
-  //
-  // calculatePriceButton.onclick = getTotalPrice;
-  // createItemButton.onclick = createNewItem;
-  //
-  // for(var i = 0; i<deleteButtons.length ; i++){
-  //   deleteButtons[i].onclick = deleteItem;
-  // }
+window.onload = function () {
+    // var calculatePriceButton = document.getElementById('calc-prices-button');
+    // var createItemButton = document.getElementById('new-item-create');
+    // var deleteButtons = document.getElementsByClassName('btn-delete');
+    //
+    // calculatePriceButton.onclick = getTotalPrice;
+    // createItemButton.onclick = createNewItem;
+    //
+    // for(var i = 0; i<deleteButtons.length ; i++){
+    //   deleteButtons[i].onclick = deleteItem;
+    // }
+
+    var carrito = {
+        listaTxt: [],
+        numItems: null,
+        granTotal: null,
+        calcularTotal: () => {
+            let qTotal = 0;
+            let montoTotal = 0;
+
+            carrito.listaTxt
+                .forEach((item => {
+
+                    let q= parseInt(item.value);
+                    let pu=10; /* TODO sacar el valor*/
+                    let monto= q*pu;
+
+                    qTotal+=q;
+                    montoTotal+=monto;
+                })
+            );
+
+
+            console.log(`num items ${qTotal}`);
+            console.log(`monto total  ${montoTotal}`);
+        }
+    };
+
+
+    function onChangeTxt(event) {
+        // let valor=this.value;
+        // console.log(event);
+        carrito.calcularTotal();
+    };
+
+    /* pra guardar la lisa de inputs txt*/
+
+
+    for (let txt of document.getElementsByClassName('txtQ')) {
+        txt.onchange = onChangeTxt;
+        carrito.listaTxt.push(txt);
+    }
+
+
 };
 ;
 console.log('LOADED');
