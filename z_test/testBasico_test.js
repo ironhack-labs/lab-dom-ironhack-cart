044d2ef21dc6c4eb311ae0045de668aa5094cfed
David H <iq.davidh@gmail.com>
2019-05-10 22:22:49 -0500

    test de contenido

--- z_test/testBasico_test.js
+++ z_test/testBasico_test.js
@@ -1,18 +1,42 @@
+let config = require("../codecept.conf");
 
-let config= require("../codecept.conf");
-
-let urlSite=config.config.helpers.Puppeteer.url;
+let urlSite = config.config.helpers.Puppeteer.url;
 
 console.log(urlSite);
 
-Feature('Que el sitio tenga las imagenes indicadas');
+Feature('Que el sitio tenga los elementos esperados');
 
-Scenario('Probando titulo', (I) => {
+Scenario('titulo/header ok', (I) => {
     I.amOnPage(urlSite);
-    I.seeInTitle("Apple Pie");
+    I.seeInTitle("Ironhack cart");
+});
+
+Scenario('sin mensajes de error', (I) => {
+    I.amOnPage(urlSite);
     I.dontSee('Page Not Found');
     I.dontSee('Error code');
     I.dontSee('Call Stack');
-    I.see('Ingredients');
+});
+
+
+Scenario('elementos requeridos para formadd', (I) => {
+    I.amOnPage(urlSite);
+    let listaIDelements = ["txtAddProductoNombre", "txtAddProductoDesc","txtAddProductoQ","txtAddProductoPU","cmdAdd"];
+
+    listaIDelements.forEach(id => {
+        I.seeElementInDOM('#'+id);
+    });
+
+});
+
+
+Scenario('elementos requeridos en carrito', (I) => {
+    I.amOnPage(urlSite);
+    let listaIDelements = ["panItems","labCarritoMonto","labCarritoNumQ"];
+
+    listaIDelements.forEach(id => {
+        I.seeElementInDOM('#'+id);
+    });
+
 
 });
