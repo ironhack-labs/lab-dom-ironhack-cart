97f4d802359e9ded250fb8800c62cbfc2685792f
davidh <iq.davidh@gmail.com>
2019-05-11 16:51:56 -0500

    i

--- z_test/testItemCarrito_test.js
+++ z_test/testItemCarrito_test.js
@@ -9,11 +9,30 @@ Feature('Que el carrito funcione con los eventos de modificar cantidad ');
 
 Scenario('elementos estado inicial en carrito', (I) => {
     I.amOnPage(urlSite);
-     let listaIDelements = ["panItemssss"];
+    let listaIDelements = ["panItem"];
 
     listaIDelements.forEach(id => {
-        I.seeElementInDOM('#'+id);
+        I.seeElementInDOM('#' + id);
     });
 
+
+    /* verificar los datos iniciales*/
+
+    let pathRoot1 = '#panItems .itemCarrito:first-child ';
+
+    let listaElementosIniciales = [
+        {path: pathRoot1 + 'input.txtQ', valorEsperado: 1},
+        {path: pathRoot1 + 'first-child .nombre', valorEsperado: 'Iron Buble-head'}
+    ];
+
+
+    listaElementosIniciales.forEach(data => {
+        console.log(data);
+        I.seeInField(data.path, data.valorEsperado);
+    });
+    // .itemCarrito:first-child input.txtQ
+
+
+    //I.fillField('user[email]', 'miles@davis.com');
 
 });
