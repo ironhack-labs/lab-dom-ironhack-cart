09e62f3b3c1e0fd0366f59542e6fd080c9845995
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-05-11 01:18:54 -0500

    1era iteracion

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -10,12 +10,44 @@ function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
+// function getTotalPrice() {
+  
+//   let unitCost = createQuantityInput();
+//   let unitQty = document.getElementById('product__qty-input').value;
+//   let totalPrice = (unitCost * unitQty).toFixed(2);
 
+//   document.getElementById('product__price').innerText = `$${totalPrice}`;
+
+//   return totalPrice;
+// }
+// 
+// function createQuantityInput(){
+//   let unitPriceLabel = document.getElementById('product__cost').innerText;
+//   let unitPriceStringArr = unitPriceLabel.split('');
+//   let unitPriceNumArr = unitPriceStringArr.filter(e => e != '$');
+//   let unitPrice = parseFloat(unitPriceNumArr.join(''));
+  
+//   return unitPrice;
+// }
+
+function getTotalPrice() {
+  
+  let unitCost = createQuantityInput();
+  let unitQty = document.getElementById('product__qty-input').value;
+  let totalPrice = (unitCost * unitQty).toFixed(2);
+
+  document.getElementById('product__price').innerText = `$${totalPrice}`;
+
+  return totalPrice;
 }
 
 function createQuantityInput(){
-
+  let unitPriceLabel = document.getElementById('product__cost').innerText;
+  let unitPriceStringArr = unitPriceLabel.split('');
+  let unitPriceNumArr = unitPriceStringArr.filter(e => e != '$');
+  let unitPrice = parseFloat(unitPriceNumArr.join(''));
+  
+  return unitPrice;
 }
 
 function createDeleteButton(){
@@ -44,7 +76,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
