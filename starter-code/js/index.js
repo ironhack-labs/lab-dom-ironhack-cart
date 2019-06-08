b517b7632ecb62bef603e8b6eefc7eabcd2cd851
Sonia Meller <mellersonia@gmail.com>
2019-06-08 20:12:26 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,16 +2,29 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
+let $amountOfUnits= document.querySelector("input").value;
+let $unitPrice= document.querySelector('.unit-price').innerHTML;
+let $inputButton = document.querySelector("input")
+let $productTotal = document.querySelector(".product-price").innerHTML
+$inputButton.onclick = function () {
+  // console.log("The quantity of items has changed")
+  getPriceByProduct()
+}
 
+//CHECK COMMENTS!
+function getPriceByProduct(/*itemNode*/){
+  let value= (document.querySelector("input").value)/*Using the variable $amountOfUnits doesnt work*/*$unitPrice;
+  // console.log("The button was clicked and the value is", value)
+  document.querySelector(".product-price").innerHTML = value
 }
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
-
+/*Can't declare variables 🤯  and cant make the math*/
+function getTotalPrice() { 
+  Number(document.querySelector(".total").innerHTML)+=Number(document.querySelector(".product-price").innerHTML);
 }
 
 function createQuantityInput(){
@@ -43,10 +56,10 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // calculatePriceButton.onclick = getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
