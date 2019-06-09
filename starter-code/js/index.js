606c66c4b8094c7fd8f6153187084dc98bdaaf09
Gabriel <gabelmo92@hotmail.com>
2019-06-09 17:45:26 +0200

    add fancy css

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,21 +1,5 @@
-// SELECTORS
-
-// let cost = document.querySelectorAll(".cost")
-// let qty= document.querySelectorAll(".qty-in")
-// let totalPrice = document.querySelectorAll(".total")
-// let calBtn = document.querySelector(".btn-calc")
-
-//let btn = document.querySelectorAll(".btn-delete")
-// let item = document.querySelector(".products")
-// let items = document.querySelector(".product")
-
-// let newPrice = document.querySelector(".newPrice")
-// let newItem = document.querySelector(".newItem")
-
-// Operations
-
 function updateUnitPrice() {
-  let cost = document.querySelectorAll('.cost');
+  let cost = document.querySelectorAll(".cost");
   let qty = document.querySelectorAll(".qty-in");
   let totalPrice = document.querySelectorAll(".total");
   for (let i = 0; i < cost.length; i++) {
@@ -37,82 +21,68 @@ function deleteItem() {
   let buum = event.currentTarget.parentNode.parentNode.remove();
 }
 
+function createQuantityInput() {
+  let quantityContainer = createItemNode("div", "");
+  quantityContainer.setAttribute("class", "quantity-container col");
 
-function createQuantityInput(){
-  let quantityContainer = createItemNode('div','');
-  quantityContainer.setAttribute('class', 'quantity-container col')
-  
-  let quantLabel = createItemNode('label', 'QTY');
-  quantLabel.setAttribute('for', 'qty-in');
-  quantLabel.setAttribute('class', 'col quantity');
-   
-  let quantInput = createItemNode('input','');
-  quantInput.setAttribute('type', 'number');
-  quantInput.setAttribute('class', 'col qty-in');
-    quantInput.setAttribute('min', '0');
-    quantInput.setAttribute('class', 'col qty-in');
-  quantInput.setAttribute('placeholder', 'Quantity');
-  quantInput.setAttribute('onblur', 'updateUnitPrice()');
+  let quantLabel = createItemNode("label", "QTY");
+  quantLabel.setAttribute("for", "qty-in");
+  quantLabel.setAttribute("class", "col quantity");
 
+  let quantInput = createItemNode("input", "");
+  quantInput.setAttribute("type", "number");
+  quantInput.setAttribute("class", "col qty-in");
+  quantInput.setAttribute("min", "0");
+  quantInput.setAttribute("class", "col qty-in");
+  quantInput.setAttribute("placeholder", "Quantity");
+  quantInput.setAttribute("onblur", "updateUnitPrice()");
 
-   quantityContainer.appendChild(quantLabel);
+  quantityContainer.appendChild(quantLabel);
   quantityContainer.appendChild(quantInput);
 
-  
-   return quantityContainer;
- }	
+  return quantityContainer;
+}
 
+function createDeleteButton() {
+  let btnContainer = createItemNode("div", "");
+  btnContainer.setAttribute("class", "col");
 
- function createDeleteButton(){	
-  let btnContainer = createItemNode('div','');
-  btnContainer.setAttribute('class', 'col')
+  let deleteBtn = createItemNode("button", "Delete");
+  deleteBtn.setAttribute("class", "btn btn-delete");
+  deleteBtn.setAttribute("onclick", "deleteItem()");
 
-  let deleteBtn = createItemNode('button', 'Delete');
-  deleteBtn.setAttribute('class', 'btn btn-delete');
-  deleteBtn.setAttribute('onclick', 'deleteItem()');
-  
-   btnContainer.appendChild(deleteBtn);
+  btnContainer.appendChild(deleteBtn);
 
-   return btnContainer;
+  return btnContainer;
+}
 
- }	
+function createTotal() {
+  let quantityNode = createItemNode("div", "");
+  quantityNode.setAttribute("class", "col");
 
+  let quantSpan = createItemNode("span", "0");
+  quantSpan.setAttribute("class", "total");
 
- function createTotal(){	
-   let quantityNode = createItemNode('div','');
-   quantityNode.setAttribute('class', 'col')
+  let quantCoin = createItemNode("span", " €");
 
-  
-  let quantSpan = createItemNode('span', '0');
-  quantSpan.setAttribute('class', 'total');
-
-  let quantCoin = createItemNode('span', ' €');
-
-   
   quantityNode.appendChild(quantSpan);
   quantityNode.appendChild(quantCoin);
 
-   return quantityNode;
+  return quantityNode;
+}
 
-
-
- }	
-
-
- function createItemNode(dataType, itemData){	
+function createItemNode(dataType, itemData) {
   let elem = document.createElement(dataType);
   elem.innerHTML = itemData;
 
+  return elem;
+}
 
-   return elem;
-}	
-
-
- function createNewItemRow(itemName, itemUnitPrice){
-  let deleteButtons = document.querySelectorAll('.btn-delete');
-  let parentContainer = document.querySelector('.products');
-  let rowElement = createItemNode('div','');
-  rowElement.setAttribute('class', 'product');
+function createNewItemRow(itemName, itemUnitPrice) {
+  let deleteButtons = document.querySelectorAll(".btn-delete");
+  let parentContainer = document.querySelector(".products");
+  let rowElement = createItemNode("div", "");
+  rowElement.setAttribute("class", "product");
 
   let quantity = createQuantityInput();
   let quantNode = createTotal();
@@ -124,77 +94,52 @@ function createQuantityInput(){
   rowElement.appendChild(quantNode);
   rowElement.appendChild(btnDelete);
 
+  parentContainer.insertBefore(rowElement, parentContainer.lastChild);
 
-   parentContainer.insertBefore(rowElement, parentContainer.lastChild);
-
-
-   for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
+}
 
+function createNewItem() {
+  let prodName = document.querySelector(".newName");
+  let prodPrice = document.querySelector(".newPrice");
 
- }	
+  let itemName = createItemNode("div", "");
+  itemName.setAttribute("class", "col");
 
+  let nameSpan = createItemNode("span", prodName.value);
+  nameSpan.setAttribute("class", "name");
 
- function createNewItem(){
-  let prodName = document.querySelector('.newName');
-  let prodPrice = document.querySelector('.newPrice');
+  let itemPrice = createItemNode("div", "");
+  itemPrice.setAttribute("class", "col");
 
-  let itemName = createItemNode('div','');
-  itemName.setAttribute('class', 'col')
-  
+  let priceSpan = createItemNode("span", prodPrice.value);
+  priceSpan.setAttribute("class", "cost");
 
-  let nameSpan = createItemNode('span', prodName.value);
-  nameSpan.setAttribute('class', 'name');
+  let coin = createItemNode("span", " €");
 
-  let itemPrice = createItemNode('div','');
-  itemPrice.setAttribute('class', 'col')
-
-  
-  let priceSpan = createItemNode('span', prodPrice.value);
-  priceSpan.setAttribute('class', 'cost');
-
-  
-
-  let coin = createItemNode('span', ' €');
-
-  
   itemName.appendChild(nameSpan);
 
   itemPrice.appendChild(priceSpan);
   itemPrice.appendChild(coin);
 
-   createNewItemRow(itemName, itemPrice);
+  createNewItemRow(itemName, itemPrice);
+}
 
+setInterval(randomRgb, 800);
 
- }
-
- setInterval(randomRgb, 800);
-
- function randomColor(num) {          
+function randomColor(num) {
   return Math.floor(Math.random() * num);
-}     
- function randomRgb() {      
-   let color =  "rgb("
-   + randomColor(255) + ","
-             + randomColor(255) + ","
-             + randomColor(255) + ")";
-             document.querySelector("#head").style.color = color;
-         }
- 
- 
-         
-
-
-// window.onload = function() {
-//   var calculatePriceButton = document.querySelector("btn-calc");
-//   var createItemButton = document.querySelector("create");
-//   var deleteButtons = document.querySelector("btn-delete");
-
-//   calculatePriceButton.onclick = getFinalPrice();
-//   createItemButton.onclick = createItem();
-
-//   for (var i = 0; i < deleteButtons.length; i++) {
-//     deleteButtons[i].onclick = deleteItem();
-//   }
-// };
+}
+function randomRgb() {
+  let color =
+    "rgb(" +
+    randomColor(255) +
+    "," +
+    randomColor(255) +
+    "," +
+    randomColor(255) +
+    ")";
+  document.querySelector("#head").style.color = color;
+}
