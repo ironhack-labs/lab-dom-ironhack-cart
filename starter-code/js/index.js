80d3456a2e8e1c38afcc60594678310c88c944e0
Sebastien Morelle <smorel14@gmail.com>
2019-06-10 10:49:10 +0100

    seb cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,13 +1,102 @@
+
+
+
+//let $totalItemPrice = document.querySelector('.totalItemPrice');
+//$totalItemPrice.innerText = "!"
+ //console.log( $TotalItemPrice );
+
+
 function deleteItem(e){
+}
+
+//function getPriceByProduct(itemNode){
+function getPriceByProduct(){
+//calc-prices-button totalItemPrice
+//"ItemPrice" "TotalItemPrice" input
+let $itemPriceString = document.querySelectorAll('.itemPrice');
+let $totalItemPrice = document.querySelectorAll('.totalItemPrice');
+let $input = document.querySelectorAll('.inputQty');
+console.log(document.querySelectorAll('.inputQty'))
+
+console.log(parseFloat($itemPriceString[0].innerText.replace('$','')));
+let $itemPrice = [];
+for(let i = 0; i < $itemPriceString.length; i++){
+  $itemPrice.push(parseFloat($itemPriceString[i].innerText.replace('$', '')));
+}
+console.log($itemPrice[0]);
+console.log($totalItemPrice[0].innerText);
+console.log($input[0].value);
+
+for(let i=0; i <$itemPrice.length; i++){
+  $totalItemPrice[i].innerText = '$' + $itemPrice[i] * $input[i].value;
+  }
+
+  console.log($totalItemPrice[0].innerText)
 
 }
 
-function getPriceByProduct(itemNode){
+//getPriceByProduct()
+let $button = document.querySelector('.calc-prices-button')
+$button.onclick = getPriceByProduct;
+
+
+
+function createNewItem(){
+
+
+  let $itemList = document.querySelector('.itemList');
+  let $newInputItem = document.querySelector('.inputItem')
+  let $newInputPrice = document.querySelector('.inputPrice')
+
+  console.log($newInputItem);
+  console.log($newInputPrice);
+
+  
+
+  let newItem = ` <div class = "btn">
+  <div class = "ItemName" > ${ $newInputItem.value }</div>
+  <div class = "itemPrice">${ $newInputPrice.value }</div>
+  <div class = "quantity" > QTY: <input type="text" class="inputQty"> </div>
+  <div><span class = "totalItemPrice"> $0.00 </span></div>
+  <div><button class="btn-delete"> Delete </button></div>
+</div>`
+
+  $itemList.innerHTML += newItem;
+  console.log('Add Prduct was clicked')
+ 
+
 
 }
+let $newButton = document.querySelector('.newButton')
+$newButton.onclick = createNewItem;
+
+
+
+
+/*
+$button.onclick = function (){
+  getPriceByProduct()
+}
+
+
+
+
+window.onload = function(){
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+
+  
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+};
+
 
 function updatePriceByProduct(productPrice, index){
-
 }
 
 function getTotalPrice() {
@@ -34,19 +123,5 @@ function createNewItemRow(itemName, itemUnitPrice){
 
 }
 
-function createNewItem(){
 
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+*/
