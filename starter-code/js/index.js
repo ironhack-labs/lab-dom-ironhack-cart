9cdaa4261adc1213acde33765370b6c2f13118c6
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-06-13 00:23:10 -0500

    finished the create button but it doesnt work correctly, will finish tomorrow

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -34,25 +34,50 @@ function getTotalPrice() {
   //total.innerText = `$${price * qty}.00`;
 }
 
-function createQuantityInput() {}
+function createQuantityInput() {
+  let qty = document.createElement("div");
+  qty.innerHTML = `<div class="quantity">
+  <label for="submit">QTY</label>
+  <input type="text" class="input" />
+</div>
+<div class="total">$0.00</div>`;
+  document.querySelector(".new").append(qty);
+}
 
-function createDeleteButton() {}
+function createDeleteButton() {
+  let cb = document.createElement("div");
+  cb.innerHTML = `     <div class="total">$0.00</div>     <div class="delete">
+  <button class="btn btn-delete">Delete</button>
+</div>`;
+  document.querySelector(".new").append(cb);
+}
 
 function createQuantityNode() {}
 
 function createItemNode(dataType, itemData) {}
 
-function createNewItemRow(itemName, itemUnitPrice) {}
+function createNewItemRow(itemName, itemUnitPrice) {
+  let div = document.createElement("div");
+  div.innerHTML = `<div class="info new"><div class="product"><span>${itemName}</span></div><div class="price">$${itemUnitPrice}</div> </div>`;
+  document.querySelector(".newItem").prepend(div);
+  createQuantityInput();
+  createDeleteButton();
+}
 
-function createNewItem() {}
+function createNewItem() {
+  pName = document.querySelector(".productName").value;
+  pPrice = document.querySelector(".productPrice").value;
+
+  createNewItemRow(pName, pPrice);
+}
 
 window.onload = function() {
   var calculatePriceButton = document.getElementById("calc-prices-button");
-  //var createItemButton = document.getElementById("new-item-create");
+  var createItemButton = document.getElementById("new-item-create");
   var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for (i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
