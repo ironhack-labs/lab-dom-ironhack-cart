ef94113059dc8ac974f43f6df182f55b5e3a7266
sofia <sofleos@gmail.com>
2019-06-13 09:38:38 -0500

    daily webdev ft Sofia De la Cueva

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,14 +1,36 @@
+const products = document.querySelectorAll('.product')
+const cost = document.querySelectorAll('.cost')
+const label = document.querySelector('.label')
+const total = document.querySelector('.total')
+const del = document.querySelector('.delete')
+const pricesBtn = document.querySelector('.btn-success')
+const deleteBtn = document.querySelector('.btn-delete')
+
+
+
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
+  let precio=  itemNode.innerText
+  precio = precio.substr(1);
+  precio = parseInt(precio)
+  return precio
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(i){
 
-}
+  let unit = getPriceByProduct(cost[i])
+  let cost = unit * parseInt(quantity.value)
+  let signo="$"
+  final=signo + cost + ".00"
+  total[i].innerText=final
+  
+  
+  }
+
 
 function getTotalPrice() {
 
