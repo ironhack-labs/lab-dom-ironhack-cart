2a8b9293a96921002374346090f5a68999987507
NeverSettels <settels.eric@gmail.com>
2019-06-12 21:50:16 -0500

    calculates price of one product for different quantities

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,21 +1,32 @@
+const qtyIntem = document.querySelectorAll("input")
+const product = document.querySelector(".product") 
+const name = document.querySelector('.pr-name')
+const priceStr = document.querySelectorAll(".pr-price")
+const deleteBtn = document.querySelector('.btn-delete')
+const clacBtn = document.querySelector('.btn-success')
+const itemTotal = document.querySelector(".item-total")
+
+
+
 function deleteItem(e){
 
 }
 
 function getPriceByProduct(itemNode){
-
+  const priceNum = parseInt(itemNode[0].textContent.substring(1))
+  return priceNum
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(){
+  itemTotal.textContent = `$${getTotalPrice()}.00`
 }
 
 function getTotalPrice() {
-
+ return getPriceByProduct(priceStr)*createQuantityInput()
 }
 
 function createQuantityInput(){
-
+  return qtyIntem[0].value
 }
 
 function createDeleteButton(){
@@ -38,13 +49,17 @@ function createNewItem(){
 
 }
 
+clacBtn.onclick = function(){
+  updatePriceByProduct()
+}
+
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+ //calculatePriceButton.onclick = getTotalPrice;
+ //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
