2198b88ef2d7c3fbd09c4364a183c9b8f19ecb41
chuca1 <diego.gm99@hotmail.com>
2019-06-13 00:15:04 -0500

    no pude hacer el paso 5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,46 @@
-function deleteItem(e){
+let todo = document.querySelectorAll(".around")
+let name = document.querySelector("#name")
+let precio = document.querySelectorAll(".precio")
+let quiantity = document.querySelectorAll(".input")
+let total = document.querySelectorAll(".total")
+let borrar = document.querySelectorAll(".delete")
+let seacabo = document.querySelector("#final")
+let nuevo = document.querySelector(".nuevo")
 
+function delete_row(e)
+{ 
+  e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode)
+  seacabo = document.querySelector("#final")
+  borrar = document.querySelectorAll(".delete")
+  total = document.querySelectorAll(".total")
+  quiantity = document.querySelectorAll(".input")
+  precio = document.querySelectorAll(".precio")
+  name = document.querySelector("#name")
+  todo = document.querySelectorAll(".around")
+  nuevo = document.querySelector(".nuevo")
+  ultimo()
 }
-
 function getPriceByProduct(itemNode){
-
+    let uprecio=  itemNode.innerText
+    uprecio = uprecio.substr(1);
+    upprecio = parseInt(uprecio)
+    return uprecio
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(i){
+      let unit = getPriceByProduct(precio[i])
+      let precios = unit * parseInt(quiantity[i].value)
+      let signo="$"
+      final=signo + precios + ".00"
+      total[i].innerText=final
 }
 
 function getTotalPrice() {
-
+  let final = 0
+  for  (let i = 0 ; i< precio.length;i++){
+    final += parseInt((total[i].innerText).substr(1))
+  }
+  return final
 }
 
 function createQuantityInput(){
@@ -30,23 +59,20 @@ function createItemNode(dataType, itemData){
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(){
+    let nuevo_nombre = querySelector("#nuevo_n")
+    let nuevo_precio = querySelector("#nuevo_p")
 
-}
+
+} 
 
 function createNewItem(){
 
 }
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+function  ultimo(){
+  for  (let i = 0 ; i< precio.length;i++){
+    updatePriceByProduct(i)
   }
-};
+  let terminado= getTotalPrice()
+  seacabo.innerText="Total Price: $"+terminado
+}
