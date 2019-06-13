69f6afc8bde8aa76c4d2525cf650a33ae2867a83
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-06-12 23:19:12 -0500

    Finished up to iteration 2, the only problem i have so far is positioning my styles correctly

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,57 @@
-function deleteItem(e){
+function deleteItem(e) {}
 
+function getPriceByProduct(itemNode) {
+  let qty = document.querySelectorAll("input[class=input]");
+  let priceArr = [];
+  for (i = 0; i < itemNode.length; i++) {
+    p = parseInt(itemNode[i].innerText.replace(/\$/g, ""));
+    q = parseInt(qty[i].value);
+    priceArr.push(p * q);
+    updatePriceByProduct(priceArr, i);
+  }
 }
 
-function getPriceByProduct(itemNode){
+function updatePriceByProduct(productPrice, index) {
+  let total = document.querySelectorAll(".total");
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
+  total[index].innerText = `$${productPrice[index]}.00`;
 }
 
 function getTotalPrice() {
+  let price = document.querySelectorAll(".price");
 
+  getPriceByProduct(price);
+
+  //total.innerText = `$${price * qty}.00`;
 }
 
-function createQuantityInput(){
+function createQuantityInput() {}
 
-}
+function createDeleteButton() {}
 
-function createDeleteButton(){
+function createQuantityNode() {}
 
-}
+function createItemNode(dataType, itemData) {}
 
-function createQuantityNode(){
+function createNewItemRow(itemName, itemUnitPrice) {}
 
-}
+function createNewItem() {}
 
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+window.onload = function() {
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  //var createItemButton = document.getElementById("new-item-create");
+  //var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for (var i = 0; i < deleteButtons.length; i++) {
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
+
+//thingsi will use for later
+/*
+.innerText.
+parseInt(
+*/
