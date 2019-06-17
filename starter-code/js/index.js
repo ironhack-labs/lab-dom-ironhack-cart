14b1a30f2fb2e41c1a9e531c77179fea6c516cb4
Ashley Arrue <ashleydarrue@gmail.com>
2019-06-17 15:05:04 -0400

    25% complete

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,17 +3,32 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+  
+    
 }
 
 function updatePriceByProduct(productPrice, index){
-
+ 
 }
 
-function getTotalPrice() {
+function getTotalPrice(e) {
+  let total = document.getElementsByClassName("totalPrice")
+  let qty =document.getElementsByClassName("quantity");
+  let productPrice = document.getElementsByClassName("productCost");
+  let x = productPrice[0].innerHTML.split('$');
+  let price = parseFloat(x[i]);
+  totalPrice = price * parseFloat(qty[i].value);
+  console.log(totalPrice);
+ total[0].innerHTML= totalPrice.toFixed(2);
+var i;
+ for (i = 0 ; i <= productPrice.length; i++);
+ for (i = 0 ; i <= price.length; i++);
+ for (i = 0 ; i <=qty.length; i++);
+
+
+
 
 }
-
 function createQuantityInput(){
 
 }
@@ -42,10 +57,9 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+ 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
+  //createItemButton.onclick = createNewItem;
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
