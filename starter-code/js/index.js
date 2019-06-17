abb1e78d69b0903c391ea3d9447f427bd9c95b12
yi-ting Chiang <tarynchiang@gmail.com>
2019-06-17 19:00:02 -0400

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,56 @@
-function deleteItem(e){
-
+function priceCalculation(){
+  
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  //price calculation
+  let calculate = document.getElementById('calc-prices-button');
+  
+  calculate.onclick = function(){
+    let prices = document.getElementsByClassName('items-price');
+    let priceArr = [];
+    let totalPrice = 0;
+    let q = document.getElementsByClassName('quantity');
+    for(let i=0; i<prices.length; i++){
+      priceArr.push(parseFloat(prices[i].innerHTML.slice(1)));
+      priceArr[i] = priceArr[i]*q[i].value;
+      let total = document.getElementsByClassName('perPrice');
+      total[i].innerHTML = `$${priceArr[i].toFixed(2)}`;
+      totalPrice += priceArr[i];
+    }
+    totalPrice = parseFloat(totalPrice).toFixed(2);
+    let finalResult = document.getElementById("totalPrice");
+    finalResult.innerHTML = `$${totalPrice}`;
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  }
+  
+  //delete items
+  let deletes = document.getElementsByClassName('btn-delete');
+  let container = document.getElementById('item-list');
+  for(let i=0;i<deletes.length;i++){
+    deletes[i].onclick = function(e){
+      let parent = e.currentTarget.parentNode.parentNode;
+      container.removeChild(parent);
+    }
+  }
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  //add items
+  let create = document.getElementById("create-item");
+  create.onclick = function(){
+    let name = document.getElementById("add-name").value;
+    let price = document.getElementById("add-price").value;
+    let doc = document.getElementsByClassName("div1")[0];
+    let clone = doc.cloneNode(true);
+    document.getElementById("item-list").appendChild(clone);
+    let lastnameArr = document.getElementsByClassName("name");
+    let lastname = lastnameArr[lastnameArr.length-1];
+    let lastpriceArr = document.getElementsByClassName("items-price");
+    let lastprice = lastpriceArr[lastpriceArr.length-1];
+    lastname.innerHTML = name;
+    lastprice.innerHTML = `$${price.toFixed(2)}`;
+    document.getElementById("add-name").value = "";
+    document.getElementById("add-price").value = "";
   }
 };
+
