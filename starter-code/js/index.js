d29d5e6aec6aeb3e6b5b6b8aa7d45dc0880abbca
Manuel Garcia <mgarc881@fiu.edu>
2019-06-18 17:45:18 -0400

    done af

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,55 @@
-function deleteItem(e){
 
-}
+let rows = document.getElementsByClassName("product");
 
-function getPriceByProduct(itemNode){
+let priceColumn = document.getElementsByClassName("price");
+let qtyColumn = document.getElementsByClassName("quantity");
+let totalColumn = document.getElementsByClassName("finalProduct");
+let deleteBtn = document.getElementsByClassName("btn-delete");
+let newName = document.getElementById('name');
+let newPrice = document.getElementById('newPrice');
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+function calculateTotal(){
+  let totalPrice = document.getElementById('totalPrice');
+  let total = 0;
+  for(let i = 0; i < rows.length; i++) {
+    const price = Number(priceColumn[i].innerText.split("$")[1]);
+    const qty = qtyColumn[i].value;
+    console.log(price);
+    total += price*qty;
   }
-};
+  console.log(totalPrice)
+  totalPrice.innerText = total;
+}
+function calculatePrices() {
+  for(let i = 0; i < rows.length; i++) {
+    const price = Number(priceColumn[i].innerText.split("$")[1]);
+    const qty = qtyColumn[i].value;
+    
+    const total = price * qty;
+    totalColumn[i].innerText = total; 
+  }
+  calculateTotal();
+}
+
+function deleteRow(e){
+  e.parentNode.parentNode.remove()
+}
+
+function addProduct(){
+  let productList = document.getElementById('productList');
+  var nodeName = document.createElement("div");
+ let nameInput = document.getElementById('name').value;
+ let priceInput = document.getElementById('newPrice').value;
+  nodeName.innerHTML = `
+  <div class="product">
+  <div><span>${nameInput}</span></div>
+  <div><span class="price">$${priceInput}</span></div>
+  <div><label>QTY</label><input class="quantity" type="number" min="0" value="0"></div>
+  <div class="finalProduct"><span></span></div>
+  <div><button onclick="deleteRow(this)" class="btn-delete">delete</button></div> 
+  </div>
+  `
+  productList.appendChild(nodeName);
+
+  
+}
