e3b61e412b0d871069e51af8267f5a0f78d96aa0
Nicolle Gonzalez <nicolle.ngonzalez@gmail.com>
2019-06-19 09:51:00 -0400

    Linked create list item button to click  listener

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,5 @@
+let theShopList = document.getElementById(`shop-list`);
+
 function deleteItem(e){
   let curButton = e.currentTarget;
   let targetProductDiv = curButton.parentNode.parentNode;
@@ -20,7 +22,6 @@ function getTotalPrice() {
   console.log(shopListDivs.length);
   let listTotal = 0.00;
   for(let i = 0; i < shopListDivs.length; i++){
-    let product = document.getElementsByClassName('product')[i].innerHTML;
     let price = document.getElementsByClassName('price')[i].innerHTML.replace(`$`,``);
     let quantity = document.querySelectorAll('.quantity > input')[i].value;
     let total = Number((price * quantity).toFixed(2));
@@ -32,27 +33,52 @@ function getTotalPrice() {
 
 
 function createQuantityInput(){
+  let newDiv = document.createElement(`div`);
+  //add class to div we created above- still havent done this
 
+  let inputStr = `<div><span class="quantity"> <label>QTY</label><input type="text"></span></div>`
+  // newDiv.innerHTML = inputStr; line 49(maybe?) below
+  // console.log(newDiv);
+  console.log(inputStr);
+
+  let newTotalProductCost= document.createElement(`div`);
+  let totalStr = `<div><span class="total-product-cost">$0.00</span></div>`
+  console.log(totalStr);
+  newTotalProductCost.innerHTML = totalStr;
+  console.log(newTotalProductCost);
+
+  console.log(inputStr + totalStr);
+  newDiv.innerHTML = inputStr + totalStr;
+
+
+  // document.getElementbyID thing then shop-list.apend(newDiv)
+  // shop-list.append(newDiv)
+
+
+  // console.log(inputStr + totalStr);
 }
 
 function createDeleteButton(){
-
+  let newDiv = document.createElement(`div`);
+  let domStr = `<div><button class="btn btn-delete">Delete</button></div>`;
 }
 
-function createQuantityNode(){
+// function createQuantityNode(){
 
-}
+// }
 
-function createItemNode(dataType, itemData){
+// function createItemNode(dataType, itemData){
 
-}
+// }
 
-function createNewItemRow(itemName, itemUnitPrice){
+// function createNewItemRow(itemName, itemUnitPrice){
 
-}
+// }
 
 function createNewItem(){
-
+  let newShopItem = document.createElement(`div`);
+  // newShopItem.innerHTML = 
+  theShopList.appendChild(newShopItem);
 }
 
 window.onload = function(){
@@ -66,4 +92,6 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+
+  createItemButton.onclick = createQuantityInput;
 };
