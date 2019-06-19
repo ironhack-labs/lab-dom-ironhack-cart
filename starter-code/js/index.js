2d84da498d70dd44de378222518b066b8bce636c
Gulfiya Isyangulova <gulfiyaisyangulova@Gulfiyas-MacBook-Pro.local>
2019-06-19 10:23:57 -0400

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,66 @@
-function deleteItem(e){
 
-}
 
-function getPriceByProduct(itemNode){
 
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
+window.onload = function () {
+  console.log("Calling window onload!");
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  //this is wherre you're going to add the delete click event
+  let deleteButtons = document.getElementsByClassName("btn-delete");
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
-};
+}
+
+function deleteItem(e) {
+  const container = e.currentTarget.parentNode.parentNode;
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+    
+  }
+}
+
+
+
+function getTotalPrice() {
+  let totalPrice = 0;
+  let productPrices = document.getElementsByClassName("cost-unit");
+
+  //  console.log(productPrice);
+  let quantity = document.getElementsByClassName("quantity");
+  console.log(quantity);
+  let eachPrice = document.getElementsByClassName("each-price");
+  let pricesArr = [...productPrices];
+  let quantArr = [...quantity];
+  let eachPriceArr = [...eachPrice];
+  for (var i = 0; i < pricesArr.length; i++) {
+
+    let currentTotal = pricesArr[i].innerHTML * quantArr[i].value;
+    eachPrice[i].innerHTML = currentTotal;
+
+    totalPrice += pricesArr[i].innerHTML * quantArr[i].value;
+    //  console.log(totalPrice);
+
+  }
+  let finalPrice = document.getElementById("total-price")
+  finalPrice.innerHTML = totalPrice;
+  console.log(totalPrice);
+
+
+
+}
+
+function createNewItems() {
+  console.log("createNewItems function running")
+  let newRow = document.createElement('div');
+  let theContainer = document.getElementsByTagName()
+}
+
+
+
+
+
+
+
