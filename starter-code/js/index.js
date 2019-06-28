407dbf193cbedbabae0772e3c5409be9c1604649
Mark Trevail <trevail.m@gmail.com>
2019-06-28 17:44:35 +0200

    changes

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,77 +1,106 @@
-// -----------------------------------------------------------------------------------------
-// Deleting items
-// -----------------------------------------------------------------------------------------
+// // -----------------------------------------------------------------------------------------
+// // Deleting items
+// // -----------------------------------------------------------------------------------------
 
-function deleteItem(e){
+// function deleteItem(e){
   
-}
+// }
 
-// -----------------------------------------------------------------------------------------
-// Calculating Product total price
-// -----------------------------------------------------------------------------------------
+// // -----------------------------------------------------------------------------------------
+// // Calculating Product total price
+// // -----------------------------------------------------------------------------------------
 
+// // Constructor for Product object
 
+// function Product(productName, productUnitPrice, productQTY) {
+//   this.productName = productName;
+//   this.productUnitPrice = parseFloat(productUnitPrice);
+//   this.productQTY = parseFloat(productQTY);
+  
+//   this.productTotalPrice = this.productQTY * this.productUnitPrice;
+// }
 
-  // Get 
-function getProductInfo() {
-}
+// // Function to generate Product info directory
 
-function getProductUnitPrice(itemNode){
+// function makeProductDirectory() {
 
-}
+//   let productDirectory = [];
+//   let productCollection = document.querySelectorAll(".product");
 
-function calcProductTotalPrice(productPrice, index){
+//   for(let i = 0; i < productCollection.length; i++) {
+//     let productName =  productCollection[i].getElementsByClassName("product-name")[0].innerHTML
+//     let productUnitPrice =  productCollection[i].getElementsByClassName("product-unit-price")[0].innerHTML
+//     let productQTY =  productCollection[i].getElementsByClassName("product-qty")[0].value
 
-}
+//     let prodObj = new Product(productName, productUnitPrice, productQTY);
 
-function updatePrices() {
+//     productDirectory.push(prodObj);
+//     console.log(prodObj);
+//   }
 
-}
-
-// -----------------------------------------------------------------------------------------
-// Creating new items
-// -----------------------------------------------------------------------------------------
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-// -----------------------------------------------------------------------------------------
-// Creating the event listeners
-// -----------------------------------------------------------------------------------------
-
-window.onload = function(){
-
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.addEventListener("click", updatePrices);
-  createItemButton.addEventListener("click", createNewItem);
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].addEventListener("click", deleteItem);
-  }
+// return productDirectory;
+// }
 
 
-};
+// function getProductUnitPrice(itemNode){
+
+// }
+
+// function calcProductTotalPrice(productPrice, index){
+
+// }
+
+// function updatePrices() {
+
+// }
+
+// // -----------------------------------------------------------------------------------------
+// // Creating new items
+// // -----------------------------------------------------------------------------------------
+
+// function createQuantityInput(){
+
+// }
+
+// function createDeleteButton(){
+
+// }
+
+// function createQuantityNode(){
+
+// }
+
+// function createItemNode(dataType, itemData){
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
+
+// // -----------------------------------------------------------------------------------------
+// // Creating the event listeners
+// // -----------------------------------------------------------------------------------------
+
+// window.onload = function(){
+
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+//   var deleteButtons = document.getElementsByClassName('btn-delete');
+
+//   productDirectory = makeProductDirectory();
+//   console.log(productDirectory);
+
+//   calculatePriceButton.addEventListener("click", updatePrices);
+//   createItemButton.addEventListener("click", createNewItem);
+
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].addEventListener("click", deleteItem);
+//   }
+
+
+// };
--- starter-code/js/test.js
+++ starter-code/js/test.js
@@ -1,18 +1,86 @@
-var productCollection = document.querySelectorAll(".product");
+// -----------------------------------------------------------------------------------------
+// Constructors
+// -----------------------------------------------------------------------------------------
 
-for(let i = 0; i < productCollection.length; i++) {
-  let productName =  productCollection[i].getElementsByClassName("product-name")[0].innerHTML
-  let productUnitPrice =  productCollection[i].getElementsByClassName("product-unit-price")[0].innerHTML
-  let productQTY =  productCollection[i].getElementsByClassName("product-qty")[0].value
+// Product
 
-  console.log(productName, productUnitPrice, productQTY);
+function Product(productName, productUnitPrice, productQTY) {
+  this.productName = productName;
+  this.productUnitPrice = parseFloat(productUnitPrice);
+  this.productQTY = parseFloat(productQTY);
+
+  this.productTotalPrice = 0; // Initial value
+
+  this.calcProductTotalPrice = function(productQTY, productUnitPrice) {
+    return productQTY * productUnitPrice;
+  };
+
 }
 
+// Shop Manager (a.k.a The Brain)
 
-// var firstProduct = document.querySelectorAll(".product")[0];
+function Shop(){
 
-// var firstProductPrice = firstProduct.getElementsByClassName("price-product")[0].innerHTML
-// console.log(firstProductPrice);
+  // Initial values
+  this.totalPrice = 0;
+  this.cartInfo = [];
 
-// //.getElementsByClassName("product-name")
+  // Function that creates the product directory
+  this.getCartInfo = function() {
+
+    let productCollection = document.querySelectorAll(".product");
+  
+    for(let i = 0; i < productCollection.length; i++) {
+      let productName =  productCollection[i].getElementsByClassName("product-name")[0].innerHTML
+      let productUnitPrice =  productCollection[i].getElementsByClassName("product-unit-price")[0].innerHTML
+      let productQty =  productCollection[i].getElementsByClassName("product-qty")[0].value
+  
+      let prodObj = new Product(productName, productUnitPrice, productQty);
+  
+      cartInfo.push(prodObj);
+    }
+    return cartInfo;
+  };
+};
+
+// Card - has current state?
+
+
+
+// // Shop
+
+// function Shop(productDirectory){
+
+//   this.productDirectory = productDirectory;
+
+
+//   // Function to calculate total price and update all prices
+//   this.totalPrice = function(productDirectory) {
+//     let totalPrice = 0;
+
+//     for(let i=0; i < productDirectory.length; i++) {
+//       totalPrice += productDirectory[i].productTotalPrice;            
+//     }
+
+//     return totalPrice;
+//   }
+
+
+
+};
+ var shop = new ShopManager()
+
+
+
+
+// -----------------------------------------------------------------------------------------
+// Run things (on events via listeners)
+// -----------------------------------------------------------------------------------------
+
+productDirectory = ShopManager.makeProductDirectory();
+totalPrice = ShoppingCart.calcTotalPrice(productDirectory);
+ShopManager.updatePrices(productDirectory, totalPrice);
+
+console.log(productDirectory);
+
 
