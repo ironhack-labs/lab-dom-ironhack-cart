ae106a1dde7e95273f706df70d066d7514683e53
Niklas Eicker <git@nikl.me>
2019-06-28 15:39:43 +0200

    finished add/delete/calculate functionality

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,122 @@
-function deleteItem(e){
+let cart;
+let calculatePriceButton;
+let createItemButton;
+let deleteButtons;
 
+let items = [];
+
+function ShopItem(name, price) {
+  this.name = name;
+  this.price = price;
+  this.count = 0;
+  this.lastInput;
+  this.getHtml = function () {
+    let item = document.createElement("div");
+    item.classList.add("item");
+    item.setAttribute("data-item-num", items.length)
+    item.innerHTML = `
+    <div class="name">
+      <span>${this.name}</span>
+    </div>
+    <div class="price">
+      <span>$${this.price.toFixed(2)}</span>
+    </div>
+    <div class="quantity">
+      <label for="item">QTY</label>
+      <input type="text" placeholder="0" id="quantity"${ this.lastInput ? ' value="' + this.lastInput + '"' : ""}>
+    </div>
+    <div class="totalPrice">
+      <span>${this.count ? "$" + (this.count * this.price).toFixed(2) : "$0.00" }</span>
+    </div>
+    <div class="delete">
+      <button class="btn btn-delete">Delete</button>
+    </div>`
+    return item;
+  }
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
+function addItem(item) {
+  cart.appendChild(item.getHtml());
+  items.push(item);
+  deleteButtons = document.getElementsByClassName('btn-delete');
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
+}
+
+function deleteItem(e) {
+  let item = e.target.parentNode.parentNode;
+  let index = parseInt(item.dataset["itemNum"]);
+  if ((index !=0 && !index) || index > items.length) {
+    throw new Error("Invalid item number: " + index);
+  }
+  items.forEach((elem, i) => {
+    if ( i < index ) return;
+    let element = cart.querySelector(`[data-item-num="${i}"]`);
+    if ( i === index ) {
+      cart.removeChild(element);
+      return;
+    }
+    element.dataset["itemNum"] = i - 1;
+  })
+  items.splice(index, 1);
+  updateTotal();
+}
+
+function updatePriceByProduct(item, element) {
+  let input = element.querySelector(".quantity input");
+  item.lastInput = input.value;
+  let parsed = parseInt(item.lastInput);
+  if (parsed) {
+    // ignore minus...
+    item.count = Math.abs(parsed);
+    input.classList.remove("mark-wrong");
+  } else {
+    item.count = 0;
+    if (item.lastInput) input.classList.add("mark-wrong");
+  }
+  element.querySelector(".totalPrice span").innerHTML = `${item.count ? "$" + (item.count * item.price).toFixed(2) : "$0.00" }`
+}
+
+function calculatePrices() {
+  let itemElements = document.getElementsByClassName("item");
+  items.forEach((item, i) => {
+    let element = itemElements[i];
+    if (!element) {
+      throw new Error("Cannot find the element for item Nr. " + i);
+    }
+    updatePriceByProduct(item, element);
+  })
+  updateTotal();
+  return false;
+}
+
+function updateTotal() {
+  let total = items.reduce((sum, item) => sum += item.count * item.price, 0)
+  document.querySelector(".shop .total h2 span").innerHTML = `$${total.toFixed(2)}`
+}
+
+function newItemFromInput(e) {
+  let newItemMask = document.getElementsByClassName("new-item-mask")[0];
+  let name = newItemMask.querySelector(".name input").value;
+  let priceInput = newItemMask.querySelector(".price input");
+  let price = parseFloat(priceInput.value);
+  if (price) {
+    // ignore minus...
+    addItem(new ShopItem(name, Math.abs(price)));
+    priceInput.classList.remove("mark-wrong");
+  } else {
+    if (priceInput.value) priceInput.classList.add("mark-wrong");
+  }
+}
+
+window.onload = function () {
+  calculatePriceButton = document.getElementById('calc-prices-button');
+  createItemButton = document.getElementById('new-item-create');
+  cart = document.getElementsByClassName('cart')[0];
+
+  calculatePriceButton.onclick = calculatePrices;
+  createItemButton.onclick = newItemFromInput;
+
+  addItem(new ShopItem("IronBubble-head", 25))
 };
