2b6d2baa09c0e6d9826f7fc9c16be070957ac51e
Alejandro Salas <asalas.wd@outlook.com>
2019-06-27 23:14:08 -0400

    done (some css to fix)

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,70 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+let button = document.getElementById('calculate-prices-btn');
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+button.onclick = function(){ 
+  let allPrices = document.getElementsByClassName("product-price");
+  let allQty = document.getElementsByClassName("quantity");
+  let priceSum = document.getElementsByClassName("price-times-qty");
+  let totalPriceText = document.getElementById('total-price');
+  let totalPrice = 0;
+  
+  for(let i = 0; i < allPrices.length; i++){
+    priceSum[i].innerHTML = (allPrices[i].innerHTML)*(allQty[i].value);
+    totalPrice += Number(priceSum[i].innerHTML);
   }
-};
+
+  totalPriceText.innerHTML = `\$${totalPrice}`;  
+}
+
+
+let deleteBtn = document.getElementsByClassName("btn-delete");
+
+for(let counter = 0; counter < deleteBtn.length; counter++){
+  deleteBtn[counter].onclick = function(e){
+    let parent = document.getElementById('all-items');  
+    let child = e.currentTarget.parentNode.parentNode;
+    parent.removeChild(child);
+  }
+}
+
+let createBtn = document.getElementById("create-item");
+
+createBtn.onclick = function(){
+  let list = document.getElementById("all-items");
+  let newDiv = document.createElement('div');
+  newDiv.setAttribute('class', 'product-line')
+  let newProductName = document.getElementById('new-product-name').value;
+  let newProductPrice = document.getElementById('new-product-price').value;
+  newDiv.innerHTML = `<div>
+    <span class="product-name">${newProductName}</span>
+  </div>
+  <div>
+    $<span class="product-price">${newProductPrice}</span>
+  </div>
+  <div>
+    <label>QTY</label>
+    <input class="quantity" type="text" name="qty" placeholder="0">      
+  </div>
+  <div>
+    $<span class="price-times-qty">0.00</span>
+  </div>
+  <div>
+    <button class="btn btn-delete">Delete</button>
+  </div>`
+  list.appendChild(newDiv);
+  document.getElementById('new-product-name').value = "";
+  document.getElementById('new-product-price').value = "";
+  let newDeleteBtn = deleteBtn[deleteBtn.length - 1];
+  newDeleteBtn.onclick = function(){
+    let parent = document.getElementById('all-items');  
+    let child = newDeleteBtn.parentNode.parentNode;
+    parent.removeChild(child);
+  }
+
+}
+
+
+
+
+} //end window.onload
