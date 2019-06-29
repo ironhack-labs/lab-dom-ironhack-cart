4a1b66d770655d39765b3cde6ea4b2e072095032
Dusty Garon <dustygaron@hotmail.com>
2019-06-29 11:59:17 -0400

    MIA PTWD - Dusty Garon

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,80 @@
-function deleteItem(e){
+window.onload = function () {
+  // ==========================
+  // CALCULATE PRICES
+  // ==========================
+  let calcPricesButton = document.querySelector('#calc-prices-button')
 
-}
+  calcPricesButton.onclick = function () {
+    let grandTotal = 0
+    let products = document.getElementsByClassName('product')
 
-function getPriceByProduct(itemNode){
+    for (let i = 0; i < products.length; i++) {
+      let unitCost = document.querySelector(`.product:nth-child(${i + 1}) .unit-cost > span`).innerHTML
+      let qty = Number(document.querySelector(`.product:nth-child(${i + 1}) .qty > input`).value)
 
-}
+      let lineItemTotal = unitCost * qty
+      // console.log(unitCost)
+      grandTotal += lineItemTotal
 
-function updatePriceByProduct(productPrice, index){
+      let lineItemPrice = document.querySelector(`.product:nth-child(${i + 1}) .total > span`)
+      lineItemPrice.innerHTML = lineItemTotal
+    }
 
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+    document.querySelector('#grand-total span').innerHTML = grandTotal
   }
-};
+
+  // ==========================
+  // DELETE ROW
+  // ==========================
+  let deleteButton = document.querySelectorAll('.btn-delete')
+
+  let deleteRow = (e) => {
+    e.currentTarget.parentElement.parentElement.remove()
+  }
+
+  for (let i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].onclick = deleteRow
+  }
+
+  // make the last delete button work
+  deleteButton[deleteButton.length - 1].onclick = deleteRow
+
+  // ==========================
+  // ADD NEW PRODUCT
+  // ==========================
+  let createNewProductButton = document.querySelector('#create-new-product-button')
+
+  createNewProductButton.onclick = function () {
+    let newProductName = document.querySelector('.new-product-name input').value
+    let newProductCost = document.querySelector('.new-product-cost input').value
+
+    let newProductRow = document.createElement('div')
+    newProductRow.classList.add('product')
+
+    newProductRow.innerHTML = `
+      <div class="product-name">
+        <span>${newProductName}</span>
+      </div>
+      <div class="unit-cost">
+        $<span>${newProductCost}</span>
+      </div>
+      <div class="qty">
+        <label for="qty">QTY</label>
+        <input type="text">
+      </div>
+      <div class="total">
+        $<span>0</span>
+      </div>
+      <div>
+        <button class="btn btn-delete">Delete</button>
+      </div>
+    `
+    let productRowsContainer = document.querySelector('#product-rows-container').appendChild(newProductRow)
+
+    document.querySelectorAll('.btn-delete')[document.querySelectorAll('.btn-delete').length - 1].onclick = deleteRow;
+
+    // clears inputs
+    document.querySelector('.new-product-name input').value = ''
+    document.querySelector('.new-product-cost input').value = ''
+  }
+} // END ONLOAD
