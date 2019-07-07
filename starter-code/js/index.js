ad069400c346c1706614a0bb94b0d2d4c5fa85b2
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-07 16:37:13 +0200

    IronCart1.0

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,12 +1,12 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
@@ -14,39 +14,108 @@ function getTotalPrice() {
 
 }
 
-function createQuantityInput(){
+function createQuantityInput() {
 
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
 
 }
 
-function createQuantityNode(){
+function createQuantityNode() {
 
 }
 
-function createItemNode(dataType, itemData){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItem(){
+function createNewItem() {
 
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+window.onload = function () {
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //Aqui tenemos precio del producto
+  //producto1
+  let price = document.getElementById("precio-producto")
+  priceProduct = price.innerText
+  priceProductClean = priceProduct.replace("$", "")
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  //producto2
+  let price2 = document.getElementById("precio-producto2")
+  priceProduct2 = price2.innerText
+  priceProductClean2 = priceProduct2.replace("$", "")
+
+  //Aqui tenemos el precio acumulado
+  //producto1
+  let cantidad = document.getElementById("precio-acumulado")
+  cantidadProduct = cantidad.innerText
+  cantidadProductClean = cantidadProduct.replace("$", "")
+
+  //producto2
+  let cantidad2 = document.getElementById("precio-acumulado2")
+  cantidadProduct2 = cantidad2.innerText
+  cantidadProductClean2 = cantidadProduct2.replace("$", "")
+
+  //Aqui tenemos la cantidad de precios
+  //producto1
+  let inputQty = document.getElementById("cantidad")
+
+  //producto2
+  let inputQty2 = document.getElementById("cantidad2")
+
+
+  //Boton calcular precio
+  let buttonPrices = document.getElementById("buttonPrices")
+
+
+  //Aqui esta el precio total
+
+  let totalPrice = document.getElementById("precio-total")
+  totalPriceProduct = totalPrice.innerText
+  totalPriceProductClean = totalPriceProduct.replace("$", "")
+
+  //Aqui esta el total de la suma
+
+
+  buttonPrices.onclick = () => {
+
+    cantidadProductClean = priceProductClean * inputQty.value
+    cantidadProductClean2 = priceProductClean2 * inputQty2.value
+
+    cantidad.innerHTML = `$${cantidadProductClean}.00`
+    cantidad2.innerHTML = `$${cantidadProductClean2}.00`
+
+    totalPrice.innerHTML = `$${cantidadProductClean + cantidadProductClean2}.00`
+
   }
-};
+  //ID botón delete 1
+  buttonDelete = document.getElementById("remove")
+
+  //ID botón delete 2
+  buttonDelete2 = document.getElementById("remove2")
+
+  //ID contenedor a borrar 1
+  let deleteButt = document.getElementById("delete-div")
+
+  //ID contenedor a borrar 2
+  let deleteButt2 = document.getElementById("delete-div2")
+
+  //Función borrar objeto 1
+  buttonDelete.onclick = () => {
+
+    deleteButt.remove()
+
+  }
+
+  //Función borrar objeto 2
+  buttonDelete2.onclick = () => {
+
+    deleteButt2.remove()
+  }
+}
