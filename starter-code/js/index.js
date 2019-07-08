4e7f0d48519e5d4f51404f45e93f364479050769
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-08 09:19:20 +0200

    cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,26 @@
-function deleteItem(e){
+function deleteItem(){
 
+  const productos = document.querySelectorAll('.producto') 
+  console.log(productos)
+  for (let i = 0; i < productos.length; i++) {
+    productos[i].style.color = 'red'
+    
 }
 
-function getPriceByProduct(itemNode){
+  
+}
 
+function getPriceByProduct(){
+  let priceUnit = document.getElementById("precioUnidad").innerHTML
+  let quantityUnit = document.getElementById("quantity").value
+  let totalPrice = priceUnit * quantityUnit
+  document.getElementById("numTotal").innerHTML =  totalPrice
+}
+function getPriceByProduct2(){
+  let priceUnitCamiseta = document.getElementById("precioUniCamiseta").innerHTML
+  let quantityUnitCamiseta = document.getElementById("quantityCamiseta").value
+  let totalPriceCamiseta = priceUnitCamiseta * quantityUnitCamiseta
+  document.getElementById("numTotalCamiseta").innerHTML =  totalPriceCamiseta
 }
 
 function updatePriceByProduct(productPrice, index){
@@ -11,6 +28,13 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+
+let sumatorio = parseInt(document.getElementById("numTotal").innerHTML)
+let sumatorio2 = parseInt(document.getElementById("numTotalCamiseta").innerHTML)
+console.log(sumatorio)
+let totalCompra = sumatorio + sumatorio2
+console.log (totalCompra)
+document.getElementById("totalCompraPrecio").innerHTML = totalCompra + "€"
 
 }
 
@@ -39,12 +63,13 @@ function createNewItem(){
 }
 
 window.onload = function(){
+ 
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  calculatePriceButton.onclick = getPriceByProduct;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
