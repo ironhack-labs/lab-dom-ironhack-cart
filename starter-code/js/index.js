3aa84162f138332e06526a088daa5e48f5576144
Julia Ramos <juliaramosguedes@gmail.com>
2019-07-12 21:36:12 -0300

    almost done. has a bug in qtt.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,53 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
+// let qttsArray = []
 
 function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  let addName = document.getElementById('add-name');
+  let addPrice = document.getElementById('add-price');
+  document.getElementById('products').innerHTML += `
+  <div class="product">
+  <div class="space"><span class="name">${addName.value}</span></div>
+  <div>$<span class="price">${Number(addPrice.value).toFixed(2)}</span></div>
+  <div><label>QTY <input type="number" value="1" class="quantity" onchange="priceUpdate()"></label></div>
+  <div>$<span class="total-product">${Number(addPrice).toFixed(2)}</span></div>
+  <div><button class="btn btn-delete">Delete</button></div>
+  </div>
+  `;
+  
+  addName.value = '';
+  addPrice.value = '';
+  priceUpdate();
+  
+  const deleteButtons = document.querySelectorAll('.btn-delete');
+  
+  for (let i = 0; i < deleteButtons.length ; i++) {
+    deleteButtons[i].onclick = () => {
+      deleteButtons[i].parentNode.parentNode.remove();
+      priceUpdate();
+    }
   }
-};
+  
+  // for (let i = 0; i < document.querySelectorAll('.quantity').length; i++) {
+  //   document.querySelectorAll('.quantity')[i].value = qttsArray[i]
+  //   }
+  
+  }
+  
+  function priceUpdate(){
+  let qtts = document.querySelectorAll('.quantity');
+  let prices = document.querySelectorAll('.price');
+  let totals = document.querySelectorAll('.total-product');
+  let totalPrice = 0;
+
+  for (let i = 0; i < qtts.length; i++) {
+    let currPrice = parseFloat(prices[i].innerText);
+    let currQtt = qtts[i].value;
+    // qttsArray.push(currQtt)
+    let currTotal = currPrice * currQtt;
+    totals[i].innerText = Number(currTotal).toFixed(2);
+    totalPrice += currTotal;
+
+
+  }
+
+  document.getElementById('total-price').innerText = Number(totalPrice).toFixed(2);
+}
