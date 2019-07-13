2cfc01f774f31bc7570df1dd655d3e6e76ca2016
Dayana Mick <dayanamick@gmail.com>
2019-07-13 17:48:57 +0200

    begin

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,73 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
-
+//iterate and for each of them calculate the price
 function getTotalPrice() {
+  let stringValue = document.querySelector(".cost-per-unit > span").innerText.substring(1, 3)
+  let cost = parseInt(stringValue)
+  let quantity = document.querySelector("#quantity").value
+  document.querySelector(".total-price").innerText = (`$` + `${cost * quantity}.00`)
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem() {
 
 }
 
-function createNewItem(){
-
+function copyAddProduct(name, unitPrice) {
+  let firstProduct = document.getElementsByClassName('product')[0];
+  let newProduct = firstProduct.cloneNode(true);
+  document.getElementsByClassName('title')[0].appendChild(newProduct);
+  newProduct.querySelector(".product-name > span").innerText = name
+  newProduct.querySelector(".cost-per-unit > span").innerText = `$` + unitPrice + `.00`
 }
 
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+window.onload = function () {
+  var calculatePriceButton = document.getElementById('calculate-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
+
+  copyAddProduct("IronShirt", "15")
+  // copyAddProduct()
+  // copyAddProduct()
+  // copyAddProduct()
+  // copyAddProduct()
+  // copyAddProduct()
+
 };
+
+//
