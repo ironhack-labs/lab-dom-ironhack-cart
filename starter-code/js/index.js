694245b521fbb520d0b009eb43992704d2b11f02
Christoph Butenschön <c.butenschoen@gmail.com>
2019-07-16 17:24:14 +0200

    added itemDelete(), which deletes items from the cart when the delete button of the corresponding product-row is clicked

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,7 @@
 function deleteItem(e) {
-
+  /*this looks more complicated than needed, but the exercise
+  clearly demands we use e.currentTargent, parentNode and removeChild() */
+  e.currentTarget.parentNode.parentNode.parentNode.removeChild(e.currentTarget.parentNode.parentNode)
 }
 
 
@@ -19,6 +21,7 @@ function getTotalPrice() {
     document.querySelectorAll('.wrapper')[i].querySelector('.priceTimesQuantity').innerHTML = updatePriceByProduct(i) + "$"
     totalPrice = totalPrice + updatePriceByProduct(i)
   }
+  //I purposefully use getElementById and getElementsByTagName here instead of just querySelectorAll in order to train using them, too
   document.getElementById('totalPriceDiv').getElementsByTagName('h2')[0].getElementsByTagName('span')[0].innerHTML = totalPrice + "$"
 }
 
@@ -49,13 +52,13 @@ function createNewItem() {
 window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   //   var createItemButton = document.getElementById('new-item-create');
-  //   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
   //   createItemButton.onclick = createNewItem;
 
-  //   for (var i = 0; i < deleteButtons.length; i++) {
-  //     deleteButtons[i].onclick = deleteItem;
-  //   } 
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = deleteItem;
+  }
 };
 
