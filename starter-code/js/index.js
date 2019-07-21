5e32e717fd34685ce4de3a94cf7173640f05a124
Chanty Herrmann <chanty@Chantys-MacBook-Air.local>
2019-07-21 09:42:55 +0200

    first try

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,44 +1,87 @@
-function deleteItem(e){
 
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
+//ITERATION 3: Calculating the total price for the product
+// var total = quantity + price
+// var quanitity = document.getElementsByClassName("quanitityInput")
 function getTotalPrice() {
-
+  //first get the item container, start with the first item [0]
+  var productItems = document.getElementsByClassName('product-items')[0]
+  //then get all the different elements selected:
+  var productRow = productItems.getElementsByClassName('product-row')
+  var total = 0
+  //loop over it 
+  for (var i = 0; i < productRow.length; i++) {
+    //define a var that takes the position of the chosen item
+    var newProductRow = productRow[i]
+    //get price and quantity of the row
+    var priceElement = newProductRow.getElementsByClassName("setPrice")[0]
+    var quantitiyElement = newProductRow.getElementsByClassName("quanitityInput")[0]
+    console.log(priceElement)
+    var price = parseFloat(priceElement.innerText.replace('$', ''))
+    var quanitity = quantitiyElement.value
+    //start at 0 ^line 49
+    console.log(price)
+    //we want a total + a new value if product is added
+    var total = (price * quanitity) + total
+  }
+  //we start at the priceTag of the products, we want just one [0] and it should set value to toal 
+  //?DO I have to choose [0] because the method says "getElements", plural
+  document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total
 }
 
-function createQuantityInput(){
+//Iteration4 Delete 
 
-}
+// Select the parent that contains all of the product wrapper divs
+// Use the function removeChild we saw in DOM Manipulators
+// 💡 Use e.currentTarget to access the "Delete" button that was just clicked and select the parent node of an HTML element with parentNode.
 
-function createDeleteButton(){
+// function deleteItem(e) {
+//define all items position that are deleted
+var newProductDeleted = newProductDeleted[i]
+// Select all the "Delete" buttons
+var allDeleted = newProductDeleted.getElementsByClassName("btn-delete")[0]
+// For each button, assign a click event that will:
+// Select the wrapper div that contains all the HTML for the product that should be deleted
 
-}
 
-function createQuantityNode(){
+// }
 
-}
+// function getPriceByProduct(itemNode) {
 
-function createItemNode(dataType, itemData){
+// }
 
-}
+// function updatePriceByProduct(productPrice, index) {
 
-function createNewItemRow(itemName, itemUnitPrice){
+// }
 
-}
 
-function createNewItem(){
 
-}
+// function createQuantityInput() {
 
-window.onload = function(){
+// }
+
+// function createDeleteButton() {
+
+// }
+
+// function createQuantityNode() {
+
+// }
+
+// function createItemNode(dataType, itemData) {
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice) {
+
+// }
+
+// function createNewItem() {
+
+// }
+
+window.onload = function () {
+  let element = document.getElementsByClassName("quanitityInput")[0]
+  element.value = "0";
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -46,7 +89,7 @@ window.onload = function(){
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
