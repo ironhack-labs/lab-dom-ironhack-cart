587a1d18a31b8ba4226bab89ab299dd6241efe3a
Chanty Herrmann <chanty@Chantys-MacBook-Air.local>
2019-07-24 17:33:03 +0200

    not working

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,95 +1,75 @@
-
-//ITERATION 3: Calculating the total price for the product
-// var total = quantity + price
-// var quanitity = document.getElementsByClassName("quanitityInput")
-function getTotalPrice() {
-  //first get the item container, start with the first item [0]
-  var productItems = document.getElementsByClassName('product-items')[0]
-  //then get all the different elements selected:
-  var productRow = productItems.getElementsByClassName('product-row')
-  var total = 0
-  //loop over it 
-  for (var i = 0; i < productRow.length; i++) {
-    //define a var that takes the position of the chosen item
-    var newProductRow = productRow[i]
-    //get price and quantity of the row
-    var priceElement = newProductRow.getElementsByClassName("setPrice")[0]
-    var quantitiyElement = newProductRow.getElementsByClassName("quanitityInput")[0]
-    console.log(priceElement)
-    var price = parseFloat(priceElement.innerText.replace('$', ''))
-    var quanitity = quantitiyElement.value
-    //start at 0 ^line 49
-    console.log(price)
-    //we want a total + a new value if product is added
-    var total = (price * quanitity) + total
-  }
-  //we start at the priceTag of the products, we want just one [0] and it should set value to toal 
-  //?DO I have to choose [0] because the method says "getElements", plural
-  document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total
+function deleteItem(e) {
+  //my products are in <div><div>
+  //my delete button is in <div><div><div>
+  //I want to remove the products divs from my button divs
+  var productContainer = e.currentTarget.parentElement
+  var deleteContainer = e.currentTarget.parentElement.parentElement
+  deleteContainer.removeChild(productContainer)
 }
-
-//Iteration4 Delete 
-
-// Select the parent that contains all of the product wrapper divs
-// Use the function removeChild we saw in DOM Manipulators
-// 💡 Use e.currentTarget to access the "Delete" button that was just clicked and select the parent node of an HTML element with parentNode.
-
-// function deleteItem(e) {
-//define all items position that are deleted
-var newProductDeleted = newProductDeleted[i]
-// Select all the "Delete" buttons
-var allDeleted = newProductDeleted.getElementsByClassName("btn-delete")[0]
-// For each button, assign a click event that will:
-// Select the wrapper div that contains all the HTML for the product that should be deleted
-
-
-// }
-
-// function getPriceByProduct(itemNode) {
-
-// }
-
-// function updatePriceByProduct(productPrice, index) {
-
-// }
-
-
-
-// function createQuantityInput() {
-
-// }
-
-// function createDeleteButton() {
-
-// }
-
-// function createQuantityNode() {
-
-// }
-
-// function createItemNode(dataType, itemData) {
-
-// }
-
-// function createNewItemRow(itemName, itemUnitPrice) {
-
-// }
-
-// function createNewItem() {
-
-// }
-
-window.onload = function () {
-  let element = document.getElementsByClassName("quanitityInput")[0]
-  element.value = "0";
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].onclick = deleteItem;
+// each products price:  let itemPrice = document.getElementsByClassName("setPrice")[0].innerText returns 25.00, [1] 15.00
+// each products quantity let productQuantity
+function getPriceByProduct(itemNode) {
+  let productQuantity = parseFloat(itemNode.getElementsByClassName("quanitityInput")[0].value)
+  let itemPrice = parseFloat(itemNode.getElementsByClassName("setPrice")[0].innerHTML)
+  return itemPrice * productQuantity
+}
+//if quantity is changed > loop over, want to have each input ?how to add do <div total price>
+function updatePriceByProduct() {
+  var newQuantity = document.getElementsByClassName("totalPrice")
+  for (var i = 0; i < newQuantity.length; i++) {
+    var input = newQuantity[i]
+    input.addEventListener("change") //
   }
-};
+
+  //Calculating the total price of all products
+  function getTotalPrice() {
+    // product-row > items 
+    let items = document.getElementsByClassName("product-row")
+    var total = 0
+    for (let i = 0; i < items.length; i++) {
+      let newItem = items[i]
+      let totalPrice = getPriceByProduct(newItem)
+      newItem.getElementsByClassName("totalPrice")[0].innerHTML = totalPrice
+      total = totalPrice + total
+    }
+    document.getElementById("cart-total-price").innerHTML = total
+  }
+
+
+
+  function createQuantityInput() {
+
+  }
+
+  function createDeleteButton() {
+
+  }
+
+  function createQuantityNode() {
+
+  }
+
+  function createItemNode(dataType, itemData) {
+
+  }
+
+  function createNewItemRow(itemName, itemUnitPrice) {
+
+  }
+
+  function createNewItem() {
+    //clone the product-row, input new procudt, appendChild
+  }
+
+  window.onload = function () {
+    var calculatePriceButton = document.getElementById('calc-prices-button');
+    var createItemButton = document.getElementById('new-item-create');
+    var deleteButtons = document.getElementsByClassName('btn-delete');
+
+    calculatePriceButton.onclick = getTotalPrice;
+    createItemButton.onclick = createNewItem();
+
+    for (var i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].onclick = deleteItem;
+    }
+  };
