62108e1f081f5db5a6ca3fdb2dbffcbf00558181
Augusta Raposo <gutaraposomarques@gmail.com>
2019-07-27 16:52:16 +0200

    update

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,37 @@
 function deleteItem(e){
 
 }
-
-function getPriceByProduct(itemNode){
-
+// itemNode
+function getPriceByProduct(){
+  let valueItem = document.getElementsByClassName('value');
+   
+  return Number(valueItem[0].innerText);
 }
+function getQtyByProduct(){
+	let qtyProduct = document.getElementsByClassName('quantity')[0].value;
+	return Number(qtyProduct);
+}
+
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
+	let sumPerProcuct = 0;
+    let elemh2= document.getElementsByTagName("h2");
+    debugger
+    let whoknows = elemh2[0].children;
+    debugger
+    let textspan = whoknows[0].innerHTML;
 
+    let elemSumPerProduct =  document.getElementsByClassName('sumperproduct')[0];
+
+    let val = getPriceByProduct();
+    let qt= getQtyByProduct();
+    sumPerProcuct = val * qt;
+    elemSumPerProduct.innerHTML = sumPerProcuct;
+    elemTotalPrice.innerHTML = sumPerProcuct;
 }
 
 function createQuantityInput(){
@@ -30,23 +50,42 @@ function createItemNode(dataType, itemData){
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItemRow(row){
 
+  const clone = row.cloneNode(true);
+
+  const container = document.getElementsByClassName("container")[0];
+
+  container.appendChild(clone);
+  debugger
 }
 
 function createNewItem(){
 
 }
 
+
 window.onload = function(){
+  let row = document.getElementsByClassName("row")[0];
+  
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
+  // var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  ;
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // let span = valueitem.children[0].innerHTML // $25.00
+  
+  const addRowBtn = document.getElementById("addRow")
+    addRowBtn.addEventListener("click",  function(){
+      createNewItemRow(row);
+  })
+  
+  // calculatePriceButton.onclick = print;
+  // console.log(valueitem);
+  // //getTotalPrice;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+  // for(var i = 0; i<deleteButtons.length ; i++){
+  //   deleteButtons[i].onclick = deleteItem;
+  // }
 };
