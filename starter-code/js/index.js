f308557c6db5669841ca8fb066181ff784903ddf
Augusta Raposo <gutaraposomarques@gmail.com>
2019-07-31 14:19:43 +0200

    go

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -10,10 +10,13 @@ function getQtyByProduct() {
 
 function getTotal() {
     let total = 0;
-    let valueItem = document.getElementsByClassName('value');
+    let valueItem = document.getElementsByClassName('valueitem');
     let qtyProduct = document.getElementsByClassName('quantity');
     let subtotal = document.getElementsByClassName('totalperproduct');
+    debugger
+
     for (let i = 0; i < qtyProduct.length; i++) {
+      debugger
         let firstValue = Number(valueItem[i].innerText);
         let secondValue = Number(qtyProduct[i].value);
         let product = firstValue * secondValue;
@@ -31,7 +34,7 @@ function updateTotalPrice(value) {
 }
 
 
-let deleteRom = (event) => {
+let deleteRow = (event) => {
     let father = document.getElementsByClassName('rows')[0];
     let children = father.childNodes;
     let cellulla = event.target;
@@ -49,7 +52,7 @@ let createNewRow = (event) => {
 
     cleanInput(firstInput);
     cleanInput(secondInput);
-    debugger
+    if (rows.length === 1) setup();
 }
 
 
@@ -58,65 +61,71 @@ let cleanInput = (input) => {
 }
 
 let setup = () => {
-    
+    let calculatePrice = document.getElementById("calc-prices-button");
+    let qtyProduct = document.getElementsByClassName('quantity');
+    let divResult = document.getElementsByClassName("result")[0];
+    if (qtyProduct === 1) {
+        calculatePrice.style.visibility = "visible";
+        divResult.hidden = false;
+    } else if (qtyProduct === 0) {
+
+        calculatePrice.style.visibility = "hidden";
+        divResult.hidden = true;
+    }
 }
-function createNewItem(description,receivedValue ){
-  let row = document.createElement('row');
-  row.setAttribute("class", "row");
 
-  let product = document.createElement('row');
-  product.setAttribute("class", "product");
-  let spanDescrition = document.createElement('span');
-  spanDescrition.innerHTML = description ;
-  product.appendChild(spanDescrition);
-  row.appendChild(product);
+function createNewItem(description, receivedValue) {
+    let row = document.createElement('row');
+    row.setAttribute("class", "row");
 
-  let valueitem = document.createElement('row');
-  valueitem.innerHTML = "$";
-  valueitem.setAttribute("class", "valueitem");
-  let value = document.createElement('span');
-  value.innerHTML = receivedValue ;
-  valueitem.appendChild(value);
-  row.appendChild(valueitem);
+    let product = document.createElement('row');
+    product.setAttribute("class", "product");
+    let spanDescrition = document.createElement('span');
+    spanDescrition.innerHTML = description;
+    product.appendChild(spanDescrition);
+    row.appendChild(product);
 
-  let amount = document.createElement('row');
-  amount.setAttribute("class", "amount");
-  let label = document.createElement('label');
-  label.setAttribute("for", "quantity");
-  label.innerHTML = "QTY";
-  amount.appendChild(label);
-  let input = document.createElement('input');
-  input.setAttribute("class", "quantity");
-  input.setAttribute("type", "text");
-  input.setAttribute("name", "quantity");
-  amount.appendChild(input);
-  row.appendChild(amount);
+    let valueitem = document.createElement('row');
+    valueitem.innerHTML = "$";
+    let value = document.createElement('span');
+    value.setAttribute("class", "valueitem");
+    value.innerHTML = receivedValue;
+    valueitem.appendChild(value);
+    row.appendChild(valueitem);
 
-  let totalperproduct = document.createElement('row');
-  totalperproduct.innerHTML = "$";
-  let spanDetails = document.createElement('span');
-  spanDetails.setAttribute("class", "totalperproduct");
-  spanDetails.innerHTML = "0.0";
-  totalperproduct.appendChild(spanDetails);
-  row.appendChild(totalperproduct);
-  
-  let btnDelete = document.createElement('button');
-  btnDelete.setAttribute("class", "btn btn-delete");
-  btnDelete.innerHTML = "Delete";
-  row.appendChild(btnDelete);
-  return row;
- 
+    let amount = document.createElement('row');
+    amount.setAttribute("class", "amount");
+    let label = document.createElement('label');
+    label.setAttribute("for", "quantity");
+    label.innerHTML = "QTY";
+    amount.appendChild(label);
+    let input = document.createElement('input');
+    input.setAttribute("class", "quantity");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "quantity");
+    amount.appendChild(input);
+    row.appendChild(amount);
+
+    let totalperproduct = document.createElement('row');
+    totalperproduct.innerHTML = "$";
+    let spanDetails = document.createElement('span');
+    spanDetails.setAttribute("class", "totalperproduct");
+    spanDetails.innerHTML = "0.0";
+    totalperproduct.appendChild(spanDetails);
+    row.appendChild(totalperproduct);
+
+    let btnDelete = document.createElement('button');
+    btnDelete.setAttribute("class", "btn btn-delete");
+    btnDelete.innerHTML = "Delete";
+    btnDelete.addEventListener("click", deleteRow);
+    row.appendChild(btnDelete);
+    return row;
+
 }
 
 
 window.onload = function() {
-    
-    let calculatePrice = document.getElementById("calc-prices-button");
-    calculatePrice.style.visibility ="hidden";
-    let divResult = document.getElementsByClassName("result")[0];
-    divResult.hidden = true;
-
-
+    setup();
     let createButton = document.getElementsByClassName("btn-create")[0];
     createButton.onclick = createNewRow;
     const calculatePriceButton = document.getElementById("calc-prices-button");
