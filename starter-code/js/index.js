4e205ec66a45c120cfd788541c00dfb7237d8ca9
maerd19 <revolutions_333@hotmail.com>
2019-08-06 17:52:39 -0500

    almost there

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 function deleteItem(e){
-
+  console.log(e)
 }
 
 function getPriceByProduct(itemNode){
@@ -11,15 +11,16 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-  let elements = 2;
+  var containerDiv = document.getElementById('products');
+  var elements = containerDiv.getElementsByClassName('product').length;
   let final = 0;
   for(let i = 1; i <= elements; i++) {
     let quantity = document.getElementById('quantity' + i).value;
-    let cost = document.getElementById('productCost' + i).innerHTML;  
+    let cost = document.getElementById('productCost' + i).innerHTML;
     let result = quantity * cost.slice(1);
     document.getElementById('finalPrice' + i).innerHTML = `$${result}`;
     final += result;
-  }  
+  }
   document.getElementById('finalPrice').innerHTML = `$${final}`
 }
 
@@ -44,7 +45,33 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
+  var container = document.getElementById('products');
+  var productName = document.getElementById('newProductName').value;
+  var productCost = document.getElementById('newProductCost').value;
+  var containerDiv = document.getElementById('products');
+  var count = containerDiv.getElementsByClassName('product').length;
 
+  // console.log(`productName: ${productName}. productCost: ${productCost}. Elements: ${count}`);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div id="product${count + 1}" class="product">
+          <div id="productName${count + 1}">
+            <span>${productName}</span>
+          </div>
+          <div>
+            <span id="productCost${count + 1}">$${productCost}.00</span>
+          </div>
+          <div>
+            QTY: <input type="text" id="quantity${count + 1}">
+          </div>
+          <div>
+            <span id="finalPrice${count + 1}">$0.00</span>
+          </div>
+          <div id="delete${count + 1}">
+            <button class="btn btn-delete" onclick="deleteItem(event)">Delete</button>
+          </div>
+        </div>`;
+        container.appendChild(div);
 }
 
 window.onload = function(){
