a234e6c76b1827f424090dc34662c676477605d1
Doro Hassan <dorothea.hassan@gmail.com>
2019-08-18 17:34:15 +0200

    interation 1 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,22 +1,43 @@
+
+
+
 function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(){
+  let productPrice = document.querySelector('.price span').innerHTML; // select the class name and the span
+  let quantity = document.getElementById('quantity-value').value;
+  let proPrice = parseFloat(productPrice.slice(2));
+  //console.log(proPrice);
+  let arrPrice = [];
+  let arrQuantity = [];
+  arrPrice.push(proPrice);
+  arrQuantity.push(Number(quantity));
+  let endPrice = arrPrice * arrQuantity;
+  return endPrice.toFixed(2); 
 }
 
-function updatePriceByProduct(productPrice, index){
-
+function updatePriceByProduct(){
+  let price = getPriceByProduct();
+  const priceSpan = document.getElementById('endPrice');
+  priceSpan.innerText = '';
+  let span = document.createElement('span');
+  span.innerText = '$ ' + price;
+  priceSpan.appendChild(span);
 }
 
 function getTotalPrice() {
+  getPriceByProduct();
+  updatePriceByProduct();
+  
 
 }
 
 function createQuantityInput(){
-
+  
 }
+// to change the string price to a number , erase the $ and the .
 
 function createDeleteButton(){
 
@@ -39,12 +60,12 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var calculatePriceButton = document.getElementById('calc-btn');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
