7945427b570323efec8645bbc6bff83fdf7db4f9
Doro Hassan <dorothea.hassan@gmail.com>
2019-08-19 09:48:13 +0200

    not completely done but supmitable

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,7 +6,7 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(){
-  let productPrice = document.querySelector('.price span').innerHTML; // select the class name and the span
+  let productPrice = document.querySelector('#price span').innerHTML; // select the class name and the span
   let quantity = document.getElementById('quantity-value').value;
   let proPrice = parseFloat(productPrice.slice(2));
   //console.log(proPrice);
@@ -34,10 +34,8 @@ function getTotalPrice() {
 
 }
 
-function createQuantityInput(){
-  
-}
-// to change the string price to a number , erase the $ and the .
+// function createQuantityInput(){
+// }
 
 function createDeleteButton(){
 
@@ -52,11 +50,27 @@ function createItemNode(dataType, itemData){
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
-
+  
 }
 
 function createNewItem(){
+  let container = document.getElementsByClassName('container');
+  // container.innerHTML += `<div class="container"></div>`;
 
+  container = document.createElement('div');
+  container.classList.add('conainter');
+  container.appendChild(container);
+
+  // <div id="container">
+  //   <div> <span>item name</span> </div>
+  //   <div class="price"> <span>$ 15,00</span> </div>
+  //   <div class="quantity"> <form> <label for="quality">QTY</label> <input id="quantity-value" type="number" value="1"> </form> </div>
+  //   <div> <span id="endPrice">$ 0,00</span> </div>
+  //   <div> <button class="btn btn-delete" type="reset" value="Delete">Delete</button> </div>
+  //   <div> <button id="new-item-create" class="btn btn-inserItem" type="submit" value="Submit">Insert Item</button> </div>
+  // </div>
+
+  console.log('hey');
 }
 
 window.onload = function(){
@@ -65,7 +79,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
