01a6dd34a52eabbd9c3a4a68175dbed58eca9e35
Miguel Carrilho <mangelott@gmail.com>
2019-08-21 15:28:55 +0100

    LIS-FTWD-MiguelCarrilho-#156

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars":[0],
+        "semi":[2, "always"]
+    }
+};
--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,72 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
 function getTotalPrice() {
+  for(let $product of document.getElementsByClassName('product')){
+    const $totalOrder = $product.querySelector('.total');
+    const $unit = $product.querySelector('.cost');
+    const $quantity = $product.querySelector('.qty');
+
+    const unit = parseInt($unit.innerText);
+    const quant = parseInt($quantity.value);
+    const total = unit * quant
+    
+   /* const $triggerCalc = document.getElementById('calc-prices-button');
+    const $triggerDel = document.getElementById('btn-delete');
+    const $totalOrder = document.getElementById('total1');
+    const $unit = document.getElementById('costUnit1');
+    const $quantity = document.getElementById('qty1');
+
+    const unit = parseInt($unit.innerText);
+    const quant = parseInt($quantity.value);
+    const total = unit * quant;
+    */
+    $totalOrder.innerText = total;
+  }
+}
+
+function createQuantityInput() {
 
 }
 
-function createQuantityInput(){
+function createDeleteButton() {
 
 }
 
-function createDeleteButton(){
+function createQuantityNode() {
 
 }
 
-function createQuantityNode(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createItemNode(dataType, itemData){
+function createNewItemRow(itemName, itemUnitPrice) {
 
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
+function createNewItem() {
 
 }
 
-function createNewItem(){
-
-}
-
-window.onload = function(){
+window.onload = function () {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
 };
