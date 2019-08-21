a2de12683552a286578fd88506fbfe3370f56dd2
beneditaalmeida <benedita@MacBook-Pro-de-Benedita.local>
2019-08-21 15:29:25 +0100

    dom exercise

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -8,11 +8,43 @@ function getPriceByProduct(itemNode){
 
 function updatePriceByProduct(productPrice, index){
 
+  function getTotalPrice() {
+    const allProducts = document.getElementsByClassName("wrapper-product")
+    let sum = 0;
+    for (let product of allProducts){
+      let unitPriceSpan = product.querySelector(".price");
+      let unitPrice = parseFloat(unitPriceSpan.textContent)
+  
+      let quantity = product.querySelector("input").value;
+      quantity = parseFloat(quantity);
+  
+      let totalPrice = quantity * unitPrice;
+      sumTotal = totalPrice;
+      let totalPriceString = totalPrice.toFixed(2) + "€";
+  
+      let totalPriceSpan = product.querySelector(".total-value");
+      totalPriceSpan.textContent = totalPriceString;
+    }
+  let sumTotalSpan = document.querySelector(".calc-prices-button");
+  let sumTotalString = sumTotal.toFixed(2) + "€";
+  sumTotalSpan.textContent = sumTotalString;
+
 }
 
-function getTotalPrice() {
+// function getTotalPrice() {
+//   const allProducts = document.getElementsByClassName("wrapper-product")
+//   let totalPrice = 0;
+//   for (let product of allProducts){
+//     let unitPrice = product.querySelector(".price");
+//     let quantity = product.querySelector("input").value;
+//     totalPrice = (quantity * unitPrice).toFixed(2) + "€";
+//   }
+//   let sumTotalSpan = document.querySelector(".calc-prices-button");
+//   let sumTotalString = sumTotal.toFixed(2) + "€";
+//   sumTotalSpan.textContent = sumTotalString;
+// }
 
-}
+
 
 function createQuantityInput(){
 
@@ -50,3 +82,5 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
