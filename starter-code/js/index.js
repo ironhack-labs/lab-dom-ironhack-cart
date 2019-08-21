02ddcf244f9f5036da2353bbf6066891a7a8b3d9
Larissa <dejamepienso@gmail.com>
2019-08-21 18:36:29 -0500

    Add frists 3 iterations

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,52 @@
-function deleteItem(e){
+const bigTot = document.querySelector('#bigTotal')
 
+document.querySelector("#calc").onclick = () => {
+  calcPrice()
 }
 
-function getPriceByProduct(itemNode){
-
+document.querySelector(".btn-create").onclick = () => {
+  addProduct()
 }
 
-function updatePriceByProduct(productPrice, index){
 
-}
+function calcPrice (){
+  let prices = document.querySelectorAll('.price')
+  let qty = document.querySelectorAll(".qty")
+  let subtotal = document.querySelectorAll('.subtotal')
+  let totalPrice = 0;
+  let total = 0;
 
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for (i = 0; i < prices.length; i++) {
+    total = parseFloat(prices[i].innerHTML)
+    total *= parseFloat(qty[i].value)
+    subtotal[i].innerText = `$ ${total}`
+    totalPrice += total
   }
-};
+  bigTot.innerText = totalPrice
+}
+
+
+function addProduct (){
+  let pr = document.querySelector('.newproduct')
+  let parent = document.querySelector('.cart')
+  let newPrd = document.querySelector('#newproduct')
+  let bubbb =  document.querySelector('.buble')
+  
+ let newww = newPrd.val
+
+ let closest = pr.closest('li > .price')
+
+  console.log(closest)
+
+let newItem = `
+<li class="row prdct">
+<span class="buble"></span>
+<span class="price"></span>
+<input type="number" class="qty" value="0">
+<span class="subtotal"></span>
+<button class="btn-delete">Delete</button>
+</li>
+`
+parent.insertAdjacentHTML("afterend", newItem)
+
+}
