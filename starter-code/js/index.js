b3c9d433fd8f5fec9b9cfda51ecd9ededa65581d
kidistca <kidistca@gmail.com>
2019-08-21 15:28:16 +0100

    cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,17 +2,35 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
 
+function getPriceByProduct(itemNode){
+  return document.getElementById("unitprice").innerText;
 }
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
-function getTotalPrice() {
-
+function getTotalPrice(){
+  // console.log("DEBUG getTotalPrice called");
+      const unitprice = parseFloat(document.getElementById("unitprice").innerText);
+      const quantity = parseFloat(document.getElementById("firstinput").value);
+      const totalprice = quantity * unitprice;
+      console.log(totalprice);
+      // return totalprice;
 }
+
+// calculatePriceButton.addEventListener('click', function getTotalPrice() {
+//   // function(event){
+//     console.log("CLICKED!");
+
+//    console.log(quantity);
+//   //  console.log(totalprice);
+  
+  
+//   });
+//   // return unitPrice;
+
 
 function createQuantityInput(){
 
@@ -44,7 +62,7 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
