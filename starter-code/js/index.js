6e3e875192186482b29d51cf370abbaf177c1ef7
erivasf <elias_rf@icloud.com>
2019-08-21 15:06:47 -0500

    DOOOONE

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,62 @@
-function deleteItem(e){
+const btnCalculate = document.querySelector('#calculate')
+const btnCreate = document.querySelector('.btn-create')
+const main = document.querySelector('main div')
+const yaEsLoMenos = document.querySelector('#totalPrice')
+const yaNoTeQuiero = document.querySelectorAll('.btn-delete')
 
-}
+const calculatePrice = () => {
+  const prices = document.querySelectorAll('.price')
+  const qty = document.querySelectorAll('.qty input')
+  const tot = document.querySelectorAll('.total')
+  let totalPrice = 0;
+  let total = 0;
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  for (i = 0; i < prices.length; i++) {
+    total = parseFloat(prices[i].innerHTML.match(/[0-9]/g).join('')) / 100
+    total *= parseFloat(qty[i].value)
+    tot[i].innerText = `$ ${total.toFixed(2)}`
+    totalPrice += total
   }
-};
+  yaEsLoMenos.innerText = totalPrice
+}
+const deleteProduct = (e) => {
+  const seVa = e.target.parentElement
+  console.log(seVa)
+  main.removeChild(seVa)
+
+  calculatePrice()
+}
+
+const addProduct = () => {
+  const inputName = document.querySelector('#create input')
+  const inputPrice = document.querySelectorAll('#create input')[1]
+
+  if (inputName.value.trim() === '' || inputPrice.value.trim() === '')
+    return
+
+  const div = document.createElement('div')
+  const spanName = document.createElement('span')
+  spanName.innerText = inputName.value
+  const spanPrice = document.createElement('span')
+  spanPrice.innerText = `$ ${parseFloat(inputPrice.value).toFixed(2)}`
+  spanPrice.className = 'price'
+
+  div.appendChild(spanName)
+  div.appendChild(spanPrice)
+
+  div.innerHTML += '<span class="qty"><label for="QTY">QTY</label> <input type="number" value=0></span><span class="total" > $0 .00 </span>'
+
+  const btnBorrar = document.createElement('button')
+  btnBorrar.innerText = 'Delete'
+  btnBorrar.className = 'btn-delete btn delete'
+  btnBorrar.onclick = deleteProduct
+  div.appendChild(btnBorrar)
+
+  main.appendChild(div)
+  inputName.value = ''
+  inputPrice.value = ''
+}
+
+btnCalculate.onclick = calculatePrice
+btnCreate.onclick = addProduct
+yaNoTeQuiero[0].onclick = deleteProduct
