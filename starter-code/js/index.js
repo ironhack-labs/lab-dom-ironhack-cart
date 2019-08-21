9f44eee055aa9b2228fd74c493a7593f99ea3ef0
Daniel Mendes Valero <dmendesvalero@gmail.com>
2019-08-21 15:28:09 +0100

    exercise lab, not done too much

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,23 @@
+// const test = document.getElementById('unitPrice').innerText;
+// console.log(test)
+// input.addEventListener('keydown', function (event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log('In first listener');
+// });
+
+/*
+let calculatePrice = document.getElementById('calc-prices-button')
+
+let calculateAcumulade = document.getElementById('acumulate');
+
+ calculateAcumulade.addEventListener('click', function(event){
+   let amount = parseInt(document.getElementById('unitPrice').innerText) * document.getElementById('quantity');
+   return document.getElementById('acumulate');
+});
+*/
+
+
 function deleteItem(e){
 
 }
@@ -11,11 +31,18 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  let unitPrice = document.getElementById('unitPrice').textContent;
+  let input = document.getElementById('input').value; //este es el label con la cantidad
+  let totalPrice = unitPrice * input;
 
+  //return;
 }
 
 function createQuantityInput(){
-
+  // let input = document.getElementById('input').value;
+  // let unitPrice = document.getElementById('unitPrice').textContent;
+  // let acumulate= unitPrice * input;
+  // return document.getElementById('acumulate');
 }
 
 function createDeleteButton(){
@@ -39,14 +66,14 @@ function createNewItem(){
 }
 
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+   var calculatePriceButton = document.getElementById('calc-prices-button');
+   var createItemButton = document.getElementById('new-item-create');
+   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+   calculatePriceButton.onclick = getTotalPrice;
+   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+   for(var i = 0; i<deleteButtons.length ; i++){
+     deleteButtons[i].onclick = deleteItem;
+   }
+ };
