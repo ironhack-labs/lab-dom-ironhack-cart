3b9847028f9661ccc9c22820a32f50a31956882c
chris bolivar <christopherbolivar@hotmail.com>
2019-08-22 16:24:56 -0400

    updates the total price of a single item depending on the quantity

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -14,7 +14,7 @@ for (i = 0; i < Object.keys(prodObj).length; i++) {
     string += "<div class='col-sm-3'><span>" + prodObj[i].name + "</span></div>";
     string += "<div class='col-sm-2'><span>" + "$" + prodObj[i].price + "</span></div>";
     string += "<div class='col-sm-3'><label>QTY</label><input id='input" + [i] + "'type='text'></div>";
-    string += "<div class='col-sm-2'><span class='total'" + [i] + ">" + '$0.00' + "</span></div>";
+    string += "<div class='col-sm-2'><span id='total" + [i] + "'>" + '$0.00' + "</span></div>";
     string += "<div class='col-sm-2'><button class='btn btn-delete'>delete</button></div>";
     string += "</div>";
     document.querySelector(".products").innerHTML += string;
@@ -22,31 +22,29 @@ for (i = 0; i < Object.keys(prodObj).length; i++) {
 }
 
 
-document.querySelector(".btn-succes").onclick = function(e) {
+document.querySelector(".btn-success").onclick = function(e) {
     let quant = [];
 
     for (i = 0; i < Object.keys(prodObj).length; i++) {
 
         let query = "#input" + [i]
-
-        let total = ".total" + [i]
         console.log(query)
-        console.log(total)
         quant.push(document.querySelector(query).value);
-        document.querySelectorAll(".total" + [i]).innerHTML = prodObj[i].price * quant[i]
-        console.log(prodObj[i].price * quant[i])
+
     }
-    /*  
+
     for (i = 0; i < Object.keys(prodObj).length; i++) {
 
-        let total = ".total" + [i]
-        document.querySelector(".total" + [i]).innerHTML = prodObj[i].price * quant[i]
+        let total = "#total" + [i]
+        console.log(total);
+        document.querySelector("#total" + [i]).innerHTML = "$" + prodObj[i].price * quant[i]
         console.log(prodObj[i].price * quant[i])
     }
-    */
+
 
 
 }
+
 
 
 
