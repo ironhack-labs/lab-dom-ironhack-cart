8f0e86d1ea08b2ec2d62d44d0e44d5a0a765a782
chris bolivar <christopherbolivar@hotmail.com>
2019-08-22 14:08:10 -0400

    dynamiclly pushed out HTML elements via javascript

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,70 @@
-function deleteItem(e){
+var prodObj = [{
+        name: "Iron Bubble Head",
+        price: 25
+    },
+    {
+        name: "Iron Shirt",
+        price: 15
+    }
+]
+
+for (i = 0; i < Object.keys(prodObj).length; i++) {
+    let getValue = "";
+    var string = "<div class='row'>";
+    string += "<div class='col-sm-3'><span>" + prodObj[i].name + "</span></div>";
+    string += "<div class='col-sm-2'><span>" + "$" + prodObj[i].price + "</span></div>";
+    string += "<div class='col-sm-3'><label>QTY</label><input id='input" + [i] + "'type='text'></div>";
+    string += "<div class='col-sm-2'><span class='total'" + [i] + ">" + '$0.00' + "</span></div>";
+    string += "<div class='col-sm-2'><button class='btn btn-delete'>delete</button></div>";
+    string += "</div>";
+    document.querySelector(".products").innerHTML += string;
 
 }
 
-function getPriceByProduct(itemNode){
+
+document.querySelector(".btn-succes").onclick = function(e) {
+    let quant = [];
+
+    for (i = 0; i < Object.keys(prodObj).length; i++) {
+
+        let query = "#input" + [i]
+
+        let total = ".total" + [i]
+        console.log(query)
+        console.log(total)
+        quant.push(document.querySelector(query).value);
+        document.querySelectorAll(".total" + [i]).innerHTML = prodObj[i].price * quant[i]
+        console.log(prodObj[i].price * quant[i])
+    }
+    /*  
+    for (i = 0; i < Object.keys(prodObj).length; i++) {
+
+        let total = ".total" + [i]
+        document.querySelector(".total" + [i]).innerHTML = prodObj[i].price * quant[i]
+        console.log(prodObj[i].price * quant[i])
+    }
+    */
+
 
 }
 
-function updatePriceByProduct(productPrice, index){
 
+
+
+/* 
+
+window.onload = function() {
+    var calculatePriceButton = document.getElementById('calc-prices-button');
+    var createItemButton = document.getElementById('new-item-create');
+    var deleteButtons = document.getElementsByClassName('btn-delete');
+
+    calculatePriceButton.onclick = getTotalPrice;
+    createItemButton.onclick = createNewItem;
+
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].onclick = deleteItem;
+    }
 }
 
-function getTotalPrice() {
 
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+*/
