996a23d1dbb36d9d4fa692da0c1c9c6fe7056309
Christian Roque <croq008@fiu.edu>
2019-08-22 21:42:49 -0400

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,31 @@
-function deleteItem(e){
 
-}
 
-function getPriceByProduct(itemNode){
 
-}
 
-function updatePriceByProduct(productPrice, index){
 
-}
 
 function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  let total = 0
+  let i = 0
+  let price = document.getElementsByClassName('price')
+  let updated = document.getElementsByClassName('updated')
+  let quantity = document.querySelector(`#shopping > div:nth-child(${i+1}) > form > input`);
+  // let totalFinal = document.querySelector()
+  
+  while (i < price.length) {
+     total += Number(price[i].innerHTML) * quantity.value
+     updated[i].textContent = Number(price[i].innerHTML) * quantity.value
+    i++
   }
-};
+  return total
+}
+
+// window.onload = function() {
+  
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   var createItemButton = document.getElementById('new-item-create');
+
+//   calculatePriceButton.onclick = getTotalPrice;
+//   createItemButton.onclick = createNewItem;
+// }
+
