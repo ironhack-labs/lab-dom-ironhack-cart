63826ce607100ce00dfa72f9d127f01d3a4e20f8
Marven Mathelier <marven250@hotmail.com>
2019-08-24 20:20:26 -0400

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,44 +1,37 @@
-/* function deleteItem(e){
-
+function deleteItem(){
+  window.t= this;
+    t.parentNode.parentNode.remove()
 }
 
-function getPriceByProduct(itemNode){
 
+function getTotalPrice(e) {
+    let price= document.getElementsByClassName("price")
+    let qty= document.getElementsByClassName("qty")
+    let subTotal= document.getElementsByClassName("totPrice")
+    for(var i= 0; i<price.length; i++){
+    subTotal[i].innerHTML="$"+ Number(price[i].innerHTML.slice(2)) * Number(qty[i].value)
+    }
+
+  }
+
+function createNewItem(e){
+  let namer = window.prompt("What is the name?")
+  let pricer = window.prompt("What is the price?")
+  let newLine = `<div class="wrapper">
+        <div> <span> ${namer} </span> </div>
+        <div><span class="price"> $${pricer} </span></div>
+        <div class="quantity"><label for="qty1">QTY</label><input class="qty" type="text" placeholder="0"></div>
+        <div><span class="totPrice">$0.00</span></div>
+        <div><button class="btn-delete btn">Delete</button></div>
+      </div>
+      <br>`
+      document.getElementById("bigWrap").innerHTML += newLine
+  init()
 }
 
-function updatePriceByProduct(productPrice, index){
+window.onload = init
 
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
+function init(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
@@ -49,8 +42,10 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-}; */
+}; 
 
+
+/*
 let price1 = document.getElementById("price1").innerHTML.slice(2);
 
 
@@ -62,3 +57,5 @@ calculatePrice.onclick= function(){
    let totprice=price1 * quantity;
 document.getElementById("totPrice1").innerHTML = "$" + totprice
 }
+
+*/
