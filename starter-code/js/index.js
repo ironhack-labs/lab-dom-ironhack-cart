b8919611f5d405cb15f573ac939ee4982ba917fc
Unai Bolivar <ujbolivar@gmail.com>
2019-08-26 09:21:00 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,51 +1,54 @@
 function deleteItem(e){
-
+  
 }
 
 function getPriceByProduct(itemNode){
-
+  let productPrice = products.querySelector("product-price");
+  let quantity = products.querySelector("quantity");
+  return productPrice * quantity
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  
 }
 
 function getTotalPrice() {
-
+  let totalPrice = 0;
+  
 }
 
 function createQuantityInput(){
-
+  
 }
 
 function createDeleteButton(){
-
+  
 }
 
 function createQuantityNode(){
-
+  
 }
 
 function createItemNode(dataType, itemData){
-
+  
 }
 
 function createNewItemRow(itemName, itemUnitPrice){
-
+  
 }
 
 function createNewItem(){
-
+  
 }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
-
+  
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
-
+  
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
