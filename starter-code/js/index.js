1c3a46409ce7ff1fea99c222082ebc6346ff4539
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-08-26 09:26:46 +0200

    [MAD DE] Pablo Giral

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,72 @@
-function deleteItem(e){
 
-}
+// function deleteItem(e){
 
-function getPriceByProduct(itemNode){
+// }
 
-}
+// function getPriceByProduct(itemNode){
 
-function updatePriceByProduct(productPrice, index){
+// }
 
-}
+// function updatePriceByProduct(productPrice, index){
+
+// // }
 
 function getTotalPrice() {
-
+let itemQuantity = document.querySelector(".itemQty").value;
+let itemUnitPrice = document.querySelector(".price");
+let totalProdPrice = document.querySelector(".totalPrice");
+// let unitParse = parseFloat(itemUnitPrice);
+totalProdPrice.innerHTML = itemQuantity * itemUnitPrice.innerHTML;
+console.log(itemQuantity);  
+// console.log(unitParse);
+console.log(totalProdPrice);
 }
+// getTotalPrice();
+// console.log(totalProdPrice);
+// console.log(itemQuantity);
+// console.log(itemUnitPrice);
+//   (document.getElementById("itemQty").value) * 
+// }
 
-function createQuantityInput(){
+// function createQuantityInput(){
 
-}
+// }
 
-function createDeleteButton(){
+// function createDeleteButton(){
 
-}
+// }
 
-function createQuantityNode(){
+// function createQuantityNode(){
 
-}
+// }
 
-function createItemNode(dataType, itemData){
+// function createItemNode(dataType, itemData){
 
-}
+// }
 
-function createNewItemRow(itemName, itemUnitPrice){
+// function createNewItemRow(itemName, itemUnitPrice){
 
-}
+// }
 
-function createNewItem(){
+// function createNewItem(){
 
-}
+// }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  // var createItemButton = document.getElementById('new-item-create');
+  // var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  calculatePriceButton.onclick = getTotalPrice  ;
+  // createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
+
+
+// document.querySelector(".btn-success").onclick = getTotalPrice();
+
+// document.getElementById("randomPrice").onclick = function() {
+// document.getElementById("price").value = Math.random();
 };
