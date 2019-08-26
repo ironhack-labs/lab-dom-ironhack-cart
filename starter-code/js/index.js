abb08b764a9a04fe71fb220c74ee9834018746aa
rashier <rashier13@gmail.com>
2019-08-26 09:12:18 +0200

    i cannt make all exercice

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,64 @@
-function deleteItem(e){
 
+
+// function deleteItem(e){
+// }
+
+ function updatePriceByProduct() {
+  var priceUnit = parseInt(document.querySelector("#priceValue").innerHTML);
+  priceUnit = priceUnit * parseInt(document.querySelector("#quatity").value);
+  document.querySelector("#totalPriceProduct").innerHTML = priceUnit;
+  console.log(document.querySelector("#totalPriceProduct").innerHTML);  
 }
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+window.onload = function() {
+  var priceUnittot = document.querySelector("#calcPrices");
+  priceUnittot.onclick = updatePriceByProduct;
 };
+
+// function getPriceByProduct(itemNode){
+
+// }
+
+// function updatePriceByProduct(productPrice, index){
+
+// }
+
+// function getTotalPrice() {
+
+// }
+
+// function createQuantityInput(){
+
+// }
+
+// function createDeleteButton(){
+
+// }
+
+// function createQuantityNode(){
+
+// }
+
+// function createItemNode(dataType, itemData){
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
+
+//  window.onload = function(){
+//    var calculatePriceButton = document.getElementById('calc-prices-button');
+//    var createItemButton = document.getElementById('new-item-create');
+//    var deleteButtons = document.getElementsByClassName('btn-delete');
+
+//    calculatePriceButton.onclick = getTotalPrice;
+//    createItemButton.onclick = createNewItem;
+
+//    for(var i = 0; i<deleteButtons.length ; i++){
+//      deleteButtons[i].onclick = deleteItem;
+//    }
+//  };
