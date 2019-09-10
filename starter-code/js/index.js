ec5822907c4e65c5e8f4481a033c0e55768ba708
Nikola du Preez <nikola_dupreez@hotmail.com>
2019-09-10 12:42:56 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,43 +1,92 @@
-function deleteItem(e){
+// Iteration 1 & 3 
+function retrieveUnitPrice(itemNum){
+  let unitPriceCollection = document.querySelectorAll('.products .row .unit-price');
+  let unitPrice = unitPriceCollection[itemNum].innerHTML;
+  return parseFloat(unitPrice);
+};
 
-}
+function retrieveItemQuantity(itemNum){
+  let itemQuantityCollection = document.querySelectorAll('.products .row .quantity');
+  let unitQuantity = itemQuantityCollection[itemNum].value;
 
-function getPriceByProduct(itemNode){
+  if (!unitQuantity){
+    unitQuantity = 0;
+  }
+  return parseInt(unitQuantity);
+};
 
-}
+function calcPricePerProduct(itemNum){
+  let price = retrieveUnitPrice(itemNum);
+  let quantity  = retrieveItemQuantity(itemNum);
+  let totalPriceProduct =  (price * quantity).toFixed(2);
+  return totalPriceProduct;
+};
 
-function updatePriceByProduct(productPrice, index){
+function getTotalPrice(){
+  let totalPriceCollection = document.querySelectorAll('.products .row .total-price');
+  let cartTotalPrice = document.getElementById('cart-total-price');
+  let cartTotal = 0;
+  
+  for (i = 0; i < totalPriceCollection.length; i++){
+    let totalPrice = calcPricePerProduct(i);
+    totalPriceCollection[i].innerHTML = totalPrice;
+    cartTotal += parseFloat(calcPricePerProduct(i));
+  };
+  cartTotalPrice.innerHTML = cartTotal.toFixed(2);
+};
 
-}
+// Iteration 2 & 5
+function createNewItemRow(name, price){
+  let products = document.getElementById('products');
+  let newItem = document.createElement('div');
+  newItem.classList.add('row');
+  newItem.innerHTML = `
+                      <span class='unit-name'>${name}</span>
+                      <p>$<span class='unit-price'>${price}</span></p>
+                      <div>
+                          <label for=units-form>QTY</label>
+                          <input name='units-form' type='text' class='quantity'>
+                      </div>
+                      <div>
+                          <p>$<span class='total-price'>0.00</span></p>
+                      </div>
+                      <div>
+                          <button type='button' class='btn btn-delete'>Delete</button>
+                      </div>
+                     `;
+  products.appendChild(newItem);
+};
 
-function getTotalPrice() {
+function clearInput(){
+  let inputCollection = document.getElementsByClassName('create-input');
 
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
+  for (i = 0; i < inputCollection.length; i++){
+    let input = inputCollection[i];
+    input.value = '';
+  };
+};
 
 function createNewItem(){
+  let inputProduct = document.getElementById('product-name').value;
+  let inputPrice = parseFloat(document.getElementById('product-price').value);
+  let price = inputPrice.toFixed(2);
 
-}
+  createNewItemRow(inputProduct, price);
+  window.onload();
+  clearInput();
+};
 
+// Iteration 4
+function deleteItem(e){
+  let buttonClicked = e.currentTarget;
+  let productsContainer = document.getElementById('products');
+  let product = buttonClicked.parentElement.parentElement;
+  productsContainer.removeChild(product);
+
+  getTotalPrice();
+};
+
+// Run functions with events
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
@@ -48,5 +97,5 @@ window.onload = function(){
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
-  }
+  };
 };
