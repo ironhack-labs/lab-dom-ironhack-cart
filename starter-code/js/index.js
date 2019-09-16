1dcb19c8814e54f42fc4aea93698bc81f3936bc4
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-16 18:20:13 -0300

    stuck on Iteration 5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 function deleteItem(e){
-
+  e.parentNode.removeChild(e)
 }
 
 function getPriceByProduct(itemNode){
@@ -11,6 +11,18 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
+  var price = document.getElementsByClassName('unit-price')
+  var qnt = document.getElementsByClassName('qnt')
+  var itemTotalPrice = document.getElementsByClassName('item-total-price')
+  var cartTotalPrice = document.getElementById("cart-total-price")
+  var cont = 0
+
+  for(var i = 0; i<itemTotalPrice.length ; i++){
+    cont += price[i].innerHTML * qnt[i].value
+    itemTotalPrice[i].innerHTML = "$"+price[i].innerHTML * qnt[i].value;
+  }
+cartTotalPrice.innerHTML = cont
+
 
 }
 
@@ -35,13 +47,17 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-
+  console.log("hellow ");
+  
 }
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+
+  console.log(createItemButton);
+  
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
