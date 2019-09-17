20819a22ca73b11f999e74139e1a74eddb7f8061
Guilherme Ribeiro <guilherme@martizi.com>
2019-09-17 19:59:23 -0300

    first commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,6 @@
-function deleteItem(e){
-
+function deleteItem(event){
+	let productLine = event.target.parentElement.parentElement
+	document.body.removeChild(productLine)
 }
 
 function getPriceByProduct(itemNode){
@@ -11,7 +12,16 @@ function updatePriceByProduct(productPrice, index){
 }
 
 function getTotalPrice() {
-
+	let cnt = 0
+	let totalPrice = 0
+	let productPrices = document.querySelectorAll(".product-price")
+	for (product of productPrices){
+		let result = parseFloat(product.innerHTML) * parseFloat(document.querySelectorAll(".product-qty")[cnt].value)
+		document.querySelectorAll(".product-total-price")[cnt].innerHTML = result
+		totalPrice = totalPrice + result
+		cnt = cnt + 1
+	}
+	document.getElementById("total-price").innerHTML = totalPrice
 }
 
 function createQuantityInput(){
@@ -35,8 +45,12 @@ function createNewItemRow(itemName, itemUnitPrice){
 }
 
 function createNewItem(){
-
-}
+	produto = document.querySelectorAll(".product-line")[0];
+	clone = produto.cloneNode(produto);
+	clone.children[0].children[0].children[0].innerHTML = document.getElementById('product-name-input').value;
+	clone.children[1].children[0].innerHTML = document.getElementById('product-name-input').value
+	document.body.appendChild(clone)
+}	
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
@@ -50,3 +64,11 @@ window.onload = function(){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+
+
+
+
+
+
