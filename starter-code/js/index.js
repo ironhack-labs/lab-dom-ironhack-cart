75232ac524a6bb003933588c6b0a00dfec75e3e5
Victor Nevola <victornevolapn@gmail.com>
2019-09-17 19:44:22 -0300

    Done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,40 +1,94 @@
 function deleteItem(e){
 
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
+  e.target.parentElement.remove();
+  getTotalPrice(e)
 
 }
 
-function getTotalPrice() {
+function getTotalPrice(even) {
+  let div = document.getElementById('sumAll');
+  let listTotais = document.querySelectorAll('.total');
+  let total = 0.00;
+  let titlePrice = document.querySelector('h2');
 
-}
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
+  for(let i =0; i<listTotais.length; i+=1){
+     total += parseFloat(listTotais[i].innerText);
+    };
+  
+    if(titlePrice < 1 ){
+      let sumAll = document.createElement('h2');
+      div.appendChild(sumAll);
+      sumAll.innerText = `Total Price: $ ${total}`;
+    } else{
+      titlePrice.innerText = `Total Price: $ ${total}`;
+    }
 
 }
 
 function createNewItem(){
+
+  let productNew = document.querySelector('#newProduct').value;
+  let unitPriceNew = document.querySelector('#newPrice').value;
+  let products = document.querySelector('#products');
+
+  if (productNew !== "" && unitPriceNew !== 0){
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'wrapper');
+    div.innerHTML = `
+  
+    <span class = 'productName'> ${productNew} </span>  
+  
+    <div class = 'price'> Unit Value $:
+    <span class = 'costUnit'> ${parseFloat(unitPriceNew).toFixed(2)} </span> 
+    </div>
+  
+    <label class = 'label'> QTY <input class = 'quantity' type="number" name="quantidade" value= 0 />  </label> 
+  
+    <div class = 'totaly'>Total $:
+    <span class = 'total'> 0.00 </span> 
+    </div>
+  
+    <button class = 'btn btn-delete' name = 'btn-delete'> Delete </button> `
+  
+    products.appendChild(div);
+  
+    update();
+
+  }
+
+
+
+}
+
+function totalPrice(event){
+
+  let unit = event.target.parentElement.parentElement.querySelector('.costUnit').innerText;
+  let quantity = event.target.value;
+  let total = event.target.parentElement.parentElement.querySelector('.total');
+  let acount = parseFloat(unit * quantity);
+  total.innerText = acount;
+
+  }
+
+function update (){
+
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var atualizatotal = document.getElementsByClassName('quantity');
+
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
+
+  for(let i = 0; i<atualizatotal.length; i+=1){
+    atualizatotal[i].onchange = totalPrice;
+  }
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
 
 }
 
@@ -42,9 +96,14 @@ window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
+  var atualizatotal = document.getElementsByClassName('quantity');
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
+
+  for(let i = 0; i<atualizatotal.length; i+=1){
+    atualizatotal[i].onchange = totalPrice;
+  }
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
