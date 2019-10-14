b66508d3748a32b8eba2c6c904c1abdbb095e070
Antoine BERNIER <antoine.bernier@gmail.com>
2019-10-14 14:46:31 +0200

    1st commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,1 +1,11 @@
-// Start here :)
+var $cart = document.querySelector('#cart tbody');
+var $calc = document.getElementById('calc');
+
+function updateSubtot($product) {
+  // Iteration 1.1
+}
+
+function calcAll() {
+  // Iteration 1.2
+}
+$calc.onclick = calcAll;
