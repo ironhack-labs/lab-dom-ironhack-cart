815f18447eab0f96f5c02762d246e4abca846b36
Luis Zarza <luiszarza@Luiss-MacBook-Pro.local>
2019-10-25 21:02:22 +0200

    Trying to finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,26 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+let $product = document.getElementsByClassName('product')
+let product = [...$product]
+console.log($product)
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let aux1 = $product.getElementsByTagName("span")[1]
+  let aux2 = $product.getElementsByTagName("input")[0]
+  let price = parseFloat(aux1.innerHTML)
+  let qty = parseFloat(aux2.value)
+  $product.getElementsByTagName('span')[2].innerHTML = price * qty
+  return(qty*price)
 }
 
 function calcAll() {
-  // Iteration 1.2
+  let total = 0
+  for (i = 0; i < product.length; i++) {
+   total += updateSubtot($product[i])
+  }
+  document.getElementById("total").innerHTML = total
 }
+
 $calc.onclick = calcAll;
+
+//= updateSubtot(product[i])
