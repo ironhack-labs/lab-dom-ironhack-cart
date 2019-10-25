7a237eac38f7ebeb85846c9fdd496f85e1230917
F-naranjo <francisco.naranjorodriguez@gmail.com>
2019-10-25 17:39:18 +0200

    variables qty and pu declared and defined

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,23 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let qty = document.querySelector('.qty input').getAttribute("value");
+  let pu = document.querySelector('.pu > span').innerText;
+  let subTotPrice = qty * pu ;
+  let quantity = "test";
+  //update subtotal
+  return subTotPrice
 }
 
 function calcAll() {
   // Iteration 1.2
 }
 $calc.onclick = calcAll;
+
+
+
+
+updateSubtot(product)
