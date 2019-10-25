bfc4b23994010ed8e2bb349bff87dc04f52f2133
Rebecca Wood <rebecca@Rebeccas-MacBook-Air.local>
2019-10-25 19:11:03 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,36 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let cart = document.querySelector('#cart tbody');
+let calc = document.getElementById('calc');
+console.log(calc)
+// Iteration 1.1
 
-function updateSubtot($product) {
-  // Iteration 1.1
+let product = document.getElementsByClassName("product")
+
+function updateSubtot(row) {
+  console.log(row)
+  let quantity1 = row.getElementsByTagName("input")[0].value
+  let quantity = row.getElementsByClassName("pu")[0].getElementsByTagName('span')[0].innerHTML
+  console.log(quantity1 * quantity)
+  console.log(quantity, quantity1)
+  return quantity1 * quantity
 }
+
+
+updateSubtot(product[0]);
+
+// let subtotal = subt.getElementsByClassName("subtot")[0].value
+
 
 function calcAll() {
-  // Iteration 1.2
+
+  let productCopy = [...product]
+
+  productCopy.forEach(element => {
+    updateSubtot(element)
+  });
+
+
 }
-$calc.onclick = calcAll;
+
+// Iteration 1.2
+
+calc.onclick = calcAll
