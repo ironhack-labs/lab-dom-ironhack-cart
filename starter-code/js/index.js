85b0e6df5c910813077c2f4eecb26338892f7b8e
eduardo <eduardo@DESKTOP-LPB6IGQ.localdomain>
2019-10-26 18:24:19 +0200

    carrito de ironHack a medio acabar y mal hecho

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,40 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+let ironBubbleHead = document.getElementById("ironBubble-head").innerHTML;
+
+let ironBubbleHead2 = document.getElementById("ironBubble-head2").innerHTML;
+
+let ironIndPrice = parseInt(document.getElementById("ironPrice").innerHTML);
+
+let ironIndPrice2 = parseInt(document.getElementById("ironPrice2").innerHTML);
+
+function updateSubtot() {
+
+  let total = 0;
+  let total2 = 0;
+  let absTotal = 0;
+
+  total = ironIndPrice * parseInt(document.getElementById("ironQuantity").value);
+
+  document.getElementById("ironSubtotal").innerHTML = total;
+
+  total2 = ironIndPrice2 * parseInt(document.getElementById("ironQuantity2").value);
+
+  document.getElementById("ironSubtotal2").innerHTML = total2;
+
+  absTotal = total + total2;
+  
+  document.getElementById("absoluteTotal").innerHTML = absTotal;
 }
 
 function calcAll() {
   // Iteration 1.2
 }
-$calc.onclick = calcAll;
+
+let button = document.getElementById("calc");
+
+button.onclick = function(btn){
+  btn.preventDefault();
+  updateSubtot();
+}
