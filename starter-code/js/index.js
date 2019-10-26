e42526f5ea2e64d235dbbb6e1b5c5c6e95285855
Alfredo del Canto <adelcanto@Alfredos-MBP.home>
2019-10-26 21:28:30 +0200

    Iteration-4 DONE

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,32 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let $price = parseInt($product.querySelector(".pu span").innerHTML);
+  let quantity = parseInt($product.querySelector(".qty input").value);
+  let subtotal = $price * quantity;
+  $product.querySelector(".subtot span").innerHTML = subtotal;
+  return subtotal
 }
 
 function calcAll() {
-  // Iteration 1.2
+  let productArr = [...document.querySelectorAll(".product")];
+  let totalPrice = 0;
+  productArr.forEach(function (element) {
+    totalPrice += updateSubtot(element);
+  });
+  // let sumOfSubs = updateSubtot(document.querySelectorAll(element));
+  document.querySelector("h2 span").innerHTML = totalPrice;
 }
+
 $calc.onclick = calcAll;
+
+function deleteProduct() {
+  btnArr = document.querySelectorAll(".rm .btn")
+  for (let i = 0; i < btnArr.length; i++) {
+    btnArr[i].onclick = function () {
+      btnArr[i].parentElement.parentElement.remove()
+    }
+  }
+}
+
+deleteProduct();
