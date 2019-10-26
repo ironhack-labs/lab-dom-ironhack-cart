671680cba2d42b9264dd34e84c4ffb53f7a114c3
Enrique Montaño Ocaña <enrique@Enriques-MacBook-Pro.local>
2019-10-26 23:57:32 +0200

    iteration 1, 2 & 3 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,27 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  
+  let unitPrice = $product.querySelector('.pu>span');
+  let unitQuantity = $product.querySelector('.qty input[type=number]').value;
+  
+  let subTotal = $product.querySelector('.subtot>span');
+  subTotal.innerText = parseFloat(unitPrice.innerText)*unitQuantity;
+
+  return parseFloat(subTotal.innerText);
 }
 
 function calcAll() {
-  // Iteration 1.2
+  
+  let productsList = document.querySelectorAll('.product');
+  let totalPrice = 0;
+  
+  for(let i = 0; i < productsList.length; i++) {
+    totalPrice += updateSubtot(productsList[i]);
+  }
+
+  console.log(totalPrice);
+  document.querySelector('h2 span').innerText = totalPrice;
 }
+
 $calc.onclick = calcAll;
