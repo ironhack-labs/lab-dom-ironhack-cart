b19ce173aff84f1b34e5eee3ec58ad31417e21af
Estefania del Castillo Medrano <ecasmed@gmail.com>
2019-10-26 21:31:54 +0200

    comit-1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,49 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
+let $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let subtotal = $product.querySelector(".subtot > span");
+  let precioUnitario = $product.querySelector(".pu > span");
+  let cantidad = $product.querySelector(".qty input[type = number]").value;
+  subtotal.innerText = parseFloat(precioUnitario.innerText) * cantidad;
+  return subtotal.innerText; //devuelve string
 }
 
 function calcAll() {
-  // Iteration 1.2
+  let listaProductos = document.querySelectorAll(".product");
+  listaProductos.forEach(updateSubtot);
+  console.log(listaProductos);
 }
+
 $calc.onclick = calcAll;
+
+function totalSum(){
+let totality = document.querySelectorAll(".subtot");
+}
+
+/*let myButton = document.getElementsByClassName("btn btn-success");
+button.onclick = function(btn){
+  btn.preventDefault();
+  updateSubtot(ironBubbleHead);
+  return subtot > spam * subtot > spam;
+ };
+ */
+//console.log(total);
+
+//var columnas=e.querySelectorAll(".subtot > spam");
+ 
+        // obtenemos los valores de la cantidad y importe
+        //var cantidad=parseFloat(columnas[3].);
+        //var importe=parseFloat(columnas[3].textContent);
+ 
+        // mostramos el total por fila
+        //columnas[3].textContent=(cantidad*importe).toFixed(2);
+ 
+        //total+=cantidad*importe;
+    //});
+ 
+    // mostramos la suma total
+    //var filas=document.querySelectorAll("#miTabla tfoot tr td");
+    //filas[1].textContent=total.toFixed(2);
+
+
