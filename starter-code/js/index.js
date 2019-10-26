c771cdd148c6b5b765787574ab78427f99f2f4de
root <root@DESKTOP-8FSC3E1.localdomain>
2019-10-26 19:54:30 +0200

    Delete button does not work yet, Add tomorrow

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,36 @@
+// Whaaaaaaaaaaattttttttttarethiiiiiiiiisssss
+
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
-}
+  let subtotal = $product.querySelector(`.subtot>span`);
+  let unitPrice = $product.querySelector(`.pu>span`);
+  let quantity = $product.querySelector(`.qty input[type=number]`).value;
+  subtotal.innerText=parseFloat(unitPrice.innerText)*quantity;
+  return parseFloat(subtotal.innerText);
+  }
+  
+  function calcAll() {
+  let productList= document.querySelectorAll(`.product`);
+  productList.forEach(updateSubtot);
+  console.log(productList)
+  }
+  $calc.onclick = calcAll;  
+  
+  myButton.onclick = function(btn){
+    btn.preventDefault();
+    calcAll();
+  }
 
-function calcAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calcAll;
+  function calcAll() {
+    let productList= document.querySelectorAll('.product');
+    let totalPrice=0;
+    for(let i=0; i<productList.length; i++){
+    totalPrice+=updateSubtot(productList[i])
+    }
+    console.log(totalPrice)
+    totalPrice=document.querySelector('h2 span').innerText = totalPrice;
+    }
+
+    // lo carga en la consola solo :(
