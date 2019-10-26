b0c71acd21a63a9b1aa2c95075f85eab00ce24db
javiview <javieragudo3d@gmail.com>
2019-10-26 23:15:00 +0200

    Cambio por QuerySelector, funcionabilidad con solo un elemento

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,22 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $product = document.querySelectorAll(".product")
 
-function updateSubtot($product) {
+function updateSubtot() {
   // Iteration 1.1
+  let priceUnit = document.querySelector(".pu span").innerText;
+  let quantity = document.querySelector(".qty input").value;
+  
+  let subTotal = priceUnit * quantity;
+  
+  return subTotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  let subTotal = updateSubtot();
+  let changeSubtot = document.querySelector(".subtot span");
+  changeSubtot.innerText = subTotal;
 }
 $calc.onclick = calcAll;
+ 
