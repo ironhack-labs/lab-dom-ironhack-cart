e17438a405aa673b4e51ba1ab9c9e2d5e5c00bfe
fnaranjo <francisco.naranjorodriguez@gmail.com>
2019-10-26 16:26:07 +0200

    Implemented subtotal, total and remove product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,23 +1,62 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
-var $
+var $product = document.querySelector('.product');
+let $products = document.querySelectorAll('.product');
+let $deleteButtons = document.querySelectorAll('.btn-delete');
+let table = document.querySelector('tbody');
+// let $deleteButton = document.querySelector('.btn-delete');
 
-function updateSubtot($product) {
+$products.forEach(function(e){
+  updateSubtot(e)
+  e.querySelector('input').addEventListener('change',function(){
+  updateSubtot(e);
+});
+})
+
+function updateSubtot(product) {
   // Iteration 1.1
-  let qty = document.querySelector('.qty input').getAttribute("value");
-  let pu = document.querySelector('.pu > span').innerText;
-  let subTotPrice = qty * pu ;
-  let quantity = "test";
+  let quantity = product.querySelector('input').value;
+  let unitPrice = product.querySelector('.pu span').innerText;
+  let subTotPriceNode = product.querySelector('.subtot span');
+  let subTotPrice = quantity * unitPrice;
   //update subtotal
+  console.log("llamada a subtotal ok");
+  subTotPriceNode.innerHTML = subTotPrice;
   return subTotPrice
 }
 
-function calcAll() {
-  // Iteration 1.2
+  function calcAll() {
+    let totalNode = document.querySelector('h2 span')
+    let allSubTotals = document.querySelectorAll('.product');
+    let totalCart = 0;
+    allSubTotals.forEach(subtotal => {
+            updateSubtot(subtotal); 
+            totalCart += updateSubtot(subtotal)
+    });
+
+    totalNode.innerHTML = totalCart;
+  }
+
+  $calc.onclick = function(e){
+    e.preventDefault()
+    calcAll();
+  }
+
+function deleteItem($deleteButton){
+  // console.log($deleteButton.parentNode.parentNode.rowIndex)
+      // productToRemove.parentNode.deleteRow(productToRemove.rowIndex);
+      let productRow = $deleteButton.parentNode.parentNode.rowIndex -1;
+      table.deleteRow(productRow);
 }
-$calc.onclick = calcAll;
+
+  $deleteButtons.forEach(function(d){
+    d.onclick = function(e){
+    e.preventDefault()
+    deleteItem(d);
+    }
+    })
 
 
 
-
-updateSubtot(product)
+  
+  
