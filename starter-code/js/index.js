50a0fc5b9870fe7eb0c5e4937ce0b11acd7bed69
fnaranjo <francisco.naranjorodriguez@gmail.com>
2019-10-26 19:32:43 +0200

    Cart Full de mango

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,59 +4,95 @@ var $product = document.querySelector('.product');
 let $products = document.querySelectorAll('.product');
 let $deleteButtons = document.querySelectorAll('.btn-delete');
 let table = document.querySelector('tbody');
-// let $deleteButton = document.querySelector('.btn-delete');
+let createButton = document.getElementById('create');
+let numberOfCells = document.querySelector('tr').cells.length;
 
-$products.forEach(function(e){
-  updateSubtot(e)
-  e.querySelector('input').addEventListener('change',function(){
-  updateSubtot(e);
-});
-})
+table.addEventListener('DOMNodeInserted', loadButtons);
+table.addEventListener('DOMNodeInserted', instaCalc);
+
+function createProduct() {
+  let nameOfProduct = document.querySelector('.new input[type="text"').value;
+  let priceOfProduct = document.querySelector('.new input[type="number"').value;
+  let lastIndexOfTable = table.rows.length
+  let whereToCells = table.rows[lastIndexOfTable - 1];
+
+  table.insertRow(lastIndexOfTable).setAttribute('class','product');
+
+  for (i = 1; i <= numberOfCells; i++) {
+    table.rows[lastIndexOfTable].insertCell(whereToCells);
+  }
+
+  table.rows[lastIndexOfTable].cells[0].setAttribute('class', 'name');
+  table.rows[lastIndexOfTable].cells[1].setAttribute('class', 'pu');
+  table.rows[lastIndexOfTable].cells[2].setAttribute('class', 'qty');
+  table.rows[lastIndexOfTable].cells[3].setAttribute('class', 'subtot');
+  table.rows[lastIndexOfTable].cells[4].setAttribute('class', 'rm');
+
+  table.rows[lastIndexOfTable].cells[0].innerHTML = ` <span>${nameOfProduct}</span>`;
+  table.rows[lastIndexOfTable].cells[1].innerHTML = ` $<span>${priceOfProduct}</span>`;
+  table.rows[lastIndexOfTable].cells[2].innerHTML = ` <label><input type="number" value="1" min="0"></label>`;
+  table.rows[lastIndexOfTable].cells[3].innerHTML = ` $<span>0</span>`;
+  table.rows[lastIndexOfTable].cells[4].innerHTML = ` <button class="btn btn-delete">Delete</button>`;
+
+}
+
+function deleteItem($deleteButton) {
+  // console.log($deleteButton.parentNode.parentNode.rowIndex)
+  // productToRemove.parentNode.deleteRow(productToRemove.rowIndex);
+  let productRow = $deleteButton.parentNode.parentNode.rowIndex - 1;
+  table.deleteRow(productRow);
+  console.log("se ha pulsado")
+}
 
 function updateSubtot(product) {
-  // Iteration 1.1
   let quantity = product.querySelector('input').value;
   let unitPrice = product.querySelector('.pu span').innerText;
   let subTotPriceNode = product.querySelector('.subtot span');
   let subTotPrice = quantity * unitPrice;
-  //update subtotal
-  console.log("llamada a subtotal ok");
   subTotPriceNode.innerHTML = subTotPrice;
   return subTotPrice
 }
 
-  function calcAll() {
-    let totalNode = document.querySelector('h2 span')
-    let allSubTotals = document.querySelectorAll('.product');
-    let totalCart = 0;
-    allSubTotals.forEach(subtotal => {
-            updateSubtot(subtotal); 
-            totalCart += updateSubtot(subtotal)
-    });
-
-    totalNode.innerHTML = totalCart;
-  }
-
-  $calc.onclick = function(e){
-    e.preventDefault()
-    calcAll();
-  }
-
-function deleteItem($deleteButton){
-  // console.log($deleteButton.parentNode.parentNode.rowIndex)
-      // productToRemove.parentNode.deleteRow(productToRemove.rowIndex);
-      let productRow = $deleteButton.parentNode.parentNode.rowIndex -1;
-      table.deleteRow(productRow);
+function calcAll() {
+  let totalNode = document.querySelector('h2 span')
+  let allSubTotals = document.querySelectorAll('.product');
+  let totalCart = 0;
+  allSubTotals.forEach(subtotal => {
+    updateSubtot(subtotal);
+    totalCart += updateSubtot(subtotal)
+  });
+  totalNode.innerHTML = totalCart;
 }
 
-  $deleteButtons.forEach(function(d){
-    d.onclick = function(e){
+$calc.onclick = function (e) {
+  e.preventDefault()
+  calcAll();
+}
+
+createButton.onclick = function (e) {
+  e.preventDefault()
+  createProduct();
+}
+
+function loadButtons(){
+  $deleteButtons = document.querySelectorAll('.btn-delete');
+  
+  $deleteButtons.forEach(function (d) {
+  d.onclick = function (e) {
     e.preventDefault()
     deleteItem(d);
-    }
-    })
+  }
+})
+}
 
+function instaCalc(){
+  let $products = document.querySelectorAll('.product');
+  $products.forEach(function (e) {
+    e.querySelector('input').addEventListener('change', function () {
+      updateSubtot(e);
+    });
+  })
+}
 
-
-  
-  
+loadButtons();
+instaCalc();
