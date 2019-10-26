017b89ad9be7431dd2ad5037f55de19ed53ae7e5
Fran <fmjerez@icloud.com>
2019-10-26 18:49:04 +0200

    First cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,30 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+//document.body.style.background='red';
+
+
+
+
 function updateSubtot($product) {
-  // Iteration 1.1
+  //Metemos en la variable Subtotal, el valor que hay en el valor que le pasamos ($product)
+ let subtotal = $product.querySelector('.subtot>span');
+ let precioUnitario = $product.querySelector('.pu>span');
+ let cantidad = $product.querySelector('.qty input[type=number]').value;
+ 
+
+ subtotal.innerText=parseInt(precioUnitario.innerText)*cantidad;
+
+ return subtotal.innerText;//devuelve string Importante!!!!
 }
 
 function calcAll() {
-  // Iteration 1.2
+let listaProductos= document.querySelectorAll('.product');
+listaProductos.forEach(updateSubtot);
+
 }
+
 $calc.onclick = calcAll;
+
+
+
