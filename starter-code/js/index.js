dae1db62575fc458aa373a4d67c22334040d22e9
Flor <florcitaurbinati@hotmail.com>
2019-10-27 08:23:35 +0100

    iteration 1 and 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,27 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+let products = document.querySelectorAll(".product");
+let totalPrice = document.querySelector("h2 span");
+let individualPrices = [];
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let price = $product.querySelector(".pu span");
+  let quantity = $product.querySelector("input[type=number]").value;
+  let subtotal = $product.querySelector(".subtot span");
+  subtotal.innerText = parseInt(price.innerText) * quantity;
+  let number = $product.querySelector(".subtot span").innerText;
+  individualPrices.push(parseInt(number));
+  return subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  products.forEach(updateSubtot);
+  let totalCart = individualPrices.reduce(function (a, b) {
+    return a + b;
+  })
+  totalPrice.innerText = totalCart;
 }
+
 $calc.onclick = calcAll;
