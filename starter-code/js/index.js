ecb65dce5188d68f85862ae1fe1e7401486b4824
javiview <javieragudo3d@gmail.com>
2019-10-27 15:57:11 +0100

    Iteration 3 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -5,23 +5,7 @@ let $total = document.querySelector("#total").innerText;
 
 let $prueba = document.querySelectorAll(".product .pu > span");
 
-// function updateSubtot ($product) {
-//   // Iteration 1.1
-//   let priceUnit = $product.querySelector(".pu span").innerText;
-//   let quantity = $product.querySelector(".qty input").value;
-  
-//   let subTotal = priceUnit * quantity;
-  
-//   return subTotal;
-// }
-
-// function calcAll() {
-//   // Iteration 1.2
-//   let subTotal = updateSubtot($product);
-//   let changeSubtot = document.querySelector(".subtot span");
-//   changeSubtot.innerText = subTotal;
-  
-// }
+// Esta es la idea de donde tengo la Iteration 1 y 2 aprox
 // for (let i = 0; i < $product.length; i++) {
 //   let uP = $product[i].querySelector(".pu span").innerText;
 //   let q = $product[i].querySelector(".qty input").value;
@@ -45,11 +29,20 @@ function updateSubtot ($product) {
 
 function calcAll() {
   // Iteration 1.2
+  //Actualiza SubTotal de cada Producto.
   let subTotal = updateSubtot($product);
   for (let i = 0; i < subTotal.length; i++){    
   let changeSubtot = $product[i].querySelector(".subtot span");
   changeSubtot.innerText = subTotal[i];
   }
+  //Suma y Actualiza el Total.
+  let total = 0;
+  let totalNum = document.querySelectorAll(".product .subtot > span");
+  for (let i = 0; i < totalNum.length; i++) {
+  total += parseInt(totalNum[i].textContent);
+}
+
+document.querySelector("#total").innerText = total;
 } 
 $calc.onclick = calcAll;
 
