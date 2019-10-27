c52b41ea278fbcd7247f38ab7d1f16dd5e0e9382
eduardo <eduardo@DESKTOP-LPB6IGQ.localdomain>
2019-10-27 13:38:32 +0100

    con botón delete mejorado

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 let tbody = document.querySelector("tbody")
-let elements = document.querySelectorAll("tbody > tr")
+let elements = document.querySelectorAll("tbody > tr");
 let pu = document.querySelectorAll(".pu > span");
 let qty = document.querySelectorAll(".qty > label > input");
 let subtot = document.querySelectorAll(".subtot > span");
@@ -7,6 +7,8 @@ let del_button = document.querySelectorAll(".btn-delete");
 let calc = document.getElementById('calc');
 let total = 0;
 let subtotalList = [];
+
+console.log(subtot[0].innerHTML);
 
 function updateSubtot($product) {
     total = 0;
@@ -29,7 +31,20 @@ function delete_element() {
     for(let i=0; i<del_button.length; i++){
         del_button[i].onclick = function(btn){
             btn.preventDefault();
-            tbody.removeChild(elements[i]);
+            tbody.removeChild(elements[i]);  
+            elements = document.querySelectorAll("tbody > tr");
+            pu = document.querySelectorAll(".pu > span");
+            qty = document.querySelectorAll(".qty > label > input");
+            subtot = document.querySelectorAll(".subtot > span");
+            del_button = document.querySelectorAll(".btn-delete");
+            total = 0;
+            let listForSubtotal = []
+            for(let j=0; j<subtot.length; j++){
+                number = parseInt(subtot[j].innerHTML);
+                listForSubtotal.push(number);
+            }
+            subtotalList = listForSubtotal;
+            calcAll();
         }
     }
 }
