26f5534f99f2d8a9b0877a40a04eb636258c4fc3
eduardo <eduardo@DESKTOP-LPB6IGQ.localdomain>
2019-10-27 02:33:29 +0100

    código con querySelector

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,37 +1,30 @@
-let ironBubbleHead = document.getElementById("ironBubble-head").innerHTML;
+let element = document.querySelectorAll("tbody > tr")
+let pu = document.querySelectorAll(".pu > span");
+let qty = document.querySelectorAll(".qty > label > input");
+let subtot = document.querySelectorAll(".subtot > span");
+let calc = document.getElementById('calc');
+let total = 0;
+let subtotalList = [];
 
-let ironBubbleHead2 = document.getElementById("ironBubble-head2").innerHTML;
-
-let ironIndPrice = parseInt(document.getElementById("ironPrice").innerHTML);
-
-let ironIndPrice2 = parseInt(document.getElementById("ironPrice2").innerHTML);
-
-function updateSubtot() {
-
-  let total = 0;
-  let total2 = 0;
-  let absTotal = 0;
-
-  total = ironIndPrice * parseInt(document.getElementById("ironQuantity").value);
-
-  document.getElementById("ironSubtotal").innerHTML = total;
-
-  total2 = ironIndPrice2 * parseInt(document.getElementById("ironQuantity2").value);
-
-  document.getElementById("ironSubtotal2").innerHTML = total2;
-
-  absTotal = total + total2;
-
-  document.getElementById("absoluteTotal").innerHTML = absTotal;
+function updateSubtot($product) {
+    total = 0;s
+    subtotalList = [];
+    for(let i=0; i<$product.length; i++){
+        subtotal = pu[i].innerHTML * qty[i].value;
+        subtot[i].innerHTML = subtotal;
+        subtotalList.push(subtotal);
+    }
 }
 
 function calcAll() {
-  // Iteration 1.2
+    for(let i=0; i<subtotalList.length; i++){
+        total += subtotalList[i];
+    }
+    document.getElementById("totalResult").innerHTML = total;
 }
 
-let button = document.getElementById("calc");
-
-button.onclick = function(btn){
-  btn.preventDefault();
-  updateSubtot();
+calc.onclick = function(btn){
+    btn.preventDefault();
+    updateSubtot(element);
+    calcAll();
 }
