43fd9531cc6af13d2d4aba97baf119503c979590
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-10-27 11:57:11 +0100

    Lab complete, all iterations done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,124 @@
-function deleteItem(e){
+var productName = document.querySelector("#productName>span");
 
-}
+var unitPrice = document.querySelector(".unitPrice>span");
 
-function getPriceByProduct(itemNode){
+var vraperKing = document.querySelector(".wrapperKing");
 
-}
+var eachRow = document.querySelectorAll(".wrapper");
 
-function updatePriceByProduct(productPrice, index){
+var totalPrice = document.querySelector("h2>span");
 
-}
+var calcPricesButton = document.querySelector("#calcPrices>.btn");
 
-function getTotalPrice() {
+var input = document.querySelector("input");
 
-}
+var inputNewName = document.querySelector(".productName input");
 
-function createQuantityInput(){
+var btnCreate = document.querySelector(".btn-create");
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+var addClassName = function(element, name) {
+  element.className = name;
 };
+
+
+function calcPriceForEachRow(rows) {
+  var totalPriceValue = 0;
+
+  rows.forEach(function(row) {
+    var productPriceNumber = 0;
+    var totalPriceRow = row.querySelector(".totalPrice>span");
+    var unitPrice = row.querySelector(".unitPrice span");
+    var totalPricePerItem = row.querySelector(".totalPrice>span");
+    var inputNumber = Number(row.querySelector("input").value);
+
+    productPriceNumber += Number(unitPrice.textContent);
+
+    function getPriceByProduct(element) {
+      totalPricePerItem.textContent = `${inputNumber * element}.00`;
+    }
+    
+    getPriceByProduct(productPriceNumber);
+
+    totalPriceValue += Number(totalPriceRow.textContent);
+  });
+  totalPrice.textContent = `${totalPriceValue}.00`;
+}
+
+function totalPriceValue(rows) {
+  rows.forEach(function(row) {
+    var totalPrice = 0;
+    var totalPriceRow = row.querySelector(".totalPrice>span");
+    totalPrice += Number(totalPriceRow.textContent);
+  });
+}
+
+function createRow() {
+  var newDiv = document.createElement("div");
+
+  var inputNewPrice = document.querySelector(".unitPrice input");
+
+  function createDiv(element) {
+    newDiv;
+    element.appendChild(newDiv);
+    return newDiv;
+  }
+
+  function createRowBody() {
+    var wrapper = createDiv(wrapperKing);
+    addClassName(wrapper, "wrapper");
+    wrapper.innerHTML = `
+  
+  <div class="productName">
+    <span>${inputNewName.value}</span>
+  </div>
+  
+  <div class="unitPrice">
+    $<span>${inputNewPrice.value}</span>
+  </div>
+
+  <div class="quantity">
+    <label for="">QTY</label>
+    <input type="number" name="" value="1">
+  </div>
+
+    <div class="totalPrice">
+      $<span>0.00</span>
+    </div>
+
+    <div>
+    <button class="btn btn-delete">
+      Delete
+    </button>
+  </div>
+  `;
+  }
+  createRowBody();
+};
+
+function selectDeleteButton(eachrow) {
+  eachrow.forEach(function(row) {
+    var deleteBtn = row.querySelector(".btn-delete");
+    function deleteRow() {
+      row.remove(row);
+    }
+    deleteBtn.onclick = deleteRow;
+  });
+}
+
+function createResponsiveRow() {
+  createRow();
+  var eachRow = document.querySelectorAll(".wrapper");
+  selectDeleteButton(eachRow);
+  totalPriceValue(eachRow);
+}
+
+function calcPriceForEachRowResponsive () {
+  var eachRow = document.querySelectorAll(".wrapper");
+  calcPriceForEachRow(eachRow);
+}
+
+selectDeleteButton(eachRow);
+
+btnCreate.onclick = createResponsiveRow;
+
+calcPricesButton.onclick = calcPriceForEachRowResponsive;
