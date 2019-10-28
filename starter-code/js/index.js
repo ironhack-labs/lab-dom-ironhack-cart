42236009941736b054360608383f87fdc22948d5
JorgeArevalo24 <56602457+JorgeArevalo24@users.noreply.github.com>
2019-10-28 10:37:33 +0100

    MAD PP

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,34 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  // Iteration 1.
+
+  let price = $product.querySelector("#put").innerHTML
+
+  let quanty = $product.querySelector("#prueba").value
+
+  return price * quanty
 }
 
+
+// let button = document.getElementById("calc")
+
+// console.log(button)
+// button.onclick = function () {
+
+//   let input = document.getElementsByTagName('input')
+//   console.log(input)
+
+
+// }
+
 function calcAll() {
-  // Iteration 1.2
+  document.querySelectorAll(".product").forEach(product => {
+    let priceProduct = updateSubtot(product)
+    product.querySelector("#subtot").innerHTML = priceProduct
+
+  })
+
+
 }
 $calc.onclick = calcAll;
