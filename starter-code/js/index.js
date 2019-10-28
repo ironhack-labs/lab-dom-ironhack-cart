56b39e4aa37a47a484de1108d1468e59f4ea3aa4
Ninoska Cairos <ninocairos@NinoCairos.home>
2019-10-28 07:51:01 +0100

    ironhack cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,24 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var $cart = document.querySelector("#cart tbody");
+var $calc = document.getElementById("calc");
+let $product = document.getElementsByClassName("product");
+let product = [...$product];
+console.log(product);
 
-function updateSubtot($product) {
+function updateSubtot(product) {
   // Iteration 1.1
+  let price = product.getElementsByTagName("span")[1].innerHTML;
+  let quantity = product.getElementsByTagName("input")[0].value;
+
+  return price * quantity;
 }
 
 function calcAll() {
   // Iteration 1.2
+  for (let i = 0; i < product.length; i++) {
+    product[i].getElementsByTagName("span")[2].innerHTML = updateSubtot(
+      product[i]
+    );
+    updateSubtot(product[i]);
+  }
 }
 $calc.onclick = calcAll;
