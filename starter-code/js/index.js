01ea12b20c152bbaf5f0eac63df904b7f9710c64
Jaxaver <jaxaver@LAPTOP-HG48MLL7.localdomain>
2019-10-28 10:47:40 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,50 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+
+//const prices = document.getElementsByClassName("pu")
+//
+//const arrayPu = [...prices];
+//const price = arrayPu[0].children[0];
+//
+const subT = document.getElementsByClassName("subTot");
+const arraySubTot = [...subT]
+const prod = document.getElementsByClassName("product");
+
+
+
+// console.log(num)
+
+
+
+
+
 function updateSubtot($product) {
-  // Iteration 1.1
+
+  let price = $product.querySelector(".pu");
+  let quantity = $product.querySelector(".qty");
+  let sub = price * quantity;
+
+  //arraySubTot.append(sub)
+  //arraySubTot.push(sub)
+
+  sub = subT.innerHTML;
+
+
 }
 
+
+console.log(prod);
+
+const arrayTot = [...prod]
+
+
 function calcAll() {
-  // Iteration 1.2
+  arrayTot.forEach(function updateSubtot($product) => {
+    return sub;
+  });
+
+  console.log(prod)
+  updateSubtot(prod)
 }
 $calc.onclick = calcAll;
