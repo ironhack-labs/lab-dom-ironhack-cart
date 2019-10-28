0bba3510696beb0f8ec0a3e86021e3392f79d2a8
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-10-28 08:28:57 +0100

    almost done, can't update price and quantity with the newItem instead of it has the same value of the previous div

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,23 +1,68 @@
-var containers = document.querySelectorAll("#div-container");
+// Settings for the functions
+var masterContainer = document.getElementById("master-container-div");
+var containers = document.querySelectorAll(".div-container");
 var productName = document.querySelectorAll(".product-name");
 var productPrice = Number(
   document.querySelectorAll(".product-price").textContent
 );
 var quantity = document.querySelectorAll(".quantity");
 var totalPrice = document.querySelectorAll(".total-price");
+var newProductName = document.querySelector(".new-product").textContent;
+var newProductPrice = Number(document.querySelector(".new-product-price"));
+var createNewItem = document.querySelector("#new-item-create");
 
-// console.log(containers);
+function wrapper() {
+  // var masterContainer = document.getElementById("master-container-div");
+  deleteItem(masterContainer);
+  getPriceByProduct(masterContainer);
+  getTotalPrice(masterContainer);
+  createItem(masterContainer);
+}
 
+createNewItem.addEventListener("click", wrapper);
+
+// delete Item from the button
+// newItem
+function createItem(masterContainer) {
+  var newItem = document.createElement("div");
+  newItem.className = "div-container";
+  masterContainer.append(newItem);
+  var newProductName = document.querySelector(".new-product").value;
+  console.log(newProductName);
+  var newProductPrice = document.querySelector(".new-product-price").value;
+  newItem.innerHTML += `<div>
+      <span class="product-name">${newProductName}</span>
+    </div>
+    <div>$<span class="product-price">${newProductPrice}</span></div>
+    <div>
+      <label for="quantity" class="qty">QTY</label>
+      <input
+        type="number"
+        name="quantity"
+        min="0"
+        value="0"
+        class="quantity"
+      />
+    </div>
+    <div><span class="total-price">$0</span></div>
+    <div><button class="btn btn-delete">Delete</button></div>`;
+  getPriceByProduct();
+  getTotalPrice();
+  deleteItem();
+}
+// delete Item from the button
 function deleteItem() {
   containers.forEach(function(container) {
-    var deleteButton = container.querySelector(".btn-delete");
-    deleteButton.addEventListener("click", function() {
-      container.innerHTML = "";
+    var deleteButtons = container.querySelector(".btn-delete");
+    deleteButtons.addEventListener("click", function() {
+      container.remove(container);
     });
   });
 }
+// call the function
 deleteItem();
 
+// print the price for each item
 function getPriceByProduct() {
   containers.forEach(function(container) {
     var productPrice = container.querySelector(".product-price").textContent;
@@ -28,6 +73,7 @@ function getPriceByProduct() {
 }
 getPriceByProduct();
 
+// print the total price
 function getTotalPrice() {
   getPriceByProduct();
   var totalByProduct = 0;
@@ -39,31 +85,18 @@ function getTotalPrice() {
     document.querySelector(".total").innerHTML = `$ ${Number(totalByProduct)}`;
   });
 }
-
 getTotalPrice();
-
-function createQuantityInput() {}
-
-function createDeleteButton() {}
-
-function createQuantityNode() {}
-
-function createItemNode(dataType, itemData) {}
-
-function createNewItemRow(itemName, itemUnitPrice) {}
-
-function createNewItem() {}
 
 window.onload = function() {
   var calculatePriceButton = document.getElementById("calc-prices-button");
-  //var createItemButton = document.getElementById("new-item-create");
+  var createItemButton = document.querySelector("#new-item-create");
   var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
-  //createItemButton.onclick = createNewItem;
+  createItemButton.onclick = createNewItem;
 
-  // for (var i = 0; i < deleteButtons.length; i++) {
-  //   deleteButtons[i].onclick = deleteItem;
-  // }
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = deleteItem;
+  }
 };
 window.onload();
