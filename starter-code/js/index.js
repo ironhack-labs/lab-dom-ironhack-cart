5c6a04b6ea9f4e7f233f44acfe052a3e1a730721
Julian Phillips <julian.p@me.com>
2019-10-29 09:24:53 -0400

    Completion of iteration 2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,19 +1,29 @@
 let calcButton = document.getElementById('calc');
 let cart = document.getElementsByClassName('product');
 
-function updateSubtot(product) {
-    // Iteration 1.1
-}
+// Iteration 1
+// calcButton.onclick = () => {
+//     let unitPrice = Number(document.querySelector('.pu > span').innerText);
+//     let quantity = Number(document.querySelector('.qty > label > input').value);
+//     let subToteSpan = document.querySelector('.subtot > span');
 
-function calcAll() {
-    // Iteration 1.2
-}
+//     subToteSpan.innerText = quantity * unitPrice;
+//     console.log(quantity * unitPrice);
+// };
 
+// Iteration 2
 calcButton.onclick = () => {
-    let unitPrice = Number(document.querySelector('.pu > span').innerText);
-    let quantity = Number(document.querySelector('.qty > label > input').value);
-    let subToteSpan = document.querySelector('.subtot > span');
+    let rows = document.getElementsByClassName('product');
 
-    subToteSpan.innerText = quantity * unitPrice;
-    console.log(quantity * unitPrice);
+    // You can't actually for in or forEach on the returned value of what is pulled from HTML
+    for (let i = 0; i < rows.length; i++) {
+        let unitPrice = Number(rows[i].querySelector('.pu > span').innerText);
+        let quantity = Number(
+            rows[i].querySelector('.qty > label > input').value,
+        );
+        let subToteSpan = rows[i].querySelector('.subtot > span');
+
+        subToteSpan.innerText = quantity * unitPrice;
+        console.log(quantity * unitPrice);
+    }
 };
