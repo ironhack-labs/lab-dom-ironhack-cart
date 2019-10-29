5006b0405f3e7886f7d0144a0908a1e6e0f7b7db
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-29 09:39:53 -0400

    update js

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,35 +1,66 @@
-// iteration 1
-var calcButton = document.getElementById('calc')
+var calcButton = document.getElementById('calc');
 
-calcButton.onclick = function(){
-  
-  let unitPrice = Number(document.querySelector(".pu > span").innerText);
-  
-  let quantity = Number(document.querySelector(".qty > label > input").value);
-  
-  
-  let subtoteSpan = document.querySelector(".subtot > span").innerText
-  
-  subtoteSpan.innerText = quantity * unitPrice
-}
+// iteration 1 version
+// calcButton.onclick = function(){
 
-// iteration 2 
-calcButton.onclick = function(){
-  let rows = document.getElementsByClassName('product')
-  console.log(rows)
+//   let unitPrice = Number(document.querySelector('.pu > span').innerText);
+//   // everything is always a String if you grab it through the DOM so we have to coerce it into a number
 
-  for(let i = 0; rows.length; i++){
+
+//   let quantity = Number(document.querySelector('.qty > label > input').value);
+
+
+//   let sutToteSpan = document.querySelector('.subtot > span');
+
+//   sutToteSpan.innerText = quantity * unitPrice;
+
+// }
+
+
+
+// iteration 2 version
+calcButton.onclick = function () {
+
+  let rows = document.getElementsByClassName('product');
+
+  console.log(rows);
+
+  for (let i = 0; i < rows.length; i++) {
+
     console.log(rows[i]);
 
-    let unitPrice = Number(rows[i].querySelector(".pu > span").innerText);
+    let unitPrice = Number(rows[i].querySelector('.pu > span').innerText);
+    // everything is always a String if you grab it through the DOM so we have to coerce it into a number
 
-    let quantity = Number(rows[i].querySelector(".qty > label > input").value);
+
+    let quantity = Number(rows[i].querySelector('.qty > label > input').value);
 
 
-    let subtoteSpan = rows[i].querySelector(".subtot > span").innerText
+    let sutToteSpan = rows[i].querySelector('.subtot > span');
 
-    subtoteSpan.innerText = quantity * unitPrice
+    sutToteSpan.innerText = quantity * unitPrice;
+
   }
+
+
+
+
+
+
+
+
+
 }
 
-document.querySelector('.btn-delete')
+
+
+
+
+
+
+
+
+document.querySelector('.btn-delete').onclick = function (e) {
+
+  console.log(e.currentTarget.parentElement.parentElement);
+}
