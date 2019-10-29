342ca2a5d03dca145d57dfb7e97d34b3f9d217fa
jpcervera6 <jpcervera6@gmail.com>
2019-10-29 10:41:33 -0400

    final class version

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,37 +1,110 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var calcButton = document.getElementById('calc');
 
-//updateSubtot.onclick = function ($product) {
+// iteration 1 version
+// calcButton.onclick = function(){
+
+//   let unitPrice = Number(document.querySelector('.pu > span').innerText);
+//   // everything is always a String if you grab it through the DOM so we have to coerce it into a number
+
+
+//   let quantity = Number(document.querySelector('.qty > label > input').value);
+
   
-//};
+//   let sutToteSpan = document.querySelector('.subtot > span');
 
-function calcAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calcAll;
+//   sutToteSpan.innerText = quantity * unitPrice;
+
+// }
 
 
-function updateText() {
-  var x = document.querySelector("#cart > tbody > tr:nth-child(1) > td.qty > label > input[type=number]").value;
-  document.querySelector("#cart > tbody > tr:nth-child(1) > td.qty > label > input[type=number]").value = x;
-  //console.log(document.querySelector("#cart > tbody > tr:nth-child(1) > td.qty > label > input[type=number]").value);
-  var y = document.querySelector("#cart > tbody > tr:nth-child(1) > td.subtot > span");
-  y.innerHTML = Number(document.querySelector("#cart > tbody > tr:nth-child(1) > td.pu > span").innerHTML)*document.querySelector("#cart > tbody > tr:nth-child(1) > td.qty > label > input[type=number]").value;
 
-    }
+// iteration 2 version
+calcButton.onclick = function(){
+  let theTotal = 0;
 
-    function updateText1() {
-      var w = document.querySelector("#cart > tbody > tr:nth-child(2) > td.qty > label > input").value;
-      document.querySelector("#cart > tbody > tr:nth-child(2) > td.qty > label > input").value= w;
-      //console.log(document.querySelector("#cart > tbody > tr:nth-child(2) > td.qty > label > input").value);
-      var z = document.querySelector("#cart > tbody > tr:nth-child(2) > td:nth-child(4) > span");
-      z.innerHTML = Number(document.querySelector("#cart > tbody > tr:nth-child(2) > td.pu > span").innerHTML)*document.querySelector("#cart > tbody > tr:nth-child(2) > td.qty > label > input").value;
-    
-        }
+  let rows = document.getElementsByClassName('product');
 
+  //console.log(rows);
+
+ for(let i = 0; i < rows.length; i++){
+
+  //console.log(rows[i]);
+
+   let unitPrice = Number(rows[i].querySelector('.pu > span').innerText);
+  // everything is always a String if you grab it through the DOM so we have to coerce it into a number
+
+
+  let quantity = Number(rows[i].querySelector('.qty > label > input').value);
+  let productTotal = Number(quantity*unitPrice);
+
+  
+  let sutToteSpan = rows[i].querySelector('.subtot > span');
+
+  sutToteSpan.innerText = productTotal;
+
+  theTotal += productTotal;
+
+  document.querySelector('h2 span').innerText = theTotal;
+  
+  
+ }
+ 
  
 
-  
+}
 
 
 
+let deleteBtns = document.getElementsByClassName('btn-delete');
+
+for(let i=0; i< deleteBtns.length; i++){
+    deleteBtns[i].onclick = deleteTheRow;
+}
+
+function deleteTheRow(event){
+  event.currentTarget.parentElement.parentElement.remove();
+}
+
+document.getElementById('create').onclick = function(){
+
+  let newName = document.querySelector('.new > td > input[type="text"]').value;
+
+  let newPrice = document.querySelector('.new > td > input[type="number"]').value;
+
+ 
+let newRowInnerHtml = `
+<td class="name">
+  <span>${newName}</span>
+</td>
+<td class="pu">
+  $<span>${newPrice}</span>
+</td>
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0">
+  </label>
+</td>
+<td class="subtot">
+  $<span>0</span>
+</td>
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>
+`
+
+let newRow = document.createElement('tr');
+newRow.classList.add('product');
+
+newRow.innerHTML = newRowInnerHtml;
+
+newRow.querySelector('.btn-delete').onclick = deleteTheRow;
+
+document.querySelector('tbody').appendChild(newRow);
+
+
+
+ document.querySelector('.new > td > input[type="text"]').value = "";
+
+document.querySelector('.new > td > input[type="number"]').value = "";
+
+}
