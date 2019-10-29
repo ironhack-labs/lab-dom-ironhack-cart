ed46f0ed243f6f7f21cbac26d46e7a550ab6385d
Julian Phillips <julian.p@me.com>
2019-10-29 00:50:17 -0400

    Cleared prior work, reapproaching problem

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,13 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+function updateSubtot(product) {
+    // Iteration 1.1
 }
 
 function calcAll() {
-  // Iteration 1.2
+    // Iteration 1.2
 }
-$calc.onclick = calcAll;
+
+calc.onclick = () => {
+    console.log('Just got clicked');
+};
