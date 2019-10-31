0ebb1a255d8dd0168cdcb87166c2413b1eff50c7
EChColorado <edxz7c@gmail.com>
2019-10-31 00:34:59 -0600

    Done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,13 +2,11 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 // ---------------------------------------
 let $prod = document.querySelector('.product');
-
-
-// let inputListener = document.querySelector(".qty input")
+let createBtn = document.querySelector('#create')
 
 function updateSubtot({target}) {
   // Iteration 1.1
-  let price = parseFloat($prod.querySelector(".pu span").innerText)
+  let price = parseFloat(target.parentNode.parentNode.parentNode.querySelector(".pu span").innerText)
   let subTot = isNaN(parseInt(target.value)) ? 0 : price*parseInt(target.value)
   target.parentNode.parentNode.parentNode.querySelector(".subtot span").innerText = subTot
   return subTot;
@@ -24,49 +22,28 @@ function calcAll() {
   return sum;
 }
 
+//Add product
+$cart.addEventListener('keyup',updateSubtot)
+function create({target}){
+  let product = target.parentNode.parentNode.querySelector('input[type="text"]')
+  let price = target.parentNode.parentNode.querySelector('input[type="number"]')
+  if(product.value!=='' && price.value!=='') {
+    const rowItem = $prod.cloneNode(true)
+    rowItem.querySelector('.name span').innerText = product.value
+    rowItem.querySelector('.pu span').innerHTML = price.value 
+    rowItem.querySelector('.qty input').innerHTML = 0 
+    $cart.append(rowItem)
+    product.value = ''
+    price.value   = ''
+  }
 
-
-
-function remove(e){
-  // $cart.removeChild(target.parentNode.parentNode.remove())
-  let node = e.currentTarget;
-  node.parentNode.parentNode.remove()
 }
 
 $calc.onclick = calcAll;
-
-//Add product
-
-const addProduct = (e) => {
-  const row = $prod.cloneNode(true)
-  $cart.append($prod.cloneNode(true))
-  
-}
-
-
-
-// inputListener.addEventListener('keyup', updateSubtot)
-$cart.addEventListener('keyup',updateSubtot)
-
-
-
-deleteButtons = document.querySelectorAll('.btn-delete')
-
-// for(let i=0; i<=deleteButtons.length;i++){
-//   deleteButtons[i].addEventListener('click',remove)
-// }
-
-
-function create({target}){
-    let product = document.querySelector('input[type="text"]')
-    let {value} = product.innerText
-    product.value = ''
-    let price = document.querySelector('input[type="number"]')
-
-
-}
-
-
-let createBtn = document.querySelector('tfoot')
-
 createBtn.addEventListener('click', create)
+
+$cart.addEventListener('click', e => {
+  if(e.target.classList.contains('btn-delete')){
+      e.target.parentNode.parentNode.remove();
+  }
+})
