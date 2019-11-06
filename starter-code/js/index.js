47a5e647f316bcfd1fe61bae03d10f1c285b29c7
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-11-06 17:39:58 -0600

    Iteration 1 | done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,25 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
+let $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let unitPrice = $product.querySelector(".pu span").innerHTML;
+  unitPrice = Number(unitPrice);
+
+  let quantity = $product.querySelector(".qty input").value;
+  quantity = Number(quantity);
+
+  let subtotal = unitPrice * quantity;
+
+  let subtotEle = $product.querySelector(".subtot span");
+  subtotEle.innerHTML = subtotal;
+
+  return subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  $product = $cart.getElementsByClassName("product")[0]
+  updateSubtot($product);
 }
 $calc.onclick = calcAll;
