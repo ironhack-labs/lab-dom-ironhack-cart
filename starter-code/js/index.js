45051695e6623a55e681dd24b524390919d9c26f
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2019-11-07 11:17:14 -0600

    Done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -36,11 +36,62 @@ function calcAll() {
 $calc.onclick = calcAll;
 
 let btnCollection = $cart.getElementsByClassName("btn-delete");
-let row;
 
 for (let i = 0; i < btnCollection.length; i++){
   btnCollection[i].onclick = function(e){
+    let row;
     row = e.currentTarget.parentElement.parentElement;
     $cart.removeChild(row);
+  }
+}
+
+let btnCreate = document.getElementById("create");
+
+btnCreate.onclick = function(){
+  let row = document.querySelector("#cart tfoot .new")
+  let inputs = row.getElementsByTagName("input");
+  let name = inputs[0];
+  let price = inputs[1];
+
+  if (name.value != "" && price.value != ""){
+    let newRow = document.createElement("tr");
+    newRow.setAttribute("class", "product");
+  
+    let htmlRow = `
+        <td class="name">
+            <span></span>
+          </td>
+      
+          <td class="pu">
+            $<span></span>
+          </td>
+      
+          <td class="qty">
+            <label>
+              <input type="number" value="0" min="0">
+            </label>
+          </td>
+      
+          <td class="subtot">
+            $<span>0</span>
+          </td>
+      
+          <td class="rm">
+            <button class="btn btn-delete">Delete</button>
+          </td>
+  
+        </tr>`
+    
+    newRow.innerHTML = htmlRow;
+  
+    newName = newRow.querySelector(".name span");
+    newName.innerHTML = name.value;
+
+    newPrice = newRow.querySelector(".pu span")
+    newPrice.innerHTML = price.value;
+
+    $cart.appendChild(newRow);
+    name.value = "";
+    price.value = "";
   }
 }
