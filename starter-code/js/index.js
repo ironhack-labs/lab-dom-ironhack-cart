1874d767dc965bf42ace45bb4460adf2b2fbf649
Veronica <veronica1506@gmail.com>
2019-11-14 19:56:55 -0500

    start code

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,17 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
+function updateSubtot(product) {
   // Iteration 1.1
+  // let result
+  // let unitPrice = product.querySelector('.pu').innerText;
+  // let quantity = product.querySelector('.qty > label > input').value 
+  // result = Number(unitPrice.substr(1)) * Number(quantity)
+
+  // product.querySelector('.subtot').innerText = result
+  for (let i =0 ; i < product.length; i++) {
+    console.log(product[i])
+  }
 }
 
 function calcAll() {
