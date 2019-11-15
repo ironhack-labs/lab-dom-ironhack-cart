d32226d9072005f2fb66785e232a4c2b55b2c848
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-11-15 05:38:23 -0500

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,38 +1,116 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var cart = document.querySelector('#cart tbody');
+var calc = document.getElementById('calc');
 
-// The updateSubtot function will calculate the subtotal for a given product.
-
-// Complete the updateSubtot function that :
-
-// have 1 parameter: the tr element of the product (we can call it $product)
-// calculates the subtotal price, from :
-// the unit price HTML element of the product
-// the quantity HTML element of the product
-// updates the HTML with the new product's subtotal
-// returns the subtotal value
-let subtot = document.querySelector('.subtot span:first-child');
-let price = document.querySelector('.pu span:first-child');
-let qty = document.getElementsByTagName('input')[0]; // or leave it as it is, outside
-let total = document.getElementsByTagName('span')[3];
+// Iteration 1.1
+// // 1. Create elements location that we need to target.
+let rows = document.getElementsByClassName('product');
+let totalPlace = document.querySelector('H2 span');
+let qty = document.querySelector('.qty input');
+let total = 0;
+// // 2.Click event.
 qty.onclick = updateSubtot;
-// total.onclick = function() {
-//   console.log(qty.value);
-//   // input.value = '';
-// };
+calc.onclick = calcAll;
 
-function updateSubtot($product) {
-  // Iteration 1.1
-  console.log($product);
-  if ($product.target === qty) {
-    subtot.innerHTML = price.innerHTML * qty.value;
-    return subtot;
+// // 3. Call functions onclick.
+function updateSubtot(product) {
+  let sum = 0;
+  for (let i = 0; i < rows.length; i++) {
+    let currentProduct = rows[i];
+    let subTotal = currentProduct.querySelector('.subtot');
+    let quantity = currentProduct.querySelector('.qty input').value;
+    let currPrice = currentProduct.querySelector('.pu span').innerHTML;
+    subTotal.innerHTML =
+      '$' + (Number(quantity) * Number(currPrice)).toFixed(2);
+    sum += Number(quantity) * Number(currPrice);
   }
+  total = '$' + sum.toFixed(2);
 }
 
+// Iteration 1.2
 function calcAll() {
-  // Iteration 1.2
-
-  total.innerHTML = subtot.innerHTML;
+  totalPlace.innerText = total;
 }
-$calc.onclick = calcAll;
+
+// Iteration 2: Add another product
+
+let createNewRow = document.getElementById('create');
+
+createNewRow.onclick = function() {
+  let newProduct = document.querySelector('#new-product').value;
+  let newPrice = Number(document.querySelector('#new-price').value).toFixed(2);
+
+  let tr = document.createElement('tr');
+  tr.setAttribute('class', 'product');
+  // tr.classList.add('product'); //another way of adding attribute.
+
+  let newRowFiller = `
+              <td class="name">
+                <span>${newProduct}</span>
+              </td>
+
+              <td class="pu">
+                $
+                <span>${newPrice}</span>
+              </td>
+
+              <td class="qty">
+                <label>
+                  <input type="number" value="0" min="0" />
+                </label>
+              </td>
+
+              <td class="subtot">
+                $
+                <span>0</span>
+              </td>
+
+              <td class="rm">
+                <button class="btn btn-delete">Delete</button>
+              </td>
+        `;
+  tr.innerHTML = newRowFiller;
+  cart.appendChild(tr);
+
+  tr.querySelector('.btn-delete').onclick = deleteProduct;
+  tr.querySelector('.qty input').onclick = updateSubtot;
+};
+
+let buttonDelete = document.querySelector('.btn-delete');
+buttonDelete.onclick = deleteProduct;
+function deleteProduct(e) {
+  e.target.parentElement.parentElement.remove();
+}
+
+// // This is just example if you want to generate some tags.
+
+// let tr = document.createElement('tr');
+//   tr.setAttribute('class', 'name');
+// let classNames = ['name', 'pu', 'qty', 'subtot', 'rm'];
+// for (let i = 0; i < classNames.length; i++) {
+//   tr.appendChild(elemGenerator('', 'td'));
+// }
+
+// function elemGenerator(text, el) {
+//   switch (el) {
+//     case 'td':
+//       let td = document.createElement('td');
+//       td.textContent = text;
+//       return td;
+//     case 'label':
+//       let label = document.createElement('label');
+//       label.textContent = text;
+//       return label;
+//     case 'input':
+//       let input = document.createElement('input');
+//       input.textContent = text;
+//       return input;
+//     case 'button':
+//       let button = document.createElement('button');
+//       button.textContent = text;
+//       return button;
+//     case 'button':
+//       let span = document.createElement('span');
+//       button.textContent = text;
+//       return span;
+//   }
+// }
