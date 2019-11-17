08e71a57a1d257a0ec5a0076800ff036d90de6eb
Ernesto <02ernesto.rodriguez@gmail.com>
2019-11-17 01:34:28 -0600

    Kart Ironhack 80% done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,58 @@
-function deleteItem(e){
-
+// var $cart = document.querySelector('#cart tbody');
+// var $calc = document.getElementById('calc');
+const container = document.querySelector('.container')
+const products = document.querySelector('.products')
+const calculatePriceButton = document.getElementById('calc-prices-button')
+const createItemButton = document.querySelector('.new-item-create')
+const totalPriceSpan = document.getElementById('total-price-span')
+ 
+const deleteItem = e => {
+  products.removeChild(e.currentTarget.parentNode.parentNode) 
 }
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+const getPriceByProduct = () => { 
+  const inputQty = document.getElementsByClassName('label')
+  for(let i = 0; i < inputQty.length; i++) {
+    inputQty[i].onkeyup = updatePriceByProduct
   }
-};
+}
+
+const updatePriceByProduct = e => { 
+  e.currentTarget.parentNode.parentNode.querySelector('.quantity').innerHTML = '$' + Number(e.currentTarget.value * e.currentTarget.parentNode.parentNode.querySelector('.cost').innerHTML.split('$')[1]).toFixed(2)
+}
+
+const getTotalPrice = () => {
+  let sumPrices = 0
+  const totalPrices = document.querySelectorAll('.quantity')
+  totalPrices.forEach(price => sumPrices += Number(price.innerHTML.split('$')[1]));
+  totalPriceSpan.innerHTML = `$${sumPrices.toFixed(2)}`
+}
+
+const createDeleteButton = () => {
+  const deleteButtons = document.getElementsByClassName('btn-delete')
+  for(let i = 0; i < deleteButtons.length ; i++) {
+    deleteButtons[i].onclick = deleteItem
+  }
+}
+
+ const createNewItem = () => {
+
+   const nameProduct = document.getElementById('name-product').value
+   const costProduct = document.getElementById('cost-product').value
+   if(nameProduct && costProduct) {
+     const divProduct = document.createElement('div')
+     divProduct.setAttribute('class', 'product')
+  
+     
+     products.appendChild(divProduct)
+     createDeleteButton()
+     getPriceByProduct()
+   }
+ }
+
+ window.onload = function(){
+   createDeleteButton()
+   calculatePriceButton.onclick = getTotalPrice
+   createItemButton.onclick = createNewItem
+   getPriceByProduct()
+ }
