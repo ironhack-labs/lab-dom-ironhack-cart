56e022de5be34e530a479250c0057973c6f7f0f9
almightyblob <dominik.antunovic@gmail.com>
2019-11-22 17:14:34 +0100

    working code up till iteration 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,42 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $product = document.getElementsByClassName("product")[0];
+var $quantity = document.getElementsByClassName("quantity")[0];
+console.log($product);
 
-function updateSubtot($product) {
+//iteration 1
+function updateTotal() {
+  var total = document.getElementById("total");
+  console.log(total);
+  total.innerHTML = 0;
+  var subtotals = document.getElementsByClassName("subtotal")
+  console.log(subtotals);
+    for(let i = 0; i< subtotals.length; i++){
+      subvalues = parseInt(subtotals[i].innerHTML)
+      total.innerHTML = parseInt(total.innerHTML) + subvalues;
+  }
+}
+
+function updateSubtot(item) {
   // Iteration 1.1
+  var quantity = parseInt(item.getElementsByClassName("quantity")[0].value);
+  var price = parseInt(item.getElementsByClassName("price")[0].innerHTML);
+  var subtotal = item.getElementsByClassName("subtotal")[0];
+  subtotal.innerHTML = quantity * price;
+}
+
+function updateAllSubtots() {
+  var items = document.getElementsByClassName("product");
+    for (var i = 0; i< items.length; i++){
+    updateSubtot(items[i]);
+}
 }
 
 function calcAll() {
   // Iteration 1.2
+    updateAllSubtots();
+    updateTotal();
 }
-$calc.onclick = calcAll;
+
+$calc.addEventListener("click", calcAll);  
+
