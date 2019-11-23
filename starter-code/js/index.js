f84a5da2e2bdc5041ed5e0d3e2c9703d04fc0434
Monika <mswidzinska87@gmail.com>
2019-11-23 21:43:25 +0100

    Iteration1.1 and 1.2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,32 +1,45 @@
+// Monika
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 // Iteration 1.1
-function updateSubtot(item) {
-    debugger
-    var price = parseInt(item.querySelectorAll(".pu span")[0].innerHTML);
-    var quantity = parseInt(item.getElementsByTagName("input")[0].value);
-    item.querySelectorAll(".subtot span")[0].innerHTML = price * quantity;
-}
 
 
+function updateSubtot($product) {
+    var price = parseInt($product.getElementsByClassName("price")[0].innerHTML);
+    var quantity = parseInt($product.getElementsByClassName("input")[0].value);
 
-$calc.onclick = function() {
-    var products = document.getElementsByClassName("product"); // get the products
-    for (let i = 0; i < products.length; i++) {
-        updateSubtot(products[i]); // loop over the products and pass each product to updateSubtotal
-    }
-
-};
-// Iteration 1.2
-function calcAll() {
-    var totalPriceForProduct = document.querySelectorAll(".subtot span");
-    var total = document.getElementById("total");
-    for (let j = 0; j < totalPriceForProduct.length; j++) {
-        total.innerHTML = parseInt(total.innerHTML) + parseInt(totalPriceForProduct[j].innerHTML);
-        debugger
-    }
-    return total;
+    var subtotal = $product.getElementsByClassName("subtotal")[0];
+    subtotal.innerHTML = quantity * price;
 
 }
 debugger
-//$calc.onclick = updateSubtot
+
+function updateAllSubtotals() {
+    var products = document.getElementsByClassName("product");
+    for (let i = 0; i < products.length; i++) {
+        updateSubtot(products[i]);
+    }
+}
+
+// Iteration 1.2
+debugger
+
+function updateFinalPrice() {
+    var totalPrice = document.getElementById("total-sum");
+    totalPrice.innerHTML = 0;
+
+    var subtotals = document.getElementsByClassName("subtotal");
+
+    for (let i = 0; i < subtotals.length; i++) {
+        subvalues = parseInt(subtotals[i].innerHTML);
+        totalPrice.innerHTML = parseInt(total.innerHTML) + subvalues;
+    }
+
+}
+
+function calcAll() {
+    // Iteration 1.2
+    updateAllSubtotals();
+    updateFinalPrice();
+}​
+$calc.addEventListener("click", calcAll);
