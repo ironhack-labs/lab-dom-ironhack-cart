ae03bfab27a3c9404a54c6bcc098e2b8a9205aed
hanny242 <hannahdhawes@gmail.com>
2019-11-24 20:11:20 +0100

    Iteration 1 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,37 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+var $products = document.getElementsByClassName("product");
+
+function updateSubtot($products) {
+
+  var productPrice = document.querySelectorAll(".pu span")[0].innerHTML;
+  var productQty = document.querySelectorAll(".qty input")[0].value;
+  var productSub = document.querySelectorAll(".subtot span")[0];
+  productSub.innerHTML = (parseInt(productPrice) * parseInt(productQty));
+  console.log(productSub)
 }
+
+function updateCartTotal($products) {
+  var productSubs = document.querySelectorAll(".subtot span");
+  var totalValue = 0;
+  var totalValueElement = document.querySelectorAll(".total span")[0];
+  console.log(productSubs);
+  for (i = 0; i < productSubs.length; i++) {
+    totalValue += parseInt(productSubs[i].innerHTML);
+    console.log(totalValue);
+  }
+  totalValueElement.innerHTML = totalValue;
+}
+
+var calculateTotalButton = document.getElementById("calc");
+calculateTotalButton.addEventListener("click", function () {
+  var item = document.getElementsByClassName("product");
+  updateSubtot(item[0]);
+  updateCartTotal(item[0]); 
+});
+
+
 
 function calcAll() {
   // Iteration 1.2
