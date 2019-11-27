eb60cf45287cb558d2ee6fea71a905a8039177ef
Malte Schenkling <schenkling@mediamarktsaturn.com>
2019-11-27 14:30:16 +0100

    Ironhack_Cart_Iteration1.2

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,16 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let priceUnit = $product.querySelector(".pu span").innerText;
+  let quantityUnit = $product.querySelector(".qty label input").value;
+  let newSubTotal = Number(priceUnit) * Number(quantityUnit);
+
+  $product.querySelector(".subtot span").innerText = newSubTotal;
+  return newSubTotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  updateSubtot(document.querySelector(".product"));
 }
 $calc.onclick = calcAll;
