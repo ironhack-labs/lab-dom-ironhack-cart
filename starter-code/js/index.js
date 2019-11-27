181e32cda937abf5cc3f4162dc7a64c577f7fbe0
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2019-11-27 14:31:55 +0100

    Iteration 1.1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,29 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
-}
 
+
+let $product = document.querySelector('.name').getElementsByTagName('span')[0].innerText;
+
+ // Iteration 1.1
+ 
+function updateSubtot($product) {
+ 
+  let unitPrice = document.querySelector('.pu').getElementsByTagName('span')[0].innerText;
+  let quantity = document.querySelector('input').value;
+  let subTotal = unitPrice * quantity;
+  let elementSubTotal = document.querySelector('.subtot').getElementsByTagName('span')[0].innerText;
+  if (quantity >= 0){
+    elementSubTotal = subTotal;
+    return elementSubTotal;
+  }
+  }
+  console.log(updateSubtot($product));
+  //updateSubtot($product);
+
+    
+ // Iteration 1.2
 function calcAll() {
-  // Iteration 1.2
+ 
 }
 $calc.onclick = calcAll;
