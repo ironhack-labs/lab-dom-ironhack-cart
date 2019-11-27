724ffaa2f8d521da508b48d71c405bf18029300a
Rochowski <rochowskim@media-saturn.com>
2019-11-27 15:52:20 +0000

    calculating subtotals

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,26 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+var $product = (document.querySelector(".product"));
+
 function updateSubtot($product) {
   // Iteration 1.1
+ 
+  const qtyInput = $product.querySelector(".qty label input");
+  const qty = qtyInput.value;
+  const unitPrice = $product.querySelector(".pu span").innerText;
+  const subTotal = qty * unitPrice;
+
+  console.log(qtyInput);
+
+  $product.querySelector(".subtot span").innerText = subTotal;
 }
+
+$product.querySelector(".qty label input").onclick = function() {
+  updateSubtot(document.querySelector(".product"));
+}
+
+//console.log(document.querySelectorAll(".product"));
 
 function calcAll() {
   // Iteration 1.2
