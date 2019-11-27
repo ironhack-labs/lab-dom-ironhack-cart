e27a921970766d7b46b0d14949ae87bfdf2a33f6
Juliane Trapp <trappj@media-saturn.com>
2019-11-27 11:37:07 +0100

    Iteration 1.1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,21 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var $cart = document.querySelector("#cart tbody");
+var $calc = document.getElementById("calc");
 
+console.log($cart.querySelector(".subtot span"));
 function updateSubtot($product) {
   // Iteration 1.1
+  let price = $cart.querySelector(".pu span").innerText;
+  let quantity = $cart.querySelector(".qty input").value;
+  let subTotalTag = $cart.querySelector(".subtot span");
+
+  let subTotal = Number(price) * Number(quantity);
+
+  subTotalTag.innerText = subTotal;
 }
 
 function calcAll() {
   // Iteration 1.2
 }
 $calc.onclick = calcAll;
+
+updateSubtot();
