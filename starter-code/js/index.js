409daa33a1c21c0a6c3a17b1fcab3905438a122b
Jason Franklin <jfrank325@gmail.com>
2020-01-14 14:00:20 +0100

    additions

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,7 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $delete = document.querySelector('.btn.btn-delete');
+var $add = document.querySelector('.new');
 // function to calculate subtotal - $product stands for the entire table row element in the html:
 function updateSubtot($product) {
   // Iteration 1.1
@@ -29,3 +31,25 @@ function calcAll() {
 }
 // click event connected to the function to calculate everything:
 $calc.onclick = calcAll;
+
+function deleteProduct() {
+  const entireProduct = document.querySelector('.product');
+  const allProducts = document.querySelector('#cart tbody');
+  allProducts.removeChild(entireProduct);
+}
+$delete.onclick = deleteProduct;
+
+function addProduct() {
+  const allProducts = document.querySelector('#cart tbody');
+  const newProduct = document.createElement('.product');
+  allProducts.appendChild(newProduct);
+}
+$add.onclick = addProduct;
+
+// function deleteProduct() {
+//   document.querySelector('.product').forEach(function(product) {
+//     const allProducts = document.querySelector('#cart tbody');
+//     allProducts.removeChild(product);
+//   });
+// }
+// $delete.onclick = deleteProduct;
