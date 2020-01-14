5c2a736ec6a4d22c634a29d54a12ac0a3724e288
stifamajstor <stjepan.crncic@gmail.com>
2020-01-14 21:43:56 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,32 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var $cart = document.querySelector("#cart tbody");
+var $calc = document.getElementById("calc");
+var $delete = document.querySelector(".btn.btn-delete");
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let basePrice = $product.querySelector(".pu > span").innerHTML;
+  let qty = $product.querySelector(".qty > label > input").value;
+  let subTotal = basePrice * qty;
+  $product.querySelector(".subtot > span").innerHTML = subTotal;
+  return subTotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  let total = 0;
+  document.querySelectorAll(".product").forEach(function($product) {
+    total += updateSubtot($product);
+  });
+  document.querySelector("h2 > span").innerHTML = total;
 }
+
 $calc.onclick = calcAll;
+
+function deleteOnClick(element) {
+  element.querySelector(".rm > button").onclick = function() {
+    $cart.removeChild(element);
+  };
+}
+document.querySelectorAll("#cart > tbody > tr").forEach(ele => {
+  deleteOnClick(ele);
+});
