cfd829dc88e2b6d675475c3c6d7eb1e4e93d4ed1
Juan Carlos <jcarlosriego@gmail.com>
2020-01-17 23:09:56 +0100

    Iteration 1.1 completed

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,24 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
+// class Product {
+//   constructor (nameParam, priceParam, amountParam) {
+//     this.name = nameParam;
+//     this.price = priceParam;
+//     this.amount = amountParam;
+//     this.subtotal = ``;
+//   }
+// }
+let product; 
+
+function updateSubtot(product) {
   // Iteration 1.1
+  let price = document.querySelector(`.pu > span`).innerText;
+  let quantity = document.querySelector(`input`).value;
+  return  product = parseFloat(price) * parseFloat(quantity);
 }
+
+
 
 function calcAll() {
   // Iteration 1.2
