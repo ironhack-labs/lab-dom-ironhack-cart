d8c395dc3a7932913032e290ba4e0dc74b41f65e
almonasterio <a.monasterio.a@gmail.com>
2020-01-17 22:48:15 +0100

    first try

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,35 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let priceUnit = document.querySelector(`.pu span`).innerHTML;
+  let quantityProduct = document.querySelector(`.qty label input`).value;
+  let subTotal =priceUnit*quantityProduct;
+  
+  return document.querySelector(`.subtot span`).innerHTML = subTotal;
+
+
+
 }
 
 function calcAll() {
-  // Iteration 1.2
+  let totalSum=0;
+  let allItems =document.querySelectorAll( `.product`);
+    allItems.forEach((product) => {
+    totalSum +=updateSubtot(product);
+    
+  })
+  return document.querySelector(`h2 span`).innerHTML =totalSum; 
+
 }
+
+
+
+  // Iteration 1.2
+
+
+
 $calc.onclick = calcAll;
+
+/*
+let table = document.querySelector('.table')
+table.childNodes.forEach(row => updateSubtot(row))*/
