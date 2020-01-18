29faec07d5279ac0ed13d16b4464de2bfbf8d4bf
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-01-18 19:33:03 +0100

    iteration 3 Total

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,16 +1,21 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
-  const $subtot = $product.querySelector('.subtot span'); 
-  const puAmount = parseInt($product.querySelector('.pu span').innerText);
-  const qtyAmount = parseInt($product.querySelector('.qty input').value);
-  $subtot.textContent = (puAmount*qtyAmount);
+function updateSubtot(product) {
+  const $subtot = product.querySelector('.subtot span'); 
+  const puAmount = parseInt(product.querySelector('.pu span').innerText);
+  const qtyAmount = parseInt(product.querySelector('.qty input').value);
+  $subtot.innerText = (puAmount*qtyAmount);
   return puAmount*qtyAmount;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  const allSubtots = document.querySelectorAll('.subtot span')
+  let subtotSum = 0;
+  allSubtots.forEach(function (subtot) {
+    updateSubtot(subtot.closest('.product'));
+    subtotSum += parseInt(subtot.innerText);
+  })
+  document.querySelector('h2 span').innerText= subtotSum.toString();
 }
-$calc.onclick = calcAll;
+$calc.addEventListener("click", calcAll);
