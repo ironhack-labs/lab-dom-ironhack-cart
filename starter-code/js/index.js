5028e3122602a38a7a4f3924dc3104b61a73ff4b
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-01-18 21:04:50 +0100

    iteration1.1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,15 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+let priceUnitary = document.getElementsByClassName('pu')[0].children[0].innerText;
+let quantity = document.getElementsByClassName('qty')[0].children[0].children[0].getAttribute('value');
+let subTotalModfyer = document.getElementsByClassName('subtot')[0].children[0];
+let subTot = priceUnitary * quantity;
+subTotalModfyer.textContent = subTot;
+return subTot;
 }
-
 function calcAll() {
   // Iteration 1.2
+  updateSubtot();
 }
 $calc.onclick = calcAll;
