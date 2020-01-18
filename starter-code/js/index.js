49e381b488a7f1077bca6f73561a50fa865b6d56
Roberto Marrero <robertomarrerop@hotmail.com>
2020-01-18 18:43:44 +0100

    ejercicio finalizado

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -41,16 +41,109 @@ function calcAll() {
 
   let arrAllProducts = document.querySelectorAll(".product")
 
-
   arrAllProducts.forEach((elm) => updateSubtot(elm))
 
   arrAllProducts.forEach((elm) => totalOrder += parseFloat(elm.querySelector('.subtot span').innerText))
-
-
 
   document.querySelector('h2 span').innerText = totalOrder
 
 }
 
-calcAll()
+
 $calc.onclick = calcAll;
+
+
+//=========================== Deleting elemnt row
+
+
+
+
+var parent = document.querySelector("tbody")
+// CurrentTarget con todas las propiedades del elemento que ha invocado la función a raíz del evento
+
+
+document.querySelectorAll('.btn-delete').forEach(elm => {
+  elm.onclick = e => {
+    parent.removeChild(elm.parentNode.parentNode)
+    console.log(parent)
+  }
+})
+
+
+//=================== Creating New Products
+let $create = document.getElementById('create');
+
+function createNewProduct() {
+
+
+
+  let inputs = []
+
+  document.querySelectorAll("tfoot input").forEach(elm => inputs.push(elm.value))
+
+  let elmntToClone = document.querySelectorAll(".product")[0];
+
+  let clnProduct = elmntToClone.cloneNode(true);
+
+  clnProduct.querySelector(".name").innerText = inputs[0]
+
+  clnProduct.querySelector(".pu span").innerText = inputs[1]
+
+  parent.appendChild(clnProduct);
+
+  parent = document.querySelector("tbody")
+
+
+  document.querySelectorAll('.btn-delete').forEach(elm => {
+    elm.onclick = e => {
+      parent.removeChild(elm.parentNode.parentNode)
+      console.log(parent)
+    }
+  })
+
+
+
+}
+
+$create.onclick = createNewProduct;
+
+
+let test = 8
+
+function testTest() {
+  test = 10
+}
+
+testTest()
+
+//console.log(test)
+
+//console.log(document.querySelector("#create"))
+
+
+
+
+
+
+
+
+
+
+//=========================== Deleting elemnt row
+
+// let $remove = document.querySelectorAll(".btn-delete");
+
+// function deleteRow() {
+//   $remove = document.querySelectorAll(".btn-delete")
+
+//   let parent = document.querySelector("tbody")
+
+
+
+//   document.querySelectorAll('.btn-delete').forEach(elm => {
+//     elm.onclick = e => parent.removeChild(elm.parentNode.parentNode)    // CurrentTarget con todas las propiedades del elemento que ha invocado la función a raíz del evento
+
+//   })
+// }
+
+// $remove.forEach((elm) => elm.onclick = deleteRow)
