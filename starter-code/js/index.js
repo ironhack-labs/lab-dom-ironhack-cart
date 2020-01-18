a5a6edf76d780f6d910bef43431de2f2d06d1bd3
Coline Velard <coline.velard@gmail.com>
2020-01-18 19:58:14 +0100

    half-done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,81 @@
-var $cart = document.querySelector('#cart tbody');
+var $cart = document.querySelector('#cart tbody'); // this is a  node
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+var $input = document.querySelector(".qty label input");
+var $inputs = document.querySelectorAll(".qty label input");
+var $subTo = document.querySelector(".subtot span");
+
+var $productsList = document.querySelectorAll(".product");
+var $price = document.querySelector(".pu span");
+var $prices = document.querySelectorAll(".pu span");
+
+var $total = document.querySelector("h2 span");
+
+
+function updateSubTot() {
+  totalPrice = $input.value * Number($price.textContent);
+  console.log(totalPrice);
+  $subTo.innerHTML = totalPrice;
+  return totalPrice;
 }
 
+
+$input.onclick = updateSubTot;
+
+
 function calcAll() {
-  // Iteration 1.2
+  $total.textContent = updateSubTot();
 }
+
 $calc.onclick = calcAll;
+
+function suppress() {
+  const $delete_btn = document.querySelector(".btn-delete");
+  const $product = document.querySelector(".product");
+  $delete_btn.onclick = $product.remove();
+} 
+
+suppress();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function updateSubTotal(product) {
+//   $productsList.forEach(function() {
+//     $subTo.innerHTML += $input.value * Number($price.textContent);
+//     console.log($subTo.innerHTML);
+//   })
+// }
+
+
+
+
+
+
+
+// function updateSubtot() {
+//   subTotalPrice = (Number($input.value) * $price.textContent);
+//   console.log(subTotalPrice);
+//   $subTo.textContent = subTotalPrice;
+// }
+
+//var $total = document.querySelector("h2 span");
+
+
+
+
+// console.log("yo", $calc)
+// $calc.onclick = calcAll
