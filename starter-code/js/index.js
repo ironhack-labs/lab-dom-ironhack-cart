555b94265655ba3a8c00a7df06f89a396a8c5952
Pablolo <pablo_olondriz@hotmail.com>
2020-01-18 20:37:09 +0100

    iteration1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,29 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+// function updateSubtot($product) {
+//   let price = document.getElementsByClassName('pu');
+//   let quantity = document.getElementsByClassName('qty');
+//   console.log(price, quantity);
+//   let totalPrice = price * quantity;
+
+
+// }
+
+
 function updateSubtot($product) {
-  // Iteration 1.1
+  let price = document.getElementsByClassName('pu')[0].children[0].innerText;
+  let quantity = document.getElementsByClassName('qty')[0].children[0].children[0].getAttribute('value');
+  let subtot = document.getElementsByClassName('subtot')[0].children[0];
+
+  let subtotalEnd = price * quantity;
+  subtot.textContent = subtotalEnd;
+  
+  return subtotalEnd;
 }
 
+
 function calcAll() {
-  // Iteration 1.2
+  updateSubtot();
 }
 $calc.onclick = calcAll;
