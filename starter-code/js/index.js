a3c6178057ec36e60cc076a4432647e433b6c8b8
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-01-18 14:41:35 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,65 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+var calc = document.getElementById('calc');
+var cart = document.querySelector('#cart tbody');
+var create = document.getElementById("create");
+var newp = document.querySelector(".new");
+
+
+function updateSubtot(product) {
+  var quantity = product.querySelector('.qty>label>input');
+  var subtot = product.querySelector('.subtot>span');
+  var pu = product.querySelector('.pu>span');
+  subtot.innerHTML = pu.textContent * quantity.value;
+  return subtot;  
 }
 
 function calcAll() {
-  // Iteration 1.2
+  var products = cart.querySelectorAll('.product');
+  let total = document.querySelector("h2 > span");
+  total.innerHTML = "";
+  let temp = 0;
+  products.forEach(product => {
+    temp += Number(updateSubtot(product).textContent);
+  });
+  total.innerHTML = temp;
 }
-$calc.onclick = calcAll;
+
+function deleteRow(products) {
+  var products = cart.querySelectorAll('.product');
+  products.forEach((product) => {
+    product.querySelector(".btn-delete").addEventListener("click", () => {
+      product.innerHTML = "";
+    })
+  });
+};
+
+function createRow() {
+  var newName = document.querySelector(".insertName");
+  var newPrice = document.querySelector(".insertPrice");
+  cart.innerHTML += 
+  `<tr class="product">
+    <td class="name">
+      <span>${newName.value}</span>
+    </td>
+    <td class="pu">
+      $<span>${newPrice.value}</span>
+    </td>
+    <td class="qty">
+      <label>
+        <input type="number" value="1" min="1">
+      </label>
+    </td>
+    <td class="subtot">
+      $<span>0</span>
+    </td>
+    <td class="rm">
+      <button class="btn btn-delete">Delete</button>
+    </td>
+  </tr>`;
+  deleteRow();
+}
+
+calc.onclick = calcAll;
+window.onload = deleteRow;
+create.onclick = createRow;
+
