1f6c504b5059e824e203ef9c14c0e88dcc6c4672
Jaime <jaimemoravallejo@gmail.com>
2020-01-19 15:56:19 +0100

    iteration 2 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,11 +3,13 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
-  let productPrice = document.querySelector(".pu span").innerText;
-  console.log(document.querySelector(".pu span").innerText)
-  let productQuantity = document.querySelector(".qty input").value;
+
+for(let i=0; i< $product.length; i++){
+  let productPrice = $product[i].querySelector(".pu span").innerText;
+  let productQuantity = $product[i].querySelector(".qty input").value;
   let productSubtotal = productPrice * productQuantity;
-  document.querySelector('.subtot span').innerText = productSubtotal;
+  $product[i].querySelector('.subtot span').innerText = productSubtotal;
+} 
 
 }
 
@@ -15,8 +17,7 @@ function calcAll() {
   // Iteration 1.2
   let $products = document.querySelectorAll('.product');
   let products = Array.from($products);
-  updateSubtot(products);
-
+updateSubtot(products)
   let result = 0;
 
   if (products !== []) {
@@ -26,3 +27,11 @@ function calcAll() {
 
 }
 $calc.onclick = calcAll;
+
+function createProduct() {
+  var create = document.querySelector('#cart tbody');
+
+  create.insertAdjacentHTML('beforeend', '<tr class="product"><td class="name"><span>Ironshirt</span></td><td class="pu">$<span>15.00</span></td><td class="qty"><label><input type="number" value="0" min="0"></label></td><td class="subtot">$<span>0</span></td><td class="rm"><button class="btn btn-delete">Delete</button></td></tr>');
+
+}
+createProduct();
