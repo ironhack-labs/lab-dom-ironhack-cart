24c74ebed2db9f21afaa57e885e097dfdf0c95a3
Jaime <jaimemoravallejo@gmail.com>
2020-01-19 09:49:48 +0100

    iteration 1 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,26 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let productPrice = document.querySelector(".pu span").innerText;
+  console.log(document.querySelector(".pu span").innerText)
+  let productQuantity = document.querySelector(".qty input").value;
+  let productSubtotal = productPrice * productQuantity;
+  document.querySelector('.subtot span').innerText = productSubtotal;
+
 }
 
 function calcAll() {
   // Iteration 1.2
+  let $products = document.querySelectorAll('.product');
+  let products = Array.from($products);
+  updateSubtot(products);
+
+  let result = 0;
+
+  if (products !== []) {
+    $products.forEach(elem => result += elem.querySelector('.pu span').innerText * elem.querySelector('.qty input').value);
+  }
+  document.querySelector('h2 span').innerHTML = result; 
+
 }
 $calc.onclick = calcAll;
