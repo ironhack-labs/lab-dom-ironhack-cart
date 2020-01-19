b0017b301d968d6c2a204d7c16cfc2c43a4491d7
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-01-19 22:35:48 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,34 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+let products = document.querySelectorAll(`.product`);
+let totAll = document.querySelector('h2 span'); 
+let subTotal = document.querySelectorAll(".subtot span");
+let puDOMEl = document.querySelectorAll(".pu span");
+let qtyDOMEl = document.querySelectorAll(".qty label input");
 
-function updateSubtot($product) {
+function updateSubtot() {
+
   // Iteration 1.1
+
+  for (let i = 0; i<products.length; i++){
+    subTotal[i].innerHTML = Number (puDOMEl[i].innerText) * Number(qtyDOMEl[i].value);
+   
+  }
+  
 }
+
+
 
 function calcAll() {
   // Iteration 1.2
+  updateSubtot();
+
+  let suma =0
+  for (let i = 0; i<products.length; i++){
+    suma += subTotal[i] ;
+    suma = totAll.innerHTML
+  }
+    console.log(totAll);
+  
 }
 $calc.onclick = calcAll;
