fcc69b697bf14b2da35682c2128c98385c35b88b
Leinadm <danielmonteru@gmail.com>
2020-01-19 23:00:53 +0100

    Iteration 4

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,6 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
-let $tbody = document.getElementById("parentProducts");
+let $tbody = document.getElementById('parentProducts');
 
 
 function updateSubtot($product) {
@@ -8,14 +8,14 @@ function updateSubtot($product) {
   const unitPrice = $product.querySelector('.pu span').innerText;
   const quantity = $product.querySelector('input').value;
   const subTotal = parseFloat(unitPrice) * parseFloat(quantity);
-  $product.querySelector(".subtot span").innerText = subTotal;
+  $product.querySelector('.subtot span').innerText = subTotal;
   return subTotal;
 }
 
 function calcAll() {
   // Iteration 1.2 - Iteration 2 - Iteration 3
   let total = 0;
-  const products = document.getElementsByClassName("product");
+  const products = document.getElementsByClassName('product');
   for (let i = 0; i < products.length; i++) {
     const p = products[i];
     total += updateSubtot(p);
@@ -23,4 +23,25 @@ function calcAll() {
   document.getElementById('total').innerText = total;
 }
 
+//Iteration 4
+
+function deleteProduct(e){
+  let tr  = e.currentTarget.parentElement.parentElement;
+  $tbody.removeChild(tr);
+  calcAll();
+  createDeleteEvents();
+}
+
+function createDeleteEvents(){
+  const deleteBtns = document.getElementsByClassName('btn-delete');
+  for (let i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener('click', deleteProduct);
+  }
+}
+
+//Iteration 5
+
+
+
 $calc.onclick = calcAll;
+createDeleteEvents();
