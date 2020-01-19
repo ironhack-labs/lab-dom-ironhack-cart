f2b7968492f106aef6d8e18c72a415a1ef11b6e6
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-19 20:33:12 +0100

    No hemos conseguido crear la estructura de html para poder añadir los inputs

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,6 @@
 var $cart = document.querySelector("#cart tbody");
 var $calc = document.getElementById("calc");
+let $create = document.getElementById("create");
 
 // Iteration 1.1  Sproduct== tr.product
 
@@ -41,13 +42,79 @@ $calc.onclick = calcAll;
 
 // Iteration 4
 
-function deleteProduct() {
-  
-  const botones = document.querySelectorAll(".btn-delete");
-  
-  botones.forEach()
-
+function deleteProduct(botones) {
+  botones.parentNode.parentNode.remove();
   //.parentNode.remove());
 }
+eliminate = document.querySelectorAll(".btn-delete");
+eliminate.forEach(elm => {
+  elm.onclick = e => deleteProduct(e.currentTarget);
+});
 
-"btn-delete".onclick = deleteProduct();
+// Iteration 5
+
+$create.onclick = createProduct;
+
+function createProduct() {
+  const inputs = document.querySelector(".new").getElementsByTagName("input");
+  //const productInputs = inputs.getElementsByTagName("input");
+
+  console.log(inputs);
+
+  const productName = inputs[0].value;
+  const productPrice = inputs[1].value;
+
+  console.log(productName);
+  console.log(productPrice);
+
+  crearFila(productName, productPrice);
+  // let fila = document.createElement("tr.product");
+  // let celdaNombre = document.createElement("td.name");
+  // let celdaPrecio = document.createElement("td.pu");
+  // let celdaCantidad = document.createElement("td.qty");
+  // let celdaSubtotal = document.createElement("td.subtot");
+  // let celdaDelete = document.createElement("td.rm");
+
+  document.querySelector(".tbody").appendChild = newProduct;
+}
+
+function crearFila(nombre, precio) {
+  let fila = document.createElement("tr.product");
+  let celdaNombre = document
+    .createElement("td")
+    .setAttribute("class", "name")
+    .createElement("span")
+    .createTextNode(nombre);
+  let celdaPrecio = document
+    .createElement("td")
+    .setAttribute("class", "pu")
+    .createElement("span")
+    .createTextNode(precio);
+  let celdaCantidad = document
+    .createElement("td")
+    .setAttribute("class", "qty")
+    .createElement("label")
+    .createElement("input")
+    .setAttribute(type, "number")
+    .setAttribute(value, 0)
+    .setAttribute(min, 0);
+
+  let celdaSubtotal = document
+    .createElement("td")
+    .setAttribute("class", "subtot")
+    .createElement("span")
+    .createTextNode(0);
+  let celdaDelete = document
+    .createElement("td")
+    .setAttribute("class", "btn btn-delete")
+    .createElement("button")
+    .createTextNode("Delete");
+
+  resultado = fila.appendChild(
+    celdaNombre,
+    celdaPrecio,
+    celdaCantidad,
+    celdaSubtotal,
+    celdaDelete
+  );
+}
