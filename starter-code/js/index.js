be97f06e3d222e4cf8f9e9152c522bfcc0330f3e
Paul Dkn <pauldkn.code@gmail.com>
2020-01-20 09:26:57 +0100

    Done with problem for delete product

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,38 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var products = document.querySelectorAll("#cart tbody .product"); 
 
-function updateSubtot($product) {
+function updateSubtot(product) {
   // Iteration 1.1
+  var pu = product.querySelector(".pu span");
+  var qty = product.querySelector(".qty input");
+  var subtot = product.querySelector(".subtot span");
+  var number = Number(pu.textContent) * Number(qty.value)
+  subtot.innerHTML = String(number)
+  return number;
 }
 
 function calcAll() {
   // Iteration 1.2
-}
+  let span = document.querySelector("h2>span");
+  span.innerHTML = "";
+  let total = 0;
+  products.forEach(product => {
+    total += updateSubtot(product);
+  });
+  span.innerHTML = String(total);
+};
 $calc.onclick = calcAll;
+
+function deleteRow(button){
+  console.log(button);
+  button.parentNode.parentNode.remove();
+}
+
+let removeBtn = document.querySelectorAll(".btn-delete");
+removeBtn.forEach(button => {
+  button.onclick = function() {
+    console.log(button);
+    deleteRow(button);
+  }
+})
