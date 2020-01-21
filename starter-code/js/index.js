1f26aa80371164686de29f1d5a4c157edeafb1d1
michael <michael@miniguide.es>
2020-01-22 00:23:47 +0100

    attempt at iteration 1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,15 @@
+/* jshint esversion: 6 */
+
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let priceUnit = Number($product.querySelector('.pu span').textContent);
+  let quantity = $product.querySelector('.qty label input').value;   // input form forces it to be a number
+  let subtotal = priceUnit * quantity;
+  $product.querySelector('.subtot span').textContent = subtotal;
+  return subtotal;
 }
 
 function calcAll() {
