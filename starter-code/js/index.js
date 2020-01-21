c84f8680f6055cb7c1c4e60e3d1876f2d4bb45fc
alex <alex@PC-AZUL.localdomain>
2020-01-21 13:14:43 +0100

    finishing updateSubtot

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,7 +3,22 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  //const selectProduct = $product.childNodes;
+  //console.log(selectProduct);
+  const priceOfProduct = parseInt(document.querySelector(".pu>span").textContent);
+  console.log("The price of the product is " + priceOfProduct)
+  
+  const quantityOfProducts = parseInt(document.querySelector(".qty>label>input").value);
+  console.log("The quantity of the products selectioned are " +quantityOfProducts)
+  
+  const subTotalOfProduct = document.querySelector(".subtot>span")
+  //console.log(subTotalOfProduct)
+  const sum = priceOfProduct*quantityOfProducts
+  console.log("The sub-total is " + sum)
+  subTotalOfProduct.innerHTML = `${sum}`
 }
+
+updateSubtot($cart)
 
 function calcAll() {
   // Iteration 1.2
