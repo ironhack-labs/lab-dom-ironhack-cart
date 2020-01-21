ab723651ce62760795e63c5095387685b374b675
michael <michael@miniguide.es>
2020-01-22 00:56:18 +0100

    iteration 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,8 +6,8 @@ var $calc = document.getElementById('calc');
 function updateSubtot($product) {
   // Iteration 1.1
   let priceUnit = Number($product.querySelector('.pu span').textContent);
-  let quantity = $product.querySelector('.qty label input').value;
-  let subtotal = priceUnit * quantity;
+  let quantity = $product.querySelector('.qty label input').value;  // input type is number
+  let subtotal = Math.round(100 * priceUnit * quantity) / 100;
   $product.querySelector('.subtot span').textContent = subtotal;
   return subtotal;
 }
@@ -15,6 +15,10 @@ function updateSubtot($product) {
 function calcAll() {
   // Iteration 1.2
   let $products = document.querySelectorAll('.product');
-  $products.forEach($product => updateSubtot($product));
+  let total = 0;
+  $products.forEach($product => {
+    total += updateSubtot($product);
+  });
+  document.querySelector('h2 span').textContent = total;
 }
 $calc.onclick = calcAll;
