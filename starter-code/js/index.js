4e38153789cfe92f501f2fb401c31298666ef596
alex <alex@PC-AZUL.localdomain>
2020-01-21 16:22:08 +0100

    finiproblems with the 2nd itreaction

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -6,21 +6,32 @@ function updateSubtot($product) {
   //const selectProduct = $product.childNodes;
   //console.log(selectProduct);
   const priceOfProduct = parseInt(document.querySelector(".pu>span").textContent);
-  console.log("The price of the product is " + priceOfProduct)
+  console.log("The price of the product is " + priceOfProduct);
   
   const quantityOfProducts = parseInt(document.querySelector(".qty>label>input").value);
-  console.log("The quantity of the products selectioned are " +quantityOfProducts)
+  console.log("The quantity of the products selectioned are " +quantityOfProducts);
   
-  const subTotalOfProduct = document.querySelector(".subtot>span")
+  const subTotalOfProduct = document.querySelector(".subtot>span");
   //console.log(subTotalOfProduct)
-  const sum = priceOfProduct*quantityOfProducts
-  console.log("The sub-total is " + sum)
-  subTotalOfProduct.innerHTML = `${sum}`
+  const sum = priceOfProduct*quantityOfProducts;
+  console.log("The sub-total is " + sum);
+  subTotalOfProduct.innerHTML = `${sum}`;
+  return subTotalOfProduct;
 }
 
-updateSubtot($cart)
+//updateSubtot($cart)
 
 function calcAll() {
   // Iteration 1.2
+  /*
+  const takeProducts = $cart.getElementsByClassName(".product");
+  console.log(takeProducts)
+  takeProducts.forEach(element => {
+    let selectProduct = document.getElementsByClassName(".name").textcontent;
+    console.log(selectProduct)
+    //updateSubtot($cart)
+  });
+  */
+updateSubtot($cart)
 }
 $calc.onclick = calcAll;
