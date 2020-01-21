55d4f0e2d5b2ff98511959c16e435bd7994cd2a2
Pablolo <pablo_olondriz@hotmail.com>
2020-01-21 13:48:55 +0100

    cleaner function updatesubtot

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,29 +1,23 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-// function updateSubtot($product) {
-//   let price = document.getElementsByClassName('pu');
-//   let quantity = document.getElementsByClassName('qty');
-//   console.log(price, quantity);
-//   let totalPrice = price * quantity;
-
-
-// }
-
 
 function updateSubtot($product) {
-  let price = document.getElementsByClassName('pu')[0].children[0].innerText;
-  let quantity = document.getElementsByClassName('qty')[0].children[0].children[0].getAttribute('value');
-  let subtot = document.getElementsByClassName('subtot')[0].children[0];
+  let price = document.querySelector('.pu > span').innerHTML;
+  let quantity = document.querySelector('.qty > label > input').value;
+  let subtot = document.querySelector('.subtot > span');
 
-  let subtotalEnd = price * quantity;
-  subtot.textContent = subtotalEnd;
-  
-  return subtotalEnd;
+  let subtotal = price * quantity;
+  subtot.textContent = subtotal;
+
+  return subtotal;
 }
 
 
 function calcAll() {
+
+
+
   updateSubtot();
 }
 $calc.onclick = calcAll;
