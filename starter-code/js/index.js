ebed3bb4f6fa6dd800091d79ff1c069db9d32833
Pablolo <pablo_olondriz@hotmail.com>
2020-01-22 14:01:09 +0100

    iteration5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,41 +4,42 @@ var $product = document.querySelector('.product');
 
 /* Iteration 1 */
 function updateSubtot($product) {
-  let $price = $product.querySelector('.pu > span').innerHTML;
-  let $quantity = $product.querySelector('.qty > label > input').value;
-  let $subtot = $product.querySelector('.subtot > span');
+  let price = $product.querySelector('.pu > span').innerHTML;
+  let quantity = $product.querySelector('.qty > label > input').value;
+  let subtot = $product.querySelector('.subtot > span');
 
-  let subtotal = $price * $quantity;
-  $subtot.textContent = subtotal;
+  let subtotal = price * quantity;
+  subtot.textContent = subtotal;
 
   return subtotal;
 }
 
 /* Iterations 2 & 3 */
 function calcAll() {
-  let $allProducts = document.querySelectorAll('.product');
-  let $finalPrice = document.querySelector('h2 > span');
+  let allProducts = document.querySelectorAll('.product');
+  let finalPrice = document.querySelector('h2 > span');
   let totalPrice = 0;
 
-  $allProducts.forEach(element => {
+  allProducts.forEach(element => {
     updateSubtot(element);
     totalPrice += updateSubtot(element);
   });
 
-  $finalPrice.textContent = totalPrice;
+  finalPrice.textContent = totalPrice;
 }
 
 $calc.onclick = calcAll;
 
 /* Iteration 4 */
-let $deleteBtns = document.querySelectorAll('.btn-delete');
+let deleteBtns = document.querySelectorAll('.btn-delete');
 
-$deleteBtns.forEach(element => {
+deleteBtns.forEach(element => {
   element.onclick = function() {
-    let $productContainer = element.parentNode.parentNode.parentNode;
-    $productContainer.removeChild(element.parentNode.parentNode);
+    let productContainer = element.parentNode.parentNode.parentNode;
+    productContainer.removeChild(element.parentNode.parentNode);
   }
 });
+
 
 /* Iteration 5 */
 let createBtn = document.getElementById('create');
@@ -50,5 +51,3 @@ createBtn.onclick = function() {
   let tBody = document.querySelector('tbody');
   tBody.insertAdjacentHTML('beforeend', `<tr class="product"><td class="name"><span>${nameProduct.value}</span></td><td class="pu">$<span>${priceProduct.value}</span></td><td class="qty"><label><input type="number" value="0" min="0"></label></td><td class="subtot">$<span>0</span></td><td class="rm"><button class="btn btn-delete">Delete</button></td></tr>`);
 }
-
-
