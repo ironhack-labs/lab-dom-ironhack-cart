e3e4974e8dea39ee5acc456b49eb5e49014b6ec1
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-22 15:15:33 +0100

    Last iterations done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,7 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 var $total = document.querySelector("body > h2 > span");
+var $createBtn = document.querySelector("#create");
 
 
 
@@ -12,7 +13,7 @@ function updateSubtot($product) {
   const subTotal = $product.querySelector(".subtot span")
 
   result = quantity * price;
-  subTotal.innerHTML = result
+  subTotal.innerHTML = result;
   return result;
 }
 
@@ -25,7 +26,7 @@ function calcAll() {
 
   productList.forEach(product => {
     total += updateSubtot(product);
-    $total.innerHTML = total;   
+    $total.innerHTML = total;  
   });
 
 }
@@ -33,5 +34,39 @@ $calc.onclick = calcAll;
 
 // Iteration 4
 
+function deleteProduct(event) {
+  $cart.removeChild(event.target.parentElement.parentElement);
+}
+
+$cart
+.querySelectorAll(".btn-delete")
+.forEach(button => (button.onclick = deleteProduct));
+
+// Iteration 5 
 
 
+function addProduct(event) {
+
+  const addName = document.querySelector(".new input[type=text]");
+  const addPrice = document.querySelector(".new input[type=number]");
+
+  const tr = document.createElement("tr");
+  tr.className = "product";
+  tr.innerHTML = `<td class="name">
+  <span>${addName.value}</span>
+</td>
+<td class="pu">$<span>${addPrice.value}</span></td>
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0" />
+  </label>
+</td>
+<td class="subtot">$<span>0</span></td>
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>`;
+
+  $cart.appendChild(tr);
+}
+
+$createBtn.onclick = addProduct;
