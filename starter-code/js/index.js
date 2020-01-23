8453809a5951feb2165c381fa669d665cac4a808
Gerard Sole <gerardsole@MacBook-Pro-de-Gerard-2.local>
2020-01-23 13:21:35 +0100

    im lost

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,17 +1,30 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+
+ 
+
 function updateSubtot($product) {
   // Iteration 1.1
-let priceUnitary = document.getElementsByClassName('pu')[0].children[0].innerText;
-let quantity = document.getElementsByClassName('qty')[0].children[0].children[0].getAttribute('value');
-let subTotalModfyer = document.getElementsByClassName('subtot')[0].children[0];
+
+let priceUnitary = $product.querySelector('.pu > span').innerHTML;
+let quantity = $product.querySelector('.qty > label > input').value;
+let subTotalModfyer = $product.querySelector('.subtot > span');
 let subTot = priceUnitary * quantity;
 subTotalModfyer.textContent = subTot;
+
 return subTot;
 }
+
 function calcAll() {
   // Iteration 1.2
-  updateSubtot();
+  let priceAll = 0;
+    document.querySelectorAll('.product').forEach( product => {
+        priceAll += updateSubtot(product)
+    });
+    document.querySelector('h2 > span').innerText = priceAll;
 }
+
 $calc.onclick = calcAll;
+
+//iteracion 4 asjhdkasdbkdsjhfbsdhjfnfajkdsjkfadsnfjklads
