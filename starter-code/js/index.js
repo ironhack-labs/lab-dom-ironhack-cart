1ef10a7e45f26d51fea2505a8cb4e6494b2e0f3a
raquelmu <raquel.muniesa@gmail.com>
2020-01-25 00:07:25 +0100

    [BCN-WDPT-1219] - Raquel Muniesa
    Iterations 1, 2 and 3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,31 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
-function updateSubtot($product) {
+function updateSubtot($poduct) {
   // Iteration 1.1
+  let priceUnit = parseFloat($poduct.querySelector(".pu > span").innerHTML);
+  let quantity = parseFloat($poduct.querySelector(".qty > label > input").value);
+  let subtotal = priceUnit * quantity;
+  $poduct.querySelector(".subtot > span").innerHTML = subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  //let btnTotal = $cart.getElementById("calc");
+  let products = document.querySelectorAll(".product");
+    for(let i= 0; i < products.length; i++){
+      updateSubtot(products[i])
+    }
+  let total = 0;
+  let subtotals = document.querySelectorAll(".subtot span");
+  for(let i = 0; i < subtotals.length; i++){
+    console.log(subtotals[i].innerHTML)
+    total += parseFloat(subtotals[i].innerHTML)
+  }    
+  document.querySelector("h2 span").innerHTML = total;
 }
 $calc.onclick = calcAll;
+
+//Iteration 4: 
+
+//Iteration 5: 
