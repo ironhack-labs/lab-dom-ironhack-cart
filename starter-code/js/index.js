fb185bca161237b1b0ee81b49c95eb1db9d33d59
alex <alex@PC-AZUL.localdomain>
2020-01-25 01:07:49 +0100

    done but not working the delete function

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,7 @@ console.clear()
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 var $del = document.getElementsByClassName("btn btn-delete"); //retorna un htmlcollection. Estos contienen los diferentes elementos en forma de lista y para poder acceder hacemos uso del for(foreach no funciona).
-//console.log($del)
 var $add = document.getElementById('create');
-//console.log($add)
 
 function updateSubtot(product) {
   // Iteration 1.1
@@ -45,32 +43,53 @@ function deleteProduct(){
   console.log("Eliminado")
   //como indico el producto que queremos eliminar?
   
-  let parent = document.getElementsByTagName("tbody")
-  let selectProduct = document.getElementsByTagName("tr");
-  let deleteProduct = parent.removeChild(selectProduct);  
+  let selectProduct = document.querySelector('tr');
+  $cart.removeChild(selectProduct);  //error saying that tbody is not a parent of tr?!
 }
 
 function addProduct(){
   console.log("Añadido")
-  let trAdd = document.querySelector(".new").nextSibling;
-  //console.log(trAdd)
-  //let nameOfNewProduct= trAdd.getElementByTagName("input").textContent;
-  //let priceOfNewProduct = trAdd.getElementByTagName("input").value;
+  let nameOfNewProduct = document.querySelector("tfoot>tr>td>input").value;
+  console.log("Esto es el nombre del nuevo producto "+ nameOfNewProduct);
+  let priceOfNewProduct = parseInt(document.querySelector("tfoot>tr>td:nth-child(2)>input").value);
+  console.log("Este es el precio del nuevo producto "+ priceOfNewProduct);
   
-  //para añadir hacer un appendchild al tbody
-  /*
-  let trNode = $cart.createElement("TR");
-  let tdNanemNode = $cart.createElement("TD .name");
-  let productName = $cart.createTextNode(`${nameOfNewProduct}`);
-  $cart.appendChild(productName)
-  */
+  let trNode = document.createElement("tr");
+  trNode.className = "product";
+  const codeOfNewProduct = `
+        <td class="name">
+          <span>${nameOfNewProduct}</span>
+        </td>
+    
+        <td class="pu">
+          $<span>${priceOfNewProduct}</span>
+        </td>
+    
+        <td class="qty">
+          <label>
+            <input type="number" value="0" min="0">
+          </label>
+        </td>
+    
+        <td class="subtot">
+          $<span>0</span>
+        </td>
+    
+        <td class="rm">
+          <button class="btn btn-delete">Delete</button>
+        </td>`
+  trNode.innerHTML = codeOfNewProduct;
+  $cart.appendChild(trNode);
+
+  let clearNameOfNewProduct = document.querySelector("tfoot>tr>td>input")
+  clearNameOfNewProduct.value = ""; //why not is possible with innerHTML?
+  priceOfNewProduct.value = "";
   
+
 }
 
 $calc.onclick = calcAll;
 for (let i=0; i<$del.length; i++){ //la unica forma de poder controlar los botones.
   $del[i].onclick = deleteProduct;
 }
-$add.onClick = addProduct;// no entiendo porque no funciona
-
-addProduct()//acordarse de luego quitarlo
+$add.onclick = addProduct;// no entiendo porque no funciona
