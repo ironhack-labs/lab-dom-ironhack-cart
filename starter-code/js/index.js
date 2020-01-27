3eb4ed8a7b3f5631f37d0f867eb811140bed8a81
alinepasetti <pasettialine@gmail.com>
2020-01-27 16:57:23 +0000

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,50 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let $pricePerUnit = parseFloat($product.querySelector('.pu span').innerText);
+  let $qty = parseInt($product.querySelector('.qty input').value);
+  //Subtotal per product
+  let $productSubtotal = $pricePerUnit * $qty;
+
+  $product.querySelector('.subtot span').innerText = $productSubtotal.toFixed(2);
+  console.log($productSubtotal);
+  return $productSubtotal;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  const $$allProducts = document.querySelectorAll('tr.product');
+  let total = 0;
+  for (let $product of $$allProducts) {
+    let subtotal = updateSubtot($product);
+    total += subtotal;
+  }
+  document.getElementById('subtotal').innerText = total.toFixed(2);
 }
-$calc.onclick = calcAll;
+calcAll();
+
+$calc.addEventListener('click', calcAll);
+
+function deleteItems(event) {
+  let $button = event.target;
+  let $productRow = $button.parentNode.parentNode;
+  let $FullList = $productRow.parentNode;
+  $FullList.removeChild($productRow);
+  calcAll();
+}
+
+const $$AlldeleteButtons = document.querySelectorAll('.btn-delete');
+for (let $deleteButton of $$AlldeleteButtons) {
+  $deleteButton.addEventListener('click', deleteItems);
+}
+
+const $newProductName = document.querySelector(".new input")
+const $newProductPrice = document.querySelector("")
+const $newProductButton = document.querySelector(".new button")
+
+function createNewRow(event) {
+  $button = event.target;
+  $FullList.createElement($productRow)
+}
+
+
+$newProductButton.addEventListener('click', createNewRow)
