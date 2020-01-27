e409d11db49f1da5d84696827a36a5db6896bc79
Felipe Lima <felipenplima@gmail.com>
2020-01-27 15:25:38 +0000

    Iteration 1.1 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,22 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
-
-function updateSubtot($product) {
+const $input = document.querySelector('.prodQty');
+const price = Number(document.querySelector('.pu span').innerText); //$price does not work
+$input.addEventListener('change', updateSubtot);
+function updateSubtot(event) {
   // Iteration 1.1
+  const inputValue = Number($input.value);
+  let total = price * inputValue;
+  let subTotal = document.querySelector('.subtot span');
+  return (subTotal.innerText = total);
 }
-
+//$calc.addEventListener('click', calcAll); => same has bellow line
+$calc.onclick = calcAll;
+const $shoppingList = document.querySelector('.product');
 function calcAll() {
   // Iteration 1.2
+  const $shoppingListItem = document.createElement('tr');
+  let finalTotal = document.querySelector('.cta span');
+  //return (finalTotal.innerText = document.querySelector('.subtot span').innerText);
 }
 $calc.onclick = calcAll;
