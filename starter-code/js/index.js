b4e1f48134312eb3a2b7584b7203bdd1d21de3c2
Joana Cruz <joanamartadacruz@gmail.com>
2020-01-27 13:30:48 +0000

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,23 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let priceUnit = $product.querySelector('.pu span').innerText,
+    numberUnit = $product.querySelector('.qty input').value,
+    totalPrice = priceUnit * numberUnit;
+  
+  $calc.addEventListener('click', (event) => {
+    let $totalButton = document.getElementsByTagName('h2')[0];
+    $totalButton.children[0].innerText += totalPrice;
+    $product.querySelector('.subtot span').innerText = totalPrice;
+  });
+  
+  return totalPrice;
 }
 
 function calcAll() {
   // Iteration 1.2
+  let $$elementsClass = document.getElementsByClassName('product'); 
+  for(let $element in $$elementsClass)
+    updateSubtot($element);
 }
 $calc.onclick = calcAll;
