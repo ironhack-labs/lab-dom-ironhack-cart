ac72f2365c0158b1acdaa241de978376482006f9
André Ferreira <mrawesomeferreira@gmail.com>
2020-01-27 17:37:07 +0000

    Need to finish last iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,86 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+
+  let $unitPrice = $product.querySelector('.pu span').innerText;
+  let $quantity = $product.querySelector('.qty label input').value;
+  let $subtotalSpan = $product.querySelector('.subtot span');
+  let subtotal = parseFloat($unitPrice) * parseInt($quantity);
+
+  $subtotalSpan.innerText = subtotal.toFixed(2);
+
+  return subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  const $$allProducts = document.querySelectorAll('tr.product');
+
+  let total = 0;
+
+  for (let $product of $$allProducts) {
+    let subtotal = updateSubtot($product);
+    total += subtotal;
+  }
+
+  const $totalSpan = document.querySelector('h2 span');
+  $totalSpan.innerText = total.toFixed(2);
 }
-$calc.onclick = calcAll;
+//$calc.onclick = calcAll;
+
+$calc.addEventListener('click', calcAll);
+
+// --------
+
+const deleteProduct = event => {
+  const $button = event.target;
+
+  const $productRow = $button.parentNode.parentNode;
+  const $tableBody = $productRow.parentNode;
+
+  $tableBody.removeChild($productRow);
+};
+
+const $$deleteButtons = document.getElementsByClassName('btn-delete');
+
+for (let $deleteButton of $$deleteButtons) {
+  $deleteButton.addEventListener('click', deleteProduct);
+}
+
+// --------
+
+const $inputNewName = document.querySelector('.new input');
+const $inputNewPrice = document.querySelector('.new input[type="number"]');
+const $newButton = document.querySelector('.new btn');
+
+const createProduct = event => {
+  const name = $inputNewName;
+  const price = $inputNewPrice;
+
+  const $tr = document.createElement('tr');
+  $tr.classList.add('product');
+  $tr.innerHTML += `<tr class="product">
+  <td class="name">
+    <span>${name}</span>
+  </td>
+
+  <td class="pu">$<span>${price}</span></td>
+
+  <td class="qty">
+    <label>
+      <input type="number" value="0" min="0" />
+    </label>
+  </td>
+
+  <td class="subtot">$<span>0</span></td>
+
+  <td class="rm">
+    <button class="btn btn-delete">Delete</button>
+  </td>
+</tr>
+  `;
+  const $removeButton = $tr.querySelector('button');
+  $cart.appendChild($tr);
+};
+
+$newButton.addEventListener('click', createProduct);
