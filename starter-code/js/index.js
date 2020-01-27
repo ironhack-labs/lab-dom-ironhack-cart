7b9849d6b97a75e816107fea3a00f203b6282f02
FilipeAVNunes <filipenunes407@hotmail.com>
2020-01-27 16:52:53 +0000

    NEED TO WORK ON THIS

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,28 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  /*
+  .calculates the subtotal price, from :
+    -the unit price HTML element of the product
+    -the quantity HTML element of the product
+  .updates the HTML with the new product's subtotal
+  .returns the subtotal value
+  */
+  let $price = document.querySelector('.pu span').innerText;
+  let $quantity = document.querySelector('.qty input').value;
+  let subtotal = $price * $quantity;
+
+  let $subtotal = document.querySelector('.subtot span');
+  $subtotal.innerText = subtotal.toFixed(2);
+
+  return subtotal;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  updateSubtot();
 }
-$calc.onclick = calcAll;
+
+// Iteration 1.2
+
+$calc.addEventListener('click', calcAll);
+// Iteration 2
