019a384802cfe6563224f7b8612be41ca0a16f85
andrewnunezbibby <andrew.bibby3@gmail.com>
2020-01-28 15:36:04 +0100

    done [PARWDFT - Andrew]

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,8 +3,8 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product, cb) {
   // Iteration 1.1
+  let price = parseInt($product.querySelector(".pu span").innerText); // can just use Number instead of parseInt
   let quantity = $product.querySelector(".qty label input").value;
-  let price = parseInt($product.querySelector(".pu span").innerText);
 
   $product.querySelector(".subtot span").innerText = price * quantity;
 
@@ -13,28 +13,30 @@ function updateSubtot($product, cb) {
 
 function calcAll() {
   // Iteration 1.2
-  let products = $cart.querySelectorAll("tr");
+  var products = document.querySelectorAll(".product");
   let total = 0;
+  console.log(products);
   products.forEach(product => updateSubtot(product, (sub => total += sub)));
 
   document.querySelector("h2 span").innerText = total;
 }
 
 function newItemBuilder() {
-  let product = event.target.parentElement.parentElement
-  let name = product.querySelector("input[type=text]").value
-  let price = product.querySelector("input[type=number]").value
-  console.log(name)
-  console.log(price)
-  createItem(name, price, 1)
+  // let product = event.target.parentElement.parentElement
+  let product = event.target.closest(".product");
+  console.log(product);
+  let name = product.querySelector("input[type=text]").value;
+  let price = product.querySelector("input[type=number]").value;
+  createItem(name, price)
 }
 
 function newItemListener() {
-  document.querySelector("#create").onclick = newItemBuilder;
+  document.querySelector("#create").onclick = () => newItemBuilder();
 }
 
 function deleteItem() {
-  let row = event.target.parentElement.parentElement;
+  let row = event.target.closest(".product");
+  //thank you Tatijana haha!
   row.parentElement.removeChild(row)
 }
 
@@ -42,8 +44,10 @@ function deleteListeners() {
   document.querySelectorAll(".btn.btn-delete").forEach(button => button.onclick = deleteItem)
 }
 
-function createItem(productName, productPrice, quantity) {
-  let newItem = document.createElement("tr").className = "product";
+function createItem(productName, productPrice) {
+  let newItem = document.createElement("tr")
+  newItem.className = "product";
+  console.log(typeof newItem)
   newItem.innerHTML = `
   <tr class="product">
   <td class="name">
@@ -65,7 +69,7 @@ function createItem(productName, productPrice, quantity) {
 
   <td class="subtot">
     $
-    <span>${updateSubtot(newItem)}</span>
+     <span>${productPrice}</span>
   </td>
 
   <td class="rm">
@@ -73,6 +77,8 @@ function createItem(productName, productPrice, quantity) {
   </td>
 </tr>
   `
+  console.log(document.querySelector("tbody").appendChild(newItem))
+  console.log(typeof newItem)
 }
 $calc.onclick = calcAll;
 deleteListeners();
