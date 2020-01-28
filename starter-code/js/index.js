7d52971035f7ff180be890d59d21379104d73164
Dianabeth <59780877+Dianabeth@users.noreply.github.com>
2020-01-28 01:35:24 +0000

    couldn't get create btn to work

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,81 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $total = document.querySelector('body > h2 > span');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let priceUnit = Number($product.querySelector('.pu span').innerHTML);
+  let quantity = Number($product.querySelector('.qty input').value);
+  let newTotal = priceUnit * quantity;
+
+  let $subTotal = $product.querySelector('.subtot span');
+
+  $subTotal.innerHTML = newTotal;
+
+  return quantity * priceUnit;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  // iterate through the products to get the total
+
+  let total = 0;
+
+  const $productList = $cart.querySelectorAll('.product');
+
+  $productList.forEach(product => (total += updateSubtot(product)));
+  $total.innerHTML = total.toFixed(2);
 }
-$calc.onclick = calcAll;
+
+const $deleteProductRow = document.getElementsByClassName('rm');
+//console.dir(deleteProductRow);
+for (let i = 0; i < $deleteProductRow.length; i++) {
+  let $deleteProductButton = $deleteProductRow[i];
+  $deleteProductButton.addEventListener('click', nowDeleteRow);
+}
+
+function nowDeleteRow(event) {
+  let $deleteBtnClicked = event.target;
+  $deleteBtnClicked.parentElement.parentElement.remove();
+}
+/*I would appreciate some guidance on how to update my total price in the cart with the new total after deletion of any row...thanks*/
+
+let $createNewProducts = document.getElementById('create.btn');
+$btn = document.createElement('button');
+$btn.addEventListener('click', createNewRows);
+
+function createNewRows(event) {
+  let $btn = event.target;
+  let $btnClicked = $btn.parentElement.parentElement;
+  $Btn = $btnClicked.getElementsByClassName('.btn');
+  const $itemsToAdd = `
+  <tfoot>
+    <tr class='new'>
+      <td>
+        <input type='text' />
+        <span> ${name}</span>
+      </td>
+
+      <td class="pu">
+        $<span>${number}</span>
+      </td>
+
+      <td class="qty">
+        <label>
+          <input type='number' value='0' min='0' />
+        </label>
+      </td>
+
+      <td class='subtot'>$<span>0</span></td>
+
+      <td class='rm'>
+        <button id='create' class='btn'>
+          Create
+        </button>
+      </td>
+    </tr>
+  </tfoot>
+  `;
+  $btn.append($itemsToAdd);
+}
+
+$calc.addEventListener('click', calcAll);
+//can not make the create button work yet
