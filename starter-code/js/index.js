bb9e69e072242ddf64644014caf9e8e61d3f731e
FilipeMorais43 <fmoraisyou@gmaile.com>
2020-02-11 15:39:07 +0000

    test

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,32 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+// var $cart = document.querySelector('#cart tbody');
+// var $calc = document.getElementById('calc');
+
+//const $button = document.querySelector('#calc');
+//const $button = document.getElementsByClassName('btn btn-success');
+
+const $button = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  const $price = $product.querySelector('.pu span');
+  const price = Number($price.innerText);
+  //console.dir($price);
+  //console.log(price);
+  const $qty = $product.querySelector('.qty input');
+  //console.dir($qty);
+  const qty = Number($qty.value);
+  const subtotal = price * qty;
+
+  const $subtotal = $product.querySelector('.subtot span');
+  $subtotal.innerText = subtotal.toFixed(2);
+  // console.dir($subtotal);
+  // console.log(subtotal);
 }
 
 function calcAll() {
   // Iteration 1.2
 }
-$calc.onclick = calcAll;
+
+$button.addEventListener('click', () => {
+  //console.log('Hi guys');
+  updateSubtot();
+});
