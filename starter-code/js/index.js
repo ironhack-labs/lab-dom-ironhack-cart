0d3ee586f7cc1d180132ca9af56c8d99c863c103
dan9b6 <dkburton0@gmail.com>
2020-02-11 17:23:02 +0000

    cart lab

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,19 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+$calc.addEventListener('click', () => updateSubtot());
+
 function updateSubtot($product) {
-  // Iteration 1.1
+  const $unitPrice = document.querySelector('.pu > span').innerText;
+  const $quantity = document.querySelector('.qty > label > input').value;
+  let $subTotal = document.querySelector('.subtot > span');
+
+  $subTotal.innerText = $unitPrice * $quantity;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  for ($subtotal of updateSubtot) {
+  }
 }
+
 $calc.onclick = calcAll;
