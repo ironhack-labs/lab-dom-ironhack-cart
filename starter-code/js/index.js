aa83476986f5e9a53b0b1f12d7b6dbfcb44fb77f
Francesco Saccone <francescosaccone95@live.it>
2020-02-18 21:39:08 +0100

    Francesco Marie DOM JAVAS WEBDEV PART TIME BERLIN

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,22 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let cart = document.querySelector('#cart tbody');
+let calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+
+
+
+
+function updateSubtot(product) {
+  let priceUnit=product.querySelectorAll(".pu span")[0].innerText
+  let quantity=product.querySelectorAll("input")[0].value
+  return priceUnit * quantity
+  
 }
-
 function calcAll() {
-  // Iteration 1.2
+  for(let i=0;i<2;i++)
+  {
+  let product=document.getElementsByClassName("product")[i]
+  let subTot=document.querySelectorAll(".subtot span")[i]
+  subTot.innerText=updateSubtot(product,i)
+  }
 }
-$calc.onclick = calcAll;
+calc.onclick = calcAll;
