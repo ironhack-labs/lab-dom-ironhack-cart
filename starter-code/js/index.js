a8d4d1095370beca6849c12fdb0b8e9c0bc27951
PiaG <piagmdf@gmail.com>
2020-02-18 21:32:32 +0100

    got stuck in console, I'm on my way if it comes to understanding

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,32 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
+let $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  // NOTE : $product is ONE element (in this case a "tr" element !! )
+  let price = Number($product.querySelector('.pu').querySelector('span').innerText)
+  console.log(price) 
+
+  let quantity = Number($product.querySelector('.qty').querySelector('input').value)
+  console.log(quantity)
+
+  let subTot = Number($product.querySelector('.subtot').querySelector('span').innerText)
+  console.log(subTot) 
+
+  subTot = price * quantity
+  console.log(price * quantity)
+  return subTot
+
+  // let subTot = Number(document.innerText)
 }
 
 function calcAll() {
+  let headingNew = document.createElement('h1')
+  headingNew.innerText = "let some text flow"
+  document.body.appendChild(headingNew)
+
   // Iteration 1.2
+  // let testCreate = "Hi there!"
+  // document.body.createElement(testCreate)
 }
 $calc.onclick = calcAll;
