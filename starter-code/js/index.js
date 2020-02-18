45ba9dd129d0557818248d480a816c622a8827bd
Solange Ohana <solangeohana@gmail.com>
2020-02-18 21:35:08 +0100

    BER WDPT2020 Solange

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,24 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  let price =  $product.querySelector(".pu span").innerHTML
+  console.log(price)
+  let qty = $product.getElementsByTagName("input")[0].value
+  console.log(qty)
+  let subTot = $product.querySelector(".subtot span").innerHTML
+
+  subTot = qty.value * price
+
+  return $product.querySelector('.subtot span').innerHTML = subTot
 }
 
+
 function calcAll() {
-  // Iteration 1.2
+  let subtot1 = updateSubtot(document.querySelector('.product')[0])
+  let subtot2 = updateSubtot(document.querySelector('.product')[1])
+  let total = 0
+  total = subtot1 + subtot2
+  console.log(total)
 }
 $calc.onclick = calcAll;
+
