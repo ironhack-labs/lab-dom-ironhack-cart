9eb4ca8219c12c0157ee1d60aae179bc774c24c1
PiaG <piagmdf@gmail.com>
2020-02-18 23:39:28 +0100

    calculating works, output doesn't

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -5,27 +5,34 @@ function updateSubtot($product) {
   // Iteration 1.1
   // NOTE : $product is ONE element (in this case a "tr" element !! )
   let price = Number($product.querySelector('.pu').querySelector('span').innerText)
-  console.log(price) 
+  //console.log(price) 
+
+  //preparing for looping
+  // let price = Number($product.querySelectorAll('.pu')[i].querySelector('span').innerText)
 
   let quantity = Number($product.querySelector('.qty').querySelector('input').value)
-  console.log(quantity)
+  //console.log(quantity)
 
-  let subTot = Number($product.querySelector('.subtot').querySelector('span').innerText)
-  console.log(subTot) 
+  //preparing for looping
+  // let quantity = Number($product.querySelectorAll('.qty')[i].querySelector('input').value)
 
-  subTot = price * quantity
-  console.log(price * quantity)
+  let subTot = Number($product.querySelector('.subtot').querySelector('span').innerText = price * quantity)
+  //console.log(subTot) 
+
+  //subTot = price * quantity
+  //console.log(price * quantity)
   return subTot
-
-  // let subTot = Number(document.innerText)
 }
 
 function calcAll() {
-  let headingNew = document.createElement('h1')
-  headingNew.innerText = "let some text flow"
-  document.body.appendChild(headingNew)
-
   // Iteration 1.2
+  let updateSingleSubTot = updateSubtot()
+  console.log(updateSingleSubTot)
+
+  // updateSingleSubTot.innerText
+  // document.createElement('h1')
+  // headingNew.innerText = "let some text flow"
+  // document.body.appendChild(headingNew)
   // let testCreate = "Hi there!"
   // document.body.createElement(testCreate)
 }
