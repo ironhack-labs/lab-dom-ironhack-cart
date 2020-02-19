4cf8e45a9335e27628b841e3c33425ed13bf23f2
Sjard Geiersbach <sjard.g@gmail.com>
2020-02-19 21:22:41 +0100

    WDPT Berlin Sjard, I cant get past iteration 2 can you help? :)

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,32 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var cart = document.querySelector('#cart tbody');
+var calc = document.getElementById('calc');
 
-function updateSubtot($product) {
-  // Iteration 1.1
+
+
+
+
+
+function updateSubtot() {
+total = 0
+
+for (let i = 0; i <= document.getElementsByClassName("pu").length; i++ )
+{ 
+
+  let unitPrice = document.getElementsByClassName("pu")[i].innerText
+  let unitQuantity = document.getElementsByClassName("qty")[i].value
+  subtot = unitPrice * unitQuantity
+document.getElementsByClassName("subtot")[i].innerText = subtot
+total = total + subtot
+
 }
+
+}
+
 
 function calcAll() {
-  // Iteration 1.2
+
+updateSubtot()
+document.getElementsByClassName("total")[0].innerText = "thisisatest"  //if I put this line before the for loop it works just fine (I adjusted the "subtot" and "total" tags a little) but for some reason this line does not run after the function before has been called. Why is that?
+
 }
-$calc.onclick = calcAll;
+calc.onclick = calcAll;
