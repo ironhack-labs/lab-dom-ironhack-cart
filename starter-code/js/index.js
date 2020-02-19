25addbc0186567764e5b4cd74e710c458f5248e1
Loriel Sher <loriel099@gmail.com>
2020-02-19 13:29:12 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,33 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+
+// document.querySelector(".qty input").value
+
+// document.querySelector(".pu span").innerText
+
+// document.querySelector(".subtot span").innerText
+
+
+
 function updateSubtot($product) {
-  // Iteration 1.1
+  let sum = 0
+  let qty = $product.querySelector(".qty input").value;
+  let price = $product.querySelector(".pu span").innerText;
+  sum = price * qty
+  let subtot = $product.querySelector(".subtot span").innerText = sum ;
+  
+  console.log(subtot)
+  // subtot.innerText = qty * pu;
+  // return subtot;
 }
 
+
+
 function calcAll() {
-  // Iteration 1.2
+  let product = document.querySelector(".product")
+  updateSubtot(product)
 }
 $calc.onclick = calcAll;
+
+
