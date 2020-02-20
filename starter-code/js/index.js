98b6bb2759a47d5585777af1bf6d8dcb6b78b24c
Francesco Saccone <francescosaccone95@live.it>
2020-02-20 20:43:55 +0100

    Francesco DOM IRONHACK CART WEBDEVPART Berlin

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,8 +2,7 @@ let cart = document.querySelector('#cart tbody');
 let calc = document.getElementById('calc');
 let buttonDelete1=document.getElementsByClassName("btn-delete")[0]
 let buttonDelete2=document.getElementsByClassName("btn-delete")[1]
-
-
+let buttonCreate=document.getElementById("create")
 
 
 
@@ -14,6 +13,9 @@ function updateSubtot(product) {
   return priceUnit * quantity
   
 }
+
+
+
 function calcAll() {
   let total=0
   for(let i=0;i<document.getElementsByClassName("product").length;i++)
@@ -26,6 +28,8 @@ function calcAll() {
   document.querySelectorAll("h2 span")[0].innerText=total
 }
 
+
+
 let removeProduct=function(event){
   let wholeTable=event.currentTarget.parentNode.parentNode.parentNode
   let productToRemove=event.currentTarget.parentNode.parentNode
@@ -33,7 +37,27 @@ let removeProduct=function(event){
 }
 
 
+
+let createProduct=function(event){
+
+  //Create a copy of <tr> element and taking the values from the user to insert in the new product
+  let addProduct=document.getElementsByClassName("product")[0].cloneNode(true)
+  let nameNewProduct=document.querySelectorAll("tfoot input")[0].value
+  let priceNewProduct=document.querySelectorAll("tfoot input")[1].value
+  let parent=document.getElementsByTagName("tbody")[0]
+
+  //setting the values acquired from the user in the new product
+  addProduct.querySelectorAll("td span")[0].innerText=nameNewProduct
+  addProduct.querySelectorAll(".pu span")[0].innerText=priceNewProduct
+  addProduct.querySelectorAll("button")[0].onclick=removeProduct
+  parent.appendChild(addProduct)
+}
+
+
 calc.onclick = calcAll;
+
 buttonDelete1.onclick=removeProduct
 buttonDelete2.onclick=removeProduct
+buttonCreate.onclick=createProduct
+
 
