47bf2cad0c3e7e761a16f59c2ee552e81b26e3b7
Manuel <MML@TOFU.fritz.box>
2020-02-20 12:50:14 +0100

    subtot for 2nd prod & total. still bad code, gonna redo it all now...

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,20 +1,16 @@
 let cart = document.querySelector('#cart tbody');
 let calc = document.getElementById('calc');
 
-let priceUnit = document.querySelector('.pu span').innerHTML
-let chosenQuant = document.querySelector('.qty input').value
-let subTotal = document.querySelector('.subtot span')
-let inputEvent = document.querySelector('.qty input')
-
-inputEvent.onclick = updateSubtot
+let input1 = document.querySelectorAll('.qty input')[0]
+input1.onchange = updateSubtot
 
 function updateSubtot() {
-    subTotal.innerHTML = Number(priceUnit) * Number(document.querySelector('.qty input').value)
-    console.log(subTotal)
-  }
+  document.querySelectorAll('.subtot span')[0].innerHTML = Number(document.querySelectorAll('.pu span')[0].innerHTML) * Number(document.querySelectorAll('.qty input')[0].value)
+  document.querySelectorAll('.subtot span')[1].innerHTML = Number(document.querySelectorAll('.pu span')[1].innerHTML) * Number(document.querySelectorAll('.qty input')[1].value)
+}
 
 function calcAll() {
-    document.querySelector("h2 span").innerHTML = document.querySelector('.subtot span').innerHTML
+    document.querySelector("h2 span").innerHTML = Number(document.querySelectorAll('.subtot span')[0].innerHTML) + Number(document.querySelectorAll('.subtot span')[1].innerHTML)
 }
 
 calc.onclick = calcAll;
@@ -32,6 +28,9 @@ function addProduct(name, price, position) {
   document.querySelectorAll(".product .qty input")[position].value = 0;
   document.querySelectorAll(".product .subtot span")[position].innerText = 0;
 
-
+  let input2 = document.querySelectorAll('.qty input')[1]
+  input2.onchange = updateSubtot
 }
+
+
 
