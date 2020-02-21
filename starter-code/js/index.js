017248f7cba7771450168c2dddf726ece4ab5569
Yvana Wagter <yvana@wagter.nl>
2020-02-21 17:36:49 +0100

    first commit, third iteration done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,23 @@
-var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
 
-function updateSubtot($product) {
-  // Iteration 1.1
-}
+let prodList = document.querySelectorAll('.product');function updateSubtot ($product) {
+    let unitPrice = parseInt($product.getElementsByClassName('test')[0].innerHTML);
+    let quantity = parseInt($product.getElementsByClassName("unit")[0].value);
+    return unitPrice * quantity;
+  };
+  
+function calcAll (){
+  let totalPrice = 0;
+  for (i = 0; i < prodList.length; i++){
+    prodList[i].getElementsByClassName('subtot')[0].innerHTML = updateSubtot(
+      prodList[i]
+      );
+      totalPrice += parseInt(updateSubtot(prodList[i]));
+  }
+    document.querySelector("#total span").innerHTML = totalPrice;
+  }
 
-function calcAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calcAll;
+  $calc.onclick = calcAll;
+
+
