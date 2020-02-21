64b9fe2f601b8dd65d78f3b2ade9da839db3b6e7
Fábio Costa <fabiodanielmc@gmail.com>
2020-02-21 17:43:59 +0100

    finish till the 3th iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,15 +4,21 @@ var $calc = document.getElementById('calc');
 function updateSubtot($product) {
   // Iteration 1.1
   let value = $product.querySelectorAll(".qty input")[0].value;
-  let price = $product.getElementsByTagName("span")[1].innerHTML;
-  debugger 
+  let price = $product.querySelectorAll(".pu span")[0].innerHTML;
   let subTot = value * price;
-  
+  $product.querySelectorAll(".subtot span")[0].innerHTML = subTot;
+  return subTot;
 }
 
+
 function calcAll() {
-  // Iteration 1.2
-  let firstProduct = document.querySelectorAll(".product")[0];
-  updateSubtot(firstProduct);
+  let productArr = document.querySelectorAll(".product");
+  let total = 0;
+  for (let i = 0; i < productArr.length; i++){
+    total += updateSubtot(productArr[i]);
+  }
+  document.querySelectorAll("#total span")[0].innerHTML = total;
 }
+
 $calc.onclick = calcAll;
+
