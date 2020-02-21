e1145de2261fce5f578445586123fbb436b87e63
Felix van Riessen <felixvanriessengurung@gmail.com>
2020-02-21 15:22:58 +0100

    almost done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,27 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $product_list = document.querySelectorAll('.product')
+
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let pQuantity = $product.querySelector('input').value
+  let pPrice = $product.querySelector('.pu span').innerHTML
+  return pQuantity * pPrice
 }
 
 function calcAll() {
   // Iteration 1.2
+  var tPrice = 0
+  for (let n=0;n<$product_list.length;n++){   
+    $product_list[n].querySelector('.subtot span').innerHTML = updateSubtot($product_list[n])
+    tPrice += updateSubtot($product_list[n])
+  }
+  document.querySelector('#totalPrice span').innerHTML = tPrice
 }
-$calc.onclick = calcAll;
+
+
+// $calc.onclick = calcAll;
+$calc.addEventListener('click', function(){
+  calcAll()
+})
