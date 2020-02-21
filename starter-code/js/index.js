fa9c1bb5d54afaf94fb09f1d71413ce168a3ae71
Fábio Costa <fabiodanielmc@gmail.com>
2020-02-21 13:16:13 +0100

    first commit

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,16 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let value = $product.querySelectorAll(".qty input")[0].value;
+  let price = $product.getElementsByTagName("span")[1].innerHTML;
+  debugger 
+  let subTot = value * price;
+  
 }
 
 function calcAll() {
   // Iteration 1.2
+  let firstProduct = document.querySelectorAll(".product")[0];
+  updateSubtot(firstProduct);
 }
 $calc.onclick = calcAll;
