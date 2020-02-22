8395f9452c5bb1fee7db3b4433d5b2908f47ebec
Shabeer Hussain <shabeerhussain@Shabeers-MacBook-Pro-2.local>
2020-02-22 12:18:42 +0100

    having fun with GIT - not

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,28 @@
+
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  var $cart1 = $product.querySelector(".pu span").innerHTML;
+  var $quantity = $product.querySelector(".qty input").value;
+  var subTotal = $cart1*$quantity
+  $product.querySelector(".subtot span").innerHTML=subTotal
+  return subTotal   
 }
 
+
+//document.getElementById('calc').addEventListener('click',calcAll)
+
 function calcAll() {
-  // Iteration 1.2
+  let $allProducts = document.querySelectorAll(".product");
+  let total = 0;
+  for(let i=0; i<$allProducts.length; i++){
+  total = total + updateSubtot($allProducts[i])
+  console.log($allProducts[i])
+  }
+  document.querySelector("h2 span").innerHTML=total
 }
+
+
 $calc.onclick = calcAll;
+
