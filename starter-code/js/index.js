45176dd3a6398fb6d6c70ba7e88cd886a5af2ccf
Francis Bukenya <frankie.bukenya@gmail.com>
2020-02-22 18:10:44 +0100

    Add 3 complete iterations

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,34 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+let prodList = document.querySelectorAll('.product');
+
 function updateSubtot($product) {
   // Iteration 1.1
+  let value = $product.querySelectorAll('.qty input')[0].value;
+  let price = $product.querySelectorAll('.pu span')[0].innerHTML;
+
+  return price * value;
 }
 
 function calcAll() {
   // Iteration 1.2
+
+  let totalPrice = 0;
+  // debugger
+
+  for (let i = 0; i < prodList.length; i++) {
+    prodList[i].querySelectorAll('.subtot span')[0].innerHTML = updateSubtot(
+      prodList[i]
+    );
+
+    totalPrice += updateSubtot(prodList[i]);
+  }
+  document.querySelector('#grandTotal span').innerHTML = totalPrice;
 }
-$calc.onclick = calcAll;
+
+// $calc.onclick = calcAll;
+
+document.querySelectorAll('#calc')[0].addEventListener('click', function() {
+  calcAll();
+});
