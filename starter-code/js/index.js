8da64fe3dc74ce0fc83dd4f44ebca1c2abc406e8
dkoushha <dimakoushha@gmail.com>
2020-02-22 15:45:28 +0100

    add comments

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,51 +3,66 @@ let qtyInputs = document.getElementsByClassName("qty");
 let deleteBtns = document.getElementsByClassName("btn-delete");
 let createBtn = document.querySelector("#create");
 
-// Iteration 1.1
+// Iteration 1: calculate product subTotal
 function updateSubtot($product) {
+  // the subTotal is price * qty
   let productPrice = $product.querySelector(".pu span").innerText;
   let productQuantity = $product.querySelector("input").value;
-
   let subTot = Number(productPrice) * Number(productQuantity);
+
+  // display the subTotal in the html
   $product.querySelector(".subtot span").innerText = subTot;
   return subTot;
 }
 
-// Iteration 3: Total
+// Iteration 3: Total of all products
 function calcAll() {
+  //
   let sum = 0;
   for (i = 0; i < products.length; i++) {
-    updateSubtot(products[i]);
-    sum += Number(products[i].querySelector(".subtot span").innerText);
+    sum += Number(updateSubtot(products[i]));
   }
   document.querySelector("h2 span").innerText = sum;
 }
 
 //  Iteration 4: Deleting a product and modify the Total
 let onClickHandler = function(e) {
+  // delete product on clicking delete Btn
   let currentButton = e.currentTarget;
+  currentButton.closest("tr").remove();
+
+  // subtract the deleted product's subtot from the total
   let subTot = currentButton.closest("tr").querySelector(".subtot span")
     .innerText;
   document.querySelector("h2 span").innerText -= Number(subTot);
-  currentButton.closest("tr").remove();
 };
 
 // ### Iteration 5: Creating new products
 function creatingNewPro() {
+  // copy a hidden product in the original table as a new product
   let product = document.getElementsByClassName("product")[0];
   let newProduct = product.cloneNode(true);
-  let listOfProducts = document.querySelector("tbody");
+  newProduct.classList.remove("templateProduct");
+
+  // get the new name and price from user
   let newProductName = document.querySelector('tfoot input[type= "text"]')
     .value;
   newProduct.querySelector(".name span").innerText = newProductName;
   let newProductPrice = document.querySelector('tfoot input[type= "number"]')
     .value;
   newProduct.querySelector(".pu span").innerText = newProductPrice;
+
+  // attach the calculation function and delete function to deleteBtn and qtyInput
   let qtyInput = newProduct.querySelector(".qty");
   qtyInput.onchange = calcAll;
   let deleteBtn = newProduct.querySelector(".btn-delete");
   deleteBtn.onclick = onClickHandler;
+
+  // add new product to the table
+  let listOfProducts = document.querySelector("tbody");
   listOfProducts.appendChild(newProduct);
+
+  // clear input fields
   document.querySelector('tfoot input[type= "number"]').value = 0;
   document.querySelector('tfoot input[type= "text"]').value = "";
 }
@@ -55,7 +70,6 @@ function creatingNewPro() {
 ///////////////
 
 // attach calculation function to all quantity input boxes
-
 for (let i = 0; i < qtyInputs.length; i++) {
   qtyInputs[i].onchange = calcAll;
 }
@@ -64,4 +78,6 @@ for (let i = 0; i < qtyInputs.length; i++) {
 for (let i = 0; i < deleteBtns.length; i++) {
   deleteBtns[i].onclick = onClickHandler;
 }
+
+// attach the creat function to the create btn
 createBtn.onclick = creatingNewPro;
