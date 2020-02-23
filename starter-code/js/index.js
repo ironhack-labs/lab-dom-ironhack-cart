66704c087f107378b25948d1d415bf9ea8a5067e
unknown <ahabey7@gmail.com>
2020-02-23 11:45:28 +0100

    Third Iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,46 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var firstrow = document.querySelectorAll('.product')[0];
 
 function updateSubtot($product) {
+  
   // Iteration 1.1
+  let unitprice = $product.querySelector('.pu span').innerHTML;
+  let quantity = $product.querySelector('.qty input').value;
+
+  //calculate product
+  let subtotal = unitprice*quantity;
+
+  console.log(unitprice);
+  console.log(quantity);
+
+  $product.querySelectorAll('.subtot span')[0].innerHTML=subtotal;
+  console.log(subtotal);
+  return subtotal;
+  
 }
 
 function calcAll() {
-  // Iteration 1.2
-}
+//number of rows
+    var rows = document.querySelectorAll('.product').length;
+    let total = 0;
+  
+     //loop through rows and add subtotal to sum
+     for (var i = 0; i<rows; i++) {
+      // get ith row, update total
+                var irow= document.querySelectorAll('.product')[i];
+                total += updateSubtot(irow);  
+     }
+     
+     //change total in html
+     document.querySelectorAll('h2 span')[0].innerHTML = total;
+     return total;
+   }
+
+
+
 $calc.onclick = calcAll;
+
+updateSubtot(firstrow);
+console.log(firstrow);
+calcAll();
