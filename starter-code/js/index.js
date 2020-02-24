dd4ec204652db19727023993be937a78e90181b1
“Alexandra <alex.muskovszky@gmail.com>
2020-02-24 09:36:24 +0100

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,19 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $productCount = document.getElementsByTagName("tbody")[0].childElementCount;
+let $total = 0;
 
-function updateSubtot($product) {
+function updateSubtot($product, i) {
   // Iteration 1.1
+  return parseInt(document.querySelectorAll('.pu span')[i].innerHTML) * parseInt(document.querySelectorAll("input")[i].value);
 }
 
 function calcAll() {
-  // Iteration 1.2
+  for (let i = 0; i < $productCount; i++) {
+    let $product = document.getElementsByClassName('product')[i].innerHTML;
+    let $subtotal = document.querySelectorAll('.subtot span')[i].innerHTML = updateSubtot($product, i);
+    $total += $subtotal;
+  }
+  document.querySelector('h2 span').innerHTML = $total;
 }
 $calc.onclick = calcAll;
