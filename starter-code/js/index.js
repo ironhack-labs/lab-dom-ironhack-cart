5216b0611b451f5461abc1f09e9faa90e648cb25
Manuel <MML@TOFU.fritz.box>
2020-02-24 22:29:44 +0100

    except new prod delete button

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -22,22 +22,18 @@ calc.onclick = calcAll;
 
 
 // delete row
-let allProducts = document.querySelector("tbody")
-let singleProduct = document.querySelector(".product")
 
 let deleteButton0 = document.getElementsByTagName("button")[0]
 let deleteButton1 = document.getElementsByTagName("button")[1]
 
-
-
 let onClickHandler = function(e) {
   console.log("button works")
-  e.currentTarget = allProducts.deleteRow(e)
+  let buttonRow = e.currentTarget.parentNode.parentNode
+  buttonRow.remove()
 }
 
 deleteButton0.onclick = onClickHandler
 deleteButton1.onclick = onClickHandler
-
 
 // new product
 let newProduct = document.querySelector(".new")
@@ -46,17 +42,18 @@ let addSection = document.querySelector("tfoot")
 let createButton = document.querySelector("#create")
 
 let newProdHandler = function() {
-  console.log("create that stuff")
 
-  let inputText = newProduct.querySelectorAll("input")[0].value
-  let inputPriceUnit = newProduct.querySelectorAll("input")[1].value
+  let inputText = newProduct.querySelectorAll("input")[0]
+  let inputPriceUnit = newProduct.querySelectorAll("input")[1]
   let newPr = singleProduct.cloneNode(true)
 
-  newPr.querySelector(".name span").innerText = inputText
-  newPr.querySelector(".pu span").innerText = inputPriceUnit
-
-
+  newPr.querySelector(".name span").innerText = inputText.value
+  newPr.querySelector(".pu span").innerText = inputPriceUnit.value  
   allProducts.appendChild(newPr)
 }
+
+// doesnt work 
+// let deleteButton2 = document.getElementsByTagName("button")[2]
+// deleteButton2.onclick = onClickHandler
 
 createButton.onclick = newProdHandler
