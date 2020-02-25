da014a138301b4182e269ff54b5c7624eece4a10
joaquim pienda <joaquimpineda@gmail.com>
2020-02-25 17:26:51 +0100

    delete working

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,47 @@
-var $cart = document.querySelector('#cart tbody');
-var $calc = document.getElementById('calc');
+var cart = document.querySelector('#cart tbody');
+var calc = document.getElementById('calc');
+let cantidad = document.querySelectorAll('.qty input')
+let borrar = document.querySelectorAll(".btn-delete")
 
-function updateSubtot($product) {
+
+function updateSubtot(articuloActual) {
+  let preciounitario = parseInt(document.querySelectorAll('.pu span')[articuloActual].innerHTML)
+  let cantidad = parseInt(document.querySelectorAll('.qty input')[articuloActual].value)
+  document.querySelectorAll('.subtot span')[articuloActual].innerHTML = preciounitario * cantidad
   // Iteration 1.1
 }
 
 function calcAll() {
   // Iteration 1.2
+  
+  let preciounitario = document.querySelectorAll('.subtot span')
+  let resultado = 0
+  for (let index = 0; index < preciounitario.length; index++) {
+    updateSubtot(index)
+    resultado += parseInt(preciounitario[index].innerHTML);
+  }
+  console.log(document.querySelectorAll("h2 span"))
+  document.querySelectorAll("h2 span")[0].innerText = resultado
+
 }
-$calc.onclick = calcAll;
+function newArticle(){
+
+}
+function borrarArticulo(e){
+  console.log(e.currentTarget)
+  e.currentTarget.parentElement.parentElement.remove()
+}
+
+function newButtonBorrarEvent(){
+  for(var i = 0; i < borrar.length; i++){
+    // console: imprime el elemento pulsado <p> 
+    borrar[i].addEventListener('click',borrarArticulo, false);
+  }
+}
+
+
+newButtonBorrarEvent()
+cantidad.onclick = calcAll;
+calc.onclick = calcAll;
+console.log(cart)
+console.log(calc)
