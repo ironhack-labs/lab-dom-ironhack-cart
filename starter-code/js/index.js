63363ab454436994f5e60351d3df675f88bd1fff
Damaris Goebel <damaris.goebel@gmx.de>
2020-02-25 17:19:01 +0100

    first try

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,10 +2,24 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  // Iteration 1.1
+  // Iteration 1.1 
+
+let value = $product.querySelectorAll(".qty input")[0].value;
+let price = $product.querySelectorAll(".pu span")[0].innerHTML;
+let subTot = value * price;
+$product.querySelectorAll(".subtot span")[0].innerHTML = subTot;
+return subTot;
+
 }
 
 function calcAll() {
   // Iteration 1.2
+  let products = document.querySelectorAll(".product");
+  let total = 0;
+  for (let i = 0; i < products.length; i++){
+    total += updateSubtot(products[i]);
+  }
+$total.innerHTML = totals
 }
 $calc.onclick = calcAll;
+
