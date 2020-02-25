a2d6db18d67ac42d12b66a7d9af1a2af07735adc
Adrian Vide <adrian.vide.paris@gmail.com>
2020-02-26 00:00:46 +0100

    pretty much done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,8 +1,14 @@
 var $cart = document.querySelector("#cart tbody");
 var $calc = document.getElementById("calc");
+var $create = document.getElementById('create');
+var $delete = document.getElementsByClassName('btn btn-delete');
 
 let prodPrice = document.querySelectorAll(".pu span");
 let prodQty = document.querySelectorAll(".qty input");
+let table = document.querySelector("tbody");
+
+// let whatever = document.querySelectorAll('.subtot span')
+
 
 function updateSubtot($product) {
   let result = prodPrice[$product].innerHTML * prodQty[$product].value;
@@ -16,34 +22,48 @@ function updateSubtot($product) {
 }
 
 function calcAll() {
+  allSubs();
+  sumSubs();
 
-  let subTotSum = updateSubtot(0);
 
-  return document.querySelector("h2 span").innerHTML = subTotSum;
+  return document.querySelector("h2 span").innerHTML = sum;
 
   // Iteration 1.2
 }
 $calc.onclick = calcAll;
-
+$create.onclick = addProduct;
 
 function addProduct() {
+  const name = document.querySelector(".new input").value;
+  const price = document.querySelectorAll(".new input")[1].value;
 
+  let newRow = document.querySelector("tbody tr").cloneNode(true);
 
-
-}
-
-function createRow(){
-
-  const name = document.querySelector('.new input').value
-  const price = document.querySelectorAll('.new input')[1].value
-
-  let table = document.querySelector("tbody")
-  let newRow = document.querySelector("tbody tr").cloneNode(true)
-  
   newRow.querySelector(".name span").innerHTML = name;
   newRow.querySelector(".pu span").innerHTML = price;
-  
-  table.appendChild(newRow)
+
+  table.appendChild(newRow);
 
   // setDeleteEventListener()
 }
+
+
+function allSubs() {
+  for (i = 0; i <= table.rows.length - 1; i++) {
+    updateSubtot(i);
+  }
+}
+
+let sum = 0;
+function sumSubs() {
+  for (i = 0; i< table.rows.length; i++) {
+    const rowValue = document.querySelectorAll('.subtot span')[i].innerHTML;
+    sum = sum + parseInt(rowValue)    
+  }
+  return sum;
+}
+
+// $delete.onclick = document.getElementById('cart').deleteRow(????)
+
+
+// El cálculo de subtotales y precios funcionan hasta el momento en el que se añade un producto nuevo con el botón, después ya no. 
