a13e38b53ecec1f4375c71ab50c4b90e1c402dd3
jsblanco <59490817+jsblanco@users.noreply.github.com>
2020-02-25 22:18:10 +0100

    Removed clutter
    Had some problems syncing through the terminal due to deleting unnecessary file, couldnt push normally, so I just pasted the code after refining so as to not have to calculate arrays endlessly every time.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,66 +1,51 @@
 var $cart = document.querySelector("#cart tbody");
 var $calc = document.getElementById("calc");
-let price = document.querySelectorAll(".pu span");
-let amount = document.querySelectorAll(".qty input");
-let subtotal = document.querySelectorAll(".subtot span");
-let total = document.querySelectorAll("h2 span")[0];
-let table = document.getElementsByTagName("tbody");
-let newRow = document.getElementsByClassName("new");
+let table = document.querySelector("tbody")
 let deleteButton = document.getElementsByClassName("btn-delete");
 let createButton = document.getElementById("create");
 let calculateButton = document.getElementsByClassName("btn-success");
 
+$calc.onclick = () => calcAll();
+createButton.onclick = () => createNew();
+calculateButton.onclick = () => calcAll();
 
 function calcAll() {
   let totalAmount = 0;
-  for (let i = 0; i < subtotal.length; i++) {
-    updateSubtot(i);
-    totalAmount += parseInt(subtotal[i].innerHTML);
+  let subtotal = 0;
+  let total = document.querySelectorAll("h2 span")[0];
+  for (let i = 0; i < table.rows.length; i++) {
+    subtotal = updateSubtot(i);
+    totalAmount += parseInt(subtotal);
   }
   total.innerHTML = totalAmount;
 }
 
 function updateSubtot(i) {
+  let price = document.querySelectorAll(".pu span");
+  let amount = document.querySelectorAll(".qty input");
+  let subtotal = document.querySelectorAll(".subtot span");
   let result = price[i].innerHTML * amount[i].value;
-  return (subtotal[i].innerHTML = result);
+  subtotal[i].innerHTML = result;
+  return result;
 }
-
 
 function createNew(){
   let name = document.querySelectorAll(".new input")[0].value;
   let value = document.querySelectorAll(".new input")[1].value;
-  let table = document.querySelector("tbody")
   let newRow = document.querySelector("tbody tr").cloneNode(true)
   newRow.querySelector(".name span").innerHTML = name;
   newRow.querySelector(".pu span").innerHTML = value;
   newRow.querySelector(".qty input").value = 0;
   table.appendChild(newRow)
   refresh();
-  calcAll();
 }
 
 function deleteRow(index) {
-  table[0].deleteRow(index);
+  table.deleteRow(index);
   refresh();
 }
 
-$calc.onclick = calcAll;
-
-createButton.onclick = () => createNew();
-
-calculateButton.onclick = () => calcAll();
-
-//Todas mis funciones dependen del array de valores, así que necesito que sus índices estén actualizados
 function refresh() {
-  price = document.querySelectorAll(".pu span");
-  amount = document.querySelectorAll(".qty input");
-  subtotal = document.querySelectorAll(".subtot span");
-  total = document.querySelectorAll("h2 span")[0];
-  table = document.getElementsByTagName("tbody");
-  newRow = document.getElementsByClassName("new");
-  deleteButton = document.getElementsByClassName("btn-delete");
-  createButton = document.getElementById("create");
-  calculateButton = document.getElementsByClassName("btn-success");
   for (let i = 0; i < deleteButton.length; i++) {
     updateSubtot(i);
     deleteButton[i].onclick = () => deleteRow(i);
