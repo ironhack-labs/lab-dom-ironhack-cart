6b80b966ba33c7a2ded67759aa1a45af6320ad32
Sjard Geiersbach <sjard.g@gmail.com>
2020-02-25 19:33:52 +0100

    Berlin WD PT Sjard

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,32 +1,56 @@
 var cart = document.querySelector('#cart tbody');
 var calc = document.getElementById('calc');
-
-
-
+let deleteButtons = document.getElementsByClassName("btn-delete");
+let createButton = document.getElementById("create")
 
 
 
 function updateSubtot() {
-total = 0
-
-for (let i = 0; i <= document.getElementsByClassName("pu").length; i++ )
+for (let i = 0; i <= document.getElementsByClassName("pu").length -1; i++ )
 { 
 
   let unitPrice = document.getElementsByClassName("pu")[i].innerText
   let unitQuantity = document.getElementsByClassName("qty")[i].value
   subtot = unitPrice * unitQuantity
 document.getElementsByClassName("subtot")[i].innerText = subtot
-total = total + subtot
-
-}
-
+total += subtot
+} 
 }
 
 
 function calcAll() {
-
+total = 0
 updateSubtot()
-document.getElementsByClassName("total")[0].innerText = "thisisatest"  //if I put this line before the for loop it works just fine (I adjusted the "subtot" and "total" tags a little) but for some reason this line does not run after the function before has been called. Why is that?
-
+document.getElementsByClassName("total")[0].innerText = total 
 }
-calc.onclick = calcAll;
+
+function deleteHandler(event) {
+ let deletedRow = event.currentTarget.parentNode.parentNode
+ deletedRow.remove()
+}
+
+
+function create() {
+
+var oldRow = document.getElementsByClassName("product")[0];
+;
+
+let newItem = document.getElementsByClassName("new")[0].parentNode.prepend(oldRow.cloneNode(true));
+document.getElementsByClassName("name")[document.getElementsByClassName("name").length -1].innerText = document.getElementById("newItem").value
+document.getElementsByClassName("pu")[document.getElementsByClassName("name").length -1].innerText = document.getElementById("newPrice").value
+}
+
+
+createButton.onclick = create
+calc.onclick = calcAll
+
+
+deleteButtons[0].onclick = deleteHandler
+deleteButtons[1].onclick = deleteHandler
+
+
+
+
+
+
+
