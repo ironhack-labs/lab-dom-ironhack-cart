b7278070dd3336aa3a3c0bd7e7ace923d0377fb1
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-02-25 18:47:04 +0100

    Completed, although there must be a better way

--- starter-code/js/tryingAltWay.js
+++ starter-code/js/tryingAltWay.js
@@ -4,25 +4,25 @@ let price = document.querySelectorAll(".pu span");
 let amount = document.querySelectorAll(".qty input");
 let subtotal = document.querySelectorAll(".subtot span");
 let total = document.querySelectorAll("h2 span")[0];
-let table = document.getElementsByTagName("tbody");
+let table = document.querySelector("tbody");
 let newRow = document.getElementsByClassName("new");
 let deleteButton = document.getElementsByClassName("btn-delete");
 let createButton = document.getElementById("create");
 let calculateButton = document.getElementsByClassName("btn-success");
 
-function updateSubtot($product) {
-  let result = price[$product].innerHTML * amount[$product].value;
-  return (subtotal[$product].innerHTML = result);
-}
+function updateSubtot() {
+  for (let i=0; i< table.rows.length; i++){
+  let result = price[i].innerHTML * amount[i].value;
+  return (subtotal[i].innerHTML = result);
+}}
 
 function calcAll() {
   let totalAmount = 0;
-  for (let i = 0; i < subtotal.length; i++) {
+  for (let i = 0; i < table.rows.length; i++) {
     updateSubtot(i);
     totalAmount += parseInt(subtotal[i].innerHTML);
   }
   total.innerHTML = totalAmount;
-  refresh();
 }
 
 let newName = document.querySelectorAll(".new input")[0];
@@ -32,8 +32,7 @@ function createNew() {
   let name = document.querySelectorAll(".new input")[0].value;
   let value = document.querySelectorAll(".new input")[1].value;
   let template = `<tr class="product"><td class="name"><span>${name}</span></td><td class="pu">$<span>${value}</span></td><td class="qty"><label><input type="number" value="0" min="0" /></label></td><td class="subtot">$<span>0</span></td><td class="rm"><button class="btn btn-delete">Delete</button></td></tr>`;
-  table[0].innerHTML += template;
-  refresh();
+  table.innerHTML += template;
   calcAll();
 }
 
@@ -43,17 +42,8 @@ function deleteRow(index) {
 }
 
 
-//Todas mis funciones dependen del array de valores, así que necesito que sus índices estén actualizados
+//Todas mis funciones dependen del array de valores, así que necesito que sus índices 
 function refresh() {
-  price = document.querySelectorAll(".pu span");
-  amount = document.querySelectorAll(".qty input");
-  subtotal = document.querySelectorAll(".subtot span");
-  total = document.querySelectorAll("h2 span")[0];
-  table = document.getElementsByTagName("tbody");
-  newRow = document.getElementsByClassName("new");
-  deleteButton = document.getElementsByClassName("btn-delete");
-  createButton = document.getElementById("create");
-  calculateButton = document.getElementsByClassName("btn-success");
   for (let i = 0; i < deleteButton.length; i++) {
     updateSubtot(i);
     deleteButton[i].onclick = () => deleteRow(i);
