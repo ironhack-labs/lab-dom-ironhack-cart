a8fdbfc049588aaaf8941f0aa59453073ed3d471
Alberto Quiñonero Ayllon <albertoquinoneroayllon@MacBook-Pro-de-Alberto-2.local>
2020-02-25 20:34:50 +0100

    mi mente ha petado

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,46 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
-
 function updateSubtot($product) {
-  // Iteration 1.1
+ let priceUnit= $product.querySelector('.pu span').innerHTML;
+ let theQuantity= $product.querySelector('.qty input').value;
+ let subTotal= $product.querySelector('.subtot span');
+  subTotal.innerHTML= Number(priceUnit *theQuantity);
+  return subTotal
 }
-
+function createRow(){
+  const name = document.querySelector('.new .name').value
+  const price = document.querySelector('.new .price').value
+  let table = document.querySelector("tbody")
+  let newRow = document.querySelector("tbody tr").cloneNode(true)
+  newRow.querySelector(".name span").innerHTML = name;
+  newRow.querySelector(".pu span").innerHTML = price;
+  table.appendChild(newRow)
+  setDeleteEventListener();
+}
 function calcAll() {
-  // Iteration 1.2
+  var row = document.querySelectorAll('.product');
+  let total = document.querySelector('h2 span')
+  let count = 0;
+  for (let i = 0; i < row.length; i++){
+    count = count + parseInt(updateSubtot(row[i]).innerHTML)
+   
+  }
+  total.innerHTML = count
+  return total
 }
-$calc.onclick = calcAll;
+$calc.onclick = calcAll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
