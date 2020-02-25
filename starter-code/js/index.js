cdad747e1925242c772716b8fb60bf433f8e2824
Lou08 <l.casals86@gmail.com>
2020-02-25 22:56:20 +0100

    envio cart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,9 +3,50 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let input = document.querySelectorAll('.qty input')[$product].value;
+  let priceUnit = document.querySelectorAll('.pu span')[$product].innerHTML;
+  let subTotal = input * priceUnit; 
+  document.querySelectorAll('.subtot span')[$product].innerHTML = subTotal;
+return subTotal
+}
+
+document.getElementById('create').onclick = newRow;
+
+
+function newRow(){
+let newProduct = document.querySelector('.new input[type="text"]');
+let newPriceUnit = document.querySelector(' .new input[type="number"]');
+let newRow = document.createElement('tr');
+newRow.className = 'fila';
+newRow.innerHTML = `<td class="name">
+<span>${newProduct.value}</span>
+</td>
+
+<td class="pu">$<span>${newPriceUnit.value}</span></td>
+
+<td class="qty">
+<label>
+  <input type="number" value="0" min="0" />
+</label>
+</td>
+
+<td class="subtot">$<span>0</span></td>
+
+<td class="rm">
+<button class="btn btn-delete">Delete</button>
+</td>`
+$cart.appendChild(newRow);
 }
 
 function calcAll() {
-  // Iteration 1.2
+  let total = 0;
+  let arraytotal = document.querySelectorAll('.subtot span');
+  for (let i = 0; i < arraytotal.length; i++) {
+    
+    total+=updateSubtot(i);    
+  }
+  document.querySelectorAll('h2 span')[0].innerHTML = total;
 }
+//$calc.onclick = updateSubtot;
 $calc.onclick = calcAll;
+//console.log('hola');
