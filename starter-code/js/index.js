b1ec07b036d487d70ba0f366c25f0ed6ac905724
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-02-25 20:26:54 +0100

    Totales y Subtotales

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,26 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var product = document.querySelector('.product');
+var $create = document.getElementById("create");
 
 function updateSubtot($product) {
-  // Iteration 1.1
-}
+	let unitPrice = $product.querySelector('.pu span').innerText;
+	let quantity = $product.querySelector('input').value;
+	let subTotal = parseFloat(unitPrice) * parseFloat(quantity);
+	$product.querySelector(".subtot span").innerText = subTotal;
+	return subTotal
+  }
 
-function calcAll() {
-  // Iteration 1.2
-}
-$calc.onclick = calcAll;
+  function calcAll() {
+	let total = 0;
+	let arrayProducts = document.querySelectorAll(".product");
+	for (let i = 0; i < arrayProducts.length; i++) {
+	  let subTotal = updateSubtot(arrayProducts[i]);
+	  total += subTotal;
+	}
+	let totalElement = document.querySelector("h2 > span");
+	totalElement.innerHTML = `${total}`;
+  }
+
+  $calc.onclick = calcAll;
+ 
