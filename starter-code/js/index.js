4a7d0d2344775fe6e8b293d929b9e450dcd9ae7a
Zetzher <fyeah16@gmail.com>
2020-02-25 20:57:22 +0100

    Done...

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,69 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+var $createButt = document.getElementById('create');
+
+
+
 function updateSubtot($product) {
-  // Iteration 1.1
+  let priceUnity = document.querySelectorAll('.pu span')[$product].innerHTML
+  let quantity = document.querySelectorAll('.qty input')[$product].value
+  let subtot_ = priceUnity * quantity;
+  document.querySelectorAll('.subtot span')[$product].innerHTML = subtot_
+  return subtot_
 }
 
-function calcAll() {
-  // Iteration 1.2
+
+
+function addItem() {
+  const name = document.querySelector('.new .name').value
+  const price = document.querySelector('.new .price').value
+  let table = document.querySelector('tbody');
+  let tr = document.createElement('tr');
+  tr.innerHTML = `<tr class="product">
+  <td class="name">
+  <span>${name}</span>
+</td>
+
+<td class="pu">$<span>${price}</span></td>
+
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0" />
+  </label>
+</td>
+
+<td class="subtot">$<span>0</span></td>
+
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>
+</tr>`
+
+  table.appendChild(tr);
+  for (let i = 0; i < $delete.length; i++) {
+    $delete[i].onclick = (e => e.currentTarget.parentElement.parentElement.remove());
+  }
 }
+
+
+
+function calcAll() {
+  let priceUnity = document.querySelectorAll('.pu span')
+  let resultado = 0;
+  for (let i = 0; i < priceUnity.length; i++) {
+    resultado += parseInt(updateSubtot(i))
+  }
+  document.querySelectorAll('h2 span')[0].innerHTML = resultado;
+  return resultado
+}
+
 $calc.onclick = calcAll;
+$createButt.onclick = addItem;
+
+
+$delete = document.getElementsByClassName('btn-delete');
+
+for (let i = 0; i < $delete.length; i++) {
+  $delete[i].onclick = (e => e.currentTarget.parentElement.parentElement.remove());
+}
