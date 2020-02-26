69d4d3b374332622f78f06107dee066f5cf75729
jsblanco <59490817+jsblanco@users.noreply.github.com>
2020-02-26 09:23:52 +0100

    Corrected two minor mistakes from decluttering
    Initial delete button now works again on webpage load
    Website will create new rows even if all rows are deleted

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,6 @@
 var $cart = document.querySelector("#cart tbody");
 var $calc = document.getElementById("calc");
-let table = document.querySelector("tbody")
+let table = document.querySelector("tbody");
 let deleteButton = document.getElementsByClassName("btn-delete");
 let createButton = document.getElementById("create");
 let calculateButton = document.getElementsByClassName("btn-success");
@@ -29,14 +29,21 @@ function updateSubtot(i) {
   return result;
 }
 
-function createNew(){
+function createNew() {
+  if (table.length >0){
   let name = document.querySelectorAll(".new input")[0].value;
   let value = document.querySelectorAll(".new input")[1].value;
-  let newRow = document.querySelector("tbody tr").cloneNode(true)
+  let newRow = document.querySelector("tbody tr").cloneNode(true);
   newRow.querySelector(".name span").innerHTML = name;
   newRow.querySelector(".pu span").innerHTML = value;
   newRow.querySelector(".qty input").value = 0;
-  table.appendChild(newRow)
+  table.appendChild(newRow);
+  }
+  else 
+  {  let name = document.querySelectorAll(".new input")[0].value;
+  let value = document.querySelectorAll(".new input")[1].value;
+  let template = `<tr class="product"><td class="name"><span>${name}</span></td><td class="pu">$<span>${value}</span></td><td class="qty"><label><input type="number" value="0" min="0" /></label></td><td class="subtot">$<span>0</span></td><td class="rm"><button class="btn btn-delete">Delete</button></td></tr>`;
+  table.innerHTML += template;}
   refresh();
 }
 
@@ -53,3 +60,7 @@ function refresh() {
   calcAll();
 }
 
+
+window.onload = function() {
+  refresh();
+};
