3e027cf650f608e5539c916cfe20985377e245ed
Alex <alexgarcia1771@outlook.com>
2020-02-26 09:22:55 +0100

    Alexandre

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,29 @@
-var $cart = document.querySelector('#cart tbody');
+var $cart = document.getElementById('#cart tbody');
 var $calc = document.getElementById('calc');
+let createButton = document.getElementById('create');
 
 function updateSubtot($product) {
-  // Iteration 1.1
-}
+let producto = document.querySelector('.pu span').innerText * document.querySelector('.qty input').value;
+let subtotal = document.querySelector('.subtot span');
+console.log(subtotal.innerHTML = producto);
 
+}
+function createRow(){
+  const name = document.querySelector('.new td input').value;
+  const price = document.querySelector('.new td .price').value;
+  let table = document.querySelector("tbody");
+  let newRow = document.querySelector("tbody tr").cloneNode(true);
+  newRow.querySelector(".name span").innerHTML = name;
+  newRow.querySelector(".pu span").innerHTML = price;
+  table.appendChild(newRow);
+  table.appendChild(newRow);
+  
+}
 function calcAll() {
-  // Iteration 1.2
+  updateSubtot();
+  
+
+
 }
 $calc.onclick = calcAll;
+createButton.onclick = createRow;
