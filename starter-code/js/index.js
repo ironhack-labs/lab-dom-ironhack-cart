89e506ab9b1752f87c9c3384b834d955b7e1e3d9
Justin Peeters <Peeters.justin@yahoo.com>
2020-02-28 14:54:12 +0100

    IronHackCart

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,29 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var randomProductRow = document.querySelectorAll(".product")
 
+
+// Iretation 1.1
 function updateSubtot($product) {
-  // Iteration 1.1
+    let pQuantity = $product.querySelector('.qty input').value;
+    let pPrice = $product.querySelector('.pu span').innerHTML;
+    return pQuantity * pPrice;
 }
+
+// Iteration 1.2
+
 
 function calcAll() {
-  // Iteration 1.2
+    var pTotal = 0
+    for (let i = 0; i < randomProductRow.length; i++) {
+        randomProductRow[i].querySelector('.subtot span').innerHTML = updateSubtot(randomProductRow[i])
+        pTotal += updateSubtot(randomProductRow[i])
+    }
+    document.querySelector('#totalAmount span').innerHTML = pTotal
 }
-$calc.onclick = calcAll;
+
+
+$calc.addEventListener("click", function() {
+    calcAll()
+
+})
