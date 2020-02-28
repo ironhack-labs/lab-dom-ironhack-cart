92535a905d2e23a02243e8a2b9567de2a293bc1e
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-02-28 15:14:47 +0100

    attempt number three

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,66 @@
-var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+let $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
 
+let prodList = document.querySelectorAll('.product');function updateSubtot ($product) {
+    let unitPrice = parseInt($product.getElementsByClassName('test')[0].innerHTML);
+    let quantity = parseInt($product.getElementsByClassName("unit")[0].value);
+    return unitPrice * quantity;
+  };
+
+function calcAll (){
 function updateSubtot($product) {
-  // Iteration 1.1
+  let unitPrice = parseInt($product.querySelector(".pu span").innerHTML);
+  let quantity = parseInt($product.querySelector(".qty input").value);
+  let subTotal = unitPrice * quantity;
+  return subTotal;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  var $calc = document.getElementById('calc');
+let $calc = document.getElementById('calc');
+let $cart = document.querySelector('#cart tbody');
+
+let prodList = document.querySelectorAll('.product');function updateSubtot ($product) {
+    let unitPrice = parseInt($product.getElementsByClassName('test')[0].innerHTML);
+    let quantity = parseInt($product.getElementsByClassName("unit")[0].value);
+    return unitPrice * quantity;
+  };
+
+function calcAll (){
+function updateSubtot($product) {
+  let unitPrice = parseInt($product.querySelector(".pu span").innerHTML);
+  let quantity = parseInt($product.querySelector(".qty input").value);
+  let subTotal = unitPrice * quantity;
+  return subTotal;
 }
-$calc.onclick = calcAll;
+
+function calcAll() {let deletepushedButton = event.target;
+  let productToRemove = deletepushedButton.closest(".product")
+  $cart.removeChild(productToRemove);
+  calcAll ()
+}
+
+let createButton = document.getElementById("create")
+createButton.addEventListener("click", createProduct)
+
+function createProduct (){
+  let descriptionInput = document.querySelector(".new input.productName");
+  let priceInput = document.querySelector(".new input.unitPrice");
+  let newProduct = document.createElement('tr')
+  newProduct.className = "product";
+  newProduct.innerHTML = `<td class="name"><span>${descriptionInput.value}</span></td>
+  <td class="pu">$<span class="test">${priceInput.value}</span></td>
+  <td class="qty"><label><input class="unit" type="number" value="0" min="0" /></label></td>
+  <td class="subtot">$<span>0</span></td>
+  <td class="rm"><button class="btn btn-delete">Delete</button></td>`
+
+  $calc.onclick = calcAll;
+  $cart.appendChild(newProduct);
+
+  descriptionInput.value = "";
+  priceInput.value = "";
+
+  let newDeleteButton = newProduct.querySelector('.rm .btn-delete');
+  newDeleteButton.addEventListener("click", deleteProduct)
+}
