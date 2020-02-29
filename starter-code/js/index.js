83640a045e935fa15088905a81ca9721455180af
Ben Balderas <benbalderas3@gmail.com>
2020-02-28 20:07:11 -0600

    iteration 3 done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -5,19 +5,21 @@ function updateSubtotal($product) {
   const productQty = Number($product.querySelector('.quantity').querySelector('input').value)
   const subtotal = $product.querySelector('.subtotal')
 
-  return subtotal.innerHTML = productPrice * productQty
+  return subtotal.innerHTML = (productPrice * productQty).toFixed(2)
 
 }
 
 function calculateAll() {
   // ITERATION 2
   const productList = document.getElementsByClassName('product')
+  const subtotals = []
 
   for (let product of productList) {
-    updateSubtotal(product)
+    subtotals.push(Number(updateSubtotal(product)))
   }
   // ITERATION 3
-  //...
+  const totalValue = document.getElementById('total-value').querySelector('span')
+  return totalValue.innerHTML = (subtotals.reduce((acc, cur) => acc + cur)).toFixed(2)
 }
 
 window.addEventListener('load', () => {
