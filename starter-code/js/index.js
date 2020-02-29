d723f683d311c4d8d1ef399d06e310c33da53a1c
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-02-29 01:20:32 -0600

    Iterations done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,23 +1,42 @@
 // ITERATION 1
+let total = 0;
 
-function updateSubtotal($product) {
-  const price = document.getElementsByClassName("price")[0].innerText.split("$")[1];
-  const quantity = document.getElementById("quantity-input").value;
-  const subtotal = parseFloat(price) * parseFloat(quantity);
-  subtotalElement = document.getElementById("subtotal-value").innerText = subtotal.toFixed(2);
+function updateSubtotal($products) {
+  for (let $product of $products) {
+    const price = $product.getElementsByClassName("price")[0].innerText.split("$")[1];
+    const quantity = $product.querySelector('.quantity').querySelector('input').value;
+    const subtotal = parseFloat(price) * parseFloat(quantity);
+    total += subtotal;
+    $product.querySelector(".subtotal").querySelector("span").innerText = subtotal.toFixed(2);
+  }
+
+  // for (let i = 0; i < products.length; i++) {
+  //   const price = products[i].querySelector(".price").innerText.split("$")[1];
+  //   const quantity = products[i].querySelector('.quantity').querySelector('input').value;
+  //   const subtotal = parseFloat(price) * parseFloat(quantity);
+  //   products[i].querySelector(".subtotal").querySelector("span").innerText = subtotal.toFixed(2);
+  // }
+
+  // products.forEach(function(row) {
+  //   const price = row.getElementsByClassName("price")[0].innerText.split("$")[1];
+  //   const quantity = row.querySelector('.quantity').querySelector('input').value;
+  //   const subtotal = parseFloat(price) * parseFloat(quantity);
+  //   row.querySelector(".subtotal").querySelector("span").innerText = subtotal.toFixed(2);
+  // });
 }
 
-  // ITERATION 2
-  function calculateAll() {
-    updateSubtotal();
-  
+//ITERATION 2
+function calculateAll() {
+  total = 0; //start the sum again to avoid the total ++
+  const $products = document.querySelectorAll('.product');
+  console.log($products)
+  updateSubtotal($products);
+  calculateTotal();
+}
 
-
-
-
-    
-  // ITERATION 3
-  //...
+// ITERATION 3
+function calculateTotal() {
+  document.getElementById("total-value").querySelector("span").innerText = total;
 }
 
 window.addEventListener("load", () => {
@@ -29,14 +48,7 @@ window.addEventListener("load", () => {
 // ITERATION 4
 
 function productRemoveListener(event) {
-  let removeButton = document.querySelectorAll("btn-remove");
-  for (let i = 0; i < removeButton.length; i++) {
-    let button = removeButton[i];
-    button.onClick = function(event) {
-      let buttonClicked = event.target;
-      buttonClicked.parentElement.remove();
-    };
-  }
+ 
 }
 
 // ITERATION 5
