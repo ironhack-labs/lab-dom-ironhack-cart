5dec0b7903d98e96a90d6859722ceedb4339fc64
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-02-28 21:52:31 -0600

    Iteration one done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,26 +1,42 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+  const price = document.getElementsByClassName("price")[0].innerText.split("$")[1];
+  const quantity = document.getElementById("quantity-input").value;
+  const subtotal = parseFloat(price) * parseFloat(quantity);
+  subtotalElement = document.getElementById("subtotal-value").innerText = subtotal.toFixed(2);
 }
 
-function calculateAll() {
   // ITERATION 2
-  //...
+  function calculateAll() {
+    updateSubtotal();
+  
+
+
+
+
+    
   // ITERATION 3
   //...
 }
 
-window.addEventListener('load', () => {
-  const $calculateTrigger = document.getElementById('calculate');
+window.addEventListener("load", () => {
+  const $calculateTrigger = document.getElementById("calculate");
 
-  $calculateTrigger.addEventListener('click', calculateAll);
+  $calculateTrigger.addEventListener("click", calculateAll);
 });
 
 // ITERATION 4
 
 function productRemoveListener(event) {
-  // ...
+  let removeButton = document.querySelectorAll("btn-remove");
+  for (let i = 0; i < removeButton.length; i++) {
+    let button = removeButton[i];
+    button.onClick = function(event) {
+      let buttonClicked = event.target;
+      buttonClicked.parentElement.remove();
+    };
+  }
 }
 
 // ITERATION 5
