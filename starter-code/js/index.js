3e43537fa521364ae6bff2a41754d57b3c0db7b9
Stefano Santonocito <oddouu@gmail.com>
2020-03-04 00:17:10 +0000

    iteration 1 appears to be done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,10 +1,25 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+  const quantity = parseInt(document.querySelector('.product input').value);
+  const unitPrice = parseInt(document.querySelector('tr.product td.price span').innerHTML);
+  const $subtotalSelector = document.querySelector('.subtotal');
+  console.log($subtotalSelector);
+  const subtotal = quantity * unitPrice;
+  $subtotalSelector.innerHTML = `$<span>${subtotal}</span>`;
+
+  console.log('quantity', quantity);
+  console.log('unit price', unitPrice);
+  console.log('subtotal', subtotal);
 }
 
 function calculateAll() {
+
+  // For development testing purposes, paste the following code inside of `calculateAll`:
+  const $singleProduct = document.querySelector('.product');
+  updateSubtotal($singleProduct);
+  // End of test
+
   // ITERATION 2
   //...
   // ITERATION 3
