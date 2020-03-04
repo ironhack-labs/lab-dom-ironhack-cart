908ec03c93e9cda876842c2f6820dc41dd39ad80
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-03-04 22:01:30 +0000

    done iteration 1/2/3

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,13 +2,25 @@
 
 function updateSubtotal($product) {
   // ...
+  const $price = $product.querySelector(".price span").innerText;
+  const $quantity = $product.querySelector(".quantity input").value;
+  const $subTotal = $product.querySelector(".subtotal span");
+  return ($subTotal.innerText = $quantity * $price);
+
 }
 
 function calculateAll() {
   // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
+  let listOfProducts = document.querySelectorAll(".product");
+  let sumOfsubTotals = 0;
+
+  for (let product of listOfProducts) {
+    console.log(product)
+    let updateProduct = updateSubtotal(product)
+    sumOfsubTotals += updateProduct
+  }
+  document.querySelector("#total-value span").innerText = sumOfsubTotals
+
 }
 
 window.addEventListener('load', () => {
