6dc57bae20733420e7ba7abd71e05c9aad50b0e5
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-03-04 22:06:04 +0000

    Missing iteration 4 and 5

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,14 +1,39 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+
+  const $price = parseInt($product.querySelector('.price span').innerText);
+  //console.log($price);
+
+  const $quantity = $product.querySelector('.quantity input').value;
+  //console.log($quantity)
+
+  let $subtotal = $product.querySelector('.subtotal span');
+  console.log($subtotal)
+  //let subtotalResult = $price * $quantity
+
+  return $subtotal.innerText = $price * $quantity;
+  
 }
+//just console logging the result of the function -- NOT IN ORIGINAL CODE
+//updateSubtotal();
 
 function calculateAll() {
+
   // ITERATION 2
-  //...
+  let listOfProducts = document.querySelectorAll(".product")
+
+  let sumOfSubtotals = 0;
+
+  for (let product of listOfProducts){
+    console.log(product);
+    let updatedProduct = updateSubtotal(product);
+
+    sumOfSubtotals += updatedProduct;
+  }
+
   // ITERATION 3
-  //...
+  document.querySelector("#total-value span").innerText = sumOfSubtotals
 }
 
 window.addEventListener('load', () => {
@@ -27,4 +52,5 @@ function productRemoveListener(event) {
 
 function createProduct(event) {
   // ...
+  //grab the whole row and use innerHTML to change the values
 }
