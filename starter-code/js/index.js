6d728ba7e362a3fb277986f0efe37e44e3346c48
Wilson Cruz <galatoide@gmail.com>
2020-03-06 23:27:20 +0000

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,14 +1,28 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+  const $price = $product.querySelector(".price span").innerText;
+  const $quantity = $product.querySelector(".quantity input").value;
+  const $subTotal = $product.querySelector(".subtotal span");
+  return ($subTotal.innerText = $quantity * $price);
+
 }
 
 function calculateAll() {
   // ITERATION 2
-  //...
+  let listOfProducts = document.querySelectorAll(".product")
+
+  let sumOfSubtotals = 0;
+
+  for (let product of listOfProducts){
+    console.log(product);
+    let updatedProduct = updateSubtotal(product);
+
+    sumOfSubtotals += updatedProduct;
+  }
+  
   // ITERATION 3
-  //...
+  document.querySelector("#total-value span").innerText = sumOfSubtotals
 }
 
 window.addEventListener('load', () => {
