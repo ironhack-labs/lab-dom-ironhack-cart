7d0230e7d40fd5b4b184c1e44f7d313474130fc9
Yai <myd.rio@hotmail.com>
2020-03-28 14:39:44 +0100

    El commit de ayer conseguí meter tooodas las interaciones obligatorias en la misma funcion. Hoy he consegui separarlas como debe. Hasta ahora, el boton delete funciona, y a su vez actualiza el precio subtotal.

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,76 +1,84 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  let primera = document.querySelectorAll('.price > span')
-  let primeraTexto = []
-  let unitPrice = []
+  let primera = document.querySelectorAll(".price > span");
+  let primeraTexto = [];
+  let unitPrice = [];
 
   primera.forEach(elm => {
-    primeraTexto.push(elm.innerHTML)
-  })
+    primeraTexto.push(elm.innerHTML);
+  });
   primeraTexto.forEach(elm => {
-    unitPrice.push(parseInt(elm, 10))
-  })
+    unitPrice.push(parseInt(elm, 10));
+  });
 
-  let numUnits = document.querySelectorAll('.quantity > input')
-  let quantityProduct = []
+  let numUnits = document.querySelectorAll(".quantity > input");
+  let quantityProduct = [];
 
   numUnits.forEach(elm => {
-    quantityProduct.push(elm.getAttribute('value'))
-  })
+    quantityProduct.push(elm.getAttribute("value"));
+  });
 
-  let multipliCate = []
+  let multipliCate = [];
 
   for (let i = 0; i < unitPrice.length; i++) {
     for (let k = 0; k < quantityProduct.length; k++) {
-      multipliCate.push(unitPrice[i = k] * quantityProduct[k = i])
+      multipliCate.push(unitPrice[(i = k)] * quantityProduct[(k = i)]);
     }
   }
 
-  let substitute = document.querySelectorAll('.subtotal > span')
-  let subTotal = []
+  let substitute = document.querySelectorAll(".subtotal > span");
+  let subTotal = [];
 
   for (let i = 0; i < multipliCate.length; i++) {
     for (let k = 0; k < substitute.length; k++) {
-      subTotal.push(substitute[i = k].innerHTML = multipliCate[i = k])
+      subTotal.push((substitute[(i = k)].innerHTML = multipliCate[(i = k)]));
     }
   }
-
-
 }
-updateSubtotal()
+updateSubtotal();
 
 function calculateAll() {
-  // ITERATION 2
-  let beSubTotal = document.querySelector('#total-value > span')
-  let suma = 0;
+  let getSubs = document.querySelectorAll(".subtotal span");
+  let subsToValue = [];
+  let valueToNums = [];
 
-  for (let i = 0; i < subTotal.length; i++) {
-    suma += subTotal[i]
+  getSubs.forEach(elm => {
+    subsToValue.push(elm.innerHTML);
+  });
+
+  subsToValue.forEach(elm => valueToNums.push(parseInt(elm, 10)));
+
+  let beSubTotal = document.querySelector("#total-value > span");
+  let sum = 0;
+
+  for (let i = 0; i < valueToNums.length; i++) {
+    sum += valueToNums[i];
   }
 
-  beSubTotal.innerHTML = suma
-
-
-
-
-
-  // ITERATION 3
-  //...
+  beSubTotal.innerHTML = sum;
 }
 
-calculateAll()
+calculateAll();
 
-window.addEventListener('load', () => {
-  const $calculateTrigger = document.getElementById('calculate');
+window.addEventListener("load", () => {
+  const $calculateTrigger = document.getElementById("calculate");
 
-  $calculateTrigger.addEventListener('click', calculateAll);
+  $calculateTrigger.addEventListener("click", calculateAll);
 });
 
 // ITERATION 4
 
+let getButtons = document.querySelectorAll(".btn.btn-remove");
+getButtons.forEach(elm => {
+  elm.addEventListener("click", productRemoveListener);
+});
+
 function productRemoveListener(event) {
-  // ...
+  let chilDren = document.querySelector(".product");
+
+  chilDren.parentNode.removeChild(chilDren);
+  calculateAll();
 }
 
 // ITERATION 5
@@ -78,7 +86,6 @@ function productRemoveListener(event) {
 function createProduct(event) {
   // ...
 }
-
 
 // For development testing purposes, paste the following code inside of `calculateAll`:
 // const $singleProduct = document.querySelector('.product');
