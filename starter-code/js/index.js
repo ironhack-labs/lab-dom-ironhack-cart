d5ef305a2058db67295c6359e93ee2c70fae884c
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-28 13:10:02 +0100

    define updateSubtotal()

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,7 +1,13 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+  const productPrice = $product.querySelector('.price > span').innerHTML;
+  let productQuantity = $product.querySelector('.quantity > input').value;
+
+  //we get and set subtotal directly from its DOM element
+
+  $product.querySelector('.subtotal > span').innerHTML =
+    productPrice * productQuantity;
 }
 
 function calculateAll() {
