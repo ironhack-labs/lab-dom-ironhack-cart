77ecf50396a69c561073a91300e4249527ef2b4d
DayanRojas <dayan18_13@hotmail.com>
2020-03-28 22:34:24 +0100

    Iteration 4  done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -14,8 +14,7 @@ function updateSubtotal($product) {
   return subTotal
 }
 
-let totalValue = document.querySelector('#total-value span').textContent;
-console.log(totalValue)
+
 
 function calculateAll() {
   const chooseProduct = document.querySelectorAll('.product');
@@ -23,22 +22,30 @@ function calculateAll() {
   let totalValue = 0
   chooseProduct.forEach(elm => {
     totalValue += updateSubtotal(elm)
-    console.log(totalValue)
   })
   let totalText = totalValue.toString();
   elementTotalValue.innerHTML = totalText;
 }
 
+
 window.addEventListener('load', () => {
   const $calculateTrigger = document.getElementById('calculate')
-
   $calculateTrigger.addEventListener('click', calculateAll);
+  let removeElemnts = document.querySelectorAll(".btn-remove");
+  console.log(removeElemnts)
+  removeElemnts.forEach(elm => {
+    elm.addEventListener('click', productRemoveListener);
+  })
 });
+
 
 // ITERATION 4
 
 function productRemoveListener(event) {
-  // ...
+  let node = event.currentTarget.parentNode.parentNode;
+  if (node.parentNode) {
+    node.parentNode.removeChild(node);
+  }
 }
 
 // ITERATION 5
