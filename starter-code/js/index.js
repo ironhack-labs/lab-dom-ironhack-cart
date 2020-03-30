8583acd8caf5f6725c00d02abc13f80c0149815d
Julian Kizeno <julianquiceno20@gmail.com>
2020-03-30 08:27:01 +0200

    Exercise Cart finished without Bonus

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,20 +1,56 @@
 // ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+
+  // console.log($product)
+  
+  let $productPrice = Number($product.querySelector('.price span').innerHTML)
+  // console.log($productPrice)
+
+  let $productQtity = Number($product.querySelector('.quantity input').value)
+  // console.log($productQtity)
+
+  let $subtotal = $productQtity * $productPrice
+  // console.log($subtotal)
+
+  $product.querySelector('.subtotal span').innerHTML = $subtotal
+  // console.log($productSubtotal)
+
 }
 
+// ITERATION 2
+
 function calculateAll() {
-  // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
+
+  let $allProducts = document.querySelectorAll('.product')
+  console.log($allProducts)
+
+  $allProducts.forEach(elem => updateSubtotal(elem))
+  
+  
+  let $subtotalProducts = document.querySelectorAll('.subtotal span')
+
+  console.log($subtotalProducts) 
+  let total = 0
+  $subtotalProducts.forEach(element => total =  total + Number(element.innerHTML))
+
+  console.log(total.toFixed(2))
+
+  document.querySelector('h2 span').innerHTML = total.toFixed(2)
+  
+
 }
+
+
 
 window.addEventListener('load', () => {
   const $calculateTrigger = document.getElementById('calculate');
+  console.log($calculateTrigger)
 
   $calculateTrigger.addEventListener('click', calculateAll);
+
+  
+  
 });
 
 // ITERATION 4
@@ -28,3 +64,4 @@ function productRemoveListener(event) {
 function createProduct(event) {
   // ...
 }
+
