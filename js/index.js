896f9334a3427a15e13229a548c4adfd60e1304d
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-02 16:34:23 +0200

    Complete ironcart draft one

--- js/index.js
+++ js/index.js
@@ -1,21 +1,68 @@
+function generateRowValues(e) {
+  creatorRow = e.currentTarget.parentElement.parentElement;
+  productName = `<span>${creatorRow.children[0].children[0].value}</span>`;
+  unitPrice = `$<span>${creatorRow.children[1].children[0].value}</span>`;
+  quantity = `<input type="number" value="0" min="0" placeholder="Quantity" />`;
+  subtotal = `$<span>0</span>`;
+  action = document.createElement("button");
+  action.addEventListener("click", e => removeProduct(e));
+  action.classList.add("btn", "btn-remove");
+  action.innerText = "Remove";
+
+  row = document.createElement("tr");
+  row.classList.add("product");
+
+  nameCell = document.createElement("td");
+  nameCell.classList.add("name");
+  nameCell.innerHTML = productName;
+
+  priceCell = document.createElement("td");
+  priceCell.classList.add("price");
+  priceCell.innerHTML = unitPrice;
+
+  quantityCell = document.createElement("td");
+  quantityCell.classList.add("quantity");
+  quantityCell.innerHTML = quantity;
+
+  subtotalCell = document.createElement("td");
+  subtotalCell.classList.add("subtotal");
+  subtotalCell.innerHTML = subtotal;
+
+  actionCell = document.createElement("td");
+  actionCell.classList.add("action");
+  actionCell.appendChild(action);
+
+  row.appendChild(nameCell);
+  row.appendChild(priceCell);
+  row.appendChild(quantityCell);
+  row.appendChild(subtotalCell);
+  row.appendChild(actionCell);
+
+  console.log(row);
+
+  let tableBody = document.querySelector("tbody");
+  tableBody.appendChild(row);
+}
+
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  let price = product.querySelector(".price span").innerText;
+  let quantity = product.querySelector(".quantity input").value;
+  let subtotal = product.querySelector(".subtotal span").innerText;
+  product.querySelector(".subtotal span").innerText = price * quantity;
+  return price * quantity;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
-
+  const products = document.querySelectorAll(".product");
+  const totalElement = document.querySelector("#total-value span");
+  let total = 0;
+  products.forEach(product => {
+    total += updateSubtotal(product);
+  });
+  totalElement.innerText = total;
   // ITERATION 3
   //... your code goes here
 }
@@ -24,8 +71,8 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  row = target.parentElement.parentElement;
+  row.remove();
 }
 
 // ITERATION 5
@@ -34,9 +81,17 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.querySelectorAll(".btn-remove");
+  removeButtons.forEach(button => {
+    button.addEventListener("click", e => removeProduct(e));
+  });
+
+  const createProduct = document.querySelector("#create");
+  createProduct.addEventListener("click", e => {
+    generateRowValues(e);
+  });
 });
