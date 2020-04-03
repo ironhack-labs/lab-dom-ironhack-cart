688874ef08a37aa7df61cf148033f56068719da6
bdean05 <benjamin_h_dean@yahoo.com>
2020-04-03 17:51:26 +0200

    My cart is full

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
+  let price = Number(product.querySelector(".price span").innerText)
+  let quantity = Number(product.querySelector(".quantity input").value)
+  let total = price * quantity
+  product.querySelector(".subtotal span").innerText = String(total)
+  return total
   //... your code goes here
 }
 
@@ -12,31 +15,64 @@ function calculateAll() {
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
-
   // ITERATION 2
-  //... your code goes here
-
+  var sum = 0;
+  var products = [...document.querySelectorAll('.product')];
+  products.forEach(function (product) {
+  var subTotal = updateSubtotal(product);
+  sum += subTotal;
+  });
   // ITERATION 3
-  //... your code goes here
+  document.querySelector("#total-value span").innerText = sum;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget; 
   console.log('The target in remove is:', target);
-  //... your code goes here
+  target.parentNode.parentNode.remove(); 
+  calculateAll();
 }
+
 
 // ITERATION 5
-
 function createProduct() {
+  
   //... your code goes here
+/*
+   var create = document.createElement("tr");
+   create.className = "product";
+   create.innerHTML = `
+          <td class="name">
+            <span>${document.querySelector(".create-name").value}</span>
+          </td>
+          <td class="price">$<span>${document.querySelector(".create-price").value}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button id="remove" class="btn btn-remove">Remove</button>
+          </td>
+          `
+   document.querySelector("#cart tbody").appendChild(create);
+   calculateAll();
+   */
 }
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  const removeBtn = document.getElementById('remove');
+  removeBtn.addEventListener('click', removeProduct);
+  const createProduct = document.getElementById('create');
+  createProduct.addEventListener('click', createProduct);
   //... your code goes here
 });
+
+// start with querying the document for all "Remove" buttons, loop through them, and add 
+// a click event listener to each, passing a named function removeProduct as the callback 
+// argument. If you need a hint on how to do this, just take a look at how we did it with 
+// adding event listener on the calculatePricesBtn.
