16f837a1cd7384be0572f18866b0e547bba05262
Hélène Stephan <helene.stephan@protonmail.com>
2020-04-07 22:36:49 +0200

    bonus done but the second one when it's used brokes the first one... need to work on it when I have time to do so

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
   const price = (product.querySelector('.price span')).innerHTML;
   const quantity = (product.querySelector('.quantity input')).value;
   const subtotalPrice = price * quantity;
@@ -10,10 +9,9 @@ function updateSubtotal(product) {
   return subtotalPrice;
 }
 
-function calculateAll() {
+ // ITERATION 2 && 3
 
-  // ITERATION 2
-
+  function calculateAll() {
   const products = document.getElementsByClassName('product');
   const arrOfProducts = [...products];
   const totalElement = document.querySelector('#total-value span');
@@ -24,27 +22,49 @@ function calculateAll() {
   totalElement.innerHTML = total;
 }
 
-// ITERATION 3
-
-
-
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log("remove clicked");
+const target = event.currentTarget;
+((target.parentNode).parentNode).parentNode.removeChild((target.parentNode).parentNode);
+calculateAll();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+console.log("create clicked");
+const target = event.currentTarget;
+tbody = document.querySelector('tbody');
+tfoot = document.querySelector('tfoot');
+let productName = "noName"; //just waiting to improve and check/tell for this error etc. so in the meaning products with no name will be noName
+if (tfoot.querySelector('input').value) productName = tfoot.querySelector('input').value;
+const productPrice = tfoot.querySelector('input[type=number]').value;
+tbody.innerHTML += ` <tr class="product">
+<td class="name">
+  <span>${productName}</span>
+</td>
+<td class="price">$<span>${productPrice}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>
+</tr>`;
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const removeProductBtn = document.getElementsByClassName('btn-remove');
+  const arrRemoveProductBtn = [...removeProductBtn];
+  arrRemoveProductBtn.forEach((product) => {
+    product.addEventListener('click', removeProduct);
+  });
+  const addProductBtn = document.getElementById('create');
+  addProductBtn.addEventListener('click', createProduct);
 });
