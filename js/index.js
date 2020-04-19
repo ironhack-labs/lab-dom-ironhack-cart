eee9535c7f78570dc11c35d029b76a3a8544826e
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-04-19 19:26:09 +0200

    corrections

--- js/index.js
+++ js/index.js
@@ -35,56 +35,36 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
+  
   console.log('product to be created !')
-  let targetName = event.currentTarget
   let table = document.querySelector("tbody")
   let tr = document.createElement("TR");
+
+  let newName = document.querySelector('#createName').value
+  let newPrice = document.querySelector('#createPrice').value
+  let newItem = 
+  `
+  <tr class="product">
+    <td class="name">
+      <span>${newName}</span>
+    </td>
+    <td class="price">$<span>${newPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>
+  ` 
+
   tr.setAttribute("class","product")
+  tr.innerHTML = newItem
   table.appendChild(tr)
 
-  let td1 = document.createElement("TD")
-  tr.appendChild(td1)
-  td1.setAttribute("class","name")
-  spanName = document.createElement('span')
-  td1.appendChild(spanName)
-  newName = document.querySelector('#createName')
-  spanName.innerText = newName.value
-
-  let td2 = document.createElement("TD")
-  tr.appendChild(td2)
-  td2.setAttribute("class","price")
-  td2.innerText = "$"
-  spanPrice = document.createElement('span')
-  td2.appendChild(spanPrice)
-  newPrice = document.querySelector('#createPrice')
-  spanPrice.innerText = newPrice.value
-
-  let td3 = document.createElement("TD")
-  tr.appendChild(td3)
-  td3.setAttribute("class","quantity")
-  quantityInput = document.createElement("input")
-  td3.appendChild(quantityInput)
-  quantityInput.setAttribute("type","number")
-  quantityInput.setAttribute("value","0")
-  quantityInput.setAttribute("min","0")
-  quantityInput.setAttribute("placeholder","Quantity")
-
-  let td4 = document.createElement("TD")
-  tr.appendChild(td4)
-  td4.setAttribute("class","subtotal")
-  td4.innerText = "$"
-  spanSubtotal = document.createElement('span')
-  td4.appendChild(spanSubtotal)
-  spanSubtotal.innerHTML = 0
-
-  let td5 = document.createElement("TD")
-  tr.appendChild(td5)
-  td5.setAttribute("class","action")
-  removeBtn = document.createElement('button')
-  td5.appendChild(removeBtn)
-  removeBtn.setAttribute("class","btn btn-remove")  
-  removeBtn.innerText = "Remove"
-  removeBtn.addEventListener('click', removeProduct)
+  let lastButton = document.querySelectorAll(".btn-remove")
+  lastButton[lastButton.length-1].addEventListener('click', removeProduct)
 
 }
 
