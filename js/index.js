048c44590dfd2e6d676ab03f2a3a0d0ebc61985e
Toni <sancheztoni.garcia@gmail.com>
2020-04-19 12:57:47 +0200

    Lab 100% finished

--- js/index.js
+++ js/index.js
@@ -1,42 +1,61 @@
-// ITERATION 1
+'use strict';
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  product.querySelector('.subtotal span').innerHTML = quantity * price;
+  return quantity * price;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  let total = 0;
+  const products = document.querySelectorAll('.product');
+  for (let i = 0; i < products.length; i++) {
+    updateSubtotal(products[i]);
+    total += updateSubtotal(products[i]);
+  }
+  let totalValue = document.querySelector('#total-value span');
+  totalValue.innerHTML = total;
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  //With 2 parentNode, we select the complete product.
+  const target = event.currentTarget.parentNode.parentNode;
+  //We remove the complete element and refresh the cart
+  target.remove();
+  calculateAll();
 }
 
-// ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  //We get the values Product name and Unit price
+  const name = document.querySelector('.create-product input[type=text]')
+  const price = document.querySelector('.create-product input[type=number]');
+  //Get a full html product & make a copy with cloneNode
+  const existingProduct = document.querySelector('.product');
+  let newProduct = existingProduct.cloneNode(true);
+  //Update the name & price values
+  newProduct.querySelector('.name span').innerHTML = name.value;
+  newProduct.querySelector('.price span').innerHTML = parseFloat(price.value);
+  // Add the removeproduct() at click event
+  newProduct.querySelector('.btn-remove').addEventListener('click', removeProduct);
+  //Get complete cart body & add the new product
+  const cart = document.querySelector('tbody');
+  cart.appendChild(newProduct);
+  // Restart the default values.
+  name.value = "";
+  price.value = 0;
 }
 
 window.addEventListener('load', () => {
+  // Calculate price button
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  //Delete buttons 
+  const deleteBtns = document.querySelectorAll('.btn-remove');
+  for (let i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener('click', removeProduct);
+  }
+  // Create button
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
 });
