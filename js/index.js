aedeff3afe1c43e0a341606172779be57def77b7
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-05-07 01:46:06 +0200

    Hecho sin el bonus

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  
   console.log('Calculating subtotal, yey!');
+
+  let price = parseFloat(product.querySelector('.price span').innerHTML);
+  let quantity = parseInt(product.querySelector('.quantity input').value);
+  let value = price * quantity;
+
+  
+ product.querySelector('.subtotal span').innerHTML = value;
+  subtotal = value;
+
+  return value;
 
   //... your code goes here
 }
@@ -9,8 +20,19 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  
+  const products = document.querySelectorAll('.product');
+  let totalGeneral = 0;
+  for (let i =0; i< products.length; i++){
+    let subtotal =  updateSubtotal(products[i])
+    totalGeneral = totalGeneral + subtotal;
+  };
+
+  let elementosTotal = document.querySelector('#total-value span')
+  elementosTotal.innerHTML = totalGeneral;
+  //[...singleProduct].map(el => { return updateSubtotal(el);})
+  
   // end of test
 
   // ITERATION 2
@@ -18,6 +40,11 @@ function calculateAll() {
 
   // ITERATION 3
   //... your code goes here
+// let total = singleProduct.querySelectorAll('.subtotal span').innerHTML;
+
+
+
+
 }
 
 // ITERATION 4
