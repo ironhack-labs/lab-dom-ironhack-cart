e8a9eb13bba4200f4e4d9614dace69a1fa2617b6
varunkumar88 <varunkumarchalotra@gmail.com>
2020-05-10 23:08:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,16 +1,64 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+
+function updateSubtotal(product) {
+  
+  let price =product.querySelectorAll(".price span")[0].innerHTML;
+let  quantity = product.querySelectorAll(".quantity input")[0].value;
+let subtotal = product.querySelectorAll(".subtotal span")[0];
+
+subtotal.innerHTML=quantity*price;  //... your code goes here
+
+
+
 }
 
+function updateSubtotal1(product2){
+
+  let price1 =product2.querySelectorAll(".price span")[0].innerHTML;
+
+let  quantity1 = product2.querySelectorAll(".quantity input")[0].value;
+let subtotal1 = product2.querySelectorAll(".subtotal span")[0];
+subtotal1.innerHTML=quantity1*price1;
+
+  
+}
+
+
+
 function calculateAll() {
+
+  //let quantity = document.querySelector(".product");
+
+ /* let prod = document.querySelector(".product").length;
+let amt = 0;
+  for (let i = 0; i<prod;i++){
+
+    var second = document.querySelectorAll('.product')[i];
+
+amt = amt +updateSubtotal(prod);
+    
+  }
+//document.querySelectorAll('h2 span')[0].innerHTML =total;
+
+
+*/
+
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
+
+  const secondProduct = document.querySelector('.product2');
   updateSubtotal(singleProduct);
+updateSubtotal1(secondProduct);
+
+let subtotal = document.querySelectorAll(".subtotal span")[0];
+let subtotal1= document.querySelectorAll(".subtotal span")[1];
+
+
+document.querySelector("#total-value span").innerHTML = parseFloat(subtotal.innerHTML+subtotal1.innerHTML);
   // end of test
 
   // ITERATION 2
@@ -24,7 +72,10 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
+
   console.log('The target in remove is:', target);
+
+  
   //... your code goes here
 }
 
