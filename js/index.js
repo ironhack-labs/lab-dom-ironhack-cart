b35bb575c07fdd74e2a4b0899a1e9db88adfcdb2
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-05-13 00:54:10 +0200

    final

--- js/index.js
+++ js/index.js
@@ -3,18 +3,35 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input'); 
+  price = price.innerHTML;
+  quantity = quantity.value;
+
+  let subtotal = product.querySelector('.subtotal span');
+  quantity * price = subtotal.innerHTML;
+
+  let finalTotal = subtotal.innerHTML;
+  return finalTotal;
 }
 
+
+// Complete the function named calculateAll(). Its purpose is to call the function updateSubtotal with every tr.product DOM node in the table#cart.
+
+// To start, remove or comment out the existing code inside the calculateAll() (the code we used for the testing purposes). Also, let's add a new product to our index.html file. Duplicate the tr with the class product, rename the product inside, and change the product price.
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+ 
+  let secondProduct = product.querySelector(".product");
+
+  let newProduct = secondProduct.cloneNode(true);
+
+  let newProduct = ('Products')
 
   // ITERATION 3
   //... your code goes here
