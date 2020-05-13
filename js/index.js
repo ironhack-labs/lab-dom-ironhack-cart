4b0965382e3fdf2cf3fe25520203331189e993c1
Michelle Lock <michelleloc@gmail.com>
2020-05-13 13:09:08 +0800

    Update index.js file with mandatory Iterations 1-3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input").value;
+  let subTotal = price.innerHTML * quantity;
+  product.querySelector(".subtotal span").innerHTML = subTotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // // code in the following two lines is added just for testing purposes.
+  // // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  const products = document.getElementsByClassName("product");
+  [...products].forEach(function (elem) {
+    updateSubtotal(elem);
+  })
   // ITERATION 3
-  //... your code goes here
+  //does innerHTML not work with querySelectorAll?
+  let nodes = document.querySelectorAll(".subtotal span");
+  let array = [];
+  [...nodes].forEach(function (elem) {
+    array.push(Number(elem.innerHTML));
+  })
+  let total = array.reduce(function (acc, elem) {
+    return acc + elem;
+  })
+  document.querySelector("#total-value span").innerHTML = total;
 }
 
 // ITERATION 4
@@ -25,7 +37,7 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  event.querySelector(".product");
 }
 
 // ITERATION 5
@@ -38,5 +50,8 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = document.querySelectorAll(".btn-remove");
+  [...removeBtns].forEach(function (elem) {
+    elem.addEventListener('click', removeProduct)
+  })
 });
