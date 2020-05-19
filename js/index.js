15d18c8fcab030ba797bdffa114012ef576712e0
Miguel Valle <miguelbachi5a@gmail.com>
2020-05-19 14:49:48 +0200

    Iteration 5: Bonus

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
+function updateSubtotal (product) {
   const price = product.querySelector('.price span').innerText
   const quantity = product.querySelector('.quantity input').value
   const subtotal = price * quantity
@@ -27,7 +27,7 @@ function calculateAll() {
 
 // ITERATION 4
 
-function removeProduct(event) {
+function removeProduct (event) {
   const target = event.currentTarget
   console.log('The target in remove is:', target)
   //... your code goes here
@@ -37,16 +37,36 @@ function removeProduct(event) {
 
 // ITERATION 5
 
-function createProduct() {
-  
+function createProduct () {
+  const tBody = document.getElementsByTagName('tbody')[0]
+  const createRow = document.getElementsByClassName('create-product')[0]
+  const inputs = [...createRow.getElementsByTagName('input')]
+
+  tBody.innerHTML += ` 
+  <tr class="product">
+    <td class="name">
+      <span>${inputs[0].value}</span>
+    </td>
+    <td class="price">$<span>${inputs[1].value}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>`
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+  const calculatePricesBtn = document.getElementById('calculate')
+  calculatePricesBtn.addEventListener('click', calculateAll)
 
   const deletePricesBtns = [...document.getElementsByClassName('btn-remove')]
   deletePricesBtns.forEach((deleteBtn) =>{
     deleteBtn.addEventListener('click', removeProduct)
   })
-});
+
+  const createBTn = document.getElementById('create')
+  createBTn.addEventListener('click', createProduct)
+})
