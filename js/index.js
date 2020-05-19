0b401c999dbf77cc9921c8f5f25f6520762711ff
Miguel Valle <miguelbachi5a@gmail.com>
2020-05-19 16:36:16 +0200

    Delete innerHtml and use appendChild

--- js/index.js
+++ js/index.js
@@ -46,35 +46,80 @@ function removeProduct (event) {
 
 function createProduct () {
   const tBody = document.getElementsByTagName('tbody')[0]
-  const createRow = document.getElementsByClassName('create-product')[0]
-  const inputs = [...createRow.getElementsByTagName('input')]
+  const createProduct = document.getElementsByClassName('create-product')[0]
+  const inputs = [...createProduct.getElementsByTagName('input')]
 
-  tBody.innerHTML += ` 
-  <tr class="product">
-    <td class="name">
-      <span>${inputs[0].value}</span>
-    </td>
-    <td class="price">$<span>${Number(inputs[1].value).toFixed(2)}</span></td>
-    <td class="quantity">
-      <input type="number" value="0" min="0" placeholder="Quantity" />
-    </td>
-    <td class="subtotal">$<span>0</span></td>
-    <td class="action">
-      <button class="btn btn-remove">Remove</button>
-    </td>
-  </tr>`
+  const newProduct = createStructure()
+  const name = newProduct.querySelector('.name span')
+  const price = newProduct.querySelector('.price span')
+  name.innerText = inputs[0].value
+  price.innerText = Number(inputs[1].value).toFixed(2)
 
+  tBody.appendChild(newProduct)
+  deleteBtn()
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate')
   calculatePricesBtn.addEventListener('click', calculateAll)
 
-  const deletePricesBtns = [...document.getElementsByClassName('btn-remove')]
-  deletePricesBtns.forEach((deleteBtn) =>{
-    deleteBtn.addEventListener('click', removeProduct)
-  })
+  deleteBtn()
 
   const createBTn = document.getElementById('create')
   createBTn.addEventListener('click', createProduct)
 })
+
+function deleteBtn () {
+  const deletePricesBtns = [...document.getElementsByClassName('btn-remove')]
+  deletePricesBtns.forEach((deleteBtn) =>{
+    deleteBtn.addEventListener('click', removeProduct)
+  })
+}
+
+function createStructure () {
+  const tr = document.createElement('tr')
+  tr.className = 'product'
+
+  const tdName = document.createElement('td')
+  tdName.setAttribute('class', 'name') 
+  const spanName = document.createElement('span')
+  spanName.innerText = 0
+  tdName.appendChild(spanName)
+  tr.appendChild(tdName)
+
+  const tdPrice = document.createElement('td')
+  tdPrice.setAttribute('class', 'price')
+  tdPrice.innerHTML = '$'
+  const spanPrice = document.createElement('span')
+  spanPrice.innerText = 0
+  tdPrice.appendChild(spanPrice)
+  tr.appendChild(tdPrice)
+
+  const tdQuantity = document.createElement('td')
+  tdQuantity.setAttribute('class', 'quantity')
+  const inputQuantity = document.createElement('input')
+  inputQuantity.setAttribute("type", "number")
+  inputQuantity.setAttribute("value", "0")
+  inputQuantity.setAttribute("min", "0")
+  inputQuantity.setAttribute("placeholder", "Quantity")
+  tdQuantity.appendChild(inputQuantity)
+  tr.appendChild(tdQuantity)
+
+  const tdSubtotal = document.createElement('td')
+  tdSubtotal.setAttribute('class', 'subtotal')
+  tdSubtotal.innerHTML = '$'
+  const spanSubtotal = document.createElement('span')
+  spanSubtotal.innerText = 0
+  tdSubtotal.appendChild(spanSubtotal)
+  tr.appendChild(tdSubtotal)
+
+  const tdAction = document.createElement('td')
+  tdAction.setAttribute('class', 'action')
+  const butonAction = document.createElement('button')
+  butonAction.setAttribute('class', 'btn btn-remove')
+  butonAction.innerText = 'Remove'
+  tdAction.appendChild(butonAction)
+  tr.appendChild(tdAction)
+
+  return tr
+}
