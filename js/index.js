b25ccdb9952eec59c64f94d953c1a344e80c6095
martaguirre91 <marta@FONETIC-MSI93.fonetic.local>
2020-05-22 13:01:36 +0200

    acabado

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@ class ProductToBuy {
     return parseInt(this.price)*parseInt(this.quantity);
   }
 }
-// ITERATION 1
+
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
   const price = product.querySelector('.price span').innerText;
@@ -21,7 +22,6 @@ function updateSubtotal(product) {
   return finalPrice;
 }
 
-// ITERATION 2
 function calculateAll() {
   const allProducts = document.querySelectorAll('.product')
   let total = 0
@@ -30,20 +30,13 @@ function calculateAll() {
   
   const totalValue = document.querySelector('#total-value span');
   totalValue.innerText = total;
-  
-  // ITERATION 3
-  //... your code goes here
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentElement.parentElement.remove();
+  calculateAll();
 }
 
-// ITERATION 5
 function createProduct() {
   const inputs = document.querySelectorAll('.create-product input');
   const inputsArr = [...inputs];
@@ -53,18 +46,34 @@ function createProduct() {
   const row = document.getElementsByClassName("product")[0]; // without products won't work
   const table = document.getElementsByTagName("tbody")[0]; 
   const clone = row.cloneNode(true); 
-  if (p.price == 0 || p.name === "") window.alert('complete the info')
-  else{
+  
+  if (p.price == 0 || p.name === "") 
+    window.alert('Please, complete all the information for the product')
+
+  else {
     clone.querySelector('.price span').innerText = p.price;
     clone.querySelector('.name span').innerText = p.name;
-    clone.querySelector('.quantity input').innerText = 0;
+    clone.querySelector('.quantity input').innerText = '';
+    clone.querySelector('.subtotal span').innerText = 0;
+    clone.querySelector('.quantity > input').value = 0;
     table.appendChild(clone); // add new row at the end of the table
+    const allRemoveButtons = document.getElementsByClassName('btn btn-remove')
+    const newRemove = allRemoveButtons[allRemoveButtons.length -1]
+    newRemove.addEventListener('click', removeProduct);
+    document.querySelectorAll('.create-product > td input')[0].value = '';
+    document.querySelectorAll('.create-product > td input')[1].value = '0';
   }
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
   const createProductBtn = document.getElementById('create');
   createProductBtn.addEventListener('click', createProduct);
+
+  const removeProductBtnArr = [...document.getElementsByClassName('btn btn-remove')];
+  for (removeProductBtn of removeProductBtnArr)
+    removeProductBtn.addEventListener('click', removeProduct);
+
 });
