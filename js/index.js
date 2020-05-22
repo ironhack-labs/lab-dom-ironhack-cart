1fcaf759c2352ae1d71eddcbdcb68f9c93b8996d
Johanna <2myadverts@gmail.com>
2020-05-22 23:18:43 +0200

    changing method to create a new pproduct

--- js/index.js
+++ js/index.js
@@ -29,32 +29,123 @@ function removeProduct(event) {
   const parentTBody = parentTRow.parentNode;
   parentTBody.removeChild(parentTRow);
   calculateAll()
+  console.log('remove clicked')
 }
+
 
 // ITERATION 5
-
-function createProduct() {
-  const productName = document.querySelectorAll('tfoot input')[0].value;
-  const unitPrice = document.querySelectorAll('tfoot input')[1].value;
-
-  const newProduct = `
-    <tr class="product">
-      <td class="name">
-        <span>${productName}</span>
-      </td>
-      <td class="price">$<span>${unitPrice}</span></td>
-      <td class="quantity">
-        <input type="number" value="0" min="0" placeholder="Quantity" />
-      </td>
-      <td class="subtotal">$<span>0</span></td>
-      <td class="action">
-        <button class="btn btn-remove" onclick="removeProduct(event)">Remove</button>
-      </td>
-    </tr>`;
-
-  const tableBody = document.getElementsByTagName('tbody')[0];
-  tableBody.innerHTML += newProduct;
+function createChildNode(tagName, innerHtml, attributes, method) {
+  const newNode = document.createElement(tagName);
+  if (attributes) {
+    Object.assign(newNode, attributes)
+  }
+  if (innerHtml) newNode.innerHTML = innerHtml;
+  if (method) newNode.addEventListener('click', removeProduct);
+  return newNode;
 }
+
+function appendNodeToParent(parentSelector, childNode) {
+  document.querySelector(parentSelector).appendChild(childNode);
+}
+
+
+function createProduct() {  
+  const newProduct = {
+    productName: document.querySelectorAll('tfoot input')[0].value,
+    unitPrice: document.querySelectorAll('tfoot input')[1].value,
+  }
+
+  const dataArrayForNewProductNode = [
+    {
+      parentSelector: 'tbody',
+      tagName: 'tr', 
+      attributes: {
+        className: 'product'
+      }
+    },
+    {
+      parentSelector: '.product:last-child ',
+      tagName: 'td', 
+      attributes: {
+        className: 'name'
+      }
+    },
+    {
+      parentSelector: '.product:last-child > .name',
+      tagName: 'span', 
+      innerHtml: newProduct.productName,
+    },
+    {
+      parentSelector: '.product:last-child ',
+      tagName: 'td', 
+      innerHtml: '$',
+      attributes: {
+        className: 'price',
+      }
+    },
+    {
+      parentSelector: '.product:last-child > .price',
+      tagName: 'span', 
+      innerHtml: (Math.round(newProduct.unitPrice * 100) / 100).toFixed(2)
+    },
+    {
+      parentSelector: '.product:last-child ',
+      tagName: 'td', 
+      attributes: {
+        className: 'quantity',
+      }
+    },
+    {
+      parentSelector: '.product:last-child > .quantity',
+      tagName: 'input', 
+      attributes: {
+        type: 'number',
+        value: '0',
+        min: '0',
+        placeholder: 'Quantity'
+      }
+    },
+    {
+      parentSelector: '.product:last-child ',
+      tagName: 'td', 
+      innerHtml: '$',
+      attributes: {
+        className: 'subtotal',
+      }
+    },
+    {
+      parentSelector: '.product:last-child > .subtotal',
+      tagName: 'span', 
+      innerHtml: 0,
+    },
+    {
+      parentSelector: '.product:last-child ',
+      tagName: 'td', 
+      attributes: {
+        className: 'action',
+      }
+    },
+    {
+      parentSelector: '.product:last-child > .action',
+      tagName: 'button', 
+      innerHtml: 'Remove',
+      attributes: {
+        className: 'btn btn-remove',
+      },
+      method: removeProduct
+    }
+  ]
+
+  dataArrayForNewProductNode.forEach(ele => {
+    return appendNodeToParent(ele.parentSelector,  createChildNode(ele.tagName, ele.innerHtml, ele.attributes, ele.method))
+  })
+
+  document.querySelectorAll('tfoot input')[0].value = '';
+  document.querySelectorAll('tfoot input')[1].value = 0;
+
+  return 
+}
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
