5b257ae228c54ff36c79a3fb6363b603e07d159d
martaguirre91 <marta@FONETIC-MSI93.fonetic.local>
2020-05-22 09:55:32 +0200

    ironcard

--- js/index.js
+++ js/index.js
@@ -1,21 +1,36 @@
-// ITERATION 1
+class ProductToBuy {
+  constructor (price, quantity, name) {
+      this.price = price;
+      this.quantity = quantity;
+      this.name = name;
+  }
 
+  finalProductPrice(price,quantity){
+    return parseInt(this.price)*parseInt(this.quantity);
+  }
+}
+// ITERATION 1
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity > input').value;
+  const subtotal = product.querySelector('.subtotal span');
+  const p = new ProductToBuy(price, quantity);
+  const finalPrice = p.finalProductPrice();
+  subtotal.innerText = finalPrice;
+  return finalPrice;
 }
 
+// ITERATION 2
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
+  const allProducts = document.querySelectorAll('.product')
+  let total = 0
+  for (singleP of allProducts)
+    total += updateSubtotal(singleP)
+  
+  const totalValue = document.querySelector('#total-value span');
+  totalValue.innerText = total;
+  
   // ITERATION 3
   //... your code goes here
 }
@@ -29,14 +44,27 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  const inputs = document.querySelectorAll('.create-product input');
+  const inputsArr = [...inputs];
+  const name = inputsArr[0].value;
+  const price = inputsArr[1].value;
+  const p = new ProductToBuy (price, 0, name);
+  const row = document.getElementsByClassName("product")[0]; // without products won't work
+  const table = document.getElementsByTagName("tbody")[0]; 
+  const clone = row.cloneNode(true); 
+  if (p.price == 0 || p.name === "") window.alert('complete the info')
+  else{
+    clone.querySelector('.price span').innerText = p.price;
+    clone.querySelector('.name span').innerText = p.name;
+    clone.querySelector('.quantity input').innerText = 0;
+    table.appendChild(clone); // add new row at the end of the table
+  }
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const createProductBtn = document.getElementById('create');
+  createProductBtn.addEventListener('click', createProduct);
 });
