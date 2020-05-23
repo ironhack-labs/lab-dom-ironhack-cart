cc032a0487ad814df19db144efdb689a66ddc832
Maria Pardo <pardo.genis@gmail.com>
2020-05-23 18:42:53 +0200

    iterations 2 & 3

--- js/index.js
+++ js/index.js
@@ -15,17 +15,19 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  const allProducts = document.getElementsByClassName('product');
+  [...allProducts].map(product => updateSubtotal(product))
+
 
   // ITERATION 3
-  //... your code goes here
+  const allSubtotals = document.querySelectorAll('.subtotal span')
+  const calculateTotal = [...allSubtotals].reduce((acc, number) => acc + Number(number.innerText), 0)
+  const total = document.querySelector('#total-value span')
+
+  total.innerText = calculateTotal;
 }
 
 // ITERATION 4
