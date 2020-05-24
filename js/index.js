b306d78c6ca6edcaf63f1fb1382c308933559a63
Old <onelifedesigning@gmail.com>
2020-05-24 18:26:32 +0200

    control

--- js/index.js
+++ js/index.js
@@ -1,42 +1,147 @@
 // ITERATION 1
+class Cart {
+    constructor() {
+        this.products = [
+            [new Product('Ironhack Rubber Duck', 25.00), 0]
+        ]
+        this.total = 0
+    }
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+    addProducts(product) {
+        if (!this.findProductByName(product.name)) {
+            this.products.push([product, 0])
+        } else {
+            this.updateProduct(product.name)
+            this.updateProduct(product.price)
+        }
+        this.printProducts()
+    }
+    findProductByName(name) {
+        let productFound = ''
+        this.products.forEach(product => {
+            if (product[0].name === name) {
+                product[1]++
+                    productFound = true
+            } else {
+                productFound = false
+            }
+        })
+        return productFound
+    }
+    updateProduct(value) {
+        this.products.forEach(product => {
+            if (product[0].name === value) {
+                product[1]++
+            }
+            if (product[0].price !== value) {
+                product[0].price = value
+            }
+        })
+    }
+    removeProducts(product) {
+        // TODO: find on this.products and delete this position on array
+        this.printProducts()
+    }
 
-  //... your code goes here
+    changeQuantity(product, quantity) {
+        //  Find product modify quantity and modify subtotalr poduct on view
+        this.calcTotal()
+    }
+    calcTotal() {
+        // TODO: refresh total sum all (product quantity * product price)
+
+    }
+    setAttrs(element, attrs) {
+        for (let key in attrs) {
+            element.setAttribute(key, attrs[key]);
+        }
+        return element
+    }
+    subTotalProduct(price, quantity) {
+        return price * quantity
+    }
+    printProducts() {
+        const tableBody = document.querySelector('#cart tbody')
+        tableBody.innerHTML = ''
+        this.products.forEach(product => {
+            const quantityAttrs = { 'type': 'number', 'value': 0, 'min': 0, 'placeholder': 'Quantity' }
+            const tr = document.createElement('tr')
+            tr.className = 'product'
+            tr.appendChild(document.createElement('td')).className = 'name'
+            tr.querySelector('.name').appendChild(document.createElement('span')).innerText = product[0].name
+            tr.appendChild(document.createElement('td')).className = 'price'
+            tr.querySelector('.price').innerText = '$'
+            tr.querySelector('.price').appendChild(document.createElement('span')).innerText = Number(product[0].price).toFixed(2)
+            tr.appendChild(document.createElement('td')).className = 'quantity'
+            tr.querySelector('.quantity').appendChild(this.setAttrs(document.createElement('input'), quantityAttrs))
+            tr.querySelector('.quantity input').setAttribute('value', product[1])
+            tr.appendChild(document.createElement('td')).className = 'subtotal'
+            tr.querySelector('.subtotal').innerText = '$'
+            tr.querySelector('.subtotal').appendChild(document.createElement('span')).innerText = this.subTotalProduct(Number(product[0].price).toFixed(2), product[1])
+            tr.appendChild(document.createElement('td')).className = 'action'
+            tr.querySelector('.action').appendChild(document.createElement('button')).className = 'btn btn-remove'
+            tr.querySelector('.action button').innerText = 'Remove'
+            tableBody.appendChild(tr)
+        });
+
+    }
+    printTotal() {
+        document.querySelector('#total-value span').innerText = this.total
+    }
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
 }
+const theCart = new Cart
+theCart.printProducts()
+theCart.printTotal()
+
+// function updateSubtotal(product) {
+//     // console.log('Calculating subtotal, yey!')
+
+//     //... your code goes here
+// }
+
+// function calculateAll() {
+//     // code in the following two lines is added just for testing purposes.
+//     // it runs when only iteration 1 is completed. at later point, it can be removed.
+//     const singleProduct = document.querySelector('.product')
+//     updateSubtotal(singleProduct)
+
+//     // end of test
+
+//     // ITERATION 2
+//     //... your code goes here
+
+//     // ITERATION 3
+//     //... your code goes here
+// }
 
 // ITERATION 4
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+// function removeProduct(event) {
+//     const target = event.currentTarget
+//         //... your code goes here
+// }
 
 // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+    const newProductBtn = document.getElementById('create')
 
-  //... your code goes here
-});
+    newProductBtn.addEventListener('click', () => {
+        const name = document.querySelector('.create-product input[type=text]').value
+        const price = document.querySelector('.create-product input[type=number]').value
+        if (!name) {
+            throw new Error('¡Need name, please!')
+        }
+        theCart.addProducts(new Product(name, price))
+        theCart.printProducts()
+    })
+})
