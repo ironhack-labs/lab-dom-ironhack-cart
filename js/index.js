9fd32034bba25b8936841d459a2fde4c2efc2b77
andresbr92 <andresbr92@gmail.com>
2020-06-07 00:57:55 +0200

    iteracion 3 con bucle forr para infinitos productos, todo comentado

--- js/index.js
+++ js/index.js
@@ -1,135 +1,106 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  
-
   //... your code goes here
-  const price = product.querySelector('.price span').innerHTML
-  const quantity = product.querySelector('.quantity input')
-  
+  const price = product.querySelector(".price span").innerHTML;
+  const quantity = product.querySelector(".quantity input");
+
   //console.log(price)
 
   //console.log(quantity.valueAsNumber)
-   
-// convertir el precio en un numero real multiplicable
-  
-  const realPrice = parseFloat(price)
 
-//adquiero el valor de quatity como un numero
+  // convertir el precio en un numero real multiplicable
 
-  const realQuantity = quantity.valueAsNumber
+  const realPrice = parseFloat(price);
 
-//consigo el subtotal del numero de productos por su precio
+  //adquiero el valor de quatity como un numero
 
-  const valueTotal = realPrice * realQuantity
+  const realQuantity = quantity.valueAsNumber;
+
+  //consigo el subtotal del numero de productos por su precio
+
+  const valueTotal = realPrice * realQuantity;
 
   //console.log(valueTotal)
 
+  //asigno el el spam a una variable para retornarla despues al dom con el valor total de los productos.
 
-//asigno el el spam a una variable para retornarla despues al dom con el valor total de los productos.
-  
-  const subtotal = product.querySelector('.subtotal span')
+  const subtotal = product.querySelector(".subtotal span");
 
-  subtotal.innerHTML = valueTotal
+  subtotal.innerHTML = valueTotal;
 
-
-    return subtotal
-  
+  return subtotal;
 }
 
 function calculateAll() {
-
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
 
-
-  
   //const singleProduct = document.querySelector('.product');
 
   //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  
 
-  const productos = document.querySelectorAll('.product')
+  const productos = document.querySelectorAll(".product");
 
   //console.log(productos)
-  
-  productos.forEach(elm => { 
+
+  productos.forEach((elm) => {
     //console.log(elm)
 
-    updateSubtotal(elm)
-
-    
-  })
+    updateSubtotal(elm);
+  });
 
   // ITERATION 3
 
+  const subtotal = document.querySelectorAll(".subtotal span");
 
-  
-
-  const subtotal = document.querySelectorAll(".subtotal span")
-
-
+  // --------CODIGO INICIAL PARA SOLO DOS ELEMENTOS EN CARRITO---------------
   //console.log(typeof( subtotall[0].innerHTML))
   //console.log(subtotall[1].innerHTML)
 
-  const realNumber1 = parseFloat(subtotal[0].innerHTML)
-  const realNumber2 = parseFloat(subtotal[1].innerHTML)
-  
+  //const realNumber1 = parseFloat(subtotal[0].innerHTML)
+  //const realNumber2 = parseFloat(subtotal[1].innerHTML)
+  //const sumatory = realNumber1 + realNumber2
+  //--------------------------------------------------------------------------
+  //---------CODIGO PARA INFINITOS ELEMENTOS EN CARRO
+  arrSubTotales = []; //creams un array para pushearle los subtotales procesados
 
-  const sumatory = realNumber1 + realNumber2
-  
-  //console.log(sumatoria)
+  //iteramos los subtotales y transformamos los valores a numbers, push al arrSubTotales []
 
-  const totalPrice = document.querySelector("#total-value span")
+  subtotal.forEach((elm) => {
+    realNumberPrueba = parseFloat(elm.innerHTML);
+
+    console.log(realNumberPrueba);
+
+    arrSubTotales.push(realNumberPrueba);
+  });
+
+  sumary = 0;
+
+  // iteramos el array con los valores de los precios y le hacemos sumatoria
+
+  arrSubTotales.forEach(function (elm) {
+    sumary += elm;
+  });
+  console.log(sumary);
+
+  //sumatoria (precio total) al DOM
+
+  const totalPrice = document.querySelector("#total-value span");
+
   //const price = product.querySelector(".price span").innerHTML;
 
-  totalPrice.innerHTML = sumatory
-  
-  
-  console.log(totalPrice)
-  
-
-  
-  
-
-
-  
-  
-
-  //console.log (typeof (realNumber))
-
-
-  
-
-  //const realSub = parseFloat(subtotall)
-  
-  
-
-
-  //console.log (realSub)
-  
-
-
-  
-  
-
-
-  
-
-
+  totalPrice.innerHTML = sumary;
 }
-
-
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log("The target in remove is:", target);
 }
 
 // ITERATION 5
@@ -138,9 +109,10 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
   //... your code goes here
 });
