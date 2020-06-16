4bfeb962b24c5af12c06a0b623546cece23554bc
Shane Schipper <shane99@vt.edu>
2020-06-16 15:08:51 -0400

    Fixed adding an item to the cart

--- js/index.js
+++ js/index.js
@@ -38,11 +38,56 @@ function removeProduct(event) {
 
 function createProduct() {
   let table = document.getElementById('cart')
-  const prodName = document.querySelector('#prodName').value
+  let tbody = document.getElementsByTagName('tbody')[0]
+  console.log(tbody)
+  let row = tbody.insertRow(table.rows.length - 2)
+  row.classList.add('product')
+  let prodName = document.querySelector('#prodName').value
   let prodPrice = document.querySelector('#prodPrice').value
   
+  // Adding the name cell
+  let cell1 = row.insertCell(0)
+  cell1.classList.add('name')
+  let nameSpan = document.createElement('SPAN')
+  nameSpan.innerHTML = prodName
+  cell1.appendChild(nameSpan)
   
-  
+  // adding the price cell
+  let cell2 = row.insertCell(1)
+  cell2.classList.add('price')
+  cell2.innerHTML = '$'
+  let priceSpan = document.createElement('SPAN')
+  priceSpan.innerHTML = prodPrice
+  cell2.appendChild(priceSpan)
+
+  // adding the quantity cell
+  let cell3 = row.insertCell(2)
+  cell3.classList.add('quantity')
+  let input = document.createElement('INPUT')
+  input.type = 'number'
+  input.value = 0
+  input.min = 0
+  input.placeHolder = 'Quantity'
+  cell3.appendChild(input)
+
+  // adding the subtotal cell
+  let cell4 = row.insertCell(3)
+  cell4.classList.add('subtotal')
+  cell4.innerHTML = '$'
+  let stSpan = document.createElement('SPAN')
+  stSpan.innerHTML = '0'
+  cell4.appendChild(stSpan)
+
+  // Creating the remove button
+  let cell5 = row.insertCell(4)
+  cell5.classList.add('action')
+  let rmvBtn = document.createElement('BUTTON')
+  rmvBtn.classList.add('btn')
+  rmvBtn.classList.add('btn-remove')
+  rmvBtn.innerHTML = 'Remove'
+  cell5.appendChild(rmvBtn)
+  rmvBtn.addEventListener('click', removeProduct)
+
 }
 
 window.addEventListener('load', () => {
