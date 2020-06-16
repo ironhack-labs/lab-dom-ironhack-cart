60e4499a0ebdb132226d9c30176b6df8913ac91e
Stephen Z <mayistep@icloud.com>
2020-06-16 16:40:23 -0400

    done w/o bouns

--- js/index.js
+++ js/index.js
@@ -4,22 +4,36 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  var price = product.querySelector('.price span').innerHTML;
+  var quantity = product.querySelector('.quantity input').value;
+  var subtotal = price * quantity
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+  //console.log(subtotal)
+  return subtotal
+  
 }
 
 function calculateAll() {
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+var subtotal2 = 0
+    document.querySelectorAll('.product').forEach(function (ele) {
+    subtotal2 = subtotal2 + updateSubtotal(ele)
+    //console.log(updateSubtotal(ele))
+  });
+ 
   // ITERATION 3
   //... your code goes here
+    document.querySelector('#total-value span').innerHTML = subtotal2
+    //console.log(subtotal2)
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
