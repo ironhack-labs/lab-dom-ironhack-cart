7eced964cab4faa5b5854be1905bc1f43312da51
Renzo Pederzoli <rdp73@miami.edu>
2020-06-16 15:45:47 -0400

    done

--- js/index.js
+++ js/index.js
@@ -22,25 +22,50 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  let pNode = target.parentNode.parentNode
-  pNode.remove()
-  calculateAll()
+  if(event.target.className === 'btn btn-remove') {
+    const target = event.target;
+    console.log('The target in remove is:', target);
+    let pNode = target.parentNode.parentNode
+    pNode.remove()
+    calculateAll()
+  }
 }
 
 // ITERATION 5
+function createProduct(event) {
+  console.log(event.target, event)
+  const name = document.querySelector(".create-product td input").value
+  const price = document.querySelector(".create-product td:nth-child(2) input").value
 
-function createProduct() {
-  //... your code goes here
+  const row = document.createElement("tr")
+  row.innerHTML = `<td class="name">
+  <span>${name}</span>
+  </td>
+  <td class="price">$<span>${price}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>`
+  row.className = "product"
+  document.querySelector("tbody").appendChild(row)
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  let arr = document.getElementsByClassName("btn btn-remove")
-  for (elem of arr) {
-    elem.addEventListener('click',removeProduct)
-  }
+  const cart = document.getElementById('cart'); //added eventlistener to whole table
+  cart.addEventListener('click',removeProduct) // this way it listens to the whole table  
+  //and doesnt matter where you click as long as the calss is the one i am looking for
+
+  // let arr = document.getElementsByClassName("btn btn-remove")
+  // for (elem of arr) {
+  //   elem.addEventListener('click',removeProduct)
+  // }
+
+  let createBtn = document.getElementById("create")
+  createBtn.addEventListener('click', createProduct)
 });
