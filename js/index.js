04b3868bb86cc679ee8931d2931adfadafc2a01f
Daniel Perz <dpere316@fiu.edu>
2020-06-17 12:55:56 -0400

    Done

--- js/index.js
+++ js/index.js
@@ -3,29 +3,43 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector(".price span").innerHTML;
+  const quantity = product.querySelector(".quantity input").value;
+  const subtotalPrice = price * quantity;
+  const subtotal = product.querySelector(".subtotal span");
+  subtotal.innerHTML = subtotalPrice;
+  return subtotalPrice
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const allProducts = document.querySelectorAll(".product");
+  
+  let sum = 0;
+  for (let i = 0; i < allProducts.length; i++) 
+  {
+    sum += updateSubtotal(allProducts[i]);
+    console.log(updateSubtotal(allProducts[i]))
+  }
 
-  // ITERATION 2
-  //... your code goes here
+  // allProducts.forEach((row) => {
+  //   updateSubtotal(row);
+  // });
 
-  // ITERATION 3
-  //... your code goes here
+  const totalPrice = document.querySelector("#total-value span");
+  totalPrice.innerHTML = sum;
+  
+ 
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode)
+  calculateAll()
 }
 
 // ITERATION 5
@@ -35,6 +49,11 @@ function createProduct() {
 }
 
 window.addEventListener('load', () => {
+
+  const removeItemBtn = document.getElementsByClassName('btn btn-remove');
+  for (elem of removeItemBtn) {
+    elem.addEventListener('click',removeProduct)
+  }
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
