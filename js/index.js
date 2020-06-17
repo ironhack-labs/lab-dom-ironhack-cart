c63e810b0943063cc271f5d4bbbea981dc69ec00
vlee13 <vlee13@gmail.com>
2020-06-17 09:18:13 -0700

    ironcart done/no bonus

--- js/index.js
+++ js/index.js
@@ -3,22 +3,41 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
-}
+    let price = product.querySelector('.price span').innerText;
+    let quantity = product.querySelector('.quantity input').value;
+    let subtotalPrice = price*quantity;
+    product.querySelector(".subtotal span").innerHTML = subtotalPrice
+    return subtotalPrice
+};
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+
+// function calculateAll() {
+//   // code in the following two lines is added just for testing purposes.
+//   // it runs when only iteration 1 is completed. at later point, it can be removed.
+//   const singleProduct = document.querySelector('.product');
+//   updateSubtotal(product);
+//   };
   // end of test
 
   // ITERATION 2
   //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+  function calculateAll () {
+    const allProducts = document.querySelectorAll('.product');
+    let total = 0
+    allProducts.forEach(eachProduct=> {
+     let subtotal = updateSubtotal(eachProduct);
+     total += subtotal 
+    });
+    document.querySelector("#total-value span").innerHTML = total
 }
+    
+  
+
+  // ITERATION 3
+  //... done above
+  
 
 // ITERATION 4
 
@@ -28,6 +47,16 @@ function removeProduct(event) {
   //... your code goes here
 }
 
+const makeDeleteButtonWork = () => {
+  let rows = document.querySelectorAll(".products");
+  rows.forEach(row => {
+    row.querySelector(".btn-remove").onclick = function(e) {
+      row.remove()
+    }
+  })
+  
+
+};
 // ITERATION 5
 
 function createProduct() {
@@ -37,6 +66,6 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  makeDeleteButtonWork()
   //... your code goes here
 });
