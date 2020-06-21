735374042b41f38231719ba854a9b4ff149030e6
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-06-21 15:38:38 +0200

    create new product completed

--- js/index.js
+++ js/index.js
@@ -43,31 +43,88 @@ document.querySelector('#total-value span').innerHTML= totprice;
 // ITERATION 4
 
 function removeProduct(event) {
+  
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  const parentEl = target.parentElement.parentElement;
+  // console.log('The target in remove is:', parentEl);
+  parentEl.parentElement.removeChild( parentEl);
+   parentEl .style.display = 'none';
+  //  parentEl.parentNode.removeChild(parentEl);
   //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
+
+  console.log( ' create clicked');
   //... your code goes here
+  if(  document.getElementsByClassName('create-product')[0].hasAttribute('id') ) {
+    document.getElementsByClassName('create-product')[0].removeAttribute("id");
+  }
+ 
+  let prodName = Array.from( document.getElementsByTagName('input')).find( (e)=>{ 
+    if(e.getAttribute("placeholder") === "Product Name") {
+        return   e.value;
+    }
+  }) ;
+
+  let prodPrice = Array.from( document.getElementsByTagName('input')).reduce( (acc,e)=>{ 
+    if(e.getAttribute("placeholder") === "Product Price") {
+      console.log(  e.value );
+          return e.value;
+    }
+  },0) ;
+ 
+  if( typeof prodName === "undefined" || prodName.value.length === 0 || Number(prodPrice) === 0 ) {
+    prodName = "";
+    console.log( prodName + prodPrice);
+    // document.getElementsByTagName('body')[0].innerHTML += `<p class="bg-warning">This is a warning background</p>`;
+    document.getElementsByClassName('create-product')[0].setAttribute("id", "alert");
+  }else{
+    prodName =  prodName.value ;
+    console.log( prodName + prodPrice);
+    let prodRow =   `<tr class="product">
+    <td class="name">
+      <span>${prodName}</span>
+    </td>
+    <td class="price">$<span>${prodPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+    </tr>`;
+    document.getElementsByTagName('tbody')[0].innerHTML += prodRow;
+    Array.from( document.getElementsByTagName('input')).forEach( (e)=>{ 
+      if(e.getAttribute("placeholder") === "Product Name" ) {
+             e.value =" ";
+      }else if( e.getAttribute("placeholder") === "Product Price") {
+        e.value = 0.0;
+      }
+    }) ;
+  }
+ 
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  // let liTags = document.getElementsByTagName('btn-remove');
-  // for(let i=0; i < liTags.length; i++){
-  //   liTags[i].addEventListener
-  // // liTags[i].onclick = function(e){
-  // //   console.log(e.currentTarget.innerHTML);
-  // // }
-  // }
-
-
+  
   const createBtn = document.getElementById('create');
-  createBtn.addEventListener('click',createProduct() );
-  //... your code goes here
+  createBtn.addEventListener('click',createProduct );
+   //... your code goes here
+
+
+  let liTags =  Array.from(document.getElementsByClassName('btn-remove') );
+  // let liTags = document.querySelectorAll('.btn-remove');
+  for(let i=0; i < liTags.length; i++){    
+    // console.log( liTags[i].event );
+    liTags[i].addEventListener("click", removeProduct);   
+  }
+
+
 });
