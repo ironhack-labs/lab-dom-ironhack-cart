a2d429eee6f3e3ef9f5cf8695e40600be1b46437
Matthew Howard <mattalanhoward@gmail.com>
2020-06-21 22:16:40 +0200

    done.  5 was taken from another student

--- js/index.js
+++ js/index.js
@@ -4,10 +4,7 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   // Step 1 Get DOM elements that hold price and quantity.
-  let price = product.querySelector(".price span")
-  // Step 2 Get value of price.
-  price = price.innerHTML
-
+  let price = product.querySelector(".price span").innerHTML
   let quantity = product.querySelector(".quantity input").value
 
   //Step 3 Save subtotal to variable.
@@ -59,18 +56,94 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
 
+  //remove product
  target.parentNode.parentNode.remove();
-  //still need to update price. 
+ 
+  //update price
   calculateAll(); 
-  
+
+
 }
 
 // ITERATION 5
 
 function createProduct() {
   console.log('this button works!')
+  
+  //select newProduct
+  let newProductName = document.querySelector(".create-product input[type='text']").value;
+  console.log(newProductName)
 
+  //select newProductPrice
+  let newProductPrice = document.querySelector(".create-product input[type='number']").value;
+  console.log(newProductPrice)
+
+  //get reference to table
+  //add tbody id to table to get the correct part of the table. 
+let tableRef = document.querySelector('tbody');
+console.log(tableRef)
+
+
+
+
+
+
+//********This was taken from someone else's work in a different class to see a more concise way of doing it.  
+tableRef.innerHTML += `<tr class="product">
+  <td class="name">
+    <span>${newProductName}</span>
+  </td>
+  <td class="price">$<span>${newProductPrice}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+</tr>`
+document.querySelector(".create-product td:first-child input").value = ""; // Clear input value
+document.querySelector(".create-product td:nth-child(2) input").value = 0; // Reset input value again.
+
+//remove button does not work after new product is created.
+
+
+
+
+//******this is what I was working on.
+  // insert row at the end of the table
+  // var newRow = tableRef.insertRow(-1);
+  // let newProductCell = newRow.insertCell(0);
+  // let newProductText = document.createTextNode(newProduct)
+  // newProductCell.appendChild(newProductText);
+
+
+  // let newPriceCell = newRow.insertCell(1);
+  // let newPrice = document.createTextNode(newProductPrice)
+  // newPriceCell.appendChild(newPrice);
+
+
+  // let newQuantityCell = newRow.insertCell(2);
+  // let newQuantityInput = document.createElement("INPUT");
+  // newQuantityInput.setAttribute("type", "number");
+  // newQuantityInput.setAttribute("value", "0");
+  // newQuantityInput.setAttribute("min", "0");
+  // newQuantityInput.setAttribute("placeholder", "Quantity");
+
+  // newQuantityCell.appendChild(newQuantityInput);
+
+  
+  // let newSubtotalCell = newRow.insertCell(3);
+  // let newSubtotal = document.createElement("value");
+  // newSubtotal.setAttribute("class", "subtotal")
+  // newSubtotalCell.appendChild(newSubtotal);
+  
 }
+
+
+
+
+
 
 const createProductBtn = document.getElementById('create');
 createProductBtn.addEventListener('click', createProduct);
@@ -84,7 +157,7 @@ window.addEventListener('load', () => {
   for (i = 0; i < removeButton.length; i++) {
     removeButton[i].addEventListener('click',removeProduct);
   }
-  console.log(removeButton)
+  // console.log(removeButton)
 
 
 
