108c628b61d24c83a687e4bff4e606aa24bcb0c2
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-06-23 10:10:40 +0200

    done

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+   //... your code goes here
+  
+   const price = product.querySelector('.price span').innerHTML
+   const quantity = product.querySelector('.quantity input').value
+   let subtotalPrice= price * quantity
+   console.log(subtotalPrice)
+   product.querySelector('.subtotal span').innerHTML = subtotalPrice
+   return subtotalPrice
 }
 
 function calculateAll() {
@@ -15,25 +21,45 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
-
+  const products = document.querySelectorAll('.product')
+  let productsSubtotalPrice = 0
+  products.forEach(function (elm) {
+    productsSubtotalPrice += updateSubtotal(elm)
+  });
   // ITERATION 3
   //... your code goes here
+  document.querySelector('#total-value span').innerHTML =productsSubtotalPrice
 }
 
-// ITERATION 4
-
+// BONUS  ITERATION 4
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  target.parentNode.remove()
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
-}
+  function createProduct() {
+    //... your code goes here
+    // Create New Row and Fields
+    const newRow = document.querySelector('.product').cloneNode(true)
+    const NewProductName= document.querySelector('.create-product td input').value 
+    const newPrice= document.querySelector('.price-new-product input').value
+    // Fill them
+    document.querySelector('.name span').innerText = newProductName
+    document.querySelector('.price span').innerText= newPrice
+  
+    const button = document.querySelector('.action .btn').cloneNode(false)
+    button.addEventListener('click', removeProduct)
+    //Add new Row to the table body and Event Listener
+    document.querySelector('tbody').appendChild(newRow)
+  }
 
+createProduct();
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
