b05076199ca4df823429e897bc62fc144255d770
Matthew Howard <mattalanhoward@gmail.com>
2020-06-25 17:52:21 +0200

    Work on 5 with own code. remove/reset do not work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  console.log("Calculating subtotal, yey!");
 
   // Step 1 Get DOM elements that hold price and quantity.
   let price = product.querySelector(".price span").innerHTML
@@ -10,8 +9,8 @@ function updateSubtotal(product) {
   //Step 3 Save subtotal to variable.
   const subtotal = (quantity*price);
 
-  //Step 4 Get DOM element for class 'subtotal'
-  let sub = product.querySelector('.subtotal span');
+  //Step 4 Get DOM element for class "subtotal"
+  let sub = product.querySelector(".subtotal span");
 
   //Step 5 Set subtotal price to the corresponding DOM element.
   sub.innerHTML = subtotal;
@@ -24,14 +23,7 @@ function updateSubtotal(product) {
 
 //this function fires with calculateButton Click.
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-
   let allProducts = document.getElementsByClassName("product");
   // console.log(allProducts)
 
@@ -42,35 +34,35 @@ function calculateAll() {
   for(i=0;i<allProducts.length;i++){
     updateSubtotal(allProducts[i])
     subTotalsum += updateSubtotal(allProducts[i])
-  }
+  };
 
   
-  // ITERATION 3
-//display subTotalsum on the DOM.
-document.querySelector("h2 span").innerHTML = subTotalsum
+// ITERATION 3
+  //display subTotalsum on the DOM.
+  document.querySelector("h2 span").innerHTML = subTotalsum
 
-}
+};
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  console.log("The target in remove is:", target);
 
   //remove product
- target.parentNode.parentNode.remove();
+  target.parentNode.parentNode.remove();
  
   //update price
   calculateAll(); 
 
 
-}
+};
+
 
 // ITERATION 5
 
 function createProduct() {
-  console.log('this button works!')
-
   //select newProduct
   let newProductName = document.querySelector(".create-product input[type='text']").value;
 
@@ -81,69 +73,118 @@ function createProduct() {
 
   //get reference to table
   //add tbody id to table to get the correct part of the table. 
-let tableRef = document.querySelector('tbody');
-console.log(tableRef)
+  let tableRef = document.querySelector("tbody");
+  console.log(tableRef)
 
 
   //insert row at the end of the table
-  var newRow = tableRef.insertRow(-1);
+  let newRow = tableRef.insertRow(-1);
   newRow.setAttribute("class","product");
 
 
+  //PRODUCT
+  //insert new row cell at index 0 for PRODUCT NAME
   let newProductCell = newRow.insertCell(0);
+
+  //set attributes to cell.
   newProductCell.setAttribute("class", "name")
-  // let newProductSpan = document.createElement("span")
-  let newProductText = document.createTextNode(newProductName)
-  newProductCell.appendChild(newProductText);
+  
+  // create span element
+  let newProductSpan = document.createElement("span");
+  //add innerHTML of newProductName (input)
+  newProductSpan.innerHTML=newProductName
+  //append span to cell
+  newProductCell.appendChild(newProductSpan)
 
 
+  //PRICE
+  //insert new row cell at index 1 for PRICE
   let newPriceCell = newRow.insertCell(1);
-  newPriceCell .setAttribute("class", "price")
-  let newPrice = document.createTextNode(newProductPrice)
-  newPriceCell.appendChild(newPrice);
+
+  //set attributes of cell.
+  newPriceCell.setAttribute("class", "price")
+  // newPriceCell.setAttribute("placeholder", "Price")
+  // create span element
+  let newPriceSpan = document.createElement("span");
+  //add innerHTML of newPriceName (input)
+  newPriceSpan.innerHTML=newProductPrice
+  //append span to cell
+  newPriceCell.innerHTML = "$";
+  newPriceCell.appendChild(newPriceSpan);
 
 
+  //QUANTITY
+  //insert new cell at index 2 for Quantity
   let newQuantityCell = newRow.insertCell(2);
+
+  //create input element
   let newQuantityInput = document.createElement("INPUT");
+  
+  //set attributes of inputs element
   newQuantityCell.setAttribute("class","quantity")
   newQuantityInput.setAttribute("type", "number");
   newQuantityInput.setAttribute("value", "0");
   newQuantityInput.setAttribute("min", "0");
   newQuantityInput.setAttribute("placeholder", "Quantity");
+
+  //append input to cell
   newQuantityCell.appendChild(newQuantityInput);
 
-  
+
+  //SUBTOTAL
+  //insert new row cell at index 3 for SUBTOTAL
   let newSubtotalCell = newRow.insertCell(3);
-  newSubtotalCell.setAttribute("class","subtotal")
-  let newSubtotal = document.createElement("value");
-  newSubtotal.setAttribute("class", "subtotal")
-  newSubtotalCell.appendChild(newSubtotal);
-  
 
+  //set attributes of cell.
+  newSubtotalCell.setAttribute("class", "subtotal")
+  // create span element
+  let newSubtotalSpan = document.createElement("span");
+    
+  //append span to cell
+  newSubtotalCell.innerHTML = "$";
+  newSubtotalCell.appendChild(newSubtotalSpan);
+
+  
+  //REMOVE BUTTON
+  //insert new row cell at index 4 for REMOVE BUTTON
   let removeButton = newRow.insertCell(4);
+
+
+  //create element 
+  let newButton = document.createElement("button")
+
+  //set attributes for button
   removeButton.setAttribute("class","action")
-  
-}
+  newButton.setAttribute("class","btn btn-remove")
+
+  //Add innerHTML
+  newButton.innerHTML = "Remove"
+
+  //Append 
+  removeButton.appendChild(newButton)
 
 
+ 
+};
+
+// let reset = document.getElementsByTagName('input')
+// for(i = 0;i<reset.length;i++){
+// reset[i].setAttribute("value","0")
+// };
+
+// console.log(reset[0])
+
+const createProductBtn = document.getElementById("create");
+createProductBtn.addEventListener("click", createProduct);
 
 
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
-
-const createProductBtn = document.getElementById('create');
-createProductBtn.addEventListener('click', createProduct);
-
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  let removeButton = document.getElementsByClassName('btn btn-remove'), i;
+  let removeButton = document.getElementsByClassName("btn btn-remove"), i;
   for (i = 0; i < removeButton.length; i++) {
-    removeButton[i].addEventListener('click',removeProduct);
+    removeButton[i].addEventListener("click",removeProduct);
   }
   // console.log(removeButton)
-
-
-
 });
