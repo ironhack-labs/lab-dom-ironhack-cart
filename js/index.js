e0bed02e58590bcd5af2bc5f2b497094374ef783
Katie Stamey <katie.m.stamey@gmail.com>
2020-07-14 22:03:11 +0200

    web-remote-2007 katie stamey

--- js/index.js
+++ js/index.js
@@ -2,6 +2,19 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  let priceValue = price.innerHTML;
+
+  const quantity = product.querySelector('input');
+
+  let total = quantity.value * priceValue;
+
+  console.log(total);
+
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = total;
+
+  return total;
 
   //... your code goes here
 }
@@ -9,16 +22,34 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const products = document.querySelectorAll('.product');
+  let total = 0;
+  for (let product of products) {
+    total += updateSubtotal(product);
+  }
+
+  let sum = document.querySelector('#total-value span');
+  sum.textContent = total;
+  
+  
   // end of test
+
+
+
+
 
   // ITERATION 2
   //... your code goes here
 
+
+
+
+
   // ITERATION 3
   //... your code goes here
 }
+
+
 
 // ITERATION 4
 
@@ -27,6 +58,9 @@ function removeProduct(event) {
   console.log('The target in remove is:', target);
   //... your code goes here
 }
+
+
+
 
 // ITERATION 5
 
