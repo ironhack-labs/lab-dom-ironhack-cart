637072f434bbd5baa2301b70003e32c8fc022157
allecramarcella <allecramarcella.dev@gmail.com>
2020-07-22 13:51:48 +0200

    incl bonus

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function updateSubtotal(product) {
   const quantityValue = quantity.valueAsNumber;
   
   price = price.innerHTML;
+  // console.log(price)
 
   const subTotal = price * quantityValue;
   // console.log(subTotal)
@@ -17,38 +18,42 @@ function updateSubtotal(product) {
  
   return productSubTotal;
   
-
-
-
-  //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  console.log(singleProduct)
-  // updateSubtotal(singleProduct);
-  // updateSubtotal(products)
-  
-
   // // ITERATION 2
 
   const products = document.querySelectorAll('.product');
-  console.log(products)
+  // console.log(products)
   const productsArr = [...products]
-  console.log(productsArr)
 
   productsArr.forEach(item => updateSubtotal(item));
-  console.log(productsArr)
-
   const subtotalUpdate = productsArr
-  console.log(productsArr)
+  // console.log(productsArr)
+  
 
   // ITERATION 3
-  const totalPrice = document.querySelector('.calculate-total button');
-  // totalPrice.innerHTML = productArr
-  console.log(totalPrice)
+  const totalValue = document.querySelector('#total-value span');
+  let totalPrice = 0
+
+  // productsArr.forEach(sumFunction)
+
+  // function sumFunction (item){
+  //   totalPrice += Number(item.querySelector('.subtotal span').innerHTML)
+  //   // console.log(typeof item.querySelector('.subtotal span').innerHTML)
+  //   console.log(totalPrice)
+  //   totalValue.innerHTML = totalPrice
+  //   console.log(totalValue)
+  // }
+
+  productsArr.forEach((item) => {
+    totalPrice += Number(item.querySelector('.subtotal span').innerHTML)
+    // console.log(typeof item.querySelector('.subtotal span').innerHTML)
+    // console.log(totalPrice)
+    totalValue.innerHTML = totalPrice
+    // console.log(totalValue)
+  })
+  
 }
 
 // ITERATION 4
@@ -56,18 +61,60 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  // const productLine = document.getElementsByClassName('product');
+  const wholeCart = target.parentNode.parentNode.parentNode
+  const productLine = target.parentNode.parentNode
+  console.log(wholeCart)
+  console.log(productLine)
+
+  wholeCart.removeChild(productLine)
 }
 
 // ITERATION 5
 
+const createNewProduct = document.getElementById('create')
+createNewProduct.addEventListener('click', createProduct)
+
 function createProduct() {
-  //... your code goes here
+ 
+  // console.log(' Test: The create product button was clicked')
+  const nameNewProduct = document.getElementById('name-new-product').value
+  const priceNewProduct = document.getElementById('price-new-product').valueAsNumber
+  
+
+  const productLine = document.querySelector('.product');
+  const parentProductLines = productLine.parentNode
+  console.log(productLine)
+  console.log(parentProductLines)
+  const cloneProductLine = productLine.cloneNode(true);
+
+  
+  parentProductLines.appendChild(cloneProductLine); //How can I make the new line appear as the last product??
+
+  let nameProduct = document.querySelector('.name span');
+  console.log(nameProduct)
+  nameProduct.innerHTML = nameNewProduct;
+  console.log(nameProduct)
+  
+  let priceProduct = document.querySelector('.price span');
+  priceProduct.innerHTML = priceNewProduct
+
+
 }
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
+  // console.log(calculatePricesBtn)
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButton = document.getElementsByClassName('btn-remove');
+  // console.log(removeButton)
+  const removeButtonArr = [...removeButton];
+  // console.log(removeButtonArr)
+  
+  removeButtonArr.forEach((item) => {
+    item.addEventListener('click', removeProduct);
+  })
 });
