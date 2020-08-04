997245d93bc0af8440995c6e26323d8840e385cb
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-04 16:57:58 +0200

    iteration 5 done

--- js/index.js
+++ js/index.js
@@ -35,6 +35,28 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
+  const [name, price] = document.querySelectorAll('tfoot input');
+  const tbody = document.getElementsByTagName('tbody')[0];
+  tbody.insertAdjacentHTML('beforeend', `
+    <tr class="product">
+      <td class="name">
+        <span>${name.value}</span>
+        </td>
+      <td class="price">$<span>${price.value}</span></td>
+      <td class="quantity">
+        <input type="number" value="0" min="0" placeholder="Quantity" />
+      </td>
+      <td class="subtotal">$<span>0</span></td>
+      <td class="action">
+        <button class="btn btn-remove">Remove</button>
+      </td>
+    </tr>
+  `)
+  const removeButtons = document.querySelectorAll('.btn-remove');
+  removeButtons.forEach(removeButton => {removeButton.removeEventListener('click', removeProduct)});
+  removeButtons.forEach(removeButton => {removeButton.addEventListener('click', removeProduct)});
+  name = "";
+  price = 0;
 }
 
 window.addEventListener('load', () => {
@@ -47,3 +69,6 @@ const removeButton = document.querySelectorAll(`.btn-remove`);
 removeButton.forEach((element) =>{
   element.addEventListener(`click`,removeProduct);
 });
+
+const createButton = document.getElementById(`create`);
+createButton.addEventListener(`click`,createProduct);
