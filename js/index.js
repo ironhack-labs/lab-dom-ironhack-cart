ea8e1cc70cca348b886b6dfb63b039a3f780b441
Maria Sharpar <masha.sharpar@gmail.com>
2020-08-04 16:39:13 +0200

    done without Bonus Iterations

--- js/index.js
+++ js/index.js
@@ -2,23 +2,48 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let price=product.querySelector('.price span');
+  let quantity=product.querySelector('.quantity input');
+  
+  let priceElem= parseFloat(price.innerText); 
+  let quantityElem = quantity.valueAsNumber;
 
-  //... your code goes here
+  subtotalElem = priceElem * quantityElem;
+
+  let subtotal=product.querySelector('.subtotal span')
+
+  subtotal.innerText = subtotalElem;
+
+  return subtotalElem;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const items = document.querySelectorAll('.product');
+
+  let total=0; 
+  
+
+  for (let i=0; i<items.length; i++) {
+    total += updateSubtotal(items[i]);
+  }
 
   // ITERATION 3
-  //... your code goes here
+
+  document.querySelector('#total-value span').innerHTML = total;
+  
+  /* let totalElem = document.querySelector('#total-value span');
+  totalElem.innerHTML = total;
+  return totalElem; */
 }
+
+// const totalValue = document.querySelector('h2 span')
 
 // ITERATION 4
 
