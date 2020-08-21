ed3df1f481b0e31a2178f0047d33c5add09cb0c9
Veronica De Leon <v.dleon@gmail.com>
2020-08-21 11:18:08 +0200

    misswed the last bonus

--- js/index.js
+++ js/index.js
@@ -41,12 +41,27 @@ function removeProduct(event) {
   const tBody = document.querySelector("tbody");
   console.log(tBody);
   tBody.removeChild(target.parentNode.parentNode);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const name = document.querySelector(".create-product td input").value;
+  const price = document.querySelectorAll(".create-product td input")[1].value;
+
+  const product = document.querySelector(".product");
+  const newProduct = product.cloneNode(true);
+
+  newProduct.querySelector(".name").innerHTML = name; //reassignment
+  newProduct.querySelector(".price").innerHTML = `<span>$${price}</span>`;
+  newProduct.querySelector(
+    ".quantity"
+  ).innerText = `<input type="number" placeholder="Quantitiy" value ="0" min="0">`;
+  newProduct.querySelector(".subtotal").innerHTML = `<span>0</span>`;
+
+  // i missed connecting the button, would like to ask one of you later
+  // newProduct.querySelector(".btn-remove").addEventListener("click", removeProduct).parentNode.append(newProduct);
 }
 
 window.addEventListener("load", () => {
@@ -60,4 +75,7 @@ window.addEventListener("load", () => {
   removeBtn.forEach((button) => {
     button.addEventListener("click", removeProduct);
   });
+
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", createProduct);
 });
