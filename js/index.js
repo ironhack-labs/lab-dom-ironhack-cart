606fd9d0f55de3b36cac9ea93bbb9a99d0edef45
Diego Molina <diegomolina83@gmail.com>
2020-08-22 18:52:38 +0200

    Done con ampliaciones

--- js/index.js
+++ js/index.js
@@ -1,40 +1,48 @@
-// ITERATION 1
-
+//clonamos un nodo product para usarlo luego con el Append
 const rowToAppend= (document.querySelector('.product')).cloneNode(true)
 
+// ITERATION 1
+//UPDATE SUBTOTAL FUNCTION
 
 function updateSubtotal(product) {
-  console.log("Calculating subtotal, yey!");
-  //... your code goes here
+  
 
   const price = product.querySelector(".price span");
 
-  const quantity = product.querySelector(".quantity input");
+  const quantity = Math.round(product.querySelector(".quantity input").value);
+ 
 
-  let subtotal = price.textContent * quantity.value;
+  let subtotal = price.textContent * quantity;
 
   const printSubtotal = product.querySelector(".subtotal span");
-  printSubtotal.innerText = subtotal;
-  return subtotal;
+  printSubtotal.innerText = subtotal.toFixed(2);
+  return subtotal;0
 }
 
+ // ITERATION 2
+//CALCULATE TOTAL FUNCTION
 
 function calculateAll() {
-  // ITERATION 2
+ 
 
   const products = document.querySelectorAll(".product");
 
+  
   products.forEach((element) => {
     updateSubtotal(element);
+    element.querySelector('.quantity input[value]').value=Math.round(element.querySelector('.quantity input[value]').value)
   });
 
   // ITERATION 3
   let total = 0;
-  products.forEach((element) => {
-    //quitamos el símbolo del dolar $
+  products.forEach((element) => 
+  {
+    //quitamos el símbolo del dolar $ Y  convertimos en decimal el subtotal
     let onlyNumber = element.querySelector(".subtotal").textContent.slice(1);
-    total += parseInt(onlyNumber);
+    total += parseFloat(onlyNumber);
   });
+
+
   const totalValue = document.querySelector("#total-value span");
   totalValue.innerText = total;
 }
@@ -42,17 +50,17 @@ function calculateAll() {
 
 
 // ITERATION 4
+//REMOVE PRODUCTS and recalculate total
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log(target)
-  console.log("The target in remove is:", target);
-  //... your code goes here
-target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
+  target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
 calculateAll()
 }
 
+
 // ITERATION 5
+// CREATE PRODUCTS
 
 function createProduct() {
   //... your code goes here
@@ -60,24 +68,33 @@ function createProduct() {
 
   const createdProductName= document.querySelector('.create-product input[type=text]')
   const createdProductPrice= document.querySelector('.create-product input[type=number]')
+  const createdProductQuantity= document.querySelector('#newProductQuantity')
+
+  //CONTROL THAT THE USER INTRODUCES THE PRODUCT
+
     if(createdProductName.value!="") 
     {
   rowToAppend.querySelector('.name span').innerText= createdProductName.value;
-  rowToAppend.querySelector('.product .price span').innerText=parseInt(createdProductPrice.value).toFixed(2)
+  rowToAppend.querySelector('.product .price span').innerText=parseFloat(createdProductPrice.value).toFixed(2)
+  rowToAppend.querySelector('.quantity input[value]').value=Math.round(createdProductQuantity.value)
   
+  //RESET VARIABLES
   createdProductName.value=""
   createdProductPrice.value=0
+  createdProductQuantity.value="1"
+  console.log(createdProductQuantity)
+
   const whereAppend= document.querySelector('tbody')
-  console.log(whereAppend)
+
   whereAppend.appendChild(rowToAppend.cloneNode(true))
   addNewBtnToRemove();
   calculateAll()
     }
-    else alert('Debes introducir un elemento')
+    else alert('Debes introducir un PRODUCTO')
 }
 
 
-//Función que incluye los botones de Remove de las nuevas líenas creadas
+//Function that includes the Remove buttons of the new lines created
 
 function addNewBtnToRemove(){
   const removeBtn= document.querySelectorAll('.btn-remove')
