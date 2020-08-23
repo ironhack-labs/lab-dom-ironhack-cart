128ff3db6de0776a5f2805fd4c6e32bf0bc39785
home <home@MacBook-Pro-de-hack.local>
2020-08-23 23:36:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,31 +1,81 @@
+// // ITERATION 1
+
+// function updateSubtotal(product) {
+//   console.log("Calculating subtotal, yey!");
+//   const price = product.querySelector(".price span").innerHTML;
+//   const quantity = product.querySelector(".quantity input").value;
+//   const subtotalPrice = price * quantity;
+//   product.querySelector(".subtotal span").innerHTML = subtotalPrice;
+//   return subtotalPrice;
+// }
+
+// function calculateAll() {
+//   // ITERATION 2
+//   const singleProduct = document.querySelectorAll(".product");
+//   singleProduct.forEach((elm) => {
+//     productsPrice += updateSubtotal(elm);
+//     console.log(productsPrice);
+//     const totalValue = (document.querySelector(
+//       "#total-value span"
+//     ).textContent = subTotal);
+//   });
+//   // ITERATION 3
+//   // let total = 0;
+//   // totalPrice = document.querySelector("#total-value span").innerHTML;
+//   // total = totalPrice.innerHTML;
+// }
+
+// // ITERATION 4
+
+// function removeProduct(event) {
+//   const target = event.currentTarget;
+//   console.log("The target in remove is:", target);
+// }
+
+// // ITERATION 5
+
+// function createProduct() {
+//   //... your code goes here
+// }
+
+// window.addEventListener("load", () => {
+//   const calculatePricesBtn = document.getElementById("calculate");
+//   calculatePricesBtn.addEventListener("click", calculateAll);
+// });
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  console.log("Calculating subtotal, yey!");
+  const price = product.querySelector(".price span").innerHTML;
+  const quantity = product.querySelector(".quantity input").value;
+  const subtotalPrice = price * quantity;
+  product.querySelector(".subtotal span").innerHTML = subtotalPrice;
+  return subtotalPrice;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  const productValue = document.querySelectorAll(".product");
+  let total = 0;
+  for (let i = 0; i < productValue.length; i++) {
+    total += updateSubtotal(productValue[i]);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  const totalValue = document.querySelector("#total-value span");
+  totalValue.innerHTML = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log("The target in remove is:", target);
+  const tableBody = document.querySelector("tbody");
+  const buttonNode = target.parentNode;
+  parentNode = buttonNode.parentNode;
+  document.querySelector("tableBody").removeChild(parentNode);
+  calculateAll();
 }
 
 // ITERATION 5
@@ -34,9 +84,7 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 });
