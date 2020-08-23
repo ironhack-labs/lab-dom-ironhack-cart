c6123620742f2377ed81fc60993e5e672d38bc64
Javier de Salas <chinostrike@gmail.com>
2020-08-23 17:16:15 +0200

    Finished

--- js/index.js
+++ js/index.js
@@ -1,42 +1,144 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span')
+  const quantity = product.querySelector('.quantity input')
+  const subtotalAmount = price.innerHTML * quantity.value 
+  const subtotal = product.querySelector('.subtotal span')
+  
+  subtotal.innerHTML = `${subtotalAmount}`
 
-  //... your code goes here
+  return subtotalAmount
+
+  
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const productList = document.querySelectorAll('.product')
+ 
+  productList.forEach (product => updateSubtotal(product))
 
+  const subtotalList = document.querySelectorAll('.subtotal span')
+  let totalValue = 0
+  subtotalList.forEach (function (subtotal) { 
+    totalValue += parseFloat(subtotal.innerHTML)
+  })
   // ITERATION 3
   //... your code goes here
+  const total = document.querySelector('#total-value span')
+  console.log(totalValue)
+  total.innerHTML = totalValue
 }
 
-// ITERATION 4
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+// No es la solución más elegante del mundo pero no se me ocurría otra 
+// forma de que se carguen los scripts para que los botones nuevos de 
+// remove funcionen
+window.addEventListener('load', mainfunction)
+window.addEventListener('click',mainfunction)
 
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
-window.addEventListener('load', () => {
+function mainfunction() {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+  /* No consigo entender cómo se relacionan event listener con la función remove product
+  para vincular el currentTarget al botón. Así que lo haré como nos explicó Germán
+  
+  const removeBtnList = document.querySelectorAll('.btn-remove')
+  removeBtnList.forEach( elm => addEventListener('click', removeProduct))
+   
+  const createBtn = document.getElementById('create')
+  createBtn.addEventListener('click', createProduct() )
+  */
+  
+  // ITERATION 4
+  //function removeProduct(event) 
+  document.querySelectorAll('.btn-remove').forEach (elm => {
+    elm.onclick = e => {
+      const buttonToDelete = e.currentTarget
+      const rowToDelete = buttonToDelete.parentNode.parentNode
+
+      rowToDelete.parentNode.removeChild(rowToDelete)
+      calculateAll()
+    }
+  })
+
+
+  // ITERATION 5  
+  document.querySelector('#create').onclick = function createProduct (e) {
+    //Creo la fila
+  
+    const newProduct = document.createElement('tr')
+    const cartTable = document.querySelector('#cart-body')
+    newProduct.setAttribute('class', "product")  
+
+    cartTable.appendChild(newProduct)
+   
+    //Creo los td
+    //Name
+    const formName = document.querySelector('.create-product .name') 
+    const newName = document.createElement('td')
+    const newNameSpan = document.createElement('span')
+    
+    newName.setAttribute('class', "name")
+    newNameSpan.innerText = formName.value 
+
+    newProduct.appendChild(newName)
+    newName.appendChild(newNameSpan)
+
+    //Price
+    const formPrice = document.querySelector('.create-product .price') 
+    const newPrice = document.createElement('td')
+    const newPriceSpan = document.createElement('span')
+
+    newPrice.setAttribute('class', "price")
+    newPrice.innerText = '$'
+    newPriceSpan.innerText = formPrice.value 
+
+    newProduct.appendChild(newPrice) 
+    newPrice.appendChild(newPriceSpan) 
+
+    //Quantity
+    const newQuantity = document.createElement('td')
+    const newQuantityInput = document.createElement('input')
+
+    newQuantity.setAttribute('class', 'quantity')
+    newQuantityInput.setAttribute('type', 'number')
+    newQuantityInput.setAttribute('value', '0')
+    newQuantityInput.setAttribute('min','0')
+    newQuantityInput.setAttribute('placeholder', 'Quantity')
+
+    newProduct.appendChild(newQuantity) 
+    newQuantity.appendChild(newQuantityInput)
+
+    //Subtotal
+    const newSubtotal = document.createElement('td')
+    const newSubtotalSpan = document.createElement('span')
+    
+    newSubtotal.setAttribute('class', "subtotal")
+    newSubtotal.innerText = '$'
+    newSubtotalSpan.innerText = '0'
+
+    newProduct.appendChild(newSubtotal)
+    newSubtotal.appendChild(newSubtotalSpan)
+
+    //Remove
+    const newRemove = document.createElement('td')
+    const newRemoveBtn = document.createElement('button')
+
+    newRemove.setAttribute('class', 'action')
+    newRemoveBtn.setAttribute('class', 'btn')
+    newRemoveBtn.setAttribute('class', 'btn-remove')
+    newRemoveBtn.innerText = 'Remove'
+
+    newProduct.appendChild(newRemove)
+    newRemove.appendChild(newRemoveBtn)
+
+    formName.value = ''
+    formPrice.value = 0
+  }
+  
+}
+
