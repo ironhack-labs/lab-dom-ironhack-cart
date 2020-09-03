2c6a5ba6e57d878cd1b276af40676c9170582abb
Bouziane <bey.bouziane@live.fr>
2020-09-03 17:32:22 +0200

    Bonus : Iteration 4 done

--- js/index.js
+++ js/index.js
@@ -15,17 +15,19 @@ function calculateAll() {
   // ITERATION 2
   //... your code goes here
   const allProducts = document.getElementsByClassName("product");
-  let total = Array.from(allProducts).map((item) => updateSubtotal(item));
+  let total = Array.from(allProducts).map((item) => updateSubtotal(item).innerText);
 
-  
   // ITERATION 3
   //... your code goes here
   let displayTotal = document.querySelector("#total-value span");
   function totalReducer(sum, item){
     //+ before to convert them into numbers because they are string right now
+    console.log(sum.innerText);
+    console.log(item.innerText);
     return +sum.innerText + +item.innerText;
   }
-  displayTotal.innerText = total.reduce(totalReducer);
+  displayTotal.innerText = total.reduce((acc, value) => acc + +value, 0);
+  // console.log(displayTotal.innerText);
 }
 
 // ITERATION 4
@@ -34,6 +36,9 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  target.parentElement.parentElement.outerHTML = "";
+  //update the price
+  calculateAll();
 }
 
 // ITERATION 5
@@ -47,4 +52,6 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  removeBtn.forEach((btn) => btn.onclick = removeProduct);
 });
