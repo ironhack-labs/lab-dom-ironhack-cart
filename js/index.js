d42a69c83c299da8e06b3aa2f07194d3c91673fc
Bouziane <bey.bouziane@live.fr>
2020-09-03 16:59:15 +0200

    Lab done

--- js/index.js
+++ js/index.js
@@ -12,24 +12,20 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
   //... your code goes here
   const allProducts = document.getElementsByClassName("product");
   let total = Array.from(allProducts).map((item) => updateSubtotal(item));
-  console.log(total);
 
+  
   // ITERATION 3
   //... your code goes here
   let displayTotal = document.querySelector("#total-value span");
-  const totalReducer = (sum, item) => sum + item;
-  displayTotal.innerText = total.reduce((totalReducer));
-  console.log(displayTotal.innerText);
+  function totalReducer(sum, item){
+    //+ before to convert them into numbers because they are string right now
+    return +sum.innerText + +item.innerText;
+  }
+  displayTotal.innerText = total.reduce(totalReducer);
 }
 
 // ITERATION 4
