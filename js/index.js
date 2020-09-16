f7f33e07ab108215d30f1fcc290cafa9f17f9ffa
Marie Kleinschmidt <maklein9@googlemail.com>
2020-09-16 10:20:59 +0200

    everything but bonus

--- js/index.js
+++ js/index.js
@@ -1,21 +1,55 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
-}
+  function updateSubtotal(product) {
+    console.log("here I am")
+    console.log(product);
+    
+    //console.log(document.querySelectorAll('.price span'))
+    const price = product.querySelector('.price span').innerHTML;
+    const quantity = product.querySelector('.quantity input').value;
 
-function calculateAll() {
+    let subtotal = price * quantity
+
+    product.querySelector(".subtotal span").innerHTML = subtotal
+
+    return subtotal;
+    // ... your code goes here
+  }
+
+
+
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  //const singleProduct = document.querySelectorAll('.product')[0];
+  // const singleProduct = document.querySelector('.product');
+  
+  // updateSubtotal(singleProduct);
+
+  // const secondProduct = document.querySelectorAll('.product')[1];
+  // updateSubtotal(secondProduct);
+  
+  function calculateAll() {
+    let sum = 0
+    let listOfAllProducts = document.querySelectorAll(".product")
+    for (let index = 0; index < listOfAllProducts.length; index++) {
+      const product = listOfAllProducts[index];
+      updateSubtotal(product);
+      let subtotalText = product.querySelector(".subtotal span").innerText
+      console.log(subtotalText)
+      console.log(Number(subtotalText))
+      sum += Number(subtotalText)
+      console.log(sum)
+      document.querySelector("#total-value span").innerText = sum
+    }
+  //const quantity = product.querySelector(".quantity input").value 
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  
   // ITERATION 3
   //... your code goes here
 }
