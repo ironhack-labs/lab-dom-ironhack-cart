13c61d8f4a3c0933a510f573aed1275e8ad08d35
eliot <eliot@pop-os.localdomain>
2020-09-16 22:26:06 -0500

    Done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,56 @@
-// ITERATION 1
+function updateInput(product) {
+    product.addEventListener("input", (e) => {
+        let $modifiedProduct = e.currentTarget
+        updateSubtotal($modifiedProduct)
+    })
+}
+
+function updateBtn(product) {
+    product.querySelector(".btn-remove").addEventListener("click", (e) => {
+        removeProduct(e)
+        calculateAll()
+    })
+}
+
+let $products = document.querySelectorAll('.product')
+$products.forEach(product => {
+    updateInput(product)
+    updateBtn(product)
+})
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+    const quantity = product.querySelector(".quantity input").value
+    const price = product.querySelector(".price span").innerText
+    const subTotal = (quantity * price).toFixed(2)
+    product.querySelector(".subtotal span").innerText = subTotal
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+    $products = document.querySelectorAll('.product')
+    let total = 0
+    $products.forEach(product => {
+        total += parseFloat(product.querySelector(".subtotal span").innerText)
+    })
+    document.querySelector("#total-value span").innerText = total.toFixed(2)
 }
 
 // ITERATION 4
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+// function removeProduct(event) {
+//     const target = event.currentTarget;
+//     console.log('The target in remove is:', target);
+//     //... your code goes here
+// }
 
-// ITERATION 5
+// // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
+// function createProduct() {
+//     //... your code goes here
+// }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
+    const createProductBtn = document.getElementById('create');
+    createProductBtn.addEventListener('click', createProduct);
 });
