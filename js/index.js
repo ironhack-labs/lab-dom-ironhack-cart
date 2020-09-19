15465bec1d8b9454e5005957b90b8258d29e61a5
tinaCanCode <mc.scherzberg@googlemail.com>
2020-09-19 14:40:30 +0200

    completed cart including bonus iterations

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  const price = product.querySelector('.price span').innerHTML;
+  const price = product.querySelector('.price span').innerText;
   const quantity = product.querySelector('.quantity input').value;
 
   let subtotal = price * quantity;
 
-  product.querySelector(".subtotal span").innerHTML = subtotal;
+  product.querySelector(".subtotal span").innerText = subtotal;
 
   return subtotal;
 
@@ -39,26 +39,61 @@ function calculateAll() {
   //console.log(document.querySelector("#total-value span"))
 
   // ITERATION 3
-  document.querySelector("#total-value span").innerHTML = total;
+  document.querySelector("#total-value span").innerText = total;
 }
 
-// ITERATION 4
+// ITERATION 4 - Alternative use .remove() function
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  let productToBeRemoved = target.parentNode.parentNode
+  console.log(productToBeRemoved)
+  productToBeRemoved.parentNode.removeChild(productToBeRemoved)
+
+  calculateAll()
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  console.log("Create was clicked")
+
+  let newName = document.getElementById("new-product-name").value
+  let newPrice = document.getElementById("new-product-price").value
+  //console.log(newName)
+  //console.log(newPrice)
+
+  let productNode = document.querySelector("tr.product")
+  //console.log(productNode)
+  let newProduct = productNode.cloneNode(true)
+  newProduct.querySelector(".price span").innerHTML = newPrice
+  newProduct.querySelector(".name span").innerHTML = newName
+  //console.log(newProduct)
+  let productsParent = document.querySelector("tbody")
+  //console.log(productsParent)
+  productsParent.appendChild(newProduct)
+
+  document.getElementById("new-product-name").value = "";
+  document.getElementById("new-product-price").value = "0";
+
+  newProduct.querySelector(".btn-remove").addEventListener("click", removeProduct);
+
+  // Add eventlistener to remove button
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = Array.from(document.querySelectorAll("button.btn-remove"))
+  //console.log(removeBtns)
+  removeBtns.forEach( btn => {
+    btn.addEventListener("click", removeProduct);
+  })
+
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", createProduct);
+
 });
