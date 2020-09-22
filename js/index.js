29f9abfebf19928c9e9c00f57e18f33e371d53d7
Lovro <hodak.lovro@gmail.com>
2020-09-22 21:08:45 +0200

    slowly but slowly

--- js/index.js
+++ js/index.js
@@ -1,23 +1,44 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
+function updateSubtotal(product) {
+  //console.log('Calculating subtotal, yey!');
+  
   //... your code goes here
+  let price = product.querySelector('.price span').innerText
+  //console.log(price)
+  let quantity = product.querySelector('.quantity input').value
+  //console.log(quantity)
+  let subtotalMath = (price * quantity)
+  let subtotal = product.querySelector('.subtotal span')
+  subtotal.innerText = subtotalMath
+  return subtotalMath
+  
+  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
+  //console.log(singleProduct)
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  let allProducts = document.querySelectorAll('.product')
+  let totalAllProducts = 0
+  allProducts.forEach((elem) => {
+    updateSubtotal(elem)
+    totalAllProducts += updateSubtotal(elem)
+    //console.log(elem)
+  })
   // ITERATION 3
   //... your code goes here
+  let totalPrice = document.querySelector('#total-value span')
+  totalPrice.innerText = totalAllProducts
+
 }
 
 // ITERATION 4
@@ -25,7 +46,15 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  console.log(event.target.parentNode.parentNode)
   //... your code goes here
+  //event.currentTarget
+  //removeChild
+  //parentNode
+  let bigParent = event.target.parentNode.parentNode
+  bigParent.parentNode.removeChild(bigParent)
+
+  
 }
 
 // ITERATION 5
@@ -39,4 +68,11 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+
+  const removeBtns = document.querySelectorAll('.btn-remove')
+  removeBtns.forEach((elem) => {
+    elem.addEventListener('click', (rem) => {
+      removeProduct(rem)
+    })
+  })
 });
