0ac004e6949d887e763f4b55d98cedd6a4670e8c
ZuzanaAK <kubatovazuzana@gmail.com>
2020-09-22 18:48:46 +0100

    ironhack cart

--- js/index.js
+++ js/index.js
@@ -3,21 +3,42 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
-}
+  const price = product.querySelector(".price span").innerHTML;
+  const quantity = product.querySelector(".quantity input").value;
+
+  const subOperation = price*quantity
+
+  console.log (subOperation)
+
+  let sub = product.querySelector(".subtotal span");
+
+  sub.innerHTML = subOperation
+
+return subOperation
+
+};
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ 
+ // const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let allProducts = document.querySelectorAll('.product');
+  let mySum=0;
+    allProducts.forEach((elem) =>{
+      console.log (elem)
+      console.log (updateSubtotal(elem))
+    mySum += updateSubtotal(elem)
+  })
+
 
   // ITERATION 3
   //... your code goes here
+let totalPrice = document.querySelector ("#total-value span")
+totalPrice.innerText = mySum
+
 }
 
 // ITERATION 4
