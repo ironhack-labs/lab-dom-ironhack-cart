3c651a774ee201101ca8bdf9db87878359ab1b98
JJCExcelsior <excelsior.creator@gmail.com>
2020-09-23 08:59:18 +0200

    Init commit

--- js/index.js
+++ js/index.js
@@ -1,31 +1,52 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  const subtotal = product.querySelector(".subtotal span");
 
-  //... your code goes here
+  const priceNum = price.innerHTML;
+  const quantityNum = quantity.value;
+
+  const subtotalNum = priceNum * quantityNum;
+  subtotal.innerHTML = subtotalNum;
+
+  console.log("Calculating subtotal, yey!");
+
+  return subtotalNum;
 }
-
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+  let total = 0
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll(".product");
+
+  products.forEach((singleProduct) => {
+    totalSum += updateSubtotal(singleProduct);
+    console.log(totalSum);
+  });
 
   // ITERATION 3
-  //... your code goes here
+  const total = document.querySelector("#total-value span");
+  total.innerHTML = totalSum;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode.parentNode;
+  console.log("The target in remove is:", target);
+  target.parentNode.removeChild(target);
+
+  const total = document.querySelector("#total-value span");
+  const subtotal = target.querySelector(".subtotal span");
+
+  total.innerHTML = total.innerHTML - subtotal.innerHTML;
 }
 
 // ITERATION 5
