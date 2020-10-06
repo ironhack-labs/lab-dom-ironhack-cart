74e1841576d3ffa3ea43a2b0d5068b2d52a02ce1
erifylo <erif.evangelou@gmail.com>
2020-10-06 20:34:41 +0200

    [FT-BCN-0920] Erifyli Evangelou

--- js/index.js
+++ js/index.js
@@ -2,23 +2,61 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  
 
+  console.log(price);
+  console.log(quantity);
+  
+
+  let subTotal = price*quantity
+  
+  console.log(subTotal);
+
+  let finalSubTotal = product.querySelector('.subtotal span');
+
+  finalSubTotal.innerHTML = subTotal
+  console.log(subTotal)
+
+  return (finalSubTotal.innerHTML = subTotal)
   //... your code goes here
 }
 
-function calculateAll() {
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  function calculateAll() {
+      const productos = document.getElementsByClassName("product");/// giati???
+      console.log(productos);
+
+      var total = 0;
+
+      for (let i = 0; i < productos.length; i++) {
+      updateSubtotal(productos[i]);
+      total += updateSubtotal(productos[i]);
+
+      
+    }
+    let totalisimo = document.querySelector('#total-value span');
+    console.log(total);
+    totalisimo.innerHTML = total;
+    
+    
+  }
+
+  
+  
 
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
