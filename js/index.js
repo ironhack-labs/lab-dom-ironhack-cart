e5b4337044561e72da451df4c0f2d6ec53d43bc6
Sara Amantegui <saiduna@gmail.com>
2020-10-06 20:44:22 +0200

    iteration 2

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+function updateSubtotal(product) {
+  console.log('Calculating Prices, clicked!');
+  let getPrice = product.querySelector('.price span').innerHTML;
+  let getQuantity = document.querySelector('quantity').innerHTML;
+  let calculatedSubtotal = getPrice * getQuantity;
+  let getSubtotal = product.querySelector('.subtotal').getAttribute('span');
+  getSubtotal.setAttribute('span',calculatedSubtotal);
+
+updateSubtotal();
 }
 
-function calculateAll() {
+
+//function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ // const singleProduct = document.querySelector('.product');
+
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  //Obtener los nodos para cada fila de producto
+  //Añadir new product al index.html
+  //Duplicar la tr con la clase product
+  //Renombrar el product dentro
+  //Cambiar el precio
+
+let getProducts = getElementsByClassName('product');
+let newProduct = document.createElement("product");
+newProduct.setAttribute("")
+
+
+
 
   // ITERATION 3
   //... your code goes here
