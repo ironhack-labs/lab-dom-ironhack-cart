776b822553362769abf6727794295f71fcda06a4
Lexirem <me.vallribera@gmail.com>
2020-10-06 19:35:59 +0200

    iter 2 done

--- js/index.js
+++ js/index.js
@@ -14,14 +14,19 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
+  const calculAll = [...document.querySelectorAll('.product')];
+  calculAll.forEach(singleProduct => updateSubtotal(singleProduct));
+  
+  console.log(calculateAll);
   //... your code goes here
 
   // ITERATION 3
+  const 
   //... your code goes here
 }
 
