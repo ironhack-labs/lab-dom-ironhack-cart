e6fb6d358d15e90043197f84417cc936bc3b46c9
Esteve Martin <esteve.martin@mpasolutions.es>
2020-10-07 09:23:45 +0200

    Tiempo hasta caida de internet! ole ole!

--- js/index.js
+++ js/index.js
@@ -2,21 +2,36 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  const price = parseInt(product.querySelector('.price span').innerHTML);
+  const quantity =  parseInt(product.querySelector('.quantity input').value);
+  let subtotal = price*quantity;
+  product.querySelector('.subtotal span').textContent = subtotal;
+  return subtotal;
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
+  const productCollection = document.querySelectorAll('.product');
+  var totalPrice=0;
+  // console.log(productCollection);
+  productCollection.forEach((element)=>{
+    
+    totalPrice += updateSubtotal(element);
+    console.log(totalPrice)
+  });
+  console.log("Total Price: " + totalPrice)
+  console.log(productCollection);
   //... your code goes here
 
   // ITERATION 3
+  document.getElementById('total-value').innerHTML ="Total: $<span>"+totalPrice+"</span>";
   //... your code goes here
 }
 
@@ -37,6 +52,14 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  //... your code goes here
+});
+
+window.addEventListener('remove', () => {
+  
+  const itemToRemove = document.getElementsByClassName('btn-remove');
+  itemToRemove.addEventListener('click', removeProduct);
 
   //... your code goes here
 });
