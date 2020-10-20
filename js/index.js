e8bb3ce7fd0615acfe264e3467a2e81f6f605947
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-20 22:39:10 +0200

    did the last iteration

--- js/index.js
+++ js/index.js
@@ -18,17 +18,34 @@ function calculateAll() {
   const displayTotal = document.querySelector('#total-value span')
   return displayTotal.innerHTML = endTotal
 }
+const itemList = document.querySelector('#cart tbody')
 
 // ITERATION 4
 function removeProduct(event) {
   const target = event.currentTarget;
-  const itemList = document.querySelector('#cart tbody')
   itemList.removeChild(target.parentNode.parentNode)
 }
 
 // ITERATION 5
 function createProduct() {
-  //... your code goes here
+  const newProductName = document.querySelector('.create-product .product-name input')
+  const newProductPrice = document.querySelector('.create-product .product-price input')
+  let newProductLine = document.createElement('tr')
+  newProductLine.innerHTML = `<td class="name">
+            <span>${newProductName.value}</span>
+          </td>
+          <td class="price">$<span>${newProductPrice.value}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>`
+  newProductLine.classList.add('product')
+  itemList.appendChild(newProductLine)
+  newProductName.value = ''
+  newProductPrice.value = '0'
 }
 
 window.addEventListener('load', () => {
@@ -39,4 +56,7 @@ window.addEventListener('load', () => {
   for (let i = 0; i < removeButtonsGroup.length; i++) {
     removeButtonsGroup[i].addEventListener('click', removeProduct)
   }
+
+  const createButton = document.querySelector('#create')
+  createButton.addEventListener('click', createProduct)
 });
