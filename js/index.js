5927dac8c40b3323fdd0af8230f8a5a6145c3d85
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-20 22:17:04 +0200

    did iteration 4

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
   const price = product.querySelector('.price span');
   const quantity = product.querySelector('.quantity input')
@@ -21,15 +20,13 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const itemList = document.querySelector('#cart tbody')
+  itemList.removeChild(target.parentNode.parentNode)
 }
 
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
 }
@@ -38,5 +35,8 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButtonsGroup = document.querySelectorAll('.btn-remove')
+  for (let i = 0; i < removeButtonsGroup.length; i++) {
+    removeButtonsGroup[i].addEventListener('click', removeProduct)
+  }
 });
