5f9e83737c6685d6236005c595359a343b8004c0
Rich Alossi <alossix@gmail.com>
2020-10-20 17:57:23 +0200

    Iteration 3 done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
   let price = product.querySelector(".price span");
   let quantity = product.querySelector(".quantity");
@@ -8,21 +7,19 @@ function updateSubtotal(product) {
   let subtotalVal = priceVal * quantityVal;
   let subtotal = product.querySelector(".subtotal span");
   subtotal.innerHTML = subtotalVal;
-  console.log("Calculating subtotal, yey!");
+  return subtotalVal;
 }
-
 // ITERATION 2
 function calculateAll() {
   let totalPrice = 0;
   let products = document.querySelectorAll(".product");
   products.forEach((prod) => {
-    updateSubtotal(prod);
+    totalPrice += updateSubtotal(prod);
   });
   // ITERATION 3
+  document.querySelector("#total-value span").innerText = totalPrice;
 }
-
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log("The target in remove is:", target);
