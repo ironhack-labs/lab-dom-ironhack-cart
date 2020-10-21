c34d09a45ca0ee99d43344e6b41070db91e189f0
Tomi Scattiniclear <tomasscattini@gmail.com>
2020-10-21 19:20:15 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,40 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+    const $price = product.querySelector('.price span').innerHTML
+    const $quantity = product.querySelector('.quantity input').value
+    let $subtotal = product.querySelector('.subtotal span')
+    $subtotal.innerHTML = $price * $quantity
+    return $subtotal.innerHTML
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+    let $product = document.getElementsByClassName('product')
+    let $total = document.querySelector('#total-value span')
+    let total = 0
+    for (let i = 0; i < $product.length; i++) {
+        total += parseFloat(updateSubtotal($product[i]))
+    }
+    $total.innerHTML = total
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
+    //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+    //... your code goes here
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+    //... your code goes here
 });
