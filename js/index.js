1867306707240148dcddc5e324dec9dbc427363e
teof95 <teof95@gmail.com>
2020-10-21 17:58:17 +0200

    iteration 1&2 done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,76 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+    console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+    //getting the first element with class="price" and "quantity" from the query.
+    const priceSpanElement = product.querySelector('.price span');
+    const quantityInputElement = product.querySelector('.quantity input')
+
+    //converting the priceSpanElement string into a number
+    const price = parseFloat(priceSpanElement.innerText);
+
+    //setting the quantity value as a number
+    const quantity = quantityInputElement.valueAsNumber;
+
+    //calculating the subtotal
+    const subTotal = price * quantity
+
+    //getting the first element with class="subtotal" from the query.
+    const subTotalElement = product.querySelector('.subtotal span');
+
+    //sending subtotalelement to inner text
+    subTotalElement.innerText = subTotal;
+    return subTotal;
 }
 
+
+//   // code in the following two lines is added just for testing purposes.
+//   // it runs when only iteration 1 is completed. at later point, it can be removed.
+
+// function calculateAll() {
+//     const singleProduct = document.querySelector('.product');
+//     updateSubtotal(singleProduct);
+// }
+
+// ITERATION 2
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+    const cartItemElements = document.getElementsByClassName('cart-item')
 
-  // ITERATION 3
-  //... your code goes here
+    let totalValue = 0;
+
+    for (const cartItemElement of cartItemElements) {
+        totalValue += updateSubtotal(cartItemElement);
+    }
+
+
+    //     // ITERATION 3
+    document.querySelector('#total-value span').innerHTML = totalValue;
+    subTotalElement.innerText = subTotal;
+    updateSubtotal(totalValue);
+    return totalValue;
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
+    //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+    //... your code goes here
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+    //... your code goes here
 });
