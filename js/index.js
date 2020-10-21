0c6427c276280ed6f6b8c1d8fce5057486d81476
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-21 17:39:23 +0200

    implemented feedback and adjusted some last details

--- js/index.js
+++ js/index.js
@@ -1,27 +1,28 @@
-//Global HTML selectors
-const displayTotal = document.querySelector('#total-value span')
+//Global HTML selector
 const itemList = document.querySelector('tbody')
-const newProductName = document.querySelector('.product-name input')
-const newProductPrice = document.querySelector('.product-price input')
+
 
 function updateSubtotal(product) {
-  const price = product.querySelector('.price span');
+  const price = product.querySelector('.price span')
   const quantity = product.querySelector('.quantity input')
   const subTotal = product.querySelector('.subtotal span')
-  let totalValue = Number(price.innerHTML) * parseFloat(quantity.value)
+
+  const totalValue = Number(price.innerHTML) * parseFloat(quantity.value)
   subTotal.innerHTML = totalValue
+
   return totalValue
 }
 
 function calculateAll() {
-  let endTotal = 0
+  const displayedTotal = document.querySelector('#total-value span')
   const allProducts = document.querySelectorAll('.product')
+  let endTotal = 0
 
   for (product of allProducts) {
     endTotal += updateSubtotal(product)
   }
 
-  return displayTotal.innerHTML = endTotal
+  return displayedTotal.innerHTML = endTotal
 }
 
 function removeProduct(event) {
@@ -32,6 +33,7 @@ function removeProduct(event) {
 
 function activateRemoveButtons() {
   const removeButtonsGroup = document.querySelectorAll('.btn-remove')
+
   for (button of removeButtonsGroup) {
     button.addEventListener('click', removeProduct)
   }
@@ -39,6 +41,8 @@ function activateRemoveButtons() {
 
 function createProduct() {
   let newProductRow = document.createElement('tr')
+  const newProductName = document.querySelector('.product-name input')
+  const newProductPrice = document.querySelector('.product-price input')
 
   if (newProductName.value === '') {
     alert(`Please enter a product name`)
@@ -48,7 +52,7 @@ function createProduct() {
     newProductRow.innerHTML = `<td class="name">
             <span>${newProductName.value}</span>
           </td>
-          <td class="price">$<span>${newProductPrice.value}</span></td>
+          <td class="price">$<span>${Number(newProductPrice.value).toFixed(2)}</span></td>
           <td class="quantity">
             <input type="number" value="0" min="0" placeholder="Quantity" />
           </td>
@@ -56,6 +60,7 @@ function createProduct() {
           <td class="action">
             <button class="btn btn-remove">Remove</button>
           </td>`
+
     newProductRow.classList.add('product')
     itemList.appendChild(newProductRow)
     newProductName.value = ''
@@ -65,8 +70,8 @@ function createProduct() {
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+  const calculatePricesBtn = document.getElementById('calculate')
+  calculatePricesBtn.addEventListener('click', calculateAll)
 
   activateRemoveButtons()
 
