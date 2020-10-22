46974bf2cab0dd553ef4e7a9f3e8dc26631f60ad
Maya Ausset <mayaausset@gmail.com>
2020-10-22 09:30:19 +0200

    done

--- js/index.js
+++ js/index.js
@@ -13,18 +13,23 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  console.log('blabla');
-  // end of test
+  
+  let totalValue = 0; 
 
-  // ITERATION 2
-  //... your code goes here
+  const cartItemElements = document.getElementsByClassName('product');
+  for (cartItemElement of cartItemElements){
+    totalValue += updateSubtotal(cartItemElement)
+  }
+  
+  console.log(totalValue)
 
+  document.querySelector("#total-value span").innerHTML = totalValue
+  //console.log(total);
+  
   // ITERATION 3
-  //... your code goes here
+  
 }
 
 // ITERATION 4
