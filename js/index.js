4f852513b25f018098cca08c4d4f374544b084a2
sophiepa <sophiepa@hotmail.com>
2020-10-23 15:09:48 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+function updateSubtotal(product) {
+  
+  const price = product.querySelector('.price span').innerHTML 
+
+  const quantity = product.querySelector(".quantity input").value
+  const subtotal = Number(price) * Number(quantity);
+  const subtotalElement = product.querySelector(".subtotal span")
+  subtotalElement.innerHTML = subtotal;
+  return subtotal 
 }
+
+
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+
+  
+   const cartItemElements = document.getElementsByClassName('product');
+
+  let totalValue = 0;
+
+  for(let cartItemElement of cartItemElements) {
+    totalValue += updateSubtotal(cartItemElement);
+  }
+ 
+
+
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector("#total-value span").innerHTML = totalValue
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
 
+  const productRow = target.parentNode.parentNode;
+
+  const productTable = productRow.parentNode;
+
+  productTable.removeChild(productRow);
+
+  calculateAll();
+}
 // ITERATION 5
 
 function createProduct() {
