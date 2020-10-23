53d3d746aabf7de07cbcd09b0e05ca3f1ba462ca
monibix <monibixv2@gmail.com>
2020-10-23 11:46:24 +0200

    Iteración 1, 2, 3, 4

--- js/index.js
+++ js/index.js
@@ -1,42 +1,65 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  //STEP 1 , 2
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity = product.querySelector('.quantity input').value
+  //STEP 3
+  let subtotalResult = price * quantity
+  //STEP 4
+  let subtotal = product.querySelector('.subtotal span')
+  //STEP 5
+  subtotal.innerText = subtotalResult
+  return subtotalResult
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  let allproducts = document.getElementsByClassName('product')
+  let prodArr = [...allproducts]
+  let total = 0
+  console.log(prodArr)
+  prodArr.forEach(x => total += updateSubtotal(x)) 
+  // ITERATION 3 
+  let displayTotal = document.querySelector('#total-value span')
+  displayTotal.innerText = total
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  let parent = target.parentNode.parentNode
+  parent.remove(target)
 }
 
-// ITERATION 5
-
+// ITERATION 5 --- No acabado 
 function createProduct() {
-  //... your code goes here
+  //Accediendo a name y price para extraer los valores del input
+  const name = document.querySelector('table input[type=text]').innerText
+  const unitPrice = document.querySelector('table input[placeholder=ProductPrice]')
+  name.innerHTML = name.value
+  unitPrice.innerText = unitPrice.value
+  let priceNum = parseInt(unitPrice.value)
+  
+  //Clonando toda la fila con cloneNode()
+  let trClone = document.querySelector('.product')
+  let clonedRow = trClone.cloneNode(true)
+  //Intentando modificar valor name y price... 
+
+  //Añade la nueva línea debajo del body de la tabla. 
+  let body = document.querySelector('tbody')
+  body.appendChild(clonedRow)
 }
 
 window.addEventListener('load', () => {
+  //Calculate subtotals
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  //Remove product
+  const removeBtn = document.getElementsByClassName('btn-remove');
+  let removeArr = [...removeBtn]
+  removeArr.forEach(x => x.addEventListener('click', removeProduct))
+  //Add products
+  const addProduct = document.querySelector('#create')
+  addProduct.addEventListener('click', createProduct)
 });
