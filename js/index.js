8631ce3b7938ded8fb08fca1aa80f52c9b492889
Paricia Muñoz <patrimdm96@gmail.com>
2020-10-24 20:54:11 +0100

    [WEB-DEV DE] Patricia Muñoz

--- js/index.js
+++ js/index.js
@@ -1,42 +1,93 @@
+
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input')
+  const productOF = (price.innerHTML) * (quantity.value)
+  const subTotalProd = product.querySelector('.subtotal span')
+  subTotalProd.innerHTML = productOF.toFixed(2)
+  return productOF
 }
+
+
+
+// ITERATION 2 & 3
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const prodArray = document.querySelectorAll('.product')
+  const totalValue = document.querySelector('#total-value span')
+  let sum = 0
+  prodArray.forEach(function (prod) {
+    sum += updateSubtotal(prod)
+  })
+  totalValue.innerHTML = sum.toFixed(2)
 }
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const td = target.parentNode
+  const tr = td.parentNode
+  const tbody = tr.parentNode
+  
+  tbody.removeChild(tr)
+  calculateAll()
 }
+
+
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const targetBtn = event.currentTarget;
+
+  const newRow = document.createElement('tr')
+  newRow.classList.add('product')
+  document.querySelector('tbody').appendChild(newRow)
+
+  let tdName = newRow.insertCell(0)
+  tdName.classList.add('name')
+  tdName.innerHTML = '<span class="newProd">Ironhack Product</span>'
+  document.querySelector('.newProd').innerText = "Ironhack Product" // can't retrieve input info
+
+  let tdPrice = newRow.insertCell(1)
+  tdPrice.classList.add('price')
+  tdPrice.innerHTML = '$<span class="newPrice">00.00</span>'
+  document.querySelector('.newPrice').innerText = 12 // can't retrieve input info
+
+  let tdQuantity = newRow.insertCell(2)
+  tdQuantity.classList.add('quantity')
+  tdQuantity.innerHTML = '<input type="number" value="0" min="0" placeholder="Quantity" />'
+
+  let tdSubtotal = newRow.insertCell(3)
+  tdSubtotal.classList.add('subtotal')
+  tdSubtotal.innerHTML = '$<span>0</span>'
+
+  let tdAction = newRow.insertCell(4)
+  tdAction.classList.add('action')
+  tdAction.innerHTML = '<button class="btn btn-remove">Remove</button>'
+
 }
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtnsArray = document.querySelectorAll('.product .btn-remove')
+  removeBtnsArray.forEach(function (elm) {
+    elm.addEventListener('click', removeProduct)
+  })
+
+  const createNewProduct = document.getElementById('create')
+  createNewProduct.addEventListener('click', createProduct)
+
 });
+
+
+
