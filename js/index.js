989642d22c771639d8255c042be87eadd6394647
Loreto AVoces <loretoavoces@gmail.com>
2020-10-25 21:33:27 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML
+
+  const quantity = product.querySelector('.quantity input').value
+
+
+  const subtotal = product.querySelector('.subtotal span')
+  subtotal.innerText = price * quantity
+  return subtotal
 }
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  const products = document.querySelectorAll('.product')
+  let sumTotal = 0
+  
+  products.forEach(function (product) {
+    let aux = updateSubtotal(product)
+    sumTotal += parseInt(aux.innerText)
+  })
 
   // ITERATION 3
-  //... your code goes here
+
+  const totalPrice = document.querySelector('#total-value span')
+  totalPrice.innerText = sumTotal
+
 }
+
 
 // ITERATION 4
 
@@ -26,6 +46,9 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+
+
+
 }
 
 // ITERATION 5
@@ -37,6 +60,18 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+
+  const buttons = document.querySelectorAll('.btn')
+  
+  buttons.forEach(function (eachButton) {
+    eachButton.onclick = function (removeProduct) {
+      
+  
+
+    }
+  })
+
 
   //... your code goes here
 });
