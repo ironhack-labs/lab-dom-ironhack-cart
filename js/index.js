30517716447f0d1e7b49dacb7fa119a92943a63e
dreyanfranco <dreyanfranco@gmail.com>
2020-10-25 18:45:06 +0100

    done

--- js/index.js
+++ js/index.js
@@ -4,6 +4,22 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+
+  // neeed to get html? inner.html?
+  const price = product.querySelector('.price span');
+  // getting value of input
+  const quantity = product.querySelector('.quantity input');
+  // getting subtotal
+  const subtotal = product.querySelector('.subtotal span');
+  // getting all three values
+
+  // console.log(price.innerHTML, quantity.value, subtotal.innerHTML);
+
+  return subtotal.innerHTML = price.innerHTML * quantity.value;
+
+  // to check if its returing price and quantity elements, in other words accesing (getter)
+
+
 }
 
 function calculateAll() {
@@ -16,22 +32,67 @@ function calculateAll() {
   // ITERATION 2
   //... your code goes here
 
+  //getting nodelist (array of tr.products)
+  const cartContainer = document.querySelectorAll('.product');
+
   // ITERATION 3
   //... your code goes here
+
+  //had to check code from prework
+  let sum = 0;
+  // since cartContainer access product which contains an array (nodelist) I will have to access to each value.
+  cartContainer.forEach(function (element) {
+    sum += updateSubtotal(element);
+  })
+
+  // cartContainer.forEach(element => {
+  //   sum += updateSubtotal(element);
+  // });
+
+  // get total-value -> span - getElementById it doesn't access to dollar sign
+
+  const totalValue = document.querySelector('#total-value span');
+
+  return totalValue.innerHTML = sum.toFixed(2);
 }
+
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  // console.log('The target in remove is:', target);
+  
   //... your code goes here
+  
+  // need to target class product to remove the whole thing 
+  target.parentElement.parentElement.remove();
+
+  // const cellsNode = target.parentNode;
+  // const productNode = td.parentNode;
+  // const tableNode = tr.parentNode;
+
+  // tableNode.removeChild(productNode);
+  calculateAll();
 }
 
 // ITERATION 5
 
-function createProduct() {
+function createProduct(event) {
+  const createProductTarget = event.target
   //... your code goes here
+
+  // need to access 
+  const newProductName = document.querySelector('.create-product input');
+  const newProductPrice = document.querySelector('.create-product .quantity');
+  const newProductQuantity = document.querySelector('.create-product .quantity');
+  const newProductSubtotal = document.querySelector('.create-product .subtotal');
+  const newProductButton = document.getElementById('create');
+  
+
+  calculateAll();
 }
 
 window.addEventListener('load', () => {
@@ -39,4 +100,13 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+
+  // accesing .btn-remove
+  const removeCartProducts = document.querySelectorAll('.product .btn-remove');
+  removeCartProducts.forEach(function (element2) {
+    element2.addEventListener('click', removeProduct);
+  });
+
+  const createCartProducts = document.getElementById('create');
+  createCartProducts.addEventListener('click', createProduct);
 });
