82a4423bfd50e2694c975cd242c473bb80082f62
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2020-10-25 19:20:00 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,30 +1,54 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let price = product.querySelector('.price span');
+  price = price.innerHTML;
+
+  let quantity = product.querySelector('input');
+
+  let subtotal1 = price * quantity.value;
+
+  let subtotal = product.querySelector('.subtotal span');
+
+  subtotal.innerHTML = subtotal1;
 }
+
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
+ 
   // ITERATION 2
-  //... your code goes here
+
+  const rubberDuck = document.querySelector('.rubberDuck');
+  const beachTowel = document.querySelector('.beachTowel');
+
+  updateSubtotal(rubberDuck);
+  updateSubtotal(beachTowel);
+
 
   // ITERATION 3
-  //... your code goes here
+  
+  let subtotalRubberDuck = document.querySelector('.rubberDuck .subtotal span');
+  subtotalRubberDuck = subtotalRubberDuck.innerHTML;
+  subtotalRubberDuck = parseFloat(subtotalRubberDuck);
+
+  let subtotalBeachTowel = document.querySelector('.beachTowel .subtotal span');
+  subtotalBeachTowel = subtotalBeachTowel.innerHTML;
+  subtotalBeachTowel = parseFloat(subtotalBeachTowel);
+
+  let total1 = subtotalRubberDuck + subtotalBeachTowel;
+
+  let total = document.querySelector('#total-value span');
+  total.innerHTML = total1;
 }
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+
   //... your code goes here
 }
 
@@ -38,5 +62,22 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  /* Esta parte es la de los botones "remove" pero no funciona.
+  Mi idea era: como me devuelve un array con los 2 botones, era con la variable "index"
+  captar el indice de la posicion de los botones y luego incluirlo en el addEventListener
+  Para buscar el fallo hice un typeof a "element" y me da en consola 2 objetos y ese es
+  el problema, intentar buscar el indice de la los objetos como si fuesen arrays.
+  */
+
+  const removeProductBtn = document.querySelectorAll('.btn-remove');
+  removeProductBtn.forEach(element => {
+    // console.log(typeof element);
+    let index = removeProductBtn.indexOf("element");
+    removeProductBtn[index].addEventListener('click', removeProduct);
+  });
+
+
   //... your code goes here
+
+
 });
