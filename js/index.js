49e14eebf2eba037877304029aaf143f0bf55c3b
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-10-27 20:15:32 +0100

    Done everything except final bonus question

--- js/index.js
+++ js/index.js
@@ -1,42 +1,102 @@
 // ITERATION 1
-
+' use strict'
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantityInput = document.querySelector('.quantity input')
+  const cost = Number(price.innerHTML);
+  const input = Number(quantityInput.value);
+  const totalProduct = cost * input;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = totalProduct;
+  return totalProduct;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.querySelectorAll('.product');
+  let total = 0;
 
-  // ITERATION 2
-  //... your code goes here
+  products.forEach((product) => {
+    var productTotal = updateSubtotal(product);
+    total += productTotal;
+  })
 
-  // ITERATION 3
-  //... your code goes here
+  console.log(total);
+  const totalValue = document.querySelector('#total-value span')
+  return totalValue.textContent = total;
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
+  const parentNode = document.querySelector('tbody');
+  const currentProduct = document.querySelector('.product');
+  parentNode.removeChild(currentProduct)
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
+
+const removeButtons = document.querySelectorAll('.btn-remove');
+removeButtons.forEach((removeButton) => removeButton.addEventListener('click', removeProduct));
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const name = document.querySelector('.create-product td:first-child input');
+  const unitPrice = document.querySelector('.create-product td:nth-child(2) input');
+  const nameInput = name.value;
+  const unitPriceInput = unitPrice.value;
+  const parentNode = document.querySelector('tbody');
+  const newProduct = document.createElement("tr");
+  newProduct.innerHTML = `<td class="name">
+  <span>${nameInput}</span>
+</td>
+<td class="price">$<span>${unitPriceInput}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>`
+  parentNode.appendChild(newProduct);
+  newProduct.setAttribute('id', 'new-product');
 }
+
+// function updateSubtotalOfNewProduct(newProduct) {
+//   console.log('Calculating subtotal, yey!');
+//   const newProduct = document.querySelector('#new-product');
+//   const price = newProduct.querySelector('.price span');
+//   const quantityInput = newProduct.querySelector('.quantity input')
+//   const cost = Number(price.innerHTML);
+//   const input = Number(quantityInput.value);
+//   const totalProduct = cost * input;
+//   const subtotal = newProduct.querySelector('.subtotal span');
+//   subtotal.innerHTML = totalProduct;
+//   return totalProduct;
+// }
+
+// function calculateAllNewProducts() {
+//   const newProducts = document.querySelectorAll('#new-product');
+//   let total = 0;
+
+//   newProducts.forEach((newProduct) => {
+//     var productTotal = updateSubtotal(newProduct);
+//     total += productTotal;
+//   })
+
+// console.log(total);
+// const totalValue = newProduct.querySelector('#total-value span')
+// return totalValue.textContent = total;
+
+// }
+
+const createProductButton = document.querySelector('#create');
+createProductButton.addEventListener('click', createProduct);
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
 });
