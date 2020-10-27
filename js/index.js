51a82c3ffcb40f5a368aa4403a722ac96f04d989
Facundo Gomez <gomezvera.f@gmail.com>
2020-10-27 12:25:59 +0100

    completed iterations 1-3

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
   const price = product.querySelector('.price span').innerText
   let quantity = product.querySelector('.quantity input').value
   // console.log(price)
   // console.log(quantity)
   let subtotal = product.querySelector('.subtotal span')
   subtotal.innerText = price * quantity
+  let subTot = price * quantity
   // console.log(subtotal)
   //... your code goes here
   let subVal = subtotal.innerText
   // console.log(subVal)
-  return subVal
+  return subTot
 }
 
 function calculateAll() {
@@ -20,9 +21,12 @@ function calculateAll() {
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   // let parent = document.querySelector('.product').parentNode
   // const singleProduct = document.querySelector('.product');
+  let subTot = 0
   document.querySelectorAll('.product').forEach(product => {
    updateSubtotal(product)
+   subTot = updateSubtotal(product) + subTot
   } )
+  document.querySelector('#total-value span').innerText = subTot
 }
   //   if (state.glutenFreeCrust) {
   //     oneMush.classList.add('crust-gluten-free');
