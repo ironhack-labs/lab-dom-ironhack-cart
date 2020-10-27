3f5561fa117f05680e82f3647718e4c5f9774ed0
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-10-27 22:50:51 +0100

    3 iterations done. computer hyperventilates

--- js/index.js
+++ js/index.js
@@ -1,27 +1,41 @@
 function updateSubtotal(product) {
   const price = product.querySelector(".price span").innerHTML;
   const quantity = product.querySelector("input").value;
-  subtotal = product.querySelector(".subtotal");
-  subtotal.innerHTML = "$" + quantity * price;
+  subtotal = product.querySelector(".subtotal span");
+  subtotal.innerHTML = (quantity * price).toFixed(2);
 }
 
+
 function calculateAll() {
-  //const singleProduct = document.querySelector(".product");
-  //updateSubtotal(singleProduct);
   const allProducts = document.querySelectorAll(".product");
   allProducts.forEach((el)=>{
-    console.log(el);
     updateSubtotal(el);
   });
+} 
+
+function sumTotal (){
+  //const test = document.querySelector(".subtotal span").innerHTML;
+  //console.log("test: "+test);
+  const allSubtotals = document.querySelectorAll(".subtotal span");
+  //console.log (allSubtotals[0].innerHTML);
+  let totalPrice = 0;
+  for(let i = 0; i<allSubtotals.length; i++){
+    let subtotal = allSubtotals[i].innerHTML
+    subtotal = Number(subtotal);
+    totalPrice += subtotal;
+  }
+  const totalValue = document.querySelector("#total-value span");
+  totalValue.innerHTML = totalPrice.toFixed(2);
 
   
+}
 
  
   
 
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
@@ -39,11 +53,10 @@ function createProduct() {
 
 window.addEventListener("load", () => {
   const calculatePricesBtn = document.getElementById("calculate");
-  calculatePricesBtn.addEventListener("click", calculateAll);
+  calculatePricesBtn.addEventListener("click", sumTotal);
   const allInputs = document.querySelectorAll("input");
   allInputs.forEach((el)=>{
     el.addEventListener("change", calculateAll);
-    console.log(el);
   });
 });
 
