7f288711e7461f890d2f02be1aec9a96b628625c
Adria <ironhackadria@DESKTOP-Q36778H.localdomain>
2020-10-27 21:52:34 +0100

    Iteration 1-2 missing 3 (total undefined, I'm stuck) and missing bonuses

--- js/index.js
+++ js/index.js
@@ -1,24 +1,42 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotal = product.querySelector('.subtotal span');
+  
+  subtotal.innerHTML = price.innerHTML * quantity.value; 
+  
+  // console.log('Calculating subtotal, yey!');
 }
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  /*const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);*/
+  
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  
+  let totalSum = 0
+  const allProducts = document.getElementsByClassName('product');
+  
+  for (let i= 0; i < allProducts.length; i++){
+     totalSum = updateSubtotal(allProducts[i]);
+  }
+ 
   // ITERATION 3
   //... your code goes here
+ 
+  const totalValue = document.querySelector('#total-value span');
+    
+  totalValue.innerHTML = totalSum;
 }
+
 
 // ITERATION 4
 
