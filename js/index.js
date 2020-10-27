cbd1bfc098c0ad6ffbd3e7bf814cd933d54f3d40
Facundo Gomez <gomezvera.f@gmail.com>
2020-10-27 12:15:47 +0100

    iteration 1 & 2 completed

--- js/index.js
+++ js/index.js
@@ -2,15 +2,38 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  const price = product.querySelector('.price span').innerText
+  let quantity = product.querySelector('.quantity input').value
+  // console.log(price)
+  // console.log(quantity)
+  let subtotal = product.querySelector('.subtotal span')
+  subtotal.innerText = price * quantity
+  // console.log(subtotal)
   //... your code goes here
+  let subVal = subtotal.innerText
+  // console.log(subVal)
+  return subVal
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // let parent = document.querySelector('.product').parentNode
+  // const singleProduct = document.querySelector('.product');
+  document.querySelectorAll('.product').forEach(product => {
+   updateSubtotal(product)
+  } )
+}
+  //   if (state.glutenFreeCrust) {
+  //     oneMush.classList.add('crust-gluten-free');
+  //   } else {
+  //     oneMush.classList.remove('crust-gluten-free');
+  //   }
+  // });
+
+// for 
+
+  // updateSubtotal(singleProduct)
   // end of test
 
   // ITERATION 2
@@ -18,7 +41,7 @@ function calculateAll() {
 
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
