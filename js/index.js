78fb3fed615932122b0f01de4c7f255b58a973cf
Anna Mosley <annahmosley@gmail.com>
2020-11-02 18:15:18 -0500

    started I5

--- js/index.js
+++ js/index.js
@@ -2,36 +2,56 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+const price= product.querySelector(`.price span`).innerHTML;
+const quantity= product.querySelector(`.quantity input`).value;
+const subtotalPrice= price * quantity;
+const subtotal= product.querySelector(`.subtotal span`);
+subtotal.innerHTML=subtotalPrice;
+console.log({price: typeof price, qty: typeof quantity});
+return subtotalPrice;
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products= document.querySelectorAll (`.product`);
 
   // ITERATION 3
-  //... your code goes here
+ let sum= 0;
+ products.forEach(singleProduct => {
+  sum += updateSubtotal(singleProduct); 
+ })
+ 
+const totalPrice = document.querySelector(`#total-value`);
+console.log({totalPrice, sum});
+totalPrice.children[0].innerText = sum;
 }
-
 // ITERATION 4
+window.addEventListener('load', ()=> {
+  addReomveToButtons();
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+});
+function addReomveToButtons () {
+  const removeBtn = document.querySelectorAll( '.btn-remove');
+  removeBtn.forEach((el)=> el.addEventListener('click', () => removeProduct(el)));
 }
+function removeProduct(event) {
+console.log(event.parentNode)
+event.parentNode.parentNode.remove();
+}
+  //... your code goes here
+//}
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
 }
 
 window.addEventListener('load', () => {
