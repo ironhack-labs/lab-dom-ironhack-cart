5f572e01a79a5dac25e2b7b1d922fbd927a566e2
mondono85 <mondono85@gmail.com>
2020-11-03 17:07:52 -0500

    done

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector(`.quantity input`).value;
+  const subtotalPrice = price * quantity;
+  const subtotal = product.querySelector(`.subtotal span`);
+  subtotal.innerHTML = subtotalPrice;
+  return subtotalPrice;
   //... your code goes here
 }
 
@@ -15,18 +20,26 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
-
+  const allProducts = document.querySelectorAll('.product');
+  const sum = [...allProducts].reduce((sum, row) => sum + updateSubtotal(row), 0)
+  console.log(sum)
   // ITERATION 3
   //... your code goes here
+  document.querySelector('#total-value > span').innerText= sum
 }
 
 // ITERATION 4
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+//function removeProduct(event) {
+ // const target = event.currentTarget;
+  //console.log('The target in remove is:', target);
   //... your code goes here
-}
+  const removeProduceButton=document.getElementsByClassName('btn btn-remove')for(let i=0; i< removeProduceButton.length;i++){
+    const button = removeProduceButton[i];button.addEventListener('click',function(event){
+      conts buttonClicked = event 
+    })
+  }
+//}
 
 // ITERATION 5
 
