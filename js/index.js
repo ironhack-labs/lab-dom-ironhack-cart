5f80ec6bf0496fe841f69a37e68693f7fb2b0b05
digomattar21 <digomattar@gmail.com>
2020-11-11 10:38:09 -0300

    Done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,93 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  let subtotal= product.querySelector('.subtotal span');
+  let soma = price*quantity;
+  subtotal.innerHTML=soma;
+  return soma;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let products = document.getElementsByClassName('product');
+  let products_array = [...products]
+  let total_array = [];
 
-  // ITERATION 2
-  //... your code goes here
+  for (p of products_array){
+    let a = updateSubtotal(p);
+    total_array.push(a);
+  }
 
-  // ITERATION 3
-  //... your code goes here
+  let total = 0;
+
+  for (let i=0;i<total_array.length;i++){
+    total+=total_array[i];
+    
+  }
+
+  document.getElementById('total-value').innerHTML = 'Total: $'+total;
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  let parent = target.parentNode;
+  let product = parent.parentNode;
+  let tbody = product.parentNode;
+  tbody.removeChild(product)
+  calculateAll()
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  var inputName= document.getElementById('inputName').value;
+  var inputQuantity= document.getElementById('inputQuantity').value;
+  var inputPrice = document.getElementById('inputPrice').value;
+  
+
+  
+  let item = document.querySelector('.product');
+  
+  let rowClone = item.cloneNode(true);
+
+  if (inputQuantity !=0 && inputName && inputPrice!=0){
+    document.getElementsByTagName('tbody')[0].appendChild(rowClone)
+  } else{
+    alert('Please Input Name, Quantity and Suggested Price')
+  }
+
+  rowClone.querySelector('.name span').innerText = inputName;
+  rowClone.querySelector('.quantity input').value = inputQuantity;
+  rowClone.querySelector('.price span').innerText = inputPrice;
+
+  calculateAll();
+
+  window.addEventListener('click',()=>{
+    const removeButton = document.getElementsByClassName('btn btn-remove');
+
+    for (btn of removeButton){
+      btn.addEventListener('click', removeProduct)
+    }
+  })
 }
 
-window.addEventListener('load', () => {
+function update() { window.addEventListener('load', () => {
+
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+  const remove_buttons = document.getElementsByClassName('btn btn-remove');
+
+  for (btn of remove_buttons){
+    btn.addEventListener('click', removeProduct)
+  }
+
+  const create = document.getElementById('create');
+  create.addEventListener('click', createProduct)
+
+})};
+update();
