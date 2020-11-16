c5098ebe75a8fbbe948da5c46859abdb1373ced8
Thiarhes <injesusservice@hotmail.com>
2020-11-15 22:19:55 -0300

    WDPT SAO 10.2020 Thiarhes

--- js/index.js
+++ js/index.js
@@ -1,37 +1,40 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  let price = product.querySelector(".price span");
-  let quantity = product.querySelector(".quantity input");
-  let subTotalCalc = price.innerHTML * quantity.value;
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  const subTotalCalc = price.innerHTML * quantity.value;
 
-  let subTotal = product.querySelector(".subtotal span");
+  const subTotal = product.querySelector(".subtotal span");
   subTotal.innerHTML = subTotalCalc;
 
   return subTotal;
 }
 
 function calculateAll() {
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  
-   let products = document.getElementsByClassName('product');
-  // const products = document.querySelectorAll(".product"); 
-  let totalSum = document.querySelector('#total-value span');
-  // for (let i = 0; i< products.length; i += 1) {
-  // updateSubtotal(products[i]);
-  // }
-  let total = 0;
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
 
-  for (product of products) {
+  const products = document.getElementsByClassName('product'); // or document.querySelectorAll('.product');
+  const finalTotal = document.querySelector('#total-value span');
+   let total = 0;
+  for (const product of products) {
     total += updateSubtotal(product);
   }
   
-  totalSum.innerHTML = total;
+  // for (let i = 0; i < products.length; i += 1) {
+  // total += updateSubtotal(products[i]);
+  // }
+  
+  finalTotal.innerHTML = total;
 }
 
+
+
+
+
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget; 
   const row = target.parentNode.parentNode;
   const parent = row.parentNode;
 
@@ -39,15 +42,14 @@ function removeProduct(event) {
 }
 
 function createProduct() {
-  const newProduct = document.querySelector(".create-product");
-  let inputProductName = newProduct.querySelector("input");
-  let inputProductNameValue = inputProductName.value;
+  const newProduct = document.querySelector('.create-product');
+  let inputProductName = newProduct.querySelector('input');
+  let inputProductNameValue = inputProductName.value; 
   let inputProductPrice = newProduct.querySelector("input[type='number']");
   let inputProductPriceValue = inputProductPrice.value;
 
-  const tableRow = document.createElement("tr");
-  tableRow.className = "product";
-  
+  const tableRow = document.createElement('tr');
+  tableRow.className = 'product';
   tableRow.innerHTML = `<td class="name">
   <span>${inputProductName}</span>
 </td>
@@ -60,13 +62,13 @@ function createProduct() {
   <button class="btn btn-remove">Remove</button>
 </td>`;
 
-  const tbody = document.querySelector("#cart tbody");
+  const tbody = document.querySelector('#cart tbody');
   tbody.appendChild(tableRow);
 
-  const removeButton = tableRow.querySelector(".btn-remove");
-  removeButton.addEventListener("click", removeProduct);
+  const removeButton = tableRow.querySelector('.btn-remove');
+  removeButton.addEventListener('click', removeProduct);
 
-  inputProductName.value = "";
+  inputProductName.value = '';
   inputProductPrice.value = 0;
 }
 
@@ -74,12 +76,12 @@ window.addEventListener("load", () => {
   const calculatePricesBtn = document.getElementById("calculate");
   calculatePricesBtn.addEventListener("click", calculateAll);
 
-  const createButton = document.getElementById("create");
-  createButton.addEventListener("click", createProduct);
+  const createButton = document.getElementById('create');
+  createButton.addEventListener('click', createProduct);
 
-  const removeButtons = document.querySelectorAll(".btn-remove");
+  const removeButtons = document.querySelectorAll('.btn-remove');
 
   for (removeButton of removeButtons) {
-    removeButton.addEventListener("click", removeProduct);
+    removeButton.addEventListener('click', removeProduct);
   }
 });
