77dd2e9e22f93810f516ebe540d32b6c9eafe7cb
Xuan Tran <tran.ttxuan91@gmail.com>
2020-11-19 16:48:05 +0100

    v1 - ?after adding a new product, NO remove/calculate price

--- js/index.js
+++ js/index.js
@@ -1,42 +1,83 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotal = product.querySelector('.subtotal span');
+
+  subtotal.innerHTML = (Number(price.textContent) * quantity.value).toString();
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll(".product");
+  products.forEach(product => {
+    updateSubtotal(product)
+  })
 
   // ITERATION 3
-  //... your code goes here
+  const total = document.querySelector("#total-value span");
+  const allSubtotal = document.querySelectorAll(".subtotal span");
+  let sum = 0;
+  allSubtotal.forEach(each => sum += Number(each.textContent));
+  total.textContent = sum.toString();
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  (target.parentNode.parentNode).remove();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
-}
+  const table = document.querySelector("#cart tbody");
+  //get information from the last column
+  const nameNewProduct = document.querySelector('.create-product input[type="text"]');
+  const unitPriceNewProduct = document.querySelector('.create-product input[type="number"]');
+  //create element
+  const row = document.createElement("tr");
+  row.innerHTML += `<td class="name"><span>${nameNewProduct.value}</span> </td>`;
+  row.innerHTML += ` <td class="price">$<span>${unitPriceNewProduct.value}</span></td>`;
+  row.innerHTML += `<td class="quantity"><input type="number" value="0" min="0" placeholder="Quantity" /></td>`;
+  row.innerHTML += ` <td class="subtotal">$<span>0</span></td>`;
+  row.innerHTML += `<td class="action"><button class="btn btn-remove">Remove</button></td>`
 
+  //show new a product
+  table.appendChild(row);
+
+  //delete input fields in last form
+  nameNewProduct.value ='';
+  unitPriceNewProduct.value ='';
+
+}
+calculateAll();
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  //remove a product
+  const allRemoveBtn = document.querySelectorAll(".btn-remove");
+  allRemoveBtn.forEach(btn => {
+    btn.addEventListener('click', removeProduct);
+  })
+
+   //add a new product
+  const addBtn = document.getElementById("create");
+  addBtn.addEventListener('click', createProduct);
+
 });
+
+//after add a new product, I can't remove it or calculate price for it. I wanna know how to update #table, list of .product
