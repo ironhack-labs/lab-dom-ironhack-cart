c0abbab1726adb7c9cdd0b9de41bc183bd2c32d4
Elisa Chen <skechen@gmail.com>
2020-11-19 17:44:55 +0100

    lab and bonus iteraion 4 completed

--- js/index.js
+++ js/index.js
@@ -2,31 +2,57 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector(".price span").textContent;
+  const quantity = product.querySelector(".quantity input").value;
+  let subtotal = Number(price) * Number(quantity);
+  product.querySelector(".subtotal span").textContent = subtotal;
+  return subtotal
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // ITERATION 1
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 2/3 using for loop
+  // let allProducts = document.querySelectorAll(".product"); 
+  // let totalPrice = 0; 
+  // for (let i=0; i < allProducts.length; i++) {
+  //   updateSubtotal(allProducts[i]);
+  // // ITERATION 3
+  //   totalPrice += Number(allProducts[i].querySelector(".subtotal span").textContent);
+  // }
+  // document.querySelector("#total-value span").textContent = totalPrice;
 
-  // ITERATION 3
-  //... your code goes here
+// ITERATION 2/3 using forEach
+  let allProducts = [... document.querySelectorAll(".product")];
+  let totalPrice = 0;
+  allProducts.forEach(function(product) {
+    updateSubtotal(product);
+    totalPrice += Number(product.querySelector(".subtotal span").textContent);
+  })
+  document.querySelector("#total-value span").textContent = totalPrice
 }
+//TAs: can you please let me know the shorter way to do this?
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  let parentNode = target.parentNode.parentNode;
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  };
+  parentNode.remove();
+  calculateAll();
 }
+console.log(document.querySelector(".btn-remove").parentNode.parentNode)
+
+
 
 // ITERATION 5
 
@@ -38,5 +64,7 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeBtn = [... document.getElementsByClassName("btn btn-remove")];
+  removeBtn.forEach( button => button.addEventListener('click', removeProduct));
   //... your code goes here
 });
