1cc6d4d082b496fd1b0751efff0d8053ac1c1682
Marine SANJUAN <marine@sanjuan.eu>
2020-11-19 19:30:18 +0100

    lab done safe for bonuses

--- js/index.js
+++ js/index.js
@@ -2,23 +2,63 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  // Step 1: Fetching the elements
 
-  //... your code goes here
+  let price = product.querySelector(".price span"); //works
+  let quantity = product.querySelector(".quantity input"); //works
+  
+  //console.log("PRICE>>", price);
+
+  //Step 2: Extracting the values
+  
+  let productPrice = price.innerHTML;
+  //console.log("toto", productPrice); 
+
+  let dynamicQty = quantity.value; 
+  //console.log("dynamicQty >>", dynamicQty); 
+
+  // Step 3: Calculate subtotal price
+
+  let subTotal = productPrice * dynamicQty;
+  //console.log(subTotal); // works
+
+  // Step 4: Fetch DOM elt of Subtotal
+
+  let domSubTotal = product.querySelector(".subtotal span");
+  //console.log( "DomSubTotal >>", domSubTotal); 
+  
+  // Step 5: set subtotal price to corresponding DOM elt and return value
+
+  return  domSubTotal.innerHTML = subTotal ;
+  //console.log("subTotalPrice >>> ",subTotalPrice); 
+  
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
-}
+// ITERATION 2
+  
+  const total = document.querySelector("#total-value span");
+  const allProducts = document.getElementsByClassName("product");
+  var allPrdtsArray  = [...allProducts];
+  let sum = 0;
+  //console.log("allProducts >>>", allProducts); //tested : works
+  // console.log(typeof allProducts); // retourne objet et pas array
+    // convertit en array
+  //console.log("array", allPrdtsArray);
+
+    allPrdtsArray.forEach(product => {
+      let pleaseWork = Number(updateSubtotal(product));
+      sum += pleaseWork;
+      
+    })
+     total.innerHTML = sum;
+     
+    };
+
+
 
 // ITERATION 4
 
@@ -38,5 +78,8 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+  const removeProductBtn = document.querySelectorAll(".btn-remove");
+  btn.addEventListener('click', removeProduct);
+
+  })
+  
