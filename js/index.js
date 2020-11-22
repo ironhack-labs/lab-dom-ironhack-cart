0daf11c865fd21e8240b92e96ece7acb5a254ba2
Gwerborc'h Mahéo <gwerborch.maheo.com>
2020-11-22 12:25:30 +0100

    need to comeback on it

--- js/index.js
+++ js/index.js
@@ -4,27 +4,46 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  //console.log(price.innerHTML);
+  let subtotal = price.innerHTML * quantity.value;
+  //console.log(subtotal);
+  let subtotalPage = product.querySelector(".subtotal span");
+  subtotalPage.innerHTML = subtotal;
+  return subtotal;
+
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+ // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  const allProducts = [...document.getElementsByClassName("createProduct")];
+  allProducts.forEach((prod) => {
+  });
   // ITERATION 3
   //... your code goes here
+  const sumSubTotal = [...document.querySelectorAll(".product .subtotal span")]
+    .map((price) => Number(price.textContent))
+    .reduce((acc, curValue) => (acc += curValue), 0);
+  console.log("sumbsubtotal", sumSubTotal);
+  const totalValueCell = document.querySelector("#total-value span");
+  totalValueCell.innerHTML = sumSubTotal;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  return target
+
   //... your code goes here
 }
 
