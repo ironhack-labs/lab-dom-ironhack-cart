2e170d76df6d5c4b69928ec69bb8043e41f63f9f
monteromario <59968975+monteromario@users.noreply.github.com>
2020-12-01 21:43:29 +0100

    Iteration 5

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function updateSubtotal(product) {
   let price = product.querySelector('.price span').innerHTML;
   let quantity = product.querySelector('.quantity input').value;
   let subtotal = price * quantity
-  product.querySelector('.subtotal span').innerHTML = subtotal
+  product.querySelector('.subtotal span').innerHTML = subtotal.toFixed(2)
 }
 
 function calculateAll() {
@@ -25,9 +25,9 @@ function calculateAll() {
   //... your code goes here
   let totalPrice = 0;
   for (i=0; i<selectedProducts.length; i++) {
-    totalPrice += parseInt(selectedProducts[i].querySelector('.subtotal span').innerHTML)
+    totalPrice += parseFloat(selectedProducts[i].querySelector('.subtotal span').innerHTML)
   }
-  document.querySelector('#total-value span').innerHTML = totalPrice
+  document.querySelector('#total-value span').innerHTML = totalPrice.toFixed(2)
 }
 
 // ITERATION 4
@@ -45,6 +45,31 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  //console.log('create detected');
+  const target = event.currentTarget;
+  //console.log('The create in create is:', target);
+  let createName = target.parentNode.parentNode.getElementsByTagName('input')[0].value;
+  let createPrice = parseFloat(target.parentNode.parentNode.getElementsByTagName('input')[1].value).toFixed(2);
+  let newRow = document.createElement('tr');
+  document.getElementsByTagName('tbody')[0].appendChild(newRow);
+  newRow.className += "product";
+  newRow.innerHTML = `<td class="name">
+            <span>${createName}</span>
+          </td>
+          <td class="price">$<span>${createPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>`
+  target.parentNode.parentNode.getElementsByTagName('input')[0].value = '';
+  target.parentNode.parentNode.getElementsByTagName('input')[1].value = '0';
+  let removeButtons = document.getElementsByClassName('btn-remove');
+  for (i=0; i<removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct)
+  }
 }
 
 window.addEventListener('load', () => {
@@ -56,5 +81,7 @@ window.addEventListener('load', () => {
   for (i=0; i<removeButtons.length; i++) {
     removeButtons[i].addEventListener('click', removeProduct)
   }
-  
+
+  let createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct)
 });
