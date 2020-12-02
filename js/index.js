26f4873168b8640affc4c92b4973e0072f0df114
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2020-12-02 21:41:17 +0100

    Lab done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,76 @@
+const productsParent = document.querySelector('tbody');
+
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const qty = product.querySelector('.quantity input');
+  const subTotal = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  let calcSubTotal = price.innerHTML * qty.value;
+  subTotal.innerHTML = calcSubTotal;
+  return calcSubTotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  let products = document.querySelectorAll('.product');
+  let totalPrice = 0;
+  products.forEach(prod => totalPrice += updateSubtotal(prod));
 
   // ITERATION 3
-  //... your code goes here
+  let total = document.querySelector('#total-value span');
+  total.innerHTML = totalPrice;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.closest(".product");
+
+  try {
+    productsParent.removeChild(target);
+    calculateAll();
+  } catch (err){
+    throw new Error('Error trying to remove the target')
+  }
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  const newProdName = document.querySelector('#newProdName').value;
+  const newProdPrice = document.querySelector('#newProdPrice').value;
+
+  if(newProdName !== '' && newProdPrice !== ''){
+    let newProduct = document.createElement('tr');
+    newProduct.className = 'product';
+    newProduct.innerHTML = `
+        <tr class="product">
+        <td class="name">
+          <span>${newProdName}</span>
+        </td>
+        <td class="price">$<span>${newProdPrice}</span></td>
+        <td class="quantity">
+          <input type="number" value="0" min="0" placeholder="Quantity" />
+        </td>
+        <td class="subtotal">$<span>0</span></td>
+        <td class="action">
+          <button class="btn btn-remove">Remove</button>
+        </td>
+      </tr>
+    `;
+    productsParent.appendChild(newProduct);
+    newProduct.querySelector('.btn-remove').addEventListener('click', removeProduct);
+  }
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  let deleteBtn = document.querySelectorAll('.btn-remove');
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener('click', removeProduct)
+  })
+
+  const newProdBtn = document.querySelector('#create');
+  newProdBtn.addEventListener('click', createProduct)
 });
