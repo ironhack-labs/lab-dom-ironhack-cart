574a04b80059d353e10d741e7f900f34bf2d913b
Alicia Parra <alicia@redparra.com>
2020-12-06 20:20:48 +0100

    Clean code.Delete unnecesary comments

--- js/index.js
+++ js/index.js
@@ -4,24 +4,14 @@ function updateSubtotal(product) {
   const subtotal = product.querySelector('.subtotal span');
   const price = product.querySelector('.price span');
   const quantity = product.querySelector('.quantity input');
-  //const result = price.innerHTML * quantity.value;
-  //console.log(price.innerHTML)
-  //console.log(quantity.value)
+  
   subtotal.innerHTML = price.innerHTML * quantity.value;
   return Number(subtotal.innerHTML);
-  //console.log(subtotal.innerHTML);
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  //const singleProduct = document.querySelector('.product');
-  //updateSubtotal(singleProduct);
-  // end of test
-
+ 
   // ITERATION 2
   const products= document.getElementsByClassName("product");
   const productsArray= [...products]
@@ -38,12 +28,12 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  //access the element an event was fired on
+  
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
-  //access its parent node and call removeChild on it
-  /*const parentButton = target.parentNode;
+  
+  /* First version
+  const parentButton = target.parentNode;
   const parentAction = parentButton.parentNode;
   const parentProduct = document.querySelector("tbody");
   parentProduct.removeChild(parentAction);*/
@@ -70,6 +60,7 @@ function createProduct() {
   
   newChild.setAttribute('class','product');//I add the class product to the new child
   
+  //I add the information the new child needs
   newChild.innerHTML =  `<tr class="product"> 
   <td class="name">
     <span>${productName}</span>
@@ -82,23 +73,12 @@ function createProduct() {
   <td class="action">
     <button class="btn btn-remove">Remove</button>
   </td>
-</tr>`
-
-  
-  //newChild.setAttribute('className','product');
-  //newChild = document.getElementsByClassName('product')[0].cloneNode(true); //I clone information to add to the new child
-
+</tr>`  
 
   parentDirection.appendChild(newChild); //I link the parent with the child
 
-  const newName= document.querySelectorAll('.name span')[document.querySelectorAll('.name span').length-1]; //I change the name of the new child
-  newName.innerHTML=productName;
-
-  const newPrice= document.querySelectorAll('.price span')[document.querySelectorAll('.price span').length-1];
-  newPrice.innerHTML=productPrice;
-
   const deleteButton= document.querySelectorAll('.btn-remove')[document.querySelectorAll('.btn-remove').length-1]
-  deleteButton.addEventListener('click', removeProduct);
+  deleteButton.addEventListener('click', removeProduct); //Adds event to the new delete button
 
   clearFields();
 
@@ -108,20 +88,7 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-  // querying the document for all "Remove" buttons
-  /*
-    const buttons = document.querySelectorAll('.btn-remove');
-    console.log(`buttons ${buttons}` )
-    //loop through them, and add a click event listener to each, passing a named function removeProduct as the callback argument.
-    buttons.forEach((button) => {
   
-  
-      button.addEventListener('click', removeProduct);
-    })
-  
-   */
   const products = document.querySelectorAll('.product');
   
   products.forEach((product) => {
