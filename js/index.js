f1b35c1d1536aa7603bd1c71865b7cf5e51a5bea
Pedro <pedroscampoy@gmail.com>
2020-12-07 12:20:33 +0100

    Finished

--- js/index.js
+++ js/index.js
@@ -1,42 +1,77 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
 
-  //... your code goes here
+  const subtotalValue = Number(price) * quantity
+
+  product.querySelector('.subtotal span').innerText = subtotalValue;
+
+  return subtotalValue
+}
+
+const enableRemove = () => {
+  const allRemoveButtons = document.getElementsByClassName('btn-remove');
+  [...allRemoveButtons].map(button => {
+    button.addEventListener('click', removeProduct);
+  })
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
-
+  const allProductsArray = document.getElementsByClassName('product');
+  const subTotals = [...allProductsArray].map((e) => updateSubtotal(e));
   // ITERATION 3
-  //... your code goes here
+  const totalValue = subTotals.reduce((sum, num) => sum + num)
+  document.querySelector('#total-value span').innerText = totalValue;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  target.parentNode.parentNode.remove()
 }
 
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
+  textInputCreate = document.querySelector('.create-product input[type=text]').value;
+  unityPriceCreate = document.querySelector('.create-product input[type=number]').value;
+
+  tbodyDOM = document.querySelector('tbody');
+
+  const rowCreate = `<tr class="product">
+    <td class="name">
+      <span>${textInputCreate}</span>
+    </td>
+    <td class="price">$<span>${unityPriceCreate}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>`
+
+  if (!textInputCreate || unityPriceCreate <= 0) {
+    alert('Please specify description and price')
+  } else {
+    //tbodyDOM.innerHTML += rowCreate
+    tbodyDOM.insertAdjacentHTML('beforeend', rowCreate);
+    enableRemove()
+  }
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  enableRemove()
+
+  const createButton = document.getElementById('create');
+  createButton.addEventListener('click', createProduct);
 });
