1ac6e769aa42e02bc424112257ab699c864a45c2
unknown <ionut_vasile85@hotmail.com>
2021-01-15 18:30:53 -0500

    1st iteration

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+// function updateSubtotal(product) {
+//   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
-}
+//   //... your code goes here
+// }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
@@ -13,30 +13,92 @@ function calculateAll() {
   updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+//   // ITERATION 2
+//   //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+//   // ITERATION 3
+//   //... your code goes here
+// }
+
+// // ITERATION 4
+
+// function removeProduct(event) {
+//   const target = event.currentTarget;
+//   console.log('The target in remove is:', target);
+//   //... your code goes here
+// }
+
+// // ITERATION 5
+
+// function createProduct() {
+//   //... your code goes here
+// }
+
+// window.addEventListener('load', () => {
+//   const calculatePricesBtn = document.getElementById('calculate');
+//   calculatePricesBtn.addEventListener('click', calculateAll);
+
+//   //... your code goes here
+// });
+
+
+
+// create a function that calculates the item * quantity 
+//place it in subtotal field
+
+
+document.querySelector('#calculate').onclick = function(){}
+
+let allProducts = document.querySelectorAll('.product')
+let total = 0
+
+for (let i = 0; i< allProducts.length; i++){
+  let product = allProducts[i]
+    console.log(eachProduct)
+    let price = eachProduct.querySelector('.price span').innerHTML
+    let value =eachProduct.querySelector('.quantity input').value
+
+
+    let subTotal = price * value
+    console.log(price)
+
+
+    eachProduct.querySelector('subtotal span').innerHTML = subTotal
+
+    total += subTotal
+    debugger
+    
+}
+eachProduct.querySelector('#total-value').innerHTML
+
 }
 
-// ITERATION 4
+document.querySelectorAll
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
 
-// ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
