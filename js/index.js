271a4be3297c293551e4fa692f325c287f625c55
Barbara Diaz <barbarad91@gmail.com>
2021-01-16 12:49:20 +0100

    Final iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
     console.log('Calculating subtotal, yey!');
 
@@ -24,23 +23,40 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
-// function removeProduct(event) {
-//     const target = event.currentTarget;
-//     console.log('The target in remove is:', target);
-//     //... your code goes here
-//     target.style.color = 'red'
-// }
-function removeProduct(target) {
-    const parent = target.closest('.product')
-    parent.remove()
-        //target.remove()
+function removeProduct(event) {
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
+    target.closest('.product').remove()
+    calculateAll()
 }
 
 // ITERATION 5
-
 function createProduct() {
-    //... your code goes here
+    const productHTML = `<tr class="product">
+                            <td class="name">
+                                <span>Ironhack Rubber Duck</span>
+                            </td>
+                            <td class="price">$<span>25.00</span></td>
+                            <td class="quantity">
+                                <input type="number" value="0" min="0" placeholder="Quantity" />
+                            </td>
+                            <td class="subtotal">$<span>0</span></td>
+                            <td class="action">
+                                <button class="btn btn-remove">Remove</button>
+                            </td>
+                        </tr>`
+    const newProduct = document.createElement('tr')
+    newProduct.setAttribute('class', 'product')
+    newProduct.innerHTML = productHTML
+    const nameInput = document.querySelector('.create-product input[type=text]')
+    newProduct.querySelector('.name span').textContent = nameInput.value
+    const priceInput = document.querySelector('.create-product input[type=number]')
+    newProduct.querySelector('.price span').textContent = priceInput.value
+    document.querySelector('#cart tbody').appendChild(newProduct)
+    nameInput.value = ''
+    priceInput.value = 0
+    calculateAll()
+    newProduct.querySelector('.btn-remove').addEventListener('click', removeProduct)
 }
 
 
@@ -48,9 +64,11 @@ window.addEventListener('load', () => {
     const calculatePricesBtn = document.getElementById('calculate');
     calculatePricesBtn.addEventListener('click', calculateAll);
 
-    //... your code goes here
-    const buttons = document.querySelectorAll('.action .btn')
-    buttons.forEach(element => {
-        element.addEventListener('click', function() { removeProduct(element) }, false)
-    })
+    const buttons = document.querySelectorAll('.btn-remove')
+    buttons.forEach(element =>
+        element.addEventListener('click', removeProduct)
+    )
+
+    const createButton = document.querySelector('#create')
+    createButton.addEventListener('click', createProduct)
 });
