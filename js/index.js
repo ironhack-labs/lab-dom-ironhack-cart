54fa28d22d0e63a446806c83c3b1a4c2930ec310
Jaime <jaimeferncast@gmail.com>
2021-01-16 16:56:51 +0100

    lab terminado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// ITERATION 1
+let ghostProduct = undefined
 
 function updateSubtotal(product) {
 
@@ -14,29 +14,66 @@ function calculateAll() {
 
   const products = Array.from(document.getElementsByClassName(`product`))
 
-  products.forEach(element => updateSubtotal(element))
+  let total = 0
 
-  // ITERATION 3
-  //... your code goes here
+  products.forEach(element => {
+    updateSubtotal(element)
+    total += Number(element.querySelector(`.subtotal span`).innerText)
+  })
+
+  document.querySelector(`#total-value span`).innerText = total
+
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const target = event.currentTarget.parentElement.parentElement
+
+  target.parentNode.removeChild(target)
+
+  document.querySelector(`#total-value span`).innerText -= target.querySelector(`.subtotal span`).innerText
+
 }
 
-// ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+
+  let name = document.querySelector(`.create-product input[type="text"]`).value
+  let unitPrice = document.querySelector(`.create-product input[type="number"]`).value
+
+  if (name && unitPrice) {
+    const newProduct = ghostProduct.cloneNode(true)
+    newProduct.querySelector(`.name span`).innerText = name
+    newProduct.querySelector(`.price span`).innerText = unitPrice
+    document.querySelector(`#cart tbody`).appendChild(newProduct)
+    document.querySelector(`.create-product input[type="text"]`).value = ""
+    document.querySelector(`.create-product input[type="number"]`).value = ""
+  } else {
+    alert(`Please provide a Product Name and a Unit Price, thank you.`)
+  }
+
+  document.querySelectorAll(`.btn-remove`).forEach(button => {
+    button.onclick = event => {
+      removeProduct(event)
+    }
+  })
+
 }
 
 window.addEventListener('load', () => { // cuando la pagina se carga del todo, ejecuta lo de dentro
+
   const calculatePricesBtn = document.getElementById('calculate') // esto devuelve el nodo con el id especificado
   calculatePricesBtn.addEventListener('click', calculateAll) // addEventListener (en este evento, ejecuta esta función)
 
-  //... your code goes here
+  ghostProduct = document.querySelector(`.product`).cloneNode(true)
+
+  const removeProductBtn = document.querySelectorAll(`.btn-remove`)
+
+  removeProductBtn.forEach(button => {
+    button.onclick = event => {
+      removeProduct(event)
+    }
+  })
+
+  document.getElementById(`create`).addEventListener(`click`, createProduct)
+
 })
