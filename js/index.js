843d53fddba821e9f9a44d1a29f27b91ebb9ffd2
Veronica Alcala <veronica.alcala@gmail.com>
2021-01-17 18:14:51 +0100

    ready

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ function updateSubtotal(product) {
   // console.log(subtotal.textContent)
   // console.log(subtotalValue)
   return subtotalValue
-  
+
 }
 
 function calculateAll() {
@@ -58,9 +58,73 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  // console.log("espero que funcione")
+  const productName = document.querySelector(".create-product").children[0].children[0].value //string valor de input
+  const textName = document.createTextNode(productName)
+  const unitPrice = document.querySelector(".create-product").children[1].children[0].value //numero valor de input
+  const numberPrice = document.createTextNode(unitPrice)
+  // console.log(productName)
+
+  const newRow = document.createElement("tr") // node
+  newRow.setAttribute("class", "product")
+  const newSpan = document.createElement("span")
+  const unitPriceSpan = document.createElement("span")
+  const subtotalSpan = document.createElement("span")
+  const removeButton = document.createElement("button")
+
+  const newInput = document.createElement("input")
+
+  //const newTableData = document.createElement("td")
+  for (i = 0; i <= 4; i++) {
+    const newTableData = document.createElement("td")
+    switch (i) {
+      case (0):
+        newTableData.setAttribute("class", "name")
+        newTableData.appendChild(newSpan)
+        newTableData.appendChild(textName)
+        break;
+      case (1):
+        newTableData.setAttribute("class", "price")
+        newTableData.appendChild(document.createTextNode("$"))
+        newTableData.appendChild(unitPriceSpan)
+        unitPriceSpan.appendChild(numberPrice)
+        break;
+      case (2):
+        newTableData.setAttribute("class", "quantity")
+        newTableData.appendChild(newInput)
+        newInput.setAttribute("type", "number")
+        newInput.setAttribute("value", "0")
+        newInput.setAttribute("min", "0")
+        newInput.setAttribute("placeholder", "Quantity")
+
+        break;
+      case (3):
+        newTableData.setAttribute("class", "subtotal")
+        newTableData.appendChild(document.createTextNode("$"))
+        subtotalSpan.appendChild(document.createTextNode("0"))
+        newTableData.appendChild(subtotalSpan)
+        break;
+      case (4):
+        newTableData.setAttribute("class", "action")
+        removeButton.setAttribute("class", "btn btn-remove")
+        newTableData.appendChild(removeButton)
+        removeButton.appendChild(document.createTextNode("Remove"))
+        removeButton.addEventListener("click", removeProduct)
+        break;
+    }
+
+    newRow.appendChild(newTableData)
+    document.querySelector("tbody").appendChild(newRow)
+    //console.log(document.querySelector("tbody"), "este es tbody")
+  }
+
+  document.querySelector(".create-product").children[0].children[0].value = ''
+  document.querySelector(".create-product").children[1].children[0].value = 0
 }
 
+
 window.addEventListener('load', () => {
+
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
@@ -71,8 +135,9 @@ window.addEventListener('load', () => {
     eachButton.onclick = (e) => {
       removeProduct(e)
     }
-    }
-  )
+  })
+  const createButton = document.querySelector("#create");
+  createButton.addEventListener('click', createProduct);
 
   //... your code goes here
 } );
