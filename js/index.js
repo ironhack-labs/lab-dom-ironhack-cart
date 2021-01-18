5b4c27f73ee4a67dfbc089007eb25c3b6f293576
unknown <ionut_vasile85@hotmail.com>
2021-01-17 23:31:47 -0500

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
 // ITERATION 1
 
-// function updateSubtotal(product) {
-//   console.log('Calculating subtotal, yey!');
 
-//   //... your code goes here
-// }
+//Function that upgrades the cost for each item as well as updating the total cost of items
+document.querySelector('#calculate').onclick = function updateSubtotal(){
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let allProducts = document.querySelectorAll('.product')
+  let totalCostOfProducts = 0
+
+
+  // loot throught each item of the table
+  allProducts.forEach(individualProd =>{
+    //gets the values from each row
+    let price = individualProd.querySelector('.price span').innerHTML
+    let quantity = individualProd.querySelector('.quantity input').value
+
+    //calculate the value of the subtotal for each item
+    let subtotal = price * quantity
+    totalCostOfProducts += subtotal
+
+    //updates the subtotal cell
+    individualProd.querySelector('.subtotal span').innerHTML = subtotal
+  })
+
+    // updates the grand total
+
+    document.querySelector('#total-value span').innerHTML = totalCostOfProducts
+}
+
+// function calculateAll() {
+//   // code in the following two lines is added just for testing purposes.
+//   // it runs when only iteration 1 is completed. at later point, it can be removed.
+//   const singleProduct = document.querySelector('.product');
+//   updateSubtotal(singleProduct);
+//   // end of test
 
 //   // ITERATION 2
 //   //... your code goes here
@@ -46,34 +67,34 @@ function calculateAll() {
 // create a function that calculates the item * quantity 
 //place it in subtotal field
 
+//-----------------------
+// document.querySelector('#calculate').onclick = function(){}
 
-document.querySelector('#calculate').onclick = function(){}
+// let allProducts = document.querySelectorAll('.product')
+// let total = 0
 
-let allProducts = document.querySelectorAll('.product')
-let total = 0
-
-for (let i = 0; i< allProducts.length; i++){
-  let product = allProducts[i]
-    console.log(eachProduct)
-    let price = eachProduct.querySelector('.price span').innerHTML
-    let value =eachProduct.querySelector('.quantity input').value
-
-
-    let subTotal = price * value
-    console.log(price)
+// for (let i = 0; i< allProducts.length; i++){
+//   let product = allProducts[i]
+//     console.log(eachProduct)
+//     let price = eachProduct.querySelector('.price span').innerHTML
+//     let value =eachProduct.querySelector('.quantity input').value
 
 
-    eachProduct.querySelector('subtotal span').innerHTML = subTotal
+//     let subTotal = price * value
+//     console.log(price)
 
-    total += subTotal
-    debugger
+
+//     eachProduct.querySelector('subtotal span').innerHTML = subTotal
+
+//     total += subTotal
+//     debugger
     
-}
-eachProduct.querySelector('#total-value').innerHTML
+// }
+// eachProduct.querySelector('#total-value').innerHTML
 
-}
+// }
 
-document.querySelectorAll
+// document.querySelectorAll
 
 
 
