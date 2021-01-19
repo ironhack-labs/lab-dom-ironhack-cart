c988c4731cce0727f89f74ab00bed94713e11547
oezge sebisteri <sebisteri@outlook.com>
2021-01-19 23:07:38 +0100

    WDFT-JAN-2021 Oezge Sebisteri

--- js/index.js
+++ js/index.js
@@ -1,9 +1,37 @@
+//Ö Testet
+//console.log('Calculating subtotal, yey!');
+var product = document.querySelector(".product")
+var products = document.getElementsByClassName("product")
+for(i=0; i<products.length; i++) {
+  console.log(products[i])
+}
+  
+  //... your code goes here
+  //var products = document.getElementsByClassName("product")
+  //console.log(products)
+  //Hier unten passt alles
+  //var product = document.querySelector('.product')
+  //console.log(products)
+  /*--
+  var price = product.querySelector('.price span').innerHTML
+  var quantity = product.querySelector('.quantity input').getAttribute("value")
+  var subtotal= price * quantity
+  product.querySelector('.subtotal span').innerHTML = subtotal
+--*/
+//Ö Testet nicht mehr
+
+
+
 // ITERATION 1
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  var price = product.querySelector('.price span').innerHTML
+  var quantity = product.querySelector('.quantity input').getAttribute("value")
+  var subtotal= price * quantity
+  product.querySelector('.subtotal span').innerHTML = subtotal
+  return subtotal
 }
 
 function calculateAll() {
@@ -15,7 +43,11 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
-
+  var products = document.getElementsByClassName("product")
+  for(i=0; i<products.length; i++) {
+    updateSubtotal(products[i])
+  }
+  
   // ITERATION 3
   //... your code goes here
 }
@@ -40,3 +72,8 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+//Test for the Console
+function print(){
+console.log(document.getElementById('calculate'));
+}
