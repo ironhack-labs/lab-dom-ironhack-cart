2153d1ea07017e6e8a93a7d2d0ecbdbff2457b01
Andrew-Moussa <seoundonline@gmail.com>
2021-01-20 19:11:29 +0100

    done/Andrew

--- js/index.js
+++ js/index.js
@@ -40,18 +40,36 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  target.parentNode.parentNode.parentNode.removeChild(target.parentNode.parentNode);
+  calculateAll();
+
   //... your code goes here
 }
 
 // ITERATION 5
 
+
 function createProduct() {
   //... your code goes here
+  const product = document.querySelector('.product');
+  const newProduct = product.cloneNode(true);
+  newProduct.querySelector('.name span').innerText = document.querySelector('.create-product td input').value
+  newProduct.querySelector('.subtotal span').innerText = 0
+  newProduct.querySelector('.price span').innerText = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]').value
+  newProduct.querySelector('.action .btn').addEventListener('click', removeProduct)
+  const newTbody = document.querySelector('tbody')
+
+  newTbody.appendChild(newProduct);
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeProductBtns = document.querySelectorAll(".btn-remove");
+  removeProductBtns.forEach(function (button) {
+    button.onclick = removeProduct;
+  })
   //... your code goes here
 });
