0d3148e5a06d7fb71974e10606b77d380d7fcc57
Mafe Bracho <mafe.bracho@gmail.com>
2021-01-20 11:04:36 +0100

    add more functionality by working on iterations 4 and 5, comment back in the uncommented lines in html document, but code is still buggy, for example i can add a new item even if the field is empty, and the subtotals are still only updated if i change the amount of items of the first item row

--- js/index.js
+++ js/index.js
@@ -4,15 +4,17 @@ function updateSubtotal(product) {
 
   const price = product.querySelector('.price span').innerHTML;
   const quantity = document.querySelector('.quantity input').value;
-  let subtotal = price * quantity;
+  let subtotal = Number(price * quantity);
 
-  product.querySelector('.subtotal span').innerHTML = subtotal;
+  product.querySelector('.subtotal span').innerText = subtotal;
 
   return subtotal;
 }
 
 function calculateAll() {
   
+  // ITERATION 2
+
   let total = 0;
   const allProducts = document.getElementsByClassName('product');
 
@@ -23,32 +25,50 @@ function calculateAll() {
   document.querySelector('#total-value span').innerHTML = total;
 
   return total;
-}
 
-  // ITERATION 2
-  //... your code goes here
 
   // ITERATION 3
   //... your code goes here
 
+}
+
 
 // ITERATION 4
 
-/* function removeProduct(event) {
+function removeProduct(event) {
+
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-} */
+  const child = target.closest('tr');
+  const parent = target.closest('tbody');
+
+  parent.removeChild(child);
+}
 
 // ITERATION 5
 
-/* function createProduct() {
-  //... your code goes here
-} */
+function createProduct() {
+
+  let newInput = document.querySelector('.create-product input').value;
+  let newPriceBefore = document.querySelector('.create-product td').nextElementSibling.querySelector('input').value;
+  let newPrice = Number(newPriceBefore).toFixed(2);
+
+  let addedNewProduct = document.querySelector('.product').cloneNode(true);
+  document.querySelector('tbody').appendChild(addedNewProduct);
+
+  let addedInput = document.querySelector('.product:last-child').firstElementChild.firstElementChild;
+  addedInput.textContent = newInput;
+  let addedPrice = document.querySelector('.product:last-child').childNodes[3].firstElementChild;
+  addedPrice.innerText = newPrice;
+}
 
 window.addEventListener('load', () => {
+
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  let removeBtns = document.querySelectorAll('.btn-remove'); 
+  removeBtns.forEach(btn => btn.onclick = removeProduct);
+
+  let addBtn = document.getElementById('create');
+  addBtn.onclick = createProduct;
 });
