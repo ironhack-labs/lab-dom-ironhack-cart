6d5c8bf00d7d322e43a9c44088ad3832bcd9cbb1
Rob TC <roberto.taboada@gmail.com>
2021-01-21 09:51:36 -0600

    hasta la iteración 3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = (product.querySelector(".price span").innerText)
+  const quantity = (product.querySelector('.quantity input').value)
+  const subTotal = price * quantity 
+  
+  let campoSubTotal = product.querySelector(".subtotal span")
 
-  //... your code goes here
+  campoSubTotal.innerText = subTotal 
+return subTotal
 }
-
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+/*  version de Viviana
+let products = document.getElementsByClassName('product')
+prodArr = [...products]
+let total=0
+for (i=0;i<prodArr.length;i++){
+  total += updateSubtotal(prodArr[i])
+  const totalPrice=document.querySelector('#total-value span')
+  totalPrice.innerHTML = total
 }
+}
+
+*/
+
+
+function calculateAll() {
+  $productos = document.querySelectorAll('.product')
+  let acumSubtotales = 0
+  $productos.forEach((producto)=>{
+    acumSubtotales+=updateSubtotal(producto)
+  })
+// ITERATION 3
+document.querySelector('#total-value span').innerText = acumSubtotales
+}
+  
+
+
+  
 
 // ITERATION 4
 
@@ -29,14 +49,82 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
-
+ 
 function createProduct() {
-  //... your code goes here
-}
+  
+  const createProdBtn =  document.querySelector('#create button')
 
+  createProdBtn.addEventListener('click',(()=>{
+    const newProdName = document.querySelector('.create-product input').innerText
+    const newProdPrice =document.querySelector('.create-product input').value
+
+    //let newRowClass = document.classList.add("product")
+    let newRowClass = document.createElement("tr")
+        newRowClass.classList.add("product")
+    let newRowName = document.createElement("td")
+        newRowName.classList.add("name")
+        newRowClass.appendChild(newRowName)
+
+        newRowNameText =document.createTextNode(`${NewProdName}`)
+    let newRowProdPrice =document.createElement("td")
+
+    let newRowQuantity
+    let newRowSubtotal
+    let newRowBtn
+    document.createElement("tr")
+  }))
+
+  /*
+  <tr class="product">
+          <td class="name">
+            <span>Ironhack Rubber Duck</span>
+          </td>
+          <td class="price">$<span>25.00</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+        </tr>
+  */
+  /*
+  <td class="name">
+            <span>Ironhack Rubber Duck</span>
+          </td>
+          <td class="price">$<span>25.00</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+  */
+  /*
+  <tr class="create-product">
+          <td>
+            <input type="text" placeholder="Product Name" />
+          </td>
+          <td>
+            <input type="number" min="0" value="0" placeholder="Product Price" />
+          </td>
+  */
+  //botonEscondeAnimales.addEventListener("click",()=>{areaAnimales.classList.add("hidden")})
+
+  
+
+
+  //<button id="create" class="btn">Create Product</button>
+/*
+}
+*/
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
 });
+
+
