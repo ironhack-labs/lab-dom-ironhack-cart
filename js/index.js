e462688f9d83514ea95be50dc4b3a3d348cb7f2a
Henrique Morikawa <henriqueyukiharu@gmail.com>
2021-02-06 11:29:45 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
   const price = product.querySelector('.price span').innerHTML
   const quantity = product.querySelector('.quantity input').value;
@@ -19,22 +17,52 @@ function calculateAll() {
 
 }
 
-// ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode.parentNode;
+  target.remove()
+  calculateAll()  
+
 }
 
-// ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  const productsList = document.querySelector('tbody')
+  productsList.innerHTML += `
+  <tr class="product">
+    <td class="name">
+      <span>${document.getElementById('newProductName').value}</span>
+    </td>
+    <td class="price">$<span>${document.getElementById('newProductPrice').value}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>`
+
+  const removeElements = document.getElementsByClassName('btn btn-remove');
+  for(let i = 0; i<removeElements.length; i++){
+    removeElements[i].onclick = removeProduct;
+  }
+  document.getElementById('newProductName').value= ''
+  document.getElementById('newProductPrice').value = 0
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeElements = document.getElementsByClassName('btn btn-remove');
+  for(let i = 0; i<removeElements.length; i++){
+    removeElements[i].onclick = removeProduct;
+  }
+
+  document.getElementById('create').onclick = createProduct;
+
 });
+
+
