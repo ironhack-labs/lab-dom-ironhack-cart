194d3b29c093b1171b61713797d1e57cbf75bab4
Juan Falconi <juanfalconi86@gmail.com>
2021-02-13 00:26:06 +0100

    iteration 1 et 2 finies, 3 pas vraiment... :-(

--- js/index.js
+++ js/index.js
@@ -1,23 +1,44 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  let price = product.querySelector(".price span");
+  let quantity = product.querySelector(".quantity input");
+  let renderedValue = quantity.value;
+  let inputPrice = price.innerText
+  // console.log(renderedValue)
+  // console.log(inputPrice)
+  let calcSubTotalPrice = inputPrice * renderedValue
+  // console.log(calcSubTotalPrice)
+  subTotal = product.querySelector(".subtotal span");
+  // console.log(subTotal)
+  return subTotal.innerHTML = calcSubTotalPrice
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let products = [...document.getElementsByClassName("product")]
+  // console.log(products)
+  // products.forEach(product => updateSubtotal(product))
+  const subTotalEachProd = products.forEach((product) =>
+    updateSubtotal(product)
+  )
 
+  
   // ITERATION 3
   //... your code goes here
+  let productOneSubTotal = document.querySelector(".subtotal span")
+  let productTwoSubTotal = document.querySelector(".product:nth-child(2) .subtotal span");
+  let total = document.querySelector("#total-value span")
+  // console.log(productOneSubTotal, productTwoSubTotal)
+  let totalFinal = productOneSubTotal + productTwoSubTotal
+  // console.log(totalFinal) ==> ça ne marche pas :-(
+    total.innerHTML = totalFinal 
+    //ça me donne un truc trop bizarre, bref, l'itération n'est pas terminée :-(
+
+
 }
 
 // ITERATION 4
