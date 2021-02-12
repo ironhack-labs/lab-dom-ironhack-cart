28390f1355e1d8e5a683f4d40fd047abce1c69ce
Romain B <romain.bnd@gmail.com>
2021-02-12 17:25:07 +0100

    add bonus

--- js/index.js
+++ js/index.js
@@ -36,18 +36,65 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log(target.parentNode.parentNode)
+  target.parentNode.parentNode.remove()
+  console.log(target.parentNode.parentNode)
+  calculateAll();
 }
 
 // ITERATION 5
+const inputs = document.querySelectorAll('.create-product input')
 
 function createProduct() {
-  //... your code goes here
+  const tbody = document.querySelector('tbody');
+  const inputs = document.querySelectorAll('.create-product input')
+
+  tbody.insertAdjacentHTML('beforeend', `
+  <tr class="product">
+    <td class="name">
+      <span>${inputs[0].value}</span>
+    </td>
+    <td class="price">$<span>${inputs[1].value}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>
+`) 
+const removeBtns = document.querySelectorAll('.btn-remove')
+removeBtns.forEach(btn => {
+  btn.addEventListener('click', removeProduct);
+})
+
+  // SOME TESTS TO REDUCE THE NUMBER OF LINES
+  // const newProduct = document.querySelector('.product');
+  // const tbody = document.querySelector('tbody');
+  // const input = document.querySelector('.create-product input')
+  // const price = document.querySelector('.price')
+  
+  // newProduct.querySelector('.name').innerText = input.value
+  // newProduct.querySelector('.quantity').innerText = price.value
+  // tbody.innerHTML += newProduct.innerHTML;  
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = document.querySelectorAll('.btn-remove')
+  removeBtns.forEach(btn => {
+    btn.addEventListener('click', removeProduct);
+  })
+
+  const create = document.querySelector('#create')
+  create.addEventListener('click', function() {
+    createProduct()
+    inputs[0].value = ""
+    inputs[0].focus()
+    inputs[1].value = 0
+  });
 });
+
