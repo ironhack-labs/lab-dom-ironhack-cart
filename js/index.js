c0ce4b71af89b2c5f18941d9d53c533a00a9d96f
Matthieu Cheynut <matthieucheynut@hotmail.com>
2021-02-12 20:53:36 +0100

    iterations 1, 2 and 3, no bonuses yet

--- js/index.js
+++ js/index.js
@@ -1,20 +1,65 @@
 // ITERATION 1
 
+const price = document.querySelectorAll(".price > span");
+const quantity = document.querySelectorAll(".quantity > input");
+const subTotal = document.querySelectorAll(".subtotal > span");
+const total = document.querySelector("#total-value span");
+
+
+//console.log(price);
+//console.log(quantity)
+//console.log(subTotal[0].innerHTML);
+console.log(total)
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  //const price = document.querySelector(".price span");
+  //const quantity = document.querySelector(".quantity input");
+  //const subTotal = document.querySelector(".subtotal span");
+  const price = document.querySelectorAll(".price > span");
+  const quantity = document.querySelectorAll(".quantity > input");
+  const subTotal = document.querySelectorAll(".subtotal > span");
+  const total = document.querySelector("#total-value span");
 
-  //... your code goes here
+  //const subTotalAll = subTotal.forEach(function(el){
+    
+    for (let i = 0; i < subTotal.length; i++){
+      subTotal[i].innerHTML = price[i].innerHTML*quantity[i].value
+      //console.log(typeof subTotal[i].innerHTML)  
+    }
+  //})
+
+let totalPrice = 0; // OH MY GOOOOD IT TOOK ME SOOOOO LONG TO FIGURE THIS OUT to avoid 50 + 15 = 5015!!!!!!!!!!!!!!! ODEOIFEH  IZEH IFHZIEO FZOFH OHEO HZE!!! A zdzadKJ OELDE DEDZ!!! EZDZEDze!!! I can die in PEACE NOW!
+  for (let i = 0; i < subTotal.length; i++){
+    totalPrice += Number(subTotal[i].innerHTML)
+    console.log(totalPrice)
+    total.innerHTML=totalPrice;
+    //console.log(typeof parseInt(total.innerHTML))
+  }
+  
+  return parseInt(total.innerHTML);
+      
+
+  // let subtotalValue = price.innerHTML * quantity.value
+
+  // subTotal.innerHTML = `${subtotalValue}`
+
 }
 
 function calculateAll() {
+  console.log('Calculating All, yey!');
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const products = document.getElementsByClassName("product");
+  updateSubtotal(products);
+
 
   // ITERATION 3
   //... your code goes here
