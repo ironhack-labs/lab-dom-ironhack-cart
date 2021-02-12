6f0a9d249d86899b27d4b027c10bd74c5da88520
Amalia Zebi <amalia.zebi@b4finance.com>
2021-02-12 17:25:16 +0100

    Iteration 1, 2 & 3 pending 4th

--- js/index.js
+++ js/index.js
@@ -1,32 +1,33 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subTotal = product.querySelector('.subtotal span');
+  const total = subTotal.innerHTML = (Number(price.innerHTML) * Number(quantity.value));
+  //console.log('Calculating subtotal, yey!');
+  return total;
 
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  let totalProduct = 0;
+  allProducts.forEach(element => totalProduct += updateSubtotal(element));
+  //console.log(allProducts);
+  const totalAll = document.querySelector('#total-value span');
+  totalAll.textContent = totalProduct;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+  const target = event.currentTarget
+  console.log('The target in remove is:', target)
+  target.parentNode.removeChild(target.parentNode);
+  calculateAll();
+};
 
 // ITERATION 5
 
