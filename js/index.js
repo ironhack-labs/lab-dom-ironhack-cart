33401b4bb9f7ec8eb4a8ffd8630dfadbac36b6d3
Fatoumata Diaby <fatoumata.diaby@outlook.com>
2021-02-16 15:44:02 +0100

    not finished

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  let newSubtotal = product.querySelector('.subtotal span');
+  newSubtotal.textContent = price.textContent * quantity.value;
 }
 
 function calculateAll() {
@@ -15,28 +17,27 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
+  // pour chaque produit, appeler updateSubtotal
+  document.querySelectorAll('.product').forEach(updateSubtotal);
 
   // ITERATION 3
   //... your code goes here
+  let sumOfSubtotals = 0;
+  document.querySelectorAll('.subtotal span').forEach(currentSubtotal => {
+    sumOfSubtotals += parseFloat(currentSubtotal.textContent);
+  }); 
+  document.querySelector('#total-value span').innerHTML =sumOfSubtotals;
 }
+
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
   //... your code goes here
 }
-
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
 }
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
 });
