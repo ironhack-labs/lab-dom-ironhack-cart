a10fcc4f6ebe15a79fb28ebd368578f29c4904eb
Linda Villa <linda.villa@alea.com>
2021-03-08 21:59:34 +0100

    Complete Iteration 3

--- js/index.js
+++ js/index.js
@@ -14,15 +14,24 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  /*const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);*/
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  //ITERATION 2
+const allProducts = document.querySelectorAll('.product');
+const productArray = Array.from(allProducts);
+const rowTotals = productArray.map(updateSubtotal);
+const sum = rowTotals.reduce(function(acc,currentValue){
+  return acc + currentValue
+});
+
 
   // ITERATION 3
-  //... your code goes here
+  const total = document.getElementById('total-value');
+  const totalAmount = total.querySelector('span');
+  totalAmount.textContent = sum;
+  
 }
 
 // ITERATION 4
