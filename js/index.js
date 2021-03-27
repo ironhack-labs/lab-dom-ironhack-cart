389dc1c088bb0d6c9b88489cf54b6ec0038e8714
Robert Valdes <62124782+Buckley212@users.noreply.github.com>
2021-03-26 21:58:33 -0400

    Finally not bad

--- js/index.js
+++ js/index.js
@@ -7,23 +7,23 @@ function updateSubtotal(product) {
   let subtotal = product.querySelector('.subtotal span');
   let newSub = quantity * price;
   subtotal.innerText = newSub;
-  return newSub;
+  return newSub; // <- !!IMPORTANT!! If you don't return the subtotal for each item, you can't calculate the subtotal in calculateAll, ad nothing is returned, yet the innerText changes. Confuses a lot of people
 }
 
 // Calculate Total
 
 function calculateAll() {
   let totalSub = 0;
-  let products = document.querySelectorAll('.product');
-  products.forEach(product => totalSub += updateSubtotal(product))
-  let total = document.querySelector('#total-value span');
+  let products = document.querySelectorAll('.product');   // <-- This is going to return an array, so if you don't target them individually, you're going to be banging your head wondering why the subtotal keeps saying NaN on the page
+  products.forEach(product => totalSub += updateSubtotal(product))    // <-- Runs the array, calculating subtotal and adding to total when called.
+  let total = document.querySelector('#total-value span');      // <-- applies total to #total-value
   total.innerText = totalSub;
 }
 
 
-// Create Products
+// Create Button, adds row to bottom
 
-function createProduct() {
+function createProduct() {                                               // <- adds a new table row, accessing the values of the inputs to use as properties
   let newName = document.querySelector('#n input').value;
   let newPrice = document.querySelector('#p input').value;
   let tBody = document.querySelector('tbody')
@@ -43,11 +43,13 @@ function createProduct() {
   </tr>
   `
 }
-//Delete Products
+
+
+//Calculates Total, adds subtotals to table rows when fired
 
 window.addEventListener('load', () => {
   let calculate = document.querySelector('#calculate');
-  calculate.addEventListener('click', calculateAll)
+  calculate.addEventListener('click', calculateAll) //<- fires calculateAll, which runs updateSubtotal on each product to update their innerText, then calculates the total and adds it below the button
 })
 //New Product
 const newProduct = document.querySelector('#create');
@@ -55,8 +57,8 @@ const newProduct = document.querySelector('#create');
 newProduct.addEventListener('click', createProduct)
 
 //Remove Button
-const removeButtons = document.querySelectorAll('.btn-remove');
+const removeButtons = document.querySelectorAll('.btn-remove'); // <- collects array of all buttons in the class
 
-removeButtons.forEach((removeButton) => removeButton.addEventListener('click', () => {
+removeButtons.forEach((removeButton) => removeButton.addEventListener('click', () => {  // <- Adds event listeners to each remove button added after new product is added above
   removeButton.parentElement.parentElement.remove()
 }))
