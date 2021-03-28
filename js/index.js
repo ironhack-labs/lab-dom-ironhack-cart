6521c507bafad9b006f04479503d686cb88951b0
Guille Muñoz <guillermomunozgonzalez@MacBook-Pro-de-Guillermo.local>
2021-03-28 18:17:09 +0200

    WEBMAD guillermo, falta solucionar errores y los bonus

--- js/index.js
+++ js/index.js
@@ -3,7 +3,26 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = document.querySelector ('.price span')
+  const quantity = document.querySelector ('.quantity input')
+  console.log(price, quantity)
+
+  const priceValue = price.innerHTML
+
+  const quantityValue = quantity.value 
+
+  console.log(quantityValue, priceValue)
+  
+   const subTotal = priceValue * quantityValue
+
+   console.log(subTotal)
+
+   const subTotaProduct = document.querySelector ('.subtotal span')
+
+   subTotaProduct.innerHTML = subTotal
+
+   return subTotal
+
 }
 
 function calculateAll() {
@@ -14,10 +33,23 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+   const allProducts = document.querySelectorAll('.product')
+
+   let total = 0
+
+   allProducts.forEach(element => {
+    total += updateSubtotal (element)
+  
+
+   });
 
   // ITERATION 3
-  //... your code goes here
+  let allSubtotal = document.querySelector('#total-value span')
+
+  allSubtotal.innerHTML = total
+
+ 
 }
 
 // ITERATION 4
