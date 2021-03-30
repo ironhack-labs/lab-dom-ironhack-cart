2fab814120a4f7f7b1d84e81f8bb50a04cc9fcfc
Chocogrenouille <thestral@gmx.net>
2021-03-30 17:33:28 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -27,12 +27,39 @@ function removeProduct(event) {
   console.log('The target in remove is:', target);
   const parentElement = target.parentNode.parentNode.parentNode;
   parentElement.removeChild(target.parentNode.parentNode);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const name = document.querySelector(".create-product input[type='text']").value;
+  const price = document.querySelector(".create-product input[type='number']").value;
+
+
+  const parentNode = document.querySelector('tbody');
+  const newItem = document.createElement('tr');
+  newItem.setAttribute('class', 'product');
+  parentNode.appendChild(newItem);
+  console.log(parentNode)
+  // const itemName = document.createElement('td').classList.add('name');
+  // itemName.appendChild(document.createElement('span').innerText(price))
+  // const itemPrice = document.createElement('td').classList.add('price');
+  // const itemQuantity = document.createElement('td').classList.add("mystyle");
+  // const itemSubtotal = document.createElement('td');
+  // const itemRemove = document.createElement('td');
+  
+  //document.createElement('block').appendChild( document.createElement('b')
+  // newItem.appendChild(itemName);
+
+  
+  // parentNode.appendChild(newItem);
+
+ 
+  // parentNode.append --> to add child element 
+  
+
+
 }
 
 window.addEventListener('load', () => {
@@ -42,4 +69,5 @@ window.addEventListener('load', () => {
   for (let i = 0; i < removeBtns.length; i++) {
     removeBtns[i].addEventListener('click', removeProduct);
   }
+  document.querySelector('#create').addEventListener('click', createProduct);
 });
