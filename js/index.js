1c1427d409bae5b7457f725ee4633c01e3697b90
Katarina Orlovska <k.orlovska@posteo.de>
2021-03-30 22:21:27 +0200

    Michelle and Katarina complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  
+
   let unitPrice = parseFloat(product.querySelector('.price span').innerText);
   let quantity = parseInt(product.querySelector('.quantity input').value);
   
@@ -13,60 +13,50 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
+  // ITERATION 2 & 3
   const productList = [...document.querySelectorAll('.product')];
   let total = 0;
 
   productList.forEach(product => total += updateSubtotal(product));
 
-  //console.log(typeof total)
-
   document.querySelector('#total-value span').innerText = +total.toFixed(2)
-
-  
-  // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-
+  // console.log('The target in remove is:', target);
   target.parentNode.parentNode.remove();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  
   let productName = document.querySelector('.create-product input[type=text]').value;
 
   let unitPrice = document.querySelector('.create-product input[type=number]').value;
 
-  //console.log(productName, unitPrice)
 
   let newProduct =  document.createElement('tr');
-  
+
   newProduct.classList.add('product');
 
-  newProduct.innerHTML =  
-      `<td class="name">
-        <span>${productName}</span>
-      </td>
-      <td class="price">$<span>${unitPrice}</span></td>
-      <td class="quantity">
-        <input type="number" value="0" min="0" placeholder="Quantity" />
-      </td>
-      <td class="subtotal">$<span>0</span></td>
-      <td class="action">
-        <button class="btn btn-remove">Remove</button>
-      </td>`;
+  newProduct.innerHTML =
+    `<td class="name">
+      <span>${productName}</span>
+    </td>
+    <td class="price">$<span>${unitPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>`;
 
-  newProduct.addEventListener('click',remove => remove.addEventListener('click', removeProduct));
-
+  newProduct.querySelector('button').addEventListener('click', removeProduct);
   document.querySelector('tbody').appendChild(newProduct);
-
 }
 
 window.addEventListener('load', () => {
@@ -78,5 +68,4 @@ window.addEventListener('load', () => {
 
   const createBtn = document.getElementById('create');
   createBtn.addEventListener('click', createProduct);
-
 });
