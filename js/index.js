c4dfcc22a017fc62b679cbf638049ea087fccdae
Chocogrenouille <thestral@gmx.net>
2021-03-30 12:34:51 +0200

    one iteration left

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = Number(product.querySelector('.price span').innerText);
+  const quantity = Number(product.querySelector('.quantity input').value);
+  const subTotal = price * quantity;
+  product.querySelector('.subtotal').innerText = subTotal;
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  let subTotal = 0;
+  for (let i = 0; i < allProducts.length; i++) {
+    subTotal += updateSubtotal(allProducts[i]);
+    console.log(subTotal);
+  }
+  document.querySelector('#total-value span').innerHTML = subTotal;
 }
 
 // ITERATION 4
@@ -25,7 +25,8 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const parentElement = target.parentNode.parentNode.parentNode;
+  parentElement.removeChild(target.parentNode.parentNode);
 }
 
 // ITERATION 5
@@ -37,6 +38,8 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const removeBtns = document.querySelectorAll('.btn.btn-remove');
+  for (let i = 0; i < removeBtns.length; i++) {
+    removeBtns[i].addEventListener('click', removeProduct);
+  }
 });
