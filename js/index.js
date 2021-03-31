bec8db3fd1cae50670a6648b3d850b3ceab1141d
triana <trianaheinz@gmail.com>
2021-03-31 20:02:15 +0200

    until iteration4 done

--- js/index.js
+++ js/index.js
@@ -1,50 +1,42 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  let price = document.querySelector('.price span');
-  let quantity = document.querySelector('.quantity input');
+  let price = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
 
-  let subtotal = Number(price.innerHTML)* Number(quantity.value);
-  
-  let sub = document.querySelector('.subtotal span');
+  let subtotal = Number(price.innerHTML) * Number(quantity.value);
 
-  return sub.innerHTML = subtotal;
+  let subT = product.querySelector('.subtotal span');
+
+  return (subT.innerHTML = subtotal);
 }
-
-
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
+  let total = 0;
+  const products = [...document.getElementsByClassName("product")];
+  products.forEach((el) => (total += updateSubtotal(el)));
 
-    // ITERATION 3
-  //... your code goes here
+  // ITERATION 3
+  const subT = document.querySelector("#total-value span");
+  subT.innerHTML = total;
 
-  /*const updateTitle = () => {
-    let liList = document.querySelectorAll('li');
-    h1.innerHTML = `Users list - ${liList.length}`;
-  }*/
-}
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const child = target.parentNode.parentNode;
+  parent.removeChild(child);
 }
+
 
 // ITERATION 5
 
 function createProduct() {
-  let  productList = document.querySelector('tbody');
+  let productList = document.querySelector('tbody');
 
-  let newProduct = document.createElement('tr', {class: "product"});
+  let newProduct = document.createElement('tr', { class: 'product' });
 
   newProduct.innerHTML = `
   <td class="name">
@@ -58,7 +50,7 @@ function createProduct() {
     <td class="action">
       <button class="btn btn-remove">Remove</button>
     </td>
-  ` ;
+  `;
 
   productList.appendChild(newProduct);
 }
