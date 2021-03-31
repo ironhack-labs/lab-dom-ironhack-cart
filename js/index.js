38b80902d6d5a2b8ef6a120df4ea7710b42608df
Taisia Makhmutova <taia.makh@gmail.com>
2021-03-31 13:25:15 +0200

    ADD iteration 2

--- js/index.js
+++ js/index.js
@@ -1,29 +1,33 @@
+let priceSelector = document.querySelector('.price span');
+let quantitySelector = document.querySelector('.quantity input');
+let subTotalSelector = document.querySelector('.subtotal span');
+let productSelection = document.getElementsByClassName('product');
+
 // ITERATION 1
 
 function updateSubtotal(product) {
-  let priceSelector = product.querySelector('.price span');
-  let quantitySelector = product.querySelector('.quantity input');
-  let subTotalSelector = product.querySelector('.subtotal span');
-
   let price = Number(priceSelector.innerHTML);
   let quantity = Number(quantitySelector.value);
 
   let subTotal = price*quantity;
   subTotalSelector.innerHTML = subTotal;
   return subTotal;
-
-  // Hola
 }
 
 function calculateAll() {
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+
+  
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let arrProducts = [...productSelection];
+  arrProducts.map(product => {
+    updateSubtotal(product);
+  })
 
   // ITERATION 3
   //... your code goes here
