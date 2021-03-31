a4f6b8b1b16137bcedead4119b7ddc40cc04820b
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-03-31 05:24:33 -0400

    1-Done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  let subTotalValue = price * quantity;
 
-  //... your code goes here
+  const subTotal = product.querySelector('.subtotal span')
+
+  subTotal.innerHTML = subTotalValue
+
+  
 }
 
 function calculateAll() {
@@ -31,12 +37,13 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+ 
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  
 });
