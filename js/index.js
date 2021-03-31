52b70a988439c20f13fdd7feb5cba8ecc9ac2f36
Sisco Falcó <siscofalcop@gmail.com>
2021-03-31 20:08:30 +0200

    bonus 4 and 5 done

--- js/index.js
+++ js/index.js
@@ -34,6 +34,37 @@ function removeProduct(event) {
 
 function createProduct() {
 //... your code goes here
+// Add a click event handler to the "Create Product" that will take a function named createProduct as a callback.
+// read values we want to create, value and price (gray line), save in var.
+
+const newTr = document.createElement('tr');
+const createProd = document.querySelector('.create-product');
+let newProductName = createProd.querySelector('#new-name');
+let newProductPrice = createProd.querySelector('#new-price');
+
+let tableRow = `<td class="name">
+  <span></span>
+</td>
+<td class="price">$<span></span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>`;
+
+
+newTr.classList.add('product');
+newTr.innerHTML = tableRow;
+newTr.querySelector('.price span').innerText = newProductPrice.value;
+newTr.querySelector('.name span').innerText = newProductName.value;
+const removeButton = newTr.querySelector('.action .btn-remove');
+removeButton.addEventListener('click', removeProduct);
+document.querySelector('tbody').appendChild(newTr);
+
+newProductName.value = "";
+newProductPrice.value = 0;
 }
 
 window.addEventListener('load', () => {
@@ -47,6 +78,9 @@ window.addEventListener('load', () => {
 
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  const addProduct = document.getElementById('create');
+  addProduct.addEventListener('click', createProduct);
 
   //... your code goes here
 });
