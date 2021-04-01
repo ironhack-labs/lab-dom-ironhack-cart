907b3c17d214a2c9410bb195c335cbf7b4b5c309
Graziele Dantas Silveira <elentary@gmail.com>
2021-03-31 23:29:45 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,57 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  
+  let subTotal = Number(price) * Number(quantity);
+  
+  product.querySelector('.subtotal span').innerText = subTotal;
 
-  //... your code goes here
+  return subTotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  let totalPrice = 0;
+
+  let products = document.querySelectorAll('.product');
+
+  products.forEach((product) => {
+    totalPrice += updateSubtotal(product);
+  });
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerText = Number(totalPrice);
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode;
+  
+  let productTarget = target.parentNode;
+
+  productTarget.remove()
+
+  calculateAll()
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  // nope...
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
 });
+
+const removeBtn = document.querySelectorAll('.btn-remove');
+  for(i = 0; i < removeBtn.length; i++) {
+    removeBtn[i].onclick = function(event) {
+      removeProduct(event);
+    }
+  }
+
+
+
+
