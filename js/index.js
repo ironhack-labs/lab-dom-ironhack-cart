b8bf8d2106046e26cf6857f1417b4bf1666c3205
Adrian <adrian.asencio92@hotmail.com>
2021-04-02 11:49:34 +0200

    Hasta iteración 4

--- js/index.js
+++ js/index.js
@@ -1,42 +1,49 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+    let price = product.querySelector('.price span');
+    let quantity = product.querySelector('.quantity input');
+    let productSubtotal = parseFloat(price.innerHTML) * quantity.value;
 
-  //... your code goes here
+    product.querySelector('.subtotal span').innerHTML = productSubtotal;
+
+    return productSubtotal
+
+
 }
 
+//ITERATION 2
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+    let productList = [...document.querySelectorAll('.product')];
 
-  // ITERATION 2
-  //... your code goes here
+    let totalPrice = 0;
 
-  // ITERATION 3
-  //... your code goes here
+    productList.forEach((product) => {
+        totalPrice += updateSubtotal(product);
+    });
+
+    //ITERATION 3
+
+    document.querySelector('#total-value span').innerHTML = totalPrice;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+    const target = event.currentTarget;
+    target.parentNode.parentNode.remove();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+    //... your code goes here
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+    //... your code goes here
 });
