fbeccd60a6b897e764bd868f9bd5f74a2fcbe421
elenamarsili <e.marsili89@gmail.com>
2021-04-14 17:01:29 +0200

    iterations 1-3 completed

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  const price = document.querySelector('.price span').innerHTML //find "price" in html doc and form this take only the inner HTML
-  const quantity =  document.querySelector('.quantity input').value //find "quantity" in html doc and from this take only the value
+  //ITERATION 1
+  const price = product.querySelector('.price span').innerHTML //find "price" in html doc and form this take only the inner HTML
+  const quantity =  product.querySelector('.quantity input').value //find "quantity" in html doc and from this take only the value
   
   const subtotal = price * quantity //calculate subtotal
   
-  const subtotalHolder = document.querySelector('.subtotal span') //find where the subtotal must go
+  const subtotalHolder = product.querySelector('.subtotal span') //find where the subtotal must go
 
   subtotalHolder.innerText = subtotal //add the subtotal to the subtotal holder
 
@@ -14,26 +15,25 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
+//ITERATION 2
 
-  const products = [...document.querySelectorAll('.product')]; //find all elements with product class and create an array of them
+  const products = [...document.querySelectorAll('.product')]; //create an array of products
+  /* products.forEach((product) => updateSubtotal(product)); */ //iterate over the products to make the subtotal
 
-  products.forEach((product) => updateSubtotal(product));
-}
+//ITERATION 3
+  let total = 0; //set total to 0
+  products.forEach(product => total += updateSubtotal(product))//iterate the products and sum the subtotal to the total
 
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const totalHolder = document.querySelector("#total-value span") // fund where the total must go
+  totalHolder.innerText = total // at the total at the total holder
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+ 
+  event.target.parentNode.parentNode.remove() //ESTA MAL: no entiendo porque me borra el producto tambien si pincho quantity
+
 }
 
 // ITERATION 5
@@ -46,5 +46,6 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = [...document.querySelectorAll('.btn-remove')];
+  removeBtns.forEach((btn) => addEventListener('click',removeProduct))
 });
