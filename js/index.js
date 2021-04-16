c700aa9d20d3d75447802bfe2474816136695144
Catuxa.aljo <catuxa.aljo@gmail.com>
2021-04-16 21:44:51 +0200

    Creadas funciones

--- js/index.js
+++ js/index.js
@@ -2,41 +2,83 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  let totalAmount = price*quantity;
+  product.querySelector('.subtotal span').innerHTML = totalAmount; 
+   
+    return totalAmount
 
-  //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let total = 0
+  let listedItems = document.getElementsByClassName('product');  
+  for(let price of listedItems){
+    updateSubtotal(price)
+    total += updateSubtotal(price)
+  }
 
+  
   // ITERATION 3
-  //... your code goes here
+ 
+ 
+ //let priceList =  document.getElementsByClassName('subtotal span').innerHTML;
+//total += priceList
+  document.querySelector('#total-value span').innerText = total
+
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+
+  const target = event.currentTarget; 
   console.log('The target in remove is:', target);
   //... your code goes here
+
+  target.parentNode.parentNode.remove()
 }
 
 // ITERATION 5
 
-function createProduct() {
+function createProduct(event) {
   //... your code goes here
+
+ const productContainer = document.querySelector('.product');
+ const product = document.createElement('tr')
+ product.classList.add('product') 
+ productContainer.append(product)
+
+ const productNameInput = document.querySelector('input[name="task"]');
+ const productName = productoInput.value;
+
+ todoList.append(task);
+  
+
+
+
+
+ 
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  const remove = document.getElementsByClassName('btn btn-remove');
+  for(let value of remove){
+    value.addEventListener('click', removeProduct);
+  }
+  const addProducts = document.getElementById('create')
+  addProducts.addEventListener('click', createProduct)
 
   //... your code goes here
 });
