1874db07647875d33189a064e94ecc8645c79ada
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2021-04-25 17:22:50 +0200

    Iteration 3 added

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@
 function updateSubtotal(product) {
   const price = product.querySelector(".price span").textContent;
   const quantity = product.querySelector(".quantity input").value;
-  product.querySelector(".subtotal span").textContent = Number(price) * Number(quantity);
+  product.querySelector(".subtotal span").textContent =
+    Number(price) * quantity;
 }
 
 function calculateAll() {
-  
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
 
@@ -17,15 +17,33 @@ function calculateAll() {
   // end of test
 
   // // ITERATION 2
-  
-    const singleProduct = document.getElementsByClassName('product');
-    for (let i =0; i<singleProduct.length; i++){
-      updateSubtotal(singleProduct[i])
-    }
-      //   // ITERATION 3
 
+  // const singleProduct = document.getElementsByClassName('product');
+  // for (let i =0; i<singleProduct.length; i++){
+  //   updateSubtotal(singleProduct[i])
+  // }
+  //   // ITERATION 3
+  // const singleProduct = document.getElementsByClassName('product');
+  // let sum=0;
+  // for (let i =0; i<singleProduct.length; i++){
+  //   updateSubtotal(singleProduct[i])
+  //   sum+=updateSubtotal(singleProduct[i]);
+  //   document.getElementsById('vt').textContent=String(sum);
+  //   return sum;
+  // }
+  let singleProduct = document.getElementsByClassName("product");
+  let sum = 0;
+  //console.log(singleProduct[0]);
+  for (let i = 0; i < singleProduct.length; i++) {
+    updateSubtotal(singleProduct[i]);
+    sum += Number(singleProduct[i].querySelector(".subtotal span").textContent);
+    //updateSubtotal(singleProduct[i]);
+    console.log(sum);
   }
- 
+  document.getElementById("total").textContent = `${sum}`;
+  return sum;
+}
+
 // ITERATION 4
 
 function removeProduct(event) {
