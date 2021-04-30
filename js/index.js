5f5daa40d3f2cb9f6b86cb704bf19b28e58dd7e7
luisve9 <luissamdoval.9@gmail.com>
2021-04-29 20:21:36 -0500

    final

--- js/index.js
+++ js/index.js
@@ -13,10 +13,13 @@ function updateSubtotal(product) {
 function calculateAll() {
   // ITERATION 2
   //... your code goes here 
-  const products = document.getElementsByClassName('product')
+  const products = [...document.getElementsByClassName('product')]
   let total = 0;
   //No pude utilizar el metodo forEach en el arreglo products ¿Por que pasa esto?
-  for(let i = 0; i < products.length; i++) {total += updateSubtotal(products[i])}
+  //for(let i = 0; i < products.length; i++) {total += updateSubtotal(products[i])}
+  products.forEach(product => {
+    total += updateSubtotal(product)
+  })
 
   // ITERATION 3
   //... your code goes here
@@ -25,9 +28,11 @@ function calculateAll() {
 }
 
 // ITERATION 4
+const remove = [...document.getElementsByClassName("btn-remove")]
+console.log(remove)
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.target.parentNode.parentNode.remove();
   console.log('The target in remove is:', target);
   //... your code goes here
 }
@@ -35,12 +40,37 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const inputs = document.querySelectorAll('.create-product td input');
+  console.log(inputs)
+  const container = document.querySelector("tbody")
+  const tr = document.createElement("tr")
+  tr.className = "product"
+  tr.innerHTML = `
+    <td class="name">
+      <span>${inputs[0].value}</span>
+    </td>
+    <td class="price">$<span>${inputs[1].value}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `
+  container.appendChild(tr)
+  const button = tr.querySelector("button");
+  button.onclick = removeProduct
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  remove.forEach(button => {
+    button.onclick = removeProduct;
+  })
+  const create = document.getElementById("create")
+  create.addEventListener('click', createProduct)
   //... your code goes here
 });
