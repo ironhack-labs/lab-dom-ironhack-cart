70e695e98d46f31f4907cd5cb871fa593e72c95b
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-05-10 23:52:08 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -7,11 +7,16 @@ function updateSubtotal(product) {
   const price = product.querySelector('.price span').textContent;
   //const quantity = product.querySelector('.quantity input[type="number"]')
   const quantity = product.querySelector('.quantity input').value;
+  //subtotalProduct calcula el total de un producto en funcion del numero de articulos
+  let subtotalProduct = price * quantity
+  //totalProduct es la variable que modifica la parte visual del css
+  const totalProduct = product.querySelector('.subtotal span');
+  totalProduct.innerHTML = subtotalProduct
+  return subtotalProduct
 
-  let subtotal = price * quantity
-  const total = product.querySelector('.subtotal span');
-  total.innerHTML = subtotal
-  return subtotal
+  //return subtotal //Es necesa
+
+  //return subtotal //Es necesario el return si luego se le va a pasar a otra funcion
   //... your code goes here
 }
 
@@ -19,9 +24,24 @@ function calculateAll() {
   //Obtenemos el primer elemento elemento del DOM
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
+  //Recorro con el metodo forEach todos los selectores de clase "product".
+  //Apunto a traves del metodo querySelectorAll
+  //Todos estos productos los almaceno en la variable Products
+  //Cada producto que recorro lo envio como argumento llamando a la funcion updateSubotal
+  //que me devolvera el importe total (subtotalProduct) para ese producto en funcion del numero de unidades compradas (cantidad)
+  //Este importe se ira sumando al del siguiente producto, acumulandose en la variable totalCart,
+  //que hemos definido como la direccion donde se encuentra el id="total-value" y el tag span
+  let total = 0
   const Products = document.querySelectorAll('.product').forEach(singleProduct => {
-    updateSubtotal(singleProduct);
+    total += updateSubtotal(singleProduct);
   });
+  let totalCart = document.querySelector('#total-value span').textContent
+  totalCart = total
+
+  // const spantotalCart = totalCart.getElementsbyTagName('span')
+
+
+
 
 
   // end of test
