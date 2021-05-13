35266d07e6c794ab2d5833ab2c256d4d1e4641e0
Andrei <andrei_1002@hotmail.com>
2021-05-13 14:25:32 +0200

    terminado tras corrección en clase

--- js/index.js
+++ js/index.js
@@ -32,18 +32,89 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
+  const targetToDelete = target.parentNode.parentNode 
+  const parentToDelete = targetToDelete.parentNode
+
+  parentToDelete.removeChild(targetToDelete)
+
+  calculateAll()
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const createProductRow = document.querySelector('.create-product')
+
+  const name = createProductRow.querySelector('td:first-child input')
+  const price = createProductRow.querySelector('td:nth-child(2) input')
+
+  if (name.value && Number(price.value)) {
+    const productNode = document.createElement("tr")
+    productNode.classList.add("product")
+    document.querySelector("tbody").appendChild(productNode)
+
+    const nameNode = document.createElement("td")
+    nameNode.classList.add('name')
+    productNode.appendChild(nameNode)
+    const nameSpan = document.createElement("span")
+    nameSpan.innerHTML = name.value
+    nameNode.appendChild(nameSpan)
+
+    const priceNode = document.createElement("td")
+    priceNode.classList.add('price')
+    priceNode.innerHTML = "$"
+    productNode.appendChild(priceNode)
+    const priceSpan = document.createElement("span")
+    priceSpan.innerHTML = Number(price.value).toFixed(2)
+    priceNode.appendChild(priceSpan)
+
+    const quantityNode = document.createElement("td")
+    quantityNode.classList.add('quantity')
+    productNode.appendChild(quantityNode)
+    const inputNode = document.createElement("input")
+    inputNode.type = "number"
+    inputNode.value = 0
+    inputNode.min = 0
+    inputNode.placeholder = "Quantity"
+    quantityNode.appendChild(inputNode)
+
+    const subtotalNode = document.createElement("td")
+    subtotalNode.classList.add('subtotal')
+    subtotalNode.innerHTML = "$"
+    productNode.appendChild(subtotalNode)
+    const subtotalSpan = document.createElement("span")
+    subtotalSpan.innerHTML = 0
+    subtotalNode.appendChild(subtotalSpan)
+
+    const removeNode = document.createElement("td")
+    removeNode.classList.add("action")
+    productNode.appendChild(removeNode)
+    const removeButton = document.createElement('button')
+    removeButton.classList.add("btn", "btn-remove")
+    removeButton.innerHTML = "Remove"
+    removeNode.appendChild(removeButton)
+
+    removeButton.addEventListener('click', removeProduct)
+
+    name.value = ""
+    price.value = 0
+  }
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.getElementsByClassName("btn-remove")
+
+  Array.from(removeButtons).forEach(button =>{
+    button.addEventListener("click", removeProduct)
+    
+  })
+
+  const createBtn = document.getElementById('create')
+  createBtn.addEventListener('click', createProduct)
 });
+
+  
