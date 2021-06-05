4664901875af15c15a0eba224ff04b1dfb1d5025
Kikec82 <kikecolo2007@yahoo.es>
2021-06-06 00:46:35 +0200

    Carrito compra

--- js/index.js
+++ js/index.js
@@ -1,24 +1,43 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let total = 0;
+  for (let i = 0; i < product.length; i++) {
+
+    //dentro de producto busco a sus hijos y el precio. 
+    const price = product[i].childNodes[3].querySelector('.price span').innerHTML
+    //dentro de producto busco a sus hijos,y la cantidad
+    const quantity = product[i].childNodes[5].querySelector('.quantity input').value;
+
+    const subTotal = quantity * price;
+    //A elementos con la class subtotal, le asigno valor de (quantity*price)
+    product[i].querySelector('.subtotal span').innerHTML = subTotal;
+    total += subTotal
+  }
+  //asigno al elemento con ID "total value" el valor de var total
+  document.querySelector('#total-value span').innerHTML = total
 }
 
+//Primero entro en clase product dentro de cart y asigno el array saliente a la var result y la uso com argumento en la función updateSubTotal. 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const result = document.getElementById('cart').getElementsByClassName('product');
+  updateSubtotal(result)
 }
+// ITERATION 2
+
+
+
+
+
+
+//... your code goes here
+
+// ITERATION 3
+
+
+//... your code goes here
+
 
 // ITERATION 4
 
@@ -37,6 +56,5 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
 });
