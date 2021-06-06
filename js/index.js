76bf77bfc483372c036a8a1ccda0179365ee0943
Luca TC <lucatuveri@outlook.it>
2021-06-06 22:52:16 +0200

    Lab solved

--- js/index.js
+++ js/index.js
@@ -40,7 +40,6 @@ function createProduct(event) {
   console.log('The target in add is:', target)
   const productName = document.querySelector('.create-product .name input ')
   const productPrice = document.querySelector('.create-product .price input ')
-  const productQuantity = document.querySelector('.create-product .quantity input ')
   const rowToAdd = document.createElement('tr')
   rowToAdd.className = 'product'
   const td1 = document.createElement('td')
@@ -48,7 +47,7 @@ function createProduct(event) {
   td1.innerText = productName.value
   const td2 = document.createElement('td')
   td2.className = 'price'
-  td2.innerText = '$' + productPrice.value
+  td2.innerText = '$'
   const td3 = document.createElement('td')
   td3.className = 'quantity'
   const td4 = document.createElement('td')
@@ -57,27 +56,28 @@ function createProduct(event) {
   const td5 = document.createElement('td')
   td5.className = 'action'
   iQuantity = document.createElement('input')
-  // <input type="number" value="0" min="0" placeholder="Quantity" />
   iQuantity.setAttribute('type', 'number')
   iQuantity.setAttribute('value', "0")
   iQuantity.setAttribute('min', "0")
   iQuantity.setAttribute('placeholder', 'Quantity')
   const sp1 = document.createElement('span')
-  sp1.innerText = 0
-  const sp2 = document.createElement('span')
-  iQuantity = document.createElement('input')
-  sp2.className = 'btn btn-remove'
-  sp2.innerText = 'Button'
+  sp1.innerText = productPrice.value
+  const sp2= document.createElement('span')
+  const btn = document.createElement('button')
+  btn.className = 'btn btn-remove'
+  btn.innerText = 'Remove'
   rowToAdd.appendChild(td1)
   rowToAdd.appendChild(td2)
   rowToAdd.appendChild(td3)
   rowToAdd.appendChild(td4)
   rowToAdd.appendChild(td5)
+  td2.appendChild(sp1)
   td3.appendChild(iQuantity)
-  td4.appendChild(sp1)
-  td5.appendChild(sp2)
-  console.log(productName.value)
-// if(productName.value==)
+  td4.appendChild(sp2)
+  td5.appendChild(btn)
+  // productName.required = true
+  // console.log(productName.value)
+  // if(productName.value==empty){don't appendChild---how could I do that?}
   document.querySelector('#cart tbody').appendChild(rowToAdd)
 
   //... your code goes here
@@ -88,7 +88,7 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   const removeProductBtn = document.querySelector('.btn-remove');
-  removeProductBtn.addEventListener('click', removeProduct);
+  removeProductBtn.addEventListener('click', removeProduct);//Why doesn't work on the new buttton remove childs?
 
   const createProductBtn = document.querySelector('.btn-create');
   createProductBtn.addEventListener('click', createProduct);
