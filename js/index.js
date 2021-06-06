5eb82df12760f3949c17d06fa45e1d987d821aa3
Sgallego <sgallegogirona@gmail.com>
2021-06-06 11:48:39 +0200

    finished cart salva

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
   const price = +product.querySelector('.price span').textContent;
   const quantity = +product.querySelector('.quantity input').value;
   const subtotal = product.querySelector('.subtotal span');
@@ -20,7 +20,6 @@ function calculateAll() {
   // end of test
   // ITERATION 2
   const products = document.querySelectorAll('.product');
-  console.log(products);
   let total = 0;
   products.forEach((elem) => {
     total += updateSubtotal(elem);
@@ -34,25 +33,53 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
+  // console.log(event.currentTarget);
   const target = event.currentTarget.parentNode.parentNode;
-
-  console.log('The target in remove is:', target);
+  // console.log(target);
+  // console.log('The target in remove is:', target);
   //... your code goes here
 
   target.parentNode.removeChild(target);
   calculateAll();
 }
 
-document.querySelectorAll('.btn-remove').forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    removeProduct(e);
+function buttonsReload() {
+  document.querySelectorAll('.btn-remove').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      removeProduct(e);
+    });
   });
-});
-
+}
+buttonsReload();
 // ITERATION 5
+
+document.querySelector('#create').addEventListener('click', createProduct);
 
 function createProduct() {
   //... your code goes here
+  const tbody = document.querySelector('tbody');
+
+  const newName = document.querySelector(
+    '.create-product input[type=text]'
+  ).value;
+  const newPrice = document.querySelector(
+    '.create-product input[type=number]'
+  ).value;
+  const productDefault = `<tr class="product">
+          <td class="name">
+            <span>${newName}</span>
+          </td>
+          <td class="price">$<span>${newPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+        </tr>`;
+  tbody.insertAdjacentHTML('beforeend', productDefault);
+  buttonsReload();
 }
 
 window.addEventListener('load', () => {
