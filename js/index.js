b9c23799fef1db492beda66a3607572c15397ff4
javierceballosaltuna <javierceballosaltuna@gmail.com>
2021-06-06 16:04:53 +0200

    me da error en la iteration 2 y no entiendo el porque, creo que la logica detras del codigo es correcta pero el testing no dice lo mismo, me he atascado en este punto

--- js/index.js
+++ js/index.js
@@ -1,24 +1,100 @@
 // ITERATION 1
 
+//const { product } = require("puppeteer");
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
+  const price = product.querySelector('.price span').textContent
+
+  const quantity = product.querySelector('.quantity input').value
+
+  const subtotal = price * quantity
+
+
+  product.querySelector('.subtotal span').textContent = subtotal
+
+
+  return (subtotal)
+
+
+
+
+
+
+
+
+
+
+
+
+
   //... your code goes here
 }
+
+
+
+
+
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let total = 0
 
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+
+  allProducts.forEach(function (item) {
+
+    const price = item.querySelector('.price span').textContent
+    const quantity = item.querySelector('.quantity input').value
+
+    const subtotals = price * quantity
+
+    subtotals = item.querySelector('.subtotal span').textContent
+
+    total = subtotals + total
+
+
+
+  })
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//... your code goes here
+
+// ITERATION 3
+
+
+
+
+
+
+//... your code goes here
+
 
 // ITERATION 4
 
