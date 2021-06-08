5f6536a216c044782f91dfc976103e69f4139db5
George DS Brooks <george@gdsbrooks.com>
2021-06-09 00:37:55 +0100

    all iterations complete. Used document fragment to add new product

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 // ITERATION 1
 
-const { product } = require("puppeteer")
-
-
 function updateSubtotal(product) {
-  const price = document.querySelector('.price span')
-  const quantity = document.querySelector('.quantity input')
-  let subtotal = document.querySelector('.subtotal span') 
-  let calculatedSubtotal = +price.innerText * +quantity.value
-  subtotal.innerText = calculatedSubtotal
-  return calculatedSubtotal
+  const price = product.querySelector('.price span').innerHTML
+  const quantity = product.querySelector('.quantity input').value
+  const subTotal = product.querySelector('.subtotal span')
+  let sub = price * quantity;
+    subTotal.innerHTML = +sub.toFixed(2)
+  return subTotal
 }
 
 function calculateAll() {
@@ -17,37 +14,74 @@ function calculateAll() {
   // // it runs when only iteration 1 is completed. at later point, it can be removed.
   // const singleProduct = document.querySelector('.product');
   // updateSubtotal(singleProduct);
-  // end of test
 
    // ITERATION 2
-  const product = document.getElementsByClassName('.product');
-  product.forEach((product) => {
-    updateSubtotal(product)
-  })
-    console.log
-
+  const products = document.querySelectorAll('.product')
+  products.forEach((product) => updateSubtotal(product))
 
   // ITERATION 3
   //... your code goes here
+  const allSubTotals = document.querySelectorAll('.product .subtotal span')
+  let total = document.querySelector('#total-value span')
+  let sum = 0
+  allSubTotals.forEach((oneTotal) => sum += +oneTotal.innerHTML)
+  total.innerText = sum.toFixed(2)
 }
 
+
 // ITERATION 4
+function initRemoveButtons() { //made into a function to call again later
+  let allRemoveButtons = document.querySelectorAll('.action button')
+allRemoveButtons.forEach((button) => button.addEventListener ('click', removeProduct))
+}
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  target.parentNode.parentNode.remove()
 }
-
 // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
+const createButton = document.querySelector('#create')
+createButton.addEventListener('click', createProduct)
 
+
+function createProduct() {
+  const target = event.currentTarget;
+
+  //create variables for inputted values
+  const newProduct = document.querySelector('tfoot input[type=text]').value
+  const newPrice = document.querySelector('tfoot input[type=number]').value
+  
+  //duplicate product row as a document Fragment
+  let range = document.createRange()
+range.selectNode(document.querySelector('.product'))
+productFragment = range.cloneContents()
+
+//target the first name and price of the document fragement and update with given values
+const productSpan = productFragment.querySelectorAll('td span ')
+productSpan.forEach((span,index,nodeList) => {
+    nodeList[0].innerText = newProduct
+    nodeList[1].innerText = (+newPrice).toFixed(2)
+  } )
+  // add initial quantity of 1
+  newQuant = productFragment.querySelector('input')
+  newQuant.value = 1
+  //calculate subtotal for fragment
+  updateSubtotal(productFragment)
+//append to the bottom of table body
+  tbodyNode = document.querySelector('tbody')
+  tbodyNode.appendChild(productFragment)
+  
+  //add remove  functionality to new roww
+  initRemoveButtons()
+  //calculate total
+  calculateAll()
+  //clear input fields
+  const inputs = document.querySelectorAll('tfoot input')
+  inputs.forEach((input) => input.value = '')
+}
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  initRemoveButtons()
 });
