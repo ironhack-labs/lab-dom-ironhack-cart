5c8d8d640d3e1363c51f24b1dba3999b57e6a15a
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-09 19:15:56 +0200

    Last attemt, unable to complete i5

--- js/index.js
+++ js/index.js
@@ -1,42 +1,106 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const subtotal = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  const total = Number(price.innerText) * Number(quantity.value);
+
+  subtotal.innerText = total;
+
+  return total;
 }
 
-function calculateAll() {
+  /*
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
+  */
 
   // ITERATION 2
-  //... your code goes here
+// call updateSuptotal with every tr.product in the tabel#cart  
 
   // ITERATION 3
-  //... your code goes here
+  //reuse subtotalAll and update the corresponding DOM element
+  //Display the value on your DOM
+
+
+function calculateAll() {
+
+  const priceAll = document.querySelectorAll('.product');
+  console.log("priceAll", priceAll)
+
+  let subtotalAll = 0;
+  priceAll.forEach((aProduct) => {
+  subtotalAll += updateSubtotal(aProduct)
+   });
+
+  console.log (subtotalAll);
+
+  const totalAll = document.querySelector('#total-value span');
+  totalAll.innerText = subtotalAll;
+  
+
+  return subtotalAll;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const row = target.parentNode.parentNode;
+  const tableBody = row.parentNode;
+  console.log('The target in remove is:', row, tableBody);
+  tableBody.removeChild(row);
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const currentRow = document.querySelectorAll('.product')[0];
+  const rowClone = currentRow.cloneNode(true);
+  const tableBody = document.getElementsByTagName('tbody');
+
+  document.tableBody.appendChild(rowClone);
+  
+
 }
 
 window.addEventListener('load', () => {
+  
   const calculatePricesBtn = document.getElementById('calculate');
+  
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+    // select remove buttons and loop through
+    const rmvButtons = document.querySelectorAll('.btn-remove');
+    rmvButtons.forEach((eachButton) => {
+      eachButton.addEventListener('click', removeProduct)
+    })
+  
+    console.log(rmvButtons);
+
+    //select create product and add click event
+    const customProduct = document.querySelector('#create');
+    customProduct.addEventListener('click', createProduct);
+
+    console.log(customProduct);
+
 });
+
+// window object refers to entire browser
+// this event listener will only execute this code when the browser has downloaded everythig
+// That is what the 'load' is referring to
+
+
+/*
+go to the documentation and look at inputHTML tag - the value is a special
+attribute that is uncommon to other elements. Only input elements have value
+attributes.
+Getting the value out of an input shouldn't be difficult with the input HTML
+
+*/
