432904bd35db81cc0ba2b3a05ee6723d0697a10f
samox_ <samirasteinmayer@web.de>
2021-06-10 16:51:06 +0200

    just the beginning untill now.... will continue

--- js/index.js
+++ js/index.js
@@ -2,19 +2,46 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let price = document.querySelector('.price span');
+  let quantity = document.querySelector('.quantity input');
+  //-->accessing the two values: works.
+  console.log(price)
+  console.log(quantity)
 
-  //... your code goes here
+  // let priceValue = price.innerHTML
+  // let quantityValue = quantity.value
+  // let subtotalPrice = priceValue * quantityValue
+
+  let subtotalPrice = price.innerHTML * quantity.value
+
+  //get the DOM element that should hold the subtotal
+  let subtotalField = document.querySelector('.subtotal span')
+
+  //set the subtotal price to the corresponding DOM element
+  subtotalField.innerHTML = '....'  ///<<<<-----here's a mistake....
+  //return subtotalPrice;
 }
+////<----doesnt work yet
+
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+              // const singleProduct = document.querySelector('.product');
+              // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+let productNr = getElemntsByClassName('product')
+    //tried:) ->>
+          // for (let i=0; productNr.length > 1; i++)
+          //     (productNr[i]) => {updateSubtotal()}
+
+          //Wo.Wie Rferenz table#cart???
+
+
 
   // ITERATION 3
   //... your code goes here
