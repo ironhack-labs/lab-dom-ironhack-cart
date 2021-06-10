c7ea53261c385c51cc4d7cccbccf8eedb2fe77cf
Jordana Queiroz <jordanavq@gmail.com>
2021-06-09 21:23:36 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,24 +1,56 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  const price = product.querySelector('.price span').innerText; //parseFloat ou Number p/ retornar um número. Usei innerText pois 25 é um texto e quero que retorne apenas o texto sem a tag, já que ja acessei a tag span em .price span.
+  const quantity =  product.querySelector ('.quantity input').value;
+  const subTotal = price * quantity;
+  product .querySelector ('.subtotal span').innerText = subTotal;
+  
+
   console.log('Calculating subtotal, yey!');
+  return subTotal
 
-  //... your code goes here
-}
+ 
+};
 
+
+// ITERATION 2
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  
+  const allProducts  = document.getElementsByClassName ('product');
+  const calculateTotal = document.querySelector('#total-value span');
 
-  // ITERATION 3
-  //... your code goes here
+  let total=0;
+
+  for (let i = 0; i<allProducts.length; i++) {
+     total += updateSubtotal(allProducts[i]);
+  }
+  calculateTotal.innerText = total;
+  console.log(total);
+
+ 
+
 }
+   // ITERATION 3
+  
+
+     
+  
+ 
+
+// code in the following two lines is added just for testing purposes.
+// it runs when only iteration 1 is completed. at later point, it can be removed.
+/*  const singleProduct = document.querySelector('.product');
+updateSubtotal(singleProduct); */
+// end of test
+
+
+
+
+
+
+
 
 // ITERATION 4
 
