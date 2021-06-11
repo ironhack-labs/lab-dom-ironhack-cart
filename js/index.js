f65867c3643a95914830badd77785368231deb19
joannakot <joanna.kot.ok@gmail.com>
2021-06-11 18:01:52 +0200

    Iteration 1 & 2

--- js/index.js
+++ js/index.js
@@ -1,24 +1,53 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  const subTotal =  price * quantity;
+  product.querySelector('.subtotal > span').innerHTML = subTotal;
 
-  //... your code goes here
+  return subTotal;
 }
+ 
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
 
+    // const products = document.querySelectorAll('.product');
+    let total = 0;
+    document.querySelectorAll('.product').forEach(function (product) {
+      console.log(product);
+      total += updateSubtotal(product);
+    });
+    console.log(total);
+    document.querySelector('#total-value').innerText = `Total: $${total}`;
+    }
+      
+      //document.querySelector('#total-value span').innerHTML = ;
+
+    //use updateSubtotal function on each product
+    //calculate sum of them
+    //use function updateSubtotal() and calculate products
+  
+
+  
+
   // ITERATION 3
   //... your code goes here
-}
+  //reuse total value and update corresponding DOM element 
+  //calculate total price for products in cart by summing all of subtotal returned by updateSubtotal()
+  //display that value on DOM
+
+//console.log(calculateAll());
 
 // ITERATION 4
 
@@ -32,6 +61,8 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  // const name
+  // const price
 }
 
 window.addEventListener('load', () => {
