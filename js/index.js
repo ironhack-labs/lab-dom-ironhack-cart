e6d3e74c76c2cde22d1d4a2f3e734b2882e059f8
samox_ <samirasteinmayer@web.de>
2021-06-14 11:56:56 +0200

    subtotal doesnt display even if its calculating.

--- js/index.js
+++ js/index.js
@@ -1,75 +1,102 @@
 // ITERATION 1
 
+//button functionality
+window.addEventListener('load', () => {
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+});
+
+//calculate subtotal
 function updateSubtotal(product) {
+
+  /////////////WORKING FOR ONE PRODUCT//////////////////
   console.log('Calculating subtotal, yey!');
-  let price = document.querySelector('.price span');
-  let quantity = document.querySelector('.quantity input');
+  let priceEl = document.querySelector('.price span');
+  let quantityInput = document.querySelector('.quantity input').value;
+  //or: document.querySelector('.quantity input').value
   //-->accessing the two values: works.
-  console.log(price)
-  console.log(quantity)
-
-  // let priceValue = price.innerHTML
-  // let quantityValue = quantity.value
-  // let subtotalPrice = priceValue * quantityValue
-
-  let subtotalPrice = price.innerHTML * quantity.value
+  console.log(priceEl.innerHTML)
+  console.log(quantityInput)
 
   //get the DOM element that should hold the subtotal
-  let subtotalField = document.querySelector('.subtotal span')
-
   //set the subtotal price to the corresponding DOM element
-  subtotalField.innerHTML = '....'  ///<<<<-----here's a mistake....
+  let subtotalPrice = document.querySelector('.subtotal span').innerHTML
+  subtotalPrice = priceEl.innerHTML * quantityInput;
   //return subtotalPrice;
+
+  ////////////////WORKING FOR QUANTITY OF PRODUCTS/////////////////
+  // console.log('Calculating subtotal, yey!');
+  // let priceEl = document.querySelectorAll('.price span');
+  // let quantityInput = document.querySelectorAll('.quantity input').value;
+  // //or: document.querySelector('.quantity input').value
+  // //-->accessing the two values: works.
+  // console.log(priceEl.innerHTML)
+  // console.log(quantityInput)
+
+  // //get the DOM element that should hold the subtotal
+  // //set the subtotal price to the corresponding DOM element
+  // let subtotalPrice = document.querySelectorAll('.subtotal span').innerHTML 
+  // subtotalPrice = priceEl.innerHTML * quantityInput;
+  // calculateAll();
+
 }
-////<----doesnt work yet
-
-
+////<----doesnt display the subTotalValue... 
 
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-              // const singleProduct = document.querySelector('.product');
-              // updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-let productNr = getElemntsByClassName('product')
-                //tried haha:) ->>
-                      // for (let i=0; productNr.length > 1; i++)
-                      //     (productNr[i]) => {updateSubtotal()}
+  let productNr = getElemntsByClassName('product');
+  for (let i = 0; productNr.length > 0; i++) {
+    updateSubtotal(productNr[i])
 
-                      //Wo.Wie Rferenz table#cart???
+    // ITERATION 3
+    //... your code goes here
+    let totalSumElements = document.querySelector('totalValue span')
 
+    ///add the elements of that array: .....
+    let totalSum = document.getElementsByName('subtotalPrice')
+    totalSum.innerHTML += updateSubtotal[i].priceEl
 
-
-  // ITERATION 3
-  //... your code goes here
-  let totalSumElements = document.querySelector('totalValue span')
-
-  ///add the elements of that array: .....
-  let totalSum = document.getElementsByName('subtotalPrice')
-  totalSum.innerHTML = 
+  }
 
 }
 
 // ITERATION 4
 
+
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
 
-// ITERATION 5
+  let removeCartItemButton = document.getElementByClassName('btn-remove');
+  for (let i = 0; i < removeCartItemButton.length; i++) {
+    let buttonR = removeCartItemButton[i];
+    buttonR.addEventListener('clicked', function (event) {
+      console.log('clicked')
+    })
 
-function createProduct() {
-  //... your code goes here
-}
+    const target = event.currentTarget;
+    target.parentElement.parentElement.remove()
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+
+    console.log('The target in remove is:', target);
+    //... your code goes here
+  }
+
+  // ITERATION 5
+
+  function createProduct() {
+    //... your code goes here
+  }
+
+  window.addEventListener('load', () => {
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
+
+    //... your code goes here
+  });
