2deaf416541f27ff30ca1b2b7d0e769b97a6cdc8
TrellinXp <Peter_My@gmx.de>
2021-07-14 13:13:29 +0200

    Calculate Subtotal and Calculate All

--- js/index.js
+++ js/index.js
@@ -2,22 +2,38 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let price =  product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
 
-  //... your code goes here
+  const priceValue = parseFloat(price.innerText);
+  const quantityValue = parseInt(quantity.value);
+
+  var subtotal = product.querySelector('.subtotal span');
+  subtotal = priceValue * quantityValue;
+  product.querySelector('.subtotal span').innerText = subtotal;
+  return priceValue * quantityValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.getElementsByClassName('product');
+  const productsArr = [...products];
 
-  // ITERATION 2
-  //... your code goes here
+  const totalValuesArr = [];
+  let totalValue = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  productsArr.forEach(product => {
+    console.log(product);
+    const subTotal = updateSubtotal(product);
+    console.log(subTotal);
+    totalValuesArr.push(subTotal);
+    totalValue += subTotal;   
+  });
+
+  console.log("Total Value "+totalValue);
+  let totalValueElement = document.getElementById('total-value');
+  totalValueElement.querySelector("span").innerText = totalValue;
 }
 
 // ITERATION 4
