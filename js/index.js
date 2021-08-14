9048bfdc4f9f6bda6711ea31bdc3e065e5c1c492
Ivan Espinoza <icaro_19@hotmail.com>
2021-08-14 22:47:10 +0200

    finish iterations

--- js/index.js
+++ js/index.js
@@ -1,20 +1,60 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // let productPrice = document.querySelector('.price span')
+  // let quantity = document.querySelector('.quantity input').value
+  let subTotal = document.querySelector('.subtotal span')
 
+  // console.log("price" + productPrice.textContent)
+  // console.log("quantity " + quantity)
+
+  console.log(product.querySelector(".quantity input ").value)
+  console.log(product.querySelector(".price span").textContent)
+
+  product.querySelector(".subtotal span").textContent = product.querySelector(".quantity input").value *
+    product.querySelector(".price span").textContent
+
+  console.log(product.querySelector(".subtotal span").textContent)
+
+  // subTotal.textContent = productPrice.textContent * quantity
+  // total.textContent = product.querySelector(".subtotal span").textContent
+
+  // console.log("subtotal :  " + subTotal.textContent)
+
+  // console.log('Calculating subtotal, yey!');
+
+
+
+
+
+
+  // console.log(typeof parseInt(product.querySelector(".subtotal span").textContent))
+  return (product.querySelector(".subtotal span").textContent)
   //... your code goes here
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const products = document.querySelectorAll('.product');
+  let total = document.querySelector('#total-value span')
+  total.textContent = 0
+
+  console.log(products.length)
+  // updateSubtotal(products);
+
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  for (i = 0; i < products.length; i++) {
+    // console.log(typeof (total.textContent))
+    total.textContent = parseFloat(total.textContent) + parseFloat(updateSubtotal(products[i]))
+    // console.log(products[i].querySelector(".quantity input").value)
+  }
+
+
 
   // ITERATION 3
   //... your code goes here
