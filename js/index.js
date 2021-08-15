754ff1435655b2f8086c16b7ee4f9f3dd02dfaed
Vladyslav <vladickbaraza@gmail.com>
2021-08-16 00:29:38 +0200

    Added some alerts

--- js/index.js
+++ js/index.js
@@ -1,53 +1,74 @@
-function updateSubtotal(product) {
-	const price = product.querySelector('.price span').innerHTML;
-	const quantity = product.querySelector('.quantity input').value;
-	const getTotal = price * quantity;
-	const modified = (product.querySelector('.subtotal span').innerHTML =
-	getTotal);
-	
+
+const getPrice = (product) => product.querySelector('.price span').innerHTML;
+const getQuantity = (product) => product.querySelector('.quantity input').value;
+const getTotal = (price, quantity) => price * quantity; 
+const getButtons = () => document.querySelectorAll('.btn-remove')
+const modify = (product) => product.querySelector('.subtotal span')
+
+// function to update subtotal
+const updateSubtotal = (product) => {
+	const price = getPrice(product)
+	const quantity = getQuantity(product)
+	const modified = modify(product).innerHTML = getTotal(price, quantity);
 	return modified;
 }
 
-function calculateAll() {
-	const singleProduct = document.querySelector('.product');
-	const updateTotal = (document.querySelector('#total-value span').innerHTML = total);
-
+// function to loop products Arr to calculate total price
+const productsLoop = () => {
 	let total = 0;
 	for (let i = 0; i < document.getElementsByClassName('product').length; i++) {
 		let subtotal = updateSubtotal(
 			document.getElementsByClassName('product')[i]
 		);
-		total = total + subtotal;
+		total += subtotal;
 	}
-
-	return updateTotal
+	return total;
 }
 
-function removeProduct(event) {
+// function to calculate all
+const calculateAll = () => {
+	const updateTotal = (document.querySelector('#total-value span').innerHTML = productsLoop())
+	return updateTotal	
+} 
+
+// function to remove products
+removeProduct = (event) => {
 	const target = event.currentTarget;
 	const buttons = (document.querySelectorAll('.btn-remove'));
-
 	target.closest('tr').remove();
 	buttons.forEach((button) => {
 		button.addEventListener('click', removeProduct)
 	})
-
 	calculateAll();
 }
 
-function deleteItem() {
-	const buttons = document.querySelectorAll('.btn-remove');
-
-	buttons.forEach((button) => {
+// function to capture clicked btn
+deleteItem = () => {
+	getButtons().forEach((button) => {
 		button.addEventListener('click', removeProduct);
 	});
 }
 
-function createProduct() {
+// function to reset product and price placeholder and value when we change it
+resetNew = () => {
+	const productName = document.querySelector('.product-name')
+	const newItemPrice = document.querySelector('.new-item-price')
+	productName.placeholder = 'Product Name'
+	productName.value = ''
+	newItemPrice.placeholder = '0'
+	newItemPrice.value = ''
+}
+
+// function to create new product
+createProduct = () => {
 	const tbody = document.querySelector('tbody')
 	const productName = document.querySelector('.product-name').value
 	const price = document.querySelector('.new-item-price').value
 
+	// condition to require productName and price
+	if (productName === "" || price === ""){
+		Swal.fire("PLEASE ADD PRODUCT NAME AND UNIT PRICE")
+	} else {
 	tbody.innerHTML += 
 	`<tr class="product">
         <td class="name">
@@ -55,21 +76,28 @@ function createProduct() {
         </td>
          <td class="price">$<span>${price}</span></td>
         <td class="quantity">
-          <input type="number"  value="0" min="0" placeholder="Quantity" />
+          <input onchange="calculateAll(this.value)" type="number"  value="0" min="0" placeholder="Quantity" />
         </td>
         <td class="subtotal">$<span>0</span></td>
         <td class="action">
           <button class="btn btn-remove">Remove</button>
         </td>
     </tr>`;
+	}
 
+	resetNew()
 	deleteItem()
+}
+
+// function to show alert with total price
+showTotal = () => {
+	Swal.fire(`TOTAL: $${productsLoop()}`)
+	calculateAll()
 }
 
 window.addEventListener('load', () => {
 	const calculatePricesBtn = document.getElementById('calculate');
-
-	calculatePricesBtn.addEventListener('click', calculateAll);
+	calculatePricesBtn.addEventListener('click', showTotal);
 });
 
 deleteItem()
