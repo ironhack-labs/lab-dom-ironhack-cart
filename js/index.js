e4bff04cb72c3bae3b03358d2d50ef6d9fa11df5
Vladyslav <vladickbaraza@gmail.com>
2021-08-15 02:38:24 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 function updateSubtotal(product) {
-	// console.log('Calculating subtotal, yey!');
-
 	const price = product.querySelector('.price span').innerHTML;
 	const quantity = product.querySelector('.quantity input').value;
 	const getTotal = price * quantity;
 	const modified = (product.querySelector('.subtotal span').innerHTML =
-		getTotal);
-
+	getTotal);
+	
 	return modified;
-
 }
 
 function calculateAll() {
 	const singleProduct = document.querySelector('.product');
+	const updateTotal = (document.querySelector('#total-value span').innerHTML = total);
 
 	let total = 0;
 	for (let i = 0; i < document.getElementsByClassName('product').length; i++) {
@@ -21,44 +19,42 @@ function calculateAll() {
 		);
 		total = total + subtotal;
 	}
-	const updateTotal = (document.querySelector('#total-value span').innerHTML = total);
+
 	return updateTotal
 }
 
 
 function removeProduct(event) {
 	const target = event.currentTarget;
-	// console.log('The target in remove is:', target);
-	// console.log("clicked");
-	target.closest('tr').remove();
 	const buttons = (document.querySelectorAll('.btn-remove'));
+
+	target.closest('tr').remove();
 	buttons.forEach((button) => {
 		button.addEventListener('click', removeProduct)
 	})
+
 	calculateAll();
 }
 
-	
-	function deleteItem (){
-		const buttons = document.querySelectorAll('.btn-remove');
-		console.log(buttons);
 
-		buttons.forEach((button) => {
-			button.addEventListener('click', removeProduct);
-		});
+function deleteItem() {
+	const buttons = document.querySelectorAll('.btn-remove');
 
-
-	}
+	buttons.forEach((button) => {
+		button.addEventListener('click', removeProduct);
+	});
+}
 
 
-	
+
 
 function createProduct() {
 	const tbody = document.querySelector('tbody')
 	const productName = document.querySelector('.product-name').value
 	const price = document.querySelector('.new-item-price').value
 
-	tbody.innerHTML += `<tr class="product">
+	tbody.innerHTML += 
+	`<tr class="product">
         <td class="name">
           <span>${productName}</span>
         </td>
@@ -70,47 +66,15 @@ function createProduct() {
         <td class="action">
           <button class="btn btn-remove">Remove</button>
         </td>
-      </tr>`;
+    </tr>`;
 
-	  let newTr = document.createElement("tr");
-	  let newTd = document.createElement('td')
-
-	  deleteItem()
-
+	deleteItem()
 }
 
 window.addEventListener('load', () => {
 	const calculatePricesBtn = document.getElementById('calculate');
+
 	calculatePricesBtn.addEventListener('click', calculateAll);
-
-
-	
-	
-
-
-
 });
 
-
 deleteItem()
-
-
-// function generateTable(productName, price){
-// 	let tr1 = document.createElement(tr);
-// 	let td1 = document.createElement(td);
-// 	let span1 = document.createElement(span);
-// 	let textSpan1 = document.createTextNode(`${productName}`);
-// 	let td2 = document.createElement(td);
-// 	let td3 = document.createElement(td);
-// 	let td4 = document.createElement(td);
-// 	let td5 = document.createElement(td);
-	
-
-
-// 	tr1.setAttribute('class', 'product')
-// 	td1.setAttribute('class', 'name')
-// 	td2.setAttribute('class', 'price')
-// 	td3.setAttribute('class', 'quantity')
-
-
-// }
