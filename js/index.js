1dbaa81a381e7868b0b96cb2565c552d0e845fd3
Osvaldo Picazo <odpicazo@gmail.com>
2021-08-17 22:53:35 +0200

    Osvaldo Picazo WDFT-Rem-Aug21 - Lab solved

--- js/index.js
+++ js/index.js
@@ -2,22 +2,45 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  // the string with the price is insie the span tag
+  // the quantity number is a value attribute inside element qith the quantity class 
+  const productPrice = price.innerText;
+  const productQuantity = quantity.value;
+  const subtotal = productPrice * productQuantity;
+  const updatedProduct = product.querySelector('.subtotal span');
+  updatedProduct.innerText = subtotal;
+  return subtotal;
 
+  //console.log("subtotal: ", subtotal)
+  //console.log(quantity.value)
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const multipleProducts = document.getElementsByClassName("product");
+  //const arrayOfProducts = Array.from(multipleProducts);
+
+  // calculate subtotal for each product
+  let totalPrice = 0;
+
+  for (element of multipleProducts) {
+    updateSubtotal(element);
+    totalPrice += updateSubtotal(element)
+  }
 
   // ITERATION 3
   //... your code goes here
+  document.querySelector("#total-value span").innerText = totalPrice;
 }
 
 // ITERATION 4
