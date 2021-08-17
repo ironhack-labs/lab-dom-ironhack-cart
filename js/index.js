f0773595bb4c193d2031585cc0aeeccc7a4c4c36
Monika Geiger <geigermonika@hotmail.hu>
2021-08-17 22:06:44 +0200

    doneeee

--- js/index.js
+++ js/index.js
@@ -37,20 +37,15 @@ function calculateAll() {
 
   // ITERATION 3
   //... your code goes here
-  // let allTotal = document.querySelectorAll('.subtotal span')
-  // console.log(allTotal)
-  // let sumArray = []
-  // for (let i = 0; i < allTotal.length; i++) {
-  //   sumArray.push(allTotal[i].innerText)
-  // }
-  // console.log(sumArray)
-  // let newValue = sumArray.reduce(function (previousValue, currentValue) {
-  //     return parseInt(previousValue) + parseInt(currentValue)
-  //   },
-  //   0)
-  // console.log(newValue)
-  // const totalPricePlace = document.getElementById('total-value')
-  // totalPricePlace.innerText = `Total: $${newValue}`
+  let allTotal = document.querySelectorAll('.subtotal span')
+  console.log(allTotal)
+  let sum = 0
+  for (let i = 0; i < allTotal.length; i++) {
+    sum += parseInt(allTotal[i].innerText)
+  }
+  console.log(sum)
+  const totalPricePlace = document.getElementById('total-value')
+  totalPricePlace.innerText = `Total: $${sum}`
 
 }
 
@@ -58,14 +53,37 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  let parent = target.parentNode.parentNode.parentNode
+  let child = target.parentNode.parentNode
+  parent.removeChild(child)
+  console.log('The target in remove is:', target, target.parentNode.parentNode.parentNode);
+  calculateAll()
   //... your code goes here
+
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+  // const target = event.currentTarget;
+  // const parent = target.parentNode.parentNode
+  const inputName = document.getElementById('inputName').value
+  const inputPrice = document.getElementById('inputPrice').value
+  console.log(inputName, inputPrice)
+  const tableRow = document.createElement("tr")
+  tableRow.classList.add("product")
+  tableRow.innerHTML = `<td class="name"><span>${inputName}</span></td><td class="price">$<span>${inputPrice}</span></td><td class="quantity"><input type="number" value="0" min="0" placeholder="Quantity"/></td><td class="subtotal">$<span>0</span></td><td class="action"><button id="newRemoveButton" class="btn btn-remove">Remove</button></td>`
+  const table = document.getElementsByTagName('tbody')
+  table[0].appendChild(tableRow)
+  const newRemoveButton = document.getElementById('newRemoveButton')
+  newRemoveButton.addEventListener('click', removeProduct)
+
+
+  // const dangerousElement = document.createElement("p")
+  // dangerousElement.innerHTML = "<script> console.log('danger')</script> I am a <span style='color: red'> text node </span> and I can only contain text, I cannot contain children"
+  // document.body.appendChild(dangerousElement);
+
 }
 
 window.addEventListener('load', () => {
@@ -73,6 +91,11 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  const removeButtons = document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct)
+  }
 
-
+  const createButton = document.getElementById('create')
+  createButton.addEventListener('click', createProduct)
 });
