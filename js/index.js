195d623bc9e2e72f5ca7a416599868978195092c
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-08-18 17:47:49 +0200

    Bonus iterations complete!

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ function calculateAll() {
       product.querySelector('.subtotal span').innerText
     );
     finalTotal += subtotals;
-    console.log(finalTotal);
+    // console.log(finalTotal);
   }
 
   // ITERATION 3
@@ -36,23 +36,62 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
-function removeProduct(event) {
-  // console.log(document.querySelector('.btn-remove'));
-  const removeButtons = document.querySelector('.btn-remove');
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+function removeProduct(event) {  
+  const target = event.currentTarget.parentNode.parentNode;
+  // const target = document.querySelector('.product');
+  console.log('The target in remove. is:', target);
+  target.parentNode.removeChild(target);
+  calculateAll();
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  let productText = document.querySelector('.create-product input[type="text"]').value;
+  let productNumber = document.querySelector('.create-product input[type="number"]').value;
+  let productTable = document.querySelector('tbody');
+  // let tableSection = document.querySelector('.product');
+  let newRow = document.createElement('tr');
+  newRow.classList.add('product');
+  productTable.appendChild(newRow);
+  newRow.innerHTML = ` <tr class="product">
+  <td class="name">
+    <span>${productText}</span>
+  </td>
+  <td class="price">$<span>${productNumber}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>
+</tr>`
+  calculateAll();
+  removeButton();
+  removeInputs();
 }
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-  //... your code goes here
+  // Create Button
+  const createButton = document.getElementById('create');
+  createButton.addEventListener('click', createProduct);
+  removeButton();
 });
+
+function removeButton() {
+  // Remove Button
+  const removeButtons = document.querySelectorAll('.btn-remove');
+  // console.log(removeButtons);
+  for (let button of removeButtons) {
+    console.log(button);
+    button.addEventListener('click', removeProduct);
+  }
+}
+
+function removeInputs() {
+  document.querySelector('.create-product input[type="text"]').value = '';
+  document.querySelector('.create-product input[type="number"]').value = 0;
+}
