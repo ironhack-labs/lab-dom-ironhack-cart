fc0b7cb88766b0788ca3bd18edae5a9ceecfa4f6
michaelporreca <michaelporreca@gmail.com>
2021-08-24 14:04:23 -0700

    completed iterations 2-3

--- js/index.js
+++ js/index.js
@@ -1,36 +1,41 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  const price = product.querySelector('.price span').textContent;
-  const quantity = product.querySelector('.quantity > input').valueAsNumber;
-  let subtotal = price * quantity;
-  let elementSubtotal = document.querySelector('.subtotal span');
-  elementSubtotal.innerHTML = subtotal;
+  // console.log('Calculating subtotal, yey!');
+
+  const priceElement = product.querySelector('.price span');
+  const quantityElement = product.querySelector('.quantity input');
+
+  const price = priceElement.innerText;
+  const quantity = quantityElement.value;
+
+  const subtotalElement = product.querySelector('.subtotal span');
+  const subtotal = price * quantity;
+
+  subtotalElement.innerText = subtotal;
+
   return subtotal;
 }
 
-// let price = product.querySelector('.price span').textContent;
-// let quanity = product.querySelector('.quantity > input').valueAsNumber; // let quanity = product.querySelector('.quantity > input').valueAsNumber;
-// let subtotal = price * quanity; // parseInt(unitPrice.innerHTML) * parseInt(quantity.value);
-
-// let elementSubtotal = document.querySelector('.subtotal span');
-// elementSubtotal.innerHTML = subtotal;
-
-// return subtotal;
-
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
+  // // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  const multipleProducts = document.querySelectorAll('.product');
+  let total = 0;
+  for (let product of multipleProducts) {
+    total += updateSubtotal(product);
+  }
+    // ITERATION 3
+  const totalPrice = document.querySelector('#total-value span');
+  totalPrice.innerHTML = total;
+}
   // ITERATION 3
   //... your code goes here
-}
 
 // ITERATION 4
 
