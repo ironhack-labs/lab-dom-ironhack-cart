06ffbae824b5cee9aef651f4ac541ce0d1e06317
alejandro <ftvalex@hotmail.com>
2021-08-30 20:49:10 +0100

    hasta la iteracion 4 con jaume
    Hecho hasta la iteracion 4 con jaume

--- js/index.js
+++ js/index.js
@@ -1,35 +1,42 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price > span').innerText;
+  const quantity = product.querySelector('.quantity > input').value;
+  const subTotal = price*quantity;
+  let subTotalText = product.querySelector('.subtotal > span');
+  subTotalText.innerHTML = subTotal;
 }
+
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
-
+  const allProducts = document.getElementsByClassName('product');
+  for (product of allProducts) {
+    updateSubtotal(product)
+  }
+  }
   // ITERATION 3
-  //... your code goes here
-}
+  let totalValue = document.querySelector('#total-value > span');
+  const subTotals = document.querySelectorAll('.subtotal > span');
+  let totalSum = 0;
+  for (subTotal of subTotals) {
+    totalSum += Number(subTotal.innerText);
+  }
+  totalValue.innerHTML = totalSum;
+
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+  
+  
+  const productElement = target.parentElement.parentElement;
+  const tableBodyElement = productElement.parentElement;
+  tableBodyElement.removeChild(productElement);
+ }
 
 // ITERATION 5
-
 function createProduct() {
   //... your code goes here
 }
