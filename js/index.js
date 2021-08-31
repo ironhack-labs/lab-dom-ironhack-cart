dc7e05c73d44431ef399b72f690fae61270c3687
andoni.resendiz <andoniresendiz@gmail.com>
2021-08-30 22:06:51 -0500

    Completé las iteraciones 1,2 y 3, pero el test no me está contando la 2 y parte de la 3

--- js/index.js
+++ js/index.js
@@ -2,22 +2,49 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let arrSumTot = []
+  for (let i = 0; i < product.length;i++){
+    const price = product[i].querySelector('.price span');
+    const quantity = product[i].querySelector('.quantity input');
+    const subtotal = product[i].querySelector('.subtotal span');
 
-  //... your code goes here
+    let priceNum = parseInt(price.innerHTML);
+    let quantityNum = parseInt(quantity.value);
+    console.log(subtotal)
+    let subtotalNum = priceNum * quantityNum;
+    subtotal.innerHTML = `${subtotalNum}`
+    arrSumTot.push(subtotalNum)
+
+  }
+  console.log(arrSumTot);
+  return arrSumTot
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
+    // ITERATION 2
+  //... your code goes here
+  let multiProduct = document.getElementsByClassName('product');
+  console.log(multiProduct)
+  //updateSubtotal(multiProduct)
+  /*
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+  */
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
-
+  
   // ITERATION 3
   //... your code goes here
+  let arrResultFinal = updateSubtotal(multiProduct);
+  let sumTotal = 0;
+  for(let i = 0; i < arrResultFinal.length; i++){
+    sumTotal += arrResultFinal[i]
+  }
+  const total = document.querySelector('#total-value span')
+  total.innerHTML = `${sumTotal}`
+  console.log(sumTotal)
 }
 
 // ITERATION 4
@@ -25,7 +52,10 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+
+
   //... your code goes here
+
 }
 
 // ITERATION 5
@@ -37,6 +67,8 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  let removeBtn = document.getElementsByClassName("btn btn-remove")
+  
+   
   //... your code goes here
 });
