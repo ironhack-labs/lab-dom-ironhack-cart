47c0db79ab1a51fa20bb4c23dcedd966748d3479
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-09-29 12:12:58 +0200

    Iteration 2 3

--- js/index.js
+++ js/index.js
@@ -1,42 +1,57 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+function updateSubtotal(productRow) {
+  //console.log('Calculating subtotal, yey!');
 
+  
+  //GET REFERENCE TO ELEMENT IN THE DOM:
 
-  const priceElm = document.querySelector(".price span"); // to do: search inside "product" row, instead of "document"
-  //const price = priceElm.innerText;
+  const priceElm = productRow.querySelector(".price span"); // to do: search inside "product" row, instead of "document"
+  const quantityElm= productRow.querySelector(".quantity input");
+  const subTotalElm = productRow.querySelector(".subtotal span");
+
+  //CALCULATE:
+  
+  
   const price = parseFloat(priceElm.innerText)
-  console.log(price);
-
-  const quantityElm= document.querySelector(".quantity input");
-  //const quantity = quantityElm.value
+  
   const quantity = parseFloat(quantityElm.value)
-  console.log(quantity);
 
   const subtotal = price * quantity;
-  //console.log(subtotal);
 
-  const subTotalElm = document.querySelector(".subtotal span");
+  //UPDATE DOM:
+
   subTotalElm.innerHTML = subtotal;
 
   return subtotal;
 
 
 }
-
+// ITERATION 2
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  let totalPrice = 0;
+  
+  
+  // step1: get all products
+
+  const productList= document.querySelectorAll(".product");
+
+  //step2 :Iterate through the list of products and call UpdateSubtotal()
+  
+  productList.forEach(function(element) {
+    const subTotalForthisProduct = updateSubtotal(element);
+
+    totalPrice = totalPrice + subTotalForthisProduct;    
+  });
+  console.log(totalPrice)
+
 
   // ITERATION 3
-  //... your code goes here
+
+  const totalElm = document.querySelector("#total-value span");
+  totalElm.innerHTML = totalPrice
+
 }
 
 // ITERATION 4
