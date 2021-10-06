d283021a41e724ac1101c5d778eb60d5d811d420
Philipp Kastl <picastle@posteo.de>
2021-10-06 19:06:00 +0200

    add iteration 5, after create remove is not working

--- js/index.js
+++ js/index.js
@@ -40,13 +40,28 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   productToRemove = document.querySelector('.product');
-  this.parentNode.parentNode.innerHTML = ``;
+  target.parentNode.parentNode.parentNode.removeChild(productToRemove);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const productName = document.getElementById('create-product-name');
+  const productPrice = document.getElementById('create-product-price');
+  const productRow = document.querySelector('tbody');
+  productRow.innerHTML += `<tr class="product">
+  <td class="name">
+    <span>${productName.value}</span>
+  </td>
+  <td class="price">$<span>${productPrice.value}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>`;
 }
 
 window.addEventListener('load', () => {
@@ -56,4 +71,6 @@ window.addEventListener('load', () => {
   removeSubtotalBtn.forEach((btn) => {
     btn.addEventListener('click', removeProduct);
   });
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
 });
