197f179d49fbfbe0a9173906d9cddc302498d37b
Joana Brito <jobritomar@gmail.com>
2021-10-07 13:48:00 +0100

    interation 1,2 and 3. Couldnt do the bonus.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,50 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price=product.querySelector(`.price span`)
+  const quantity=product.querySelector(`.quantity input`)
+  const priceValue=parseInt(price.innerHTML);
+  const quantifyValue=parseInt(quantity.value);
+
+
+
+  const subtotalValue= priceValue*quantifyValue;
+
+
+  const subtotal = product.querySelector(`.subtotal span`)
+
+  subtotal.textContent = subtotalValue
+  return subtotalValue
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
+  
   updateSubtotal(singleProduct);
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  
+  const allProduct=Array.from(document.querySelectorAll('.product'))
+
+
+  const subtotalAll = allProduct.map(product => updateSubtotal(product))
+
 
   // ITERATION 3
-  //... your code goes here
+
+  const total= document.querySelector('#total-value span')
+  
+  const totalvalue= subtotalAll.reduce((acc,cv) => acc+cv)
+
+  console.log(totalvalue)
+  total.innerText=totalvalue
+
 }
 
 // ITERATION 4
