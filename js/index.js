092c4b8f2e03fcbe8c946bd90e61d7173810b07a
RaselJam <Rasel.jam.h@gmail.com>
2021-10-22 15:17:05 +0200

    saving Proggres

--- js/index.js
+++ js/index.js
@@ -7,22 +7,23 @@ function updateSubtotal(product) {
   let price = product.querySelector('.price span').innerHTML;
   let quantity = product.querySelector('#inputQuantity').value;
   let subtotal = price * quantity;
-  let subElm = document.querySelector('.subtotal span');
+  let subElm = product.querySelector('.subtotal span');
   subElm.innerHTML = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  let totalTopay = 0;
+  let productlistElms = [...document.querySelectorAll('.product')];
+  productlistElms.forEach((productTr) => {
+    totalTopay += updateSubtotal(productTr);
+  });
+  document.querySelector('#total-value span').innerHTML = totalTopay;
 }
 
 // ITERATION 4
@@ -31,6 +32,9 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  let parentTr = target.closest('tr');
+  console.log({ parentTr });
+  document.querySelector('#cart').remove(parentTr);
 }
 
 // ITERATION 5
@@ -42,6 +46,10 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  let removeButtons = [...document.querySelectorAll('.btn-remove')];
+  removeButtons.forEach((btn) => {
+    btn.addEventListener('click', removeProduct);
+  });
 
   //... your code goes here
 });
