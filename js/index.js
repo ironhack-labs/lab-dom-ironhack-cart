c0dd40243a00932a4ec8c23922c3c189945a3fca
Fernando Cardona <fernando.cardona@the-lion.com>
2021-10-23 19:18:48 +0200

    bonus part Creo k ok

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function calculateAll() {
   // ITERATION 2
   // Para Coger a todos los elementos 'product' utilizamos document.querySelectorAll(''); (si utilizamos querySelector('') solo llamaria al primero que se encuentre).
   let products = document.querySelectorAll('.product'); 
-  // console.log(products)
+ console.log(products)
 
   //Recorremos el DOM con el loop 'forEach' para volver a ejecutar la funcion anterior 'updateSubtotal' y actualizar el 'subtotal' en todos los elementos 'product' que hemos recogido en la variable 'Products'. 
   products.forEach(eachProduct => {
@@ -50,7 +50,7 @@ function calculateAll() {
   let total=0;
   //Volvemos a recorrer con un 'foEach' los elementos almacenados en products ya actualizados sus subtotales en la variable 'products' esta vez para sumar los subtotales y almacenar el resultado en la variable 'total' que hemos creado.
   products.forEach(eachProduct => {
-    total+=parseFloat(eachProduct.querySelector('.subtotal span').innerHTML);
+    total += parseFloat(eachProduct.querySelector('.subtotal span').innerHTML);
   });
   //Seteamos (agregamos) el resultado de la suma de los products 
   document.querySelector('#total-value span').innerHTML=total;
@@ -64,21 +64,85 @@ function calculateAll() {
 
 // ITERATION 4
 
-function removeProduct(event) {
+function removeProduct(event) { 
+  //console.log('estoy aqui'); 
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+
+  
   //... your code goes here
+  //4.3 Usamos target para reconocer en que btnRemove tenemos el click y borramos el producto completo
+  target.closest(".product").remove();
+
+  //4.4 Volvemos a llamar a la funcion calculate all para actualizar el total de los productos  
+  calculateAll();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
+  console.log('vamos a crear un producto nuevo');
   //... your code goes here
+  //5.1 Clonamos el contenedor Products dentro del tbody
+  let product = document.querySelector('.product'); 
+  let clon = product.cloneNode(true);
+  let tbody = document.querySelector('tbody'); 
+  console.log(tbody);
+  tbody.appendChild(clon);
+  //5.2 Obtenemos los datos del nuevo producto nombre y lo agregamos al nuevo producto
+  
+  let newProductName = document.querySelector('.create-product input').value;
+  let newProductElement = document.querySelectorAll('.product .name');
+  let lastElement = newProductElement[newProductElement.length- 1];
+  lastElement.innerHTML = newProductName;
+  //5.2 Obtenemos los datos del nuevo producto precio y lo agregamos al nuevo producto
+  const price = product.querySelector('.price span');
+  let newProductPrice = document.querySelectorAll('.create-product input')[1].value;
+  let newProductElementPrice = document.querySelectorAll('.product .price span');
+  let lastElementPrice = newProductElementPrice[newProductElementPrice.length- 1];
+  lastElementPrice.innerHTML = newProductPrice;
+  console.log(lastElementPrice);
+  //5.2 acemos un reset de los inputs de create product
+  document.querySelector('.create-product input').value = "";
+  document.querySelectorAll('.create-product input')[1].value = 0;
+  
+  //5.3 volvemos hacer un forEach para capturar los botonesRemove del nuevo elemento
+  let botonesRemove = document.querySelectorAll('.product .btn-remove');
+  botonesRemove.forEach(eachBtnRemove => {
+    //Agregamos 'listener' para escuchar un click en cualquiera de los botones 'remove'
+    eachBtnRemove.addEventListener("click", removeProduct);
+    
+  });
+  //console.log('botonesRemove');
+
+
+  //5.10 Volvemos a llamar a la funcion calculate all para actualizar el total de los productos 
+  calculateAll();
+  
+  
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  //4.1 creamos una variable 'botonesRemove' donde almacenar todos nuestros botones 'remove' y con comprovamos que captura todo los botones remove del DOM Con un console.log
+  let botonesRemove = document.querySelectorAll('.product .btn-remove');
+  //console.log(botonesRemove);
+  //4.2 Con el loop for each recoremos todoas estos botones remove a la espear de un click con el metodo 'addEventListener' una vez ha hecho click se dispara la funcion 'removeProduct'
+  botonesRemove.forEach(eachBtnRemove => {
+    //Agregamos 'listener' para escuchar un click en cualquiera de los botones 'remove'
+    eachBtnRemove.addEventListener("click", removeProduct);
+    
+  });
+ 
 
   //... your code goes here
+  //creamos una variable 'createProduct' donde almacenar el boton 'createProduct'
+  const cloneProduct = document.getElementById('create');
+  //Comprovamos que captura todo los botones remove del DOM Con un console.log
+  //console.log(createProduct);
+  cloneProduct.addEventListener('click', createProduct);
+  
+
 });
