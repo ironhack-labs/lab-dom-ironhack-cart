321aebc6f4c1c66afefb8bcfdd129c8e41fa8c7b
Alex Lopez <alexlopezz21@gmail.com>
2021-10-24 22:29:38 +0200

    done

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span').textContent
-  const quantity = product.querySelector('.quantity input').value
+  const price = product.querySelectorAll('.price span')[0].textContent
+  const quantity = product.querySelectorAll('.quantity input')[0].value
 
 
     const sum = price*quantity
-    const subtotal = product.querySelector('.subtotal span')
+    const subtotal = product.querySelectorAll('.subtotal span')[0]
 
     subtotal.textContent = sum
   return sum
@@ -15,20 +15,31 @@ function updateSubtotal(product) {
 
 }
 
-
 function calculateAll() {
 
-    
 
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  /* const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let products = 0;
+  let allItems = document.querySelectorAll('#cart .product');
+  allItems.forEach(item => {
+    products += updateSubtotal(item)
+    
+  });
 
   // ITERATION 3
   //... your code goes here
+  document.querySelector("#total-value span").innerHTML = products;
+  
+
+
+  /* At the end of the calculateAll() function, reuse the total value you just calculated in the previous iteration 
+  and update the corresponding DOM element. Calculate the total price for the products in your cart by summing all of the subtotals 
+  returned by updateSubtotal() when it was called with each product. */
 }
 
 // ITERATION 4
