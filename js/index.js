c9a16445552205e946f988a0a330321a69ed3721
mmonereo <mauro.monereo@gmail.com>
2021-10-24 19:22:59 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-// ITERATION 1
+let productTemplateGlobal = document.querySelector('.product');
 
+// ITERATION 1
 function updateSubtotal(product) {
 
-	console.log('Calculating subtotal, yey!');
+
 
 	const price = product.querySelector(".price span").innerHTML;
 	const quantity = product.querySelector(".quantity input").value;
@@ -39,21 +40,64 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+	const target = event.currentTarget;
+	let targetProduct = target.parentNode.parentNode
+	let targetProductParent = targetProduct.parentNode;
+	targetProductParent.removeChild(targetProduct);
+	calculateAll();
 }
 
 // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
+function createProduct(event) {
+	const target = event.currentTarget.parentNode.parentNode;
+
+	const productName = target.querySelectorAll('td input')[0].value;
+	const productPrice = addZeroes(target.querySelectorAll('td input')[1].value);
+	if (!productName){
+		return;
+	}
+
+	let newProduct = productTemplateGlobal.cloneNode(true);
+	newProduct.childNodes[1].innerHTML = `<span>${productName}</span`;
+	newProduct.childNodes[3].innerHTML = `$<span>${productPrice}</span`;
+	const cart = document.querySelector('tbody');
+	cart.appendChild(newProduct);
+	let newProductbtn = newProduct.querySelector('.btn-remove');
+	
+	newProductbtn.onclick = removeProduct;
+	clearInputs(target);
+}
+
+function addZeroes(num) {
+	let value = Number(num);
+	let res = num.split(".");
+
+	if (res.length === 1 || res[1].length > 3){
+		value = value.toFixed(2);
+	}
+
+	return String(value);
+}
+
+function clearInputs(inputs){
+	inputs.querySelectorAll('td input')[0].value = "";
+	inputs.querySelectorAll('td input')[1].value = "";
 }
 
 window.addEventListener('load', () => {
 	const calculatePricesBtn = document.getElementById('calculate');
 	calculatePricesBtn.addEventListener('click', calculateAll);
 
-	const calculaRemoveBtn = document.querySelectorAll('.btn-remove');
-	calculaRemoveBtn.forEach(addEventListener('click', removeProduct));
+	const removeBtn = document.querySelectorAll('.btn-remove');
+	removeBtn.forEach( function (eachBtn) {
+		eachBtn.onclick = removeProduct;
+	});
+
+	const createBtn = document.querySelectorAll('#create');
+	createBtn.forEach(function (eachBtn) {
+		eachBtn.onclick = createProduct;
+	});
+	
 });
