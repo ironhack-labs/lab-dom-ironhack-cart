cae3a8e89e029d21b57f63c07f6ca489d31b50c9
besaga <bsgallardo93@gmail.com>
2021-10-24 22:10:45 +0200

    funcion1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span').innerHTML;
-  const quantity = product.querySelector('.quantity input').value;
+  const price = product.querySelector('.price span').innerHTML;//declaro price product es la clase del tr,.queryselector lo coge del html, entro en la clase .price de la etiqueta
+  //span y traigo el valor que hay entre esta ultima etiqeuta
+  const quantity = product.querySelector('.quantity input').value;//declaro quantity entro en la clase product .querySelexctor lo coge del html,.quantity y accedo al value que es lo que quiero cambiar con .value, entro con .algo
+  //como los objetos
 
-  const subtotal = price * quantity
+  const subtotal = price * quantity//creo una variable subtotal price * quantity
   
-  product.querySelector('.subtotal').innerHTML = '$' + subtotal
+  product.querySelector('.subtotal').innerHTML = '$' + subtotal// de la clase product coge lo escrito con el queryselector de la clase .subtotal que me dara los $ que elo pongo como un string y el subtotal que es el precio de la cantidad por el numero del producto que elijamos
 
-  return subtotal
+  return subtotal//devuelve subtotal
 }
 
 
@@ -17,8 +18,8 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const singlePGITCLroduct = document.querySelector('.product');
+  updateSubtotal(product);
   // end of test
 
   // ITERATION 2
