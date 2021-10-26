cc83be7344f7e55ca416c60656dedddb4cf68a99
Greg Munro <gregmunro90@gmail.com>
2021-10-26 19:56:39 +0200

    iterations without bonus solved

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input').value;
+
+  console.log(price, quantity);
+
+  const subtotal = price.innerHTML * quantity;
+
+  console.log(subtotal);
+
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+
+  return subtotal;
 
   //... your code goes here
 }
@@ -9,15 +21,20 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  let total = 0;
+  allProducts.forEach((product) => (total += updateSubtotal(product)));
 
   // ITERATION 3
   //... your code goes here
+
+  return (document.querySelector('#total-value span').innerHTML = total);
 }
 
 // ITERATION 4
