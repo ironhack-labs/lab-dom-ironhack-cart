2c5c9c8aaf445595d9d47f647c644e59ea705bef
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-26 19:55:19 +0200

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = document.querySelector ('.price span').innerText;
+  const quantity = document.querySelector ('.quantity input').value;
+  const calculateSubtotal = price * quantity;
+  const subtotal = document.querySelector('.subtotal span').innerText = calculateSubtotal
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
+  
 
   // ITERATION 2
   //... your code goes here
