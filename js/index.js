2f6db55afaf7321a5c13cbb01be3fd81548e95b1
Kostalexis <kostalexis.hosting@gmail.com>
2021-10-26 11:54:16 +0200

    subtotal and total works

--- js/index.js
+++ js/index.js
@@ -2,15 +2,46 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  //this takes only one product. we have to make a loop to calculateAll() function
 
   //... your code goes here
+  //create two variables (we suggest naming them price and quantity) 
+  //and use your just gained DOM manipulation skills to GET DOM elements that hold price and quantity. 
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerText = parseFloat(price.innerText) * quantity
+  return parseInt(price.innerText) * quantity;
+
+  
+  //console.log(subtotal)
+  // console.log(price)
+  // console.log(quantity)
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  var elements = document.querySelectorAll('.product');
+  /*the element is now an array. So.... we call the updateSubtotal for every element
+  of the array*/
+  console.log(elements)
+  let total =0;
+  for(element of elements){
+    total += updateSubtotal(element)
+    
+  }
+    //now we have to calculate the total
+    //.log(total)
+    const totals = document.querySelector('#total-value span');
+    totals.innerText = total
+    
+
+  
+
   // end of test
 
   // ITERATION 2
