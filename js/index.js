f60ee3a7c0a5b6a91a8402b98693fe04fe73709f
Raquel <raqhidcor@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.name Raquelraqhidcor@gmail.com>
2021-10-26 23:15:58 +0200

    Iteration 2 and iteration 3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  const price = document.querySelector ('.price span').innerText;
-  const quantity = document.querySelector ('.quantity input').value;
-  const calculateSubtotal = price * quantity;
-  const subtotal = document.querySelector('.subtotal span').innerText = calculateSubtotal
+  let price = product.querySelector('.price span').innerText;
+  let quantity = product.querySelector('.quantity input').value;
+  let subtotalPrice = (price) * (quantity);
+  subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = subtotalPrice;
+  return (subtotalPrice);
 }
 
+// ITERATION 2
 function calculateAll() {
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  
+  let total = 0;
+  let products = document.getElementsByClassName('product');
+  for (let i=0; i<products.length; i++){
+    total += updateSubtotal(products[i]);
+  }
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 3 
 
-  // ITERATION 3
-  //... your code goes here
+  let totalCart = document.querySelector('#total-value span');
+  totalCart.innerHTML = total;
 }
+
 
 // ITERATION 4
 
