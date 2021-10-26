91b6f985a12e71b98c45adf40d7d4f84ec419c86
Federico Altava Redondo <fedealtava@gmail.com>
2021-10-26 23:09:09 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,24 +1,70 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
+  let unitPrice = product.querySelector('.price span');
+  let quantity = product.querySelector('.quantity input');
+  let subtotal = Number(unitPrice.innerHTML) * Number(quantity.value);
+  let elementSubtotal = product.querySelector('.subtotal span');
+  elementSubtotal.innerHTML = subtotal;
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  return subtotal;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+// ITERATION 2
+  //... your code goes here
+  function calculateAll() {
+    let allProducts = document.getElementsByClassName('product');
+    for (let product of allProducts) {
+      updateSubtotal(product)
+     }
+
+  // ITERATION 3
+
+  let total = 0;
+    for (product of allProducts) {
+      total += updateSubtotal(product); }
+
+      return (document.querySelector('#total-value span').innerText = total);
+
+    console.log(total)
+  }
+ 
+
+ 
 
   // ITERATION 2
   //... your code goes here
+//function calculateAll() {
+  //let totalPrice = 0;
+ // let products = document.querySelectorAll('.product');
+ // for (let i = 0; i<products.length; i++) {
+   // let subtotal = updateSubtotal(products[i]);
+  //  totalPrice = totalPrice + subtotal;
+ // }
+  //return totalPrice
+//}
+ 
+
 
   // ITERATION 3
-  //... your code goes here
-}
+
+  //document.querySelector('#total-value span').innerText = calculateAll()
+
+  //let total = 0;
+    //for (product of allProducts) {
+      //total += updateSubtotal(product); }
+
+      //return (document.querySelector('#total-value span').innerText = total);
+
+    //console.log(total)
+ // }
+ 
+ // let elementTotalValue = document.querySelector('#total-value span');
+  //elementTotalValue.innerText = calculateAll();
+///
+
+//}
+
+
 
 // ITERATION 4
 
