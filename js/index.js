418e2174ef6fa6053d962f7fea3f675af84ebe10
agrimald2 <agrimaldopci18@gmail.com>
2021-10-26 12:37:30 +0200

    Olena and Alonso - Done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,65 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+    // Price and Quantity Variables
+    let price = product.querySelector('.price span').innerText;
+    let quantity = product.querySelector('.quantity input').value;
 
-  //... your code goes here
+    // Multiply those 2 variable to get the subtotal
+    let subtotal = price * quantity;
+
+    // Call the element where the subtotal should be
+    let subttotalString = product.querySelector('.subtotal span');
+
+    // Redefine his innerHTML value
+    subttotalString.innerHTML = subtotal;
+
+    //We made it return a value so then we could use it to calculate the total
+    return subtotal;
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+    // ITERATION 2
+    // Call every single product using the ClassTag
+    let products = document.getElementsByClassName("product");
 
-  // ITERATION 2
-  //... your code goes here
+    //A for with the function and each product
+    for (i = 0; i < products.length; i++) {
+        updateSubtotal(products[i]);
+    };
 
-  // ITERATION 3
-  //... your code goes here
+    // ITERATION 3
+    // Declare a subtotal variable
+    let subtotal = 0;
+
+    // With the returning value of 'subtotal' function, we SUM it to the new subtotal variable
+    for (i = 0; i < products.length; i++) {
+        subtotal = updateSubtotal(products[i]) + subtotal;
+    }
+
+    // Redifine his innerHTML value
+    let totalString = document.getElementById('total-value').querySelector('span');
+    totalString.innerHTML = subtotal;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
+    //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+    //... your code goes here
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+    const calculatePricesBtn = document.getElementById('calculate');
+    calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+    //... your code goes here
 });
