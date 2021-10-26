8e74ff851d43496e10ab6034eeaa0882c396b29c
Jones Durumsky <jonesdurumsky@gmail.com>
2021-10-26 22:07:52 +0200

    iterations 1-4

--- js/index.js
+++ js/index.js
@@ -1,31 +1,55 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //console.log('Calculating subtotal, yey!');
+  //create variables for the price and quantity
+  const price = product.querySelector('.price span');
+  //console.log(price)
+  const quantity = product.querySelector('.quantity input').value;
+  //console.log(price.innerText);
+  //console.log(quantity);
+  //create a variable to store the updated subtotal (quantity*price)
+  const subTotal = Number(quantity * price.innerText);
+  //console.log(subTotal)
+  //updating the inner text of the subtotal
+  product.querySelector('.subtotal span').innerText = subTotal;
 
-  //... your code goes here
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  //console.log(allProducts)
+  for (let product of allProducts) {
+    updateSubtotal(product);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  //Store the sum of all the subtotals in a variable
+  let sumAllSubTotals = 0;
+  for (let product of allProducts) {
+    sumAllSubTotals += updateSubtotal(product);
+  }
+  document.querySelector('#total-value span').innerText = sumAllSubTotals;
+  //change the inner of the span with the total value with the variable
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget.parentNode.parentNode;
+  const parent = event.currentTarget.parentNode.parentNode.parentNode;
+  console.log(parent)
   console.log('The target in remove is:', target);
   //... your code goes here
+  parent.removeChild(target)
 }
 
 // ITERATION 5
@@ -36,7 +60,18 @@ function createProduct() {
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
+  //console.log(calculatePricesBtn)
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const removeBtns = document.querySelectorAll('.btn.btn-remove');
+  //console.log(removeBtns)
+  removeBtns.forEach(function (btn){
+    btn.addEventListener('click', removeProduct)
+  })
+  //WHY does this not work??
+  /*
+  for (let btn in removeBtns){
+    btn.addEventListener('click', removeProduct)
+  }
+  */
 });
+
