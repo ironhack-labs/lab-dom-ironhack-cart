398e5a639f78a57d9d47fe69f61dfd7e6a2d1d66
Johannes von Kessel <johannes.v.kessel@web.de>
2021-10-26 14:29:34 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -3,23 +3,48 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let price = document.querySelector('.price span').innerText
+  let quan = document.querySelector('.quantity input').value
+  let subtotal = price * quan;
+
+
+document.querySelector(".subtotal span").innerText = subtotal
+
+return subtotal
 }
+// console.log(quan)
+
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
+  const allproducts = document.querySelectorAll('.product');
+  let total = 0;
+  for (let product of allproducts) {
+  total += updateSubtotal(product)
+  
+  console.log(product)
+  
+  
+  // document.querySelector("#total-value span").innerText = total
+
+
+  }
+
+
   //... your code goes here
 
   // ITERATION 3
   //... your code goes here
 }
-
+calculateAll()
 // ITERATION 4
 
 function removeProduct(event) {
