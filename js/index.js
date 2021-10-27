14f94fd5c7ac38e377326664b82eeb4e30cf4019
Tsaikkari <kirsi.trospe@gmail.com>
2021-10-27 16:51:24 +0200

    create product

--- js/index.js
+++ js/index.js
@@ -37,26 +37,39 @@ function removeProduct(event) {
   const parentNode = target.parentNode
   parentNode.removeChild(target)
   calculateAll()
+  // TODO: this only removes hard-coded products
 }
-
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
-  const productName = document.querySelector('.create-product input').value
+  const productName = document.querySelector(".create-product input[type='text']").value
   if (!productName) {
     return ''
   }
-  console.log(productName)
-  productName.value = ''
-  const price = document.querySelectorAll('create-product input')[1].value
-  price.value = 0
-
-  const table = document.querySelector('tbody')
-  const row = document.createElement('tr')
-  const col = document.createElement('td')
-  const span = document.createElement('span')
+  const price = document.querySelector(".create-product input[type='number']").value
   
+  let table = document.querySelector('#cart tbody');
+
+  let createdProduct = document.createElement('tr')
+  createdProduct.innerHTML = `<tr class="product">
+      <td class="name">
+        <span>${productName}</span>
+      </td>
+      <td class="price">$<span>${price}</span></td>
+      <td class="quantity">
+        <input type="number" value="0" min="0" placeholder="Quantity" />
+      </td>
+      <td class="subtotal">$<span>0</span></td>
+      <td class="action">
+        <button class="btn btn-remove">Remove</button>
+      </td>
+    </tr>` 
+  
+  table.appendChild(createdProduct)
+
+  document.querySelector(".create-product input[type='text']").value = ''
+  document.querySelector(".create-product input[type='number']").value = 0
 }
 
 window.addEventListener('load', () => {
