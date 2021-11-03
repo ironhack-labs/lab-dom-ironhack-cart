70256c897b4ffdf0f3c514405c88e7e6f18da900
Javier Herrera <jaaah0010@gmail.com>
2021-11-02 21:28:09 -0600

    First commit

--- js/index.js
+++ js/index.js
@@ -1,38 +1,92 @@
 // ITERATION 1
 
+const { ConnectionEmittedEvents } = require("puppeteer");
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  const quantity = product.querySelector('.quantity input').innerHTML;
+  const price = product.querySelector('.price span').innerHTML;
 
-  //... your code goes here
+  const quantityValue = quantity.innerHTML;
+  const priceValue = price.innerHTML;
+
+  const calcSubtotal = quantityValue * priceValue;
+
+  const displaySubTotal = product.querySelector('.subtotal span');
+
+  displaySubTotal.innerHTML = calcSubtotal;
+
+  return calcSubtotal;
+  
+
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
+function calculateAll() {
+  
   // ITERATION 2
-  //... your code goes here
+  const productsTotality = document.getElementsByClassName('.product')
+  
+  return updateSubtotal(productsTotality);
+
+ 
+
 
   // ITERATION 3
   //... your code goes here
+
+
+  const totalPrice = document.querySelector(' #total-value span')
+
+  let totalPrice = 0;
+  for(let i = 0; i < productsTotality.length){
+    
+    return 
+  }
+  
+
 }
+
+
+
+
+
+
+
+
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  
   //... your code goes here
+  const $removeButton = document.querySelectorAll('.btn btn-remove');
+  removeButton.addEventListener('click, removeProduct()'); 
+
+  for(){
+
+  }
+
 }
+
+
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
 }
+
+
+
+
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
