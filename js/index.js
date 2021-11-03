814667b35e3396826812c12018facbb88a6ac911
KuraiTsukino <sandym3000@hotmail.com>
2021-11-02 23:47:40 -0600

    res0

--- js/index.js
+++ js/index.js
@@ -1,20 +1,42 @@
 // ITERATION 1
+// Step 1. Crear dos variables price y quantity
+
+const price = product.querySelector('.price span');
+const quantity = product.querySelector ('.quantity input');
+
+// Step 2. Extraer los valores de price y quantity
+ 
+const priceVal = number(price.textContent);
+const quantityVal = quantity.value.textContent;
+
+// Step 3. Calculate subtotal price.
+
+const subtotalPrice = priceVal * quantityVal
+
+// Step 4. Unir al DOM el subtotalPrice
+
+const subTotalDom = product.querySelector(".subtotal span");
+  subTotalDom.innerHTML = subTotal;
+  subTotalDom.appendChild(subTotal);
+  return subTotal
+
+// Step 5. Poner el subtotal en el elemento del DOM
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const singleProduct = document.querySelector ('.product');
+    return updateSubtotal (singleProduct);
 }
+  
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
+ 
 
-  // ITERATION 2
-  //... your code goes here
+  // Iteración 2. Llama la función updateSubtotal con cada tr.product DOM en el table#cart.
+  
 
   // ITERATION 3
   //... your code goes here
