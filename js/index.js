da2537ffcef3cd64e0f00f7d43e6e27748e21ed2
Sebas Granara <sebastian.granara@gmail.com>
2021-11-04 11:23:01 +0100

    entrega parcial hasta Iteration 3

--- js/index.js
+++ js/index.js
@@ -3,21 +3,40 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let quantity = parseInt(
+    product.querySelector('.quantity').childNodes[1].value
+  );
+  let price = parseFloat(
+    product.querySelector('.price').childNodes[1].innerText
+  );
+
+  var subtotal = quantity * price
+
+  product.querySelector('.subtotal').childNodes[1].innerText = 
+  subtotal.toFixed(2);
+
+return subtotal
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const productList = document.querySelectorAll('.product')
+  
+  let total = 0;
+  productList.forEach((product) => {
+    total += updateSubtotal(product)
+  });
 
   // ITERATION 3
-  //... your code goes here
+  document.getElementById('total-value').childNodes[1].innerText =
+  total.toFixed(2)
 }
 
 // ITERATION 4
