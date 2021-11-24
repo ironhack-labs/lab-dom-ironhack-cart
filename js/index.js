286bf4568da9167c2ea7f9f8b78f487eb664735a
David Blanco <davidblanco@Davids-MacBook-Pro.local>
2021-11-24 20:20:39 +0100

    Avance

--- js/index.js
+++ js/index.js
@@ -15,36 +15,26 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
   const total = document.querySelector("#total-value span")
   const allSubTotals = [...document.querySelectorAll(".product")];
   //alert("Calculate Prices clicked!")
-  console.log(allSubTotals)
+  //console.log(allSubTotals)
   const totalValue = allSubTotals
     .map(product => updateSubtotal(product))
     .reduce((acc, price) => acc + price)
 
   total.innerText = String(totalValue)
-  console.log(totalValue)
-
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  //console.log('The target in remove is:', target);
   //... your code goes here
   const row = target.closest("tr")
   row?.parentNode.removeChild(row)
+  calculateAll()
 }
 
 // ITERATION 5
@@ -57,13 +47,10 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  const removeButtons = document.querySelectorAll(".btn.btn-remove")
+  const removeButtons = [...document.querySelectorAll(".btn.btn-remove")]
   //console.log(removeButtons[0], typeof removeButtons)
-  removeButtons[0].forEach(btn => {
-    //console.log(btn)
-    btn.addEventListener("click", (e) =>
-      removeProduct(e)
-    )
+  removeButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => removeProduct(e))
   }
   )
 
