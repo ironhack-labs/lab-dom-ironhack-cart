9fd49cd5ca1492ea140b3e0c01566087e2551803
Felipe Leite Mantovani <mantovani.devilz@gmail.com>
2021-11-30 22:39:38 -0300

    Fiz algo

--- js/index.js
+++ js/index.js
@@ -2,16 +2,18 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = document.querySelector('.price span');
+  const quantity = document.querySelector('.quantity input')
+  const subtotal = document.querySelector('.subtotal span') 
+  const calculations = quantity.value * price.innerText;
+  subtotal.innerText = (`${calculations}`)
 }
 
+
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const allProducts = document.getElementsByClassName('product');
+  const arrayProducts = [...allProducts]
 
   // ITERATION 2
   //... your code goes here
@@ -19,6 +21,8 @@ function calculateAll() {
   // ITERATION 3
   //... your code goes here
 }
+const allProducts = document.getElementsByClassName('product');
+const arrayProducts = [...allProducts]
 
 // ITERATION 4
 
