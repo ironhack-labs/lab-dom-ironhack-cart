c2e907e39de8e38e248ed507f07f234e2b28df99
Sergio <juliwood1980@gmail.com>
2021-12-02 23:08:02 -0300

    iteracao de 1 a 4

--- js/index.js
+++ js/index.js
@@ -1,31 +1,52 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
 
-  //... your code goes here
+  const priceContent = Number(price.textContent);
+  const quantityContent = Number(quantity.value); //aqui vai pegar o valor do input
+
+  const subtotalContent = priceContent * quantityContent;   //cálculo do subtotal do produto
+
+  //colocar esse subtotalContent no campo exato dele no html (colocar esse campo/local numa variavel)
+  const subtotal =  product.querySelector ('.subtotal span');
+
+  subtotal.innerText = subtotalContent; //innerText colocando o texto, o cálculo, lá no campo do subtotal 
+
+  return subtotalContent; 
+  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product');    //é um array de vários produtos
+  let totalMultiply = 0;
+  products.forEach(product => {            //método de array
+    totalMultiply += updateSubtotal(product);
+  });
+
 
   // ITERATION 3
-  //... your code goes here
+  let totalValue = document.querySelector('#total-value span')   //onde é dirigido o valor calculado
+  totalValue.innerText = totalMultiply;     //colocando texto da multiplicaçao de valores nesse campo
 }
 
-// ITERATION 4
+// ITERATION 4 
+//botão do remove, lá na window onde já carrega a função quando carrega a página (load), automático
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.target;
+  //preciso deletar o product, que é pai do pai do button, então:
+  const row = target.parentNode.parentNode;
+  row.remove();
+  calculateAll();       //chamo novamente para somar novamente os produtos que nao foram removidos
 }
 
 // ITERATION 5
@@ -38,5 +59,10 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.querySelectorAll('.btn btn-remove');   //é um array de vários botoes possiveis de remove
+  [...removeButtons].forEach(button => {
+    button.addEventListener('click', removeProduct);
+    //button.onclick(removeProduct);           //chamando a funçao que quero ao clicar o botao do remove
+  });
+
 });
