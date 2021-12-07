dad6da7edd632ae80eb6cb25890bc0035d7e1e73
Michael Foley <foley.p.michael@gmail.com>
2021-12-07 11:49:13 +0000

    Some functionality completed. Need to work more.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  let price = document.querySelector('.price span');
+  let quantity = document.querySelector('.quantity input').value;
+  let priceExtract = document.querySelector('.price span').innerText;
+  let subtotal = quantity * priceExtract;
+  document.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  let allProducts = document.querySelectorAll('.product');
+  for (let i = 0; i < allProducts.length; i ++){
+    updateSubtotal(allProducts[i]);
+  }
+  console.log(allProducts);
+  
+  
   // ITERATION 3
   //... your code goes here
 }
