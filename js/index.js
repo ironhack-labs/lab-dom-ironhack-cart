c7fed9e0de1536b226f9d721ffecea0250a1cd49
Rox <you@example.com>
2021-12-16 16:51:37 +0100

    almost

--- js/index.js
+++ js/index.js
@@ -1,24 +1,31 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText
+  const quantity = product.querySelector('.quantity input').value
+  const subtotalPrice = price * quantity
+
+  const subtotal = document.querySelector('.subtotal span')
+  subtotal.innerText = subtotalPrice
+
+  return subtotalPrice
+  
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.getElementsByClassName('product')
 
-  // ITERATION 2
-  //... your code goes here
+  let total = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  for(let i = 0; i < products.length; i++){
+    total += updateSubtotal(products[i])
+  }
+
+  const totalValue = document.querySelector('#total-value span')
+  totalValue.innerText = total
 }
+
 
 // ITERATION 4
 
@@ -28,15 +35,55 @@ function removeProduct(event) {
   //... your code goes here
 }
 
+
+
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const tbody = document.querySelector('#cart tbody')
+
+  const copyOfCopyTr = copyTr.cloneNode(true)
+
+  let productName = document.getElementById('product-name')
+  let productPrice = document.getElementById('product-price')
+
+  copyOfCopyTr.querySelector('.name span').innerText = productName.value
+  copyOfCopyTr.querySelector('.price span').innerText = productPrice.value
+
+  const removeButton = copyOfCopyTr.querySelector('.action button')
+
+  removeButton.addEventListener('click', (event)=>{
+    const button = event.currentTarget;
+    button.parentNode.parentNode.remove()
+  })
+
+  tbody.appendChild(copyOfCopyTr)
+
+  productName.value = ''
+  productName.placeholder = 'Product Name'
+  productPrice.value = 0
 }
 
 window.addEventListener('load', () => {
+
+  const tr = document.querySelector('#cart tbody tr')
+  copyTr = tr.cloneNode(true)
+
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.getElementsByClassName('btn-remove')
+  
+  for(let i = 0; i < removeButtons.length; i++){
+    removeButtons[i].addEventListener('click', (event)=>{
+      const button = event.currentTarget;
+      button.parentNode.parentNode.remove()
+    })
+  }
+
+  const createProductButton = document.getElementById('create')
+
+  createProductButton.addEventListener('click', createProduct)
 });
+
+
