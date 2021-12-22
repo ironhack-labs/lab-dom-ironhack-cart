dff5b398acea8a921527e1e7821c9f73de207651
andregn26 <andregn26@gmail.com>
2021-12-22 21:05:34 +0000

    logic of subtotal for the first product completed

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = document.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+  let subtotalPrice = product.querySelector('.subtotal span')
+  subtotalPrice.textContent = (price * quantity);
+  return subtotalPrice;
 }
 
+
 function calculateAll() {
+  console.log('Calculating calculateAll, yey!');
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
-
+}
   // ITERATION 2
   //... your code goes here
 
+
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
@@ -40,3 +45,4 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
