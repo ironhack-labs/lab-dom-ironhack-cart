f778da1f2237de8abdd12945b9c3aafbc68f0c67
Dominik Kaloc <dominik.kaloc@lego.com>
2022-01-11 16:03:30 +0100

    bonus 5 - only add new line, unable to modify - remoce, calculate then

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
   const price = product.querySelector('.price span').innerText;
-  const  quantity = product.querySelector('.quantity input').value;
+  const quantity = product.querySelector('.quantity input').value;
   let subtotal = product.querySelector('.subtotal span');
   const result = Number(price) * Number(quantity);
   subtotal.innerText = result;
@@ -14,14 +14,14 @@ function calculateAll() {
   // ITERATION 2
   const allProducts = document.querySelectorAll('.product');
   let counter = 0;
-  for(let i = 0; i < allProducts.length; i++){
+  console.log(allProducts.length);
+  for (let i = 0; i < allProducts.length; i++) {
     counter += updateSubtotal(allProducts[i]);
   }
 
   // ITERATION 3
   let total = document.querySelector('#total-value span');
   total.innerHTML = counter;
-  
 }
 
 // ITERATION 4
@@ -29,13 +29,34 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+  target.parentNode.parentNode.remove();
   //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const newProductName = document.querySelector(
+    '.create-product td input:first-child'
+  ).value;
+  const newProductPrice = document.querySelector('#new-price-value').value;
+  const parent = document.getElementById('table-body');
+  const newProduct = document.createElement('tr');
+  newProduct.innerHTML = `<tr class='product'>
+  <td class='name'>
+    <span>${newProductName}</span>
+  </td>
+  <td class='price'>$<span>${newProductPrice}</span></td>
+  <td class='quantity'>
+    <input type='number' value='0' min='0' placeholder="Quantity" />
+  </td>
+  <td class='subtotal'>$<span>0</span></td>
+  <td class='action'>
+    <button class='btn btn-remove'>Remove</button>
+  </td>
+</tr>`;
+  console.log(newProduct);
+  parent.appendChild(newProduct);
 }
 
 window.addEventListener('load', () => {
@@ -44,5 +65,10 @@ window.addEventListener('load', () => {
   //... your code goes here
   const removeButtons = document.getElementsByClassName('btn btn-remove');
   console.log(removeButtons);
-  removeButtons.addEventListener('click', removeProduct(removeButtons));
+  for (let j = 0; j < removeButtons.length; j++) {
+    removeButtons[j].addEventListener('click', removeProduct);
+  }
+
+  const addProduct = document.getElementById('create');
+  addProduct.addEventListener('click', createProduct);
 });
