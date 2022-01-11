d8f72afdb673e3d2aac25e644c4bac22dc2d6b60
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-11 18:23:38 +0100

    half way through Iteration 2

--- js/index.js
+++ js/index.js
@@ -2,29 +2,46 @@
 
 function updateSubtotal(product) {
 //get price and quantity from DOM
-const price = product.querySelector('.price span');
-const priceNumber = price.innerHTML;
-let quantity = document.querySelector('.quantity input');
+const price = product.querySelector('.price span').innerHTML;
 //set quantity to the value attribute
-quantity = quantity.value;
+let quantity = document.querySelector('.quantity input').value;
 //calculate sub total price and store it in a variable
-let subTotalPrice = quantity * priceNumber;
-//show the subtotal: get the object from DOM
-let subTotal = document.querySelector('.subtotal span');
-//set the new value to subTotal
-subTotal.innerHTML = subTotalPrice;
+let subTotal = quantity * price;
+//show the subtotal: get the object from DOM and set new value
+product.querySelector('.subtotal span').innerHTML = subTotal;
+console.log(subTotal);
 return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  let allProducts = document.querySelectorAll('.product');
+  console.log(allProducts);
+  //let total = 0;
+  //for each product on the list, run updateSubtotal
+  for (const product of allProducts) {
+    updateSubtotal(product);
+  }
+
+  
+
+
+
+    //total = total + subTotal;
+  
+
+
+
+
+  
+  
 
   // ITERATION 3
   //... your code goes here
