09348454ad41e5b89477f45553693ada3d4683de
KimSteenBekke <Kim.Steen.Bekke@lego.com>
2022-01-11 23:27:25 +0100

    Lab solved

--- js/index.js
+++ js/index.js
@@ -4,20 +4,35 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  var price = product.querySelector('.price span').innerText;
+  var quantity = product.querySelector('.quantity input').value;
+  let subtotal = product.querySelector('.subtotal span');
+  let result = Number(price) * Number(quantity);
+  subtotal.innerText = result;
+  return result;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+// const singleProduct = document.querySelector('.product');
+ // updateSubtotal(singleProduct);
   // end of test
+  
 
   // ITERATION 2
   //... your code goes here
+  var products = document.getElementsByClassName('product');
+  let counter = 0;
+  console.log(products.length);
+  for (let i = 0; i < products.length; i++) {
+    counter += updateSubtotal(products[i]);
+  }
 
   // ITERATION 3
   //... your code goes here
+  let total = document.querySelector('#total-value span');
+  total.innerHTML = counter;
 }
 
 // ITERATION 4
