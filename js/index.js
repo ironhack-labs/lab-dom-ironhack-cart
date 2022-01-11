096fdfb28e4e4103deea8a82ad0adf8edf1bce39
László Tarnai <laszlo.tarnai@LEGO.com>
2022-01-11 17:53:20 +0100

    done without bonus

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+// js/index.js
 
-  //... your code goes here
+function updateSubtotal(product) {
+  const price = product.querySelector('.price span');
+  const priceValue = Number(price.innerHTML);
+
+  const quantity = product.querySelector('.quantity input');
+  const quantityValue = Number(quantity.value);
+
+  const subtotal = product.querySelector('.subtotal span');
+  const subtotalValue  = priceValue * quantityValue;
+
+  subtotal.innerHTML = Number(subtotalValue);
+
+  return subtotalValue;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.getElementsByClassName("product");
+
+  let allSubtotals = 0;
+
+  for (let singleProduct of allProducts) {
+    allSubtotals += updateSubtotal(singleProduct);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  const total = document.querySelector('#total-value span');
+  total.innerHTML = allSubtotals;
 }
 
 // ITERATION 4
