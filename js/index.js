d2677e7de8634c56e01f9b955f7b0a2e336e92ab
Ricardo Ronchetti <ricardoronchetti@gmail.com>
2022-01-15 22:47:47 +0100

    updated index.html and index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,36 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText
+  const quantity = product.querySelector('.quantity input').value
+  const subtotal = price * quantity
+  const subt = product.querySelector('.subtotal span')
 
-  //... your code goes here
+  subt.innerText = subtotal
+  return subtotal
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const doubleProduct = document.querySelectorAll('.product')
+  let sum = 0
+  doubleProduct.forEach((product) => {
+    let subtDouble = updateSubtotal(product)
+    sum += subtDouble
+  })
 
   // ITERATION 3
   //... your code goes here
+  let total = document.querySelector('#total-value span')
+  total.innerText = sum
 }
 
 // ITERATION 4
