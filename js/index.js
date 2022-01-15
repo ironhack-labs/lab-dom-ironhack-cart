ee6c014dd2c88292ff56861ab14bc2ba872f66ef
andredoc <andre.documet@gmail.com>
2022-01-15 16:24:23 +0100

    ironcart iteration3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,45 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //alert('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+
+  const priceValue = price.innerHTML;
+  const quantityValue= quantity.value;
+
+  const subTotal=priceValue*quantityValue;
+  const subTotalEl = product.querySelector('.subtotal span');
+
+  subTotalEl.innerHTML=subTotal;
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const products = document.querySelectorAll('.product');
+
+  let sumSubTotal = 0;
+
+  for (let i=0; i<products.length; i++) {
+    sumSubTotal += updateSubtotal(products[i]);
+  }
 
   // ITERATION 3
   //... your code goes here
+  const totalEl = document.querySelector('#total-value span');  
+  totalEl.innerHTML=sumSubTotal;
+  
+
 }
 
 // ITERATION 4
