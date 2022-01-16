fb03645ac9a842b6b77176b6de3349fce03d89bd
VmMad <88061365+VmMad@users.noreply.github.com>
2022-01-16 22:22:01 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
 // ITERATION 1
-
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+function updateSubtotal(allproducts) {
+  allproducts.forEach(function(eachProduct) {
+    const price = eachProduct.querySelector('.price span')
+    console.log(`El precio es ${price.innerText}`)
+    const quantity = eachProduct.querySelector('.quantity input')
+    console.log(`El quantity es ${quantity.value}`)
+    const subtotalOfProduct = quantity.value * price.innerText
+    let subtotalText = eachProduct.querySelector('.subtotal span')
+    console.log(`El subtotal es ${subtotalText.innerText}`)
+    console.log(`El subtotal que quiero es ${subtotalOfProduct}`)
+    subtotalText.innerText = subtotalOfProduct
+    sumSub += subtotalOfProduct
+    return subtotalOfProduct
+  }
+  )
 }
+let sumSub= 0
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
+  
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product')
+  // tuve un problema al intentar poner aquí ^^  en lugar de querySelectorAll getElementsByClassName. no me aceptaba el codigo y me decia que products.forEach no es una función
+  updateSubtotal(allProducts);
 
-  // ITERATION 3
-  //... your code goes here
+  // ITERATION 3 
+  const targetTotal = document.querySelector('#total-value span')
+  targetTotal.innerText  = sumSub
 }
 
 // ITERATION 4
@@ -25,7 +40,11 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const btnRemove = document.querySelectorAll('.btn')
+  btnRemove.addEventListener('click', removeProduct)
+  btnRemove.forEach(eachbtnRemove => 
+    {
+    })
 }
 
 // ITERATION 5
