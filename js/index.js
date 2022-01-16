3cbab2ae8c75921f5e24cbd59580c7c43a46fba9
salva <saersalva@gmail.com>
2022-01-16 21:09:50 +0100

    una interesante manera de aprender for each, aunque no supe aacabar el 4-5 me quede con las ganas

--- js/index.js
+++ js/index.js
@@ -1,32 +1,64 @@
 // ITERATION 1
 
+
+
+
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector(`.price span`);
+  let quantity = product.querySelector(`.quantity input`);
+  let multi = price.innerText * quantity.value
+  const loq = product.querySelector(`.subtotal span `)
+  loq.innerText = multi
+  return multi
 }
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const jose = document.querySelectorAll(`.product`)
+
+  jose.forEach(eachjose => {
+    updateSubtotal(eachjose)
+  })
+
+
+
+
 
   // ITERATION 3
   //... your code goes here
-}
+  let juan = 0
+  jose.forEach(eachnumber => {
+    juan += updateSubtotal(eachnumber)
+  })
+  const ana = document.querySelector(`#total-value span`)
+  ana.innerHTML = juan
 
+}
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
   //... your code goes here
+  //let julio
+  //explosion.forEach(eachnumber=>{
+  // julio+=
+  //})
 }
+
+
 
 // ITERATION 5
 
@@ -36,7 +68,10 @@ function createProduct() {
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
+  calculatePricesBtn.addEventListener('click', calculateAll);;
   //... your code goes here
+  const explosion = document.querySelectorAll('.btn-remove');
+  explosion.forEach(eachexplosion => {
+    eachexplosion.addEventListener('click', removeProduct);
+  })
 });
