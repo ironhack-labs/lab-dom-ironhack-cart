5a16b82c39508087ac93be550d3cc6e47acc043c
Jorge Garzon <jgarzon.informatica@gmail.com>
2022-01-17 01:35:26 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 // ITERATION 1
+let clone = document.getElementsByClassName('product')[0].cloneNode(true)
+let parent = document.getElementsByClassName('product')[0].parentNode
+console.log(clone)
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  let price = product.querySelector('.price span')
+  let quantity = product.querySelector('.quantity input ')
+  let subtotal = product.querySelector('.subtotal span')
+
+  let total = price.innerHTML * quantity.value
+
+  subtotal.innerHTML = total
+
+  return total
+
 
   //... your code goes here
 }
@@ -9,9 +21,22 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
   // end of test
+  let allProducts = document.getElementsByClassName('product')
+  let totalPrice = 0
+  let totalp = document.getElementById('total-value')
+  let totalnum = totalp.querySelector('span')
+  for (i = 0; i < allProducts.length; i++) {
+    totalPrice += updateSubtotal(allProducts[i]);
+  }
+  totalnum.innerHTML = totalPrice
+
+
+
+
+
+
 
   // ITERATION 2
   //... your code goes here
@@ -24,7 +49,10 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  let parent = target.parentNode.parentNode
+  parent.parentNode.removeChild(parent)
+
+
   //... your code goes here
 }
 
@@ -32,11 +60,40 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+
+  let name = document.getElementById('product-name').value
+
+  let price = document.getElementById('product-price').value
+
+
+
+  let cloni = clone.cloneNode(true)
+  let newName = cloni.querySelector('.name span')
+  newName.innerHTML = name
+  let newPrice = cloni.querySelector('.price span')
+  newPrice.innerHTML = price
+
+
+  const button = cloni.querySelector('.btn-remove')
+  button.addEventListener('click', removeProduct);
+
+  parent.appendChild(cloni)
+
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+
+  const removeProductBtn = document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < removeProductBtn.length; i++) {
+    removeProductBtn[i].addEventListener('click', removeProduct);
+  }
+
+  const createProductBtn = document.getElementById('create')
+  createProductBtn.addEventListener('click', createProduct);
+
 
   //... your code goes here
 });
