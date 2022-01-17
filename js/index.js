31f0aff3f5baa4546e9dd7481fea2e30b9727028
Hibaber <hiba.berber@gmail.com>
2022-01-17 09:12:36 +0100

    lab done

--- js/index.js
+++ js/index.js
@@ -1,32 +1,69 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  function updateSubtotal(product) {
+    console.log('Calculating subtotal, yey!');
+//... your code goes here
+
+    const price = product.querySelector('.price span').textContent;
+    console.log(price)
+
+    const quantity = product.querySelector('.quantity input').textContent;
+    console.log(quantity)
+
+    currentValue= quantity*price;
+    console.log(currentValue)
+
+
+    const subTotal = product.querySelector('.subtotal span');
+    subTotal.innerHTML = currentValue;
+
 }
 
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+
 
   // ITERATION 2
-  //... your code goes here
+ //seleccionar el array de los nodos de productos
+  const bothProducts= document.getElementsByClassName('product').textContent;
 
+  const totalNodes = document.getElementById('total-value span').textContent;
+
+  const subTotalBothNodes= document.getElementsByClassName('subtotal span').textContent;
+
+  let sum = 0;
+
+
+  //iterar sobre el array de los productos
+  
+    for (let i = 0; i < bothProducts.length; i++){
+    updateSubtotal(bothProducts[i])
+    sum+= subTotalBothNodes[i]
+    }
   // ITERATION 3
-  //... your code goes here
+  // seleccionar los nodos del array de subtotales
+
+    totalNodes= subTotalBothNodes[i];
+    const totalText= getElementById('total-value').textcontent(totalNodes);
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
+
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+
+const removeButtons = document.getElementByClass('btn-remove');
+removeButtons.addEventListener('click', () =>{
+  for (let i=0; i<removeButtons.length;i++){
+  document.querySelector('tbody).removeChild(removeButtons)
+
+    }
 }
+
 
 // ITERATION 5
 
