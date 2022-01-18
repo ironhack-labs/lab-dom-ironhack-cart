986965f1ec155e2b4126fa2c683f538cde8af329
gabe <gabe@delgadobunch.com>
2022-01-18 15:10:54 -0500

    completed bonuses, works as intended, not passing tests

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
   let price = product.querySelector('.price span').innerHTML;
   let quantity = product.querySelector('.quantity input').value;
   // product.querySelector("sub")
@@ -36,12 +36,33 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  let value = updateSubtotal(target.parentElement.parentElement);
+  target.parentElement.parentElement.remove();
+  let ogTotal = Number(document.querySelector('#total-value span').innerHTML);
+  document.querySelector('#total-value span').innerHTML = String(
+    ogTotal - value
+  );
 }
 
 // ITERATION 5
 
-function createProduct() {
+function createProduct(event) {
   //... your code goes here
+  const values =
+    event.currentTarget.parentElement.parentElement.querySelectorAll(
+      'td input'
+    );
+  let name = values[0].value;
+  let price = values[1].value;
+  let newProduct = document.querySelector('.product').cloneNode(true);
+  newProduct.querySelector('.name span').innerHTML = name;
+  newProduct.querySelector('.price span').innerHTML = price;
+  newProduct.querySelector('.quantity input').value = 0;
+  newProduct.querySelector('.subtotal span').innerHTML = 0;
+  newProduct
+    .querySelector('.btn-remove')
+    .addEventListener('click', removeProduct);
+  document.querySelector('tbody').append(newProduct);
 }
 
 window.addEventListener('load', () => {
@@ -49,4 +70,9 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  const buttons = document.querySelectorAll('.action button');
+  for (removeBtn of buttons) {
+    removeBtn.addEventListener('click', removeProduct);
+  }
+  document.getElementById('create').addEventListener('click', createProduct);
 });
