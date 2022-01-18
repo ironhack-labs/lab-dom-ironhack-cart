58a2df5aa5ce2c3d2b36a063a19c053f1e6ba714
Lorelai <nadja.rathenow@rub.de>
2022-01-18 18:03:59 +0100

    Lab solved

--- js/index.js
+++ js/index.js
@@ -2,22 +2,33 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+const price = Number(product.querySelector('.price span').innerHTML)
+const quantity = product.querySelector('.quantity input').value
+let subtotalPrice = price * quantity;
 
-  //... your code goes here
+return product.querySelector(".subtotal span").innerHTML = subtotalPrice
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let total = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  let products = document.getElementsByClassName("product")
+    // turn HTML collection to Array
+    const productsArr = Array.from(products);
+    // can't use a for.Each loop for HTMLcollestions
+  for (let product of productsArr) {total += updateSubtotal(product)}
+
+
+  // ITERATION 3 - Total
+
+  document.querySelector("#total-value span").innerHTML = total
 }
 
 // ITERATION 4
@@ -31,12 +42,18 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  console.log(document.querySelector(".create-product input").innerText)
+  console.log(document.querySelectorAll(".create-product input").value)
+  
+
 }
+const addItemsBtn = document.getElementById("create")
+addItemsBtn.addEventListener('click', createProduct)
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const addItemsBtn = document.getElementById("create")
+  addItemsBtn.addEventListener('click', createProduct); 
 });
