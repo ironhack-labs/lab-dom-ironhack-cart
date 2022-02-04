456271427b1a6599c7959dd7091b3c5d6339279b
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-02-03 22:00:33 -0500

    iteration 1 complete

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+
+  const subtotal = price * quantity;
+
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+
+  return subtotal;
 }
 
 function calculateAll() {
