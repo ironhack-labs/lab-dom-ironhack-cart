6a9bb6f02b52e4514c15a16e413004d8dfeb62ef
Laurie Bruneau <lauriebr@hotmail.fr>
2022-03-02 00:17:11 +0100

    Lab solved (but not really)

--- js/index.js
+++ js/index.js
@@ -1,24 +1,30 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+// const { LifecycleWatcher } = require("puppeteer");
 
-  //... your code goes here
+function updateSubtotal(product) {
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  console.log(price, quantity);
+
+  const finalPrice = +price.textContent * quantity.value;
+  let subtotal = product.querySelector('.subtotal span');
+  subtotal.textContent = finalPrice;
+
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  let allProducts = cart.querySelectorAll('tr .product');
+  totalPrice = updateSubtotal(allProducts);
+
+// const singleProduct = document.querySelector('.product');
+//   updateSubtotal(singleProduct);
 
   // ITERATION 3
-  //... your code goes here
-}
+
+
 
 // ITERATION 4
 
