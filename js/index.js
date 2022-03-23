9bb6fef78f8e326e1f7713afe91077de227bc358
Laia Markixa <laiamr77@gmail.com>
2022-03-23 18:23:43 +0100

    chronometer class done, index class pending

--- js/index.js
+++ js/index.js
@@ -46,18 +46,53 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
+
   //... your code goes here
+  //target.parentNode.parentNode.remove();
+  target.closest('tr').remove();
 }
 
 // ITERATION 5
 
 function createProduct() {
+
   //... your code goes here
+  const tbody=document.querySelector('tbody');
+  const template=document.getElementById('template');
+  const clone=document.importNode(template.contentEditable, true);
+  const name=document.querySelector('create-product > td input').value;
+  const price=document.querySelector('create-product > td:nth-of-type(2) input').value;
+  clone.querySelector('.name span').textContent=name;
+  clone.querySelector('.price span').textContent=price.value.toFixed(2);
+  clone.querySelector('.btn-remove').addEventListener('click', removeProduct);
+  name.value='';
+  price.value='';
+  tbody.append(clone);
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
+  
   //... your code goes here
+  const removeButtons=document.querySelectorAll('.btn-remove');
+  removeButtons.forEach((button) => {
+    button.addEventListener('click', removeProduct);
+  });
+  
 });
+
+
+/* <tr class="create-product">
+          <td>
+            <input type="text" placeholder="Product Name" />
+          </td>
+          <td>
+            <input type="number" min="0" value="0" placeholder="Product Price" />
+          </td>
+          <td></td>
+          <td></td>
+          <td>
+            <button id="create" class="btn">Create Product</button>
+          </td>
+        </tr>  */
