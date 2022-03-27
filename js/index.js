4608e6ac39f2bda13ec978015e95b5f42fc4ef5a
Lucia de Haro <elle.dharo@gmail.com>
2022-03-27 21:54:34 +0200

    done

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  let subtotal= product.querySelector('.subtotal span');
+  let subtotalPrice= price * quantity
+  subtotal.innerHTML = subtotalPrice;
+  return subtotalPrice
+
 }
 
 function calculateAll() {
@@ -13,30 +20,94 @@ function calculateAll() {
   updateSubtotal(singleProduct);
   // end of test
 
+
+
   // ITERATION 2
   //... your code goes here
+  let sum= 0
+  const allProducts = document.getElementsByClassName('product');
+  let productsArray = Array.from(allProducts)
+  for(item of productsArray){
+    sum+= updateSubtotal(item)
+  }
+
+    
+
 
   // ITERATION 3
   //... your code goes here
+ let totalSum= 0;
+ 
+ allProducts.forEach((element) =>{
+   totalSum += updateSubtotal(element);
+ })
+
+  let= PageTotalSum = document.querySelector('#total-value span') ;
+  PageTotalSum.innerHTML= totalSum;
+
 }
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
+let child = event.currentTarget.parentNode.parentNode;
+let parent = child.parentNode;
+parent.removeChild(child);
+calculateAll();
+
+
 }
 
 // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
+function createProduct(target) {
+
+  const ProductName = document.getElementById('ProductName').value;
+  const ProductPrice = document.getElementById('ProducePrice').value;
+
+  const ProductList = document.getElementById('ProductList');
+
+  function createProduct(){
+    const ProductTemplate= document.querySelector('.product');
+
+    let newNode=ProductTemplate.cloneNode(true);
+
+    newNode.getElementsByClassName('span')[0].innerHTML = ProductName;
+    newNode.getElementsByClassName('span')[1].innerHTML = ProductPrice;
+
+    newNode.getElementsByClassName('btn-remove')[0].addEventListener('click',removeProduct);
+
+    document.querySelector('product').parentNode.appendChild(newNode);
+
+    document.getElementById('ProductName').value="";
+    document.getElementById('ProductPrice').value = 0;
+
+    calculateAll();
+
+
+  }
+    
+ 
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+
+  function removeable(){
+    const removeButtons =document.getElementsByClassName('btn-remove');
+    for(button of removeButtons) {
+      button.addEventListener('click', removeProduct)
+    }
+  }
+
+const createButton= document.getElementsById('create');
+createButton.addEventListener('click',createProduct)
+removeable()
+
+})
