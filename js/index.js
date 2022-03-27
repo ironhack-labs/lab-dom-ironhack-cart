257b0b7a9b95cbd0814551311da21c1853a264b2
AlvaroBenito <alvaro.dbg5@hotmail.com>
2022-03-27 19:24:08 +0200

    Starting Bonus: iteration 5

--- js/index.js
+++ js/index.js
@@ -51,6 +51,10 @@ function removeProduct(event) {
 
 function createProduct() {
   //... your code goes here
+  const createRow = document.querySelector('.create-product')
+  let newProductName = createRow.querySelector('input').value
+  let newProducPrice = createRow.querySelector("input[type='number']").valueAsNumber
+
 }
 
 window.addEventListener('load', () => {
@@ -62,4 +66,6 @@ window.addEventListener('load', () => {
     button.addEventListener('click', removeProduct)
   })
 
+  const createButton = document.getElementById('create')
+  createButton.addEventListener('click', createProduct)
 });
