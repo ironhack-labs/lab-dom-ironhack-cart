515ba11f1372b72d1aebade7b6414c45b2f29b77
LucasPerezDev <LucasPerezDev@gmail.com>
2022-03-29 19:21:57 -0400

    worked through the first few iterations

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 // ITERATION 1
-
+//receivesL: two variables(price & quantity), 
+//then GET DOM elements holding price & quantity.
+// extract values from product & quantity.
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+//  console.log(price, typeof price)
+  const quantity = product.querySelector('.quantity input').value;
+//  console.log(quantity, typeof quantity)
 
-  //... your code goes here
+ const subtotalPrice = Number(price) * Number(quantity);
+
+// console.log(subtotalPrice, typeof subtotalPrice)
+
+ product.querySelector('.subtotal span').innerText = subtotalPrice;
+
+return subtotalPrice; 
+ //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //  const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
