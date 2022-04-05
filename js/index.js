1ac3a27268cad81d8ca2641827196b061131e967
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-04-05 17:02:44 +0200

    first pull

--- js/index.js
+++ js/index.js
@@ -1,20 +1,46 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span')
+  
+  const quantity = product.querySelector(".quantity input")
+  const subtotal = product.querySelector(".subtotal span")
+  
+  const priceNumber = parseFloat(price.innerText)
+  const quantityNumber = parseFloat(quantity.value)
+  
+  const subtotalNumber = priceNumber * quantityNumber
+  subtotal.innerText = subtotalNumber
+  return subtotalNumber
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');*/
+  //updateSubtotal(singleProduct); 
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  const productList = document.getElementsByClassName("product");
+  
+  const productArray = [...productList];
+
+  productArray.forEach( function(tableRow){
+    const subtotal = updateSubtotal(tableRow);
+    console.log("subtotal...", subtotal)
+  });
+
+  let superSum = productArray.reduce(function (sumSubtotal, subtotal) {
+    return sumSubtotal + subtotal;
+  });
+  
+  const newTotal = document.getElementById("total-value")
+  
+  newTotal.innerText = superSum
+
+  return newTotal
 
   // ITERATION 3
   //... your code goes here
