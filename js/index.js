8703b3d0c04a250a851ce2cf3e60807182809a18
Ruben Poelen <ruben.poelen@hotmail.com>
2022-04-05 16:43:32 +0200

    Did most of it. not doing iteration 4 because it's taking way longer. and i wanna start with lab last one

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  const price = product.querySelector('.price span');
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const quantity = product.querySelector('.quantity input');
+
+  const priceNumber = parseFloat(price.innerText);
+  const quantityNumber = parseFloat(quantity.value);
+
+  const subTotalPrice = priceNumber * quantityNumber;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerText = subTotalPrice;
+  return subTotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  const products = [...document.getElementsByClassName('product')];
 
+  const totalprice = document.querySelector('#total-value span');
+
+  let totalToPay = 0;
+  products.forEach((eachProduct) => {
+    totalToPay += updateSubtotal(eachProduct);
+  });
+
+  totalprice.innerText = totalToPay;
   // ITERATION 3
   //... your code goes here
 }
@@ -23,9 +37,12 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode.parentNode;
+  target.parentNode.removeChild(target);
+
+  const total = document.querySelector('#total-value span');
+  const subtotal = target.querySelector('.subtotal span');
+  total.innerHTML = total.innerHTML - subtotal.innerHTML;
 }
 
 // ITERATION 5
