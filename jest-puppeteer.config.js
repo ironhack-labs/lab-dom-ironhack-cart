f0e4d6290f1ffa86d303877cd6ae328269cbf5cb
Julian Rivera <julianarevircoins@gmail.com>
2022-04-08 22:46:38 -0400

    done

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: 'jest-puppeteer',
   server: {
     command: 'serve . -l 4444',
     port: 4444
--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const priceHtmlElement = product.querySelector('.price span');
+  const price = Number(priceHtmlElement.innerHTML);
+
+  const quantityHtmlElement = product.querySelector('.quantity input');
+  const quantity = Number(quantityHtmlElement.value);
+
+  const subtotal = price * quantity;
+  product.querySelector('.subtotal span').textContent = subtotal;
+
+  // alert('Calculating subtotal, yey!\n' + 'Price: ' + price + '\nQuantity: ' + quantity);
+  return subtotal;
 
   //... your code goes here
 }
@@ -9,12 +17,26 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  let cartTotal = 0;
+  const products = document.querySelectorAll('.product');
+  console.log(products);
+
+  /** BOTH ARE ACCEPTABLE SOLUTIONS FOR ITERATING THROUGH OUR PRODUCTS */
+  // [...products].forEach(element => {
+  //   cartTotal = cartTotal + updateSubtotal(element);
+  // })
+  /** BOTH ARE ACCEPTABLE SOLUTIONS FOR ITERATING THROUGH OUR PRODUCTS */
+  for (let counter = 0; counter < products.length; counter++) {
+    cartTotal = cartTotal + updateSubtotal(products[counter]);
+  }
+
+  document.querySelector('#total-value span').innerText = cartTotal;
 
   // ITERATION 3
   //... your code goes here
@@ -24,14 +46,55 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const rowToBeRemoved = target.parentNode.parentNode;
+  const rowParent = target.parentNode.parentNode.parentNode;
+  rowParent.removeChild(rowToBeRemoved);
+
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+  const productNameInputElement = document.querySelector(
+    '.create-product [type="text"]'
+  );
+  const productName = productNameInputElement.value;
+
+  const productPriceInputElement = document.querySelector(
+    '.create-product [type="number"]'
+  );
+  const productPrice = productPriceInputElement.value;
+
+  console.log(productName, productPrice);
+
+  const tableBody = document.querySelector('tbody');
+
+  tableBody.insertAdjacentHTML(
+    'beforeend',
+    `
+  <tr class="product">
+    <td class="name">
+      <span>${productName}</span>
+    </td>
+    <td class="price">$<span>${Number(productPrice).toFixed(2)}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  </tr>
+  `
+  );
+
+  const removeButtons = document.querySelectorAll('.btn-remove');
+
+  const newRemoveButton = removeButtons[removeButtons.length - 1];
+
+  newRemoveButton.addEventListener('click', removeProduct);
 }
 
 window.addEventListener('load', () => {
@@ -39,4 +102,12 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  const removeButtons = document.querySelectorAll('.btn-remove');
+  console.log(removeButtons);
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct);
+  }
+
+  const createButton = document.querySelector('#create');
+  createButton.addEventListener('click', createProduct);
 });
