030816c159bac369a2b0aaa9fcc982bbf651b659
Pipokinha <pipokinha@superpro.email>
2022-04-19 23:36:47 +0100

    Solved first 3 interations

--- js/index.js
+++ js/index.js
@@ -3,21 +3,38 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.product input');
+
+  const priceValue = price.innerHTML;
+  const quantityValue = quantity.value;
+
+  const subtotalValue = priceValue * quantityValue;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = subtotalValue;
+
+  return subtotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  /* const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  let products = document.querySelectorAll('.product');
+  let totalAmount = 0;
+  products.forEach((product) => {
+    totalAmount += updateSubtotal(product);
+  });
   // ITERATION 3
-  //... your code goes here
+  
+  let totalValue = document.querySelector('#total-value span');
+  
+  totalValue.innerHTML = totalAmount
+
 }
 
 // ITERATION 4
@@ -31,7 +48,11 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  let createProductButton = document.getElementById('create');
+  createProductButton.onclick = () => {
+   
+  }
+
 }
 
 window.addEventListener('load', () => {
