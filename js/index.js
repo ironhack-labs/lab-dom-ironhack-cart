e6b4cf322b4781dbe903f4e872941edc22d0367d
“Mapy <mapy.villalobos@gmail.com>
2022-04-27 19:08:43 -0500

    done

--- js/index.js
+++ js/index.js
@@ -1,31 +1,44 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  const price = Number(product.querySelector('.price span').innerText);
+  const quantity = Number(product.querySelector('.quantity input').value);
+  let subTotal = (price * quantity);
+  product.querySelector('.subtotal span').innerText = subTotal;
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const products = document.querySelectorAll('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product');
+  products.forEach (product =>{
+    updateSubtotal(product) 
+  } )
 
   // ITERATION 3
-  //... your code goes here
+  let total = 0
+  const subTotals = document.querySelectorAll('.subtotal span')
+
+  subTotals.forEach (subTotal => {
+    total += Number(subTotal.innerText)
+  })
+  document.querySelector('#total-value span').innerText = total
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const product = target.parentNode.parentNode;
+  const table = product.parentNode
+  table.removeChild(product)
+  calculateAll()
 }
 
 // ITERATION 5
@@ -38,5 +51,8 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = document.querySelectorAll('.btn.btn-remove');
+  removeBtns.forEach (removeBtn => {
+    removeBtn.addEventListener('click',removeProduct)
+  })
 });
