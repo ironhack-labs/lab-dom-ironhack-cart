3f914777cdce1e6c51fd033367972fe5b981ed5f
Vale Quiroga <valentinqramos@outlook.com>
2022-04-28 12:27:27 -0500

    solved lab

--- js/index.js
+++ js/index.js
@@ -1,25 +1,31 @@
 // ITERATION 1
 
-//funciona bien
 function updateSubtotal(product) {
-  //console.log('Calculating subtotal, yey!');
-
   const price = product.querySelector('.price span')
   const quantity = product.querySelector('.quantity input')
   const subtotal = product.querySelector('.subtotal span')
-  
   let result = Number(price.innerText) * Number(quantity.value);
   
   subtotal.textContent = result
   return subtotal.innerText
+
+  /* 
+  ---modo de kain---
+  const price = product.querySelector('.price span');
+  const cuantity = product.querySelector('.quantity input');
+  const priceValue = parseFloat(price.innerText)
+  const quantityValue = quantity.valueAsNumber;
+
+  const subtotalValue = priceValue * quantityValue;
+  const subtotal = product.querySelector('.subtotal span')
+  subtotal.innerText = subtotalValue;
+  return subtotalValue;
+  */
 }
 
 
 function calculateAll() {
-
   // ITERATION 2
-
-  // funciona bien
   let sum = 0;
   const allProducts = document.querySelectorAll('.product')
   
@@ -27,31 +33,80 @@ function calculateAll() {
    let testing =  updateSubtotal(element)
     sum += Number(testing)
   })
+  /* 
+  ---modo da kain---
+  const products = document.getElementsByClassName('product');
+  let totalValue = 0
+  for (let product of products) {
+    totalValue += updateSubtotal(product)
+  }
+  */
 
   // ITERATION 3
-
-  //funciona bien
   let totalNum = document.querySelector('#total-value > span')
   totalNum.textContent = sum
+  /* 
+  ---modo de kain---
+  document.querySelector('#total-value span').innerHTML = totalValue
+  */
   
 }
 
 // ITERATION 4
-
-// no funciona bien.. borra solo los ya creados
-
 function removeProduct(event) {
+  //---modo de kain---
+  const target = event.currentTarget;
+  const row = target.parentNode.parentNode;
+  const parent = row.parentNode
+
+  parent.removeChild(row);
+
+  calculateAll()
+
+/*
+  ---mi codigo que solo borraba los que no fueron creados---
   const target = event.currentTarget;
   console.log('testing');
   target.parentNode.parentNode.remove()
+*/
 }
 
 
 // ITERATION 5
 
-//si crea un nuevo articulo funciona bien pero el solution lab no lo marca 
+//---modo de kain---
 
 function createProduct() {
+  const createRow = document.querySelector('.create-product');
+  let newProdNameInput = createRow.querySelector('input');
+  let newProdNameValue = newProdNameInput.value;
+  let newProdPriceInput = createRow.querySelector("input[type='number']");
+  let newProdPriceValue = Number(newProdPriceInput.valueAsNumber).toFixed(2);
+  const newTableRow = document.createElement('tr');
+  newTableRow.className = 'product';
+  newTableRow.innerHTML = `
+    <td class="name">
+      <span>${newProdNameValue}</span>
+    </td>
+    <td class="price">$<span>${newProdPriceValue}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</>
+    </td>
+  `;
+  const parent = document.querySelector('#cart tbody');
+  parent.appendChild(newTableRow);
+  const removeBtn = newTableRow.querySelector('.btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+  newProdNameInput.value = '';
+  newProdPriceInput.value = 0;
+
+  /*
+  ---mi codigo que si funciona pero deja el lab en rojo---
+
   const createBtn = document.querySelector('#create');
   const tbody = document.querySelector('tbody');
   const price = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]');
@@ -73,6 +128,7 @@ function createProduct() {
      </td>
     </tr>`
   })
+  */
 }
 createProduct()
 
