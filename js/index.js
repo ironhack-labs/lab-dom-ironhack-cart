6228aa5fe1bcfbe6f220e3fb390687b334b57807
Almudena <almudena.vigara@gmail.com>
2022-05-22 14:19:57 +0200

    Iteration 4: DONE (funciona pero no pasa test)

--- js/index.js
+++ js/index.js
@@ -32,20 +32,32 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
+  
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  console.log('The target in remove is:', target.parentNode.parentNode);
   //... your code goes here
+  
+  target.parentNode.parentNode.remove();
+  calculateAll()
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
+
+  const btnRemoveProducts = document.querySelectorAll('.product .action .btn ');
+  for (let btnRemoveProduct of btnRemoveProducts) {
+    btnRemoveProduct.addEventListener('click', (event) => removeProduct(event))
+  }
 });
+
+
+
