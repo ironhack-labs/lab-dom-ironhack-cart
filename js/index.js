126de6075793a70f5c56f222ffe981285355c45e
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-06-07 15:22:45 +0200

    Ironhack shopping cart

--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
 // ITERATION 1
 
+
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  // console.log('Calculating subtotal, yey!');
+  let price = product.querySelector('.price span').innerText;
+  console.log(price); 
+  let quantity = product.querySelector('.quantity input').value;
+  console.log(quantity); 
+  let subtotal = Number(price) * quantity;
+  product.querySelector('.subtotal span').innerText = subtotal;
+  console.log(subtotal); 
+  return subtotal;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+function calculateAll() { 
+  const allProduct = document.querySelectorAll('.product');
+  let total = 0;
+  for (let product of allProduct) {
+    total += updateSubtotal(product); 
+  } 
+  console.log(total);
+  document.querySelector('#total-value span').innerText = total;
 }
 
-// ITERATION 4
+// ITERATION 4 
 
 function removeProduct(event) {
   const target = event.currentTarget;
