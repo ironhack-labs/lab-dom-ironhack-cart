66116914c1ec5f76b412a07986c3041b7b54f76e
Fran <94910215+FranEjarque@users.noreply.github.com>
2022-06-07 15:40:40 +0200

    2nd

--- js/index.js
+++ js/index.js
@@ -4,20 +4,37 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+
+  let subtotal = price * quantity;
+  product.querySelector('.subtotal > span').innerText = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
 
+  // I need to itinerate throughout the table and get the name of the product, the price and the quantity
+    for ( product of document.getElementsByClassName('product')) {
+      updateSubtotal(product)
+    }
+
   // ITERATION 3
   //... your code goes here
+   let total = 0;
+   for ( product of document.getElementsByClassName('product')) {
+     total += updateSubtotal(product);
+   }
+   document.querySelector('#total-value > span').innerText = total 
+  
 }
 
 // ITERATION 4
