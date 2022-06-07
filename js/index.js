a154864646628418798bb548d1c0f47b6af4971c
Alexander Farmakis <alexander@movact.de>
2022-06-07 12:39:00 +0200

    done. bonus iterations i will add later

--- js/index.js
+++ js/index.js
@@ -2,22 +2,31 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  let price =  product.querySelector('.price span').innerHTML 
+  let quantity = product.querySelector('.quantity input').value;
+  let productSum = price * quantity
+  product.querySelector('.subtotal span').innerHTML = productSum;
+  console.log(productSum)
+  return productSum;
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+    let productList = document.querySelectorAll('.product')
+    let total = 0;
+    console.log(productList)
+    for(let el of productList) {
+      let subTotal = updateSubtotal(el)
+      console.log(subTotal)
+      total += subTotal
+    }
+    console.log(total)
+    document.querySelector('#total-value span').innerHTML = total;
+
 
   // ITERATION 3
-  //... your code goes here
+  
 }
 
 // ITERATION 4
