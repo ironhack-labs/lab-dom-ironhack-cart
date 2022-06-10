612c1bccd2b13858e2200939115a253975eead56
MAG-91 <mag_91@hotmail.de>
2022-06-10 13:55:35 +0200

    1Step

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
+  // console.log('Calculating subtotal, yey!');
+  // console.log(product)//.querySelector('.price span').innerText)
   //... your code goes here
+  const price = product.querySelector('.price span')
+  // console.log(price)
+  const quantity = product.querySelector('.quantity input')
+  // console.log(quantity.value)
+  const subtotal = product.querySelector('.subtotal span')
+  // console.log(subtotal)
+  subtotal.innerHTML = price.innerHTML * quantity.value
+  // NICEEE
+  const total = product.querySelector('#total-value span')
+  // console.log(total)
+
 }
 
 function calculateAll() {
@@ -11,13 +22,27 @@ function calculateAll() {
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+  const allSubs = document.querySelector('.subtotal span')
+  
+  // console.log(singleProduct)
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 2 ///////////////////////////////////////////////////////////////////////
+  
+  ///NOT WORKING
+  // let total = 0;
+  //   const products = document.getElementsByClassName('product')
+  //   for (let i = 0; i < products.length; i++) { 
+  //     // updateSubtotal(singleProduct[i])
+  //     // let toInt = parseInt(allSubs[i].innerHTML)
+  //     // total = total + toInt
+  //     console.log(products)
+  // }
 
   // ITERATION 3
-  //... your code goes here
+
+
+
 }
 
 // ITERATION 4
