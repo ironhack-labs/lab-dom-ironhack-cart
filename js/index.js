44aef42c7433ec1af2dcce24422fcd9e48e8e509
jackletran <jack.letran@moebel.de>
2022-06-22 16:39:39 +0200

    done mandatory labs with Zubair

--- js/index.js
+++ js/index.js
@@ -1,23 +1,26 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = Number(product.querySelector('.price span').innerHTML);
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotal = price * quantity;
+  product.querySelector('.subtotal span').innerHTML = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  let totalPrice = 0;
+  allProducts.forEach((element) => {
+    updateSubtotal(element);
+    totalPrice += updateSubtotal(element);
+  });
 
   // ITERATION 3
   //... your code goes here
+  document.querySelector('#total-value span').innerHTML = totalPrice;
+  console.log(totalPrice);
 }
 
 // ITERATION 4
