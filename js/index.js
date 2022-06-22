57b78b38bbd355a1a0e150b618b2bf8974a62acd
Christoph86 <ChrstphBch@web.de>
2022-06-23 01:09:39 +0200

    1_to_4

--- js/index.js
+++ js/index.js
@@ -2,30 +2,35 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let quantity = product.querySelector('.quantity input').value;
+  let price = product.querySelector('.price span').innerText;
+  // let totalvalue = document.querySelector("#total-value span").innerText;
+  let subTotal = 0;
+subTotal = (quantity * price).toFixed(2); //toFixed returns String!
+ 
+product.querySelector('.subtotal span').innerHTML = `${subTotal}`
 
-  //... your code goes here
+return subTotal;
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  let total = 0;
 
-  // ITERATION 2
-  //... your code goes here
+  console.log(document.querySelectorAll(".product"));
+  document.querySelectorAll(".product").forEach ((element) => {
+    total +=  Number(updateSubtotal(element));
+  })
 
-  // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerHTML = `${total}`
+
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  // target.parentElement.remove(); -->td  // parent*2 --> tr
+  event.currentTarget.parentElement.parentElement.remove();
 }
 
 // ITERATION 5
@@ -37,6 +42,8 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  removeBtn.forEach(element => element.addEventListener('click', removeProduct));
 });
+
