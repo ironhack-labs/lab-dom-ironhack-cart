bb3007b54cffe56cbdbc5318873912cb4af7644a
“Alexandre <coma.alexandre@gmail.com>
2022-06-22 22:02:38 +0200

    Lab solved with the begining of the bonus iteration 4

--- js/index.js
+++ js/index.js
@@ -8,41 +8,50 @@ function updateSubtotal(product) {
   
     const quantity = product.querySelector('.quantity input');
     const quantityValue = quantity.valueAsNumber;
-    
-    //const subtotalId = product.getElementsByClassName("subtotal");
-    //subtotalId.innerText = priceValue * quantityValue;
 
-    const subtotalId = product.querySelector(".subtotal span");
-    subtotalId.innerText = priceValue * quantityValue;
+    const subtotal = product.querySelector(".subtotal span");
+    subtotal.innerText = priceValue * quantityValue;
     
-    return subtotalId.innerText;
+    return subtotal.innerText;
     
-    // const subTotal = product.
-  //... your code goes here
-}
+};
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const productsHtmlCollection = document.getElementsByClassName("product");
+  const productsArr = [...productsHtmlCollection];
+  const subTotalArr = [];
+  productsArr.forEach ( element => {
+    subTotalArr.push(Number(updateSubtotal(element)));
+  }); 
 
   // ITERATION 3
-  //... your code goes here
+  const totalValue = document.querySelector('#total-value span');
+  totalValue.innerText = subTotalArr.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue
+  },0);
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  const removeBtn = document.getElementsByClassName('btn btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+  target.parentNode.removeChild(target);
+  removeProduct(target);
 }
+
 
 // ITERATION 5
 
