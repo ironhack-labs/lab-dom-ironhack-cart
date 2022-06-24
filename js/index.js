ee742d5786d7a50f51d80a09c32d21e306c1def0
Khalid918 <ahmed-a2@graduate.utm.my>
2022-06-24 22:42:10 +0800

    Solved lab

--- js/index.js
+++ js/index.js
@@ -2,23 +2,31 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  
+    const price = product.querySelector('.price span');
+    const priceValue = Number(price.innerText);
+  
+    const quantity = product.querySelector('.quantity input');
+    const quantityValue = quantity.valueAsNumber;
+    const subtotalId = product.querySelector(".subtotal span");
+    subtotalId.innerText = priceValue * quantityValue;
+    
+    return subtotal.innerText;
+    
 }
-
+const calculate= document.getElementById('calculate')
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  const singleProduct = document.querySelectorAll('.product');
+  let Total = 0;
+  for (i=0; i<singleProduct.length; i++){
+    updateSubtotal(singleProduct[i]);
+    Total = Total + updateSubtotal;
+  }
+  const myTotal = document.querySelector('h2 span');
+  myTotal.innerText = Total;
+  calculate.addEventListener('click', calculateAll);
 }
+
 
 // ITERATION 4
 
@@ -26,13 +34,19 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  const removeBtn = document.getElementsByClassName('btn btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+  target.parentNode.removeChild(target);
+  removeProduct(target);
 }
+
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
-}
+}s
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
