e790c4fd97064fb157a11e4a74ae2764708308b9
Daniela Trinchera <trinchera.dm@gmail.com>
2022-07-26 00:44:41 +0200

    I am useless, hopefully i will improve before sinking

--- js/index.js
+++ js/index.js
@@ -2,23 +2,51 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = Number(product.querySelector('.price span').innerText);
+  console.log(price);
+  const quantity = Number(product.querySelector('.quantity input').value);
+  console.log(quantity);
+  // let subtotal = product.querySelector('.subtotal span').innerText;
+  let subtotal1 = price * quantity;
+  console.log(subtotal1);
+  product.querySelector('.subtotal span').innerText = subtotal1;
+  return subtotal1;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const holdSubTotal  = document.getElementsByClassName('product');
+  for (element of holdSubTotal){
+    updateSubtotal(element);
+    console.log(holdSubTotal.innerText);
+    // let subTotalArray = document.getElementsByClassName('product')
+    // let totalHolder = subTotalArray.reduce((total, currentValue) => total + currentValue, 0).innerText;
+    }
+    
+  // const subTotalArray = Number([...holdSubTotal].innerText);
+  // console.log(subTotalArray);
+  // 
+  // console.log(totalHolder);
+  // return totalHolder
+  }
+  // const total = numbers.reduce((accumulator, currentValue) => {
+  //   console.log('accumulator is: ', accumulator, 'and current value is: ', currentValue);
+  //   return accumulator + currentValue;
+  // }, 0)
+// const products = document.getElementsByClassName('product')
+
+
 
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
