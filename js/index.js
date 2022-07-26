8dfc9a0fa4d4a055f5174d9b27c22ecc7349b116
Ryan Essel <spucti19@gmail.com>
2022-07-26 18:09:52 -0400

    lab mostly sloved. Can't get remove or add to work they way the test is requiring for it to pass

--- js/index.js
+++ js/index.js
@@ -3,36 +3,81 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
+
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input');
+  let subtotal = product.querySelector(".subtotal span");
+  let sumSubTotal = Number(price) * Number(quantity.value);
+  return subtotal.innerText = Number(sumSubTotal);
+
+
+
+
+
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+const totalValue = document.querySelector("#total-value span");
+const allInputs = document.querySelectorAll(".product");
+let grandTotal = 0;
+    for (eachInput of allInputs) {
+      updateSubtotal(eachInput)
+    }
+    
+    for (eachInput of allInputs ) {
+      grandTotal += updateSubtotal(eachInput);
+      
+    }
+    
+ totalValue.innerText = Number(grandTotal);
 }
 
-// ITERATION 4
+// ITERATION 3
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+
+// ITERATION 4 - // Don't understand how event should be used here. I understand e.target but I don't get how to apply it to this situation. 
+
+let allRemoveBtn = document.querySelectorAll(".btn-remove");
+let tableBod = document.getElementsByTagName("tbody")[0];// must use [i] inside
+let rowToRemove =  document.querySelectorAll('.product');
+for (let i = 0; i < allRemoveBtn.length; i++) {
+
+allRemoveBtn[i].onclick = function removeProduct (event) {
+  // rowToRemove[i].remove();
+  tableBod.removeChild(rowToRemove[i]);
+  calculateAll();
+}
 }
 
-// ITERATION 5
 
-function createProduct() {
+
+
+// ITERATION 5 // only partially works / makes a new button but can't delete but also can't figure out how to add the values inputed in the create button row to the new tr
+
+let createBtn = document.querySelectorAll("#create.btn")[0];
+let rowExample = document.querySelectorAll(".product")[0];
+let anyRow = document.querySelectorAll(".product")
+let lastRow = document.querySelectorAll(".product").length -1;
+let newRow = rowExample.cloneNode(true);
+let rowLength = anyRow.length;
+
+createBtn.onclick = function createProduct() {
   //... your code goes here
+  //onclick...
+  //let newRowData = create button row input.values
+  //let newRow = 
+
+  console.log("is this working?")
+  let addingRow = anyRow[lastRow].after(newRow);
+  console.log(rowLength);
+  // return addingRow;
+
 }
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
@@ -40,3 +85,47 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+
+// / THIS WORRRKKKSSSS!!!!// OTHER WAY (DOESN"T PASS TEST AND CAN"T DELETE NEW ONES IF I ADD THEM VIA the dev tools)
+
+// let allRemoveBtn = document.querySelectorAll(".btn-remove");
+// let rowToRemove =  document.querySelectorAll('tr.product');
+
+// for (let i = 0; i < allRemoveBtn.length; i++) {
+
+// allRemoveBtn[i].onclick = function () {
+//   rowToRemove[i].remove();
+//   calculateAll();
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
