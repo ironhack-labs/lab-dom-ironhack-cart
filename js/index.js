da6a14dbc77aeed9373905f2535a7e480f753795
Chlotte <charlottealleaume99@gmail.com>
2022-08-05 16:19:17 +0200

    iteration 5 implemented and tested

--- js/index.js
+++ js/index.js
@@ -15,8 +15,6 @@ function calculateAll() {
   totalValue.textContent = total;
 }
 
-// ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   let parent = target.parentNode;
@@ -24,10 +22,17 @@ function removeProduct(event) {
   calculateAll();
 }
 
-// ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  let name = document.querySelector('tfoot .create-product input[type="text"]').value;
+  let price = document.querySelector('tfoot .create-product input[type="number"]').value;
+  let template = document.getElementsByClassName('product')[0];
+  let newProduct = template.cloneNode(true);
+  newProduct.querySelector('.name span').textContent = name;
+  newProduct.querySelector('.price span').textContent = price;
+  newProduct.querySelector('.quantity input').value = 0;
+  newProduct.querySelector('.subtotal span').textContent = 0;
+  newProduct.querySelector('.btn-remove').addEventListener('click', removeProduct);
+  document.querySelector('tbody').append(newProduct);
 }
 
 window.addEventListener('load', () => {
@@ -38,4 +43,7 @@ window.addEventListener('load', () => {
   for (const btn of removeBtns){
     btn.addEventListener('click', removeProduct);
   }
+
+  const createProductBtn = document.getElementById('create');
+  createProductBtn.addEventListener('click', createProduct);
 });
