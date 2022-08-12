e1c903ceb4dfa69d5d9c946793df9fa48479515b
RedValerian <r.pino.camacho@gmail.com>
2022-08-13 00:22:16 +0200

    Day 5 - Finished

--- js/index.js
+++ js/index.js
@@ -1,42 +1,68 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = parseFloat(product.querySelector('.price span').innerText);
+  const quantity = product.querySelector('.quantity input').valueAsNumber;
+  const subtotalBox = product.querySelector('.subtotal span');
+  subtotalBox.textContent = price * quantity;
+  return parseFloat(subtotalBox.textContent);
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  // ITERATION 2 and ITERATION 3
+  const multipleProducts = document.querySelectorAll('.product');
+  // console.log(multipleProducts);
+  let totalPrice = 0;
+  [...multipleProducts].forEach(element => {
+    totalPrice += updateSubtotal(element);
+  });
+  const totalPriceBox = document.querySelector('#total-value span');
+  totalPriceBox.innerHTML = totalPrice;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget.parentNode.parentNode;
+  target.parentNode.removeChild(target);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  let products = document.querySelector('tbody');
+  let newPrice = document.querySelector('.create-product input[type=number]').valueAsNumber;
+  let newName = document.querySelector('.create-product input[type=text]').value;
+  // I dont like this, but html forced my hand
+  // I should instead use insertAdjacentElement()
+  // its better :)
+  products.insertAdjacentHTML('beforeend', `<tr class="product">
+        <td class="name">
+          <span>${newName}</span>
+        </td>
+        <td class="price">$<span>${newPrice}</span></td>
+        <td class="quantity">
+          <input type="number" value="0" min="0" placeholder="Quantity" />
+        </td>
+        <td class="subtotal">$<span>0</span></td>
+        <td class="action">
+          <button class="btn btn-remove">Remove</button>
+        </td>
+      </tr>`);
+  // It add the action listener to the delete button to the new """node"""
+  const removeProductBtns = document.querySelectorAll('.btn-remove');
+  removeProductBtns[removeProductBtns.length - 1].addEventListener('click', removeProduct);
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeProductBtns = document.querySelectorAll('.btn-remove');
+  [...removeProductBtns].forEach(element => {
+    element.addEventListener('click', removeProduct)
+  });
+
+  document.querySelector('#create').addEventListener('click', createProduct);
 });
