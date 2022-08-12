8b3d7980c9bc28e83e7f5e63471d611a4505b333
jdlms <97018563+jdlms@users.noreply.github.com>
2022-08-12 09:49:51 +0200

    friday push

--- js/index.js
+++ js/index.js
@@ -26,45 +26,26 @@ function calculateAll() {
 
   // ITERATION 3
 
-  //This took me forever. Why does the below return an HTMLCollection with a length of 1?
-  //const totalSpan = totalElement.getElementsByTagName('span')
-
-  //For classes much easier :)  product.querySelector('.price span')
-
   const totalElement = document.getElementById('total-value');
   const totalSpan = totalElement.getElementsByTagName('span')[0];
 
-  //This also had me confused. Why does:
-  // const totalSpan = totalElement.getElementsByTagName('span')[0].innerText
-  //Correctly select the span inner text, but then
-  // totalSpan = allProductsSum
-  //will not correctly adjust total on the page? Meaning, it has to be written so:
   totalSpan.innerText = allProductsSum;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-//no idea!
-  const shoppingCart = document.getElementById('cart');
-  const oneProduct = shoppingCart.getElementsByClassName('product');
-  const td = oneProduct.getElementsByClassName('action');
+  // const target = event.currentTarget;
+  // console.log('The target in remove is:', target);
 
-  td.getParent.removeChild(target);
+  singleProduct = event.currentTarget.parentNode.parentNode;
 
-  // if (target) {
-  //    oneProduct.parentNode.removeChild(oneProduct);
-  // }
-
-  // target.shoppingCart.removeChild(oneProduct);
-
-  //cart -> "product" -> "action" -> 'btn-remove"
+  console.log(singleProduct);
+  singleProduct.parentNode.removeChild(singleProduct);
+  calculateAll();
+  //tbody -> "product" -> "action" -> 'btn-remove"
   //  removeChild(child);
 }
-
-// element.style.display = "none";
 
 // ITERATION 5
 
