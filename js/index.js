4d123da77f798a7c34ea1349d2b33cbfec0e6218
Mario Fernandez <marioteleco@outlook.com>
2022-08-14 21:42:10 +0000

    Hice la primera parte. No conseguí terminar a tiempo, he visto como plantearlo pero me ha costado en interpretarlo a código. De todas maneras ha sido muy interesante este lab, me ha permitido buscar información sobre el DOM y aprender más.

--- js/index.js
+++ js/index.js
@@ -2,15 +2,42 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let price = document.querySelector(".price span").innerHTML;
+  let quantity = document.querySelector(".quantity input").value;
+  let subtotal = document.getElementById("subtotal");
+  //let subtotal2 = document.getElementById("subtotal");
 
-  //... your code goes here
+  //product 2
+  let price1 = document.querySelector(".price1 span").innerHTML;
+  let quantity1 = document.querySelector(".quantity1 input").value;
+  let subtotal1 = document.getElementById("subtotal1");
+
+  let total = quantity * price;
+  subtotal.textContent = total;
+
+  let total1 = quantity1 * price1;
+  subtotal1.textContent = total1;
+
+
+
+  console.log(quantity, price, total, subtotal);
+  console.log(quantity1, price1, total1, subtotal1);
+
+  alert("calculate price clicked!");
+
+
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
+
+  let generalTotal = document.getElementById("calculate");
+  generalTotal = total + total1;
+  // ----------------Hasta aquí he llegado, entiendo lo que hay que hacer pero plasmarlo en código tengo dificultades aún --------
   // end of test
 
   // ITERATION 2
@@ -39,4 +66,5 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+
 });
