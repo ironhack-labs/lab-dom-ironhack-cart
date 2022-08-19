9dec75a0a1fb534b39332801e582938baca87ee2
Carlos Joaquin <96665331+MrsTWINS@users.noreply.github.com>
2022-08-19 18:13:00 -0400

    cart

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span')
+  const quantity = product.querySelector('.quantity input').value
+  const subtotal = product.querySelector('.subtotal span')
+  const resultado = price.innerText * quantity
+
+  subtotal.innerText = resultado
+
+  return resultado
 }
 
 function calculateAll() {
@@ -14,10 +21,18 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let productFila = document.getElementsByClassName('product')
+  let sum = 0
+
+  for (let i = 0; i < productFila.length; i++) {
+      sum += Number(updateSubtotal(productFila[i])
+      )}
+
 
   // ITERATION 3
-  //... your code goes here
+  let calculoFinal = document.querySelector('#total-value span')
+  
+  calculoFinal.innerText = Number(sum)
 }
 
 // ITERATION 4
