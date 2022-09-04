a3f0c0648ac35d36c8d13fff6c68b4e2f36e1293
Jo Huerta <santiago@alimx.mx>
2022-09-03 22:35:13 -0500

    WDFT-MEX-AGOSTO- Jose Ezequiel Huerta

--- js/index.js
+++ js/index.js
@@ -2,22 +2,33 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  
+  const subtotal = price*quantity;  
+  return product.querySelector('.subtotal span').innerText= subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  //... your code goes 
+  let result = 0;
+  const aux = document.getElementsByClassName('product');
+
+  for (let i = 0; i < aux.length; i++) {
+    result += updateSubtotal(aux[i]);
+  }
 
   // ITERATION 3
   //... your code goes here
+  return document.querySelector('#total-value span').innerText = result;
 }
 
 // ITERATION 4
