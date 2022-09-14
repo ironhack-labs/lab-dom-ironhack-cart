a4dcb6c888b01d03e63871ca90c0672ba8a998ba
Mog Rouhi <moghadase.roohi@gmail.com>
2022-09-14 22:32:47 +0100

    complete three iterations

--- js/index.js
+++ js/index.js
@@ -1,24 +1,37 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
+ // console.log('Calculating subtotal, yey!');
   //... your code goes here
+  const price = parseFloat(product.querySelector('.price span').innerText)
+  const quantity = product.querySelector(".quantity input").valueAsNumber;
+  let subtotalPrice = price * quantity;
+  const subtotalElement = product.querySelector(".subtotal span");
+  subtotalElement.innerText = subtotalPrice;
+  return subtotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const productsAll = document.querySelectorAll("tr.product");
+  let sum1 = 0;
+  productsAll.forEach(function(element){
+    sum1 += updateSubtotal(element);
+  })
 
   // ITERATION 3
   //... your code goes here
+  let totalElem = document.querySelector("#total-value span");
+  totalElem.innerHTML = Number(sum1.toFixed(2));
 }
+ 
 
 // ITERATION 4
 
@@ -26,7 +39,13 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
-}
+  const btnRemoveArr = document.querySelectorAll(".btn btn-remove");
+  for (const btn of btnRemoveArr){
+        btn.addEventListener("click", removeProduct);
+      }
+      document.body.addEventListener("click", removeProduct)
+    }
+
 
 // ITERATION 5
 
@@ -39,4 +58,5 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+
 });
