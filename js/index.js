768128fb68568c108edb8c7fa79be8db7acace5b
Marc Logan <marclogan90@gmail.com>
2022-09-15 09:00:27 +0200

    iteration 1 & 2

--- js/index.js
+++ js/index.js
@@ -2,23 +2,81 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  // const price = parseFloat(product.querySelector('.price span').innerText)
 
-  //... your code goes here
+  // const quantity = product.querySelector(".quantity input ").valueAsNumber
+
+  // let subTotalPirce = price * quantity
+
+  // const subTotalElement = product.querySelector(".subtotal span")
+
+  // subtotalElement.innerText = subtotalPrice
+   
+
+  const price = parseFloat(product.querySelector('.price span').innerText)
+
+  const quantity = product.querySelector('.quantity input').valueAsNumber
+
+  let subtotalPrice = price * quantity
+
+  const subtotalElement = product.querySelector('.subtotal span')
+
+  subtotalElement.innerText = subtotalPrice
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);
 
-  // ITERATION 3
-  //... your code goes here
-}
+  
+
+  // let total = 0;
+
+  // const allProducts = documents.querySelectorAll(' .product')
+  //   for (let i = 0; i < allProducts.length; i++){
+  //       total = singleProduct + allProducts[i]
+  //   }
+ 
+  // const eachProduct = document.getElementsByClassName(' .product').
+
+  // console.log(eachProduct);
+
+  // let total = 0
+  //  const allProducts = document.querySelectorAll('.product');
+   
+  //  for (let i = 0; i < allProducts.length; i++) {
+  //    total += newsubtotalPrice(allProducts[i])
+ 
+  //   }
+    
+    // const productsForSale = document.getElementsByClassName(".product");
+    // productsForSaleArr = [...productsForSale]
+    // productsForSaleArr.forEach(element => {updateSubtotal(element)
+    // });
+
+    const productCollection = document.querySelectorAll('.product');
+   productCollection.forEach( function (productElm) {
+     updateSubtotal(productElm);
+   });
+    }
+      
+ // ITERATION 3
+ // ... your code goes here
+  const totalSum = document.querySelectorAll(".subtotal span")
+  let totalSumCopy = 0
+    for (let i = 0; i < totalSum.length; i++){
+      totalSumCopy += totalSum[i]
+   }
+
+   
 
 // ITERATION 4
 
