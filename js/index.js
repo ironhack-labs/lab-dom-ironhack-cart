13b59780168b50b0cc00de7a7e705fa0e7e4f1c3
MoniIgnacio <moniignacio02@gmail.com>
2022-09-27 23:24:47 +0400

    fixed some problems in It-3

--- js/index.js
+++ js/index.js
@@ -2,33 +2,51 @@
 
 function updateSubtotal(product) {
   
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
   
   const price = product.querySelector('.price span') ;
   const quantity = product.querySelector('.quantity input').value
   const subtotalPrice = price.innerHTML * quantity
   const subtotal = product.querySelector('.subtotal span')
-  subtotal.innerHTML = subtotalPrice  
+  subtotal.innerHTML = subtotalPrice 
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   const allProducts = document.querySelectorAll('.product')
   allProducts.forEach((eachProduct)=>{
     updateSubtotal(eachProduct)
+    let counter = 0
+    // obtengo subtotal de cada elemento
+    let eachSubtotal = eachProduct.querySelector('.subtotal span')
+    eachSubtotal = eachSubtotal.innerHTML
+    // lo paso a Number 
+    let eachSubtotalNumber = Number(eachSubtotal)
+    // necesito meterlo dentro del contador
+    // console.log(eachSubtotalNumber)
+    return eachSubtotalNumber
   })
+
   
 
   // ITERATION 3
 
-  // const totalPrice = updateSubtotal(product)
-  console.log(totalPrice)
+  const totalPriceDOM = document.querySelectorAll('#total-value span')
+  const totalPriceNumDOM = totalPriceDOM[0].innerHTML
+  // console.log(totalPriceNumDOM)
+  // totalPrice.forEach((eachSubtotal)=>{
+  //   let 
+
+  //   console.log(eachSubtotal.innerText)
+  // })
+
+  // console.log(totalPrice)
 
 }
 
@@ -36,7 +54,7 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  // console.log('The target in remove is:', target);
   //... your code goes here
 }
 
