2e8be773bdac31c1280f072c43947a0dca76ff12
Maxvidal17 <110576377+Maxvidal17@users.noreply.github.com>
2022-09-28 15:38:59 -0400

    Solved Lab

--- js/index.js
+++ js/index.js
@@ -2,41 +2,80 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  const price = parseFloat(product.querySelector('.price span').innerText);
 
-  //... your code goes here
+
+  const quantity = product.querySelector('.quantity input').valueAsNumber;
+
+  let subtotalPrice =  price * quantity;
+
+  const subtotalElement = product.querySelector('.subtotal span')
+
+  subtotalElement.innerText = subtotalPrice;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+  
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  const productCollection = document.querySelectorAll('.product');
+  
+  productCollection.forEach( (productElm) => {
+    updateSubtotal(productElm);
+  });
   // ITERATION 3
-  //... your code goes here
-}
+  const totalCollection = (document.querySelectorAll('.subtotal span'));
+ 
+  let totalValue = 0;
+
+  totalCollection.forEach((product) => {
+    
+    totalValue += parseFloat(product.innerText);
+
+  });
+  console.log(totalValue);
+
+
+  const totalDomElement = document.querySelector('#total-value span');
+  
+  totalDomElement.innerText = totalValue;}
 
 // ITERATION 4
 
 function removeProduct(event) {
+ 
   const target = event.currentTarget;
+  
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
+  target.parentElement.parentElement.remove()
+
+  calculateAll()
+
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
-}
+  let newRow = document.querySelector('create-product')
+
+  let newProductName = document.querySelector('.create-product input[type="text"]').value
+
+  let newProductPrice = document.querySelector('.create-product input[type="number"]').value
+  
+  console.log(newProductName)
+  
+  console.log(newProductPrice)}
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
+  
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
-});
+   const createButton = document.getElementById('create')
+  
+   createButton.addEventListener('click', createProduct)});
+
