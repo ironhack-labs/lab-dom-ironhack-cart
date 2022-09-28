9f11a65e8a42f49aaaf0c27f52f5866463b8304f
Yago <yagoponcem@gmail.com>
2022-09-28 08:13:45 +0200

    3 iteraciones salen bien, aunque con dificultad para verlo todo claro

--- js/index.js
+++ js/index.js
@@ -2,22 +2,33 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  let price = product.querySelector(".price span");
+  let quantity = product.querySelector(".quantity input")
+  let priceText = price.innerText;
+  let quantityText = quantity.value
+  let result = priceText * quantityText
+  let subtotal = product.querySelector(".subtotal span")
+   return subtotal.innerText = result
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
-
+  
   // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  let calculatePrice = 0
+  let allProducts = document.querySelectorAll(".product")
+  allProducts.forEach((eachProduct) => {
+  calculatePrice += updateSubtotal(eachProduct)
+  })
+  let totalPrice = document.querySelector("#total-value span")
+  totalPrice.innerText = calculatePrice
+  
+  
 }
 
 // ITERATION 4
@@ -25,7 +36,6 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
 
 // ITERATION 5
@@ -34,9 +44,15 @@ function createProduct() {
   //... your code goes here
 }
 
+
+
+
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  let removeButtons = document.querySelectorAll("btn btn-remove")
+  removeButtons.addEventListener("click", removeProduct())
+
 
   //... your code goes here
 });
