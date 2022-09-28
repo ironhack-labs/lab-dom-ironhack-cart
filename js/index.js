9110eca366ba539c3facee07cf6a32a58b70fce1
gustavojim <113261314+gustavojim@users.noreply.github.com>
2022-09-28 09:14:29 +0200

    Cambio 1

--- js/index.js
+++ js/index.js
@@ -4,21 +4,38 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity = product.querySelector('.quantity input').value;
+  let subtotal = quantity * price;
+  product.querySelector('.subtotal span').innerHTML = subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+// code in the following two lines is added just for testing purposes.
+// it runs when only iteration 1 is completed. at later point, it can be removed.
 
-  // ITERATION 3
-  //... your code goes here
+// const singleProduct = document.querySelector('.product');
+// updateSubtotal(singleProduct);
+
+// end of test
+
+// ITERATION 2
+//... your code goes here
+
+let arrayProducts = document.getElementsByClassName('product');
+for (let index = 0; index < arrayProducts.length; index++) {
+  updateSubtotal (arrayProducts[index]);
 }
+}
+
+// ITERATION 3
+//... your code goes here
+
+
+
+
+
 
 // ITERATION 4
 
