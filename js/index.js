652929908e9834f77e1a58cb2e1b8baefbb29588
MoniIgnacio <moniignacio02@gmail.com>
2022-09-28 10:23:16 +0400

    update the Number value IT-2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  
   // console.log('Calculating subtotal, yey!');
-  
-  const price = product.querySelector('.price span') ;
-  const quantity = product.querySelector('.quantity input').value
-  const subtotalPrice = price.innerHTML * quantity
-  const subtotal = product.querySelector('.subtotal span')
-  subtotal.innerHTML = subtotalPrice 
+
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotalPrice = price.innerHTML * quantity;
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = subtotalPrice;
 }
 
 function calculateAll() {
@@ -19,35 +18,30 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-  const allProducts = document.querySelectorAll('.product')
-  allProducts.forEach((eachProduct)=>{
-    updateSubtotal(eachProduct)
-    let counter = 0
+  const allProducts = document.querySelectorAll('.product');
+  allProducts.forEach((eachProduct) => {
+    updateSubtotal(eachProduct);
+    let counter = 0;
     // obtengo subtotal de cada elemento
-    let eachSubtotal = eachProduct.querySelector('.subtotal span')
-    eachSubtotal = eachSubtotal.innerHTML
-    // lo paso a Number 
-    let eachSubtotalNumber = Number(eachSubtotal)
-    // necesito meterlo dentro del contador
-    // console.log(eachSubtotalNumber)
-    return eachSubtotalNumber
-  })
+    let eachSubtotal = eachProduct.querySelector('.subtotal span');
+    // coercion a Number
+    eachSubtotal = +eachSubtotal.innerHTML;
+    // el contador actualmente contiene los dos subtotales de cada elemento.
+    // iterar por cada uno de ellos y agregarlo al contador
+    // eachSubtotal.forEach((element) => {
+    //   console.log(element); //--> eachSubtotal.forEach is not a function
+    // });
 
-  
+    // counter = eachSubtotal
+    return eachSubtotal;
+  });
 
   // ITERATION 3
-
-  const totalPriceDOM = document.querySelectorAll('#total-value span')
-  const totalPriceNumDOM = totalPriceDOM[0].innerHTML
-  // console.log(totalPriceNumDOM)
-  // totalPrice.forEach((eachSubtotal)=>{
-  //   let 
-
-  //   console.log(eachSubtotal.innerText)
-  // })
-
-  // console.log(totalPrice)
-
+  // accedo por id al h2 que contiene el span ($0)
+  let totalPriceDOM = document.querySelectorAll('#total-value span');
+  // accedo al valor 0 y coercion a Number
+  let totalPriceNumDOM = +totalPriceDOM[0].innerHTML;
+  // totalPriceNumDom =  counter //--> traer valor de la It-2
 }
 
 // ITERATION 4
