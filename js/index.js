bd1d939b8bf88dd5c4d13e6e0c354e7f04326802
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-10-04 13:05:13 +0100

    iterations 1 and 2, will finish 3 before class today

--- js/index.js
+++ js/index.js
@@ -2,22 +2,19 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+const price = product.querySelector('.price span').innerHTML
+let quantity = product.querySelector('.quantity input').value
+let subPrice = price*quantity
+product.querySelector('.subtotal span').innerHTML = subPrice
+return subPrice
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  
+ const singleProduct = document.getElementsByClassName('product');
+ for (i=0; i<singleProduct.length; i++)
+  updateSubtotal(singleProduct[i]); 
+ 
 }
 
 // ITERATION 4
