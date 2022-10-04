45a19e754079627743b92a944f8d5906463f1457
Miguel Curbelo <mcurbeloj@gmail.com>
2022-10-04 15:29:15 +0200

    Solved lab

--- js/index.js
+++ js/index.js
@@ -3,21 +3,39 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+
+  let priceAmount = parseFloat(price.innerHTML);
+  let quantityAmount = parseFloat(quantity.valueAsNumber); // finally returns a number and not NaN!
+
+  let subtotalPrice = priceAmount * quantityAmount;
+
+  let subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = subtotalPrice;
+  return subtotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.getElementsByClassName('product');
+  let totalPrice = 0;
+  for (let product of allProducts){
+    
+    totalPrice += updateSubtotal(product);
+  }
+  // finally works!
 
   // ITERATION 3
-  //... your code goes here
+let totalValueElement = document.querySelector('#total-value span'); //must include span to not lose currency
+totalValueElement.innerText= totalPrice;
+return totalPrice;
 }
 
 // ITERATION 4
