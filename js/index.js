dfe90fe45811e50b54f7ccb2df3e1314788ac3b0
Rian Yan <rianyan9723@gmail.com>
2022-10-07 00:00:30 +0100

    Solved lab

--- js/index.js
+++ js/index.js
@@ -3,21 +3,60 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  //const price = product.querySelector(".price span");
+  //const quantity = product.querySelector("quantity");
+  //price = product.querySelector(".price span").innerText; 
+  //quantity = product.querySelector("quantity").value;
+  //const totalcost = price * quantity; 
+  //totalcost.innerText = product.querySelector(".subtotal span");
+  //return totalcost;
+
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+
+  const priceValue = parseFloat(price.innerText);
+  
+  const quantityValue = quantity.valueAsNumber;
+
+  const subtotalValue = priceValue * quantityValue;
+
+  const subTotal = product.querySelector('.subtotal span');
+
+  subTotal.innerText = subtotalValue;
+
+  return subtotalValue;
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 2 & 3
 
-  // ITERATION 3
-  //... your code goes here
+  const myProduct = document.querySelector('.product');
+
+  updateSubtotal(myProduct);
+
+  const allProducts = document.querySelectorAll('.product');
+
+  let totalPrice = 0;
+
+  for (let product of allProducts) {
+
+    totalPrice += updateSubtotal(product);
+  }
+
+  let totalCurrency = document.querySelector('#total-value span');
+
+  totalCurrency.innerText = totalPrice;
+
+  return totalPrice;
+
+  
 }
 
 // ITERATION 4
@@ -25,7 +64,10 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
+  function removeProduct(event) {
+    const target = event.currentTarget;
+    console.log('The target in remove is:', target);
 }
 
 // ITERATION 5
