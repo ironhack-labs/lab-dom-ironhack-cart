b83e290d5e23d46966a5dffef96723b96fa62af1
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-10-11 23:38:33 +0100

    (partially) solved lab

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,3 +4,25 @@ module.exports = {
     port: 4444
   }
 };
+
+window.addEventListener('load', () => {
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+});
+
+// function calculateAll() {
+//   // code in the following two lines is added just for testing purposes.
+//   // it runs when only iteration 1 is completed. at later point, it can be removed.
+//   const singleProduct = document.querySelector('.product');
+//   updateSubtotal(singleProduct);
+//   // end of test
+
+//   // ITERATION 2
+//   //...
+//   // ITERATION 3
+//   //...
+// }
+
+//skyp interaction 0;
+// INTER 1:
+
--- js/index.js
+++ js/index.js
@@ -1,24 +1,40 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+    const price = product.querySelector('.price span');
+    const quantity = product.querySelector('.quantity input');
+    const newPrice = price.innerHTML; 
+    const newQuantity = quantity.value  // it is also possible to erase this line and recall it in line 5 as .value;
+  
+    let calculate = newPrice * newQuantity
+    let subtotal = product.querySelector('.subtotal span');
+    
+    subtotal.innerHTML = calculate;
+    return calculate;
+    }
 
-  //... your code goes here
-}
+  console.log('Calculating subtotal, yey!');
+ 
+  // ITERATION 2
 
 function calculateAll() {
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  const allProducts = document.getElementsByClassName('product'); 
+    let sum = 0
+    for (let i = 0; i < allProducts.length; i++) {
+      sum += updateSubtotal(allProducts[i]); 
+    }
 
   // ITERATION 3
-  //... your code goes here
-}
+    let prTotal = document.querySelector('#total-value span')
+    prTotal.innerHTML = sum;
+  }
 
 // ITERATION 4
 
@@ -26,7 +42,6 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
-}
 
 // ITERATION 5
 
@@ -37,6 +52,10 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
 });
+// window.addEventListener('load', () => {
+//     const removeProduct = document.getElementById('remove');
+//     removeProduct.addEventListener('click', removeAll);
+// });
+ }
+  //... your code goes here
