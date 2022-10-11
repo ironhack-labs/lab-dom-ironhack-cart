fd213328512f45a37cd0ab674a36367710bd1288
Ricardo Isidro <ricardo.isidro33@gmail.com>
2022-10-11 17:50:42 +0100

    done

--- js/index.js
+++ js/index.js
@@ -2,22 +2,39 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const newPrice =price.innerHTML
+  const newQuantity =quantity.value
+  let calculate= newPrice*newQuantity
 
+  let subTotal = product.querySelector('.subtotal span');
+
+  subTotal.innerText= calculate
+  return calculate
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ /*  const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let sum = 0;
+  let multipleProducts = document.getElementsByClassName('product');
 
+  for(let i=0; i < multipleProducts.length; i++){
+    sum += updateSubtotal(multipleProducts[i])
+  }
+
+  
   // ITERATION 3
   //... your code goes here
+  document.querySelector('#total-value span').innerHTML = sum
 }
 
 // ITERATION 4
@@ -25,7 +42,8 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+   target.parentNode.parentNode.remove()
+   calculateAll();
 }
 
 // ITERATION 5
@@ -38,5 +56,21 @@ window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const removeButtons = document.getElementsByClassName('btn-remove');
+  for(let i=0; i<removeButtons.length; i++){
+    removeButtons[i].addEventListener('click', removeProduct)
+  }
+
   //... your code goes here
 });
+
+
+
+
+
+
+
+
+
+
+
