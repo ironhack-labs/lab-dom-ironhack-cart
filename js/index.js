35c208d66b377366633718efbb2fa07da0e75747
Alex Lino <alejandrolinojaraquemada@gmail.com>
2022-10-23 20:37:21 +0200

    lab

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerText
+  const quantity = product.querySelector('.quantity input').value
+  const subtotal = document.querySelector('.subtotal span')
+  const result = price.innerText * quantity.innerText
+  subtotal.innerText = result
 
-  //... your code goes here
+  return result
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
+
   const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  updateSubtotal(singleProduct)
 
-  // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
