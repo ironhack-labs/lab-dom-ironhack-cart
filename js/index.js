c64f516eb44e2689115c1d45c0f55b9eb7175a45
David Fdez <dav.fdez.lopez@gmail.com>
2022-10-23 20:47:25 +0200

    Everything works!

--- js/index.js
+++ js/index.js
@@ -34,27 +34,67 @@ function calculateAll() {
 function removeProduct(event) {
 
   const target = event.currentTarget;
-
   const deleteGrandParent = target.parentNode.parentNode
 
   deleteGrandParent.parentNode.removeChild(deleteGrandParent)
-
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
-}
+  
+  const tableAccess = document.querySelector('tbody')
+  const newTableRow = tableAccess.insertRow()
+  const newProductCell = newTableRow.insertCell()
+  const newPriceCell = newTableRow.insertCell()
+  const newQuantityCell = newTableRow.insertCell()
+  const newSubtotalCell = newTableRow.insertCell()
+  const newActionCell = newTableRow.insertCell()
+  
+  newTableRow.setAttribute('class', 'product')
+  newProductCell.setAttribute('class', 'name')
+  newPriceCell.setAttribute('class', 'price')
+  newQuantityCell.setAttribute('class', 'quantity')
+  newSubtotalCell.setAttribute('class', 'subtotal')
+  newActionCell.setAttribute('class', 'action')
+  
+  const createProduct = document.querySelector('.create-product')
+  const inputProduct = createProduct.querySelector('input[type="text"]')
+  const newProduct = inputProduct.value
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+  const inputPrice = createProduct.querySelector('input[type="number"]')
+  const newPrice = inputPrice.value
+
+  newProductCell.innerHTML = `<span>${newProduct}</span>`
+  newPriceCell.innerHTML = `$<span>${newPrice}</span>`
+  newQuantityCell.innerHTML = `<input type="number" value="0" min="0" placeholder="Quantity" />`
+  newSubtotalCell.innerHTML = `$<span>0</span>`
+  newActionCell.innerHTML = `<button class="btn btn-remove">Remove</button>`
+
   const removeButton = document.querySelectorAll('.action button')
 
   for (i = 0; i < removeButton.length; i++) {
 
     removeButton[i].onclick = removeProduct
   }
-  //... your code goes here
+}
+
+window.addEventListener('load', () => {
+
+  const calculatePricesBtn = document.getElementById('calculate');
+  calculatePricesBtn.addEventListener('click', calculateAll);
+  
+  const removeButton = document.querySelectorAll('.action button')
+
+  for (i = 0; i < removeButton.length; i++) {
+
+    removeButton[i].onclick = removeProduct
+  }
+  
+  const addProduct = document.querySelectorAll('#create')
+
+  for (i = 0; i < addProduct.length; i++) {
+
+    addProduct[i].onclick = createProduct
+  }
 });
