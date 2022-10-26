f8270263a30b0c242da8970b7b1c266d278be072
Iwona Behnke <behnke.iwona@gmail.com>
2022-10-26 16:02:07 +0200

    Bonuses done

--- js/index.js
+++ js/index.js
@@ -28,18 +28,43 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  target.parentNode.parentNode.remove();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const inputs = document.querySelectorAll(".create-product input")
+  const name = inputs[0].value
+  const unitPrice = inputs[1].value
+  const product = document.createElement("tr")
+  product.className = "product"
+  product.innerHTML = `<td class="name">
+                        <span>${name}</span>
+                      </td>
+                      <td class="price">$<span>${unitPrice}</span></td>
+                      <td class="quantity">
+                        <input type="number" value="0" min="0" placeholder="Quantity" />
+                      </td>
+                      <td class="subtotal">$<span>0</span></td>
+                      <td class="action">
+                        <button class="btn btn-remove">Remove</button>
+                      </td>`
+  document.querySelector("tbody").appendChild(product)
+  product.querySelector(".btn-remove").addEventListener('click', removeProduct);
+
+  return product
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeProductBtns = document.getElementsByClassName("btn-remove");
+  for(let i = 0; i < removeProductBtns.length; i++){
+    removeProductBtns[i].addEventListener('click', removeProduct);
+  }
+
+  const addProductBtn = document.getElementById('create');
+  addProductBtn.addEventListener('click', createProduct);
 });
