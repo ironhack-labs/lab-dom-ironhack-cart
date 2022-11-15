39c7174baf1715b5b4bd560f98291867e351d703
asm-dev <salamanca.m@outlook.com>
2022-11-15 14:23:18 +0100

    iteration 5 --- bonus iteration, createProduct()

--- js/index.js
+++ js/index.js
@@ -35,16 +35,38 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
+
   const target = event.currentTarget;
-  let parent = target.parentNode.parentNode;
-  parent.remove();
+  //let parent = target.parentNode.parentNode; 
+  let parentProduct = target.closest(".product")
+  parentProduct.remove();
   calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  let product, clonedProduct, productsArea;
+  product = document.querySelector('.product');
+  clonedProduct = product.cloneNode(true);
+  productsArea = document.querySelector('#cart > tbody')
+   
+  productsArea.appendChild(clonedProduct);
+
+  //We modify the diff elements of the newly made product, which takes the values of the cloned one
+  //To do this, we use the input values of the create product inputs
+  let newProduct = productsArea.lastChild;
+  newProduct.querySelector('.name > span').innerText = document.querySelector('.create-product input[placeholder="Product Name"]').value;
+  newProduct.querySelector('.price > span').innerText = document.querySelector('.create-product input[placeholder="Product Price"]').value;
+  newProduct.querySelector('.quantity > input').value = 0;
+  newProduct.querySelector('.subtotal > span').innerText = 0;
+  //as well as making sure that the content is "brand new", we ensure that the functionality is what it should
+  newProduct.querySelector('.btn-remove').addEventListener('click', removeProduct);
+
+  //Reset create product inputs so we can create a new product again
+  document.querySelector('.create-product input[placeholder="Product Name"]').value = "";
+  document.querySelector('.create-product input[placeholder="Product Price"]').value = 0;
 }
 
 window.addEventListener('load', () => {
@@ -55,6 +77,7 @@ window.addEventListener('load', () => {
   const removeProductBtn = [...document.getElementsByClassName('btn-remove')];
   removeProductBtn.forEach(function(btn){btn.addEventListener('click', removeProduct)})
 
-
-  //... your code goes here
+  // ITERATION 5
+  const createProductBtn = document.getElementById('create');
+  createProductBtn.addEventListener('click', createProduct)
 });
