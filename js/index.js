78ecd7283e4477967bc845b696a68fae45c4ef34
Daniela Cima <trinchera.dm@gmail.com>
2022-11-15 17:48:26 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
   const price = Number(product.querySelector('.price span').innerText);
-  console.log(price);
+  // console.log(price);
   const quantity = Number(product.querySelector('.quantity input').value);
-  console.log(quantity);
+  // console.log(quantity);
   // let subtotal = product.querySelector('.subtotal span').innerText;
   let subtotal1 = price * quantity;
-  console.log(subtotal1);
+  // console.log(subtotal1);
   product.querySelector('.subtotal span').innerText = subtotal1;
   return subtotal1;
 }
@@ -22,49 +22,83 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
-  const holdSubTotal  = document.getElementsByClassName('product');
-  for (element of holdSubTotal){
+  const subTotalArray = [];
+  const holdSubTotal = document.getElementsByClassName('product');
+  for (element of holdSubTotal) {
     updateSubtotal(element);
-    console.log(holdSubTotal.innerText);
-    // let subTotalArray = document.getElementsByClassName('product')
+    //console.log(holdSubTotal[0].innerHTML);
+    subTotalArray.push(updateSubtotal(element));
+    // console.log(subTotalArray);
     // let totalHolder = subTotalArray.reduce((total, currentValue) => total + currentValue, 0).innerText;
-    }
-    
+  }
   // const subTotalArray = Number([...holdSubTotal].innerText);
-  // console.log(subTotalArray);
-  // 
   // console.log(totalHolder);
   // return totalHolder
-  }
-  // const total = numbers.reduce((accumulator, currentValue) => {
-  //   console.log('accumulator is: ', accumulator, 'and current value is: ', currentValue);
-  //   return accumulator + currentValue;
-  // }, 0)
-// const products = document.getElementsByClassName('product')
-
-
 
   // ITERATION 3
-  //... your code goes here
-
+  let total = document.querySelector('#total-value span');
+  let calculateTotal = 0;
+  for (let i = 0; i < subTotalArray.length; i++) {
+    calculateTotal += Number(subTotalArray[i]);
+  }
+  total.innerHTML = calculateTotal;
+}
+// const total = numbers.reduce((accumulator, currentValue) => {
+//   console.log('accumulator is: ', accumulator, 'and current value is: ', currentValue);
+//   return accumulator + currentValue;
+// }, 0)
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  // console.log('The target in remove is:', target);
+  let parent = document.getElementsByTagName('tbody')[0];
+  parent.removeChild(target.parentNode.parentNode);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  let tablebody = document.querySelector('#cart tbody');
+  const productRow = document.createElement('tr');
+  productRow.setAttribute('class', 'product');
+  const productName = document.querySelector(
+    '.create-product td input[type = text]'
+  );
+  const price = document.querySelector(
+    '.create-product td input[type = number]'
+  );
+  productRow.innerHTML += `<td class="name">
+        <span>${productName.value}</span>
+        </td>
+        <td class="price">$<span>${Number(price.value).toFixed(2)}</span></td>
+        <td class="quantity">
+          <input type="number" value="0" min="0" placeholder="Quantity" />
+        </td>
+        <td class="subtotal">$<span>0</span></td>
+        <td class="action">
+          <button class="btn btn-remove">Remove</button>
+        </td>
+      </tr>
+    `;
+  tablebody.appendChild(productRow);
+  productName.value = '';
+  price.value = '0';
+  productRow
+    .querySelector('.btn-remove')
+    .addEventListener('click', removeProduct);
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtn = document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < removeBtn.length; i++) {
+    removeBtn[i].addEventListener('click', removeProduct);
+  }
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
 });
