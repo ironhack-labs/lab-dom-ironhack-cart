0208e58089e1921f1fa1d99d39977a2d57e6c170
david de la calle <daviddlcalle@gmail.com>
2022-11-16 18:35:38 +0100

    lab completo excepto último bonus

--- js/index.js
+++ js/index.js
@@ -1,42 +1,53 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  let subtotalPrice = price * quantity;
+  let subtotal = product.querySelector('.subtotal');
+  subtotal.innerText = subtotalPrice;
+  return subtotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+  let sum = 0;
+  allProducts.forEach((product) => {
+    sum = sum + updateSubtotal(product);
+  });
 
-  // ITERATION 3
-  //... your code goes here
+  const total = document.getElementById('total-value');
+  total.querySelector('span').innerText = sum;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const parentProduct = target.parentNode.parentNode;
+  parentProduct.remove();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  const deleteBtns = document.querySelectorAll('.btn-remove');
+  deleteBtns.forEach((btn) => btn.addEventListener('click', removeProduct));
+
+  const createProduct = document.getElementById('create');
+  createProduct.addEventListener('click', createProduct);
   //... your code goes here
 });
