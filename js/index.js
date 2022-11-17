70e69be372001316bce52fa982a298345845ab6c
Henrique Mazzoleni <73548364+Henrique-mazzoleni@users.noreply.github.com>
2022-11-17 09:17:11 +0000

    code refactoring and trying out new event  Handling

--- js/index.js
+++ js/index.js
@@ -1,74 +1,58 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
   const price = product.querySelector(".price span").innerHTML
   const quantity = product.querySelector(".quantity input").value
+  const total = price * quantity;
 
-  product.querySelector(".subtotal span").innerHTML = price * quantity;
-  return price * quantity;
+  product.querySelector(".subtotal span").innerHTML = total.toFixed(2);
+  return total;
 }
 
 function calculateAll() {
-  // ITERATION 2
-  const products = document.querySelectorAll('.product')
   let total = 0;
-  products.forEach(product => total += updateSubtotal(product))
 
-  // ITERATION 3
-  const totalDisplay = document.querySelector('#total-value span')
-  totalDisplay.innerHTML = total
+  document.querySelectorAll('.product').forEach(
+    product => total += updateSubtotal(product)
+  )
+  document.querySelector('#total-value span').innerHTML = total.toFixed(2)
 }
 
-// ITERATION 4
-
 function removeProduct(event) {
-  const target = event.currentTarget.parentNode.parentNode;
-  target.parentNode.removeChild(target)
+  event.target.closest('.product').remove()
   calculateAll()
 }
 
-// ITERATION 5
-
 function createProduct(event) {
-  const createElement = event.currentTarget.parentNode.parentNode
+  const createForm = event.target.closest('.create-product')
 
-  const name = createElement.querySelector('[type=text]')
-  const price = createElement.querySelector('[type=number]')
-  
+  const name = createForm.querySelector('[type=text]')
+  const price = createForm.querySelector('[type=number]')
+
   const product = document.createElement('tr')
   product.className = 'product'
   product.innerHTML = `
     <td class="name">
       <span>${name.value}</span>
     </td>
-    <td class="price">$<span>${+price.value}</span></td>
+    <td class="price">$<span>${(+price.value).toFixed(2)}</span></td>
     <td class="quantity">
       <input type="number" value="0" min="0" placeholder="Quantity" />
     </td>
-    <td class="subtotal">$<span>0</span></td>
+    <td class="subtotal">$<span>0.00</span></td>
     <td class="action">
       <button class="btn btn-remove">Remove</button>
     </td>
   `
 
-  const tableBody = document.querySelector('#cart tbody')
-  tableBody.appendChild(product)
-
-  product.querySelector('.btn-remove').addEventListener('click', removeProduct)
+  document.querySelector('#cart tbody').appendChild(product)
 
   name.value = ''
   price.value = ''
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  const removeButtons = document.querySelectorAll('.btn-remove')
-  removeButtons.forEach(
-    button => button.addEventListener('click', removeProduct)
-  )
-
-  const createButton = document.querySelector('#create')
-  createButton.addEventListener('click', createProduct)
+  document.querySelector('body').addEventListener('click', (event) => {
+    if ([...event.target.classList].includes('btn-remove')) removeProduct(event)
+    if (event.target.id === "calculate") calculateAll()
+    if (event.target.id === "create") createProduct(event)
+  })
 });
