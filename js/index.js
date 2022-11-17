95249129e1f6f42594067c886be23f0b91e042f8
Thor Löfgren <lofgrenthor@gmail.com>
2022-11-17 22:44:43 +0100

    Complete iteration 5. Lab solved

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
   const price = +product.querySelector('.price span').innerText;
   const quantity = +product.querySelector('.quantity input').value;
 
@@ -37,7 +35,6 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.target.parentNode.parentNode;
-  console.log('The target in remove is:', target);
 
   document.querySelector('tbody').removeChild(target);
 
@@ -47,8 +44,84 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const productNameInput = document.querySelector(
+    ".create-product input[placeholder='Product Name']"
+  ).value;
+  const productPriceInput = +document.querySelector(
+    ".create-product input[placeholder='Product Price']"
+  ).value;
+
+  const newProduct = configureNewProductElement(
+    productNameInput,
+    productPriceInput
+  );
+
+  document.querySelector('tbody').appendChild(newProduct);
+
+  document.querySelector(
+    ".create-product input[placeholder='Product Name']"
+  ).value = '';
+
+  document.querySelector(
+    ".create-product input[placeholder='Product Price']"
+  ).value = 0;
 }
+
+function configureNewProductElement(productName, productPrice) {
+  const newProductEl = document.createElement('tr');
+  newProductEl.setAttribute('class', 'product');
+  document.querySelector('tbody').appendChild(newProductEl);
+
+  const productNameEl = document.createElement('td');
+  productNameEl.setAttribute('class', 'name');
+  const productNameSpan = document.createElement('span');
+  productNameEl.appendChild(productNameSpan);
+  newProductEl.appendChild(productNameEl);
+
+  const productPriceEl = document.createElement('td');
+  productPriceEl.setAttribute('class', 'price');
+  productPriceEl.innerText = '$';
+  const productPriceSpan = document.createElement('span');
+  productPriceEl.appendChild(productPriceSpan);
+  newProductEl.appendChild(productPriceEl);
+
+  const productQuantityEl = document.createElement('td');
+  productQuantityEl.setAttribute('class', 'quantity');
+  const productQuantityInput = document.createElement('input');
+  productQuantityInput.type = 'number';
+  productQuantityInput.value = 0;
+  productQuantityInput.min = 0;
+  productQuantityInput.placeholder = 'Quantity';
+  productQuantityEl.appendChild(productQuantityInput);
+  newProductEl.appendChild(productQuantityEl);
+
+  const subtotalEl = document.createElement('td');
+  subtotalEl.setAttribute('class', 'subtotal');
+  subtotalEl.innerText = '$';
+  const subtotalSpan = document.createElement('span');
+  subtotalEl.appendChild(subtotalSpan);
+  newProductEl.appendChild(subtotalEl);
+
+  const removeBtnEl = document.createElement('td');
+  const removeBtn = document.createElement('button');
+  removeBtn.setAttribute('class', 'btn btn-remove');
+  removeBtn.textContent = 'Remove';
+  removeBtnEl.appendChild(removeBtn);
+  newProductEl.appendChild(removeBtnEl);
+
+  newProductEl.querySelector('.name span').innerText = productName;
+  newProductEl.querySelector('.price span').innerText = productPrice;
+
+  newProductEl
+    .querySelector('.btn-remove')
+    .addEventListener('click', removeProduct);
+
+  return newProductEl;
+}
+
+const createProductBtn = document.getElementById('create');
+
+createProductBtn.addEventListener('click', createProduct);
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
