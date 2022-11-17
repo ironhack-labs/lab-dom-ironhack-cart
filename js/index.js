9a5ab2b85b57f33eed352be0a5be16a7c6b3b0ab
Nadine Machmeier <info@nadinemachmeier.com>
2022-11-17 17:25:35 +0000

    have to do it again at the weekend - just worked on 1-3

--- js/index.js
+++ js/index.js
@@ -1,25 +1,55 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = document.querySelectorAll('.price span').innerHTML;
+  const quantity = document.querySelectorAll('.quantity input').value;
+  const subtotal = document.querySelectorAll('.subtotal span');
+  
+  const priceNum = Number(price);
+  const quantityNum = Number(quantity)
+  const subtotalNum = priceNum * quantityNum
 
-  //... your code goes here
+  subtotal.innerHTML = subtotalNum
+  
+  console.log(subtotalNum);
+  return subtotalNum;
+// console shows number
 }
 
-function calculateAll() {
+
+  // have to think about this idea once more - I think it is not the right approach for this solution
+  // With this idea I don't get the foreach product sum ....
+  /* const priceProducts = document.getElementsByClassName('product');
+
+  priceProductsArray = [...priceProducts];
+  
+  const priceTotal = priceProductsArray.reduce((accumulator, value) => {
+    return (accumulator + value);
+  }, 0);
+
+  const priceTotalNum = Number(priceTotal);
+  updateSubtotal(priceTotalNum)
+  console.log(priceTotalNum); */
+
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ /*  const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct); */
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  function calculateAll() {
+  let productAll = document.getElementsByClassName('product');
+  let total = 0;
+  
+   productAll.forEach(product => total += updateSubtotal(product));
+
 
   // ITERATION 3
-  //... your code goes here
+  document.getElementById('total-value span').innerHTML = total;
 }
-
+// still needs to be done ---
 // ITERATION 4
 
 function removeProduct(event) {
