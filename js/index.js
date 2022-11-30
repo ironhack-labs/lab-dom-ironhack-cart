78fb339aca94ab6dd32713d42d37bcde7eb8ee7a
Lai In Chang <lavender880331@gmail.com>
2022-12-01 00:35:27 +0100

    finish iteration 1 to 3, stuck at parentNode

--- js/index.js
+++ js/index.js
@@ -9,20 +9,34 @@ function updateSubtotal(product) {
   const subtotal = price * quantity;
   const subtotalElm = product.querySelector('.subtotal span');
   subtotalElm.innerHTML = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const productElm = document.getElementsByClassName('product');
+  const productArr = [...productElm];
+  
+  productArr.map((e) => {
+    updateSubtotal(e);
+  });
 
   // ITERATION 3
   //... your code goes here
+  let sum = 0;
+  productArr.forEach((e) => {
+    sum += updateSubtotal(e);
+  });
+  const total = document.querySelector('#total-value span');
+  total.innerHTML = sum;
+  
 }
 
 // ITERATION 4
@@ -31,6 +45,14 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  const removeBtn = document.querySelectorAll('.btn');
+  removeBtn.forEach( (e) => {
+    e.addEventListener("click", (elm)=> {
+   
+      
+    })
+  }
+  )
 }
 
 // ITERATION 5
