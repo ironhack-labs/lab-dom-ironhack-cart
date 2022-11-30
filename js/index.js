75ed32fd5b3b60dab860bfe6865da94b15c48e84
juliachernik <116951695+juliachernik@users.noreply.github.com>
2022-11-30 22:41:01 +0100

    Finished iterations 1, 2 and 3

--- js/index.js
+++ js/index.js
@@ -1,32 +1,55 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  
+  const quantity = product.querySelector('.quantity input').valueAsNumber;
+  
+  const subtotal = price * quantity;
+
+  const subtotalElm = product.querySelector('.subtotal span');
+  
+  subtotalElm.innerText = subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName('product');
+  const productsArray = [...products];  // convert html collection to an array
+  productsArray.forEach(currentProduct => {
+    updateSubtotal(currentProduct);
+  });
 
   // ITERATION 3
-  //... your code goes here
+  let totalPrice = 0;
+
+  productsArray.forEach(currentProduct => {
+   const price = currentProduct.querySelector('.price span').innerHTML;
+    const quantity = currentProduct.querySelector('.quantity input').valueAsNumber;
+
+    const subtotal = price * quantity;
+
+    totalPrice += subtotal;
+
+  });
+  
+  const totalElm = document.getElementById('total-value');
+  totalElm.innerText = totalPrice;
 }
 
 // ITERATION 4
-
+//parentContainer.removeChild(myElm);
 function removeProduct(event) {
+  debugger;
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
+
+// window.addEventListener('click', (e) => {
+//   const removeBtn = document.getElementsByClassName('btn btn-remove');
+//   removeBtn.addEventListener('click', removeProduct(e));
+// });
 
 // ITERATION 5
 
