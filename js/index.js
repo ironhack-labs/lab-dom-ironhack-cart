cfd0838c28682b5a848f443557a0aa27110fe732
Rumeli Paul <rumelipaul@gmail.com>
2022-11-30 18:45:35 +0100

    Iteration 1 through 3 done. Passing half the tests- Working on Iteration 4

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  const subTotal = price * quantity;
+  const subTotalElm = product.querySelector('.subtotal span')
+  subTotalElm.innerHTML = subTotal;
+
+  console.log(price);
+  return subTotal;
 
   //... your code goes here
 }
@@ -9,15 +16,27 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
+  // ITERATION 2 // ITERATION 3
+  
   //... your code goes here
+  let sum = 0;
 
+  const allProducts = document.querySelectorAll('.product');
+  console.log(allProducts);
+  allProducts.forEach(product =>{
+   sum += updateSubtotal(product);
+  })
   // ITERATION 3
   //... your code goes here
+  
+  const totalElm = document.querySelector('#total-value span');
+  totalElm.innerHTML = sum;
+  
+  
 }
 
 // ITERATION 4
@@ -37,6 +56,7 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  
 
   //... your code goes here
 });
