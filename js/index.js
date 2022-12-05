5a9b42098c00935137dca55f4db79bfda8cd5afa
ficapp2.0 <ficapp2.0@gmail.com>
2022-12-05 23:53:07 +0100

    feat: Bonus 4 & 5

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-  
+
   //... your code goes here
   const price = +product.querySelector('.price span').innerHTML;
   const quantity = +product.querySelector('.quantity input').value;
@@ -21,15 +21,27 @@ function calculateAll() {
 
   // ITERATION 2
   //... your code goes here
-  const products = document.querySelectorAll('.product')
+  // OPCION A
+  // ********
+  // const products = document.querySelectorAll('.product');
   // products.forEach((product) => updateSubtotal(product));
+  // OPCION B
+  // ********
+  const products = [...document.querySelectorAll('.product')]
 
   // ITERATION 3
   //... your code goes here
-  let totalPrice = 0;
-  products.forEach((product) => {
-    totalPrice += updateSubtotal(product);
-  });
+  // OPCION A
+  // ********
+  // let totalPrice = 0;
+  // products.forEach((product) => {
+  //   totalPrice += updateSubtotal(product);
+  // });
+  // OPCION B
+  // ********
+  const totalPrice = products
+    .map((product) => updateSubtotal(product))
+    .reduce((total, value) => total + value, 0)
   document.querySelector('#total-value span').innerHTML = totalPrice.toFixed(2);
 }
 
@@ -37,14 +49,44 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
+  const product = target.parentNode.parentNode;
   console.log('The target in remove is:', target);
   //... your code goes here
+  document.querySelector('#cart tbody').removeChild(product);
+  calculateAll();
 }
 
 // ITERATION 5
 
-function createProduct() {
+function createProduct(event) {
   //... your code goes here
+  const target = event.currentTarget;
+  const product = target.parentNode.parentNode;
+  const productName = product.querySelectorAll('input')[0].value;
+  const productPrice = Number(product.querySelectorAll('input')[1].value).toFixed(2);
+  const newProduct = document.createElement('tr');
+
+  newProduct.setAttribute('class', 'product');
+  newProduct.innerHTML = `
+    <td class="name">
+      <span>${productName}</span>
+    </td>
+    <td class="price">$<span>${productPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `;
+  const removeBtn = newProduct.querySelector('.product .btn.btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+  document.querySelector('#cart tbody').appendChild(newProduct);
+  product.querySelectorAll('input')[0].value = '';
+  product.querySelectorAll('input')[1].value = '';
+
 }
 
 window.addEventListener('load', () => {
@@ -52,4 +94,10 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
+
+  const removeBtns = document.querySelectorAll('.product .btn.btn-remove');
+  removeBtns.forEach((removeBtn) => removeBtn.addEventListener('click', removeProduct));
 });
+
