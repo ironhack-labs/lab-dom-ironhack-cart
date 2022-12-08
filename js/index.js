70c7ed70eb98242273ef32b22d1115ede6b03d7e
Mevine Teelokee <mevine.teelokee@gmail.com>
2022-12-08 07:42:50 +0100

    Lab done(without bonus), bonus iterations will be completed soon

--- js/index.js
+++ js/index.js
@@ -1,23 +1,33 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+  const total = Number(price * quantity);
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerHTML = total;
+  return total;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let allProductChilds = document.getElementsByClassName('product');
+  let finalTotal = 0;
+  for (let i = 0; i < allProductChilds.length; i++) {
+    finalTotal += updateSubtotal(allProductChilds[i]);
+
+    let finalTotalInjection = document.querySelector('#total-value span');
+    finalTotalInjection.innerHTML = finalTotal;
+  }
 
   // ITERATION 3
-  //... your code goes here
+  // DONE AND MERGE IN ITERATION 2
 }
 
 // ITERATION 4
