17148e6ad0b069677873f1ca50a908aaf2de6be7
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2022-12-13 14:23:40 +0100

    in progress

--- js/index.js
+++ js/index.js
@@ -37,19 +37,66 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  let product = target.parentNode.parentNode;
+  let parent = document.querySelector("tbody");
+  console.log(parent);
+  parent.removeChild(product);
+
+  calculateAll();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+  
+  const name = document.querySelector(".create-product>td>input").value ;
+  const price = document.querySelectorAll(".create-product>td>input")[1].value;
+
+  let newProduct = document.getElementsByClassName("product");
+  
+  console.log(newProduct)
+  
+  //document.querySelector("tbody").appendChild(newProduct);  
+
+
+
+  // console.log(newProduct)
+    
+  // newProduct.querySelector("span").innerText = name;
+  // newProduct.querySelector(".price>span").innerText = price;
+  
+//document.body.table.tbody
+//document.querySelector("tbody").appendChild(newProduct);
+
+//newProduct.innerHTML = `<tr class="product"> 
+//   <td class="name">
+//   <span>${name}</span>
+// </td>
+// <td class="price">$<span>${price}</span></td>
+// <td class="quantity">
+//   <input type="number" value="0" min="0" placeholder="Quantity" />
+// </td>
+// <td class="subtotal">$<span>0</span></td>
+// <td class="action">
+//   <button class="btn btn-remove">Remove</button>
+// </td> 
+// <tr>`
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtns = document.getElementsByClassName('btn-remove');
+  for(let i = 0; i < removeBtns.length; i++){
+    removeBtns[i].addEventListener('click', removeProduct);
+  }
+  const create = document.getElementById("create");
+  create.addEventListener('click', createProduct); //Entender.
+
 });
+
 
 
