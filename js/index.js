f118833284237d6c7aefb57f0d5e96524889374e
Globalnet-Iron <jose.iron2023@gmail.com>
2023-01-06 20:02:34 +0100

    change

--- js/index.js
+++ js/index.js
@@ -4,20 +4,25 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = +product.querySelector('.price span').innerHtml;
+  const quantity = +product.querySelector('.quantity input').value;
+  const totalProduct = `${(price * quantity).toFixed(2)}`;
+  const subtotal = product.querySelector('.subtotal span').innerHtml = totalProduct;
+  return +totalProduct;
 }
-
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
   //... your code goes here
-
+  function calculateAll() {  
+    let subTotals = 0
+    const allProducts = document.querySelectorAll('.product')
+    allProducts.forEach(element => {
+      subTotals += updateSubtotal(element)
+    });
   // ITERATION 3
   //... your code goes here
+  const priceTotal = document.querySelector('#total-value span')
+  priceTotal.innerHTML = subTotals
+  return subTotals
 }
 
 // ITERATION 4
