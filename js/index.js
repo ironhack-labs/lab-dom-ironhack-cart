597f5c6206e1a82b49a5224cc06048e949eb1d6d
michael07200 <mickmhtong@gmail.com>
2023-01-10 20:35:19 +0100

    build: bouns Iteration 4

--- js/index.js
+++ js/index.js
@@ -27,9 +27,9 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.target;
+  const productRow = target.parentNode.parentNode;
+  productRow.parentNode.removeChild(productRow);
 }
 
 // ITERATION 5
