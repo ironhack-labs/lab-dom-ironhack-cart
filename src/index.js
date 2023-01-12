64f92313da965097e7bf414e0bc72166ae4e8494
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-01-13 00:07:40 +0100

    iteration 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const value = console.log("Calculating subtotal, yey!")
+  const price = product.querySelector(".price span").innerHTML
+  const quantity = product.querySelector(".quantity input").value
 
-  //... your code goes here
+  let finalPrice = price * quantity
+
+  let subtotal = product.querySelector(".subtotal span")
+
+  subtotal.innerHTML = finalPrice
+  return finalPrice
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const singleProduct = document.querySelector(".product")
+  updateSubtotal(singleProduct)
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let multiProducts = [...document.getElementsByClassName("product")]
+  multiProducts.forEach((element) => updateSubtotal(element))
 
   // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget
+  console.log("The target in remove is:", target)
 }
 
 // ITERATION 5
@@ -34,9 +40,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate")
+  calculatePricesBtn.addEventListener("click", calculateAll)
 
   //... your code goes here
-});
+})
