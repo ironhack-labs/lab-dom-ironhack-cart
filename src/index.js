34c92219bd29d04fdb23c08119548c96adcd167d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-01-18 16:00:32 +0100

    get started

--- src/index.js
+++ src/index.js
@@ -1,42 +1,81 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  "use strict";
+  console.log("Calculating subtotal, yey!");
 
-  //... your code goes here
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  const subtotal = product.querySelector(".subtotal span");
+
+  const priceValue = price.innerHTML;
+  const quantityValue = quantity.value;
+
+  const subtotalValue = priceValue * quantityValue;
+
+  subtotal.innerHTML = subtotalValue;
+
+  return subtotalValue;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  "use strict";
+  const allProducts = document.querySelectorAll(".product");
 
-  // ITERATION 2
-  //... your code goes here
+  let arrayAllProducts = [...allProducts];
 
-  // ITERATION 3
-  //... your code goes here
+  let total = arrayAllProducts.map(updateSubtotal).reduce((acc, curr) => acc + curr, 0);
+
+  document.querySelector("#total-value span").innerHTML = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
+  "use strict";
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const parent = target.parentNode.parentNode;
+  const table = parent.parentNode;
+
+  table.removeChild(parent);
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  "use strict";
+  const name = document.querySelector(".create-product  input[type='text']").value;
+  const price = document.querySelector(".create-product  input[type='number']").value;
+
+  console.log(price, name);
+
+  const newProductHTML = `<tr class="product">
+          <td class="name">
+            <span>${name}</span>
+          </td>
+          <td class="price">$<span>${price}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+        </tr>`;
+
+  document.querySelector("tbody tr:last-child").insertAdjacentHTML("afterend", newProductHTML);
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  "use strict";
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
-  //... your code goes here
+  const removeProductBtn = document.getElementsByClassName("btn-remove");
+  for (let item of removeProductBtn) {
+    item.addEventListener("click", removeProduct);
+  }
+
+  const createProductBtn = document.getElementById("create");
+  createProductBtn.addEventListener("click", createProduct);
 });
