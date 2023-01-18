ac8b1ba4540e072f81541ea9bd11c545ebb693d9
HenrikH <henrik.hellem@live.com>
2023-01-18 17:49:50 +0100

    Started on iteration 4

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ function updateSubtotal(product) {
 
   let subTotal = product.querySelector(".subtotal span");
   subTotal.innerHTML = price * quantity;
-  
-  console.log(subTotal)
-  return subTotal
+  return subTotal;
 }
 function calculateAll() {
   const productCollection = document.getElementsByClassName("product");
 
-  let total = 0
-  for (let i = 0; i <= productCollection.length-1; i++) {
+  let total = 0;
+  for (let i = 0; i <= productCollection.length - 1; i++) {
     total += +updateSubtotal(productCollection[i]).innerHTML;
   }
-  document.getElementById("total-value").innerText = `Total:\$${total.toFixed(2)}`
+  document.getElementById("total-value").innerText = `Total:\$${total.toFixed(
+    2
+  )}`;
 }
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log("The target in remove is:", target);
-  //... your code goes here
+  //... your code goes here 
 }
 
 // ITERATION 5
@@ -37,5 +37,9 @@ window.addEventListener("load", () => {
   const calculatePricesBtn = document.getElementById("calculate");
   calculatePricesBtn.addEventListener("click", calculateAll);
 
-  //... your code goes here
+
+
+  const removePoductBtn = document.getElementById("btn-remove");
+  removePoductBtn.addEventListener("click", removeProduct());
+
 });
