b25ec5e885087b5167a0df1a6c388ba6fb5caf18
Luke List <luke.list@gmail.com>
2023-01-18 17:57:24 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,23 +1,72 @@
 // ITERATION 1
 
+/*Step 1: Inside updateSubtotal(), create two variables
+(we suggest naming them price and quantity)*/
+
 function updateSubtotal(product) {
+  const price = product.querySelector('.price span').innerHTML;
+
+  console.log(price);
+
+  const quantity = product.querySelector('.quantity input').value;
+
+  console.log(quantity);
+
   console.log('Calculating subtotal, yey!');
 
+  
+  /*Step 3: Use the values you extracted from the above-mentioned
+  DOM elements to calculate the subtotal price.
+  You can create a new variable, and its value will be equal
+  to the product of these values.
+  Ex. If a user picked 3 Ironhack Rubber Ducks,
+  they should see that the subtotal is 75 dollars ($25 * 3 = $75). */
+
   //... your code goes here
+
+  let subtotal = quantity * price
+  let newSubtotal = product.querySelector('.subtotal span');
+  newSubtotal.innerText = subtotal;
+  return subtotal;
 }
 
+/* Now, when you are becoming a DOM manipulation ninja,
+use your skills once again to get the DOM element
+that should hold the subtotal.
+Hint: it is the element with the class subtotal. */
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
   //... your code goes here
 
+let products = document.querySelectorAll('.product')
+ 
+let totalValue = 0;
+console.log(products);
+products.forEach(product => totalValue += updateSubtotal(product))
+console.log(totalValue);
+
   // ITERATION 3
-  //... your code goes here
+
+let grandTotal = document.querySelector('#total-value span'); 
+grandTotal.innerText = totalValue;
+console.log(grandTotal);
+return totalValue;
+
+/*  newSubtotal.innerText = subtotal;
+  return subtotal; */
+
+/*function calculateAll() {
+    let total = 0;
+    let allProducts = document.querySelectorAll('.product')
+      allProducts.forEach(product => {
+    total += updateSubtotal(product)
+      })
+    let displayTotal = document.querySelector('#total-value span')
+    displayTotal.innerText = total
+    return total
+   } */
 }
 
 // ITERATION 4
