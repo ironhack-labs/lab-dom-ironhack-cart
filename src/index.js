cf4d624602fbc050abf03a4b7aaf3bf891de493f
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-01-21 16:28:31 +0100

    NaN

--- src/index.js
+++ src/index.js
@@ -4,20 +4,59 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+
+  const price = product.querySelector('.price span')
+
+  const quantity = product.querySelector('.quantity input').value
+
+  const subtotal = product.querySelector('.subtotal span')
+
+  const productPrice = price.innerHTML
+
+  const productSubtotal = quantity * productPrice
+
+  productSubtotal = subtotal.innerHTML
+
+
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+
   // end of test
+
 
   // ITERATION 2
   //... your code goes here
 
+  const severalProducts = product.getElementsByClassName('.price span')
+
+
+
+
+  // const suma = 0
+
+  // for (let i = 0; i < severalProducts; i++) {
+  //   suma += severalProducts[i]
+  // }
+
+  console.log(suma)
+
+  updateSubtotal(severalProducts)
+
+
+
+
+
   // ITERATION 3
   //... your code goes here
+
+
+
 }
 
 // ITERATION 4
