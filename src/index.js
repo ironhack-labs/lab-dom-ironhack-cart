ae200ad27961728eb500ade50f15b659730e5347
Cristina Gutiérrez <cgutierrezgm@gmail.com>
2023-01-22 23:50:46 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -1,32 +1,61 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+
+  //console.log('Calculating subtotal, yey!')
 
   //... your code goes here
+  const price = product.querySelector('.price span')
+
+  const quantity = product.querySelector('.quantity input')
+
+  const newSubtotal = price.innerText * quantity.value
+
+  const subtotal = product.querySelector('.subtotal span')
+  subtotal.innerText = newSubtotal
+
+  return newSubtotal
 }
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product')
+  //updateSubtotal(singleProduct)
   // end of test
+
 
   // ITERATION 2
   //... your code goes here
 
+  let newTotal = 0
+
+  document.querySelectorAll('.product').forEach(eachProduct => {
+    newTotal += updateSubtotal(eachProduct)
+  })
+
+
+
   // ITERATION 3
   //... your code goes here
+
+  const total = document.querySelector('#total-value span')
+  total.innerText = newTotal
+
 }
+
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  const target = event.currentTarget
+  console.log('The target in remove is:', target)
   //... your code goes here
+
+
 }
+
 
 // ITERATION 5
 
@@ -35,8 +64,8 @@ function createProduct() {
 }
 
 window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+  const calculatePricesBtn = document.getElementById('calculate')
+  calculatePricesBtn.addEventListener('click', calculateAll)
 
   //... your code goes here
-});
+})
