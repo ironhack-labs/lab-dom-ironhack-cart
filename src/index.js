cda4928f8ceaa0f5e3c6971641a8635e167dc343
CarolineKuhn <carolinekuhn2@hotmail.com>
2023-01-24 18:32:03 +0000

    Solved

--- src/index.js
+++ src/index.js
@@ -3,29 +3,42 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
-}
+  const price = product.querySelector('.price span')//.innerHTML;
+  const quantity = product.querySelector('.quantity input')//.value;
 
+ // const price1 = price.innerHTML;
+  let subTotalPrice = price.innerHTML * quantity.value;
+  let subtotal = product.querySelector('.subtotal span');
+  //subTotalPrice.innerHTML = subtotal;
+  return subtotal.innerHTML=subTotalPrice;
+  };
+  
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
+ /*// code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
-  // end of test
+  // end of test */
 
   // ITERATION 2
-  //... your code goes here
+let fullPrice = document.getElementsByClassName('product');
+let total = 0;
+for (let i = 0; i < fullPrice.length; i++) {
+  total += updateSubtotal(fullPrice[i]); 
+}
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector("#total-value span").innerHTML = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log('The target in remove is:', target.parentNode.parentNode);
+  target.parentNode.parentNode.remove() 
+ 
+  
 }
 
 // ITERATION 5
@@ -39,4 +52,9 @@ window.addEventListener('load', () => {
   calculatePricesBtn.addEventListener('click', calculateAll);
 
   //... your code goes here
+
+  const removedProduct = document.querySelectorAll(“.btn-remove”);
+  removedProduct.forEach(product => product.addEventListener(‘click’, removeProduct));
 });
+
+
