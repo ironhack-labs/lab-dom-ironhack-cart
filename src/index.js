a0ac5e75522f749d2f41f2370426bdeb66e8218a
miacsilva <miacsilvac@hotmail.com>
2023-01-25 00:20:14 +0000

    lab solved

--- src/index.js
+++ src/index.js
@@ -3,40 +3,81 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML       //store in "price" the value of price
+  const quantity = product.querySelector('.quantity input').value   //store in "quantity" the value of quantity
+  let subtotal = price * quantity                                  //calculate subtotal
+  let subTT = product.querySelector('.subtotal span')
+  subTT.innerHTML = subtotal
+  return subtotal
 }
 
+
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+ let trProducts = document.getElementsByClassName('product')
+ let sum=0
+ for(let i=0; i<trProducts.length; i++){
+    updateSubtotal(trProducts[i])
+    
+    sum+= updateSubtotal(trProducts[i])
+  }
 
-  // ITERATION 3
-  //... your code goes here
+  let total = document.querySelector('#total-value span')
+  total.innerHTML= sum
+  return total
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
+  const target = event.currentTarget
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const grandParentDiv = target.parentNode.parentNode.parentNode
+  grandParentDiv.removeChild(target.parentNode.parentNode)
+
+  calculateAll()
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  let newProduct = document.createElement('tr');
+
+  let newProductInput = document.querySelectorAll('.create-product input');
+
+  let newProductName = newProductInput[0].value
+
+  let newProductPrice = newProductInput[1].value
+
+  newProduct.innerHTML = `<td class="name">
+  <span>${newProductName}</span>
+</td>
+<td class="price">$<span>${newProductPrice}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>`
+
+  newProduct.classList.add('product')
+  newProduct.querySelector('button').addEventListener('click', removeProduct)
+  let cartTable = document.querySelector('tbody')
+  cartTable.appendChild(newProduct)
 }
+
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  let removeB = document.querySelectorAll('.btn-remove')
+  for(let i=0; i<removeB.length; i++){
+    removeB[i].addEventListener("click", removeProduct)
+  }
 });
