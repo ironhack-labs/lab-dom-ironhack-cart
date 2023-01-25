702bcf517aadca41067b633314b51c1cfec414af
jdluis <jdluisdev@gmail.com>
2023-01-25 09:01:38 +0000

    final

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span').innerText;
-  const quantity = product.querySelector('.quantity input').value;
-  let subtotal = product.querySelector('.subtotal span');
+  console.log("Calculating subtotal, yey!");
+  const price = product.querySelector(".price span").innerText;
+  const quantity = product.querySelector(".quantity input").value;
+  let subtotal = product.querySelector(".subtotal span");
 
   subtotal.innerText = price * quantity;
 
   return subtotal.innerText;
 }
 
-
 function calculateAll() {
   const products = document.querySelectorAll("#cart .product");
   let totalPrice = document.querySelector("#total-value span");
 
-  totalPrice.innerText = 0; //reset the value before re calculated
-
+  totalPrice.innerText = 0; //reset the value before re-calculated
   products.forEach((product) => {
-    totalPrice.innerText = Number(totalPrice.innerText) + Number(updateSubtotal(product));
-  })
+    totalPrice.innerText =
+      Number(totalPrice.innerText) + Number(updateSubtotal(product));
+  });
 
   return totalPrice;
 }
-
-// ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
@@ -33,12 +28,14 @@ function removeProduct(event) {
   row.remove();
 }
 
-// ITERATION 5
-
 function createProduct() {
-  const nameInput = document.querySelector('.create-product input[type="text"]').value
-  const unitPrice = document.querySelector('.create-product input[type="number"]').value
-  const tbodyDOM = document.querySelector('#cart tbody');
+  const nameInput = document.querySelector(
+    '.create-product input[type="text"]'
+  ).value;
+  const unitPrice = document.querySelector(
+    '.create-product input[type="number"]'
+  ).value;
+  const tbodyDOM = document.querySelector("#cart tbody");
 
   let template = `
     <tr class="product">
@@ -56,28 +53,21 @@ function createProduct() {
     </tr>
   `;
   tbodyDOM.innerHTML += template;
-  reloalRemoveEvents ();
+  reloalRemoveEvents();
 }
 
-function reloalRemoveEvents () {
+function reloalRemoveEvents() {
   const removesBtn = document.querySelectorAll(".btn-remove");
-  console.log(removesBtn)
+  console.log(removesBtn);
   removesBtn.forEach((element) => {
     element.addEventListener("click", removeProduct);
-  })
+  });
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
-  //... your code goes here
-
-/*   const removesBtn = document.querySelectorAll(".btn-remove");
-  removesBtn.forEach((element) => {
-    element.addEventListener("click", removeProduct);
-  }) */
-
-  const addProduct = document.querySelector('#create');
-  addProduct.addEventListener('click', createProduct);
+  const addProduct = document.querySelector("#create");
+  addProduct.addEventListener("click", createProduct);
 });
