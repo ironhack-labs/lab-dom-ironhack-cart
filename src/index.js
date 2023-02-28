4453ae3a324e19cddf1ee965275ea9d11006b8a8
Max Strandberg <maax.strandberg@gmail.com>
2023-02-28 23:50:31 +0100

    I like that you give us bonuses to do even if i never do them

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector(`.price span`);
+  const quantity = product.querySelector(`.quantity input`).value;
+  const totPrice = price.innerHTML * quantity;
+  product.querySelector(`.subtotal span`).innerText = totPrice;
+  let subTotal =  product.querySelector(`.subtotal span`).innerText;
+  
+  
+  
+  return subTotal;
+ 
+
 
   //... your code goes here
 }
@@ -9,11 +20,23 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
+  let total = 0;
+  let products = [...document.getElementsByClassName("product")];
+  for(let i=0 ;i< products.length; i++){
+    
+    total = total + parseFloat(updateSubtotal(products[i]));
+  
+  };
+  //document.getElementById(`total-value`).innerHTML = total
+
+  document.querySelector(`#total-value span`).innerHTML = total;
+//console.log(total);
+
   //... your code goes here
 
   // ITERATION 3
