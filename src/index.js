9660d1382f9b5343161242895f299f1ca847ab94
Canacepa <cassandre.lavallard@gmail.com>
2023-03-01 18:41:44 +0100

    Lab solved
    Co-authored-by: Cristian Palao <Dookietrukie@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-// ITERATION 1
+  // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const priceElm = product.querySelector('.price span');
+  const quantityElm = product.querySelector('.quantity input');
+  const subtotalElm = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  const priceValue = priceElm.innerHTML;
+  const quantityValue = quantityElm.value;
+
+  const subquantityValue = priceValue * quantityValue;
+
+  subtotalElm.innerHTML = subquantityValue;
+
+  return subquantityValue;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // ITERATION 2 / 3
 
-  // ITERATION 2
-  //... your code goes here
+  const productElm = document.getElementsByClassName('product');
+  const totalProdValueElm = document.querySelector('#total-value span');
+  let totalProductsPrice = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  for(let i = 0; i < productElm.length; i++) {
+    const currentProduct = productElm[i];
+    const currentTotal = updateSubtotal(currentProduct);
+    totalProductsPrice += currentTotal;
+  }
+
+  totalProdValueElm.innerHTML = totalProductsPrice;
 }
 
 // ITERATION 4
