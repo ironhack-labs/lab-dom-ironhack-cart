afc819d381b329127d81776d33bcfb6743b76498
Chris Weber <johndoe@example.com>
2023-03-01 19:28:19 +0100

    did it push

--- src/index.js
+++ src/index.js
@@ -2,22 +2,35 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = parseFloat(product.querySelector('.price span').innerText);
+  const quantity = parseInt(product.querySelector('.quantity input').value);
+  const subtotal = price * quantity;
+  product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
+  const singleProduct = document.querySelector(".product");
   updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let product1 = document.querySelector(".product");
+  let product2 = document.querySelector(".product2");
+  const subtotal1 = updateSubtotal(product1);
+  const subtotal2 = updateSubtotal(product2);
+
 
   // ITERATION 3
-  //... your code goes here
+      let Total = subtotal1 + subtotal2;
+      document.querySelector("#total-value span").innerText = Total;
+   
+
+
+
+
 }
 
 // ITERATION 4
@@ -25,7 +38,8 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
+  
 }
 
 // ITERATION 5
