0376ffe46a3bfe6a5df2e2d034e8049cb8e39308
Canacepa <cassandre.lavallard@gmail.com>
2023-03-01 20:03:26 +0100

    Cassandre bonus

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-  // ITERATION 1
+// ITERATION 1
 
 function updateSubtotal(product) {
-  const priceElm = product.querySelector('.price span');
-  const quantityElm = product.querySelector('.quantity input');
-  const subtotalElm = product.querySelector('.subtotal span');
+  const priceElm = product.querySelector(".price span");
+  const quantityElm = product.querySelector(".quantity input");
+  const subtotalElm = product.querySelector(".subtotal span");
 
   const priceValue = priceElm.innerHTML;
   const quantityValue = quantityElm.value;
@@ -15,14 +15,13 @@ function updateSubtotal(product) {
   return subquantityValue;
 }
 
+// ITERATION 2 / 3
 function calculateAll() {
-  // ITERATION 2 / 3
-
-  const productElm = document.getElementsByClassName('product');
-  const totalProdValueElm = document.querySelector('#total-value span');
+  const productElm = document.getElementsByClassName("product");
+  const totalProdValueElm = document.querySelector("#total-value span");
   let totalProductsPrice = 0;
 
-  for(let i = 0; i < productElm.length; i++) {
+  for (let i = 0; i < productElm.length; i++) {
     const currentProduct = productElm[i];
     const currentTotal = updateSubtotal(currentProduct);
     totalProductsPrice += currentTotal;
@@ -35,19 +34,48 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const elmRemove = target.parentNode.parentNode;
+  if (elmRemove.parentNode) {
+    elmRemove.parentNode.removeChild(elmRemove);
+  }
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const newProd = document.querySelectorAll(".create-product input");
+  const prodName = newProd[0].value;
+  const prodPrice = newProd[1].value;
+  console.log(prodName);
+  console.log(prodPrice);
+
+  const insertProd = document.querySelector('tbody')
+  insertProd.innerHTML += 
+  `<tr class="product">
+          <td class="name">
+            <span>${prodName}</span>
+          </td>
+          <td class="price">$<span>${prodPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+        </tr>`;
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
-  //... your code goes here
+  const removeButtons = document.querySelectorAll(".btn.btn-remove");
+  removeButtons.forEach((element) => {
+    element.addEventListener("click", removeProduct);
+  });
+
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", createProduct);
 });
