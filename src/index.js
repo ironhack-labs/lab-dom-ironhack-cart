15175081632fee94839ad1681e3f6c82c20bb4ee
Rohan Singh <rsingh@faruse.com>
2023-03-02 18:03:27 +0100

    done

--- src/index.js
+++ src/index.js
@@ -3,21 +3,41 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  const priceValue = parseFloat(price.innerHTML);
+  const quantityValue = parseFloat(quantity.value);
+  const subtotal = priceValue * quantityValue;
+  const subtotalElement = product.querySelector('.subtotal span');
+  subtotalElement.innerHTML = subtotal;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  //const singleProduct = document.querySelector('.product');
+
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const allProducts = document.querySelectorAll('#cart .product');
+  allProducts.forEach((product)=> {
+    updateSubtotal(product)
+  })
 
   // ITERATION 3
-  //... your code goes here
+  const products = document.querySelectorAll('.product');
+  let total = 0;
+
+  products.forEach((product => {
+    const subtotal = updateSubtotal(product);
+    total += subtotal;
+  }))
+  const totalValue = document.querySelector('total-value span')
+  totalValue.textContent = total;
 }
 
 // ITERATION 4
@@ -25,18 +45,4 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
 }
-
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
