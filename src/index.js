763a3ebea8c77655abfc9c965ccd3c96d1043e39
Wiebke Lueneburg <git@fraulueneburg.de>
2023-03-02 17:25:07 +0100

    bugfix:sum added up if button clicked more than 1x

--- src/index.js
+++ src/index.js
@@ -11,17 +11,10 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // -- start of test --
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  // -- end of test --
-
   // ITERATION 2
   // ITERATION 3
   let products = document.getElementsByClassName('product');
-  let totalPrice = +document.querySelector('#total-value span').textContent;
+  let totalPrice = 0;
   let totalPriceField = document.querySelector('#total-value span');
   for (let i=0; i < products.length; i++) {
     updateSubtotal(products[i]);
@@ -31,12 +24,10 @@ function calculateAll() {
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
   target.closest(".product").remove();
 }
-
 
 // ITERATION 5
 
