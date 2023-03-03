61e212c69e1e8a6250c00de2c2836a109e498b0d
Wiebke Lueneburg <git@fraulueneburg.de>
2023-03-03 09:44:49 +0100

    added comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  const price = +product.querySelector('.price span').textContent;
-  const quantity = +product.querySelector('.quantity input').value;
+  const price = +product.querySelector('.price span').textContent;    // "+" converts this value from a string to a number
+  const quantity = +product.querySelector('.quantity input').value;   // "+" converts this value from a string to a number
   const subtotalField = product.querySelector('.subtotal span');
   const subtotal = price * quantity;
   subtotalField.innerHTML = subtotal.toFixed(2);
-
   return subtotal;
 }
 
+// ITERATION 2
+// ITERATION 3
+// - create an array of all products (elements with .product class)
+// - create variables for the total price and the field that needs to be updated with it
+// - loop through the products array and run the updateSubtotal() function on each product
+// - after the loop, update the total price field
 function calculateAll() {
-  // ITERATION 2
-  // ITERATION 3
   let products = document.getElementsByClassName('product');
   let totalPrice = 0;
   let totalPriceField = document.querySelector('#total-value span');
@@ -24,6 +26,9 @@ function calculateAll() {
 }
 
 // ITERATION 4
+// - create a const of the element (button) that was clicked
+// - remove the element with the .product class that is closest to the clicked button
+// - recalculate the sum
 function removeProduct(event) {
   const target = event.currentTarget;
   target.closest('.product').remove();
@@ -31,6 +36,9 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
+// - get the name and price for the new product
+// - create the HTML for the new product, inserting the name and price
+// - add this HTML to the end of the table body
 function createProduct(event) {
   const target = event.currentTarget;
   const tbody = document.querySelector('tbody');
@@ -44,6 +52,10 @@ function createProduct(event) {
   tbody.appendChild(newProductRow);
 }
 
+// EVENT LISTENERS
+// - clicks on button #calculate
+// - clicks on buttons .remove
+// - clicks on button #create
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
