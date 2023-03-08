3df1e5be72feab1ee378010d0d33d31b551eb7e5
José Antonio Escudero García <jaeg90@gmail.com>
2023-03-08 21:32:03 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -45,6 +45,45 @@ function createProduct() {
     '[placeholder="Product Price"]'
   ).value;
   console.log("Product created");
+
+  const product = `
+  <tr class="product">
+          <td class="name">
+            <span>${productName}</span>
+          </td>
+          <td class="price">$<span>${productPrice}</span></td>
+          <td class="quantity">
+            <input type="number" value="0" min="0" placeholder="Quantity" />
+          </td>
+          <td class="subtotal">$<span>0</span></td>
+          <td class="action">
+            <button class="btn btn-remove">Remove</button>
+          </td>
+        </tr>
+  
+  `;
+  let newTr = document.createElement(`tr`);
+  newTr.setAttribute("class", "product");
+  newTr.innerHTML = product;
+  let table = document.getElementsByTagName("tbody")[0];
+  table.appendChild(newTr);
+  eventListener();
+}
+
+function eventListener() {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
+
+  //... your code goes here
+  const removeBtn = document.getElementsByClassName("action");
+  for (let i = 0; i < removeBtn.length; i++) {
+    removeBtn[i].addEventListener("click", removeProduct);
+    removeBtn[i].addEventListener("click", calculateAll);
+  }
+
+  //create button
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", createProduct);
 }
 
 window.addEventListener("load", () => {
