341b7279eb8fc1d2f75237c76fd7516d86ec11aa
Moriah Askenaizer <moriahaskenaizer@gmail.com>
2023-03-29 20:45:04 +0200

    lab progress

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector ('.quantity input').value;
+  const subTotal =  product.querySelector('.subtotal span') ;
+  console.log(price)
+  console.log(quantity)
+ 
+  let calculateTotal = price * quantity;
+  console.log(calculateTotal)
+  subTotal.textContent = calculateTotal
+  
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
 }
 
 function calculateAll() {
+products = document.getElementByClassName("product")
+products[0].classList.product.add("product")
   // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // // it runs when only iteration 1 is completed. at later point, it can be removed.
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // // end of test
 
   // ITERATION 2
   //... your code goes here
