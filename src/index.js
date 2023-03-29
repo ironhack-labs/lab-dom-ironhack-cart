5784d62e7d8685bf0ce51ef5f0fc620078cf4517
Silvia Flare <silviaguizan@gmail.com>
2023-03-29 22:54:15 +0200

    iteration 5 solved, two ways

--- src/index.js
+++ src/index.js
@@ -11,14 +11,12 @@ function updateSubtotal(product) {
   const priceValue = price.innerText;
   const quantityValue = quantity.value;
 
-  console.log(priceValue, quantityValue);
-
   const subtotalPrice = parseInt(priceValue) * parseInt(quantityValue);
   const subtotalDom = product.querySelector(".subtotal span");
 
   subtotalDom.innerText = subtotalPrice;
 
-  console.log(subtotalPrice);
+  console.log({ subtotalPrice, priceValue, quantityValue });
   return subtotalPrice;
 }
 
@@ -61,14 +59,7 @@ function removeProduct(event) {
 
 // ITERATION 5
 
-function createProduct() {
-  // - Create tr class(product)
-  // - td class(name) - input to span
-  // - td class(price) - input to $ + span
-  // - td class(quantity) - same as top
-  // - td class(subtotal) - same as top
-  // - td class(create) to remove class(action)
-
+function createProduct2() {
   const listProducts = document.querySelector("tbody");
 
   const newProduct = document.createElement("tr");
@@ -76,7 +67,7 @@ function createProduct() {
   listProducts.appendChild(newProduct);
 
   const createProductName = document.createElement("td");
-  newProduct.classList.add("name");
+  createProductName.classList.add("name");
   newProduct.appendChild(createProductName);
   const spanName = document.createElement("span");
   const inputSpam = document.querySelector("tfoot input");
@@ -85,33 +76,42 @@ function createProduct() {
   createProductName.appendChild(spanName);
 
   const createPrice = document.createElement("td");
-  newProduct.classList.add("price");
+  createPrice.classList.add("price");
   newProduct.appendChild(createPrice);
-  const spanPrice = document.createElement("span");
-  const inputSpamPrice = document.querySelector("tfoot input"[2]); // comorrr????
+  const inputSpamPrice = document.querySelectorAll("tfoot input")[1].value;
+  createPrice.innerHTML = `$<span>${inputSpamPrice}</span>`;
+  /* const spanPrice = document.createElement("span");
+  
   const nameValuePrice = inputSpamPrice.value;
-  spanPrice.innerText = nameValuePrice;
-  createPrice.appendChild(spanPrice);
+  spanPrice.innerText = `$${nameValuePrice}.00`;
+  createPrice.appendChild(spanPrice); */
 
   const createQuantity = document.createElement("td");
-  newProduct.classList.add("quantity");
+  createQuantity.classList.add("quantity");
   newProduct.appendChild(createQuantity);
   const inputQuantity = document.createElement("input");
+  inputQuantity.setAttribute("value", "0");
   inputQuantity.type = "number";
-  inputQuantity.placeholder = "0";
   createQuantity.appendChild(inputQuantity);
 
   const createSubtotal = document.createElement("td");
-  newProduct.classList.add("subtotal");
+  createSubtotal.classList.add("subtotal");
   newProduct.appendChild(createSubtotal);
-  const nullSubtotal = document.createElement("span");
-  const preTextSubtotal = "$" + "0"; // comorrr????
-  createSubtotal.appendChild(preTextSubtotal);
+  createSubtotal.innerHTML = `$<span>0</span>`;
+  /* const nullSubtotal = document.createElement("span");
+  nullSubtotal.innerText = "0";
+
+  createSubtotal.appendChild("$");
+  createSubtotal.appendChild(nullSubtotal); */
 
   const createRemoveButton = document.createElement("td");
-  newProduct.classList.add("action");
+  createRemoveButton.classList.add("action");
   newProduct.appendChild(createRemoveButton);
-  // comorrr????
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("btn", "btn-remove");
+  removeButton.innerText = "Remove";
+  createRemoveButton.appendChild(removeButton);
 
   console.log(
     newProduct,
@@ -122,6 +122,29 @@ function createProduct() {
     createRemoveButton,
     listProducts
   );
+}
+
+function createProduct() {
+  const listProducts = document.querySelector("tbody");
+
+  const newProduct = document.createElement("tr");
+  newProduct.classList.add("product");
+  listProducts.appendChild(newProduct);
+
+  const title = document.querySelector("tfoot input").value;
+  const price = document.querySelectorAll("tfoot input")[1].value;
+
+  newProduct.innerHTML = `<td class="name">
+  <span>${title}</span>
+</td>
+<td class="price">$<span>${price}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>`;
 }
 
 // Event Listeners -----------------------------------------
