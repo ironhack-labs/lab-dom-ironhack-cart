bf14590a5013e00661a3b44b5f1e202b87537bb7
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-03-29 17:02:05 +0200

    Lab without Bonus

--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotal = product.querySelector('.subtotal span');
 
-  //... your code goes here
+  let newSubtotal = price * quantity;
+  subtotal.textContent = newSubtotal;
+
+  return newSubtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  /*const singleProduct = document.querySelector('.product');
+  updateSubtotal(singleProduct);*/
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let total = 0;
+
+  const allProducts = document.querySelectorAll('.product');
+  allProducts.forEach((product) => {
+    total += updateSubtotal(product);
+  })
 
   // ITERATION 3
-  //... your code goes here
+  const totalPrice = document.querySelector('#total-value span');
+
+  totalPrice.textContent = total;
 }
 
 // ITERATION 4
