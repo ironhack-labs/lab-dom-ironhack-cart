74e652ee85871d4fcf0bf77d5a253ce88d1dab57
Kristof R <kristofrombauts@me.com>
2023-03-29 16:02:38 +0200

    Iteration 4 done

--- src/index.js
+++ src/index.js
@@ -40,8 +40,11 @@ function calculateAll() {
 // ITERATION 4
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const parent = event.target.parentNode.parentNode
+  parent.remove(event.target)
+  
+  calculateAll(target)
 }
 
 // ITERATION 5
@@ -50,9 +53,16 @@ function createProduct() {
   //... your code goes here
 }
 
+
+
+
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtn = document.querySelectorAll('.btn-remove');
+
+  removeBtn.forEach(button => {
+    button.addEventListener('click', removeProduct);
+  })
 });
