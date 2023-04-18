e75385c958347e17686e506699253bc846ba5e37
Ricozzo <henriqueortizpereira@gmail.com>
2023-04-18 16:58:50 +0100

    [LIS-APR-2023] Henrique Ortiz

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  let price = product.querySelector(".price span").innerText;
+  let quantity = product.querySelector(".quantity input").value;
+  
+  let subtotalText = product.querySelector(".subtotal span")
+  let subtotal = price * quantity;
+  subtotalText.innerText = subtotal;
 
-  //... your code goes here
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+let allProducts = document.getElementsByClassName("product");
+let sumTotal = 0
+for (let i = 0; i < allProducts.length; i++){
+  updateSubtotal(allProducts[i]);
+  sumTotal += updateSubtotal(allProducts[i]);
+}
+let totalAll = document.querySelector("#total-value span");
+totalAll.innerHTML= sumTotal;
 }
 
 // ITERATION 4
@@ -31,12 +33,23 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  //dar target tabela
+  let newProduct = document.getElementsByTagName("tbody")[0];
+
+  //criar uma nova table row
+
+  //ir buscar os valores que o user ecreveu nos inputs 
+
+  //target nova table row .innerHTML
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  let button = document.getElementById("calculate");
+  button.onclick = function(){
+    window.alert('Calculate prices clicked');
+  };
 });
