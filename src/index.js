283727e6a6e1db309b130eb57b53fc9cf4d80d74
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-04-18 16:38:47 +0100

    Worst Lab So Far Done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  console.log("Calculating subtotal, yey!");
 
   //... your code goes here
+  let price = product.querySelector(".price span");
+  let quantity = product.getElementsByTagName("input")[0];
+
+
+  let total = price.innerHTML * quantity.value;
+
+  let subTotal = product.querySelector(".subtotal span");
+
+  subTotal.innerHTML = total.toString();
+
+  return total;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // const singleProduct = document.querySelector('.product');
+
+  // updateSubtotal(singleProduct);
 
   // ITERATION 2
-  //... your code goes here
+
+  let products = document.getElementsByClassName("product");
+
+  let totalValue = 0;
+  
+  for (let i = 0; i<products.length; i++) {
+    totalValue += updateSubtotal(products[i]);
+  };
+
 
   // ITERATION 3
-  //... your code goes here
+  
+  let total = document.querySelector("#total-value span");
+  total.innerHTML = totalValue;
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
