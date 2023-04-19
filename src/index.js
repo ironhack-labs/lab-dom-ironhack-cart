186d8238f0fd5b3fba8d22654cdf82bd6f81c859
Elnaz Fe <elnaz.farrokhi1988@gmail.com>
2023-04-19 09:01:06 +0100

    week2day2

--- src/index.js
+++ src/index.js
@@ -2,30 +2,64 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+    //... your code goes here
+    const price = product.querySelector('.price span');
+    const quantity = product.querySelector('.quantity input');
+    const subtotal = product.querySelector('.subtotal span');
+  
+    const priceValue = price.innerHTML;
+    const quantityValue = quantity.value;
+  
+    let subtotalValue = priceValue * quantityValue;
+    subtotal.innerHTML = subtotalValue;
+    
+    return subtotalValue;
 }
 
+
+/*
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
+*/  
 
-  // ITERATION 2
+// ITERATION 2
   //... your code goes here
 
-  // ITERATION 3
+// ITERATION 3
   //... your code goes here
+
+  function calculateAll() {
+
+    const productRows = document.getElementsByClassName('product');
+  
+    let total = 0;
+    for (let i = 0; i < productRows.length; i++) {
+      const subtotal = updateSubtotal(productRows[i]);
+      total += subtotal;
+    };
+
+  const result = document.querySelector("#total-value span")
+  result.innerHTML = total.toString();  
 }
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  console.log('The target to remove is:', target.parentNode.parentNode); //parentNode is to move up in the tree.
+  target.parentNode.parentNode.remove();
+}
+
+const removeButton = document.getElementsByClassName('btn-remove');
+for(let i = 0; i < removeButton.length; i++) {
+  removeButton[i];
+  removeButton[i].addEventListener('click', removeProduct);
 }
 
 // ITERATION 5
