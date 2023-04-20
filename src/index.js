fcdebba0c56ab61fcc5d8ef1a401a090ba78d4c9
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-04-20 17:31:18 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -3,19 +3,38 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  //Accessing html elements that contain the price & quantity values
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+
+  // String the price & quantity values in a new variable
+  const quantityValue = quantity.value;
+  const priceValue = price.innerHTML;
+
+  // Calculating subtotal
+  const subTotal = priceValue * quantityValue;
+
+  // Updating subtotal in html
+  product.querySelector(".subtotal span").innerHTML = subTotal;
+
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName("product");
+  for (let i = 0; i<products.length; i++) {
+    updateSubtotal(products[i]);
 
+    console.log("product", products[i]);
+  }
+  
   // ITERATION 3
   //... your code goes here
 }
