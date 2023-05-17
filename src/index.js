c366bbd8de749143694c6679919bbb3b1fa76c8e
linhvnde <linh.nh.tyty@gmail.com>
2023-05-18 01:08:02 +0200

    added code for iteration 5 however, the remove button doesnt work in new row

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  const price = product.querySelector('.price span');
+  console.log("Calculating subtotal, yey!");
+  const price = product.querySelector(".price span");
   const priceVal = Number(price.innerText);
-  const quantity = product.querySelector('.quantity input');
-  const quantityVal = Number(quantity.value)
+  const quantity = product.querySelector(".quantity input");
+  const quantityVal = Number(quantity.value);
   const subtotalCalc = priceVal * quantityVal;
-  const subtotal = product.querySelector('.subtotal span');
-  subtotal.innerText = subtotalCalc
+  const subtotal = product.querySelector(".subtotal span");
+  subtotal.innerText = subtotalCalc;
   return subtotal.innerText;
-  }
- 
+}
+
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
@@ -20,34 +20,32 @@ function calculateAll() {
   // end of test
 
   // ITERATION 2
-const allRows = document.querySelectorAll('.product')
-console.log(allRows)
+  const allRows = document.querySelectorAll(".product");
+  console.log(allRows);
 
-let total = 0
+  let total = 0;
 
-allRows.forEach((product) => {
-  const result = updateSubtotal(product);
-  total += Number(result)
-})
+  allRows.forEach((product) => {
+    const result = updateSubtotal(product);
+    total += Number(result);
+  });
 
   // ITERATION 3
 
-const showTotalValue = document.querySelector('#total-value');
-showTotalValue.innerText = `$${total}`;
+  const showTotalValue = document.querySelector("#total-value");
+  showTotalValue.innerText = `$${total}`;
 
+  // Tried that one below but it didn't worked:
 
-// Tried that one below but it didn't worked:
+  // let allSubtotals = document.querySelectorAll('.product .subtotal')
+  // console.log(allSubtotals)
 
-// let allSubtotals = document.querySelectorAll('.product .subtotal')
-// console.log(allSubtotals)
+  // allSubtotals.reduce((acc, subtotal) =>{
+  //   console.log(subtotal.innerText)
+  //   console.log(acc)
+  //   // acc + Number(subtotal.innerText);
 
-
-// allSubtotals.reduce((acc, subtotal) =>{
-//   console.log(subtotal.innerText)
-//   console.log(acc)
-//   // acc + Number(subtotal.innerText);
-
-// },0);
+  // },0);
 
   // return totalPrice
 }
@@ -64,7 +62,37 @@ function removeProduct(event) {
 
 // ITERATION 5
 
-function createProduct() {}
+function createProduct() {
+  const createProductInputs = document.querySelectorAll(
+    ".create-product input"
+  );
+  const nameInput = createProductInputs[0];
+  const priceInput = createProductInputs[1];
+
+  const productName = nameInput.value;
+  const unitPrice = priceInput.value;
+  const newRow = document.createElement("tr");
+  newRow.classList.add("product");
+
+  newRow.innerHTML = `
+    <td class="name">
+      <span>${productName}</span>
+    </td>
+    <td class="price">$<span>${unitPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `;
+  const cart = document.querySelector("#cart tbody");
+  cart.appendChild(newRow);
+  nameInput.value = "";
+  priceInput.value = "";
+  
+}
 
 window.addEventListener("load", () => {
   const calculatePricesBtn = document.getElementById("calculate");
