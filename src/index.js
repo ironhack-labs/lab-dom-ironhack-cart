b72aff7997a2c250ce18e14616cb68d25e9155e5
Chiara888 <carolin@family-klose.de>
2023-05-17 16:41:00 +0200

    intial push

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function updateSubtotal(product) {
 
   //... your code goes here
 }
-
+ 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
