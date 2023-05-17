a408518609f5e417c377782b1bde47a47d016492
Mandy Sieks <mandy.sieks@hotmail.com>
2023-05-17 21:59:22 +0200

    add Iteration 1-3; second try

--- src/index.js
+++ src/index.js
@@ -1,41 +1,96 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  const price = product.querySelector('.price span');
-  const quantity = product.querySelector('.quantity input');
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+
   const priceValue = Number(price.innerText);
-  const quantityValue = Number(quantity.value);
-  const subtotal = product.querySelector('.subtotal span')
-  subtotal.innerText = (priceValue * quantityValue)
-  return priceValue * quantityValue
+  const quantityValue = Number(quantity.value); // it only worked after changing quanity.innerText to quanity.value
+  const subtotalValue = quantityValue * priceValue; // .value is important to access the value in the input field...
+
+  const subtotal = product.querySelector(".subtotal span");
+  subtotal.innerText = subtotalValue.toFixed(2);
+
+  return subtotalValue;
 }
+
+
+
+
+
+
+
+// Iteration 2 
+// I had the following errors: 
+
+/*dex.js:4 Uncaught TypeError: product.querySelector is not a function
+    at updateSubtotal (index.js:4:25)
+    at index.js:26:5
+    at NodeList.forEach (<anonymous>)
+    at HTMLButtonElement.calculateAll (index.js:25:15)*/
+
+// the lines doesn't fit anymore, but it went away after putting "allProducts" into the arrow-function inside the forEach function
 
 function calculateAll() {
-
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  // const singleProduct = document.querySelector('.product');
-  // updateSubtotal(singleProduct);
-  // // end of test
+
+  // const allProducts = document.getElementsByClassName("product");
+  //don't know why, but it doesn't work
+
+  const allProducts = document.querySelectorAll(".product");
+  allProducts.forEach((allProducts) => {
+    updateSubtotal(allProducts);
+  });
+
+  //const singleProduct = document.querySelector('.product');
+
+  // end of test
 
 
-let totalValue = 0
-  const allProducts = document.querySelectorAll('.product')
-  allProducts.forEach((eachProduct) => {
-  totalValue += updateSubtotal(eachProduct) 
-  })
-const total = document.querySelector('#total-value span');
-total.innerText = totalValue
+/*const totalPrice = document.getElementsByClassName("calculate-total");
+totalPrice = */
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ITERATION 3
+  //... your code goes here
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log(target)
-  console.log('The target in remove is:', target);
-  target.closest('tr').remove()
-
+  console.log("The target in remove is:", target);
+  //... your code goes here
 }
 
 // ITERATION 5
@@ -44,13 +99,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-  const removeButtons = document.querySelectorAll('.btn-remove');
-  removeButtons.forEach((element) => {
-    element.addEventListener('click', removeProduct);
-  })
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
   //... your code goes here
 });
