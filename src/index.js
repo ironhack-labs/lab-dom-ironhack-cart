db956948b76b6374a9805abc1ac89b8d0a9fdab2
Viktor N <viktor.nestorov@web.de>
2023-05-18 05:05:12 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -3,30 +3,59 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').textContent; 
+  const quantity = product.querySelector('.quantity input').value; 
+  const subtotal = price * quantity; 
+  product.querySelector('.subtotal span').textContent = subtotal; 
+  return subtotal; 
 }
+
+// ITERATION 2
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
+    // end of test
+    const allProducts = document.getElementsByClassName('product'); 
+    let total = 0; 
+    for (let product of allProducts) {
+    total += updateSubtotal(product); 
+    }
+  document.querySelector('#total-value span').textContent = isNaN(total) ? 0 : total.toFixed(2);
+}
+  
+const calculateBtn = document.getElementById('calculate');
+calculateBtn.addEventListener('click', calculateAll);
 
   // ITERATION 3
-  //... your code goes here
+
+  function updateSubtotal(product) {
+    const price = product.querySelector('.price span').textContent; 
+    const quantity = product.querySelector('.quantity input').value; 
+    const subtotal = price * quantity; 
+    product.querySelector('.subtotal span').textContent = subtotal;
+    
 }
+  
+  
+
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const product = target.parentNode.parentNode;
+  product.parentNode.removeChild(product);
+  calculateAll();
 }
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  for (let button of removeBtn) {
+  button.addEventListener('click', removeProduct);
+ }
+
 
 // ITERATION 5
 
