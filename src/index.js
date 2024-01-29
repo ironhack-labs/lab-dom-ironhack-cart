2671d11518c91d7da8affb0582087a480d5bec56
Eber Brown <eaba_89@hotmail.com>
2024-01-29 16:21:28 +0100

    iteration 2 js code for lab-dom-ironhack-cart CalculateAll part1 function

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  let quantity = document.querySelector(".quantity input").value;
-  let price = document.querySelector(".price span").innerHTML;
+  let quantity = product.querySelector(".quantity input").value;
+  let price = product.querySelector(".price span").innerHTML;
+  let subTotalEl = product.querySelector(".subtotal span");
+
   let subTotal = quantity * price;
-  console.log(subTotal);
-
-  let subTotalEl = document.querySelector(".subtotal span");
-
   subTotalEl.innerHTML = subTotal;
+
   return subTotal;
-  //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
-
+  let products = document.querySelectorAll(".product");
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    let currentProduct = products[i];
+    total += updateSubtotal(currentProduct);
+  }
   // ITERATION 3
+  console.log(total);
   //... your code goes here
 }
 
