7f584586edde6e1c4c8f67d3026a6206ce028216
Adrià Chalaux <adriachalaux@gmail.com>
2024-01-30 20:55:23 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let totalPrices = []
+const cart = document.querySelector('#cart tbody')
 
 // ITERATION 1
 
@@ -52,7 +53,6 @@ function removeProduct(event) {
   const target = event.currentTarget;
   // console.log('The target in remove is:', target);
   
-  const cart = document.querySelector('#cart tbody')
   const elementToRemove = target.parentNode.parentNode
 
   cart.removeChild(elementToRemove) // Eliminamos el elemento
@@ -62,15 +62,43 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  let productName = document.getElementById('new-name')
+  let productPrice = document.getElementById('new-price')
+
+  let newRow = document.createElement("tr")
+  newRow.classList.add('product')
+  newRow.innerHTML = `<td class="name">
+    <span>${productName.value}</span>
+  </td>
+  <td class="price">$<span>${parseFloat(productPrice.value).toFixed(2)}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button class="btn btn-remove">Remove</button>
+  </td>`
+  cart.appendChild(newRow)
+  
+  console.log(productName.value, parseFloat(productPrice.value))
+
+  // Resetear valores
+  productName.value = ""
+  productPrice.value = ""
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
+  // Delete buttons events
   const deleteBtns = document.querySelectorAll('.btn-remove');
   deleteBtns.forEach(btn => {
     btn.addEventListener('click', removeProduct)
   })
+
+  // Create product event
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
 });
