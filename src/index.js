e50848f9d393489477529c7017d7a257a80bde61
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-02-03 12:55:53 +0100

    first exercise merged with second

--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  //  console.log('Calculating subtotal, yey!');
+  
+ /* const price = product.querySelector('.price span').innerText;
+  // is this use of .innerWHATEVS correct?
 
-  //... your code goes here
+  let quantity = document.querySelector('.quantity input').value;
+
+  let subtotal = price * quantity
+
+  console.log(subtotal)
+
+  product.querySelector('.subtotal span').innerHTML = subtotal
+
+  return subtotal
+  
+/*  let subtotal = product.querySelector('.subtotal span')
+
+  subtotal.innerHTML = subtotalCalculation */
+
+  const price = product.querySelector('.price span');
+
+  const priceValue = price.innerText;
+
+  let quantity = product.querySelector('.quantity input');
+
+  let quantityValue = quantity.value;
+
+  let subtotal = product.querySelector('.subtotal span')
+
+  let subtotalValue = priceValue * quantityValue
+  
+  subtotal.innerText = subtotalValue
+
+  return subtotalValue
+  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+// const singleProduct = document.querySelector('.product');
+// updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+/* let products = [...document.getElementsByClassName("product")];
+
+products.forEach(function(product){
+const price = product.querySelector('.price span').innerText;
+console.log(price)
+})
+
+let quantity = document.querySelectorAll('.quantity input');
+
+quantity.forEach(input => {
+  console.log(input.value)
+}) */
+
+
+// ITERATION 3
+const product = document.querySelectorAll(".product")
+
+let total = 0
+
+for (let i = 0; i < product.length; i++) {
+  let singleProduct = product[i]
+//  console.log(updateSubtotal(singleProduct))
+  total = total + updateSubtotal(singleProduct)
 }
+
+//console.log(total)
+}
+
 
 // ITERATION 4
 
