d9deb0e35ed6e19b1b7eb0ba69f8df493ce26799
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-02-03 15:22:38 +0100

    clean code and add comments

--- src/index.js
+++ src/index.js
@@ -1,58 +1,41 @@
 // ITERATION 1
+
 function updateSubtotal(product) {
-  //  console.log('Calculating subtotal, yey!');
   
+  // define a price variable that stores the text content of the <span> tag inside the price class of each product
   const price = product.querySelector('.price span').textContent;
-
+  // define a quantity variable that stores the value of the <input> tag inside the quantity class for each product
   const quantity = product.querySelector('.quantity input').value;
-
+  // Define a subtotal variable that calculates the subtotal by multiplying the price by the quantity
   const subtotal = price * quantity
-
+  // Write the subtotal inside <span> tag of the subtotal class for the product
   product.querySelector('.subtotal span').innerHTML = subtotal
-
+  // return subtotal
   return subtotal
-  
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-// const singleProduct = document.querySelector('.product');
-// updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
+// ITERATION 2
 
-/* let products = [...document.getElementsByClassName("product")];
-
-products.forEach(function(product){
-const price = product.querySelector('.price span').innerText;
-console.log(price)
-})
-
-let quantity = document.querySelectorAll('.quantity input');
-
-quantity.forEach(input => {
-  console.log(input.value)
-}) */
-
+// Leaving empty because the ITERATION 1 already does the job of calculating the subtotal for all
 
 // ITERATION 3
+
+// Create a variable to store the content of products that are found inside the document using the class .product 
 const product = document.querySelectorAll(".product")
-
+// Define a total variable to store total and set it to 0
 let total = 0
-
+// Create a loop that goes through each occurrence of the previous product variable
 for (let i = 0; i < product.length; i++) {
   let singleProduct = product[i]
+// Calculate total by summing all the subtotals obtained with the for loop and calling the function that calculates the subtotal
   total = total + updateSubtotal(singleProduct)
 }
-
-console.log(total)
-
-let totalCart = document.querySelector('#total-value span')
-
+// Create variable that retrieves the <span> tag where the total is hosted
+const totalCart = document.querySelector('#total-value span')
+// Write the total amount as innerHTML of the <span> retrieved one line above
 totalCart.innerHTML = total
-
 }
 
 
