38807b508f265df61a5e277140b65946fcdb9e1b
Anna <ania.pwk.ap@gmail.com>
2024-02-04 22:10:46 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,42 +1,77 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+	console.log("Calculating subtotal, yey!");
 
-  //... your code goes here
+	//step 1 and 2
+	const price = Number(product.querySelector(".price span").innerHTML); //you should convert the price to a numeric value
+	const quantity = parseInt(product.querySelector(".quantity input").value); // .value returns a string
+
+	// step 3
+	const totalPrice = price * quantity;
+
+	// step 4. get the DOM element that should hold the subtotal.
+	const subtotalElement = product.querySelector(".subtotal span");
+
+	// step 5. set the total price to the corresponding DOM element
+	subtotalElement.innerHTML = totalPrice.toFixed(2); // Display subtotal with two decimal places
+
+	return totalPrice;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+	//TEST ON 1 ELEMENT get the first DOM element with the class product (row)
+	// const singleProduct = document.querySelector('.product');
+	// singleProduct is passed as an argument to the updateSubtotal() function.
+	// updateSubtotal(singleProduct);
 
-  // ITERATION 2
-  //... your code goes here
+	// ITERATION 2
+	// get all products
+	const products = document.getElementsByClassName("product");
 
-  // ITERATION 3
-  //... your code goes here
+	let totalPrice = 0;
+	for (let i = 0; i < products.length; i++) {
+		let currentProduct = products[i];
+		totalPrice = totalPrice + updateSubtotal(currentProduct);
+	}
+	// ITERATION 3
+	let totalElemPrice = document.getElementById("total-value");
+	totalElemPrice.innerHTML = totalPrice.toFixed(2);
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+	// gets the element on which the event was initially fired
+	const target = event.currentTarget;
+	// moves up two levels in the DOM hierarchy to
+	// reach the grandparent node of the clicked button
+	const targetParent = target.parentNode.parentNode;
+	// removes the entire row (parent node of the clicked button) from DOM
+	targetParent.removeChild(target.parentNode);
+	calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+	//... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+	// got the element with the id="calculate"
+	const calculatePricesBtn = document.getElementById("calculate");
 
-  //... your code goes here
+	// added an event listener click to it
+	calculatePricesBtn.addEventListener("click", calculateAll);
+	// This button will fire the calculateAll() function when clicked
+
+	//selects all buttons inside the class "action".
+	let productsButtonRemove = document.querySelectorAll(".action");
+	for (let i = 0; i < productsButtonRemove.length; i++) {
+		productsButtonRemove[i].addEventListener("click", removeProduct);
+	}
+	// alternative
+	// const productsButtonRemove = document.querySelectorAll(".action");
+	// productsButtonRemove.forEach((button) =>
+	// 	button.addEventListener("click", removeProduct));
 });
